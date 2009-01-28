6f12be503e66b835b37a427e2ec82264239bc71c
Anton Bondarev <anton.bondarev1978@gmail.com>
2009-01-28 15:56:38 +0000

    Delete printing from test_availability_irq

--- src/tests/test_availability_irq.c
+++ src/tests/test_availability_irq.c
@@ -9,7 +9,7 @@
 #include "timers.h"
 #include "test_availability_irq.h"
 
-volatile static BOOL tick_happened;
+volatile static BOOL tick_happened ;
 
 static void test_timer_handler(){
 	tick_happened = TRUE;
@@ -18,15 +18,19 @@ static void test_timer_handler(){
 int test_availability_irq() {
 	UINT32 id,ticks;
 	long i;
-	TRACE ("Testing availability_irq...   ");
+	//TRACE ("Testing availability_irq...   ");
 	id = 23;
 	ticks = 2;
 	// (timer value changes means ok)
+	tick_happened = FALSE;
 	set_timer(id, ticks,test_timer_handler);
 	for (i = 0 ;i<(2<<20);i++){
-		if (tick_happened) TRACE ("OK\n");
-		else TRACE ("failed\n");
+		if (tick_happened)
+			break;
 	}
 	close_timer(id);
+	//if (TRUE == tick_happened)
+	//	TRACE ("OK\n");
+	//else TRACE ("failed\n");
 	return 0;
 }
