b6dd46672244a026277dd83b82540d3d27881df2
Anton Bondarev <anton.bondarev1978@gmail.com>
2009-02-16 16:52:15 +0000

    [No commit log message]

--- src/conio/scanf.c
+++ src/conio/scanf.c
@@ -22,17 +22,18 @@ static void ungetchar(int ch) {
 
 static int scanchar(char **str) {
 	extern int getchar();
+	int ch;
 	if (str) {
-		/* **str = getchar();
-		++(*str);
-		return **str;*/
-		int ch;
-		ch = str[0][0];
-		++(*str);
+		ch = **str;
+		*str ++;
 		return ch;
 
 	} else {
-		return getchar();
+		if ('\r' == (ch = getchar()))
+		{
+			return EOF;
+		}
+		return ch;
 	}
 }
 BOOL is_space(int ch) {
@@ -147,9 +148,9 @@ static int scan(char **in, const char *fmt, va_list args) {
 				char *dst = va_arg ( args, char* );
 				int ch;
 
-				trim_leading(in);//???
+				//trim_leading(in);
 
-				while (EOF != (ch = scanchar(in)) && !is_space(ch) && widht--)
+				while (EOF != (ch = scanchar(in)) && widht--)
 					*dst++ = (char) ch;
 				*dst = '\0';
 
@@ -159,10 +160,10 @@ static int scan(char **in, const char *fmt, va_list args) {
 			case 'c':{
 				int dst;
 
-				trim_leading(in);
+				//trim_leading(in);
 
 				dst = scanchar(in);
-				va_arg ( args, char) = dst;
+				*va_arg ( args, char*) = dst;
 				++converted;
 
 			}
@@ -170,10 +171,10 @@ static int scan(char **in, const char *fmt, va_list args) {
 			case 'd': {
 				int dst;
 				dst = scan_int(in,10,widht);
-				//printf ("d%d\n", dst);
-				va_arg ( args, int ) = dst;
-				//printf ("va_arg 0x%X\n", args);
-				//printf ("va_arg 0x%X\n", &args);
+
+				*va_arg ( args, int* ) = dst;
+
+
 				++converted;
 			}
 				continue;
@@ -187,8 +188,9 @@ static int scan(char **in, const char *fmt, va_list args) {
 			case 'o': {
 				int dst;
 				dst = scan_int(in,8,widht);
-				//printf ("o%d\n", dst);
-				va_arg ( args, int ) = dst;
+
+				*va_arg ( args, int* ) = dst;
+
 				++converted;
 			}
 				continue;
@@ -202,8 +204,8 @@ static int scan(char **in, const char *fmt, va_list args) {
 			case 'x': {
 				int dst;
 				dst = scan_int(in,16,widht);
-				//printf ("x%d\n", dst);
-				va_arg ( args, int ) = dst;
+				*va_arg ( args, int* ) = dst;
+
 				++converted;
 			}
 				continue;
@@ -228,8 +230,6 @@ int scanf(const char *format, ...) {
 	int rv;
 
 	va_start ( args, format );
-	//printf ("scan addr 0x%X\n", args);
-	//printf ("scan addr 0x%X\n", format);
 	rv = scan( 0,format, args );
 	va_end ( args );
 
@@ -240,6 +240,7 @@ int sscanf(char *out, const char *format, ...)
  {
 	va_list args;
 	int rv;
+
 	va_start ( args, format );
 	rv = scan( &out,format, args );
 	va_end ( args );
