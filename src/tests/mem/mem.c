0efab53e43b1edb291a7fc1f105a2dfeb272e273
Alexey Fomin <fomin.alexey@gmail.com>
2009-02-19 17:00:35 +0000

    mem: Code review

--- src/tests/mem/mem.c
+++ src/tests/mem/mem.c
@@ -10,14 +10,14 @@
 
 char mem_keys[] = {
 #include "mem.inc"
-};
+		};
 
 void print_memory(int *addr, int amount) {
 	int i = 0, j;
 	while (i < amount) {
-		printf("0x%08x:\t", (int)(addr+i));
+		printf("0x%08x:\t", (int) (addr + i));
 		for (j = 0; j < 4; j++) {
-			printf("%08x\t", *(addr+i));
+			printf("%08x\t", *(addr + i));
 			i++;
 		}
 		printf("\n");
@@ -27,21 +27,18 @@ void print_memory(int *addr, int amount) {
 
 int mem_shell_handler(int argsc, char **argsv) {
 	SHELL_KEY arguments[MAX_SHELL_KEYS];
-	int keys_amount = parse_arg(argsc, argsv, arguments);
 	int i;
 	char *key_value;
+	int keys_amount;
 
-	if (!check_keys(arguments, keys_amount, mem_keys, sizeof(mem_keys))) {
-		printf("ERROR: mem: incorrect key entered! See help.");
+	keys_amount = parse_arg("mem", argsc, argsv, mem_keys, sizeof(mem_keys), arguments);
+
+
+	if (keys_amount < 0) {
 		return -1;
 	}
-
-	//	for (i = 0; i < keys_amount; i++) {
-//		printf("\t%c - %s\n", arguments[i].name, arguments[i].value);
-//	}
 
 	if (get_key('d', arguments, sizeof(arguments), &key_value)) {
 		print_memory(0, 50);
 	}
 }
-
