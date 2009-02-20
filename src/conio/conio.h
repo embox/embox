18d78ca43c0fce8c59bba06ebf53bb033602c039
Alexandr Batyukov <batsun@gmail.com>
2009-02-20 09:26:38 +0000

    Comments added

--- src/conio/conio.h
+++ src/conio/conio.h
@@ -6,9 +6,19 @@
 int printf (const char *str, ...);
 
 int sprintf(char *out, const char *format, ...);
+
 // scanf from standart output
+// keys: %[value]s - string (value == length)
+//		 %c		   - char
+//		 %[value]d - decimal (value == length)
+//		 %[value]o - oct
+//		 %[value]x - hex
+// returns amount of converted entries
 int scanf (const char *str, ...);
 
+// sscanf from string
+// keys: see above
+// returns amount of converted entries
 int sscanf (char *out, const char *format, ...);
 //
 void back_cursor (int count);
