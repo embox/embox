71a52c3219eb4093b88429e34144d546d7b015f3
Anton Bondarev <anton.bondarev1978@gmail.com>
2009-03-03 15:56:15 +0000

    Some refactor help for monitor

--- src/tests/help/help.c
+++ src/tests/help/help.c
@@ -4,16 +4,25 @@
  *  Created on: 02.03.2009
  *      Author: abatyukov
  */
-#include "shell.h"
 #include "types.h"
-#include "common.h"
-
+#include "conio.h"
 #include "shell.h"
+#include "help.h"
 
 
-char help_keys[] = {
+
+
+
+static char help_keys[] = {
 #include "help_keys.inc"
 };
+
+static void show_help()
+{
+	printf(
+#include "help_help.inc"
+	);
+}
 
 int help_shell_handler(int argsc, char **argsv) {
 	SHELL_KEY keys[MAX_SHELL_KEYS];
@@ -25,24 +34,20 @@ int help_shell_handler(int argsc, char **argsv) {
 	keys_amount = parse_arg("help", argsc, argsv, help_keys, sizeof(help_keys),
 				keys);
 
-	if (keys_amount < 0) {
-		printf(
-#include "help_help.inc"
-		);
+	if (keys_amount < 1) {
+		show_help();
 		return -1;
 	}
 
 	if (get_key('h', keys, keys_amount, &key_value)) {
-			printf(
-	#include "help_help.inc"
-			);
+			show_help();
 			return 0;
 		}
 
 	//need to display all possible commands
-	printf("Available commands are: \n");
+	printf("Available commands: \n");
 	for (i = 0; i < sizeof(shell_handlers) / sizeof(shell_handlers[0]); i++) {
-		printf("\t%10s\t%60s\n", shell_handlers[i].name, shell_handlers[i].description);
+		printf("%s\t\t%s\n", shell_handlers[i].name, shell_handlers[i].description);
 		}
 
 	return 0;
