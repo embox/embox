78969d01526a7da4c8303147b20e676e6af140da
Alexey Fomin <fomin.alexey@gmail.com>
2009-03-03 16:22:46 +0000

    Some refactoring in help
    see help.c

--- src/tests/help/help.c
+++ src/tests/help/help.c
@@ -9,16 +9,11 @@
 #include "shell.h"
 #include "help.h"
 
-
-
-
-
 static char help_keys[] = {
 #include "help_keys.inc"
-};
+		};
 
-static void show_help()
-{
+static void show_help() {
 	printf(
 #include "help_help.inc"
 	);
@@ -32,23 +27,35 @@ int help_shell_handler(int argsc, char **argsv) {
 	int i;
 
 	keys_amount = parse_arg("help", argsc, argsv, help_keys, sizeof(help_keys),
-				keys);
+			keys);
 
-	if (keys_amount < 1) {
+//	 old:
+//		if (keys_amount < 1) {
+//
+//	 on "help" without arguments show all commands
+//	 old version never shows commands!
+//	 .Fomka
+
+	if (keys_amount < 0) {
+		// Error state:
 		show_help();
 		return -1;
 	}
 
 	if (get_key('h', keys, keys_amount, &key_value)) {
-			show_help();
-			return 0;
-		}
+		show_help();
+		return 0;
+	}
 
 	//need to display all possible commands
 	printf("Available commands: \n");
 	for (i = 0; i < sizeof(shell_handlers) / sizeof(shell_handlers[0]); i++) {
-		printf("%s\t\t%s\n", shell_handlers[i].name, shell_handlers[i].description);
-		}
+//		old:
+//		printf("%s\t\t%s\n", shell_handlers[i].name, shell_handlers[i].description);
+//		 Put " - " in help, not in every single description
+//		 .Fomka
+		printf("%s\t\t - %s\n", shell_handlers[i].name, shell_handlers[i].description);
+	}
 
 	return 0;
 }
