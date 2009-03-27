548e35f8d13b92d1a05f24b4be67a053bac62354
Alexey Fomin <fomin.alexey@gmail.com>
2009-03-27 16:33:10 +0000

    Stub for DHCP created

---
+++ src/net/dhcp.c
@@ -0,0 +1,14 @@
+/*
+ * dhcp.c
+ *
+ *  Created on: 27.03.2009
+ *      Author: fomka
+ */
+
+#include "types.h"
+#include "dhcp.h"
+
+int dhcpDiscover() {
+	DHCP_PACKET packet;
+
+}
---
+++ src/net/dhcp.h
@@ -0,0 +1,61 @@
+/*
+ * dhcp.h
+ *
+ *  Created on: 27.03.2009
+ *      Author: fomka
+ */
+
+#ifndef DHCP_H_
+#define DHCP_H_
+
+#define BOOTREQUEST 0x1 // not sure
+#define BOOTREPLY 0x2   // not sure
+
+#include "types.h"
+
+int dhcpDiscover();
+
+typedef struct {
+	/* Message type
+	 * 2 possible values:
+	 * BOOTREQUEST (from client to server)
+	 * BOOTREPLY (from server to client)
+	 */
+	BYTE op;
+
+	BYTE htype;
+	BYTE hlen;
+
+	BYTE hops;
+	BYTE xid;
+
+
+	HWRD secs;
+	HWRD flags;
+
+	WORD ciaddr;
+	WORD yiaddr;
+	WORD siaddr;
+	WORD giaddr;
+
+	/* hardware address
+	 * Regularly MAC-address
+	 */
+	WORD chaddr[4];
+	/* Server name. (optional)
+	 * Zero-ending string
+	 */
+	char sname[64];
+
+	/* File name (for remote access)
+	 * Zero-ending string.
+	 */
+	char file[128];
+
+	/* Other options (optional)
+	 * we don't use
+	 */
+	// _type options;
+} DHCP_PACKET;
+
+#endif /* DHCP_H_ */
