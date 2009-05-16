c1f17acaab246fe83ab35f24eb52471fe3bc9b20
Alexandr Batyukov <batsun@gmail.com>
2009-05-16 12:56:35 +0000

    TFTP header added

---
+++ src/net/tftp.h
@@ -0,0 +1,53 @@
+/**
+ * \file tftp.h
+ *
+ * \date May 16, 2009
+ * \author sunnya
+ */
+
+#ifndef TFTP_H_
+#define TFTP_H_
+
+#define TFTP_PPQ	0x1
+#define TFTP_WRQ	0x2
+#define TFTP_DATA	0x3
+#define TFTP_ACK	0x4
+#define TFTP_ERR	0x5
+
+#include "types.h"
+
+typedef struct {
+	// Message type
+	HWRD op;
+	// File name. Zero ending string.
+	char filename[64];
+	// Mode. Zero ending string.
+	char mode[64];
+}TFTP_RQ_PACKET;
+
+typedef struct {
+	// Message type
+	HWRD op;
+	// Block number
+	HWRD block;
+	// Data
+	char data[512];
+}TFTP_DATA_PACKET;
+
+typedef struct {
+	// Message type
+	HWRD op;
+	// Block number
+	HWRD block;
+}TFTP_ACK_PACKET;
+
+typedef struct {
+	// Message type
+	HWRD op;
+	// Error number
+	HWRD errcode;
+	// Error message. Zero ending string.
+	char msg[64];
+}TFTP_ERR_PACKET;
+
+#endif /* TFTP_H_ */
