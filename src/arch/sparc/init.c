e96a06df5cc06f843279eb7d9de29b0d00502172
Anton Bondarev <anton.bondarev1978@gmail.com>
2009-06-10 15:45:56 +0000

    Fix problem with monitor start from SDRAM

--- src/arch/sparc/init.c
+++ src/arch/sparc/init.c
@@ -1,10 +1,9 @@
-/**
- * \file init.c
+/*
+ * init.c
  *
- * \date May 21, 2009
- * \author anton
+ *  Created on: May 21, 2009
+ *      Author: anton
  */
-
 #include "types.h"
 #include "cache.h"
 #include "leon.h"
@@ -12,36 +11,40 @@
 
 LEON_REGS * const l_regs = (PVOID) (RG_BASE);
 
-void copy_data_section() {
-	extern char _data_image_start, _data_start, _data_end, _bstart, _bend;
 
-	char *src = &_data_image_start;
-	char *dst = &_data_start;
+void copy_data_section()
+{
+    extern char _data_image_start, _data_start, _data_end, _bstart, _bend;
 
-	/* ROM has data at end of text; copy it. */
-	while (dst < &_data_end) {
-		*dst++ = *src++;
-	}
-	/* Zero bss */
+    char *src = &_data_image_start;
 
-	for (dst = &_bstart; dst < &_bend; dst++) {
-		*dst = 0;
-	}
+    char *dst = &_data_start;
 
+
+    if (0x40000000 >= (unsigned int)&_data_image_start ){
+    /* ROM has data at end of text; copy it. */
+    while (dst < &_data_end) {
+        *dst++ = *src++;
+    }
+    }
+
+    /* Zero bss */
+    for (dst = &_bstart; dst< &_bend; dst++)
+        *dst = 0;
 }
-
 int init() {
-	//TODO during too long time for simulation:(
-	copy_data_section();
-	cache_data_disable();
-	cache_instr_disable();
+    //TODO during too long time for simulation:(
+    copy_data_section();
+    cache_data_enable(FALSE);
+    cache_instr_enable(FALSE);
 
-	irq_init_handlers();
-	uart_init();
-	timers_init();
-	eth_init();//interfaces
-	icmp_init();
-	udp_init();
+    irq_init_handlers();
+    uart_init();
+    timers_init();
 
-	return 0;
+    eth_init();//interfaces
+    icmp_init();
+    udp_init();
+
+    return 0;
 }
