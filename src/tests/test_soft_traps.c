aba852b3733615d9ae3ece0344c7b89e65146790
Nikolay Korotkiy <sikmir@gmail.com>
2009-06-27 19:04:46 +0000

    [No commit log message]

--- src/tests/test_soft_traps.c
+++ src/tests/test_soft_traps.c
@@ -4,15 +4,18 @@
  *  Created on: 26.06.2009
  *      Author: sunnya
  */
+#include "common.h"
+
 unsigned int volatile test_soft_traps_variable;
 #define TEST_SOFT_TRAP_NUMBER 0x10 //trap 0x90
+
 int test_soft_traps()
 {
     unsigned int temp = test_soft_traps_variable;
     __asm__ __volatile__  ("ta %0\n\t"::"i" (TEST_SOFT_TRAP_NUMBER));
 
     if (temp != (test_soft_traps_variable + 1)){
-        printf ("Incorrect software traps handling\n");
+        TRACE("Incorrect software traps handling\n");
         return -1;
     }
     return 0;
