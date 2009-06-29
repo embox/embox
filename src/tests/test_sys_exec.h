d49da512fa25f8fadad0f0886d0cf36dba33fc9d
Alexandr Batyukov <batsun@gmail.com>
2009-06-29 12:34:14 +0000

    test_sys_exec

---
+++ src/tests/test_sys_exec.h
@@ -0,0 +1,16 @@
+/*
+ * test_sys_exec.h
+ *
+ *  Created on: 29.06.2009
+ *      Author: sunnya
+ */
+
+#ifndef TEST_SYS_EXEC_H_
+#define TEST_SYS_EXEC_H_
+
+#ifndef __ASSEMBLER__
+#include "sys.h"
+int test_sys_exec();
+#endif // __ASSEMBLER__
+
+#endif /* TEST_SYS_EXEC_H_ */
