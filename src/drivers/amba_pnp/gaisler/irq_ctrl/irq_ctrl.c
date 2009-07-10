be01c1a1e5a89d9a602675f27a2f4d0e95d3d5ff
Anton Bondarev <anton.bondarev1978@gmail.com>
2009-07-10 18:06:52 +0000

    [No commit log message]

--- src/drivers/amba_pnp/gaisler/irq_ctrl/irq_ctrl.c
+++ src/drivers/amba_pnp/gaisler/irq_ctrl/irq_ctrl.c
@@ -27,9 +27,9 @@ int irq_ctrl_init(){
 	TRY_CAPTURE_APB_DEV (&amba_dev, VENDOR_ID_GAISLER, DEV_ID_GAISLER_INTERRUPT_UNIT);
 
 	amba_dev.show_info = NULL;//show_module_info;
-	if (INTERRUPT_BASE != amba_dev.bar[0].start) {
+	if (IRQ_CTRL_BASE != amba_dev.bar[0].start) {
         TRACE("irq_controller base is %x instead of correct value %x\n",
-                amba_dev.bar[0].start, INTERRUPT_BASE);
+                amba_dev.bar[0].start, IRQ_CTRL_BASE);
         return -1;
     }
 	dev_regs = (IRQ_REGS *)amba_dev.bar[0].start;
