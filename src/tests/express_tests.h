e2c7691081b857651a031bf66e42dc1735d6b0a4
Anton Bondarev <anton.bondarev1978@gmail.com>
2009-07-12 09:42:32 +0000

    Change declaration express test macros

--- src/tests/express_tests.h
+++ src/tests/express_tests.h
@@ -12,6 +12,7 @@ typedef struct _EXPRESS_TEST_DESCRIPTOR {
 	int (*exec)();
 } EXPRESS_TEST_DESCRIPTOR;
 
+/*
 #define REGISTER_EXPRESS_TEST(descr) static void _register_express_test(){ \
     __asm__( \
             ".section .express_tests\n\t" \
@@ -24,6 +25,12 @@ typedef struct _EXPRESS_TEST_DESCRIPTOR {
 	static int exec(); \
 	static const EXPRESS_TEST_DESCRIPTOR _descriptor = { name, exec }; \
 	REGISTER_EXPRESS_TEST(_descriptor);
+*/
+
+#define DECLARE_EXPRESS_TEST(name, exec) \
+    static int exec(); \
+    static const EXPRESS_TEST_DESCRIPTOR _descriptor = { name, exec }; \
+    static const EXPRESS_TEST_DESCRIPTOR *_pdescriptor __attribute__ ((section(".express_tests"))) = &_descriptor;
 
 int express_tests_execute();
 
