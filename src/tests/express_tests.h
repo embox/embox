9a5926ff1da09621d99562c79c705940f051b627
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2009-07-29 11:36:48 +0000

    [No commit log message]

--- src/tests/express_tests.h
+++ src/tests/express_tests.h
@@ -12,21 +12,6 @@ typedef struct _EXPRESS_TEST_DESCRIPTOR {
 	int (*exec)();
 } EXPRESS_TEST_DESCRIPTOR;
 
-/*
-#define REGISTER_EXPRESS_TEST(descr) static void _register_express_test(){ \
-    __asm__( \
-            ".section .express_tests\n\t" \
-            ".word %0\n\t" \
-            ".text\n" \
-            : :"i"(&descr)); \
-            }
-
-#define DECLARE_EXPRESS_TEST(name, exec) \
-	static int exec(); \
-	static const EXPRESS_TEST_DESCRIPTOR _descriptor = { name, exec }; \
-	REGISTER_EXPRESS_TEST(_descriptor);
-*/
-
 #define DECLARE_EXPRESS_TEST(name, exec) \
     static int exec(); \
     static const EXPRESS_TEST_DESCRIPTOR _descriptor = { name, exec }; \
