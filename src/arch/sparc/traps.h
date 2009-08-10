1d152bfe0ec6669c30e4e5c13358b9b76d2ddae1
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2009-08-10 16:53:14 +0000

    bad trap handler now shows the registers

--- src/arch/sparc/traps.h
+++ src/arch/sparc/traps.h
@@ -36,6 +36,8 @@ typedef struct _TRAP_CONTEXT {
 	WORD y;
 	/* global registers */
 	WORD globals[8];
+	/* input registers */
+	WORD ins[8];
 } TRAP_CONTEXT;
 
 #define STACKFRAME_SZ     sizeof(STACKFRAME)
@@ -99,7 +101,7 @@ typedef struct _TRAP_CONTEXT {
 
 /* compute sizes by hand (see above) */
 #define STACKFRAME_SZ     (REG_WINDOW_SZ + (1+6+1)*4)
-#define TRAP_CONTEXT_SZ   ((1+1+1+1+8)*4)
+#define TRAP_CONTEXT_SZ   ((1+1+1+1+8+8)*4)
 
 /* Offsets for trap context structure  */
 #define PT_PSR    0x0
@@ -114,12 +116,26 @@ typedef struct _TRAP_CONTEXT {
 #define PT_G5     0x24
 #define PT_G6     0x28
 #define PT_G7     0x2c
+#define PT_I0     0x30
+#define PT_I1     0x34
+#define PT_I2     0x38
+#define PT_I3     0x3c
+#define PT_I4     0x40
+#define PT_I5     0x44
+#define PT_I6     0x48
+#define PT_I7     0x4c
 
 #define LOAD_PT_GLOBALS(base_reg) \
 	ld      [%base_reg + STACKFRAME_SZ + PT_G1], %g1; \
 	ldd     [%base_reg + STACKFRAME_SZ + PT_G2], %g2; \
 	ldd     [%base_reg + STACKFRAME_SZ + PT_G4], %g4; \
 	ldd     [%base_reg + STACKFRAME_SZ + PT_G6], %g6;
+
+#define LOAD_PT_INS(base_reg) \
+	ldd [%base_reg + STACKFRAME_SZ + PT_I0], %i0; \
+	ldd [%base_reg + STACKFRAME_SZ + PT_I2], %i2; \
+	ldd [%base_reg + STACKFRAME_SZ + PT_I4], %i4; \
+	ldd [%base_reg + STACKFRAME_SZ + PT_I6], %i6;
 
 #define LOAD_PT_YREG(base_reg, scratch) \
 	ld      [%base_reg + STACKFRAME_SZ + PT_Y], %scratch; \
@@ -133,6 +149,7 @@ typedef struct _TRAP_CONTEXT {
 #define LOAD_PT_ALL(base_reg, pt_psr, pt_pc, pt_npc, g_scratch) \
 	LOAD_PT_YREG(base_reg, g_scratch) \
 	LOAD_PT_GLOBALS(base_reg) \
+	LOAD_PT_INS(base_reg) \
 	LOAD_PT_PRIV(base_reg, pt_psr, pt_pc, pt_npc)
 
 #define STORE_PT_GLOBALS(base_reg) \
@@ -140,6 +157,12 @@ typedef struct _TRAP_CONTEXT {
 	std     %g2, [%base_reg + STACKFRAME_SZ + PT_G2]; \
 	std     %g4, [%base_reg + STACKFRAME_SZ + PT_G4]; \
 	std     %g6, [%base_reg + STACKFRAME_SZ + PT_G6];
+
+#define STORE_PT_INS(base_reg) \
+	std %i0, [%base_reg + STACKFRAME_SZ + PT_I0]; \
+	std %i2, [%base_reg + STACKFRAME_SZ + PT_I2]; \
+	std %i4, [%base_reg + STACKFRAME_SZ + PT_I4]; \
+	std %i6, [%base_reg + STACKFRAME_SZ + PT_I6];
 
 #define STORE_PT_YREG(base_reg, scratch) \
 	rd      %y, %scratch; \
@@ -152,6 +175,7 @@ typedef struct _TRAP_CONTEXT {
 
 #define STORE_PT_ALL(base_reg, reg_psr, reg_pc, reg_npc, g_scratch) \
 	STORE_PT_PRIV(base_reg, reg_psr, reg_pc, reg_npc) \
+	STORE_PT_INS(base_reg) \
 	STORE_PT_GLOBALS(base_reg) \
 	STORE_PT_YREG(base_reg, g_scratch) \
 
--- src/kernel/irq.c
+++ src/kernel/irq.c
@@ -28,8 +28,29 @@ void dispatch_trap(BYTE tt, UINT32 *sp) {
 	}
 }
 
-void dispatch_bad_trap(int tt, int pc, int npc, int psr) {
-	TRACE("! tt: %x, pc: %x, npc: %x, psr: %x\n", tt, pc, npc, psr);
+void dispatch_bad_trap(TRAP_CONTEXT * r, WORD tt) {
+	REG_WINDOW *rw;
+
+	TRACE("BAD TRAP (%02x)\n", tt);
+
+	TRACE("PSR: %08x PC: %08x NPC: %08x Y: %08x\n",
+	       r->psr, r->pc, r->npc, r->y);
+	TRACE("%%G: %08x %08x  %08x %08x  %08x %08x  %08x %08x\n",
+	       r->globals[0], r->globals[1], r->globals[2], r->globals[3],
+	       r->globals[4], r->globals[5], r->globals[6], r->globals[7]);
+	TRACE("%%O: %08x %08x  %08x %08x  %08x %08x  %08x %08x\n",
+	       r->ins[0], r->ins[1], r->ins[2], r->ins[3],
+	       r->ins[4], r->ins[5], r->ins[6], r->ins[7]);
+
+	rw = (REG_WINDOW *) r->ins[6];
+
+	TRACE("%%L: %08x %08x  %08x %08x  %08x %08x  %08x %08x\n",
+	       rw->locals[0], rw->locals[1], rw->locals[2], rw->locals[3],
+	       rw->locals[4], rw->locals[5], rw->locals[6], rw->locals[7]);
+	TRACE("%%I: %08x %08x  %08x %08x  %08x %08x  %08x %08x\n",
+	       rw->ins[0], rw->ins[1], rw->ins[2], rw->ins[3],
+	       rw->ins[4], rw->ins[5], rw->ins[6], rw->ins[7]);
+
 	while (1)
 		;
 }
