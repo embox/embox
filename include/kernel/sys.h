364452a13d9e3f46fb2e66d3d73f0bb19e1fa072
Anton Bondarev <anton.bondarev1978@gmail.com>
2009-08-27 07:36:16 +0000

    Add header

---
+++ include/kernel/sys.h
@@ -0,0 +1,22 @@
+/**
+ * \file sys.h
+ *
+ * \date 09.06.2009
+ * \author Eldar Abusalimov
+ */
+
+#ifndef SYS_H_
+#define SYS_H_
+
+#include "asm/cpu_context.h"
+#include "asm/types.h"
+
+typedef int (*EXEC_FUNC)(int argc, char **argv);
+
+void context_save(CPU_CONTEXT * pcontext);
+void context_restore(CPU_CONTEXT * pcontext);
+int sys_exec_start(EXEC_FUNC f, int argc, char **argv);
+void sys_exec_stop();
+void sys_halt();
+
+#endif /* SYS_H_ */
