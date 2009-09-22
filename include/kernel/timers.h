d9f5e7b9278c9555c5fec4e45b9ede905213112b
Andrey Baboshin <andrey.baboshin@gmail.com>
2009-09-22 09:30:42 +0000

    Change code formating

--- include/kernel/timers.h
+++ include/kernel/timers.h
@@ -7,46 +7,46 @@
 
 #ifndef __ASSEMBLER__
 
-/*
+/**
  * Initialization of timers subsystem
  */
 int timers_init ();
 
 typedef void (*TIMER_FUNC) (UINT32 id);
 
-/*
+/**
  * Set 'handle' timer with 'id' identity for executing every 'ticks' ms.
  */
 BOOL set_timer (UINT32 id, UINT32 ticks, TIMER_FUNC handle);
 
-/*
+/**
  * Shut down timer with 'id' identity
  */
 void close_timer (UINT32 id);
 
-/*
+/**
  * Sleep a number of 'ms' millisecond.
  */
 void sleep (unsigned int ms);
 
 //void calibrate_sleep ();
 
-/*
+/**
  * Quantity of ms after starting the system
  */
 UINT32 get_sys_time ();
 
-/*
+/**
  * Save timers context. Now saving only one context.
  */
 int timers_context_save ();
 
-/*
+/**
  * Restore context by it number.
  */
 int timers_context_restore (int context_number);
 
-/*
+/**
  * Shutdown timers subsystem.
  */
 void timers_off ();
