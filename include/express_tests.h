309b149962cc8d4ecc3346162a869c4f8f398d54
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2009-11-16 14:19:12 +0000

    DECLARE_EXPRESS_TEST uses __semicolon

--- include/express_tests.h
+++ include/express_tests.h
@@ -4,8 +4,11 @@
  * \author anton
  * \details
  */
+
 #ifndef EXPRESS_TESTS_H_
 #define EXPRESS_TESTS_H_
+
+#include "common.h"
 
 typedef void (*info_func_t)();
 
@@ -16,19 +19,16 @@ typedef struct _express_test_descriptor {
 	info_func_t info_func;
 } express_test_descriptor_t;
 
-#define DECLARE_EXPRESS_TEST(name, exec) \
-    static int exec(); \
-    static const express_test_descriptor_t _descriptor##exec = { name, exec, TRUE, NULL }; \
-    static const express_test_descriptor_t *_pdescriptor##exec \
-		__attribute__ ((used, section(".express_tests"))) \
+#define __semicolon ;
+#define DECLARE_SMART_EXPRESS_TEST(name, exec, on_boot, info_func) \
+	static int exec() __semicolon \
+	static const express_test_descriptor_t _descriptor##exec = { name, exec, on_boot, info_func } __semicolon \
+	static const express_test_descriptor_t \
+		*_pdescriptor##exec __attribute__ ((used, section(".express_tests"))) \
 		= &_descriptor##exec
 
-#define DECLARE_SMART_EXPRESS_TEST(name, exec, execute_on_boot, info_func) \
-	static int exec(); \
-	static const express_test_descriptor_t _descriptor##exec = { name, exec, execute_on_boot, info_func }; \
-	static const express_test_descriptor_t *_pdescriptor##exec \
-		__attribute__ ((used, section(".express_tests"))) \
-		= &_descriptor##exec
+#define DECLARE_EXPRESS_TEST(name, exec) \
+		DECLARE_SMART_EXPRESS_TEST(name, exec, TRUE, NULL)
 
 int express_tests_execute();
 
--- src/testsuites/cpu_context/test_cpu_context.c
+++ src/testsuites/cpu_context/test_cpu_context.c
@@ -4,6 +4,7 @@
  *  \date Jan 26, 2009
  *  \author Anton Bondarev
  */
+
 #include "common.h"
 #include "asm/cpu_context.h"
 #include "kernel/sys.h"
@@ -13,7 +14,7 @@ DECLARE_EXPRESS_TEST("CPU context", exec);
 
 /**
  * in this trap function contex_save takes place
- * this function save all procesor context to pointed memory
+ * this function save all processor context to pointed memory
  * then call function context_restore
  * @return 0 if success
  */
