4a8e012b4ac8610cf3fbbfa5a3b3cc998fb0d13b
Alexey Fomin <fomin.alexey@gmail.com>
2009-11-19 18:26:54 +0000

    Refactoring express_tests

--- src/testsuites/express_tests.c
+++ src/testsuites/express_tests.c
@@ -14,7 +14,7 @@ int express_tests_execute() {
 	extern express_test_descriptor_t *__express_tests_start, *__express_tests_end;
 	express_test_descriptor_t ** p_test = &__express_tests_start;
 	int i, total = (int) (&__express_tests_end - &__express_tests_start);
-	int passed = 0, failed = 0;
+	int passed = 0, failed = 0, skipped = 0;
 
 	TRACE("\nRunning express tests subsystem (total tests: %d)\n\n", total);
 
@@ -31,13 +31,14 @@ int express_tests_execute() {
 			LOG_ERROR("Broken express test descriptor: can't find exec function for test %s\n", (*p_test)->name);
 			continue;
 		}
-		if (!(*p_test)->execute_on_boot)
-		{
-			TRACE("Test \"%s\" was not marked to be executed on boot. Execute it manually if needed\n", (*p_test)->name);
-			continue;
-		}
 
 		TRACE("Testing %s ... ", (*p_test)->name);
+		if (!(*p_test)->execute_on_boot)
+		{
+			TRACE("SKIPPED\n");
+			skipped++;
+			continue;
+		}
 		// TODO magic constants
 		int result = sys_exec_start((*p_test)->exec);
 		if (result == -1) {
@@ -56,8 +57,9 @@ int express_tests_execute() {
 
 	}
 
-	TRACE("\nPassed: %d\n", passed);
+	TRACE("\nSkipped: %d", skipped);
+	TRACE("Passed: %d\n", passed);
 	TRACE("Failed: %d\n", failed);
 
-	return /*(passed == total)*/(failed == 0) ? 0 : -1;
+	return (failed == 0) ? 0 : -1;
 }
--- src/user/expresstest/expresstest.c
+++ src/user/expresstest/expresstest.c
@@ -17,6 +17,18 @@ static const char *man_page =
 DECLARE_SHELL_COMMAND(COMMAND_NAME, exec, COMMAND_DESC_MSG, HELP_MSG, man_page)
 ;
 
+
+#define DEFAULT_NAME_STR "NONAME"
+#define BROKEN_DESCRIPTOR_STR "BROKEN"
+#define ONBOOT_TEST_STR "ONBOOT"
+#define MANUAL_TEST_STR "MANUAL"
+#define NO_INFO_ERROR_STR "No info func available for that test"
+#define WRONG_TEST_NUMBER_ERROR_STR "Wrong test number entered"
+#define PASSED_CODE 0
+#define FAILED_STR "FAILED"
+#define PASSED_STR "PASSED"
+
+
 static void print_express_tests() {
 	extern express_test_descriptor_t *__express_tests_start,
 			*__express_tests_end;
@@ -28,16 +40,19 @@ static void print_express_tests() {
 	for (i = 0; i < total; i++, p_test++) {
 		TRACE("%3d) ", i);
 		if (NULL == (*p_test)) {
-			TRACE("BROKEN\n");
+			TRACE(BROKEN_DESCRIPTOR_STR"\n");
 			continue;
 		}
 		if (NULL == ((*p_test)->name)) {
-			TRACE("NONAME ");
+			TRACE(DEFAULT_NAME_STR" ");
 		}
 		TRACE("%10s - ", (*p_test)->name);
 		if (NULL == ((*p_test)->exec)) {
-			TRACE("BROKEN\n");
+			TRACE(BROKEN_DESCRIPTOR_STR"\n");
 			continue;
+		}
+		else {
+			TRACE(MANUAL_TEST_STR"\n");
 		}
 	}
 }
@@ -47,7 +62,9 @@ static express_test_descriptor_t *get_express_test(int test_num) {
 			*__express_tests_end;
 	express_test_descriptor_t **p_test = &__express_tests_start;
 	int total = (int) (&__express_tests_end - &__express_tests_start);
+
 	if (test_num < 0 || test_num > total - 1) {
+		TRACE("total: %d\ncurrent: %d\n\n\n", total, test_num);
 		return NULL;
 	}
 	p_test += test_num;
@@ -71,6 +88,7 @@ static int exec(int argsc, char **argsv) {
 				show_help();
 				return -1;
 			}
+			break;
 		case 'i':
 			if (test_num == -1) {
 				LOG_ERROR("%s: -i: missing -n before -i.\n", COMMAND_NAME);
@@ -83,26 +101,35 @@ static int exec(int argsc, char **argsv) {
 			return 0;
 		}
 	} while (-1 != nextOption);
-
 	if (test_num == -1) {
 		print_express_tests();
 		return 0;
 	}
-
 	p_test = get_express_test(test_num);
+	if (p_test == NULL) {
+		TRACE(WRONG_TEST_NUMBER_ERROR_STR"\n");
+		return -1;
+	}
 	if (run_info_func) {
 		if (p_test->info_func == NULL) {
-			LOG_ERROR("No info func available for that test\n");
+			LOG_ERROR(NO_INFO_ERROR_STR"\n");
 			return -1;
 		}
 		p_test->info_func();
 		return 0;
 	}
-
 	if (p_test->exec == NULL) {
 		LOG_ERROR("Unable to execute express test\n");
 		return -1;
 	}
-	p_test->exec();
+	TRACE("Testing %s ... ", p_test->name == NULL ?
+			DEFAULT_NAME_STR : p_test->name);
+	int result = sys_exec_start(p_test->exec);
+	if (result == PASSED_CODE) {
+		TRACE(PASSED_STR"\n");
+	} else {
+		TRACE(FAILED_STR"\n");
+	}
+
 	return 0;
 }
