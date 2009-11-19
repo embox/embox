6c69f2510fb44a49d8c5ec50e1ee03eb56a9b473
Anton Bondarev <anton.bondarev1978@gmail.com>
2009-11-19 10:55:33 +0000

    Move cpu_conf.h to arch folder

--- include/asm-sparc/cpu_conf.h
+++ include/asm-sparc/cpu_conf.h
@@ -8,8 +8,4 @@
 /* Desired uart baud rate */
 #define UART_BAUD_RATE  38400
 
-#define UART_SCALER_VAL  ((((CORE_FREQ*10) / (8 * UART_BAUD_RATE))-5)/10)
-/* Timing Calculation */
-#define TIMER_SCALER_VAL (CORE_FREQ/1000000 - 1)
-
 #endif //_CPU_CONF_
--- include/autoconf.h
+++ include/autoconf.h
@@ -1,2 +1,3 @@
 #include "../scripts/autoconf.h"
 #include "../scripts/on_boot_express_tests.h"
+#include "asm/cpu_conf.h"
