edcc34c4d61b1ea92e08d1820ab5c989cd0167aa
Nikolay Korotkiy <sikmir@gmail.com>
2009-11-20 18:02:05 +0000

    Fix config tests, add atoi

--- include/ctype.h
+++ include/ctype.h
@@ -19,21 +19,21 @@ extern unsigned char _ctype[];
 
 #define __ismask(x) (_ctype[(int)(unsigned char)(x)])
 
-/* Сhecks for an alphanumeric character. */
+/* Checks for an alphanumeric character. */
 #define isalnum(c)      ((__ismask(c)&(_U|_L|_D)) != 0)
-/* Сhecks for an alphabetic character. */
+/* Checks for an alphabetic character. */
 #define isalpha(c)      ((__ismask(c)&(_U|_L)) != 0)
-/* Сhecks for a control character. */
+/* Checks for a control character. */
 #define iscntrl(c)      ((__ismask(c)&(_C)) != 0)
-/* Сhecks for a digit (0 through 9). */
+/* Checks for a digit (0 through 9). */
 #define isdigit(c)      ((__ismask(c)&(_D)) != 0)
-/* Сhecks for any printable character except space. */
+/* Checks for any printable character except space. */
 #define isgraph(c)      ((__ismask(c)&(_P|_U|_L|_D)) != 0)
-/* Сhecks for a lower-case character. */
+/* Checks for a lower-case character. */
 #define islower(c)      ((__ismask(c)&(_L)) != 0)
-/* Сhecks for any printable character including space. */
+/* Checks for any printable character including space. */
 #define isprint(c)      ((__ismask(c)&(_P|_U|_L|_D|_SP)) != 0)
-/* Сhecks for any printable character which is not a space or an alphanumeric character. */
+/* Checks for any printable character which is not a space or an alphanumeric character. */
 #define ispunct(c)      ((__ismask(c)&(_P)) != 0)
 /* Checks for white-space characters. */
 #define isspace(c)      ((__ismask(c)&(_S)) != 0)
@@ -42,7 +42,7 @@ extern unsigned char _ctype[];
 /* Checks for a hexadecimal digits. */
 #define isxdigit(c)     ((__ismask(c)&(_D|_X)) != 0)
 
-/* Сhecks whether c is a 7-bit unsigned char value that fits into the ASCII character set. */
+/* Checks whether c is a 7-bit unsigned char value that fits into the ASCII character set. */
 #define isascii(c) (((unsigned char)(c))<=0x7f)
 #define toascii(c) (((unsigned char)(c))&0x7f)
 
--- src/lib/stdlib/stdlib.c
+++ src/lib/stdlib/stdlib.c
@@ -36,3 +36,80 @@ int ch_to_digit(char ch, int base) {
         }
         return -1;
 }
+
+double atof(const char *s) {
+//TODO: with -msoft-float don't work
+/*        char    c;
+        double  val = 0, power = 1;
+        unsigned char sign = 0;
+
+skipws:
+        c = *s;
+        if(!isdigit(c) && c != '-' && c != '+') {
+                s++;
+                goto skipws;
+        }
+        if(c == '-') {
+                sign++;
+                s++;
+        } else if(c == '+') {
+                s++;
+        }
+        if(!isdigit(*s)) {
+                goto skipws;
+        }
+val:
+        c = *s;
+        if(isdigit(c)) {
+                val = val * 10 + (c - '0');
+                s++;
+                goto val;
+        }
+
+        if (c == '.') {
+                s++;
+        }
+power:
+        c = *s;
+        if(isdigit(c)) {
+                val = val * 10 + (c - '0');
+                power *= 10;
+                s++;
+                goto power;
+        }
+        return (sign ? -1 : 1)*(val / power);*/
+}
+
+int atoi(const char *s) {
+        char   c;
+        int    a = 0;
+        unsigned char sign = 0;
+
+skipws:
+        c = *s;
+        if(!isdigit(c) && c != '-' && c != '+') {
+        	s++;
+                goto skipws;
+        }
+        if(c == '-') {
+                sign++;
+                s++;
+        } else if(c == '+') {
+        	s++;
+        }
+        if(!isdigit(*s)) {
+                goto skipws;
+        }
+conv:
+        c = *s;
+        if(isdigit(c)) {
+                a = a*10 + (c - '0');
+                s++;
+                goto conv;
+        }
+        return sign ? -a : a;
+}
+
+long int atol(const char *nptr) {
+
+}
