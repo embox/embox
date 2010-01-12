8734bf805adb1149e171ac1cfee5244e563e6b3e
Darya Teplykh <dteplykh@tepkom.ru>
2010-01-12 12:53:44 +0000

    Option -n is corrected

--- src/user/mem/mem.c
+++ src/user/mem/mem.c
@@ -1,8 +1,7 @@
 /**
- * \file mem.c
- *
- * \date 13.02.2009
- * \author Alexey Fomin
+ * @file mem.c
+ * @date 13.02.2009
+ * @author Alexey Fomin
  */
 #include "shell_command.h"
 
@@ -21,15 +20,16 @@ char mem_keys[] = {
 
 void mem_print(uint32_t *addr, long int amount) {
 	long i = 0;
-	addr = (uint32_t *) ((uint32_t) addr & 0xFFFFFFFC);
+	uint32_t *cur_addr = (uint32_t *) ((uint32_t) addr & 0xFFFFFFFC);
 	while (i < amount) {
 		if (0 == (i % 4)) {
-			printf("0x%08x:\t", (int) (addr + i));
+			printf("0x%08x:\t", cur_addr);
 		}
-		printf("0x%08x\t", *(addr + i));
+		printf("0x%08x\t", (uint32_t *) cur_addr);
 		if (3 == (i % 4)) {
 			printf("\n");
 		}
+		cur_addr++;
 		i++;
 	}
 }
@@ -37,48 +37,48 @@ void mem_print(uint32_t *addr, long int amount) {
 typedef void TEST_MEM_FUNC(uint32_t *addr, long int amount);
 
 static int exec(int argsc, char **argsv) {
-        uint32_t *address = (uint32_t *) 0x70000000L;
-        long int amount = 100L;
-        int nextOption;
-        getopt_init();
-        do {
-                nextOption = getopt(argsc, argsv, "a:h");
-                switch(nextOption) {
-                case 'h':
-                        show_help();
-                        return 0;
-                case 'a':
-            		if (optarg == NULL) {
-                                LOG_ERROR("mem: -a: hex value for address expected.\n");
-                                show_help();
-                                return -1;
-                        }
-                        if (!sscanf(optarg, "0x%x", &address)) {
-                                LOG_ERROR("mem: -a: hex value for address expected.\n");
-                                show_help();
-                                return -1;
-                        }
-                        break;
-                case 'n':
-            		if (optarg == NULL) {
-                                LOG_ERROR("mem: -n: hex or integer value for number of bytes expected.\n");
-                                show_help();
-                                return -1;
-                        }
-                        if (!sscanf(optarg, "0x%x", amount)) {
-                                if (!sscanf(optarg, "%d", &amount)) {
-                                        LOG_ERROR("mem: -n: hex or integer value for number of bytes expected.\n");
-                                        show_help();
-                                        return -1;
-                                }
-                        }
-                        break;
-                case -1:
-                        break;
-                default:
-                	return 0;
-                }
-        } while(-1 != nextOption);
+	uint32_t *address = (uint32_t *) 0x70000000L;
+	long int amount = 100L;
+	int nextOption;
+	getopt_init();
+	do {
+		nextOption = getopt(argsc, argsv, "a:hn:");
+		switch(nextOption) {
+		case 'h':
+			show_help();
+			return 0;
+		case 'a':
+			if (optarg == NULL) {
+				LOG_ERROR("mem: -a: hex value for address expected.\n");
+				show_help();
+				return -1;
+			}
+			if (!sscanf(optarg, "0x%x", &address)) {
+				LOG_ERROR("mem: -a: hex value for address expected.\n");
+				show_help();
+				return -1;
+			}
+			break;
+		case 'n':
+			if (optarg == NULL) {
+				LOG_ERROR("mem: -n: hex or integer value for number of bytes expected.\n");
+				show_help();
+				return -1;
+			}
+			if (!sscanf(optarg, "0x%x", &amount)) {
+				if (!sscanf(optarg, "%d", &amount)) {
+					LOG_ERROR("mem: -n: hex or integer value for number of bytes expected.\n");
+					show_help();
+					return -1;
+				}
+			}
+			break;
+		case -1:
+			break;
+		default:
+			return 0;
+		}
+	} while(-1 != nextOption);
 
 	mem_print(address, amount);
 	return 0;
