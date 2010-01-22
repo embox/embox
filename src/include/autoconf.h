d1371574b7be13884142f2daffd7da62154d049b
Alexey Fomin <fomin.alexey@gmail.com>
2010-01-22 16:34:01 +0000

    Remove on_boot_express_tests.h. No need in that any more

--- src/include/autoconf.h
+++ src/include/autoconf.h
@@ -1,9 +1,7 @@
 /**
- * @file
+ * @file autoconf.h
  */
 
 #define CONFIG(option) (defined(CONFIG_##option) && CONFIG_##option)
 
-/* TODO =\ -- Eldar*/
-#include "../../scripts/on_boot_express_tests.h"
 #include "asm/cpu_conf.h"
