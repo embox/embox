3ec09b0106f1e4bbc9bc768ee22fd57f3abb450a
Anton Bondarev <anton.bondarev1978@gmail.com>
2010-02-10 16:14:53 +0000

    Some refactoring in sock files

--- src/include/net/sock.h
+++ src/include/net/sock.h
@@ -14,7 +14,7 @@
 #include <net/net.h>
 
 typedef struct {
-        spinlock_t    slock;
+	spinlock_t slock;
 } socket_lock_t;
 
 /**
@@ -25,16 +25,17 @@ typedef struct {
  * @param skc_bound_dev_if bound device index if != 0
  */
 struct sock_common {
-        unsigned short          skc_family;
-        volatile unsigned char  skc_state;
-        unsigned char           skc_reuse;
-        int                     skc_bound_dev_if;
-/*        struct hlist_node       skc_node;
-        struct hlist_node       skc_bind_node;
-        atomic_t                skc_refcnt;
-        unsigned int            skc_hash;
-*/
-        struct proto            *skc_prot;
+	unsigned short skc_family;
+	volatile unsigned char skc_state;
+	unsigned char skc_reuse;
+	int skc_bound_dev_if;
+#if 0
+	struct hlist_node skc_node;
+	struct hlist_node skc_bind_node;
+	atomic_t skc_refcnt;
+	unsigned int skc_hash;
+#endif
+	struct proto *skc_prot;
 };
 
 /**
@@ -58,74 +59,89 @@ struct sock_common {
  * @sk_destruct: called at sock freeing time, i.e. when all refcnt == 0
  */
 typedef struct sock {
-	struct sock_common      __sk_common;
+	struct sock_common __sk_common;
 #define sk_family               __sk_common.skc_family
 #define sk_prot                 __sk_common.skc_prot
-	unsigned char		sk_protocol;
-	unsigned short		sk_type;
-	int			sk_rcvbuf;
-	socket_lock_t           sk_lock;
+	unsigned char sk_protocol;
+	unsigned short sk_type;
+	int sk_rcvbuf;
+	socket_lock_t sk_lock;
 	struct {
-	        struct sk_buff *head;
-	        struct sk_buff *tail;
+		struct sk_buff *head;
+		struct sk_buff *tail;
 	} sk_backlog;
-	int			sk_sndbuf;
-	unsigned long 		sk_flags;
+	int sk_sndbuf;
+	unsigned long sk_flags;
 
-	struct sk_buff_head     sk_receive_queue;
-	struct sk_buff_head     sk_write_queue;
+	struct sk_buff_head sk_receive_queue;
+	struct sk_buff_head sk_write_queue;
 
-	struct socket           *sk_socket;
-	void                    *sk_user_data;
+	struct socket *sk_socket;
+	void *sk_user_data;
 
-	void (* sk_state_change) (struct sock *sk);
-	void (* sk_data_ready) (struct sock *sk, int bytes);
-	void (* sk_write_space) (struct sock *sk);
-	void (* sk_error_report) (struct sock *sk);
-	int (* sk_backlog_rcv) (struct sock *sk, sk_buff_t *pack);
-	void (* sk_destruct) (struct sock *sk);
+	void (* sk_state_change)(struct sock *sk);
+	void (* sk_data_ready)(struct sock *sk, int bytes);
+	void (* sk_write_space)(struct sock *sk);
+	void (* sk_error_report)(struct sock *sk);
+	int (* sk_backlog_rcv)(struct sock *sk, sk_buff_t *pack);
+	void (* sk_destruct)(struct sock *sk);
 } sock_t;
 
-/* Sock flags */
+/** Sock flags */
 enum sock_flags {
-        SOCK_DEAD,
-        SOCK_DONE,
-        SOCK_DESTROY,
-        SOCK_BROADCAST
+	SOCK_DEAD, SOCK_DONE, SOCK_DESTROY, SOCK_BROADCAST
 };
 
+/** Protocol specific functions */
 typedef struct proto {
-        void (*close)(sock_t *sk, long timeout);
-        int  (*connect)(sock_t *sk, sockaddr_t *uaddr, int addr_len);
-        int  (*disconnect)(sock_t *sk, int flags);
-        sock_t *(*accept)(sock_t *sk, int flags, int *err);
-        int  (*ioctl)(struct sock *sk, int cmd, unsigned long arg);
-        int  (*init)(sock_t *sk);
-        int  (*setsockopt)(struct sock *sk, int level,
-                            int optname, char *optval, int optlen);
-        int  (*getsockopt)(struct sock *sk, int level,
-                            int optname, char *optval, int *option);
-        int  (*sendmsg)(struct kiocb *iocb, sock_t *sk, struct msghdr *msg, size_t len);
-        int  (*recvmsg)(struct kiocb *iocb, sock_t *sk, struct msghdr *msg,
-        			    size_t len, int noblock, int flags, int *addr_len);
-        int  (*bind)(sock_t *sk, sockaddr_t *uaddr, int addr_len);
-        int  (*backlog_rcv)(sock_t *sk, sk_buff_t *skb);
-        void (*hash)(struct sock *sk);
-        void (*unhash)(struct sock *sk);
-        unsigned int            obj_size;
-    	char			name[32];
+	void (*close)(sock_t *sk, long timeout);
+	int (*connect)(sock_t *sk, sockaddr_t *uaddr, int addr_len);
+	int (*disconnect)(sock_t *sk, int flags);
+	sock_t *(*accept)(sock_t *sk, int flags, int *err);
+	int (*ioctl)(struct sock *sk, int cmd, unsigned long arg);
+	int (*init)(sock_t *sk);
+	int (*setsockopt)(struct sock *sk, int level, int optname, char *optval,
+			int optlen);
+	int (*getsockopt)(struct sock *sk, int level, int optname, char *optval,
+			int *option);
+	int (*sendmsg)(struct kiocb *iocb, sock_t *sk, struct msghdr *msg,
+			size_t len);
+	int (*recvmsg)(struct kiocb *iocb, sock_t *sk, struct msghdr *msg,
+			size_t len, int noblock, int flags, int *addr_len);
+	int (*bind)(sock_t *sk, sockaddr_t *uaddr, int addr_len);
+	int (*backlog_rcv)(sock_t *sk, sk_buff_t *skb);
+	void (*hash)(struct sock *sk);
+	void (*unhash)(struct sock *sk);
+	unsigned int obj_size;
+	char name[32];
 } proto_t;
 
+/** Initializes structure sock's pull */
+extern void sk_init(void);
+
+/** Allocates structure sock with specific parameters
+ * @family - Protocol family (PF_INIT for example)
+ * @priority -not use now
+ * @prot - pointer to the proto structure
+ */
+extern sock_t *sk_alloc(/*struct net *net,*/int family, gfp_t priority,
+		proto_t *prot);
+
+/** returns specified structure sock into pull */
+extern void sk_free(sock_t *sk);
+
+/** This function used by all transports to attempt to queue received packets*/
+extern int sock_queue_rcv_skb(sock_t *sk, sk_buff_t *skb);
+
+#if 0
+TODO NETSOCK: functions are not realized now
 extern int proto_register(proto_t *prot, int alloc_slab);
 extern void proto_unregister(proto_t *prot);
 
-extern void sk_init(void);
-extern sock_t *sk_alloc(/*struct net *net,*/ int family, gfp_t priority, proto_t *prot);
-extern void sk_free(sock_t *sk);
 extern sock_t *sk_clone(const sock_t *sk, const gfp_t priority);
 extern int sock_setsockopt(socket_t *sock, int level, int op, char *optval, unsigned int optlen);
 extern int sock_getsockopt(socket_t *sock, int level, int op, char *optval, int *optlen);
 extern sk_buff_t *sock_alloc_send_skb(sock_t *sk, unsigned long size, int noblock, int *errcode);
-extern int sock_queue_rcv_skb(sock_t *sk, sk_buff_t *skb);
+#endif
 
 #endif /* SOCK_H_ */
--- src/net/core/sock.c
+++ src/net/core/sock.c
@@ -1,7 +1,11 @@
 /**
  * @file sock.c
  *
- * @details Generic socket support routines.
+ * @brief Generic socket support routines.
+ *
+ * @details Contains functions for working with all type of structure sock.
+ *          It includes free sock pull.
+ *
  * @date 12.08.09
  * @author Nikolay Korotky
  */
@@ -11,74 +15,79 @@
 #include <kernel/irq.h>
 #include <net/skbuff.h>
 #include <net/sock.h>
+#include <net/udp.h>
 
 typedef struct sock_info {
-        struct sock sk;
-        struct list_head list __attribute__ ((aligned (4)));
+	/*FIXME NETSOCK: now we use just udp_sock pull. It is the biggest of sock
+	 *  structure now. But we must allocate sock with size equals obj_size
+	 *  member in proto structure.
+	 */
+	struct udp_sock sk;
+	struct list_head list __attribute__ ((aligned (4)));
 } sock_info_t __attribute__ ((aligned (4)));
 
 static sock_info_t socks_pull[MAX_KERNEL_SOCKETS];
 static LIST_HEAD(head_free_sock);
 
 void __init sk_init(void) {
-        int i;
-        for (i = 0; i < array_len(socks_pull); i++) {
-                list_add(&(&socks_pull[i])->list, &head_free_sock);
-        }
+	int i;
+	for (i = 0; i < array_len(socks_pull); i++) {
+		list_add(&(&socks_pull[i])->list, &head_free_sock);
+	}
 }
-
+/* allocates proto structure for specified protocol*/
 static struct sock *sk_prot_alloc(struct proto *prot, gfp_t priority,
-                int family) {
-        struct sock *sock;
-        struct list_head *entry;
+		int family) {
+	struct sock *sock;
+	struct list_head *entry;
 
-        unsigned long irq_old = local_irq_save();
+	unsigned long irq_old = local_irq_save();
 
-        if (list_empty(&head_free_sock)) {
-                local_irq_restore(irq_old);
-                return NULL;
-        }
-        entry = (&head_free_sock)->next;
-        list_del_init(entry);
-        sock = (struct sock *) list_entry(entry, sock_info_t, list);
+	if (list_empty(&head_free_sock)) {
+		local_irq_restore(irq_old);
+		return NULL;
+	}
+	entry = (&head_free_sock)->next;
+	list_del_init(entry);
+	sock = (struct sock *) list_entry(entry, sock_info_t, list);
 
-        local_irq_restore(irq_old);
+	local_irq_restore(irq_old);
 
-        return sock;
+	return sock;
 }
-
+/* returns specified structure sock into pull */
 static void sk_prot_free(struct proto *prot, struct sock *sk) {
-        sock_info_t *sock_info;
-        unsigned long irq_old;
-        if (NULL == sk) {
-                return;
-        }
+	sock_info_t *sock_info;
+	unsigned long irq_old;
+	if (NULL == sk) {
+		return;
+	}
 
-        irq_old = local_irq_save();
-        sock_info = (sock_info_t *) sk;
-        list_add(&sock_info->list, &head_free_sock);
-        local_irq_restore(irq_old);
+	irq_old = local_irq_save();
+	sock_info = (sock_info_t *) sk;
+	list_add(&sock_info->list, &head_free_sock);
+	local_irq_restore(irq_old);
 }
 
-struct sock *sk_alloc(/*struct net *net,*/ int family, gfp_t priority,
-                      struct proto *prot) {
-        struct sock *sk;
-        sk = sk_prot_alloc(prot, 0, family);
-        if(sk) {
-    		sk->sk_family = family;
-    		sk->sk_prot = prot;
+struct sock *sk_alloc(/*struct net *net,*/int family, gfp_t priority,
+		struct proto *prot) {
+	struct sock *sk;
+	sk = sk_prot_alloc(prot, 0, family);
+	if (sk) {
+		sk->sk_family = family;
+		sk->sk_prot = prot;
 	}
 	return sk;
 }
 
 void sk_free(struct sock *sk) {
 	if (sk->sk_destruct) {
-                sk->sk_destruct(sk);
-        }
-        sk_prot_free(sk->sk_prot, sk);
+		sk->sk_destruct(sk);
+	}
+	sk_prot_free(sk->sk_prot, sk);
 }
 
 int sock_queue_rcv_skb(struct sock *sk, struct sk_buff *skb) {
 	skb_queue_tail(&sk->sk_receive_queue, skb);
-        return 0;
+	return 0;
 }
