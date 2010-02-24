d5a3c91f9f66a21bb027c63d334286213da7c91d
Nikolay Korotkiy <sikmir@gmail.com>
2010-02-24 07:42:47 +0000

    [No commit log message]

--- src/net/socket.c
+++ src/net/socket.c
@@ -12,33 +12,30 @@
 #include <string.h>
 #include <common.h>
 #include <kernel/module.h>
-#include <net/skbuff.h>
 #include <net/socket.h>
 #include <net/udp.h>
 #include <net/ip.h>
-#include <net/if_ether.h>
-#include <net/net_pack_manager.h>
 #include <net/protocol.h>
 #include <net/kernel_socket.h>
 
 #include <linux/init.h>
 
 int __init sock_init(void) {
-	kernel_sock_init();
-	sk_init();
 	extern inet_protosw_t *__ipstack_sockets_start, *__ipstack_sockets_end;
 	inet_protosw_t ** p_netsock = &__ipstack_sockets_start;
+	kernel_sock_init();
+	sk_init();
 	for (; p_netsock < &__ipstack_sockets_end; p_netsock++) {
 		/*register socket type*/
 		 TRACE("Added sock type 0x%X for proto 0x%X\n", (* p_netsock)->type, (* p_netsock)->protocol);
 	}
-	/* Initialize sock. */
-	//sk_init();
 
 	/* Initialize skbuff. */
 	skb_init();
 	return 0;
 }
+
+/* TODO: remove all below from here */
 
 int socket(int domain, int type, int protocol) {
 	struct socket *res;
@@ -64,7 +61,7 @@ int close(int sockfd) {
 
 int send(int sockfd, const void *buf, int len, int flags) {
 	struct socket *sock = sockfd_lookup(sockfd);
-        kernel_sendmsg(NULL, sock, NULL, len);
+	kernel_sendmsg(NULL, sock, NULL, len);
 	return len;
 }
 
