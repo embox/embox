21fe3881af96148dadd503df078c289694db61f0
Anton Bondarev <anton.bondarev1978@gmail.com>
2010-03-11 14:10:36 +0000

    setjmp for microblaze

---
+++ src/arch/microblaze/include/asm/linkage.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ *
+ * @data 11.03.2010
+ * @author Anton Bondarev
+ */
+
+#ifndef ASM_LINKAGE_H_
+#define ASM_LINKAGE_H_
+
+#define C_ENTRY(name)	.globl name; .align 4; name
+
+#endif /* ASM_LINKAGE_H_ */
---
+++ src/arch/microblaze/include/asm/setjmp.h
@@ -0,0 +1,32 @@
+/**
+ * @file
+ *
+ * @brief Microblaze definition objects for setjmp
+ *
+ * @data 11.03.2010
+ *
+ * @author Anton Bondarev
+ */
+
+#ifndef SETJMP_H_
+#define SETJMP_H_
+
+#include <types.h>
+
+#ifndef __ASSEMBLER__
+
+typedef struct {
+	/* 0x0 */uint32_t sp; /* Stack pointer.  */
+	/* 0x4 */uint32_t lp; /* Link pointer.  */
+	/* 0x8 */uint32_t regs[13]; /* Callee-saved registers r18-r30.  */
+} __jmp_buf[1];
+
+#endif /* __ASSEMBLER__ */
+
+#define JB_SP        0x00
+#define JB_LP        0x04
+#define JB_REGS      0x08
+
+#define JB_SIZE     (4 * 15)
+
+#endif /* SETJMP_H_ */
