897f7cbeb0e85cce7fb57536d26839ae858f353c
Nikolay Korotkiy <sikmir@gmail.com>
2010-03-17 14:30:37 +0000

    Small fix ls command

--- src/cmds/ls/ls.c
+++ src/cmds/ls/ls.c
@@ -11,29 +11,56 @@
 
 #define COMMAND_NAME     "ls"
 #define COMMAND_DESC_MSG "list directory contents"
-#define HELP_MSG         "Usage: ls [-p path] [-h]"
+#define HELP_MSG         "Usage: ls [-hl] path"
+
 static const char *man_page =
 	#include "ls_help.inc"
 	;
 
 DECLARE_SHELL_COMMAND(COMMAND_NAME, exec, COMMAND_DESC_MSG, HELP_MSG, man_page);
 
+static void print_list(FS_FILE_ITERATOR iter_func) {
+	FILE_INFO file_info;
+	while (NULL != iter_func(&file_info)){
+		printf("%s\n", file_info.file_name);
+	}
+}
+
+static void print_long_list(FS_FILE_ITERATOR iter_func) {
+	FILE_INFO file_info;
+	char mode[] = "---";
+	printf("%16s | %4s | %10s | %10s\n", "name", "mode", "size", "size on disk");
+	while (NULL != iter_func(&file_info)){
+		if(file_info.mode & FILE_MODE_RO) {
+			mode[0] = 'r';
+		}
+		if(file_info.mode & FILE_MODE_WO) {
+			mode[1] = 'w';
+		}
+		if(file_info.mode & FILE_MODE_XO) {
+			mode[2] = 'x';
+		}
+		printf("%16s |  %3s | %10d | %10d\n", file_info.file_name, mode,
+				file_info.size_in_bytes, file_info.size_on_disk);
+	}
+}
+
 static int exec(int argsc, char **argsv) {
 	char *path = "/";
+	int long_list = 0;
 	FSOP_DESCRIPTION *fsop;
 	FS_FILE_ITERATOR iter_func;
-	FILE_INFO file_info;
 
 	int nextOption;
 	getopt_init();
 	do {
-		nextOption = getopt(argsc, argsv, "p:h");
+		nextOption = getopt(argsc - 1, argsv, "lh");
 		switch(nextOption) {
 		case 'h':
-				show_help();
+			show_help();
 			return 0;
-		case 'p':
-			path = optarg;
+		case 'l':
+			long_list = 1;
 			break;
 		case -1:
 			break;
@@ -41,6 +68,10 @@ static int exec(int argsc, char **argsv) {
 			return 0;
 		}
 	} while(-1 != nextOption);
+
+	if(argsc > 1) {
+		path = argsv[argsc - 1];
+	}
 
 	if (NULL == (fsop = rootfs_get_fsopdesc(path))){
 		LOG_ERROR("can't find fs %s\n", path);
@@ -53,10 +84,11 @@ static int exec(int argsc, char **argsv) {
 		LOG_ERROR("can't find iterator func for fs %s\n", path);
 		return 0;
 	}
-	printf("%16s | %8s | %10s | %10s\n", "name", "mode", "size", "size on disk");
-	while (NULL != iter_func(&file_info)){
-		printf("%16s | %8X | %10d | %10d\n", file_info.file_name, file_info.mode,
-				file_info.size_in_bytes, file_info.size_on_disk);
+
+	if(long_list) {
+		print_long_list(iter_func);
+	} else {
+		print_list(iter_func);
 	}
 
 	return 0;
