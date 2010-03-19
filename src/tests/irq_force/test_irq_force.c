18d00b57276b8a15ce0ff28a23d7b6c0107ef300
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2010-03-19 00:23:36 +0000

    separating irq_nesting test from irq_force

--- src/tests/irq_force/test_irq_force.c
+++ src/tests/irq_force/test_irq_force.c
@@ -8,8 +8,7 @@
 
 #include "common.h"
 
-#include <types.h>
-#include <string.h>
+#include <stdbool.h>
 
 #include <embox/test.h>
 #include <kernel/irq.h>
@@ -20,32 +19,20 @@
 EMBOX_TEST(run);
 EMBOX_TEST_EXPORT(run_irq_force);
 
-volatile static bool irq_first, irq_second;
+volatile static bool irq_happened;
 
 static irq_return_t test_isr(irq_nr_t irq_nr, void *dev_id) {
-	if (!irq_first) {
-		TRACE(" first(%d) { ", irq_nr);
-		irq_first = true;
-		interrupt_force(TEST_IRQ_NR + 1);
-		TRACE(" } first ");
-	} else if (!irq_second) {
-		TRACE(" second(%d) ", irq_nr);
-		irq_second = true;
-	} else {
-		TRACE(" third(%d) ", irq_nr);
-	}
+	irq_happened = true;
 	return IRQ_HANDLED;
 }
 
 static int run(void) {
 	int error;
 
-	irq_first = false;
-	irq_second = false;
+	irq_happened = false;
 
 	if (0 != (error = irq_attach(TEST_IRQ_NR, test_isr, 0x0, NULL,
-			"test_irq_first")) || 0 != (error = irq_attach(TEST_IRQ_NR + 1,
-			test_isr, 0x0, NULL, "test_irq_second"))) {
+			"test_irq_force"))) {
 		TRACE("irq_attach failed: %s\n", strerror(error));
 		return -1;
 	}
@@ -53,7 +40,6 @@ static int run(void) {
 	interrupt_force(TEST_IRQ_NR);
 
 	irq_detach(TEST_IRQ_NR, NULL);
-	irq_detach(TEST_IRQ_NR + 1, NULL);
 
-	return irq_first && irq_second ? 0 : -1;
+	return irq_happened ? 0 : -1;
 }
---
+++ src/tests/irq_nesting/test_irq_nesting.c
@@ -0,0 +1,60 @@
+/**
+ * @file
+ *
+ * @date 17.03.2010
+ * @author Eldar Abusalimov
+ */
+
+#include "common.h"
+
+#include <types.h>
+
+#include <embox/test.h>
+#include <kernel/irq.h>
+#include <hal/interrupt.h>
+
+#define TEST_OUTER_IRQ_NR 10
+#define TEST_INNER_IRQ_NR (TEST_OUTER_IRQ_NR + 1)
+
+EMBOX_TEST(run);
+
+volatile static bool outer_irq, inner_irq, irq_nested;
+
+static irq_return_t test_isr(irq_nr_t irq_nr, void *dev_id) {
+	if (!outer_irq) {
+		outer_irq = true;
+		interrupt_force(TEST_INNER_IRQ_NR);
+		if (inner_irq) {
+			irq_nested = true;
+		} else {
+			TRACE("no inner irq received being inside outer irq handler\n");
+		}
+	} else if (!inner_irq) {
+		inner_irq = true;
+	} else {
+		TRACE("unexpected irq nr %d\n", irq_nr);
+	}
+	return IRQ_HANDLED;
+}
+
+static int run(void) {
+	int error;
+
+	outer_irq = false;
+	inner_irq = false;
+	irq_nested = false;
+
+	if (0 != (error = irq_attach(TEST_OUTER_IRQ_NR, test_isr, 0x0, NULL,
+			"test_irq_first")) || 0 != (error = irq_attach(TEST_INNER_IRQ_NR,
+			test_isr, 0x0, NULL, "test_irq_second"))) {
+		TRACE("irq_attach failed: %s\n", strerror(error));
+		return -1;
+	}
+
+	interrupt_force(TEST_OUTER_IRQ_NR);
+
+	irq_detach(TEST_OUTER_IRQ_NR, NULL);
+	irq_detach(TEST_INNER_IRQ_NR, NULL);
+
+	return outer_irq && inner_irq && irq_nested ? 0 : -1;
+}
