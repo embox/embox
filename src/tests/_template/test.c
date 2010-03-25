2f2205146b23169c8db98a0096b7d922eeeca2d7
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2010-03-25 22:38:41 +0000

    Simplify test template as much as possible
    Add brief comments about how to include the test into the build
    (fixes #30).

---
+++ src/tests/_template/test.c
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * @brief This file is derived from EMBOX test template.
+ */
+
+#include <embox/test.h>
+
+EMBOX_TEST(run);
+
+/**
+ * The test itself.
+ *
+ * @return the test result
+ * @retval 0 on success
+ * @retval nonzero on failure
+ */
+static int run(void) {
+	int result = 0;
+
+	/*
+	 * The test itself.
+	 */
+
+	return result;
+}
