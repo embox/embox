b7899f127419e94a5d5b0aa4e7f544cac4e7885f
Nikolay Korotkiy <sikmir@gmail.com>
2010-03-26 12:07:24 +0000

    traps_core stub

---
+++ src/arch/sparc/kernel/traps_core.c
@@ -0,0 +1,34 @@
+/**
+ * @file
+ * @brief SPARC implementation traps framework
+ *
+ * @date 26.03.2010
+ * @author Nikolay Korotky
+ */
+
+#include <types.h>
+#include <embox/kernel.h>
+#include <hal/traps_core.h>
+
+static traps_env_t *old_env;
+static traps_env_t cur_env;
+
+void traps_enable(void) {
+
+}
+
+void traps_disable(void) {
+
+}
+
+void traps_save_env(traps_env_t *env) {
+
+}
+
+void traps_restore_env(traps_env_t *env) {
+
+}
+
+void traps_set_env(traps_env_t *env) {
+
+}
