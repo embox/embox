0eeda19627fd663b26ad873e4880c00601eaadc5
Fedor Burdun <ignwah@gmail.com>
2010-04-17 00:01:12 +0000

    Create simply test for page alloc as system test

--- src/tests/page_alloc/test_page_alloc.c
+++ src/tests/page_alloc/test_page_alloc.c
@@ -12,6 +12,9 @@
 
 #include <lib/page_alloc.h>
 
+#define TEST_COUNT 10
+#define TEST_STACK_SIZE	(2*TEST_COUNT+2)
+
 #if 0
 //#define DONOTUSE_PAGE_ALLOC           /* for testing test of page alloc =) */
 //#define INTERACTIVE_TEST                      /* */
@@ -22,64 +25,6 @@
 #define MAX_PAGE_FOR_ALLOC 0x10
 
 
-/**
- * set of page structure
- */
-typedef struct test_list {
-	pmark_t *page;
-	struct test_list *next;
-}test_list_t;
-
-/**
- * alloc set of page
- */
-test_list_t* page_set_alloc( int count ) {
-	test_list_t* fr = NULL;
-	test_list_t* tmp = NULL;
-	int i;
-	for (i=0;i<count;++i) {
-		#ifdef VERBOSE_DEBUG_OUT
-		printf("%d ",i);
-		#endif
-		tmp = malloc(sizeof(test_list_t));
-		#ifdef VERBOSE_DEBUG_OUT_MALLOC
-		printf("\t\t\tMALLOC (page set): %08x\n",tmp);
-		#endif
-		#ifndef DONOTUSE_PAGE_ALLOC
-		if (!(tmp->page = page_alloc())) {
-			#ifdef VERBOSE_DEBUG_OUT
-			printf("\t\tPAGE_ALLOC: No memory enough\n");
-			#endif
-			free(tmp);
-			return fr;
-		}
-		tmp->next = fr;
-		fr = tmp;
-		#endif
-	}
-	return fr;
-}
-
-/**
- * free set of page
- */
-void free_page_set(test_list_t *list) {
-	test_list_t* cur = list;
-	while (cur != NULL) {
-		#ifdef VERBOSE_DEBUG_OUT
-		printf("* ");
-		#endif
-		list = cur->next;
-		#ifndef DONOTUSE_PAGE_ALLOC
-		page_free(cur->page);
-		#endif
-		#ifdef VERBOSE_DEBUG_OUT_MALLOC
-		printf("\t\t\tFREE (page set): %08x\n",cur);
-		#endif
-		free(cur);
-		cur = list;
-	}
-}
 
 /**
  * count the number of free pages
@@ -119,85 +64,6 @@ size_t allow_page_count() {
 		cur = cur->pprev;
 	}
 	return PAGE_QUANTITY-fr;
-}
-
-/**
- * stack as list
- */
-typedef struct stack_ {
-	test_list_t* list;
-	struct stack_* next;
-}stack_t;
-
-stack_t *stack;
-
-/**
- * push in stack
- */
-void push(int count) {
-	#ifdef VERBOSE_DEBUG_OUT
-	printf("\tpush %d pages: ",count);
-	#endif
-	stack_t* tmp = malloc(sizeof(stack_t));
-	#ifdef VERBOSE_DEBUG_OUT_MALLOC
-	printf("\t\t\MALLOC (stack elem): %08x\n",tmp);
-	#endif
-	tmp->list = page_set_alloc(count);
-	tmp->next = stack;
-	stack = tmp;
-	#ifdef VERBOSE_DEBUG_OUT
-	printf("\n");
-	#endif
-}
-
-/**
- * pop from stack
- */
-void pop() {
-	if (stack==NULL) {
-		return;
-	}
-	printf("\tpop pages from ");
-	stack_t* tmp;
-
-	#ifdef STACK_POP_FROM_HEAD
-
-	printf("head: ");
-	tmp = stack->next;
-	free_page_set(stack->list);
-	#ifdef VERBOSE_DEBUG_OUT_MALLOC
-	printf("\t\t\FREE (stack elem): %08x\n",stack);
-	#endif
-	free(stack);
-	stack = tmp;
-
-	#else /* if STACK POP FROM TAIL */
-
-	printf("tail: ");
-
-	if (stack->next == NULL) {
-		tmp = stack->next;
-		free_page_set(stack->list);
-		#ifdef VERBOSE_DEBUG_OUT_MALLOC
-		printf("\t\t\FREE (stack elem): %08x\n",stack);
-		#endif
-		free(stack);
-		stack = tmp;
-	} else {
-		tmp = stack;
-		while (tmp->next->next != NULL) {
-			tmp = tmp->next;
-		}
-		free_page_set(tmp->next->list);
-		#ifdef VERBOSE_DEBUG_OUT_MALLOC
-		printf("\t\t\FREE (stack elem): %08x\n",stack);
-		#endif
-		free(tmp->next);
-		tmp->next = NULL;
-	}
-
-	#endif
-	printf("\n");
 }
 
 /**
@@ -241,68 +107,38 @@ void memory_check() {
 EMBOX_TEST(run);
 
 static int run(void) {
-	#if 0
-	printf("TEST PAGE_ALLOC NOW. COUNT OF PAGES: %ld\n",(long) PAGE_QUANTITY);
-	const int max_page_for_alloc = MAX_PAGE_FOR_ALLOC;
-	int test_count = TEST_COUNT;
-	int tpcount,tptype;
-	int i;
+	void * pointers[TEST_STACK_SIZE];
+	void **first,**last;
+	int test_id=0;
+	int callowed=0,cfree=0;
+	first = pointers; last = pointers;
 
-	page_alloc_init();
+	/* push 2 pop 1 - while it is best idea */
 
-	#ifdef INTERACTIVE_TEST
-	printf("Input: number of tests :: ");
-	scanf("%d",&test_count);
-	#endif
-	for (i=0;i<test_count;++i) {
-		#ifndef INTERACTIVE_TEST
-		tpcount = random() % max_page_for_alloc + 1;
-		tptype  = random() % 2;
-		#else
-		printf("Input: type action [ 0 - pop | 1 - push ] , number of page :: ");
-		scanf("%d %d",&tptype,&tpcount);
-		#endif
-
-		printf("Test #%d: ",i);
-		memory_check();
-
-		if (stack!=NULL) {
-			if (tptype) {
-			#ifdef VERBOSE_DEBUG_OUT
-				printf("PUSH:\n");
-			#endif
-				push(tpcount);
-			} else {
-			#ifdef VERBOSE_DEBUG_OUT
-				printf("POP:\n");
-			#endif
-				pop();
+	do {
+		if (++test_id<TEST_COUNT) {
+			if (NULL == (*last = page_alloc())) {
+				printf("No memory! ");
+				break;
 			}
-		} else {
-			printf("PUSH because have NULL:\n");
-			push(tpcount);
+			last = ++last < pointers+TEST_STACK_SIZE ? last : pointers;
+
+			if (NULL == (*last = page_alloc())) {
+				printf("No memory! ");
+				break;
+			}
+			last = ++last < pointers+TEST_STACK_SIZE ? last : pointers;
+
+			callowed += 2;
 		}
+		page_free(*first);
+		first = ++first < pointers+TEST_STACK_SIZE ? first : pointers;
+		cfree += 1;
 
-		#ifdef VERBOSE_DEBUG_OUT
-		do_allpage();
-		#endif
-	}
+	} while (first!=last); /* queue is not empty */
 
-	while (stack!=NULL) {
-		printf("(free last): ");
-		memory_check();
-		pop();
-		#ifdef VERBOSE_DEBUG_OUT
-		do_allpage();
-		#endif
-	}
+	printf("Allowed %d free %d: ",callowed,cfree);
 
-	printf("END: ");
-	memory_check();
-
-	printf("\n\nMEMORY BAD SITUATION: %d\n",count_of_error);
-
-	#endif
-	return 0;
+	return callowed == cfree ? 0 : 1;
 }
 
