c27a25d79367f22ad1c20635da20097aa2a5a243
Nikolay Korotkiy <sikmir@gmail.com>
2010-04-21 13:21:48 +0000

    Add linux-compat headers for types and printk

---
+++ src/linux-compat/include/linux/kernel.h
@@ -0,0 +1,6 @@
+#ifndef _LINUX_KERNEL_H
+#define _LINUX_KERNEL_H
+
+#include <kernel/printk.h>
+
+#endif
---
+++ src/linux-compat/include/linux/types.h
@@ -0,0 +1,6 @@
+#ifndef _LINUX_TYPES_H
+#define _LINUX_TYPES_H
+
+#include <types.h>
+
+#endif
