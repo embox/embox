210118a4111b1a1f4c8584b70600fb237feabfd0
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2010-04-27 17:43:59 +0000

    first draft of lib dependencies (#60)
    Add memcpy & memset from glibc as an example of library override
    feature implemented through dependencies (disabled so far).

--- mk/embuild.mk
+++ mk/embuild.mk
@@ -5,15 +5,24 @@
 ifndef _embuild_mk_
 _embuild_mk_ := 1
 
-EMBUILD_DUMP_MK := $(CODEGEN_DIR)/embuild_cache.mk
+#EMBUILD_DEBUG = 1
 
-#$(info Running EMBuild [$(MAKELEVEL)])
+ifdef EMBUILD_DEBUG
+$(info Running EMBuild [$(MAKELEVEL)])
+endif
 
 include $(MK_DIR)/traverse.mk
 include $(MK_DIR)/util.mk
 
+EMBUILD_DUMP_MK := $(CODEGEN_DIR)/embuild_cache.mk
+
 # Guard symbol. Use it only in contexts with $(dir) defined.
+ifndef EMBUILD_DEBUG
 _ = EMBUILD/$(abspath $(dir))/
+else
+_ = $(if $(dir),EMBUILD/$(abspath $(dir))/, \
+      $(error using guard symbol outside dir scope, dir value is $(value dir)))
+endif
 
 # Traverse always defines SELFDIR before entering sub-makefile.
 dir = $(SELFDIR)
@@ -94,8 +103,24 @@ __PACKAGES_PROCESS = $(info Processing packages) \
     $(eval $(value define_package_symbols)) \
   )
 
-# Canonical unit name is one with package prefix.
-canonize_name = \
+# Get list of all modules and libraries.
+unit_def = \
+  $(foreach unit,$1, \
+    $(unit) $(eval UNIT_DEFINED-$(unit) := $(dir)) \
+  )
+
+# Mods that are always included to the resulting image
+# (with their dependencies satisfied, of course).
+__MODS_CORE = $(info Listing essentials) $(call mod_collect,MODS_CORE)
+# Regular mods.
+__MODS           = $(info Listing mods) $(call mod_collect,MODS)
+
+mod_collect = $(sort \
+  $(foreach dir,$(DIRS),$(call unit_def,$(call canonize_mod_name,$($_$1)))) \
+)
+
+# Canonical mod name is one with package prefix.
+canonize_mod_name = \
   $(foreach name,$1, \
     $(call check_name,$(name)) \
     $(if $(findstring .,$(name)), \
@@ -104,42 +129,20 @@ canonize_name = \
     ) \
   )
 
-# Get list of all modules and libraries.
-unit_def = \
-  $(foreach unit,$1, \
-    $(unit) $(eval UNIT_DEFINED-$(unit) := $(dir)) \
-  )
-
-mod_collect = \
-  $(sort $(foreach dir,$(DIRS),$(call unit_def,$(call canonize_name,$($_$1)))))
-
-# Mods that are always included to the resulting image
-# (with their dependencies satisfied, of course).
-__MODS_CORE = $(info Listing essentials) $(call mod_collect,MODS_CORE)
-# Regular mods.
-__MODS           = $(info Listing mods) $(call mod_collect,MODS)
-
-lib_collect = $(sort $(foreach dir,$(DIRS),$(call unit_def,$($_$1))))
-
 # Libraries.
 __LIBS           = $(info Listing libs) $(call lib_collect,LIBS)
+
+lib_collect = $(sort $(foreach dir,$(DIRS),$(call unit_def,$($_$1))))
 
 # Common units handling: source assignments and flags.
 
 wildcard_srcs = $(wildcard $(1:%=$(dir)/%))
 
-unit_package = $(basename $(unit))
-unit_name = $(patsubst .%,%,$(suffix $(unit)))
+mod_package = $(basename $(unit))
+mod_name = $(patsubst .%,%,$(suffix $(unit)))
 
 unit_symbol = $($_$1-$(unit)) \
-  $(if $(filter $($_PACKAGE),$(unit_package)),$($_$1-$(unit_name)))
-
-#############################
-#  $(eval WARNING_SRCS += $(abspath $(src))) \
-#  $(eval WARNING_SRC-$(abspath $(src))-ctx_dir := $(dir)) \
-#  $(eval WARNING_SRC-$(abspath $(src))-ctx_unit := $(unit)) \
-#  $(eval WARNING_SRC-$(abspath $(src))-UNIT := $(UNIT-$(abspath $(src)))) \
-#  $(eval WARNING_SRC-$(abspath $(src))-DEFINED := $(UNIT-$(abspath $(src))-DEFINED))
+  $(if $(filter $($_PACKAGE),$(mod_package)),$($_$1-$(mod_name)))
 
 unit_srcs_check_warn = \
   $(info $(warning_str) \
@@ -188,7 +191,7 @@ define define_unit_symbols
   CFLAGS-$(unit)   := $(strip   $(CFLAGS-$(unit)))
 endef
 
-__UNITS_PROCESS = $(info Processing all units) \
+__UNITS_PROCESS = $(info Processing units) \
   $(foreach unit,$(MODS) $(LIBS), \
     $(eval $(value define_unit_symbols_prepare)) \
     $(foreach dir,$(DIRS), \
@@ -197,68 +200,80 @@ __UNITS_PROCESS = $(info Processing all units) \
     $(eval $(value define_unit_symbols)) \
   )
 
-# Here goes mods specific functions (dependencies and so on).
-unit = $(mod)
+# Deal with mods and libs dependencies
 
-# The sub-graph of all module dependencies (either direct or indirect).
-MOD_DEPS_DAG = $(sort $(call mod_deps_dag_walk,$1))
-mod_deps_dag_walk = $(foreach mod,$1,$(call $0,$(DEPS-$(mod))) $(mod))
-
-# User should list only existing mods in dependency list. Check it is true.
-mod_deps_filter = \
+# User should list only existing units in dependency list. Check it is true.
+# Params:
+#  1. Unit dependencies
+#  2. Set of allowed dependency values
+unit_deps_filter = \
   $(foreach dep, \
-    $(if $(filter-out $(MODS),$1), \
-      $(info $(warning_str) Undefined dependencies for mod $(mod):: \
-        $(filter-out $(MODS),$1)) \
-      $(filter $(MODS),$1), \
-      $1 \
-    ), \
-    $(eval MOD-$(mod)-DEP-$(dep)-DEFINED := $(dir)) \
+    $(or $(and $(filter-out $2,$1), \
+               $(info $(warning_str) Undefined dependencies for unit $(unit):: \
+                 $(filter-out $2,$1)) $(filter $2,$1) \
+         ),$1), \
+    $(eval UNIT-$(unit)-DEP-$(dep)-DEFINED := $(dir)) \
     $(dep) \
   )
 
-define define_mod_symbols_prepare
-  DEPS-$(mod) :=
+mod_deps_filter = $(call unit_deps_filter,$(call canonize_mod_name,$1),$(MODS))
+lib_deps_filter = $(call unit_deps_filter,$1,$(LIBS))
+
+define define_deps_symbols_prepare
+  DEPS-$(unit) :=
 endef
 
-define define_mod_symbols_per_directory
-  DEPS-$(mod) := $(DEPS-$(mod)) \
-    $(call mod_deps_filter,$(call canonize_name,$(call unit_symbol,DEPS)))
+define define_deps_symbols_per_directory
+  DEPS-$(unit) := $(DEPS-$(unit)) $(call $(deps_filter),$(call unit_symbol,DEPS))
 endef
 
-define define_mod_symbols
-  DEPS-$(mod) := $(sort $(DEPS-$(mod)))
+define define_deps_symbols
+  DEPS-$(unit) := $(sort $(DEPS-$(unit)))
 endef
 
-# After dependency info has been collected for all mods we should check that
-# the dependency graph is true DAG.
-mod_detect_cycle_deps = \
-  $(if $(filter $(mod),$1), \
+# Invokes define_deps_symbols_xxx in right order for each unit from the
+# specified list.
+# Params:
+#  1. Units list
+#  2. Dependency filter callback var name
+invoke_define_deps_symbols = \
+  $(foreach unit,$(1),$(foreach deps_filter,$(2), \
+    $(eval $(value define_deps_symbols_prepare)) \
+    $(foreach dir,$(DIRS),$(eval $(value define_deps_symbols_per_directory))) \
+    $(eval $(value define_deps_symbols)) \
+  ))
+
+# After dependency info has been collected for all units we should check that
+# the dependency graphs are true DAGs.
+deps_detect_cycles = \
+  $(if $(filter $(unit),$1), \
     $(foreach pair, \
-        $(call mod_dep_pairs,$2 $(lastword $3),$3 $(firstword $2)), \
-      $(info $(call error_str_dir,$(MOD-$(subst /,-DEP-,$(pair))-DEFINED)) \
+        $(call unit_dep_pairs,$2 $(lastword $3),$3 $(firstword $2)), \
+      $(info $(call error_str_dir,$(UNIT-$(subst /,-DEP-,$(pair))-DEFINED)) \
         Cyclic dependency definition here:: $(subst /, -> ,$(pair))) \
     ) \
-    $(error Dependency cycle:: $(mod) $(strip $(3:%= -> %))), \
-    $(foreach parent,$(if $(m),$(m),$(mod)),$(foreach m,$1,\
-      $(call $0,$(DEPS-$(m)), \
-        $2 $(parent),$3 $(m)) \
+    $(error Dependency cycle:: $(unit) $(strip $(3:%= -> %))), \
+    $(foreach parent,$(if $(u),$(u),$(unit)),$(foreach u,$1,\
+      $(call $0,$(DEPS-$(u)), \
+        $2 $(parent),$3 $(u)) \
     )) \
-  )
-mod_dep_pairs = $(join $(1:%=%/),$(2))
+  ) # I don't understand anymore how does this shit work but it does, trust me.
+unit_dep_pairs = $(join $(1:%=%/),$(2))
 
-# Define dependency info for each mod and then perform graph check.
-__MODS_PROCESS = $(info Processing mod deps) \
-  $(foreach mod,$(MODS), \
-    $(eval $(value define_mod_symbols_prepare)) \
-    $(foreach dir,$(DIRS), \
-      $(eval $(value define_mod_symbols_per_directory)) \
-    ) \
-    $(eval $(value define_mod_symbols)) \
-  ) \
-  $(foreach mod,$(MODS), \
-    $(call mod_detect_cycle_deps,$(DEPS-$(mod))) \
+# Define dependency info for each unit and then perform graph check.
+__DEPS_PROCESS = $(info Processing dependencies) \
+  $(call invoke_define_deps_symbols,$(MODS),mod_deps_filter) \
+  $(call invoke_define_deps_symbols,$(LIBS),lib_deps_filter) \
+  $(foreach unit,$(MODS) $(LIBS), \
+    $(call deps_detect_cycles,$(DEPS-$(unit))) \
   )
+
+# The sub-graph of all module dependencies (either direct or indirect).
+MOD_DEPS_DAG = $(sort $(call deps_dag_walk,$1))
+deps_dag_walk = $(foreach unit,$1,$(call $0,$(DEPS-$(unit))) $(unit))
+
+t-sort = \
+
 
 # The real work is done here.
 # This code is evaluated during symbol cache generation.
@@ -270,11 +285,16 @@ MODS_CORE := $(__MODS_CORE)
 MODS := $(sort $(__MODS) $(MODS_CORE))# Essential mods are so essential...
 LIBS := $(__LIBS)
 $(__UNITS_PROCESS)
-$(__MODS_PROCESS)
+$(__DEPS_PROCESS)
+# TODO perform t-sort on LIBS
 $(info Done.)
 else
 # If possible, get information from cache.
+ifneq ($(wildcard $(EMBUILD_DUMP_MK)),)
+include $(EMBUILD_DUMP_MK)
+else
 -include $(EMBUILD_DUMP_MK)
+endif
 endif
 
 # Perform units postprocessing: expand objects and define rules for them.
@@ -283,9 +303,9 @@ define define_unit_obj_rules
   OBJS-$(unit) := $(call SRC_TO_OBJ,$(SRCS-$(unit)))
   $(OBJS-$(unit)) : unit := $(unit)
   $(OBJS-$(unit)) : override CPPFLAGS += \
-     $(CPPFLAGS-$(unit_package)) $(CPPFLAGS-$(unit)) $(CPPFLAGS-$(abspath $@))
+                                $(CPPFLAGS-$(unit)) $(CPPFLAGS-$(abspath $@))
   $(OBJS-$(unit)) : override CFLAGS += \
-           $(CFLAGS-$(unit_package)) $(CFLAGS-$(unit)) $(CFLAGS-$(abspath $@))
+                                    $(CFLAGS-$(unit)) $(CFLAGS-$(abspath $@))
   $(OBJS-$(unit)) : override CPPFLAGS := $(strip $(CPPFLAGS))
   $(OBJS-$(unit)) : override CFLAGS := $(strip $(CFLAGS))
 endef
@@ -294,6 +314,8 @@ define define_lib_rules
 	$(AR) $(ARFLAGS) $@ $(^:%= \$N	%)
 endef
 define define_mod_obj_rules
+  $(OBJS-$(unit)) : override CPPFLAGS += $(CPPFLAGS-$(mod_package))
+  $(OBJS-$(unit)) : override CFLAGS   += $(CFLAGS-$(mod_package))
   $(OBJS-$(unit)) : override CPPFLAGS += -D__EMBUILD_MOD__='$(subst .,$$,$(unit))'
 endef
 
@@ -328,12 +350,13 @@ $(foreach mod,$(MODS_ENABLE),$(if $(RUNLEVEL-$(mod)),, \
   $(eval RUNLEVEL-$(mod) := 2)\
 ))
 
+ifdef EMBUILD_DEBUG
 __print_units = \
   $(if $(foreach e,$2, \
     $(info ($1) $e $(if $3,	(defined in $(UNIT_DEFINED-$e))))x \
   ),,$(info (none)))
 
-#print_units = \
+print_units = \
   $(info All unints: ) \
   $(call __print_units,MOD,$(MODS),defined) \
   $(call __print_units,LIB,$(LIBS),defined)
@@ -347,11 +370,12 @@ print_units += \
   $(info Mods included to satisfy dependencies: ) \
   $(call __print_units,DEPS,\
     $(filter-out $(MODS_ENABLE) $(MODS_CORE),$(MODS_BUILD)))
+endif
 
 image_init:
 	$(strip \
 		$(print_undefined_mods) \
-		$(do_not_print_units) \
+		$(print_units) \
 	)
 
 # Here goes dump generating stuff needed to speed-up a build.
@@ -378,7 +402,7 @@ else
 	@$(PRINTF) '$(call dump_var_symbol,SRCS,$(MODS) $(LIBS))' >> $@
 	@$(PRINTF) '$(call dump_var_symbol,CPPFLAGS,$(MODS) $(LIBS))' >> $@
 	@$(PRINTF) '$(call dump_var_symbol,CFLAGS,$(MODS) $(LIBS))' >> $@
-	@$(PRINTF) '$(call dump_var_symbol,DEPS,$(MODS))' >> $@
+	@$(PRINTF) '$(call dump_var_symbol,DEPS,$(MODS) $(LIBS))' >> $@
 	@$(PRINTF) '$(call dump_var,SUBDIRS_LDFLAGS)' >> $@
 	@$(PRINTF) '$(call dump_var_symbol,UNIT_DEFINED,$(MODS) $(LIBS))' >> $@
 endif
