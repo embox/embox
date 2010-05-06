f83c17d49397667ba0d45d81b25923cdcba550a5
Anton Bondarev <anton.bondarev1978@gmail.com>
2010-05-06 11:43:05 +0000

    Change pointing system devices (timer ind irq_ctrl)
    Fixes #86.

---
+++ src/drivers/interrupts/mb_interrupt.c
@@ -0,0 +1,94 @@
+/**
+ * @file
+ *
+ * @brief Low level functions for interrupt controller
+ *
+ * @date 23.11.2009
+ * @author: Anton Bondarev
+ */
+
+#include <types.h>
+#include <bitops.h>
+#include <hal/interrupt.h>
+#include <embox/kernel.h>
+#include <asm/cpu_conf.h>
+
+/**
+ * Microblaze interrupt controller registers definitions.
+ */
+typedef volatile struct irqc_regs {
+	uint32_t isr; /**< interrupt status register */
+	uint32_t ipr; /**< interrupt pending register */
+	uint32_t ier; /**< interrupt enable register */
+	uint32_t iar; /**< interrupt acknowledge register */
+	uint32_t sie; /**< set interrupt enable bits */
+	uint32_t cie; /**< clear interrupt enable bits */
+	uint32_t ivr; /**< interrupt vector register */
+	uint32_t mer; /**< master enable register */
+} irqc_regs_t;
+
+#define MER_HIE_BIT     30
+#define MER_ME_BIT      31
+
+#define MER_HIE              REVERSE_MASK(MER_HIE_BIT)
+#define MER_ME               REVERSE_MASK(MER_ME_BIT)
+
+static irqc_regs_t * irqc = (irqc_regs_t * )XILINX_INTC_BASEADDR;
+
+void interrupt_init(void){
+	irqc->mer = 0;
+	irqc->ier = 0;
+	irqc->iar = ~(0x0);
+	/* after set HIE bit we will not can clear it again.
+	 * It's a problem because we will couldn't testing interrupts by soft
+	 * (we will could use function irqc_force).
+	 */
+	irqc->mer = MER_HIE | MER_ME;
+}
+
+void interrupt_enable(interrupt_nr_t irq_num){
+	set_bit(irqc->ier, irq_num);
+}
+
+void interrupt_disable(interrupt_nr_t irq_num){
+	clear_bit(irqc->ier, irq_num);
+}
+
+
+
+//TODO this not set in microblaze
+void interrupt_force(interrupt_nr_t irq_num){
+
+}
+
+void interrupt_clear(interrupt_nr_t irq_num){
+	set_bit(irqc->iar,irq_num);
+}
+
+/*
+ * Microblaze specific functions:
+ */
+
+__interrupt_mask_t irqc_get_isr_reg(void) {
+	return irqc->isr;
+}
+
+void irqc_set_mask(__interrupt_mask_t mask){
+	irqc->ier = mask;
+}
+
+__interrupt_mask_t irqc_get_mask(void){
+	return irqc->ier;
+}
+
+/* Following functions never be used */
+#if 0
+int irqc_get_status(interrupt_nr_t irq_num){
+	return get_bit(irqc->ier, irq_num);
+}
+
+int irqc_disable_all(void){
+	irqc->mer &= ~MER_ME;
+	return 0;
+}
+#endif
---
+++ src/drivers/timers/mb_timer.c
@@ -0,0 +1,95 @@
+/**
+ * @file
+ *
+ * @brief Implementation for microblaze timers
+ *
+ * @date 19.11.2009
+ * @author Anton Bondarev
+ */
+
+#include <types.h>
+#include <bitops.h>
+#include <kernel/irq.h>
+#include <kernel/panic.h>
+#include <asm/cpu_conf.h>
+#include <hal/clock.h>
+
+#define CONFIG_SYS_TIMER_PRELOAD     (CPU_CLOCK_FREQ/1000)
+
+/*bits definition of cntl/status (tcsr) register*/
+#define TIMER_ENALL_BIT  21      /**< ENALL */
+#define TIMER_PWM_BIT    22      /**< PWM */
+#define TIMER_INT_BIT    23      /**< TxINT */
+#define TIMER_ENT_BIT    24      /**< ENT */
+#define TIMER_ENIT_BIT   25      /**< ENIT */
+#define TIMER_LOAD_BIT   26      /**< LOAD */
+#define TIMER_ARHT_BIT   27      /**< ARHT */
+#define TIMER_CAPT_BIT   28      /**< CAPT */
+#define TIMER_GENT_BIT   29      /**< GENT */
+#define TIMER_UDT_BIT    30      /**< UDT */
+#define TIMER_MDT        31      /**< MDT */
+
+/** enable both timers t0 and t1. clearing this bit isn't change state ENT bit */
+#define TIMER_ENABLE_ALL    REVERSE_MASK(TIMER_ENALL_BIT)
+/** interrupt was pending. Write '1' for clearing this bit*/
+#define TIMER_INT           REVERSE_MASK(TIMER_INT_BIT)
+/** enable timer*/
+#define TIMER_ENABLE        REVERSE_MASK(TIMER_ENT_BIT)
+/** enable timer interrupt*/
+#define TIMER_INT_ENABLE    REVERSE_MASK(TIMER_ENIT_BIT)
+/** load value from tlr register*/
+#define TIMER_RESET         REVERSE_MASK(TIMER_LOAD_BIT)
+/** set reload mode*/
+#define TIMER_RELOAD        REVERSE_MASK(TIMER_ARHT_BIT)
+/** set down count mode*/
+#define TIMER_DOWN_COUNT    REVERSE_MASK(TIMER_UDT_BIT)
+
+/**
+ * Structure one of two timers. Both timers need only for pwm mode
+ */
+typedef volatile struct timer_regs {
+	uint32_t tcsr; /**< control/status register TCSR */
+	uint32_t tlr; /**< load register TLR */
+	uint32_t tcr; /**< timer/counter register */
+} timer_regs_t;
+
+/**
+ * Microblaze timer module contains two timer module, each of them is described
+ * in @link srtuct timer_regs @endlink
+ */
+typedef volatile struct mb_timers {
+	timer_regs_t tmr0;
+	timer_regs_t tmr1;
+} mb_timers_t;
+
+static mb_timers_t *timers = (mb_timers_t *) XILINX_TIMER_BASEADDR;
+#define timer0 (&timers->tmr0)
+
+
+/*we must use proxy for interrupt handler because we must clean bit in register
+ * timer.
+ */
+static irq_return_t clock_handler(irq_nr_t irq_nr, void *dev_id) {
+	timer0->tcsr |= TIMER_INT;
+	// XXX Anton irq_func_tmr_1mS bad style
+	clock_tick_handler(irq_nr,dev_id);
+	return IRQ_HANDLED;
+}
+
+void clock_init(void) {
+	/*set clocks period*/
+	timer0->tlr = CONFIG_SYS_TIMER_PRELOAD;
+	/*clear interrupts bit and load value from tlr register*/
+	timer0->tcsr = TIMER_INT | TIMER_RESET;
+	/*start timer*/
+	timer0->tcsr = TIMER_ENABLE | TIMER_INT_ENABLE | TIMER_RELOAD
+			| TIMER_DOWN_COUNT;
+
+	if (0 != irq_attach(XILINX_TIMER_IRQ, clock_handler, 0, NULL, "mbtimer")) {
+		panic("mbtimer irq_attach failed");
+	}
+}
+
+void clock_setup(useconds_t useconds) {
+
+}
