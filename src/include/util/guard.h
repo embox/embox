4a004476c1d5761100675e06eed7fec88fc22e0d
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2010-06-20 18:53:42 +0000

    Documenting guard util

--- src/include/util/guard.h
+++ src/include/util/guard.h
@@ -1,6 +1,9 @@
 /**
  * @file
- * @brief TODO
+ * @brief Simple facility for producing unique symbols.
+ * @details Guards are useful when defining multiple "anonymous" (named using
+ * the same pattern) variables. In such cases guard prevents "variable
+ * redefinition" compile-time error.
  *
  * @date 14.06.2010
  * @author Eldar Abusalimov
@@ -11,7 +14,20 @@
 
 #include <impl/util/guard.h>
 
+/**
+ * Prepends guard to the specified symbol.
+ *
+ * @param s symbol (or macro which expand to symbol) to protect
+ * @return symbol prefixed with the guard
+ */
 #define GUARD_PREFIX(s) __GUARD_PREFIX(s)
+
+/**
+ * Appends guard to the specified symbol.
+ *
+ * @param s symbol (or macro which expand to symbol) to protect
+ * @return symbol suffixed with the guard
+ */
 #define GUARD_SUFFIX(s) __GUARD_SUFFIX(s)
 
 #endif /* UTIL_GUARD_H_ */
