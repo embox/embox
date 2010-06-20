1a3ce684a7dbf418b08a27a4cce9dc6986e77384
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2010-06-20 18:38:20 +0000

    Refactoring diffuse arrays implementation

--- src/include/impl/util/array.h
+++ src/include/impl/util/array.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief TODO
+ * @brief Implements distributed arrays backed by linker section SORT facility.
  *
  * @date 13.06.2010
  * @author Eldar Abusalimov
@@ -10,6 +10,8 @@
 # error "Do not include this file directly, use <util/array.h> instead!"
 #endif /* UTIL_ARRAY_H_ */
 
+#include <stddef.h>
+
 #include <util/macro.h>
 #include <util/guard.h>
 
@@ -18,11 +20,11 @@
  * placed together successively each after each.
  */
 
-/* The array symbol is placed in HEAD section. */
+/* The symbol for the array name is placed in head section. */
 #define __ARRAY_DIFFUSE_SECTION_HEAD(s_array) \
 		__ARRAY_DIFFUSE_SECTION(s_array, 0, head)
 
-/* BODY hosts the array elements. */
+/* Body hosts the array elements (excepting terminator, if any). */
 #define __ARRAY_DIFFUSE_SECTION_BODY(s_array) \
 		__ARRAY_DIFFUSE_SECTION(s_array, 1, body)
 
@@ -42,41 +44,31 @@
 #define __ARRAY_DIFFUSE_SECTION(s_array, order, tag) \
 		".array_diffuse." MACRO_STRING(s_array##_##order##_##tag) ".rodata"
 
-/* Each array entry is placed in a variable with following name. */
-#define __ARRAY_DIFFUSE_ENTRY(s_array, s_guard) \
-	GUARD_SUFFIX(__array_diffuse_entry__##s_array##__##s_guard)
+/* Every array entry, group of entries or marker symbol is backed by an
+ * individual array (empty for markers) defined as follows
+ *  __extension__ bypasses compiler warnings about empty arrays*/
+#define __ARRAY_DIFFUSE_ENTRY_DEF(type, s_entry, _section) \
+	__extension__ type s_entry[] __attribute__ ((used, section(_section)))
 
-#define __ARRAY_DIFFUSE_ENTRY_DEF(s_array, s_guard, _section) \
-	static typeof(s_array[0]) __ARRAY_DIFFUSE_ENTRY(s_array, s_guard) \
-		__attribute__ ((used, section(_section)))
+/* Diffuse arrays implementation-private entries are named as follows
+ * to prevent namespace pollution. */
+#define __ARRAY_DIFFUSE_PRIVATE(s_array, s_tag) \
+	__array_diffuse_private__##s_array##__##s_tag
 
-/* Marker is just zero-size variable. Prefix prevents namespace pollution. */
-#define __ARRAY_DIFFUSE_MARKER(s_array, s_guard) \
-	GUARD_SUFFIX(__array_diffuse_entry__##s_array##__##s_guard)
-
-#define __ARRAY_DIFFUSE_MARKER_DEF(type, s_array, s_guard, _section) \
-		__ARRAY_DIFFUSE_MARKER_RAW_DEF(type, \
-			__ARRAY_DIFFUSE_MARKER(s_array, s_guard), _section)
-
-#define __ARRAY_DIFFUSE_MARKER_RAW_DEF(type, s_marker, _section) \
-	__extension__ type s_marker[0] __attribute__ ((used, section(_section)))
-
-/* Common part included in each array definition. */
-#define __ARRAY_DIFFUSE_COMMON_DEF(element_type, array_name) \
-		__ARRAY_DIFFUSE_MARKER_RAW_DEF(element_type, array_name, \
-				__ARRAY_DIFFUSE_SECTION_HEAD(array_name))
+#define __ARRAY_DIFFUSE_PRIVATE_DEF(type, s_array, s_tag, section) \
+		__ARRAY_DIFFUSE_ENTRY_DEF(type, \
+				__ARRAY_DIFFUSE_PRIVATE(s_array, s_tag), section)
 
 /* Here goes API implementation. */
 
 #define __ARRAY_DIFFUSE_DEF(element_type, array_name) \
-		__ARRAY_DIFFUSE_COMMON_DEF(element_type, array_name); \
-		__ARRAY_DIFFUSE_MARKER_DEF(element_type, array_name, tail, \
-				__ARRAY_DIFFUSE_SECTION_TAIL(array_name))
+		__ARRAY_DIFFUSE_DEF_TERMINATED(element_type, array_name, /* empty */)
 
 #define __ARRAY_DIFFUSE_DEF_TERMINATED(element_type, array_name, terminator) \
-		__ARRAY_DIFFUSE_COMMON_DEF(element_type, array_name); \
-		__ARRAY_DIFFUSE_ENTRY_DEF(array_name, tail, \
-				__ARRAY_DIFFUSE_SECTION_TAIL(array_name)) = terminator
+		__ARRAY_DIFFUSE_ENTRY_DEF(element_type, array_name, \
+				__ARRAY_DIFFUSE_SECTION_HEAD(array_name)) = { /* empty */ }; \
+		__ARRAY_DIFFUSE_PRIVATE_DEF(element_type, array_name, tail, \
+				__ARRAY_DIFFUSE_SECTION_TAIL(array_name)) = { terminator }
 
 #define __ARRAY_DIFFUSE_DEF_STATIC(element_type, array_name) \
 		__ARRAY_DIFFUSE_DEF(static element_type, array_name)
@@ -84,7 +76,20 @@
 #define __ARRAY_DIFFUSE_DEF_TERMINATED_STATIC(element_type, name, terminator) \
 		__ARRAY_DIFFUSE_DEF_TERMINATED(static element_type, name, terminator)
 
-#define __ARRAY_DIFFUSE_ADD(array_name, element) \
-	__ARRAY_DIFFUSE_ENTRY_DEF(array_name, TODO, \
-			__ARRAY_DIFFUSE_SECTION_BODY(array_name)) = element
+#define __ARRAY_DIFFUSE_ADD(array_name, ...) \
+		__ARRAY_DIFFUSE_ADD_NAMED(array_name, \
+				GUARD_SUFFIX(__ARRAY_DIFFUSE_PRIVATE(array_name, entry)), \
+				__VA_ARGS__)
+
+#define __ARRAY_DIFFUSE_ADD_NAMED(array_name, ptr_name, ...) \
+		__ARRAY_DIFFUSE_ENTRY_DEF(static typeof(array_name[0]), ptr_name, \
+				__ARRAY_DIFFUSE_SECTION_BODY(array_name)) = { __VA_ARGS__ }
+
+#define __ARRAY_DIFFUSE_SIZE(array_name) __extension__ ({ \
+		extern typeof(array_name) __ARRAY_DIFFUSE_PRIVATE(array_name, tail); \
+		(size_t) (__ARRAY_DIFFUSE_PRIVATE(array_name, tail) - array_name); \
+	})
+
+#define __ARRAY_SIZE(array_name) \
+	(sizeof(array_name) / sizeof((array_name)[0]))
 
