30de9ca9e7bdc6dca2cab47b164e59c071aab7d5
Alexandr Batyukov <batsun@gmail.com>
2010-06-28 16:52:58 +0000

    Nothing significant

--- src/tests/divzero/divzero.c
+++ src/tests/divzero/divzero.c
@@ -2,7 +2,7 @@
  * @file
  *
  * @date 26.05.2010
- * @author sunnya
+ * @author Alexander Batyukov, Anton Kozlov
  */
 
 #include <types.h>
@@ -15,7 +15,8 @@
 EMBOX_TEST(run);
 
 static volatile uint32_t a = 17;
-static was_in_trap = 0;
+static int was_in_trap = 0;
+
 /* MMU data access exception handler */
 static int dfault_handler(uint32_t trap_nr, void *data) {
 	TRACE("\n ]:-> division by zero\n");
@@ -38,6 +39,7 @@ static int run() {
 
 	traps_restore_env(&old_env);
 
-	TRACE("\t\ta = %d\n", a);
+	/* TRACE("\t\ta = %d\n", a); */
+
 	return !was_in_trap;
 }
