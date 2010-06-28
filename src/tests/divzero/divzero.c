a2278a1621da9cafbd9a876a638ab2fe02d13dae
Anton Kozlov <drakon.mega@gmail.com>
2010-06-28 15:03:09 +0000

    Improve test

--- src/tests/divzero/divzero.c
+++ src/tests/divzero/divzero.c
@@ -14,17 +14,19 @@
 /* declare test in system */
 EMBOX_TEST(run);
 
-#define DIV_BY_ZERO     0x06
+#define DIV_BY_ZERO     0x2a
 
+static volatile uint32_t a = 17;
+static was_in_trap = 0;
 /* MMU data access exception handler */
 static int dfault_handler(uint32_t trap_nr, void *data) {
 	TRACE("\n ]:-> division by zero\n");
+	was_in_trap = 1;
 	/* skip instruction */
 	return 0;
 }
 
 static int run() {
-	volatile uint32_t a = 17;
 	volatile uint32_t zero = 0;
 
 	traps_env_t old_env;
@@ -39,5 +41,5 @@ static int run() {
 	traps_restore_env(&old_env);
 
 	TRACE("\t\ta = %d\n", a);
-	return a;
+	return !was_in_trap;
 }
