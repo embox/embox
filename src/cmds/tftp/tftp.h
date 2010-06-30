e3591ac0afa446f063ed393f0679c656895b1614
Nikolay Korotkiy <sikmir@gmail.com>
2010-06-30 21:00:36 +0000

    usage help

--- src/cmds/tftp/tftp.h
+++ src/cmds/tftp/tftp.h
@@ -15,7 +15,7 @@
 /*
  * Trivial File Transfer Protocol (IEN-133)
  */
-#define SEGSIZE 200       /* data segment size */
+#define SEGSIZE 512       /* data segment size */
 #define PKTSIZE SEGSIZE+4 /* full packet size */
 
 #define TFTP_TRANSFER_PORT 69 /* default well known port */
