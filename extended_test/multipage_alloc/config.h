12a3df64d5e868bf5ee2515d178d286bab8146b4
Fedor Burdun <ignwah@gmail.com>
2010-07-02 12:13:46 +0000

    Merge multipage allocators. #137

---
+++ extended_test/multipage_alloc/config.h
@@ -0,0 +1,11 @@
+
+//#define DONOTUSE_PAGE_ALLOC 		/* for testing test of page alloc =) */
+#define INTERACTIVE_TEST			/* */
+//#define VERBOSE_DEBUG_OUT			/* Some advanced output (list of page marks) */
+//#define VERBOSE_DEBUG_OUT_MALLOC
+#define STACK_POP_FROM_HEAD		/* or tail */ // I'm not sure that is working
+#define TEST_COUNT 1000
+#define MAX_PAGE_FOR_ALLOC 0x7ff
+#define CONFIG_PAGE_SIZE 0x1000
+#define PAGE_QUANTITY 0xffff
+
