e708baba8b6bd70b6d419267168ce9b520412398
Anton Bondarev <anton.bondarev1978@gmail.com>
2010-07-04 14:29:49 +0000

    microblaze MMU interface (#141)

--- src/arch/microblaze/include/asm/hal/mm/mmu_core.h
+++ src/arch/microblaze/include/asm/hal/mm/mmu_core.h
@@ -12,9 +12,7 @@
 
 #include <types.h>
 #include <bitops.h>
-
-/* quantity of utlb records */
-#define UTLB_QUANTITY_RECORDS    0x40
+#include <hal/mm/mmu_types.h>
 
 
 #define RTLBLO_I_BIT           REVERSE_BIT(29)
@@ -47,32 +45,6 @@
 	var = (virt_addr | (size << RTLBHI_SIZE_BIT) | \
 			(1 << RTLBHI_V_BIT))
 
-/** Microblaze specific MMU table record */
-typedef struct __mmu_table_record {
-	uint32_t tlbhi;  /**< register rtlbhi */
-	uint32_t tlblo;  /**< register rtlblo */
-} __mmu_table_record_t;
 
-typedef __mmu_table_record_t __mmu_table_t[UTLB_QUANTITY_RECORDS];
-
-typedef uint32_t __mmu_page_flags_t;
-
-typedef uint32_t __mmu_paddr_t;
-typedef uint32_t __mmu_vaddr_t;
-typedef uint32_t __mmu_pgd_t;
-typedef uint16_t __mmu_ctx_t; /* 256 process id and error error code*/
-
-/**
- * Describes structure for MMU environment for Microblaze architecture.
- */
-typedef struct __mmu_env {
-	uint32_t status;                    /**< MMU enabled/disabled */
-
-	__mmu_table_record_t * utlb_table;  /**< Pointer to utlb content */
-
-	size_t data_fault_cnt;              /**< Counter for data page faults */
-	size_t inst_fault_cnt;              /**< Counter for instruction page faults */
-	__mmu_vaddr_t fault_addr;           /**< Last fault address */
-}__mmu_env_t;
 
 #endif /* MICROBLAZE_MMU_CORE_H_ */
