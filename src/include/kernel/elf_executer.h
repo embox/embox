91d89938efb08592a33303daa95c6422c6b566ec
Dmitry Avdyukhin <dimonbv@gmail.com>
2010-07-15 12:31:04 +0000

    Elf executer. Maybe works. Nobody knows

---
+++ src/include/kernel/elf_executer.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @brief Describes methods to execute elf files.
+ *
+ * @date 15.07.2010
+ * @author Avdyukhin Dmitry
+ *
+ */
+
+#ifndef ELF_EXECUTER_H_
+#define ELF_EXECUTER_H_
+
+
+
+#endif /* ELF_EXECUTER_H_ */
---
+++ src/kernel/elf_executer.c
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 15.07.2010
+ * @author Avdyukhin Dmitry
+ *
+ */
+
+#include <lib/elf_reader.h>
+#include <string.h>
+#include <types.h>
+#include <errno.h>
+#include <stdio.h>
+
+int elf_execute(FILE *file) {
+	Elf32_Ehdr *EH;
+	Elf32_Phdr *EPH;
+	int counter;
+	int ent_size;
+	int (*function_main)(int argc, char *argv[]);
+	int result;
+	EH = (Elf32_Ehdr *)file;
+	/* TODO bad sizes. */
+	EPH = (Elf32_Phdr *)((char *)EH + EH->e_phoff);
+
+	counter = EH->e_phnum;
+	printf("\n");
+	while(counter--) {
+		ent_size = EH->e_phentsize;
+		if (EPH->p_type == 1) { /* Type = PT_LOAD. */
+			/* Physical address equals to virtual. */
+			memcpy((void *)EPH->p_vaddr, (char *)EH + EPH->p_offset, EPH->p_memsz);
+		}
+		EPH += 1;
+	}
+
+	printf("\nData allocated. \nTrying to start at %ld(0x%x)\n", EH->e_entry, EH->e_entry);
+
+	function_main = (int (*)(int argc, char *argv[]))EH->e_entry;
+	result = (*function_main) (0, 0);
+
+	printf("\n result : %d\n", result);
+	return 0;
+}
