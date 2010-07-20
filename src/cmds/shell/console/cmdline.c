efb28d4713322d61d85be45fc7f8d7270eb53341
Fedor Burdun <ignwah@gmail.com>
2010-07-20 14:37:17 +0000

    Try fix

---
+++ src/cmds/shell/console/cmdline.c
@@ -0,0 +1,209 @@
+/**
+ * @file
+ * @brief Internal representation of command line entity.
+ *
+ * @date 08.02.2009
+ * @author Eldar Abusalimov
+ */
+#include "cmdline.h"
+#include <math.h>
+#include <string.h>
+#include <embox/kernel.h>
+
+CMDLINE * cmdline_init(CMDLINE *this) {
+	CMDLINE_HISTORY *history;
+	if (this == NULL) {
+		return NULL;
+	}
+
+	history = this->history;
+	history->array[0][0] = '\0';
+	history->array[1][0] = '\0';
+	history->index = 0;
+	this->string[0] = '\0';
+	this->length = 0;
+	this->cursor = 0;
+	this->history_cursor = 0;
+	this->is_insert_mode = 0;
+	return this;
+}
+
+static bool cmdline_history_move_to(CMDLINE *cmdline, int to) {
+	int new_pos;
+	CMDLINE_HISTORY * history = cmdline->history;
+	if (to < 0) {
+		to = to + (-to / CMDLINE_HISTORY_SIZE + 1) * CMDLINE_HISTORY_SIZE;
+	}
+	new_pos = to % CMDLINE_HISTORY_SIZE;
+	if (cmdline->history_cursor == new_pos) {
+		return false;
+	}
+	if (history->array[new_pos][0] == '\0' && new_pos != history->index) {
+		return false;
+	}
+
+	if (cmdline->history_cursor == history->index) {
+		strncpy(history->array[history->index], cmdline->string,
+				array_len(history->array[history->index]));
+	}
+
+	cmdline->history_cursor = new_pos;
+	strcpy(cmdline->string, history->array[new_pos]);
+	cmdline->length = strlen(cmdline->string);
+	cmdline->cursor = cmdline->length;
+	return true;
+}
+
+inline static bool cmdline_history_move_by(CMDLINE *cmdline, int by) {
+	return cmdline_history_move_to(cmdline, cmdline->history_cursor + by);
+}
+
+inline bool cmdline_history_backward(CMDLINE *cmdline) {
+	return cmdline_history_move_by(cmdline, -1);
+}
+
+inline bool cmdline_history_forward(CMDLINE *cmdline) {
+	return cmdline_history_move_by(cmdline, 1);
+}
+
+bool cmdline_history_new_entry(CMDLINE *cmdline) {
+	CMDLINE_HISTORY * history = cmdline->history;
+	if (cmdline->string[0] == '\0') {
+		return false;
+	}
+
+	if (0 == strcmp(cmdline->string, history->array[(history->index
+			+ CMDLINE_HISTORY_SIZE - 1) % CMDLINE_HISTORY_SIZE])) {
+		cmdline->length = 0;
+		cmdline->cursor = 0;
+			cmdline->string[0] = '\0';
+		return false;
+	}
+
+	strcpy(history->array[history->index], cmdline->string);
+
+	history->index = (history->index + 1) % CMDLINE_HISTORY_SIZE;
+	history->array[history->index][0] = '\0';
+	history->array[(history->index + 1) % CMDLINE_HISTORY_SIZE][0] = '\0';
+
+	cmdline->history_cursor = history->index;
+	cmdline->length = 0;
+	cmdline->cursor = 0;
+	cmdline->string[0] = '\0';
+
+	return true;
+}
+
+bool cmdline_cursor_move_to(CMDLINE *cmdline, int to) {
+	int old_cursor = cmdline->cursor;
+	to = min(cmdline->length, to);
+	to = max(0, to);
+	cmdline->cursor = to;
+	return cmdline->cursor != old_cursor;
+}
+
+inline bool cmdline_cursor_move_by(CMDLINE *cmdline, int by) {
+	return cmdline_cursor_move_to(cmdline, cmdline->cursor + by);
+}
+
+inline bool cmdline_cursor_right(CMDLINE *cmdline) {
+	return cmdline_cursor_move_by(cmdline, 1);
+}
+
+inline bool cmdline_cursor_left(CMDLINE *cmdline) {
+	return cmdline_cursor_move_by(cmdline, -1);
+}
+
+inline bool cmdline_cursor_home(CMDLINE *cmdline) {
+	return cmdline_cursor_move_to(cmdline, 0);
+}
+
+inline bool cmdline_cursor_end(CMDLINE *cmdline) {
+	return cmdline_cursor_move_to(cmdline, cmdline->length);
+}
+
+bool cmdline_chars_delete(CMDLINE *this, int len) {
+	int i;
+	if (this == NULL) {
+		return false;
+	}
+
+	len = min(len, this->length - this->cursor);
+	len = max(0, len);
+	if (len == 0) {
+		return false;
+	}
+
+	this->length -= len;
+	for (i = this->cursor; i < this->length; ++i) {
+		this->string[i] = this->string[i + len];
+	}
+	this->string[this->length] = '\0';
+
+	return true;
+}
+
+bool cmdline_chars_backspace(CMDLINE *this, int len) {
+	int old_cursor;
+	if (this == NULL) {
+		return false;
+	}
+
+	old_cursor = this->cursor;
+	len = max(0, len);
+
+	cmdline_cursor_move_by(this, -len);
+
+	return cmdline_chars_delete(this, old_cursor - this->cursor);
+}
+
+bool cmdline_chars_insert(CMDLINE *this, char *ch, int len) {
+	int i;
+	if (this == NULL) {
+		return false;
+	}
+
+	len = min(len, CMDLINE_MAX_LENGTH - this->length);
+	len = max(0, len);
+
+	for (i = 0; (i < len) && ch[i]; ++i) {
+	}
+	len = min(len, i);
+	if (len == 0) {
+		return false;
+	}
+
+	this->length += len;
+	this->cursor += len;
+	if(!this->is_insert_mode) {
+		for (i = this->length; i >= this->cursor; --i) {
+			this->string[i] = this->string[i - len];
+		}
+	}
+	for (i = 0; i < len; ++i) {
+		this->string[this->cursor - len + i] = ch[i];
+	}
+
+	return true;
+}
+
+bool cmdline_dc2_reverse(CMDLINE *this) {
+	cmdline_chars_insert(this, "\r(reverse-i-search)`':", 22);
+	this->cursor -= 2;
+	return true;
+}
+
+bool cmdline_dc4_reverse(CMDLINE *this) {
+	char tmp;
+	if (this->cursor != this->length) {
+		tmp = this->string[this->cursor];
+		this->string[this->cursor] = this->string[this->cursor - 1];
+		this->string[this->cursor - 1] = tmp;
+		this->cursor++;
+	} else {
+		tmp = this->string[this->length - 1];
+		this->string[this->length - 1] = this->string[this->length - 2];
+		this->string[this->length - 2] = tmp;
+	}
+	return true;
+}
---
+++ src/cmds/shell/console/screen_out.c
@@ -0,0 +1,119 @@
+/**
+ * @file
+ *
+ * @date 28.02.2009
+ * @author Eldar Abusalimov
+ */
+#include "screen.h"
+#include <drivers/terminal.h>
+#include <assert.h>
+#include <embox/kernel.h>
+
+static void transmit_string(SCREEN *this, const char *str) {
+	while (*str) {
+		terminal_transmit(this->terminal, *str++, 0);
+	}
+}
+
+/**
+ * moves screen cursor by specified value
+ * @param by - delta to move screen cursor by
+ */
+static void transmit_move_cursor_by(SCREEN *this, int by) {
+	if (by == 0) {
+		return;
+	}
+
+	terminal_transmit(this->terminal, by > 0 ? TERMINAL_TOKEN_CURSOR_RIGHT
+			: TERMINAL_TOKEN_CURSOR_LEFT, 1, by > 0 ? by : -by);
+}
+
+static void move_cursor_to(SCREEN *this, int col) {
+	/* TODO range checking */
+
+	if (this->cursor + 3 < col || col < this->cursor) {
+		/* it is more easy to move cursor by special command*/
+		transmit_move_cursor_by(this, col - this->cursor);
+		this->cursor = col;
+	} else if (this->cursor < col) {
+		/* rewrite a few chars*/
+		for (; this->cursor < col; ++this->cursor) {
+			terminal_transmit(this->terminal, this->string[this->cursor], 0);
+		}
+	}
+
+}
+
+void screen_out_update(SCREEN *this, CMDLINE *cmdline) {
+	int i;
+	bool dirty = true;
+
+	if (this == NULL) {
+		return;
+	}
+
+	for (i = 0; cmdline->string[i]; i++) {
+		if (this->string[i] == '\0') {
+			dirty = false;
+		}
+		if (!dirty || this->string[i] != cmdline->string[i]) {
+			move_cursor_to(this, i);
+			assert(i == this->cursor);
+			terminal_transmit(this->terminal, cmdline->string[i], 0);
+			this->cursor++;
+			this->string[i] = cmdline->string[i];
+		}
+	}
+
+	if (dirty && this->string[i] != '\0') {
+		move_cursor_to(this, i);
+		terminal_transmit(this->terminal, TERMINAL_TOKEN_ERASE_SCREEN, 1,
+				TERMINAL_TOKEN_PARAM_ERASE_DOWN_RIGHT);
+	}
+	this->string[i] = '\0';
+
+	move_cursor_to(this, cmdline->cursor);
+}
+
+void screen_out_puts(SCREEN *this, char *str) {
+	if (this == NULL) {
+		return;
+	}
+	if (str != NULL) {
+		transmit_string(this, str);
+	}
+	terminal_transmit(this->terminal, TERMINAL_TOKEN_CR, 0);
+	terminal_transmit(this->terminal, TERMINAL_TOKEN_LF, 0);
+}
+
+void screen_out_show_prompt(SCREEN *this, const char *prompt) {
+	static const char * default_prompt = "";
+	if (this == NULL) {
+		return;
+	}
+
+	this->prompt = (prompt != NULL) ? prompt : default_prompt;
+
+	screen_out_puts(this, NULL);
+	*this->string = '\0';
+	this->cursor = 0;
+
+#if 0
+		screen_set_foreground_color(SGR_COLOR_RED);
+		terminal_transmit(this->terminal, TERMINAL_TOKEN_CURSOR_SAVE_ATTRS, NULL);
+#endif
+
+	terminal_transmit(this->terminal, TERMINAL_TOKEN_SGR, 1,
+			TERMINAL_TOKEN_PARAM_SGR_FG_RED);
+	terminal_transmit(this->terminal, TERMINAL_TOKEN_SGR, 1,
+			TERMINAL_TOKEN_PARAM_SGR_INTENSITY_BOLD);
+
+	transmit_string(this, prompt);
+
+
+	terminal_transmit(this->terminal, TERMINAL_TOKEN_SGR, 1,
+			TERMINAL_TOKEN_PARAM_SGR_RESET);
+#if 0
+	screen_set_foreground_color(SGR_COLOR_BLACK);
+#endif
+}
