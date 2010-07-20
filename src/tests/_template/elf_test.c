849da9c51a09ad60b056b1a38ef608852feade87
Dmitry Avdyukhin <dimonbv@gmail.com>
2010-07-20 14:46:59 +0000

    Elf test

--- src/tests/_template/elf_test.c
+++ src/tests/_template/elf_test.c
@@ -1,11 +1,20 @@
 /**
  * @file
- * @brief This file is derived from EMBOX test template.
+ * @brief Test function elf_execute.
+ *
+ * @date 20.07.2010.
+ * @author Avdyukhin Dmitry.
  */
 
 #include <embox/test.h>
+#include <kernel/elf_executer.h>
 
 EMBOX_TEST(run);
+
+/**
+ * Executed file address.
+ */
+#define FILE_ADDRESS 0x45000000
 
 /**
  * The test itself.
@@ -17,9 +26,7 @@ EMBOX_TEST(run);
 static int run(void) {
 	int result = 0;
 
-	/*
-	 * The test itself.
-	 */
+	elf_execute((FILE *)FILE_ADDRESS);
 
 	return result;
 }
