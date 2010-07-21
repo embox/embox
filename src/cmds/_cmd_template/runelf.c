5d00fc32797dd709ad77630d7ef4aa4190ef2055
Dmitry Avdyukhin <dimonbv@gmail.com>
2010-07-21 10:50:25 +0000

    "runelf" command. Hoped to be a last step

---
+++ src/cmds/_cmd_template/runelf.c
@@ -0,0 +1,68 @@
+/**
+ * @file
+ * @brief Run elf file from chosen address.
+ *
+ * @date 21.07.2010
+ * @author Avdyukhin Dmitry
+ */
+#include <shell_command.h>
+#include <kernel/scheduler.h>
+#include <kernel/elf_executer.h>
+
+#define COMMAND_NAME     "runelf"
+#define COMMAND_DESC_MSG "execute elf file"
+#define HELP_MSG \
+	"Usage: runelf [-h] [-a addr]"
+
+#define THREAD_STACK_SIZE 0x10000
+
+static struct thread *thread;
+static char thread_stack[THREAD_STACK_SIZE];
+static unsigned int file;
+
+static const char *man_page =
+	#include "runelf_help.inc"
+;
+
+DECLARE_SHELL_COMMAND(COMMAND_NAME, exec, COMMAND_DESC_MSG, HELP_MSG, man_page);
+
+static void run(void){
+	elf_execute((FILE *)file);
+}
+
+static int exec(int argsc, char **argsv) {
+	int op;
+	getopt_init();
+	do {
+		op = getopt(argsc, argsv, "ha:");
+		switch(op) {
+		case 'a':
+			if ((optarg == NULL) || (*optarg == '\0')) {
+				TRACE("runelf: missed address value\n");
+				return -1;
+			}
+			if (sscanf(optarg, "0x%x", &file) > 0) {
+				run();
+#if 0
+				thread = thread_create(run, thread_stack + THREAD_STACK_SIZE);
+				thread_start(thread);
+				scheduler_start();
+				scheduler_stop();
+#endif
+				return 0;
+			}
+			TRACE("runelf: invalid value \"%s\".\nthe number expected.\n", optarg);
+			return -1;
+		case 'h':
+			show_man_page();
+			return 0;
+		case -1:
+			show_help();
+			return 0;
+		default:
+			TRACE("runelf: invalid arguments.\n", optarg);
+			return -1;
+		}
+	} while (op != -1);
+	return 0;
+}
