ec602736e387863313dfad3a38493f6892191a78
Anton Bondarev <anton.bondarev1978@gmail.com>
2010-09-19 19:24:37 +0000

    reviewing ARM code (#176)

---
+++ src/arch/arm/include/asm/regs.h
@@ -0,0 +1,23 @@
+/**
+ * @file
+ *
+ * @brief Defines register convention for arm architecture
+ *
+ * @date 19.09.2010
+ * @author Anton Bondarev
+ */
+
+#ifndef ARM_REGS_H_
+#define ARM_REGS_H_
+
+#ifdef __ASSEMBLER__
+
+#define pc      r15
+
+#define lr      r14
+
+#define sp      r13
+
+#endif /* __ASSEMBLER__ */
+
+#endif /* ARM_REGS_H_ */
