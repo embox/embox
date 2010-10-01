79fa9c067a2efbd307ee052c0832f6371802c0cb
Anton Bondarev <anton.bondarev1978@gmail.com>
2010-10-01 11:53:53 +0000

    review lego-nxt platform

--- platform/lego_nxt/hardware_init_hook.c
+++ platform/lego_nxt/hardware_init_hook.c
@@ -1,18 +1,8 @@
-/**
- * @file arch.c
- * @brief ARM basic arch work
- *
- * @date 21.06.2010
- * @author Anton Kozlov
- */
-
 #include <hal/reg.h>
-#include <drivers/watchdog.h>
 
 #include <drivers/at91sam7s256.h>
 
-#if 0
-static void initialize_main_clock() {
+void hardware_init_hook(void) {
 	REG_STORE(AT91C_CKGR_MOR, AT91C_CKGR_MOSCEN | 6 << 8 );
 	while (! (REG_LOAD(AT91C_PMC_SR) | AT91C_PMC_MOSCS));
 
@@ -26,19 +16,4 @@ static void initialize_main_clock() {
 
 	REG_STORE(AT91C_PMC_MCKR, AT91C_PMC_CSS_PLL_CLK | AT91C_PMC_PRES_CLK_2);
 	while (! (REG_LOAD(AT91C_PMC_SR) | AT91C_PMC_MCKRDY));
-}
-#endif
-
-void arch_init(void) {
-#if 0
-	watchdog_disable();
-	initialize_main_clock();
-#endif
-}
-
-void arch_idle(void) {
-}
-
-void arch_shutdown(void) {
-	while(1);
 }
