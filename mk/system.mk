6333147ec6d9967e6895b58004aef4663b96280b
Anton Bondarev <anton.bondarev1978@gmail.com>
2010-10-04 13:58:04 +0000

    Clear arch/kernel make files, set default system devices
    (Fixed #186)

--- mk/system.mk
+++ mk/system.mk
@@ -12,13 +12,6 @@ else
 $_DEPS-interrupt += $(DEFAULT_SYS_IRQCTRL)
 endif
 
-ifdef CONFIG_SYS_WATCHDOG
-$_DEPS-embox.driver.abstract.watchdog     += $(CONFIG_SYS_WATCHDOG)
-else
-#default watchdog
-$_DEPS-embox.driver.abstract.watchdog     += $(DEFAULT_SYS_WATCHDOG)
-endif
-
 ifdef CONFIG_SYS_MOD_DIAG
 $_DEPS-embox.driver.abstract.mod_diag     += $(CONFIG_SYS_MOD_DIAG)
 else
