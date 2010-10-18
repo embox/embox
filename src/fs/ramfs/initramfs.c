dd202736bec424eef2892f75a7534a06b823a6c9
Nikolay Korotkiy <sikmir@gmail.com>
2010-10-18 12:37:59 +0000

    Fix cpio data align

--- src/fs/ramfs/initramfs.c
+++ src/fs/ramfs/initramfs.c
@@ -12,27 +12,38 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * The pathname is followed by NUL bytes so that the total size of the fixed
+ * header plus pathname is a multiple of four. Likewise, the file data is
+ * padded to a multiple of four bytes.
+ */
+#define N_ALIGN(len) ((((len) + 1) & ~3) + 2)
+#define F_ALIGN(len) (((len) + 3) & ~3)
+
 static cpio_newc_header *parse_item(cpio_newc_header *cpio_h, char *name) {
 	char *s;
 	size_t i;
 	unsigned long parsed[12], file_size, start_addr, mode;
 	char buf[9];
 	buf[8] = '\0';
-	//TRACE("magic=%s\n", cpio_h->c_magic);
-	if (!strcmp(cpio_h->c_magic, MAGIC)) {
-		TRACE("Newc ASCII CPIO format not recognized!\n");
+	if (memcmp(cpio_h->c_magic, MAGIC_OLD_BINARY, 6)==0) {
+		TRACE("Use -H newc option for create cpio arch\n");
+		return NULL;
+	}
+	if (memcmp(cpio_h->c_magic, MAGIC_NEWC, 6)) {
+		TRACE("Newc ASCII CPIO format not recognized.\n");
 		return NULL;
 	}
 	s = (char*)cpio_h;
 	for (i = 0, s += 6; i < 12; i++, s += 8) {
 		memcpy(buf, s, 8);
 		parsed[i] = strtol(buf, NULL, 16);
-		//TRACE("buf[%d]=%s, %d\n", i, buf, parsed[i]);
 	}
 
 	strncpy(name, (char*)cpio_h + sizeof(cpio_newc_header), parsed[11]);
+	name[parsed[11]] = '\0';
 	file_size  = parsed[6];
-	start_addr = (unsigned long)cpio_h + sizeof(cpio_newc_header) + parsed[11];
+	start_addr = (unsigned long)cpio_h + sizeof(cpio_newc_header) + N_ALIGN(parsed[11]);
 	mode       = parsed[1];
 
 	if(0 == strcmp(name, "TRAILER!!!")) {
@@ -41,7 +52,7 @@ static cpio_newc_header *parse_item(cpio_newc_header *cpio_h, char *name) {
 		//TODO: set start_addr, file_size, mode.
 		vfs_add_path(name, NULL);
 	}
-	return (cpio_newc_header*)(start_addr + file_size);
+	return (cpio_newc_header*)F_ALIGN(start_addr + file_size);
 }
 
 int unpack_to_rootfs(void) {
@@ -50,7 +61,7 @@ int unpack_to_rootfs(void) {
 	unsigned char buff_name[CONFIG_MAX_LENGTH_FILE_NAME];
 
 	if(&_ramfs_end == &_ramfs_start) {
-		TRACE("No availible ramfs\n");
+		TRACE("No availible initramfs\n");
 		return -1;
 	}
 	TRACE("cpio initramfs at 0x%08x\n", &_ramfs_start);
--- src/include/lib/cpio.h
+++ src/include/lib/cpio.h
@@ -16,48 +16,56 @@
 
 /* New ASCII Format */
 typedef struct {
-        char c_magic[6];    /* must be "070701" */
-        char c_ino[8];
-        char c_mode[8];
-        char c_uid[8];
-        char c_gid[8];
-        char c_nlink[8];
-        char c_mtime[8];
-        char c_filesize[8]; /* must be 0 for FIFOs and directories  */
-        char c_devmajor[8];
-        char c_devminor[8];
-        char c_rdevmajor[8];
-        char c_rdevminor[8];
-        char c_namesize[8];
-        char c_check[8];
+	char c_magic[6];    /* must be "070701" */
+	char c_ino[8];
+	char c_mode[8];
+	char c_uid[8];
+	char c_gid[8];
+	char c_nlink[8];
+	char c_mtime[8];
+	/**
+	 * Must be 0 for FIFOs and directories.
+	 */
+	char c_filesize[8];
+	char c_devmajor[8];
+	char c_devminor[8];
+	char c_rdevmajor[8];
+	char c_rdevminor[8];
+	char c_namesize[8];
+	/**
+	 * This field is always set to zero by writers and ignored by
+	 * readers.  See the next section for more details.
+	 */
+	char c_check[8];
 } cpio_newc_header;
 
 /* Value for the field `c_magic'.  */
-#define MAGIC	"070701"
+#define MAGIC_NEWC       "070701"
+#define MAGIC_OLD_BINARY "070707"
 
 /* Values for c_mode, OR'd together: */
 
-#define C_IRUSR		000400
-#define C_IWUSR		000200
-#define C_IXUSR		000100
-#define C_IRGRP		000040
-#define C_IWGRP		000020
-#define C_IXGRP		000010
-#define C_IROTH		000004
-#define C_IWOTH		000002
-#define C_IXOTH		000001
+#define C_IRUSR	        000400
+#define C_IWUSR	        000200
+#define C_IXUSR	        000100
+#define C_IRGRP	        000040
+#define C_IWGRP	        000020
+#define C_IXGRP	        000010
+#define C_IROTH	        000004
+#define C_IWOTH	        000002
+#define C_IXOTH	        000001
 
-#define C_ISUID		004000
-#define C_ISGID		002000
-#define C_ISVTX		001000
+#define C_ISUID	        004000
+#define C_ISGID	        002000
+#define C_ISVTX	        001000
 
-#define C_ISBLK		060000
-#define C_ISCHR		020000
-#define C_ISDIR		040000
-#define C_ISFIFO	010000
-#define C_ISSOCK	0140000
-#define C_ISLNK		0120000
-#define C_ISCTG		0110000
-#define C_ISREG		0100000
+#define C_ISBLK	        060000
+#define C_ISCHR	        020000
+#define C_ISDIR	        040000
+#define C_ISFIFO        010000
+#define C_ISSOCK        0140000
+#define C_ISLNK	        0120000
+#define C_ISCTG	        0110000
+#define C_ISREG	        0100000
 
 #endif /* _CPIO_H */
