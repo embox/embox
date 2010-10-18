6fc2bd5af9bb895298fb6323530bc60ce4321c26
Anton Kozlov <drakon.mega@gmail.com>
2010-10-18 13:49:59 +0000

    twi.h added

---
+++ platform/lego_nxt/include/drivers/twi.h
@@ -0,0 +1,32 @@
+/* @file twi.h
+ * @brief TWI communication for Lego AVR
+ *
+ * @date 15.10.2010
+ * @author Anton Kozlov
+ */
+
+#define NXT_AVR_ADDRESS 1
+#define NXT_AVR_N_OUTPUTS 4
+#define NXT_AVR_N_INPUTS  4
+
+typedef struct{
+  // Raw values
+	uint8_t power;
+	uint8_t pwm_frequency;
+	int8_t output_percent[NXT_AVR_N_OUTPUTS];
+	uint8_t output_mode;
+	uint8_t input_power;
+} __attribute__((packed)) to_avr;
+
+void twi_init(void);
+
+/**
+ * Send through TWI, should be used to normal communication
+ */
+void twi_send(uint32_t dev_addr, const uint8_t *data, uint32_t count);
+
+/**
+ * Raw send through TWI, should be used in special cases,
+ * e.g. first initializing, etc.
+ */
+void twi_write(uint32_t dev_addr, const uint8_t *data, uint32_t nBytes);
