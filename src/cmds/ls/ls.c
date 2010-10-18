77f00725f350babd6385c51787683fcf63f4c9bf
Anton Bondarev <anton.bondarev1978@gmail.com>
2010-10-18 12:56:25 +0000

    Improve ls command (Fixed #211)

--- src/cmds/ls/ls.c
+++ src/cmds/ls/ls.c
@@ -20,51 +20,58 @@ static const char *man_page =
 
 DECLARE_SHELL_COMMAND(COMMAND_NAME, exec, COMMAND_DESC_MSG, HELP_MSG, man_page);
 
-#if 0
-static void print_list(FS_FILE_ITERATOR iter_func) {
-	FILE_INFO file_info;
-	while (NULL != iter_func(&file_info)){
-		printf("%s\n", file_info.file_name);
+static void print_long_list(char *path, node_t *nod, int recursive) {
+	struct list_head *p;
+	char mode[] = "---";
+	printf("%16s | %4s | %10s \n", "name", "mode", "size");
+
+	list_for_each(p, &(nod->leaves)) {
+		printf("%16s | %4s | %d \n",  (char *)((node_t*)list_entry(p, node_t, neighbors))->name, "xxx", 0);
 	}
 }
 
-static void print_long_list(FS_FILE_ITERATOR iter_func) {
-	FILE_INFO file_info;
-	char mode[] = "---";
-	printf("%16s | %4s | %10s | %10s\n", "name", "mode", "size", "size on disk");
-	while (NULL != iter_func(&file_info)){
-		if(file_info.mode & FILE_MODE_RO) {
-			mode[0] = 'r';
+
+static void print_folder(char *path, node_t *nod, int recursive) {
+	struct list_head *p;
+	list_for_each(p, &(nod->leaves)) {
+		if (recursive) {
+			if(0 == strcmp(path, "/")) {
+				printf("%s\n",  (char *)((node_t*)list_entry(p, node_t, neighbors))->name);
+			} else {
+				printf("%s/%s\n", path, (char *)((node_t*)list_entry(p, node_t, neighbors))->name);
+				strcat(path, (char *)((node_t*)list_entry(p, node_t, neighbors))->name);
+				print_folder(path, (node_t*)list_entry(p, node_t, neighbors), recursive);
+			}
+		} else {
+			printf("%s\n",  (char *)((node_t*)list_entry(p, node_t, neighbors))->name);
 		}
-		if(file_info.mode & FILE_MODE_WO) {
-			mode[1] = 'w';
-		}
-		if(file_info.mode & FILE_MODE_XO) {
-			mode[2] = 'x';
-		}
-		printf("%16s |  %3s | %10d | %10d\n", file_info.file_name, mode,
-				file_info.size_in_bytes, file_info.size_on_disk);
 	}
 }
-#endif
+typedef void (*print_func_t)(char *path, node_t *nod, int recursive);
 
 static int exec(int argsc, char **argsv) {
-	char *path = "/";
 	int long_list = 0;
 	node_t *nod;
-	struct list_head *p;
+	char path[0x100];
+
+	int recursive = 0;
+	volatile print_func_t print_func = print_folder;
 
 	int nextOption;
 	getopt_init();
 	do {
-		nextOption = getopt(argsc - 1, argsv, "lh");
+		nextOption = getopt(argsc - 1, argsv, "Rlh");
 		switch(nextOption) {
 		case 'h':
 			show_help();
 			return 0;
 		case 'l':
 			long_list = 1;
+			print_func = print_long_list;
 			break;
+		case 'R':
+			recursive = 1;
+			print_func = print_folder;
 		case -1:
 			break;
 		default:
@@ -72,14 +79,15 @@ static int exec(int argsc, char **argsv) {
 		}
 	} while(-1 != nextOption);
 
-	if(argsc > 1) {
-		path = argsv[argsc - 1];
+	if(argsc > 2) {
+		sprintf(path, "%s", argsv[argsc - 1]);
+	}
+	else {
+		sprintf(path, "/");
 	}
 
 	nod = vfs_find_node(path, NULL);
-	list_for_each(p, &(nod->leaves)) {
-		printf("%s\n", (node_t*)list_entry(p, node_t, neighbors)->name);
-	}
+	print_func(path, nod, recursive);
 
 	return 0;
 }
