4bea9358709389b71f4200dc0277e6b95bcd79ed
Nikolay Korotkiy <sikmir@gmail.com>
2010-10-26 12:10:29 +0000

    autofix codestyle, Duff's device optimization strncpy

--- src/lib/string/strncpy.c
+++ src/lib/string/strncpy.c
@@ -9,13 +9,26 @@
 
 char *strncpy(char *dst, const char *src, size_t n) {
 	char *ret = dst;
-
+#if 1
 	while (n && (*dst++ = *src++)) {
 		n--;
 	}
 	while (n--) {
 		*dst++ = '\0';
 	}
-
+#else /* Duff's device optimization */
+	size_t count = (n + 7) / 8;
+	switch (n % 8) {
+	case 0: do { *dst = *src++;
+	case 7:      *dst = *src++;
+	case 6:      *dst = *src++;
+	case 5:      *dst = *src++;
+	case 4:      *dst = *src++;
+	case 3:      *dst = *src++;
+	case 2:      *dst = *src++;
+	case 1:      *dst = *src++;
+		} while (--count > 0);
+	}
+#endif
 	return ret;
 }
