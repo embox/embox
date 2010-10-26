754ab3b573687a6d336c3ae57a8b0dd6ef9b3900
Nikolay Korotkiy <sikmir@gmail.com>
2010-10-26 14:03:55 +0000

    Fix snake

--- platform/lego_nxt/tests/snake/snake.c
+++ platform/lego_nxt/tests/snake/snake.c
@@ -27,8 +27,8 @@ static char field[SIZE_X * SIZE_Y];
 static char dir[SIZE_X * SIZE_Y];
 
 typedef struct {
-    int x;
-    int y;
+	int x;
+	int y;
 } point;
 
 static point head, tail, diff;
@@ -44,9 +44,7 @@ static point nil = {0,0};
 static int score = 0;
 
 static void draw_nxt_white(point p) {
-	int x = p.x * 4;
-	int y = p.y / 2;
-	int i;
+	int i, x = p.x * 4, y = p.y / 2;
 	char fill = 0xf0;
 	if (p.y & 1)
 		fill = 0x0f;
@@ -56,11 +54,9 @@ static void draw_nxt_white(point p) {
 }
 
 static void draw_nxt_bitmap(point p, uint8_t *bitmap) {
-	int x = p.x * 4;
-	int y = p.y / 2;
-	int i;
+	int i, x = p.x * 4, y = p.y / 2;
 	for (i = 0; i < 4; i++) {
-	   display_buffer[y][x++] |= bitmap[i] << (4 * (p.y & 1));
+		display_buffer[y][x++] |= bitmap[i] << (4 * (p.y & 1));
 	}
 }
 
@@ -90,91 +86,89 @@ static void draw_nxt_fruit(point p) {
 }
 #endif
 static int f2d(int x, int y) {
-    return x + y * SIZE_X;
+	return x + y * SIZE_X;
 }
 
 static point d2f(int offset) {
-    point p = {offset % SIZE_Y, offset / SIZE_Y};
-    return p;
+	point p = {offset % SIZE_Y, offset / SIZE_Y};
+	return p;
 }
 
 static void snake_init(void) {
-    int j = SIZE_Y / 2;
-    int si = (SIZE_X - SNAKE_LEN) /2;
-    int i;
-    head.y = tail.y = j;
-    tail.x = si;
-    for (i=0;i<SNAKE_LEN;i++) {
+	int i, j = SIZE_Y / 2;
+	int si = (SIZE_X - SNAKE_LEN) /2;
+	head.y = tail.y = j;
+	tail.x = si;
+	for (i=0;i<SNAKE_LEN;i++) {
 		field[f2d(si + i,j)] = SNK;
 		dir  [f2d(si + i,j)] = 'l';
-    }
-    head.x = si + i - 1;
+	}
+	head.x = si + i - 1;
 }
 
 static void draw(point p, char c) {
-    field[f2d(p.x,p.y)] = c;
+	field[f2d(p.x,p.y)] = c;
 }
 
 static int space(char c) {
-    return (c == '.' || c == ' ');
+	return (c == '.' || c == ' ');
 }
 
 static int random(void) {
-    rand_seed += cnt_system_time();
-    return rand_seed;
+	rand_seed += cnt_system_time();
+	return rand_seed;
 }
 
 static void fruit_draw(void) {
-    int t = random() % ((SIZE_X - 1) * (SIZE_Y - 1));
-    while (!space(field[t])) {
+	int t = random() % ((SIZE_X - 1) * (SIZE_Y - 1));
+	while (!space(field[t])) {
 		t = random() % ((SIZE_X - 1) * (SIZE_Y - 1));
 	}
-    draw(d2f(t), FRU);
+	draw(d2f(t), FRU);
 	draw_nxt_fruit(d2f(t));
 }
 
 static void refresh(void) {
-	display_clear_screan();
 	int i, j;
-    for (i = 0; i < SIZE_Y; i++) {
+	display_clear_screan();
+	for (i = 0; i < SIZE_Y; i++) {
 		for (j = 0; j < SIZE_X; j++) {
 			if (!space(field[f2d(j,i)])) {
 				point p = {j, i};
 				draw_nxt_middle(p);
 			}
 		}
-    }
+	}
 }
 
 static void field_init(void) {
-    int i;
-    int j;
-    for (i = 0; i < SIZE_X; i++)
+	int i, j;
+	for (i = 0; i < SIZE_X; i++)
 		for (j = 0; j < SIZE_Y; j++)
 			field[f2d(i,j)] = '.';
-    for (i = 0; i < SIZE_X; i++) {
+	for (i = 0; i < SIZE_X; i++) {
 		field[f2d(i,0)] = field[f2d(i,SIZE_Y - 1)] = '-';
-    }
-    for (i = 0; i < SIZE_Y; i++) {
+	}
+	for (i = 0; i < SIZE_Y; i++) {
 		field[f2d(0,i)] = field[f2d(SIZE_X - 1,i)] = '|';
 	}
-    snake_init();
+	snake_init();
 	refresh();
 
-    fruit_draw();
+	fruit_draw();
 }
 
 static point point_plus(point p1, point p2) {
-    point p = {p1.x + p2.x, p1.y + p2.y};
-    return p;
+	point p = {p1.x + p2.x, p1.y + p2.y};
+	return p;
 }
 
 static char field_char(point p) {
-    return field[f2d(p.x,p.y)];
+	return field[f2d(p.x,p.y)];
 }
 
 static char dir_char(point p) {
-    return dir[f2d(p.x,p.y)];
+	return dir[f2d(p.x,p.y)];
 }
 
 
@@ -200,68 +194,68 @@ static char button_dispatch(uint32_t btns) {
 }
 
 static point dispatch(char c) {
-    switch (c) {
+	switch (c) {
 	case 'h':
-	    return dxm;
+		return dxm;
 	case 'j':
-	    return dyp;
+		return dyp;
 	case 'k':
-	    return dym;
+		return dym;
 	case 'l':
-	    return dxp;
-    }
-    return nil;
+		return dxp;
+	}
+	return nil;
 
 }
 
 #if 0
 static int pcmp(point p1, point p2) {
-    return p1.x - p2.x + p1.y - p2.y;
+	return p1.x - p2.x + p1.y - p2.y;
 }
 #endif
 
 static int snake_update(void) {
-    point p = point_plus(head, diff);
-    char new_pos = field_char(p);
+	point p = point_plus(head, diff);
+	char new_pos = field_char(p);
 
-    dir[f2d(head.x, head.y)] = diff_char;
+	dir[f2d(head.x, head.y)] = diff_char;
 
-    switch (new_pos) {
+	switch (new_pos) {
 	case FRU:
-	    if (!speed_count--) {
-		if (sleep_time >= 50)
-		    sleep_time -= 20;
-		speed_count = SPEED_INC;
-	    }
+		if (!speed_count--) {
+			if (sleep_time >= 50)
+				sleep_time -= 20;
+			speed_count = SPEED_INC;
+		}
 		draw_nxt_white(head);
 		draw_nxt_middle(head);
 		draw_nxt_connect(head, diff);
 		draw_nxt_white(p);
 		draw_nxt_head(p);
-	    head = p;
-	    score += 10;
-	    draw(p,SNK);
-	    fruit_draw();
+		head = p;
+		score += 10;
+		draw(p,SNK);
+		fruit_draw();
 		nxt_lcd_force_update();
 		switch_bank();
-	    break;
+		break;
 	case ' ':
 	case '.':
-	    draw(tail,'.');
+		draw(tail,'.');
 		draw_nxt_white(tail);
 
-	    draw(p, SNK);
+		draw(p, SNK);
 		draw_nxt_white(head);
 		draw_nxt_middle(head);
 		draw_nxt_connect(head, diff);
 		draw_nxt_head(p);
 
-	    head = p;
-	    tail = point_plus(dispatch(dir_char(tail)), tail);
+		head = p;
+		tail = point_plus(dispatch(dir_char(tail)), tail);
 		nxt_lcd_force_update();
-	    break;
+		break;
 	default:
-	    draw(p,'X');
+		draw(p,'X');
 		draw_nxt_white(p);
 		draw_nxt_die(p);
 		draw_nxt_white(head);
@@ -269,21 +263,21 @@ static int snake_update(void) {
 		draw_nxt_connect(head, diff);
 		nxt_lcd_force_update();
 
-	    return 0;
-    }
-    return 1;
+		return 0;
+	}
+	return 1;
 }
 
 static int last_valid;
 
 static int valid(point p) {
-    return (last_valid = !(p.x == 0 && p.y == 0));
+	return (last_valid = !(p.x == 0 && p.y == 0));
 }
 
 static int exec() {
 	display_clear_screan();
-    field_init();
-    diff = dxp;
+	field_init();
+	diff = dxp;
 	while (snake_update()) {
 		char ch;
 		point d = nil;
@@ -301,7 +295,7 @@ static int exec() {
 			diff = d;
 			diff_char = ch;
 		}
-    }
-    return 0;
+	}
+	return 0;
 }
 
