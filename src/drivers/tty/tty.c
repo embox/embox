f574b4ed497a0e36b39a107ca64f1490eb8bc5c3
Anton Bondarev <anton.bondarev1978@gmail.com>
2010-11-17 16:24:58 +0000

    Work on TTY driver (#197)

--- src/drivers/tty/tty.c
+++ src/drivers/tty/tty.c
@@ -7,13 +7,22 @@
  * @author Anton Bondarev
  */
 
+#include <string.h>
 #include <drivers/tty.h>
+
+tty_device_t *cur_tty;
 
 int tty_init(void) {
 	return 0;
 }
 
-int tty_register(void) {
+int tty_register(tty_device_t *tty) {
+	cur_tty = tty;
+	return 0;
+}
+
+int tty_unregister(tty_device_t *tty) {
+	cur_tty = tty;
 	return 0;
 }
 
@@ -24,10 +33,10 @@ int tty_get_uniq_number(void) {
  * add parsed char to receive buffer
  */
 int tty_add_char(tty_device_t *tty, int ch) {
-	if ('\n' == ch && !out_busy) {
+	if ('\n' == ch && !tty->out_busy) {
 		/*add string to output buffer*/
-		memcpy(tty->out_buff, tty->rx_buff, cnt);
-		tty->out_buff = '\0';
+		memcpy(tty->out_buff, tty->rx_buff, tty->rx_cnt);
+		tty->out_buff[tty->rx_cnt] = '\0';
 		tty->rx_cnt = 0;
 		tty->out_busy = true;
 		return 1;
@@ -46,7 +55,7 @@ uint8_t* tty_readline(tty_device_t *tty) {
 }
 
 static uint32_t tty_scanline(uint8_t line[TTY_RXBUFF_SIZE + 1], uint32_t size) {
-	int i;
+	int i = 0;
 	while('\n' == line[i ++]) {
 		if (0 == (size --))
 			return 0;
