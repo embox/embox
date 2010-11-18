0033bfe566d59eca3d15433ec03c9952dce4cd7d
Vladimir Mukhin <vladimirsanich0@gmail.com>
2010-11-18 07:16:22 +0000

    Trying synchronize threads

--- src/tests/threads/thread_tmp.c
+++ src/tests/threads/thread_tmp.c
@@ -1,6 +1,8 @@
 /**
  * @file
- * @brief studying threads
+ * @brief We have 2 threads. When inc_thread start it makes increment
+ * 			and checking pipe at every iteration. When i becomes equal pipe data
+ * 			dec_thread starting.
  *
  * @date 11/2010
  * @author Muhin Vladimir
@@ -15,15 +17,38 @@
 #define THREAD_STACK_SIZE 0x1000
 
 static char inc_stack[THREAD_STACK_SIZE];
+static char dec_stack[THREAD_STACK_SIZE];
 
 static struct thread *inc_thread;
+static struct thread *dec_thread;
 
+const int flag = 50;
+
+int pipe[2] = {50, 0};  //our small pipe :-)
+						//first element is number of iterations before synchronize,
+						//second - some addition data
 EMBOX_TEST(run);
+
+static void synchronize(){
+	thread_stop(inc_thread);
+}
 
 static void inc_run(void) {
 	int i;
 	for (i = 0; i < 256; i++) {
+		if (i == pipe[0]){
+			pipe[1] = i;
+			synchronize();
+		};
 		TRACE("%d", i);
+		TRACE(" ");
+	}
+}
+
+static void dec_run(void){
+	int j, k = pipe[1];
+	for (j = k; j > 0; j--){
+		TRACE("%d", j);
 		TRACE(" ");
 	}
 }
@@ -31,7 +56,10 @@ static void inc_run(void) {
 static int run() {
 	TRACE("\nHi! It's 256-thread!\n");
 	inc_thread = thread_create(inc_run, inc_stack + THREAD_STACK_SIZE);
+	dec_thread = thread_create(dec_run, dec_stack + THREAD_STACK_SIZE);
 	assert(inc_thread != NULL);
+	assert(dec_thread != NULL);
+	thread_start(dec_thread);
 	thread_start(inc_thread);
 	scheduler_start();
 	scheduler_stop();
