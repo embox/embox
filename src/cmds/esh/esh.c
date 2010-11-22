79a605d0e895355593cc2c1088434d69bded136e
Fedor Burdun <ignwah@gmail.com>
2010-11-22 21:38:01 +0000

    A little cleaning. #193

--- src/cmds/esh/esh.c
+++ src/cmds/esh/esh.c
@@ -5,37 +5,7 @@
 #include <lib/readline.h>
 #include <stdio.h>
 
-#if 0
-#define USE_READLINE
-//#define USE_FOPEN
-
-#ifdef USE_READLINE
-#else
-#endif
-
-#ifdef USE_FOPEN
-#include <fs/file.h>
-#endif
-
-#ifdef USE_READLINE
-const char* cmd_1 = "help";
-const char* cmd_2 = "ls";
-
-char * __readline(char *arg) {
-	return cmd_2;
-}
-#endif
-#endif
-
 static int esh_start(void) {
-#if 0
-	char cmd[255];
-	char promt[255];
-	char *cmd_ptr = &cmd[0];
-	char *promt_ptr = &promt[0];
-	promt_ptr = ">";
-#endif
-
 	int ret_code;
 	SHELL_COMMAND_DESCRIPTOR *scd;
 	printk("\nModule `esh' started.\n");
@@ -48,42 +18,18 @@ static int esh_start(void) {
 	for (;;) {
 		/*cmd_ptr = __readline(promt_ptr); */
 		line = readline(CONFIG_SHELL_PROMPT);
-		shell_command_descriptor_find_first(line, -1);
+		TRACE("\nreceived '%s' \n", line);
 
-		#if 1
-			printk("CmdLine: %s, RunCommand: %s\n",line , scd->name);
-		#endif
+		/* scd = shell_command_descriptor_find_first(line, -1);
 
-		ret_code = shell_command_exec(scd, 0, NULL);
+		TRACE("CmdLine: %s, RunCommand: %s\n",line , scd->name);
+
+		ret_code = shell_command_exec(scd, 0, NULL); */
 
 		freeline(line);
 	}
 
 	fclose(ff);
-
-
-#if 0
-#ifdef USE_FOPEN
-	char tmp;
-	FILE *ff = fopen("/dev/uart","r");
-	for (;;) {
-		fread(&tmp , 1 , 1 , ff);
-		printk("readed %c -- %d \n", tmp , tmp);
-		fwrite(&tmp , 1 , 1 , ff);
-	}
-#endif
-
-#ifdef USE_READLINE
-	for (;;) {
-		cmd_ptr = __readline(promt_ptr);
-		shell_command_descriptor_find_first(cmd_ptr, -1);
-		#if 1
-			printk("CmdLine: %s, RunCommand: %s\n",cmd_ptr, scd->name);
-		#endif
-		ret_code = shell_command_exec(scd, 0, NULL);
-	}
-#endif
-#endif
 
 	return 0;
 }
