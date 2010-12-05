d7c26ed5082cb58e4a2c776e2a87aeae15396e1e
Darya Dzendzik <daryadzen@gmail.com>
2010-12-05 14:02:56 +0000

    show how work and don't work fill function

--- platform/lego_nxt/tests/lcd/test_fill.c
+++ platform/lego_nxt/tests/lcd/test_fill.c
@@ -15,19 +15,45 @@ extern int display_fill(uint8_t, uint8_t, uint8_t, uint8_t, int);
 
 static int run_fill(void) {
 	//int i, j;
+	uint8_t buff[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 	display_clear_screen();
-	char pict_buff1[8] = {0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF};
-	display_fill(10, 10, 40, 10, 1);
-	usleep(1000);
-	display_fill(15, 16, 10, 1, 0);
-	usleep(1000);
-	display_fill(1, 2, 1, 1, 1);
-	display_fill(2, 3, 1, 1, 1);
-	display_fill(3, 4, 1, 1, 1);
-	display_fill(4, 5, 1, 1, 1);
-	display_fill(5, 6, 1, 1, 1);
-	display_fill(6, 7, 1, 1, 1);
-	usleep(2000000);
+	display_draw(70, 1, 1,	8, &buff[0]);
+	display_draw(70, 3, 1,	8, &buff[0]);
+	display_draw(70, 5, 1,	8, &buff[0]);
+	display_draw(70, 7, 1,	8, &buff[0]);
+
+	display_fill(50, 1, 8, 40, 1);
+	display_fill(60, 3, 8, 40, 0);
+	display_fill(80, 0, 8, 40, 1);
+	display_fill(90, 9, 8, 40, 0);
+
+	display_fill(0, 0, 1, 1, 1);
+	display_fill(1, 1, 1, 1, 1);
+	display_fill(2, 2, 1, 1, 1);
+	display_fill(3, 3, 1, 1, 1);
+	display_fill(4, 4, 1, 1, 1);
+	display_fill(5, 5, 1, 1, 1);
+	display_fill(6, 6, 1, 1, 1);
+	display_fill(7, 7, 1, 1, 1);
+
+
+
+	display_draw(10, 1, 1,	8, &buff[0]);
+	display_draw(10, 3, 1,	8, &buff[0]);
+	display_draw(10, 5, 1,	8, &buff[0]);
+	display_draw(10, 7, 1,	8, &buff[0]);
+
+	//usleep(800);
+
+	display_fill(20, 0, 1, 1, 0);
+	display_fill(21, 1, 1, 1, 0);
+	display_fill(22, 2, 1, 1, 0);
+	display_fill(23, 3, 1, 1, 0);
+	display_fill(24, 4, 1, 1, 0);
+	display_fill(25, 5, 1, 1, 0);
+	display_fill(26, 6, 1, 1, 0);
+	display_fill(27, 7, 1, 1, 0);
+	usleep(800000);
 
 
 	/*for (i=0; i<10 ;i++) {
