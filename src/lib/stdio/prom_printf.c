276f286524dca573aab55afacf2c79161f374ba0
Anton Bondarev <anton.bondarev1978@gmail.com>
2010-12-08 16:02:04 +0000

    prom_printf

---
+++ src/lib/stdio/prom_printf.c
@@ -0,0 +1,36 @@
+/**
+ * @file
+ *
+ * @brief
+ *
+ * @date 10.11.2010
+ * @author Anton Bondarev
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <types.h>
+
+int __print(void (*printchar_handler)(char **str, int c), char **out, const char *format, va_list args);
+
+static void printchar(char **str, int c) {
+	if (str) {
+		**str = c;
+		++(*str);
+	} else {
+		putchar(c);
+	}
+}
+
+
+int prom_printf(const char *format, ...) {
+	int ret;
+	va_list args;
+
+	va_start(args, format);
+	ret = __print(printchar, 0, format, args);
+	va_end(args);
+
+	return ret;
+}
+
