468c1fbc548f517eaa9aa1209933e7e45c7cabce
Nikolay Korotkiy <sikmir@gmail.com>
2010-12-17 18:44:20 +0000

    x86: Fix in/out

--- src/arch/x86/include/asm/io.h
+++ src/arch/x86/include/asm/io.h
@@ -10,14 +10,56 @@
 #ifndef ASM_X86_IO_H_
 #define ASM_X86_IO_H_
 
-static inline uint8_t inb(uint16_t port) {
-	uint8_t value;
-	__asm__ __volatile__ ("inb %w1, %b0" : "=a"(value) : "Nd" (port));
-	return value;
+static inline uint8_t in8(unsigned long port) {
+	uint8_t tmp;
+	__asm__ __volatile__(
+		"inb %w1, %b0"
+		: "=a"(tmp)
+		: "Nd"(port)
+	);
+	return tmp;
 }
 
-static inline void outb(uint8_t value, uint16_t port) {
-	__asm__ __volatile__ ("outb %b0, %w1" : : "a" (value), "Nd" (port));
+static inline uint8_t in16(unsigned long port) {
+	uint16_t tmp;
+	__asm__ __volatile__(
+		"inw %w1, %w0"
+		: "=a"(tmp)
+		: "Nd"(port)
+	);
+	return tmp;
 }
+
+static inline uint8_t in32(unsigned long port) {
+	uint32_t tmp;
+	__asm__ __volatile__(
+		"in %w1, %0"
+		: "=a"(tmp)
+		: "Nd"(port)
+	);
+	return tmp;
+}
+
+#define inb(port) in8(port)
+
+#define out8(val, port)                  \
+	__asm__ __volatile__(            \
+		"outb %b0, %w1"          \
+		: : "a"(val), "Nd"(port) \
+	)
+
+#define out16(val, port)                 \
+	__asm__ __volatile__(            \
+		"outw %w0, %w1"          \
+		: : "a"(val), "Nd"(port) \
+	)
+
+#define out32(val, port)                 \
+	__asm__ __volatile__(            \
+		"out %0, %w1"            \
+		: : "a"(val), "Nd"(port) \
+	)
+
+#define outb(value, port)  out8(value, port)
 
 #endif /* ASM_X86_IO_H_ */
