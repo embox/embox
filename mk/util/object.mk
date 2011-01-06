8134ec00da21bea60f0deea06e0ad6324da413b9
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-01-06 00:45:48 +0000

    #233: still nothing meaningful

--- mk/util/object.mk
+++ mk/util/object.mk
@@ -1,4 +1,5 @@
 #
+#   Date: Dec 20, 2010
 # Author: Eldar Abusalimov
 #
 
@@ -28,8 +29,26 @@ object_class_define_field =
 # Params:
 #  1. Class name
 #  2. Method name
-#  3.
+#  3. Function to bind
 object_class_define_method =
+
+# Params:
+#  1. Instance on which to invoke the method
+#  2. Method to invoke
+# ... Args
+invoke =
+
+# Params:
+#  1. Instance which's field to get
+#  2. The field
+# Returns: The field value
+get =
+
+# Params:
+#  1. Instance which's field to set
+#  2. The field
+#  3. The value to set
+set =
 
 __object_instance_counter :=
 __object_instance_counter_get = $(words $(__object_instance_counter))
