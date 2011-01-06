4088e7939ef94892103196f1262bd4815280c65d
Anton Bondarev <anton.bondarev1978@gmail.com>
2011-01-06 14:33:34 +0000

    APIC controller

--- src/drivers/interrupts/apic_interrupt.c
+++ src/drivers/interrupts/apic_interrupt.c
@@ -16,26 +16,7 @@
 #include <asm/traps.h>
 #include <asm/io.h>
 
-#define PIC1            0x20   /* IO base address for master PIC */
-#define PIC2            0xA0   /* IO base address for slave PIC */
-#define PIC1_COMMAND    PIC1
-#define PIC1_DATA       (PIC1+1)
-#define PIC2_COMMAND    PIC2
-#define PIC2_DATA       (PIC2+1)
-
-#define PIC_EOI         0x20   /* End-of-interrupt command code */
-
-#define ICW1_ICW4       0x01   /* ICW4 (not) needed */
-#define ICW1_SINGLE     0x02   /* Single (cascade) mode */
-#define ICW1_INTERVAL4  0x04   /* Call address interval 4 (8) */
-#define ICW1_LEVEL      0x08   /* Level triggered (edge) mode */
-#define ICW1_INIT       0x10   /* Initialization - required! */
-
-#define ICW4_8086       0x01   /* 8086/88 (MCS-80/85) mode */
-#define ICW4_AUTO       0x02   /* Auto (normal) EOI */
-#define ICW4_BUF_SLAVE  0x08   /* Buffered mode/slave */
-#define ICW4_BUF_MASTER 0x0C   /* Buffered mode/master */
-#define ICW4_SFNM       0x10   /* Special fully nested (not) */
+#include <drivers/apic.h>
 
 void interrupt_init(void) {
 	/* remap PICs */
@@ -50,6 +31,8 @@ void interrupt_init(void) {
 	out8(0x02, PIC2_DATA);
 	out8(0x01, PIC2_DATA);
 	out8(0x00, PIC2_DATA);
+
+	//out8(0xFF, PIC2_DATA);
 }
 
 void interrupt_enable(interrupt_nr_t interrupt_nr) {
@@ -60,18 +43,4 @@ void interrupt_enable(interrupt_nr_t interrupt_nr) {
 //	}
 }
 
-void irq_handler(pt_regs_t regs) {
-	/* Send an EOI (end of interrupt) signal to the PICs.
-	   If this interrupt involved the slave. */
-	//FIXME: regs->trapno?
-	if (regs.trapno >= 40) {
-		/* Send reset signal to slave. */
-		out8(PIC2_COMMAND, PIC_EOI);
-	}
-	/* Send reset signal to master. (As well as slave, if necessary). */
-	out8(PIC1_COMMAND, PIC_EOI);
-#ifdef CONFIG_IRQ
-	irq_dispatch(regs.trapno - 32);
-#endif
-}
 
