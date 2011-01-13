a7a2c1f4728c02d5399ab5f8231efe9d2ba6ff51
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-01-13 03:40:38 +0000

    #96: variable information utils

--- mk/util/var_info.mk
+++ mk/util/var_info.mk
@@ -20,8 +20,7 @@ include util/common.mk
 # Returns: Human-readable multiline string containing variable information.
 #
 define var_info_string
-$(call assert_called,var_info_string,$0) \
-  $(call assert,$1,Variable name is empty) Variable [$1] info:
+$(call assert,$1,Variable name is empty)Variable [$1] info:
    flavor: [$(flavor $1)]
    origin: [$(origin $1)]
     value: [$(value $1)]
@@ -40,8 +39,61 @@ endef
 #
 # See also: var_info_string
 #
-var_info = $(call assert_called,var_info,$0)$(info $(call var_info_string,$1))
+var_info = \
+  $(info $(call var_info_string,$1))
 
-var_undefined = $(call make_bool,$(findstring undefined,$(flavor $1)))
+##
+# Function: var_defined
+#
+# Checks whether the specified variable is defined.
+#
+# Params:
+#  1. The target variable name
+#
+# Returns: true if the specified variable exists, false otherwise
+#
+var_defined = \
+  $(call not,$(call var_undefined,$1))
+
+##
+# Function: var_simple
+#
+# Checks whether the specified variable is defined as statically expanded.
+#
+# Params:
+#  1. The target variable name
+#
+# Returns: true if the specified variable exists and its flavor is 'simple',
+#          false otherwise
+#
+var_simple = \
+  $(call make_bool,$(findstring simple,$(flavor $1)))
+
+##
+# Function: var_recursive
+#
+# Checks whether the specified variable is defined as recursively expanded.
+#
+# Params:
+#  1. The target variable name
+#
+# Returns: true if the specified variable exists and its flavor is 'recursive',
+#          false otherwise
+#
+var_recursive = \
+  $(call make_bool,$(findstring recursive,$(flavor $1)))
+
+##
+# Function: var_undefined
+#
+# Checks whether the specified variable is not defined.
+#
+# Params:
+#  1. The target variable name
+#
+# Returns: true if the variable has not been defined yet, false otherwise
+#
+var_undefined = \
+  $(call make_bool,$(findstring undefined,$(flavor $1)))
 
 endif # __util_var_info_mk
