fddbf7be50412d275993aeda6cfa7ccdd210cbdf
Anton Bondarev <anton.bondarev1978@gmail.com>
2011-02-09 13:42:23 +0000

    prom_printf

--- src/include/kernel/prom_printf.h
+++ src/include/kernel/prom_printf.h
@@ -11,8 +11,10 @@
 #define PROM_PRINTF_H_
 
 #include <stdio.h>
-
+#if defined(CONFIG_PROM_PRINTF)
 extern int prom_printf(const char *format, ...) __attribute__ ((format
 				(printf, 1, 2)));
-
+#else
+#define prom_printf(...)  do ; while(0)
+#endif
 #endif /* PROM_PRINTF_H_ */
