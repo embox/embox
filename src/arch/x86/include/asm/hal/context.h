171e1aa5f50691c9cfee195fc8dbe4c4d715540e
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-02-26 16:03:47 +0000

    Context switch for x86

--- src/arch/x86/include/asm/hal/context.h
+++ src/arch/x86/include/asm/hal/context.h
@@ -2,10 +2,13 @@
 # error "Do not include this file directly!"
 #endif /* HAL_CONTEXT_H_ */
 
-#include <setjmp.h>
 #include <types.h>
 
 struct context {
-	jmp_buf buf;
+	/* 0x00 */uint32_t eip; /**< TODO */
+	/* 0x04 */uint32_t ebx; /**< TODO */
+	/* 0x08 */uint32_t edi; /**< TODO */
+	/* 0x0c */uint32_t esi; /**< TODO */
+	/* 0x10 */uint32_t ebp; /**< TODO */
+	/* 0x14 */uint32_t esp; /**< TODO */
 };
-
