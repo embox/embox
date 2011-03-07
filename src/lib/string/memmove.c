cdf21d7369e4ada0bc5073d91b96ab44507bf957
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-03-07 03:17:23 +0000

    Rewrite memcpy using memmove, also rewrite memmove itself

--- src/lib/string/memmove.c
+++ src/lib/string/memmove.c
@@ -1,12 +1,40 @@
+/* This file is derived from the public domain implementation
+ * by David MacKenzie <djm@gnu.ai.mit.edu>.  */
+
 /**
  * @file
+ * @brief Implementation of memmove and memcpy functions.
  *
  * @date 23.11.09
- * @author Nikolay Korotky
+ * @author Eldar Abusalimov
  */
+
 #include <string.h>
 
-void *memmove(void *dst, const void *src, size_t n) {
-	bcopy (src, dst, n);
-	return dst;
+void *memcpy(void *dst, const void *src, size_t n) {
+	return memmove(dst, src, n);
 }
+
+void *memmove(void *_dst, const void *_src, size_t length) {
+	char *dst = _dst;
+	const char *src = _src;
+
+	if (src > dst) {
+		/* Moving from hi mem to low mem; start at beginning.  */
+		while (length--) {
+			*dst++ = *src++;
+		}
+
+	} else if (src != dst) {
+		/* Moving from low mem to hi mem; start at end.  */
+		src += length;
+		dst += length;
+		while (length--) {
+			*--dst = *--src;
+		}
+
+	}
+
+	return _dst;
+}
+
