7b422f41521c29fe2a8f5afc879eafd3a0c7a9c6
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-03-12 01:16:03 +0000

    #256: Refactoring ARP

--- src/cmds/arp.c
+++ src/cmds/arp.c
@@ -5,10 +5,12 @@
  * @date 24.04.09
  * @author Nikolay Korotky
  */
+
 #include <embox/cmd.h>
+
 #include <getopt.h>
-#include <net/arp.h>
 #include <netutils.h>
+#include <net/arp.h>
 
 EMBOX_CMD(exec);
 
@@ -16,38 +18,38 @@ static void print_usage(void) {
 	printf("Usage: arp [-i if] [-s|d] [-a host] [-m hwaddr] [-h]\n");
 }
 
-static int print_arp_cache(void *ifdev) {
+static void print_arp_cache(void *ifdev) {
 	size_t i;
 	unsigned char mac[18];
 	net_device_t *net_dev;
 	struct in_addr addr;
+
+	printf("Address\t\t\tHWtype\tHWaddress\t\tFlags\tIface\n");
+
 	for (i = 0; i < ARP_CACHE_SIZE; i++) {
-		if ((arp_tables[i].state == 1) &&
-		   (ifdev == NULL || ifdev == arp_tables[i].if_handler)) {
+		if ((arp_tables[i].state == 1) && (ifdev == NULL || ifdev
+				== arp_tables[i].if_handler)) {
 			net_dev = arp_tables[i].if_handler->dev;
 			macaddr_print(mac, arp_tables[i].hw_addr);
 			addr.s_addr = arp_tables[i].pw_addr;
-			TRACE("%s\t\t%s\t%s\t%c\t%s\n", inet_ntoa(addr),
-				arp_tables[i].if_handler->dev->type==1?"ether":"",
-				mac, arp_tables[i].flags==ATF_COM?'C':'P', net_dev->name);
+			printf("%s\t\t%s\t%s\t%c\t%s\n", inet_ntoa(addr),
+					arp_tables[i].if_handler->dev->type == 1 ? "ether" : "",
+					mac, arp_tables[i].flags == ATF_COM ? 'C' : 'P',
+					net_dev->name);
 		}
 	}
-	return 0;
 }
 
 static int exec(int argsc, char **argsv) {
-	int nextOption;
+	int opt;
 	struct in_addr addr;
 	unsigned char hwaddr[ETH_ALEN];
 	in_device_t *ifdev = NULL;
 	int op = -1;
+
 	getopt_init();
-	do {
-		nextOption = getopt(argsc, argsv, "hdsa:m:i:");
-		switch(nextOption) {
-		case 'h':
-			print_usage();
-			return 0;
+	while (-1 != (opt = getopt(argc, argv, "hdsa:m:i:"))) {
+		switch (opt) {
 		case 'd':
 			op = 0;
 			break;
@@ -56,40 +58,41 @@ static int exec(int argsc, char **argsv) {
 			break;
 		case 'a':
 			if (0 == inet_aton(optarg, &addr)) {
-				LOG_ERROR("wrong ip addr format (%s)\n", optarg);
+				printf("arp: invalid IP address: %s\n", optarg);
 				return -1;
 			}
 			break;
 		case 'm':
 			if (NULL == macaddr_scan((unsigned char *) optarg, hwaddr)) {
-				LOG_ERROR("wrong mac addr format %s\n", optarg);
+				printf("arp: invalid MAC address: %s\n", optarg);
 				return -1;
 			}
 			break;
 		case 'i':
 			if (NULL == (ifdev = inet_dev_find_by_name(optarg))) {
-				LOG_ERROR("can't find interface %s\n", optarg);
+				printf("arp: can't find interface %s\n", optarg);
 				return -1;
 			}
 			break;
-		case -1:
-			break;
+		case '?':
+		case 'h':
+			print_usage();
+			/* FALLTHROUGH */
 		default:
 			return 0;
 		}
-	} while (-1 != nextOption);
+	};
 
-	switch(op) {
-	case 0:
-		arp_delete_entity(ifdev, addr.s_addr, hwaddr);
+	if (op == -1) {
+		print_arp_cache(ifdev);
 		return 0;
-	case 1:
-		arp_add_entity(ifdev, addr.s_addr, hwaddr, ATF_PERM);
-		return 0;
-	default:
-		break;
 	}
-	printf("Address\t\t\tHWtype\tHWaddress\t\tFlags\tIface\n");
-	print_arp_cache(ifdev);
+
+	if (op) {
+		arp_add_entity(ifdev, addr.s_addr, hwaddr, ATF_PERM);
+	} else {
+		arp_delete_entity(ifdev, addr.s_addr, hwaddr);
+	}
+
 	return 0;
 }
