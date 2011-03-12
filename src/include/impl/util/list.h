3b15eb27064facc4d0231f0b2a77cb1ce762fcf6
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-03-12 19:13:28 +0000

    #254: something more for list

--- src/include/impl/util/list.h
+++ src/include/impl/util/list.h
@@ -22,37 +22,3 @@ struct list {
 
 #define __list_link_element(link, element_type, link_member) \
 		structof(link, element_type, link_member)
-
-#if 0
-#define list_foreach(element, list, link_member) \
-	  __list_foreach(element, list, link_member)
-
-#define list_get_first(list, element_type, link_member) \
-	  __list_get_first(list, element_type, link_member)
-
-#define list_get_last(list, element_type, link_member) \
-	  __list_get_last(list, element_type, link_member)
-
-#define list_add_first(element, list, link_member) \
-	  __list_add_first(element, list, link_member)
-
-#define list_add_last(element, list, link_member) \
-	  __list_add_last(element, list, link_member)
-
-#define list_remove(element, link_member) \
-	  __list_remove(element, link_member)
-
-#define list_link_element(link, element_type, link_member) \
-	  __list_link_element(link, element_type, link_member)
-
-#define list_foreach_link(link, list) \
-	  __list_foreach_link(link, list)
-
-inline static void list_add_first_link(struct list_link *link, struct list *list);
-inline static void list_add_last_link(struct list_link *link, struct list *list);
-
-inline static struct list_link *list_get_first_link(struct list *list);
-inline static struct list_link *list_get_last_link(struct list *list);
-
-inline static void list_remove_link(struct list_link *link);
-#endif
--- src/include/util/list.h
+++ src/include/util/list.h
@@ -15,14 +15,17 @@ struct list;
 
 struct list_link;
 
+extern void list_init(struct list *list);
+extern void list_link_init(struct list_link *link);
+
 #define list_foreach(element, list, link_member) \
 	  __list_foreach(element, list, link_member)
 
-#define list_get_first(list, element_type, link_member) \
-	  __list_get_first(list, element_type, link_member)
+#define list_first(list, element_type, link_member) \
+	  __list_first(list, element_type, link_member)
 
-#define list_get_last(list, element_type, link_member) \
-	  __list_get_last(list, element_type, link_member)
+#define list_last(list, element_type, link_member) \
+	  __list_last(list, element_type, link_member)
 
 #define list_add_first(element, list, link_member) \
 	  __list_add_first(element, list, link_member)
@@ -39,11 +42,13 @@ struct list_link;
 #define list_foreach_link(link, list) \
 	  __list_foreach_link(link, list)
 
+extern void list_empty(struct list *list);
+
 extern void list_add_first_link(struct list_link *link, struct list *list);
 extern void list_add_last_link(struct list_link *link, struct list *list);
 
-extern struct list_link *list_get_first_link(struct list *list);
-extern struct list_link *list_get_last_link(struct list *list);
+extern struct list_link *list_first_link(struct list *list);
+extern struct list_link *list_last_link(struct list *list);
 
 extern void list_remove_link(struct list_link *link);
 
