c73984f0caf29f78c049a8d2ffcf580702de4098
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-03-19 18:54:26 +0000

    #262: oops, ## instead of # in test_assert_xxx macros

--- src/include/embox/test.h
+++ src/include/embox/test.h
@@ -40,30 +40,30 @@
 	  __test_fail(reason)
 
 #define test_assert(condition) \
-	  __test_assert(#condition) /* # prevents condition from expansion. */
+	  __test_assert(##condition) /* ## prevents condition from expansion. */
 
 #define test_assert_true(value) \
-	  __test_assert_true(#value)
+	  __test_assert_true(##value)
 
 #define test_assert_false(value) \
-	  __test_assert_false(#value)
+	  __test_assert_false(##value)
 
 #define test_assert_zero(value) \
-	  __test_assert_zero(#value)
+	  __test_assert_zero(##value)
 
 #define test_assert_not_zero(value) \
-	  __test_assert_not_zero(#value)
+	  __test_assert_not_zero(##value)
 
 #define test_assert_null(value) \
-	  __test_assert_null(#value)
+	  __test_assert_null(##value)
 
 #define test_assert_not_null(value) \
-	  __test_assert_not_null(#value)
+	  __test_assert_not_null(##value)
 
 #define test_assert_equal(actual, expected) \
-	  __test_assert_equal(#actual, #expected)
+	  __test_assert_equal(##actual, ##expected)
 
 #define test_assert_not_equal(actual, expected) \
-	  __test_assert_not_equal(#actual, #expected)
+	  __test_assert_not_equal(##actual, ##expected)
 
 #endif /* EMBOX_TEST_H_ */
