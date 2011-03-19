8f8acd34a9ca22dfec0cb2d5f0dda63fe7c07950
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-03-19 19:28:01 +0000

    #262: some more thoughts about unit testing API

--- src/include/embox/test.h
+++ src/include/embox/test.h
@@ -33,36 +33,101 @@
 #define TEST_CASE(description) \
 	  __TEST_CASE(description)
 
-#define test_pass() \
-	  __test_pass()
-
+/**
+ * Fails a test with the given @a reason.
+ *
+ * This causes the following message to be logged:
+@verbatim
+failed: test_fail(<reason>)
+	at <file>:<line>
+@endverbatim
+ *
+ * @param reason (optional)
+ *   String literal containing the description of the test failure.
+ * @note
+ *   This macro function never returns.
+ */
 #define test_fail(reason) \
 	  __test_fail(reason)
 
+/**
+@verbatim
+failed: test_assert(<##condition>)
+	at <file>:<line>
+@endverbatim
+ */
 #define test_assert(condition) \
 	  __test_assert(##condition) /* ## prevents condition from expansion. */
 
+/**
+@verbatim
+failed: test_assert_true(<##value>)
+	at <file>:<line>
+@endverbatim
+ */
 #define test_assert_true(value) \
 	  __test_assert_true(##value)
 
+/**
+@verbatim
+failed: test_assert_false(<##value>)
+	at <file>:<line>
+@endverbatim
+ */
 #define test_assert_false(value) \
 	  __test_assert_false(##value)
 
+/**
+@verbatim
+failed: test_assert_zero(<##value>): <value>
+	at <file>:<line>
+@endverbatim
+ */
 #define test_assert_zero(value) \
 	  __test_assert_zero(##value)
 
+/**
+@verbatim
+failed: test_assert_not_zero(<##value>)
+	at <file>:<line>
+@endverbatim
+ */
 #define test_assert_not_zero(value) \
 	  __test_assert_not_zero(##value)
 
+/**
+@verbatim
+failed: test_assert_null(<##value>): <value>
+	at <file>:<line>
+@endverbatim
+ */
 #define test_assert_null(value) \
 	  __test_assert_null(##value)
 
+/**
+@verbatim
+failed: test_assert_not_null(<##value>)
+	at <file>:<line>
+@endverbatim
+ */
 #define test_assert_not_null(value) \
 	  __test_assert_not_null(##value)
 
+/**
+@verbatim
+failed: test_assert_equal(<##actual>, <##expected>): <actual>, expected <expected>
+	at <file>:<line>
+@endverbatim
+ */
 #define test_assert_equal(actual, expected) \
 	  __test_assert_equal(##actual, ##expected)
 
+/**
+@verbatim
+failed: test_assert_not_equal(<##actual>, <##expected>): <actual>
+	at <file>:<line>
+@endverbatim
+ */
 #define test_assert_not_equal(actual, expected) \
 	  __test_assert_not_equal(##actual, ##expected)
 
