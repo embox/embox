4b51a538bb0e47387222900738aea54cd49f22ca
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-03-20 00:53:53 +0000

    #263: Split test/core.c into registry and run

---
+++ src/framework/test/registry.c
@@ -0,0 +1,46 @@
+/**
+ * @file
+ * @brief Registry of available tests framework implementation.
+ *
+ * @date Dec 4, 2008
+ * @author Anton Bondarev
+ *         - Initial implementation
+ * @author Alexey Fomin
+ *         - Adding level implementation code
+ * @author Eldar Abusalimov
+ *         - Reworking handler search algorithm
+ *         - Distinguishing test suites and test cases
+ */
+
+#include <framework/test/api.h>
+
+#include <stddef.h>
+#include <string.h>
+
+#include <util/array.h>
+#include <mod/core.h>
+
+static int test_mod_enable(struct mod *mod);
+
+const struct mod_ops __test_mod_ops = {
+	.enable = &test_mod_enable,
+};
+
+ARRAY_SPREAD_DEF(const struct test_suite, __test_registry);
+
+static int test_mod_enable(struct mod *mod) {
+	return test_suite_run((struct test_suite *) mod_data(mod));
+}
+
+const struct test_suite *test_lookup(const char *name) {
+	const struct test_suite *test;
+
+	test_foreach(test) {
+		if (strcmp(test_name(test), name) == 0) {
+			return test;
+		}
+	}
+
+	return NULL;
+}
+
--- src/framework/test/run.c
+++ src/framework/test/run.c
@@ -1,30 +1,25 @@
 /**
  * @file
- * @brief Embox test framework implementation.
+ * @brief Test suite invocation and runtime support.
  *
  * @date Dec 4, 2008
  * @author Anton Bondarev
  *         - Initial implementation
- * @author Alexey Fomin
- *         - Adding level implementation code
  * @author Eldar Abusalimov
- *         - Reworking handler search algorithm
- *         - Distinguishing test suites and test cases
+ *         - Separating from tests registry code
+ *         - Assertions support
  */
-
-#include <framework/test/api.h>
-#include <test/tools.h>
 
 #include <assert.h>
 #include <errno.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <string.h>
 #include <setjmp.h>
 
-#include <util/array.h>
 #include <util/location.h>
-#include <mod/core.h>
+
+#include <framework/test/api.h>
+#include "assert_impl.h"
 
 /**
  * Runtime context for a test case.
@@ -35,21 +30,8 @@ struct test_run_context {
 
 struct test_run_context *current;
 
-static int test_mod_enable(struct mod *mod);
-
-const struct mod_ops __test_mod_ops = {
-	.enable = &test_mod_enable,
-};
-
-ARRAY_SPREAD_DEF(const struct test_suite, __test_registry);
-ARRAY_SPREAD_DEF(const struct test_failure, __test_failures);
-
 static int test_case_run(const struct test_case *test_case);
 static const struct __test_assertion_point *test_run(test_run_t run);
-
-static int test_mod_enable(struct mod *mod) {
-	return test_suite_run((struct test_suite *) mod_data(mod));
-}
 
 int test_suite_run(const struct test_suite *test) {
 	int total = 0, failures = 0;
@@ -124,17 +106,5 @@ void __test_assertion_handle0(int pass,
 	assert(point != NULL);
 	assert(current != NULL);
 	longjmp(current->before_run, (int) point);
-}
-
-const struct test_suite *test_lookup(const char *name) {
-	const struct test_suite *test;
-
-	test_foreach(test) {
-		if (strcmp(test_name(test), name) == 0) {
-			return test;
-		}
-	}
-
-	return NULL;
 }
 
