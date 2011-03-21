9488322fa9b3bc933e7577de4e28a192c354e9d6
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-03-21 02:51:31 +0000

    #262: Refactoring test_run (extracting output code)

--- src/framework/test/run.c
+++ src/framework/test/run.c
@@ -35,12 +35,22 @@ static int test_case_run(const struct test_case *test_case,
 		const struct __test_fixtures *fixtures);
 static const struct __test_assertion_point *test_run(test_case_run_t run);
 
+static void handle_suite_setup_failure(const struct test_suite *, int code);
+static void handle_suite_teardown_failure(const struct test_suite *, int code);
+static void handle_suite_result(const struct test_suite *, int failures,
+		int total);
+
+static void handle_case_setup_failure(const struct test_case *, int code);
+static void handle_case_teardown_failure(const struct test_case *, int code);
+static void handle_case_result(const struct test_case *,
+		const struct __test_assertion_point *failure);
+
 int test_suite_run(const struct test_suite *test) {
 	const struct test_case *test_case;
 	const struct __test_fixtures *fixtures;
 	__test_fixture_t fx;
 	const char *name;
-	int total = 0, failures = 0;
+	int failures = 0, total = 0;
 	int ret;
 
 	if (NULL == test) {
@@ -53,8 +63,7 @@ int test_suite_run(const struct test_suite *test) {
 	TRACE("test: running %s ", name);
 
 	if ((fx = *fixtures->setup) != NULL && (ret = fx()) != 0) {
-		TRACE("\n\tsuite fixture setup failed with code %d: %s\n",
-				ret, strerror(-ret));
+		handle_suite_setup_failure(test, ret);
 		return -EINTR;
 	}
 
@@ -71,18 +80,11 @@ int test_suite_run(const struct test_suite *test) {
 	}
 
 	if ((fx = *fixtures->teardown) != NULL && (ret = fx()) != 0) {
-		TRACE("\n\tsuite fixture tear down failed with code %d: %s\n",
-				ret, strerror(-ret));
+		handle_suite_teardown_failure(test, ret);
 		return -EINTR;
 	}
 
-	if (failures > 0) {
-		TRACE("\n\ttesting %s (%s) failed\n"
-				"\t\t%d/%d failures\n",
-				 name, test->description, failures, total);
-	} else if(!failures) {
-		TRACE(" done\n");
-	}
+	handle_suite_result(test, failures, total);
 
 	return failures;
 }
@@ -90,41 +92,24 @@ int test_suite_run(const struct test_suite *test) {
 static int test_case_run(const struct test_case *test_case,
 		const struct __test_fixtures *fixtures) {
 	const struct __test_assertion_point *failure;
-	const struct location *test_loc, *fail_loc;
 	__test_fixture_t fx;
 	int ret;
 
 	if ((fx = *fixtures->setup_each) != NULL && (ret = fx()) != 0) {
-		TRACE("\n\tcase fixture setup failed with code %d: %s\n",
-				ret, strerror(-ret));
+		handle_case_setup_failure(test_case, ret);
 		return -EINTR;
 	}
 
 	failure = test_run(test_case->run);
 
 	if ((fx = *fixtures->teardown_each) != NULL && (ret = fx()) != 0) {
-		TRACE("\n\tcase fixture tear down failed with code %d: %s\n",
-				ret, strerror(-ret));
+		handle_case_teardown_failure(test_case, ret);
 		return -EINTR;
 	}
 
-	if (!failure) {
-		TRACE(".");
-		return 0;
-	}
+	handle_case_result(test_case, failure);
 
-	test_loc = &test_case->location;
-	fail_loc = &failure->location.input;
-
-	TRACE("\n\tfailure at %s : %d, in function %s\n"
-			"\t\t%s\n",
-			fail_loc->file, fail_loc->line, failure->location.func,
-			failure->reason);
-	TRACE("\t   case at %s : %d\n"
-			"\t\t\"%s\"\n\t",
-			test_loc->file, test_loc->line, test_case->description);
-
-	return -1;
+	return !!failure;
 }
 
 static const struct __test_assertion_point *test_run(test_case_run_t run) {
@@ -151,3 +136,60 @@ void __test_assertion_handle0(int pass,
 	longjmp(current->before_run, (int) point);
 }
 
+static void handle_suite_setup_failure(const struct test_suite *test_suite,
+		int code) {
+	TRACE("\n\tsuite fixture setup failed with code %d: %s\n",
+			code, strerror(-code));
+}
+
+static void handle_suite_teardown_failure(const struct test_suite *test_suite,
+		int code) {
+	TRACE("\n\tsuite fixture tear down failed with code %d: %s\n",
+			code, strerror(-code));
+}
+
+static void handle_suite_result(const struct test_suite *test_suite,
+		int failures, int total) {
+	if (failures > 0) {
+		TRACE("\n\ttesting %s (%s) failed\n"
+				"\t\t%d/%d failures\n",
+				test_name(test_suite), test_suite->description,
+				failures, total);
+	} else if (!failures) {
+		TRACE(" done\n");
+	}
+
+}
+
+static void handle_case_setup_failure(const struct test_case *test_case,
+		int code) {
+	TRACE("\n\tcase fixture setup failed with code %d: %s\n",
+			code, strerror(-code));
+}
+
+static void handle_case_teardown_failure(const struct test_case *test_case,
+		int code) {
+	TRACE("\n\tcase fixture tear down failed with code %d: %s\n",
+			code, strerror(-code));
+}
+
+static void handle_case_result(const struct test_case *test_case,
+		const struct __test_assertion_point *failure) {
+	const struct location *test_loc, *fail_loc;
+
+	if (!failure) {
+		TRACE(".");
+		return;
+	}
+
+	test_loc = &test_case->location;
+	fail_loc = &failure->location.input;
+
+	TRACE("\n\tfailure at %s : %d, in function %s\n"
+			"\t\t%s\n",
+			fail_loc->file, fail_loc->line, failure->location.func,
+			failure->reason);
+	TRACE("\t   case at %s : %d\n"
+			"\t\t\"%s\"\n\t",
+			test_loc->file, test_loc->line, test_case->description);
+}
