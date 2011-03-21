173495953e759a4b45a18b88f49d2a7326399786
Alexandr Batyukov <batsun@gmail.com>
2011-03-21 12:39:47 +0000

    Fix 1ac65e9

--- src/arch/arm/include/asm/setjmp.h
+++ src/arch/arm/include/asm/setjmp.h
@@ -20,12 +20,16 @@
 #ifndef ARM_SETJMP_H_
 #define ARM_SETJMP_H_
 
-/*
+/**
  * All callee preserved registers:
  * v1 - v7, fp, ip, sp, lr, f4, f5, f6, f7
  */
 #define _JBLEN 23
 
+#ifndef __ASSEMBLER__
+
 typedef	int __jmp_buf[_JBLEN];
+
+#endif
 
 #endif /*ARM_SETJMP_H_*/
