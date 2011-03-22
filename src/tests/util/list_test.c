b42f85d77e18865498b18875202da6c93dcd9179
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-03-22 18:32:16 +0000

    #254: Rewrite list test using fixtures

--- src/tests/util/list_test.c
+++ src/tests/util/list_test.c
@@ -13,13 +13,17 @@
 
 EMBOX_TEST_SUITE("util/list test");
 
+TEST_SETUP(setup);
+
 struct element {
 	int some_stuff;
 	struct list_link m_link;
 };
 
+struct element e;
+struct list l;
+
 TEST_CASE("list_link_element should cast link member out to its container") {
-	struct element e;
 	struct list_link *link = &e.m_link;
 	test_assert_equal(list_link_element(link, struct element, m_link), &e);
 }
@@ -51,27 +55,22 @@ TEST_CASE("list_link_init should have the same effect as static initializer") {
 }
 
 TEST_CASE("list_empty should return true for just created list") {
-	struct list l = LIST_INIT(&l);
 	test_assert_true(list_empty(&l));
 }
 
 TEST_CASE("list_first should return null for empty list") {
-	struct list l = LIST_INIT(&l);
 	test_assert_null(list_first(&l, struct element, m_link));
 }
 
 TEST_CASE("list_last should return null for empty list") {
-	struct list l = LIST_INIT(&l);
 	test_assert_null(list_last(&l, struct element, m_link));
 }
 
 TEST_CASE("list_first_link should return null for empty list") {
-	struct list l = LIST_INIT(&l);
 	test_assert_null(list_first_link(&l));
 }
 
 TEST_CASE("list_last_link should return null for empty list") {
-	struct list l = LIST_INIT(&l);
 	test_assert_null(list_last_link(&l));
 }
 
@@ -117,3 +116,8 @@ TEST_CASE("test_list_first_on_a_single_element_list_should_return_the_element")
 }
 #endif
 
+static int setup(void) {
+	list_init(&l);
+	list_link_init(&e.m_link);
+	return 0;
+}
