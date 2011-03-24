70332675d7866a15eb72075b5cdc2ecd02665c13
Alina Kramar <kramar.alina@gmail.com>
2011-03-24 22:46:29 +0000

    Rewrite docs for sched_policy

--- src/include/kernel/thread/sched_policy.h
+++ src/include/kernel/thread/sched_policy.h
@@ -1,7 +1,8 @@
 /**
  * @file
- * @brief Defines the simplest operations in scheduler.
- * They are used is a part of other functions.
+ * @brief Defines the simplest operations in scheduler. Implementation depends
+ * on the scheduling algorithm. Part of method is used in other functions.
+ * All of policy must implement its interface.
  *
  * @date 22.04.2010
  * @author Dmitry Avdyukhin
@@ -14,45 +15,54 @@
 #include __impl_x(kernel/thread/types.h)
 
 /**
- * Function, executed when thread initializes.
- */
+ * Initializes all that is necessary for scheduling algorithm.
+ *  */
 extern void sched_policy_init(void);
 
 /**
- * Function, executed when thread starts.
+ * Is called then scheduler start.
  */
 extern void sched_policy_start(void);
 
 /**
- * Function, executed when scheduler stops.
- * Must be made in such way, that allows correctly call then sched_start.
+ * Executes when scheduler stops.
+ * Should be done in such way, that allows correctly call then sched_start.
  */
 extern void sched_policy_stop(void);
 
 /**
- * Adds new thread in thread list.
+ * Adds a new thread at scheduler. Should add thread into list of thread
+ * which are executed without changing the current thread.
  * @param added_thread
- * @return true when rescheduling is necessary
+ *  thread to adding
+ * @return
+ *  true when rescheduling is necessary. Otherwise @return false
  */
 extern bool sched_policy_add(struct thread *added_thread);
 
 /**
- * Move current_thread pointer to the next thread.
- * @param prev_thread thread, which have worked just now.
+ * Switches the current thread pointer to the most appropriate thread. Should
+ * change current thread. Threads execution order is implementation dependent.
+ * @param prev_thread
+ *  thread which runs now.
+ * @return
+ *  most appropriate for the execution thread
  */
 extern struct thread *sched_policy_switch(struct thread *prev_thread);
 
 /**
- * Removes thread from the scheduler.
+ * Removes thread from the scheduler. Shouldn't change current thread.
  * If thread doesn't exist in scheduler, there must be NO ERROR.
- * @param removed_thread thread to remove.
- * @return true when rescheduling is necessary
+ * @param removed_thread
+ *  thread to remove
+ * @return true when rescheduling is necessary. Otherwise @return false
  */
 extern bool sched_policy_remove(struct thread *removed_thread);
 
 /**
- * Gets current thread.
- * @return thread that runs now
+ * Gets current thread. Shouldn't change current thread between calls of switch
+ * @return
+ *  thread that runs now
  */
 extern struct thread *sched_policy_current(void);
 
