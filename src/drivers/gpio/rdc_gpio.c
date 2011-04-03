2d180eb082a1a137a888e35567eeee8910b3dfb6
Nikolay Korotkiy <sikmir@gmail.com>
2011-04-03 21:05:04 +0000

    x86: Work on #273

---
+++ src/drivers/gpio/rdc_gpio.c
@@ -0,0 +1,79 @@
+/**
+ * @file
+ * @brief RDC GPIO driver
+ *
+ * @date 31.03.11
+ * @author Nikolay Korotkiy
+ */
+#include <asm/io.h>
+#include <linux/init.h>
+#include <embox/unit.h>
+
+EMBOX_UNIT_INIT(gpio_init);
+
+#define RDC_CONTROL  0x80003848
+#define RDC_DATA     0x8000384c
+
+#define GPIO_RTCRD    16 /* Red LED */
+#define GPIO_RTCAS    15 /* Reset button */
+
+static unsigned long g_last_value = 0xffffffff;
+
+static inline void set_control(unsigned long mask) {
+	unsigned long tmp;
+	/* Select control register */
+	out32(RDC_CONTROL, 0xcf8);
+	tmp = inl(0xcfc);
+	/* raise to set GPIO function */
+	tmp |= mask;
+	out32(tmp, 0xcfc);
+}
+
+static inline void set_data(unsigned long mask) {
+	out32(RDC_DATA, 0xcf8);
+	g_last_value |= mask;
+	out32(g_last_value, 0xcfc);
+}
+
+static inline void clear_data(unsigned long mask) {
+	out32(RDC_DATA, 0xcf8);
+	g_last_value &= ~mask;
+	out32(g_last_value, 0xcfc);
+}
+
+int gpio_get_value(unsigned long mask) {
+	unsigned long tmp;
+	out32(RDC_DATA, 0xcf8);
+	tmp = in32(0xcfc);
+	return (tmp & mask) ? 1 : 0;
+}
+
+void gpio_input(unsigned long mask) {
+	/* raise logic level to turn off pull-down */
+	set_data(mask);
+	/* select as GPIO function */
+	set_control(mask);
+}
+
+void gpio_output(unsigned long mask, int value) {
+	if (value) {
+		set_data(mask);
+	} else {
+		clear_data(mask);
+	}
+	set_control(mask);
+}
+
+void gpio_set_value(unsigned long mask, int value) {
+	if (value) {
+		set_data(mask);
+	} else {
+		clear_data(mask);
+	}
+}
+
+static int __init gpio_init(void) {
+	//TODO:
+	return 0;
+}
+
