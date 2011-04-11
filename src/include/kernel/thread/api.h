0d1d08f4eec6ca963c9222dfd1da5448e70c6326
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-04-11 20:36:53 +0000

    more thread API docs

--- src/include/kernel/thread/api.h
+++ src/include/kernel/thread/api.h
@@ -39,15 +39,18 @@ typedef __thread_priority_t thread_priority_t;
 #define THREAD_PRIORITY_TOTAL \
 	(THREAD_PRIORITY_MIN - THREAD_PRIORITY_MAX + 1)
 
+/** User-space thread. */
 #define THREAD_FLAG_USER      (0x1 << 0)
+/** Thread is created in detached state. */
 #define THREAD_FLAG_DETACHED  (0x1 << 1)
+/** Thread is created suspended. */
 #define THREAD_FLAG_SUSPENDED (0x1 << 2)
 
 /**
  * Iterates over the list of all threads existing in the system.
  *
  * @param thread
- *   @code struct thread * @endcode iteration variable.
+ *   <em> struct thread * </em> iteration variable.
  */
 #define thread_foreach(thread) \
 	  __thread_foreach(thread)
@@ -78,12 +81,37 @@ extern struct thread *thread_self(void);
  * The new thread starts execution by invoking @a run with @a arg as its sole
  * argument.
  *
- * Depending on the given @a flags the behavior may differ as follows:
+ * A memory for the thread control block and its stack is dynamically allocated
+ * from an internal object pool. As far as an order of threads execution is
+ * generally undefined, resource reclamation becomes a bit complicated.
  *
+ * Thread becomes terminated either when it returns from its @a run function or
+ * after explicit call to #thread_exit(). In order to give other threads a
+ * chance to safely synchronize with the given thread or to inspect its return
+ * value, the memory can't be freed immediately after the thread terminates.
+ * Thus resources used by the thread may remain occupied even if it terminates.
+ *
+ * Kernel tracks an internal state of a thread called detach state which is
+ * either @c joinable or @c detached. Resources of a terminated thread are
+ * released when one of the following conditions becomes satisfied:
+ *
+ *   - The thread is in the joinable state and another thread has called
+ *     #thread_join() on it.
+ *
+ *   - The thread has been detached using #thread_detach() method or it was
+ *     initially created in the detached state using #THREAD_FLAG_DETACHED
+ *     flag.
+ *
+ * To avoid possible race conditions and memory leaks TODO
+ *
+ * @param p_thread
+ * @param flags
+ *   Depending on the given @a flags the behavior may differ as follows:
+ *     @n
  *   - If no flags are specified (@a 0 is passed as the value of @a flags
- *     argument), then the new thread is created in a @c joinable state (see
+ *     argument), then the new thread is created in a joinable state (see
  *     below) and starts immediately.
- *
+ *     @n
  *   - The #THREAD_FLAG_SUSPENDED flag suspends the execution of the thread
  *     being created instead of starting it immediately.
  *     The effect is alike as if #thread_suspend() has been called on the
@@ -92,12 +120,10 @@ extern struct thread *thread_self(void);
  *     Using this flag allows the caller to perform some additional actions
  *     on the newly created thread safely without a risk of being preempted by
  *     it. Adjusting its priority is an obvious example.
- *
+ *     @n
  *   - Presence of the #THREAD_FLAG_DETACHED flag causes the thread to be
- *     created in a so-called @c detached state.
+ *     created in a detached state.
  *
- * @param p_thread
- * @param flags
  * @param run
  * @param arg
  * @return
