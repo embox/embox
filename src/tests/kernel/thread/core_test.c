65e0afd843c2aa20b82dfa92ac818fea0417c19f
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-04-18 22:44:57 +0000

    Fix some comments in threads API test

--- src/tests/kernel/thread/core_test.c
+++ src/tests/kernel/thread/core_test.c
@@ -13,50 +13,59 @@
 
 EMBOX_TEST_SUITE("test for thread API");
 
-static void *foo_run(void *arg) {
+static void *arg_invert_run(void *arg) {
 	return (void *) ~((unsigned long) arg);
+}
+
+TEST_CASE("thread_create with default flags should return -EINVAL if the "
+		"first argument is NULL") {
+	test_assert_equal(thread_create(NULL, 0, arg_invert_run, (void *) 1),
+			-EINVAL);
+}
+
+TEST_CASE("thread_create should return -EINVAL if thread function is NULL") {
+	struct thread *foo;
+
+	test_assert_equal(-EINVAL, thread_create(&foo, 0, NULL, NULL));
+}
+
+TEST_CASE("thread_create with THREAD_FLAG_DETACHED should return zero and "
+		"shouldn't touch the pointer") {
+	struct thread *bar = (struct thread *) 0xa5a5a5a5;
+
+	test_assert_zero(
+			thread_create(&bar, THREAD_FLAG_DETACHED, arg_invert_run, NULL));
+	test_assert_equal(bar, (struct thread *) 0xa5a5a5a5);
+}
+
+TEST_CASE("thread_create with THREAD_FLAG_DETACHED should return zero even if "
+		"the pointer is NULL") {
+	test_assert_zero(
+			thread_create(NULL, THREAD_FLAG_DETACHED, arg_invert_run, NULL));
 }
 
 TEST_CASE("thread_join should retrieve the result of thread execution") {
 	struct thread *foo;
 	void *ret;
 
-	test_assert_zero(thread_create(&foo, 0, foo_run, (void *) 42UL));
+	test_assert_zero(thread_create(&foo, 0, arg_invert_run, (void *) 42UL));
 	test_assert_zero(thread_join(foo, &ret));
 	test_assert_equal(ret, (void *) ~42UL);
 }
 
-TEST_CASE("thread_create should return EINVAL when thread function is NULL") {
+TEST_CASE("thread_resume should return 0 if the thread was created with "
+		"THREAD_FLAG_SUSPENDED flag") {
 	struct thread *foo;
 
-	test_assert_equal(-EINVAL, thread_create(&foo, 0, NULL, NULL));
-}
-
-TEST_CASE("If flag in THREAD_FLAG_DETACHED, thread_create should return NULL and "
-		"pointer to struct thread should be not changed") {
-	struct thread *bar = (struct thread *) 0xa5a5a5a5;
-
-	test_assert_zero(thread_create(&bar, THREAD_FLAG_DETACHED, foo_run, NULL));
-	test_assert_equal(bar, (struct thread *) 0xa5a5a5a5);
-}
-
-TEST_CASE("thread_create should return EINVAL after calls with NULL pointer to "
-		"thread") {
-	test_assert_equal(-EINVAL, thread_create(NULL, 0, foo_run, (void *) 1));
-}
-
-TEST_CASE("thread_resume should return EINVAL because thread wasn't suspended") {
-	struct thread *foo;
-
-	test_assert_zero(thread_create(&foo, 0, foo_run, NULL));
-	test_assert_equal(thread_resume(foo), -EINVAL);
-}
-
-TEST_CASE("thread_resume should return 0 because thread was suspended") {
-	struct thread *foo;
-
-	test_assert_zero(thread_create(&foo, THREAD_FLAG_SUSPENDED, foo_run, NULL));
+	test_assert_zero(thread_create(&foo, THREAD_FLAG_SUSPENDED, arg_invert_run, NULL));
 	test_assert_zero(thread_resume(foo));
 }
 
+TEST_CASE("thread_resume should return -EINVAL if the thread hasn't been "
+		"suspended") {
+	struct thread *foo;
+
+	test_assert_zero(thread_create(&foo, 0, arg_invert_run, NULL));
+	test_assert_equal(thread_resume(foo), -EINVAL);
+}
 
