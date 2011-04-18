fc1aa4076c396c9239e8746ed52b421fe18249ba
Anton Kozlov <drakon.mega@gmail.com>
2011-04-18 20:20:39 +0000

    pins subsytem rewrite

--- src/drivers/pins/pins.c
+++ src/drivers/pins/pins.c
@@ -15,7 +15,7 @@
  * (very strange if it doesn't, using simply scheme)
  */
 
-#define N_PINS (sizeof(int))
+#define N_PINS 8 * (sizeof(int))
 
 struct handler_item_t {
 	int mask;
@@ -26,22 +26,48 @@ static struct handler_item_t handlers[N_PINS];
 
 static int n_handler = 0;
 
+static int mon_mask = 0; /* mask of monitorable pins */
+
+static inline int get_first_bit(int val) {
+	return val & (~val + 1);
+}
+
+static inline int get_first_bit_n(int val) {
+    register int i = get_first_bit(val);
+	register int j = 0;
+	i >>= 1;
+	while (i) {
+		j ++;
+		i >>= 1;
+	}
+	return j;
+}
+
 irq_return_t irq_pin_handler(irq_nr_t irq_nr, void *data) {
-	int i;
 	int current = pin_get_input(~0);
-	int changed = pin_get_input_changed();
-	for (i = 0; i < n_handler; i++) {
-		if (changed & handlers[i].mask) {
-			handlers[i].handler(handlers[i].mask & current, handlers[i].mask);
+	int changed = pin_get_input_changed() & mon_mask;
+	while (changed) {
+		int i = get_first_bit_n(changed);
+		struct handler_item_t *hnd = &handlers[i];
+		if (hnd->handler != NULL) {
+			hnd->handler(hnd->mask & current, hnd->mask);
 		}
+		changed &= ~(get_first_bit(changed));
 	}
 	return IRQ_HANDLED;
 }
 
 void pin_set_input_monitor(int mask, pin_handler_t pin_handler) {
-	handlers[n_handler].handler = pin_handler;
-	handlers[n_handler].mask = mask;
-	n_handler++;
+	int tmask = 0x1;
+	int i;
+	for (i = 0; i < N_PINS; i++) {
+		if (mask & tmask) {
+			handlers[i].handler = pin_handler;
+			handlers[i].mask = mask;
+		}
+		tmask <<= 1;
+	}
+	mon_mask |= mask;
 	pin_config_input(mask);
 	pin_set_input_interrupt(mask);
 }
