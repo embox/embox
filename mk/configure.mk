3faba5200caad0c237a707bb3766408d3c0089fa
Nikolay Korotkiy <sikmir@gmail.com>
2011-04-21 09:08:04 +0000

    Close #274

--- mk/configure.mk
+++ mk/configure.mk
@@ -3,7 +3,7 @@
 #
 
 HOSTCC  = gcc
-SVN_REV = `svn info $(ROOT_DIR) | grep Rev: | awk '{print $$4}'`
+SVN_REV = $(shell svn info $(ROOT_DIR) | grep Rev: | awk '{print $$4}')
 HOSTCPP = $(HOSTCC) -E
 
 HOSTCC_MAJOR := \
@@ -66,7 +66,11 @@ $(config_h) $(config_lds_h) :
 	$(HOSTCPP) -Wp, -P -undef -nostdinc $(HOSTCC_CPPFLAGS) $(DEFS:%=-D%) \
 	-MMD -MT $@ -MF $@.d $(MK_DIR)/confmacro.S \
 		| sed 's/$$N/\n/g' | sed 's/$$define/#define/g' > $@
+ifeq ($(SVN_REV),)
+	@echo "svn cmd not found"
+else
 	@echo "#define CONFIG_SVN_REV $(SVN_REV)" >> $@
+endif
 
 $(AUTOCONF_FILES) : $(MK_DIR)/configure.mk \
   | check_conf_dir mkdir # these goals shouldn't force target to be updated
