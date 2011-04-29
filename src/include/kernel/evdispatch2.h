71d46f08cff7839c831b750bf20dddc7ab34e208
Nikolay Malkovsky <malkovskynv@gmail.com>
2011-04-29 19:43:30 +0000

    Add another version of event dispatcher implementation
    This version binds message to a handler directly.

---
+++ src/include/kernel/evdispatch2.h
@@ -0,0 +1,40 @@
+/**
+ * @brief  Event dispatcher header
+ * @details
+ 	This implementation of event dispatching doesn't bind message to
+ 	a specific number, and then by number returning the handler.
+ 	Here we will bind message to a handler directly.
+ *
+ * @date 28.04.2011
+ * @author Malkovsky Nikolay
+ */
+
+#ifndef EVDISPATCH2_H_
+#define EVDISPATCH2_H_
+
+#include <lib/list.h>
+
+/** Structure, describing messages, sent to handlers. */
+struct event_msg {
+	/** specified handler to handle the message */
+	void (*handler)(void *data);
+	/** data in message */
+	void *data;
+	/** the list of message in message queue */
+	struct list_head list;
+};
+
+/**
+ * Call handles for all message in queue.
+ */
+extern void event_dispatch(void);
+
+/**
+ * Send message to handler
+ * @param id, that corresponds to message
+ * @param data, that will be send in message
+ */
+extern void event_send(void (*handler)(void *data), void *data);
+
+
+#endif /* EVDISPATCH_H_ */
---
+++ src/kernel/evdispatch/evdispatch2.c
@@ -0,0 +1,74 @@
+/**
+ * @brief Event dispatcher implementation
+ *
+ * @date 28.04.2011
+ * @author Malkovsky Nikolay
+ */
+
+#include <lib/list.h>
+#include <util/pool.h>
+#include <kernel/evdispatch2.h>
+
+/** Maxumum messages count */
+#define MAX_MSG_COUNT_IN_QUEUE 10
+
+/** pool for message in queue */
+POOL_DEF(msg_queue_pool, struct event_msg, MAX_MSG_COUNT_IN_QUEUE);
+
+/** Queue of messages, sent to handlers */
+static LIST_HEAD(msg_queue);
+
+#include <embox/unit.h>
+#define make_cache_name(name) \
+		""#name""
+#include <kernel/mm/slab.h>
+EMBOX_UNIT_INIT(event_dispatcher_init);
+
+static int event_dispatcher_init(void) {
+	cache_t *cache;
+
+	cache = cache_create(make_cache_name(msg_queue_cache), sizeof(struct event_msg), MAX_MSG_COUNT_IN_QUEUE);
+
+	return 0;
+}
+
+void event_dispatch() {
+	while (!list_empty(&msg_queue)) {
+		struct list_head *result = msg_queue.next;
+		struct event_msg *msg = list_entry(result, struct event_msg, list);
+		list_del(result);
+
+		msg->handler(msg->data);
+
+		pool_free(&msg_queue_pool, msg);
+	}
+}
+
+/**
+ * Create the message to add it to queue.
+ *
+ * @param id id, that corresponds to type of message
+ * @param data data, that will be send in message
+ * @return pointer to created message
+ */
+static struct event_msg *create_message(void (*handler)(void *data), void *data) {
+	struct event_msg *msg = pool_alloc(&msg_queue_pool);
+	msg->handler = handler;
+	msg->data = data;
+	return msg;
+}
+
+/**
+ * Sends message.
+ *
+ * @param handler, handler to handle a massage
+ * @param data, message data
+ */
+void event_send(void (*handler)(void *data), void *data) {
+	struct event_msg *msg;
+
+	msg = create_message(handler, data);
+	list_add_tail(&msg->list, &msg_queue);
+}
+
+
