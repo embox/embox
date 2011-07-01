d888a4b8bfd4e87cf29b376313a97ccbc723148c
Anton Kozlov <drakon.mega@gmail.com>
2011-07-01 13:16:17 +0000

    nxt_bluetooth to work with any usart

--- src/tests/bluetooth/nxt_bt.c
+++ src/tests/bluetooth/nxt_bt.c
@@ -13,6 +13,7 @@
 
 #include <drivers/bluetooth.h>
 #include <drivers/nxt_bluetooth.h>
+#include <drivers/pins.h>
 
 static bt_message_t msg;
 
@@ -20,15 +21,23 @@ static uint8_t tx_buff[256];
 
 EMBOX_TEST(nxt_bluetooth_test);
 
+#define OLIMEX_SAM7_LED1 (1 << 17)
 
 static int nxt_bluetooth_test(void) {
 	int len;
+	int res;
 	msg.length = 0;
 	msg.type = 0x2f;
 	len = bt_wrap(&msg, tx_buff);
 	TRACE("bt_write start\n");
+	pin_config_output(OLIMEX_SAM7_LED1);
+	pin_clear_output(OLIMEX_SAM7_LED1);
 
 	bluetooth_write(tx_buff,len);
 	usleep(250);
-	return (bt_mod_version != 0xffff ? 0 : -1);
+	res = (bt_mod_version != 0xffff );
+	if (res) {
+	    pin_set_output(OLIMEX_SAM7_LED1);
+	}
+	return (res ? 0 : -1);
 }
