d6945116a7e69db4e4d05f002b552a8703f5ddd3
Dmitry Zubarevich <dmitryzubarevich@gmail.com>
2011-07-01 11:48:44 +0000

    [No commit log message]

---
+++ src/include/embox/net.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief Main Embox netpack include file for user netpacks.
+ *
+ * @date 01.07.11
+ * @author Dmitry Zubarevich
+ */
+
+#ifndef NET_H_
+#define NET_H_
+
+#include <framework/mod/self.h>
+
+#endif /* NET_H_ */
