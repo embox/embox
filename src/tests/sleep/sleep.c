889740a1abd9fdc1567d06b5c7524d5091b4e36b
Fedor Burdun <ignwah@gmail.com>
2011-07-06 10:43:10 +0000

    test cases comment for embox.test.sleep

--- src/tests/sleep/sleep.c
+++ src/tests/sleep/sleep.c
@@ -14,12 +14,29 @@
 EMBOX_TEST_SUITE("sleep suite");
 
 #define EPSILON_BORDER 10
+#define TIME_TO_SLEEP 100
 
-TEST_CASE("simple one sleep") {
+/**
+ *  sleep( any_time )
+ *  assert that real time for sleep different with any_time is less than EPSILON_BORDER
+ */
+TEST_CASE("one sleep") {
+	/* TODO: change uint32_t to system time_t type */
 	uint32_t cur_time = cnt_system_time();
 	uint32_t epsilon;
-	usleep(3000);
-	epsilon = abs( (int)(cnt_system_time() - cur_time) - (int)3000 );
+	usleep(TIME_TO_SLEEP);
+	epsilon = abs( (int)(cnt_system_time() - cur_time) - (int)TIME_TO_SLEEP );
 	test_assert_true(epsilon<EPSILON_BORDER);
+}
+
+/**
+ *  run ~3 threads (~3 times) with 1,2,3 sec wait
+ *  assert right done sequence of threads
+ */
+TEST_CASE("simple multithreaded check") {
+}
+
+TEST_CASE("sleep sort") {
+	/* without print to screen, using EMIT Buffer */
 }
 
