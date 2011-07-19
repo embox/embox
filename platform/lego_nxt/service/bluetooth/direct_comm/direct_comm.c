65e39f1d1601e25f7c51fc3304e0131b0d60699e
Anton Kozlov <drakon.mega@gmail.com>
2011-07-19 15:31:25 +0000

    direct comm fixes + 'embox' identification by keep-a-live dc command

--- platform/lego_nxt/service/bluetooth/direct_comm/direct_comm.c
+++ platform/lego_nxt/service/bluetooth/direct_comm/direct_comm.c
@@ -26,11 +26,11 @@ static enum {
 	COMM_REPLY
 } reader_state;
 
-int reply_need = 0;
+static int reply_need = 0;
 
-int command = 0;
-int size = 0;
-int type = 0;
+static int command = 0;
+static int size = 0;
+static int type = 0;
 
 EMBOX_UNIT_INIT(nxt_direct_comm_init);
 
@@ -60,12 +60,19 @@ static int handle_type(uint8_t *buff) {
 	return 0;
 }
 
-static void reply_handle(uint8_t status) {
-	uint8_t reply[] = {0x02, 0x00, 0x00};
-	reply[1] = command;
-	reply[2] = status;
+/*two part array */
+static uint8_t out_size[2];
+static uint8_t __attribute__ ((unused))  out_addit[16];
+
+static void reply_handle(uint8_t status, int addit_len) {
+	/* its special to write to out_addit */
+	out_size[0] = (addit_len) & 0xff;
+	out_size[1] = 0;
+	out_size[2] = 0x02;
+	out_size[3] = command;
+	out_size[4] = status;
 	reply_need = 0;
-	bluetooth_write(reply, 3);
+	bluetooth_write(out_size, 2 + addit_len);
 }
 
 static int handle_comm(uint8_t *buff) {
@@ -76,24 +83,36 @@ static int handle_comm(uint8_t *buff) {
 
 #define EDGE 200
 
-static int sensor_send(uint8_t sensor_id) {
-	nxt_sensor_t *sens = nxt_get_sensor(sensor_id);
-	sensor_val_t tmp = 0;
-	uint8_t out[16];
-	memset(out, 0, 16);
-	out[0] = 0x02;
-	out[1] = command;
-	out[4] = 0;
-	tmp =  nxt_sensor_get_val(sens);
-	out[8] = (tmp >> 8) & 0xff;
-	out[9] = tmp & 0xff;
-	out[14] = (tmp > EDGE ? 0 : 1);
-	reply_need = 0;
-	bluetooth_write(out, 16);
+static int sensor_send(uint8_t sensor_id, int &addit_len) {
+	sensor_val_t sens_val =  nxt_sensor_get_val(nxt_get_sensor(sensor_id));
+	memset(out_addit, 0, 16);
+
+	out_addit[3] = sensor_id;
+	out_addit[4] = 1;
+	out_addit[5] = 1;
+	out_addit[8] = (sens_val >> 8) & 0xff;
+	out_addit[9] = sens_val & 0xff;
+	out_addit[10] = out_addit[8];
+	out_addit[11] = out_addit[9];
+	out_addit[14] = (sens_val > EDGE ? 0 : 1);
+	*addit_len = 16;
 	return 0;
 }
 
-static int handle_body(uint8_t *buff) {
+static int keep_alive_send(int *addit_len) {
+	*addit_len = 4;
+#ifdef LEGO_COMPATIBLE
+	TRACE("Hi! I'm Lego =(\n");
+	return 0;
+#else
+	TRACE("Hi! I'm Embox!\n");
+	return 1;
+#endif
+
+}
+
+
+static int handle_body(uint8_t *buff, int *addit_len) {
 	reader_state = COMM_TYPE;
 	uint8_t power;
 	switch (command) {
@@ -106,16 +125,20 @@ static int handle_body(uint8_t *buff) {
 			nxt_motor_set_power(NXT_MOTOR_B, power);
 			nxt_motor_set_power(NXT_MOTOR_C, power);
 		}
+		*addit_len = 0;
 		return 0;
 		break;
 	case DC_GET_INPUT_VALUES:
-		sensor_send(buff[0]);
-		return 0;
+		return sensor_send(buff[0], &addit_len);
+		break;
+	case DC_KEEP_ALIVE:
+		return keep_alive_send(&addit_len);
 		break;
 	case DC_EX_SET_M_OUTPUT_STATE:
 		nxt_motor_set_power(NXT_MOTOR_A, buff[0]);
 		nxt_motor_set_power(NXT_MOTOR_B, buff[1]);
 		nxt_motor_set_power(NXT_MOTOR_C, buff[2]);
+		*addit_len = 0;
 		return 0;
 		break;
 	case DC_QREAL_HELLO:
@@ -133,16 +156,17 @@ static int handle_size(uint8_t *buff) {
 }
 
 static int direct_comm_handle(int msg, uint8_t *buff) {
-	int ret_val = 0;
+	int next_read_cnt = 0;
 	uint8_t *cbuf;
 	uint8_t status;
+	int addit_len;
 	if (msg == BT_DRV_MSG_CONNECTED) {
 	    bluetooth_read(direct_comm_buff, MSG_SIZE_BYTE_CNT);
 	} else if (msg == BT_DRV_MSG_READ) {
 	    switch (reader_state) {
 	    case COMM_SIZE:
 		    reader_state = COMM_TELEGRAM;
-		    ret_val = handle_size(buff);
+		    next_read_cnt = handle_size(buff);
 		    break;
 	    case COMM_TELEGRAM:
 		    cbuf = buff;
@@ -150,18 +174,18 @@ static int direct_comm_handle(int msg, uint8_t *buff) {
 		    cbuf += 1;
 		    handle_comm(cbuf);
 		    cbuf += 1;
-		    status = handle_body(cbuf);
+		    status = handle_body(cbuf, &addit_len);
 		    if (reply_need) {
-			    reply_handle(status);
+			    reply_handle(status, addit_len);
 		    }
 		    command = 0;
 		    reader_state = COMM_SIZE;
-		    ret_val = MSG_SIZE_BYTE_CNT;
+		    next_read_cnt = MSG_SIZE_BYTE_CNT;
 		    break;
 	    default:
 		    break;
 	    }
-	    bluetooth_read(direct_comm_buff, ret_val);
+	    bluetooth_read(direct_comm_buff, next_read_cnt);
 	}
 	return 0;
 }
