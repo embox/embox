4ceb9ec69fecf73dd033a3c85cb2b047e294f2c9
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-08-23 17:30:52 +0000

    Fix a warning produced by sparc-elf-4.4.2

--- src/arch/sparc/kernel/traps.c
+++ src/arch/sparc/kernel/traps.c
@@ -31,7 +31,7 @@ void bad_trap_handler(uint8_t tt, struct pt_regs *r) {
 			rw->locals[4], rw->locals[5], rw->locals[6], rw->locals[7]);
 	TRACE("%%I: %08x %08x  %08x %08x  %08x %08x  %08x %08x\n",
 			rw->ins[0], rw->ins[1], rw->ins[2], rw->ins[3],
-			rw->ins[4], rw->ins[5], rw->ins[6], rw->ins[7]);
+			rw->ins[4], rw->ins[5], (uint32_t) rw->fp, (uint32_t) rw->ret_pc);
 
 	while (1)
 		;
