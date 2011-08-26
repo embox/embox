f7151aee03f3daf6ecf54a6b332b61da4d53fcbc
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-08-26 00:19:57 +0000

    Rename count.c to critical.c, move big functions into it

--- src/include/kernel/critical/api.h
+++ src/include/kernel/critical/api.h
@@ -17,10 +17,8 @@
 #define CRITICAL_IRQ_LOCK         0x0000003f
 #define CRITICAL_IRQ_HANDLER      0x00000fc0
 #define CRITICAL_SOFTIRQ_LOCK     0x0003f000
-#define CRITICAL_SOFTIRQ_DISPATCH 0x00040000
-#define CRITICAL_SOFTIRQ_HANDLER  0x01f80000
-#define CRITICAL_SCHED_LOCK       0x7e000000
-#define CRITICAL_SCHED_DISPATCH   0x80000000
+#define CRITICAL_SOFTIRQ_HANDLER  0x00fc0000
+#define CRITICAL_SCHED_LOCK       0x3f000000
 
 #include __impl_x(kernel/critical/critical_impl.h)
 
@@ -44,57 +42,8 @@ extern int critical_inside(critical_t level);
 extern void critical_enter(critical_t level);
 extern void critical_leave(critical_t level);
 
-static inline void critical_dispatch_pending(void) {
-	extern struct critical_dispatcher *__critical_dispatch_queue;
-	struct critical_dispatcher *d;
-	__critical_t mask;
-	__critical_t count = __critical_count_get();
-	ipl_t ipl;
-
-	ipl = ipl_save();
-
-	while ((d = __critical_dispatch_queue) && !((mask = d->mask) & count)) {
-		__critical_dispatch_queue = d->next;
-		d->mask = ~mask;
-
-		d->dispatch();
-	}
-
-	ipl_restore(ipl);
-}
-
 static inline int critical_pending(struct critical_dispatcher *d) {
 	return d->mask & 0x1;
-}
-
-static inline void critical_request_dispatch(struct critical_dispatcher *d) {
-	extern struct critical_dispatcher *__critical_dispatch_queue;
-	struct critical_dispatcher **pp = &__critical_dispatch_queue;
-	__critical_t inv_mask;
-	ipl_t ipl;
-
-	assert(d != NULL);
-
-	ipl = ipl_save();
-	if (critical_pending(d)) {
-		ipl_restore(ipl);
-		return;
-	}
-
-	inv_mask = d->mask;
-
-	while (*pp && !((*pp)->mask & inv_mask)) {
-		pp = &(*pp)->next;
-	}
-
-	d->next = *pp;
-	*pp = d;
-
-	d->mask = ~inv_mask;
-
-	ipl_restore(ipl);
-
-	critical_dispatch_pending();
 }
 
 extern void critical_request_dispatch(struct critical_dispatcher *d);
---
+++ src/kernel/critical/critical.c
@@ -0,0 +1,61 @@
+/**
+ * @file
+ * @brief TODO
+ *
+ * @date 25.03.11
+ * @author Eldar Abusalimov
+ */
+
+#include <kernel/critical/api.h>
+
+__critical_t __critical_count;
+
+static struct critical_dispatcher *dispatch_queue;
+
+void critical_dispatch_pending(void) {
+	struct critical_dispatcher *d;
+	__critical_t mask;
+	__critical_t count = __critical_count_get();
+	ipl_t ipl;
+
+	ipl = ipl_save();
+
+	while ((d = dispatch_queue) && !((mask = d->mask) & count)) {
+		dispatch_queue = d->next;
+		d->mask = ~mask;
+
+		d->dispatch();
+	}
+
+	ipl_restore(ipl);
+}
+
+void critical_request_dispatch(struct critical_dispatcher *d) {
+	struct critical_dispatcher **pp = &dispatch_queue;
+	__critical_t inv_mask;
+	ipl_t ipl;
+
+	assert(d != NULL);
+
+	ipl = ipl_save();
+	if (critical_pending(d)) {
+		ipl_restore(ipl);
+		return;
+	}
+
+	inv_mask = d->mask;
+
+	while (*pp && !((*pp)->mask & inv_mask)) {
+		pp = &(*pp)->next;
+	}
+
+	d->next = *pp;
+	*pp = d;
+
+	d->mask = ~inv_mask;
+
+	ipl_restore(ipl);
+
+	critical_dispatch_pending();
+}
+
