0ef5b10dd3993e205ed8206337bc704b2fb1769b
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-08-27 20:55:58 +0000

    Rewrite irq_nesting test

--- src/tests/irq/irq_nesting.c
+++ src/tests/irq/irq_nesting.c
@@ -5,9 +5,6 @@
  * @author Eldar Abusalimov
  */
 
-#include <types.h>
-#include <string.h>
-
 #include <embox/test.h>
 #include <kernel/irq.h>
 #include <hal/interrupt.h>
@@ -15,48 +12,35 @@
 #define TEST_OUTER_IRQ_NR 10
 #define TEST_INNER_IRQ_NR (TEST_OUTER_IRQ_NR + 1)
 
-EMBOX_TEST(run);
-
-volatile static bool outer_irq, inner_irq, irq_nested;
+EMBOX_TEST_SUITE("IRQ tests");
 
 static irq_return_t test_isr(irq_nr_t irq_nr, void *dev_id) {
-	if (!outer_irq) {
-		outer_irq = true;
+	test_emit((int) dev_id);
+	if (irq_nr == TEST_OUTER_IRQ_NR) {
+		test_emit('(');
 		interrupt_force(TEST_INNER_IRQ_NR);
-		for (volatile int i = 0; i < 100; ++i) {
-			/* some processors needs few cycle to reassert irq */
-		}
-		if (inner_irq) {
-			irq_nested = true;
-		} else {
-			TRACE("no inner irq received being inside outer irq handler\n");
-		}
-	} else if (!inner_irq) {
-		inner_irq = true;
-	} else {
-		TRACE("unexpected irq nr %d\n", irq_nr);
+		test_emit(')');
 	}
+	test_emit('x');
 	return IRQ_HANDLED;
 }
 
-static int run(void) {
-	int error;
+TEST_CASE("TODO") {
+	test_emit('a');
 
-	outer_irq = false;
-	inner_irq = false;
-	irq_nested = false;
+	test_assert_zero(irq_attach(TEST_OUTER_IRQ_NR, test_isr, 0,
+			(void *) '0', "test_irq_outer"));
+	test_assert_zero(irq_attach(TEST_INNER_IRQ_NR, test_isr, 0,
+			(void *) '1', "test_irq_inner"));
 
-	if (0 != (error = irq_attach(TEST_OUTER_IRQ_NR, test_isr, 0x0, NULL,
-			"test_irq_first")) || 0 != (error = irq_attach(TEST_INNER_IRQ_NR,
-			test_isr, 0x0, NULL, "test_irq_second"))) {
-		TRACE("irq_attach failed: %s\n", strerror(-error));
-		return -1;
-	}
-
+	test_emit('{');
 	interrupt_force(TEST_OUTER_IRQ_NR);
+	test_emit('}');
 
-	irq_detach(TEST_OUTER_IRQ_NR, NULL);
-	irq_detach(TEST_INNER_IRQ_NR, NULL);
+	test_assert_zero(irq_detach(TEST_OUTER_IRQ_NR, (void *) '0'));
+	test_assert_zero(irq_detach(TEST_INNER_IRQ_NR, (void *) '1'));
 
-	return outer_irq && inner_irq && irq_nested ? 0 : -1;
+	test_emit('z');
+
+	test_assert_emitted("a{0(1x)x}z");
 }
