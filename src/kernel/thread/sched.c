639446bebb89f226a763ddd9026e6dac17814511
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-08-30 15:37:43 +0000

    oops, remove duplicated code from sched.c and sched_lock.h

--- src/kernel/thread/sched.c
+++ src/kernel/thread/sched.c
@@ -281,36 +281,3 @@ static int unit_fini(void) {
 	return 0;
 }
 
-
-/**
- * Locks the scheduler which means disabling thread switch until
- * #sched_unlock() is called. Each lock must be balanced with the corresponding
- * unlock.
- *
- * @see sched_unlock()
- */
-void sched_lock(void) {
-	critical_enter(CRITICAL_SCHED_LOCK);
-}
-
-/**
- * Unlocks the scheduler after #sched_lock(). Must be called on the
- * previously locked scheduler only. Outermost unlock (which corresponds to
- * the first lock call) also dispatches pending thread switches (if any).
- *
- * @see sched_lock()
- */
-void sched_unlock(void) {
-	critical_leave(CRITICAL_SCHED_LOCK);
-	critical_dispatch_pending();
-}
-
-/**
- * Does the same as #sched_unlock() except that the outermost call does not
- * perform a check for pending switches.
- *
- * @note This function is not intended for wide usage.
- */
-void sched_unlock_noswitch(void) {
-	critical_leave(CRITICAL_SCHED_LOCK);
-}
