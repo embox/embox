f2a7b47172a595540746fae55f76bbd914e544d1
Anton Bondarev <anton.bondarev1978@gmail.com>
2011-09-06 14:10:28 +0000

    Move task's test to example

--- src/example/kernel/task/task.c
+++ src/example/kernel/task/task.c
@@ -7,16 +7,18 @@
  */
 
 #include <types.h>
+#include <stdio.h>
+#include <errno.h>
+#include <framework/example/self.h>
 #include <kernel/task.h>
 #include <kernel/thread/api.h>
-#include <embox/test.h>
 
 extern int task_create_with_io(struct task **new, struct task *parent, FILE *stdin, FILE *stdout, FILE *stderr);
 extern ssize_t write(int fd, const void *buf, size_t nbyte);
 extern int thread_create_task(struct thread **p_thread, unsigned int flags,
 		void *(*run)(void *), void *arg, struct task *tsk);
 
-EMBOX_TEST(task_io_example_run);
+EMBOX_EXAMPLE(task_io_example_run);
 
 static void *thread_handler(void *data) {
 	char ch = 'a';
@@ -27,12 +29,13 @@ static void *thread_handler(void *data) {
 	return thread_self();
 }
 
-static int task_io_example_run(void) {
+static int task_io_example_run(int argc, char **argv) {
 	struct task *new_task;
 	struct thread *thd;
 	void *ret;
 	FILE *file = fopen("/dev/uart", "rw");
 
+	printf("file opened\n");
 	task_create_with_io(&new_task, NULL, file, file, file);
 
 	thread_create_task(&thd, 0, thread_handler, NULL, new_task);
