388907464b07add98f9f1477cca67a84b43608c5
Anton Kozlov <drakon.mega@gmail.com>
2011-09-06 12:22:48 +0000

    Split file in fs file & kernel file abstraction

--- src/include/fs/file.h
+++ src/include/fs/file.h
@@ -9,6 +9,7 @@
 #define FILE_H_
 
 #include <types.h>
+#include <kernel/file.h>
 
 typedef struct file {
 	const char                    *f_path;
@@ -17,22 +18,6 @@ typedef struct file {
 	unsigned int                   f_mode;
 	unsigned int                   f_pos;
 } file_t;
-
-typedef void  *(*FILEOP_OPEN)(const char *file_name, const char *mode);
-typedef int    (*FILEOP_CLOSE)(void * file);
-typedef size_t (*FILEOP_READ)(void *buf, size_t size, size_t count, void *file);
-typedef size_t (*FILEOP_WRITE)(const void *buf, size_t size, size_t count, void *file);
-typedef int    (*FILEOP_FSEEK)(void *file, long offset, int whence);
-typedef int    (*FILEOP_IOCTL)(void *file, int request, va_list args);
-
-typedef struct file_operations {
-	FILEOP_OPEN  fopen;
-	FILEOP_CLOSE fclose;
-	FILEOP_READ  fread;
-	FILEOP_WRITE fwrite;
-	FILEOP_FSEEK fseek;
-	FILEOP_IOCTL ioctl;
-} file_operations_t;
 
 typedef struct stat {
 	int       st_dev;     /* ID of device containing file */
---
+++ src/include/kernel/file.h
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * @brief Kernel file abstraction
+ *
+ * @date 06.09.11
+ * @author Anton Kozlov
+ */
+
+typedef void  *(*FILEOP_OPEN)(const char *file_name, const char *mode);
+typedef int    (*FILEOP_CLOSE)(void * file);
+typedef size_t (*FILEOP_READ)(void *buf, size_t size, size_t count, void *file);
+typedef size_t (*FILEOP_WRITE)(const void *buf, size_t size, size_t count, void *file);
+typedef int    (*FILEOP_FSEEK)(void *file, long offset, int whence);
+typedef int    (*FILEOP_IOCTL)(void *file, int request, va_list args);
+
+typedef struct file_operations {
+	FILEOP_OPEN  fopen;
+	FILEOP_CLOSE fclose;
+	FILEOP_READ  fread;
+	FILEOP_WRITE fwrite;
+	FILEOP_FSEEK fseek;
+	FILEOP_IOCTL ioctl;
+} file_operations_t;
+
