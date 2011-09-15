7b96839039a3fc203adf1c7234c510172a6dc3c1
Anton Kozlov <drakon.mega@gmail.com>
2011-09-15 15:45:50 +0000

    forgoten file added

---
+++ platform/olimex/include/service/robobot_car.h
@@ -0,0 +1,17 @@
+/**
+ * @file
+ *
+ * @date 18.07.11
+ * @author Anton Kozlov
+ */
+
+#ifndef SERVICE_ROBOBOT_CAR_H_
+#define SERVICE_ROBOBOT_CAR_H_
+
+#define ROBOBOT_ID_SIZE		    4
+
+#define ROBOBOT_MSG_SIZE	    4
+#define ROBOBOT_HEADER_SIZE	    2
+
+#endif /* SERVICE_ROBOBOT_CAR_H_ */
+
