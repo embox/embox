c25d48f2f2a56ba6e4e22aabefa8365d524f0e72
Anton Kozlov <drakon.mega@gmail.com>
2011-09-15 15:36:03 +0000

    printk must use diag_putc?

--- src/lib/stdio/printk.c
+++ src/lib/stdio/printk.c
@@ -9,16 +9,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <types.h>
+#include <kernel/diag.h>
 
 int __print(void (*printchar_handler)(char **str, int c),
 		char **out, const char *format, va_list args);
 
-static void printchar(char **str, int c) {
+static void diag_printchar(char **str, int c) {
 	if (str) {
 		**str = c;
 		++(*str);
 	} else {
-		putchar(c);
+		diag_putc(c);
 	}
 }
 
@@ -27,7 +28,7 @@ int printk(const char *format, ...) {
 	va_list args;
 
 	va_start(args, format);
-	ret = __print(printchar, 0, format, args);
+	ret = __print(diag_printchar, 0, format, args);
 	va_end(args);
 
 	return ret;
