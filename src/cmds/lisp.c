c47dbff7b85a1bc501795814c7f7a17efbe499c7
Anton Kozlov <drakon.mega@gmail.com>
2011-09-16 14:37:54 +0000

    lisp init file auto load

--- src/cmds/lisp.c
+++ src/cmds/lisp.c
@@ -15,5 +15,10 @@ EMBOX_CMD(exec);
 extern int lisp5000_main(int argc, char *argv[]);
 
 static int exec(int argc, char **argv) {
-	return lisp5000_main(argc, argv);
+	char *av[argc + 2];
+	for (int i = argc; i > 0; i--) {
+		av[i + 1] = argv[i];
+	}
+	av[1] = "init500lite.lisp";
+	return lisp5000_main(argc + 1, av);
 }
