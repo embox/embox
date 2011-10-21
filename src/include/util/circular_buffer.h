86801d92187a8bb795dab8fe410c8dd2d6bc9186
Anton Kozlov <drakon.mega@gmail.com>
2011-10-21 10:52:41 +0000

    circular buffer

---
+++ src/include/util/circular_buffer.h
@@ -0,0 +1,46 @@
+/**
+ * @file
+ * @brief Circular buffer interface
+ *
+ * @date 21.10.2011
+ * @author Anton Kozlov
+ */
+
+#ifndef UTIL_CIRCULAR_BUFFER_H_
+#define UTIL_CIRCULAR_BUFFER_H_
+
+#include <stddef.h>
+
+	struct c_buf {
+	int len;
+	int size;
+	int beg;
+	int end;
+	void *buf;
+};
+
+extern int __c_buf_add(struct c_buf *buf, void *elem, size_t elem_size);
+
+extern int __c_buf_get(struct c_buf *buf, void *elem, size_t elem_size);
+
+#define CIRCULAR_BUFFER_DEF(name, elem_type, len) \
+	__CIRCULAR_BUFFER_DEF(name, name##_storage, elem_type, len)
+
+#define __CIRCULAR_BUFFER_DEF(name, storage_nm, elem_type, req_len) \
+	static elem_type storage_nm[req_len];\
+	static struct c_buf name = {\
+		.len = req_len,\
+		.size = 0,\
+		.beg = 0,\
+		.end = 0,\
+		.buf = (void *) storage_nm\
+	}
+
+#define c_buf_add(name, elem) \
+	__c_buf_add(&name, (void *) &elem, sizeof(elem))
+
+#define c_buf_get(name, elem) \
+	__c_buf_get(&name, (void *) &elem, sizeof(elem))
+
+#endif
+
---
+++ src/util/circular_buffer.c
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * @brief Circular buffer realisation
+ *
+ * @date 21.10.2011
+ * @author Anton Kozlov
+ */
+
+#include <stddef.h>
+#include <string.h>
+#include <util/circular_buffer.h>
+
+int __c_buf_add(struct c_buf *buf, void *elem, size_t elem_size) {
+	char *buf_pool = buf->buf;
+	if (buf->size >= buf->len) {
+		return -1;
+	}
+	buf->size++;
+	memcpy(buf_pool + (buf->beg * elem_size), (const void *) elem,
+			elem_size);
+	buf->beg = (buf->beg + 1) % buf->len;
+	return 0;
+}
+
+int __c_buf_get(struct c_buf *buf, void *elem, size_t elem_size) {
+	char *buf_pool = buf->buf;
+	void *ret = buf_pool + (buf->end * elem_size);
+	if (buf->size == 0) {
+		return -1;
+	}
+	buf->size--;
+	buf->end = (buf->end + 1) % buf->len;
+	memcpy(elem, ret, elem_size);
+	return 0;
+}
+
