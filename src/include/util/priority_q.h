177c5826b4db2cf4247f4533db0ff46e98ab0399
Nikolay Malkovsky <malkovskynv@gmail.com>
2011-10-25 19:07:13 +0000

    priority_q.h added (definitions for priority queue data structure)

---
+++ src/include/util/priority_q.h
@@ -0,0 +1,47 @@
+/**
+ * @brief Defenitions for priority queue data structure
+ *
+ * @date October 25, 2011
+ * @author Malkovsky Nikolay
+ */
+
+#ifndef PRIORITY_Q_
+#define PRIORITY_Q_
+
+/**
+ * @brief Structure representing the queue itself
+ */
+struct priority_q {
+	 /* TODO replace static array for a vector or something */
+	int a[32];
+	int size;
+};
+
+/**
+ * @brief Inserts specific value in queue priority_q
+ *
+ * @param priority_q queue to insert element into
+ * @param value value to insert into queue
+ */
+inline void insert(struct priority_q *pq, int value);
+
+/**
+ * @brief pops the lowest element from the queue
+ */
+inline void pop(struct priority_q *pq);
+
+/**
+ * @brief Returns the lowest element in the queue
+ *
+ * @param pq *priority_q to find the lowest element in
+ * @return -1 when queue is empty, value of the lowest element in queue
+ */
+/* TODO better implementatoin in case when queue is empty */
+#define front(pq) (pq->size == 0 ? -1 : pq->a[0])
+
+/**
+ * @brief Returns whether queue is empty or not
+ */
+#define empty(pq) (pq->size == 0)
+
+#endif
