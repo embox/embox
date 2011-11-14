dd0e9c0ef8310c5bb3447da72a84f488d5aa87ab
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-11-14 10:32:14 +0000

    Move ascii_table to common.mk

--- mk/core/common.mk
+++ mk/core/common.mk
@@ -52,6 +52,22 @@ $$ := $$
 # inside.
 $(\s) :=
 
+ascii_table = \
+       SOH STX ETX EOT ENQ ACK BEL  BS TAB  LF  VT  FF  CR  SO  SI   \
+   DLE DC1 DC2 DC3 DC4 NAK SYN ETB CAN  EM SUB ESC  FS  GS  RS  US   \
+    SP   !   "  \#   $   %   &   '   (   )   *   +   ,   —   .   /   \
+     0   1   2   3   4   5   6   7   8   9   :   ;   <   =   >   ?   \
+     @   A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   \
+     P   Q   R   S   T   U   V   W   X   Y   Z   [   \   ]   ^   _   \
+     `   a   b   c   d   e   f   g   h   i   j   k   l   m   n   o   \
+     p   q   r   s   t   u   v   w   x   y   z   {   |   }   ~ DEL
+ascii_table := $(strip $(value ascii_table))
+
+# Params:
+#   1. Char code.
+ascii_char = \
+	$(word $1,$(ascii_table))
+
 ## Identity function.
 id = $1
 
