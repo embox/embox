a8d48f64962eba1bf1070ef85139de6dacfa5d35
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-11-14 09:55:15 +0000

    Improve var introspection utils

--- mk/util/var/info.mk
+++ mk/util/var/info.mk
@@ -6,8 +6,6 @@
 ifndef __util_var_info_mk
 __util_var_info_mk := 1
 
-include core/common.mk
-
 ##
 # Function: var_info_string
 #
@@ -15,10 +13,9 @@ include core/common.mk
 # Information includes flavor, origin and value of the specified variable.
 #
 # Params:
-#  1. The target variable name
-#
-# Returns: Human-readable multiline string containing variable information.
-#
+#   1. The target variable name.
+# Return:
+#   Human-readable multiline string containing variable information.
 define var_info_string
 Variable [$1] info:
    flavor: [$(flavor $1)]
@@ -33,14 +30,12 @@ endef
 # Prints the information about the specified variable using $(info) function.
 #
 # Params:
-#  1. The target variable name
-#
-# Returns: nothing
-#
+#   1. The target variable name.
+# Return:
+#   Nothing.
 # See also: var_info_string
-#
 var_info = \
-  $(info $(call var_info_string,$1))
+  $(info $(var_info_string))
 
 ##
 # Function: var_simple
@@ -48,13 +43,12 @@ var_info = \
 # Checks whether the specified variable is defined as statically expanded.
 #
 # Params:
-#  1. The target variable name
-#
-# Returns: true if the specified variable exists and its flavor is 'simple',
-#          false otherwise
-#
+#   1. The target variable name.
+# Return:
+#   The argument if the specified variable exists and its flavor is 'simple',
+#   empty otherwise.
 var_simple = \
-	$(findstring simple,$(flavor $1))
+	$(if $(findstring simple,$(flavor $1)),$1)
 
 ##
 # Function: var_recursive
@@ -62,13 +56,12 @@ var_simple = \
 # Checks whether the specified variable is defined as recursively expanded.
 #
 # Params:
-#  1. The target variable name
-#
-# Returns: true if the specified variable exists and its flavor is 'recursive',
-#          false otherwise
-#
+#   1. The target variable name.
+# Return:
+#   The argument if the specified variable exists and its flavor is 'recursive',
+#   empty otherwise.
 var_recursive = \
-	$(findstring recursive,$(flavor $1))
+	$(if $(findstring recursive,$(flavor $1)),$1)
 
 ##
 # Function: var_defined
@@ -76,12 +69,12 @@ var_recursive = \
 # Checks whether the specified variable is defined.
 #
 # Params:
-#  1. The target variable name
-#
-# Returns: true if the specified variable exists, false otherwise
-#
+#   1. The target variable name.
+# Return:
+#   The argument if the specified variable exists, regardless its flavor,
+#   empty otherwise.
 var_defined = \
-	$(filter simple recursive,$(flavor $1))
+	$(if $(findstring undefined,$(flavor $1)),,$1)
 
 ##
 # Function: var_undefined
@@ -89,11 +82,11 @@ var_defined = \
 # Checks whether the specified variable is not defined.
 #
 # Params:
-#  1. The target variable name
-#
-# Returns: true if the variable has not been defined yet, false otherwise
-#
+#   1. The target variable name.
+# Return:
+#   The argument if the variable with such name has not been defined yet,
+#   empty otherwise.
 var_undefined = \
-	$(findstring undefined,$(flavor $1))
+	$(if $(findstring undefined,$(flavor $1)),$1)
 
 endif # __util_var_info_mk
