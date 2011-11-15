37edc47f7ec3ea3e161d3773aab0d55f3e9c87ce
Alex Kalmuk <alexkalmuk@gmail.com>
2011-11-15 08:11:03 +0000

    build graph from file
    You can specify: graph declaration, types of nodes, links, rules.

--- src/pnet/netfilter/rules_init.c
+++ src/pnet/netfilter/rules_init.c
@@ -18,12 +18,23 @@
 #include <util/member.h>
 #include <stdio.h>
 #include <pnet/graph.h>
+#include <util/macro.h>
 
 #define RULE_ELEMS_COUNT 6
 
 EMBOX_UNIT_INIT(init);
 
-static net_node_t match_node;
+#define NODES_COUNT 10
+
+//TODO naming nodes as in file
+struct __node {
+	char *name;
+	net_node_t node;
+};
+
+static struct __node nodes[NODES_COUNT];
+
+static net_node_t get_node_by_name(char *name);
 
 static const char *rules[] = {
 	#include <pnet/pnet_rules.inc>
@@ -61,26 +72,70 @@ static int form_rule(match_rule_t rule, const char *rule_elem, int num) {
 		pnet_rule_set_src_udp_port(rule, *(uint16_t*) rule_elem);
 		break;
 	case 6:
-		rule->next_node = pnet_get_module(rule_elem);
+		rule->next_node = get_node_by_name((char*)rule_elem);
 		break;
 	}
 
 	return 0;
 }
 
+static char* parse_type(const char *rule) {
+	char *cur;
+
+	for(cur = (char*)rule; *cur != ':'; cur++)
+		;
+
+	*cur = '\0';
+	cur++;
+
+	return cur;
+}
+
+static net_node_t get_node_by_name(char *name) {
+	for (int i = 0; i < NODES_COUNT; i++) {
+		if (strcmp(nodes[i].name, name) == 0) {
+			return nodes[i].node;
+		}
+	}
+
+	return NULL;
+}
+
 static int init(void) {
 	const char *rule_elem;
+	char *tmp;
 	int cur = 0;
+	int node_cur = 0;
+	int i = 0;
 	match_rule_t new_rule;
-	net_node_matcher_t match;
+	net_node_t src_node;
+	net_node_t dst_node;
 	net_node_t match_node;
+	net_node_matcher_t match;
+	struct pnet_graph *graph;
 
-	match_example();
 	new_rule = pnet_rule_alloc();
 
-	array_foreach(rule_elem, rules, ARRAY_SIZE(rules)) {
-		if (strncmp("RULES", rule_elem, 5) == 0) {
+	for(i = 0; i < ARRAY_SIZE(rules); i++) {
+		rule_elem = rules[i];
+
+		if(strncmp("TYPE", rule_elem, 4) == 0) {
+			tmp = (char*)rule_elem;
+			rule_elem = parse_type(rule_elem);
+			nodes[node_cur].node = pnet_get_module(tmp + 5);
+			nodes[node_cur].name = (char*)rule_elem;
+			pnet_graph_add_node(graph, nodes[node_cur++].node);
+		} else if(strncmp("GRAPH", rule_elem, 5) == 0) {
+			graph = pnet_graph_create();
+		} else if (strncmp("RULES", rule_elem, 5) == 0) {
+			match_node =  get_node_by_name((char*)rule_elem + 6);
 			match = (net_node_matcher_t) match_node;
+			cur = 0;
+		} else if(strncmp("LINK", rule_elem, 4) == 0) {
+			src_node = get_node_by_name((char*)rule_elem + 5);
+			rule_elem = rules[++i];
+			dst_node = get_node_by_name((char*)rule_elem);
+			pnet_node_link(src_node, dst_node);
 		} else {
 			cur++;
 			form_rule(new_rule, rule_elem, cur);
@@ -91,6 +146,8 @@ static int init(void) {
 			}
 		}
 	}
+
+	pnet_graph_start(graph);
 
 	return 0;
 }
