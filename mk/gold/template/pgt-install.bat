9b96e12526809e5980601fa6f37f1b92bc80a285
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-11-21 13:58:53 +0000

    pgt-install.bat

---
+++ mk/gold/template/pgt-install.bat
@@ -0,0 +1,22 @@
+@echo off
+rem Copies necessary program template files to the specified directory.
+
+if "%~1"=="" (
+echo %~nx0: Missing directory operand
+echo Usage: %~nx0 DIRECTORY
+echo     where DIRECTORY is Templates directory of your GOLD installation.
+echo Example: %~nx0 "C:\Program Files\GOLD Parser Builder 5\Templates\"
+exit /B
+)
+
+if not exist "%~f1\" (
+echo Directory "%~f1" does not exist.
+exit /B
+)
+
+@echo on
+
+copy /Y %~dp0\main.pgt.mk    "%~f1\GNU Make - MyBuild - Main.pgt"
+copy /Y %~dp0\tables.pgt.mk  "%~f1\GNU Make - MyBuild - Tables.pgt"
+copy /Y %~dp0\symbols.pgt.mk "%~f1\GNU Make - MyBuild - Symbols.pgt"
+copy /Y %~dp0\rules.pgt.mk   "%~f1\GNU Make - MyBuild - Rules.pgt"
