6ae8278d2c4838c42b99434c189dc2036376a820
Anton Kozlov <drakon.mega@gmail.com>
2011-11-26 12:08:05 +0000

    kernel watchdog option

--- src/include/framework/net/sock/self.h
+++ src/include/framework/net/sock/self.h
@@ -12,14 +12,14 @@
 #include __impl_x(framework/net/sock/self_impl.h)
 
 #define EMBOX_NET_SOCK(_type, _protocol, _prot, _ops, _no_check, 	\
-						net_proto_family)   						\
-	static inet_protosw_t _##_type = { 								\
-			.type = _type,											\
-			.protocol = _protocol,									\
-			.prot = &_prot,                							\
-			.ops = &_ops, 											\
-			.no_check = _no_check									\
-		};															\
+						net_proto_family)   	\
+	static inet_protosw_t _##_type = { 				\
+			.type = _type,					\
+			.protocol = _protocol,				\
+			.prot = &_prot,                			\
+			.ops = &_ops, 					\
+			.no_check = _no_check				\
+		};							\
 		__EMBOX_NET_SOCK(_##_type, net_proto_family)
 
 #endif /* FRAMEWORK_NET_SOCK_SELF_H_ */
