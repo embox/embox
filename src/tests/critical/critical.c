e0cae5760f4b59732cf47af4a1f37e4cd478ca64
Nikolay Korotkiy <sikmir@gmail.com>
2011-11-30 10:03:01 +0000

    Fix critical test

--- src/tests/critical/critical.c
+++ src/tests/critical/critical.c
@@ -1,6 +1,9 @@
 /**
  * @file
  * @brief Kernel critical API test.
+ *
+ * @date 07.08.10
+ * @author Eldar Abusalimov
  */
 
 #include <embox/test.h>
@@ -19,14 +22,14 @@ TEST_TEARDOWN(case_teardown);
 TEST_CASE("critical_inside should return false "
 		"when called outside any critical section") {
 	test_assert_false(critical_inside(CRITICAL_IRQ_LOCK));
-	test_assert_false(critical_inside(CRITICAL_SOFTIRQ));
+	test_assert_false(critical_inside(CRITICAL_SOFTIRQ_LOCK));
 	test_assert_false(critical_inside(CRITICAL_SCHED_LOCK));
 }
 
 TEST_CASE("critical_allows should return true "
 		"when called outside any critical section") {
 	test_assert_true(critical_allows(CRITICAL_IRQ_LOCK));
-	test_assert_true(critical_allows(CRITICAL_SOFTIRQ));
+	test_assert_true(critical_allows(CRITICAL_SOFTIRQ_LOCK));
 	test_assert_true(critical_allows(CRITICAL_SCHED_LOCK));
 }
 
@@ -35,32 +38,32 @@ static int run(void) {
 	critical_enter(CRITICAL_SCHED_LOCK);
 
 	test_assert_true(critical_inside(CRITICAL_SCHED_LOCK));
-	test_assert_false(critical_inside(CRITICAL_SOFTIRQ));
+	test_assert_false(critical_inside(CRITICAL_SOFTIRQ_LOCK));
 	test_assert_false(critical_inside(CRITICAL_IRQ_LOCK));
 
 	test_assert_false(critical_allows(CRITICAL_SCHED_LOCK));
-	test_assert_true(critical_allows(CRITICAL_SOFTIRQ));
+	test_assert_true(critical_allows(CRITICAL_SOFTIRQ_LOCK));
 
 	critical_enter(CRITICAL_SOFTIRQ);
 
 	test_assert_true(critical_inside(CRITICAL_SCHED_LOCK));
-	test_assert_true(critical_inside(CRITICAL_SOFTIRQ));
+	test_assert_true(critical_inside(CRITICAL_SOFTIRQ_LOCK));
 	test_assert_false(critical_inside(CRITICAL_IRQ_LOCK));
 
 	test_assert_false(critical_allows(CRITICAL_SCHED_LOCK));
-	test_assert_false(critical_allows(CRITICAL_SOFTIRQ));
+	test_assert_false(critical_allows(CRITICAL_SOFTIRQ_LOCK));
 
 	critical_enter(CRITICAL_IRQ_LOCK);
 
 	test_assert_true(critical_inside(CRITICAL_SCHED_LOCK));
-	test_assert_true(critical_inside(CRITICAL_SOFTIRQ));
+	test_assert_true(critical_inside(CRITICAL_SOFTIRQ_LOCK));
 	test_assert_true(critical_inside(CRITICAL_IRQ_LOCK));
 
 	test_assert_false(critical_allows(CRITICAL_SCHED_LOCK));
-	test_assert_false(critical_allows(CRITICAL_SOFTIRQ));
+	test_assert_false(critical_allows(CRITICAL_SOFTIRQ_LOCK));
 
 	critical_leave(CRITICAL_IRQ_LOCK);
-	critical_leave(CRITICAL_SOFTIRQ);
+	critical_leave(CRITICAL_SOFTIRQ_LOCK);
 	critical_leave(CRITICAL_SCHED_LOCK);
 
 	return 0;
@@ -73,7 +76,7 @@ static int suite_setup(void) {
 	/* Test must not be invoked within any critical section. */
 	int inside_any = 0;
 	inside_any |= critical_inside(CRITICAL_IRQ_LOCK);
-	inside_any |= critical_inside(CRITICAL_SOFTIRQ);
+	inside_any |= critical_inside(CRITICAL_SOFTIRQ_LOCK);
 	inside_any |= critical_inside(CRITICAL_SCHED_LOCK);
 	return inside_any ? -EPERM : 0;
 }
@@ -85,9 +88,9 @@ static int case_setup(void) {
 
 static int case_teardown(void) {
 	critical_t critical, levels[] = {
-			CRITICAL_IRQ_LOCK,
-			CRITICAL_SOFTIRQ,
-			CRITICAL_SCHED_LOCK,
+		CRITICAL_IRQ_LOCK,
+		CRITICAL_SOFTIRQ_LOCK,
+		CRITICAL_SCHED_LOCK,
 	};
 
 	array_foreach(critical, levels, ARRAY_SIZE(levels)) {
