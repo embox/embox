b3a3e2b35c183921e69937f1a9dd3d06d5bfbbf8
Alex Kalmuk <alexkalmuk@gmail.com>
2011-11-30 14:03:01 +0000

    IP fragmentation on receiving. #345

--- src/include/net/ip_fragment.h
+++ src/include/net/ip_fragment.h
@@ -7,36 +7,15 @@
 
 #include <types.h>
 #include <util/list.h>
-#include <util/slist.h>
 #include <net/in.h>
 #include <net/ip.h>
+#include <net/skbuff.h>
+#include <util/macro.h>
 
 #define MAX_BUFS_CNT 0x10
-#define MAX_HOLES_CNT MAX_BUFS_CNT * 0x10
-
-struct hole {
-	struct list_link next_hole;
-	int begin;
-	int end;
-};
-
 /**
- * Datagram receive buffer descriptor
+ *	return skbuff contain complete data
  */
-typedef struct dgram_buf {
-	struct slist_link next_buf;		/* linked list pointers	*/
-	in_addr_t		  saddr;
-	in_addr_t		  daddr;
-	uint16_t		  id;
-	uint8_t		      protocol;
-	uint8_t		      complete;
-	struct list_link  holes;
-	int				  len; /* total length of original datagram	*/
-}dgram_buf_t;
-
-#define dgram_buf_foreach_ptr(dgram_buf) \
-	slist_foreach(dgram_buf, __extension__ ({   \
-	extern struct slist __dgram_buf_list; &__dgram_buf_list; \
-	}), next_buf)
+extern struct sk_buff *ip_defrag(struct sk_buff *skb);
 
 #endif /* NET_IP_FRAGMENT_H */
--- src/net/ipv4/ip_fragment.c
+++ src/net/ipv4/ip_fragment.c
@@ -1,29 +1,56 @@
 /**
+ * @brief IP Fragmentation
  * @date 25.11.2011
+ * @author Alexander Kalmuk
  */
 
 #include <net/ip_fragment.h>
 #include <net/skbuff.h>
 #include <mem/objalloc.h>
+#include <string.h>
+#include <kernel/timer.h>
+#include <net/udp.h>
 
-OBJALLOC_DEF(__dgram_bufs, struct dgram_buf, MAX_BUFS_CNT);
-//OBJALLOC_DEF(__holes, struct hole, MAX_HOLES_CNT);
-
-struct slist __dgram_buf_list = SLIST_INIT(&__dgram_buf_list);
-
-struct frag {
-	int		begin;
-	int		end;
+/**
+ * Datagram receive buffer
+ */
+struct dgram_buf {
+	struct sk_buff *next_skbuff;
+	struct sk_buff *prev_skbuff;
+	struct list_head next_buf; /* linked list pointers	*/
+	in_addr_t         saddr;
+	in_addr_t         daddr;
+	uint16_t          id;
+	uint8_t           protocol;
+	int               meat;
+	int               len; /* total length of original datagram	*/
+	struct sys_timer  *timer;
 };
 
-static inline dgram_buf_t* ip_find(struct iphdr *iph) {
+static LIST_HEAD(__dgram_buf_list);
+OBJALLOC_DEF(__dgram_bufs, struct dgram_buf, MAX_BUFS_CNT);
+
+#define dgram_buf_foreach(dgram_buf) \
+	list_for_each_entry(dgram_buf, &__dgram_buf_list, next_buf)
+
+#define skbuff_for_each(skb, buf) \
+	for(skb = buf->next_skbuff;   \
+	   ((struct list_head*)skb)!=(struct list_head*)buf; \
+	   skb = skb->next)
+
+static struct dgram_buf *buf_create(struct iphdr *iph);
+static void buf_delete(struct dgram_buf *buf);
+static void ip_frag_dgram_buf(struct dgram_buf *buf, struct sk_buff *skb);
+static struct sk_buff *build_packet(struct dgram_buf *buf);
+
+static inline struct dgram_buf *ip_find(struct iphdr *iph) {
 	struct dgram_buf *buf;
 
-	dgram_buf_foreach_ptr(buf) {
-		if(buf->daddr == iph->saddr &&
-			buf->saddr == iph->daddr &&
-			buf->protocol == iph->proto &&
-			buf->id == iph->id) {
+	dgram_buf_foreach(buf) {
+		if (buf->daddr == iph->saddr
+			&& buf->saddr == iph->daddr
+			&& buf->protocol == iph->proto
+			&& buf->id == iph->id) {
 			return buf;
 		}
 	}
@@ -31,41 +58,113 @@ static inline dgram_buf_t* ip_find(struct iphdr *iph) {
 	return NULL;
 }
 
-static void put_frag_in_hole (struct hole *hole, struct frag *fragment) {
+static void ip_frag_dgram_buf(struct dgram_buf *buf, struct sk_buff *skb) {
+	struct sk_buff *tmp;
+	int was_added = 0;
+	int offset, tmp_offset;
+	int data_len, end;
 
-}
+	offset = ntohs(skb->nh.iph->frag_off);
+	offset &= IP_OFFSET;
+	offset <<= 3;
+	data_len = skb->len - (skb->h.raw - skb->data) - UDP_HEADER_SIZE;
+	end = offset + data_len;
 
-static void ip_frag_dgram_buf (struct dgram_buf *buf, struct sk_buff *skb) {
-	struct frag fragment;
-	struct iphdr *iph;
-	struct hole *hole;
-	struct list_link *hole_link;
-
-	iph = skb->nh.iph;
-	fragment.begin = iph->frag_off * 8;
-	fragment.end = fragment.begin + (iph->ttl - (iph->ihl) * 4);
-
-	list_for_each(hole_link, &buf->holes){
-		hole = list_entry (hole_link, struct hole, next_hole);
-		if ((hole->begin <= fragment.begin) && (hole->end >= fragment.end)) {
-			put_frag_in_hole(hole, &fragment);
+	skbuff_for_each(tmp, buf) {
+		tmp_offset = ntohs(tmp->nh.iph->frag_off);
+		tmp_offset &= IP_OFFSET;
+		tmp_offset <<= 3;
+		if (offset < tmp_offset) {
+			list_add((struct list_head *) skb, (struct list_head *) tmp->prev);
+			was_added = 1;
+			break;
 		}
 	}
 
-	put_frag_in_hole(hole, &fragment);
+	if (!was_added) {
+		list_add_tail((struct list_head *) skb, (struct list_head *) buf);
+	}
+
+	buf->meat += data_len;
+	if (end > buf->len) {
+		buf->len = end;
+	}
 }
 
-struct sk_buff *ip_defrag(struct sk_buff *skb) {
-	struct dgram_buf *buf;
+//TODO there are two all data coping now. May be it is possible to do only one.
+static struct sk_buff *build_packet(struct dgram_buf *buf) {
+	struct sk_buff *skb;
+	struct sk_buff *tmp;
+	int offset = 0;
+	int ihlen;
 
-	if ((buf = ip_find(skb->nh.iph)) != NULL) {
-		ip_frag_dgram_buf(buf, skb);
-	} else {
-		buf = (dgram_buf_t *) objalloc(&__dgram_bufs);
-		list_link_init(&buf->holes);
-		slist_add_first(buf, &__dgram_buf_list, next_buf);
+	tmp = buf->next_skbuff;
+
+	ihlen = (tmp->h.raw - tmp->data) + UDP_HEADER_SIZE;
+	skb = alloc_skb(buf->len + ihlen, 0);
+	memcpy(skb->data, tmp->data, ihlen);
+
+	skb->h.raw = skb->data + (tmp->h.raw - tmp->data);
+	skb->nh.raw = skb->data + (tmp->nh.raw - tmp->data);
+	skb->mac.raw = skb->data + (tmp->mac.raw - tmp->data);
+
+	skb->protocol = tmp->protocol;
+	skb->dev = tmp->dev;
+	skb->len = buf->len + ihlen;
+
+	while(!list_empty((struct list_head *)buf)) {
+		memcpy(skb->data + ihlen + offset, tmp->data + ihlen, tmp->len - ihlen);
+		offset += tmp->len - ihlen;
+		list_del((struct list_head *)tmp);
+		kfree_skb(tmp);
+		tmp = buf->next_skbuff;
 	}
+
+	buf_delete(buf);
 
 	return skb;
 }
 
+static struct dgram_buf *buf_create(struct iphdr *iph) {
+	struct dgram_buf *buf;
+	//sys_timer_t *timer;
+
+	buf = (struct dgram_buf*) objalloc(&__dgram_bufs);
+	if (!buf)
+		return NULL;
+
+	INIT_LIST_HEAD((struct list_head *)buf);
+	INIT_LIST_HEAD(&buf->next_buf);
+	list_add_tail(&buf->next_buf, &__dgram_buf_list);
+	buf->protocol = iph->proto;
+	buf->id = iph->id;
+	buf->saddr = iph->saddr;
+	buf->daddr = iph->daddr;
+	buf->len = 0;
+	buf->meat = 0;
+
+	return buf;
+}
+
+static void buf_delete(struct dgram_buf *buf) {
+	list_del((struct list_head*)buf);
+	//pool_free(&__dgram_bufs, (void*)buf);
+}
+
+struct sk_buff *ip_defrag(struct sk_buff *skb) {
+	struct dgram_buf *buf;
+	int mf_flag;
+
+	if ((buf = ip_find(skb->nh.iph)) == NULL) {
+		buf = buf_create(skb->nh.iph);
+	}
+
+	ip_frag_dgram_buf(buf, skb);
+
+	mf_flag = skb->nh.iph->frag_off & IP_MF;
+	if (!mf_flag && buf->meat == buf->len) {
+		return build_packet(buf);
+	}
+
+	return skb;
+}
