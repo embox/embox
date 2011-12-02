133b70b5d689ea1bde5520f42fe9e6c42a719e65
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-12-02 12:35:06 +0000

    Yet another attempt to rewrite x86 IPL

--- src/arch/x86/include/asm/hal/ipl.h
+++ src/arch/x86/include/asm/hal/ipl.h
@@ -10,34 +10,43 @@
 #endif /* HAL_IPL_H_ */
 
 #include <types.h>
+#include <asm/flags.h>
 
 #include <hal/interrupt.h>
 
 typedef uint32_t __ipl_t;
 
+// XXX Now it acts as a flag indicating whether module is initialized. -- Eldar
 extern __ipl_t __cur_ipl;
 
 extern void apic_init(void);
 
 static inline void ipl_init(void) {
-	__cur_ipl = 0;
-	apic_init();
-	__asm__ __volatile__ ("sti;\n\t");
+	if (!__cur_ipl) {
+		apic_init();
+	}
+	__cur_ipl = 1;
+
+	__asm__ __volatile__ ("sti;\n\t" : : : "memory");
 }
 
 static inline unsigned int ipl_save(void) {
 	unsigned int ret;
-	__asm__ __volatile__ ("cli;\n\t");
-	ret = __cur_ipl;
-	__cur_ipl = 15;
+
+	__asm__ __volatile__ (
+		"pushf;\n\t"
+		"pop %0;\n\t"
+		"cli;\n\t"
+		: "=&r" (ret)
+		:
+		: "memory"
+	);
 
 	return ret;
 }
 
 static inline void ipl_restore(unsigned int ipl) {
-	__asm__ __volatile__("cli;\n\t");
-	__cur_ipl = ipl;
-	if (0 == ipl) {
-		__asm__ __volatile__("sti;\n\t");
+	if (ipl & X86_EFLAGS_IF) {
+		__asm__ __volatile__ ("sti;\n\t" : : : "memory");
 	}
 }
