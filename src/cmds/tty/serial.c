39f9abd0462cb0d80c0c3121489da1060dbf4f14
Anton Bondarev <anton.bondarev2310@gmail.com>
2011-12-04 13:48:22 +0000

    Fix lisp-demo template. It compiled now

--- src/cmds/tty/serial.c
+++ src/cmds/tty/serial.c
@@ -6,7 +6,7 @@
  */
 
 #include <string.h>
-#include <drivers/serial.h>
+#include <kernel/diag.h>
 #include <drivers/tty_ng.h>
 #include <embox/cmd.h>
 #include <cmd/shell.h>
@@ -30,17 +30,17 @@ static void serial_make_active(struct tty_buf *tty) {
 
 	const char *ch_buf = clrscr;
 	while (*ch_buf != '\0') {
-		uart_putc(*ch_buf++);
+		diag_putc(*ch_buf++);
 	}
 
 	if (cnt == BUF_SIZE) {
 		while (cnt --) {
-			uart_putc(tty->out_buf[ps]);
+			diag_putc(tty->out_buf[ps]);
 			ps = (ps + 1) % BUF_SIZE;
 		}
 	} else {
 		for (int i = 0; i < ps; i++) {
-			uart_putc(tty->out_buf[i]);
+			diag_putc(tty->out_buf[i]);
 		}
 	}
 	act_id = tty->id;
@@ -52,7 +52,7 @@ static void serial_make_inactive(struct tty_buf *tty) {
 
 static void serial_pc(struct tty_buf *tty, char ch) {
 	if (act_id == tty->id) {
-		uart_putc(ch);
+		diag_putc(ch);
 	}
 
 	tty->out_buf[pos[tty->id]] = ch;
