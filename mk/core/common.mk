f947ae46ca61d19b45dd497e0b733b0edc587985
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-12-05 12:00:15 +0000

    Fix ascii char corresponding to hyphen ('-')

--- mk/core/common.mk
+++ mk/core/common.mk
@@ -55,7 +55,7 @@ $(\s) :=
 ascii_table = \
        SOH STX ETX EOT ENQ ACK BEL  BS TAB  LF  VT  FF  CR  SO  SI   \
    DLE DC1 DC2 DC3 DC4 NAK SYN ETB CAN  EM SUB ESC  FS  GS  RS  US   \
-    SP   !   "  \#   $   %   &   '   (   )   *   +   ,   —   .   /   \
+    SP   !   "  \#   $   %   &   '   (   )   *   +   ,   -   .   /   \
      0   1   2   3   4   5   6   7   8   9   :   ;   <   =   >   ?   \
      @   A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   \
      P   Q   R   S   T   U   V   W   X   Y   Z   [   \   ]   ^   _   \
