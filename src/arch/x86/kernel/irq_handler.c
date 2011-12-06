cfe0fa1c71bd358de8db67f79859fd81cb81b9b3
Oleg Medvedev <oleg.medvedev@lanit-tercom.com>
2011-12-06 10:53:06 +0000

    Commit for #361

--- src/arch/x86/kernel/irq_handler.c
+++ src/arch/x86/kernel/irq_handler.c
@@ -15,14 +15,12 @@
 #include <drivers/i8259.h>
 #include <assert.h>
 
-static int irqHandlerIns = 0;
-static int irqHandlerOuts = 0;
-
 void irq_handler(pt_regs_t *regs) {
 	int irqn = regs->trapno - 0x20;
 	/* Send an EOI (end of interrupt) signal to the PICs.
 	   If this interrupt involved the slave. */
 	interrupt_disable(irqn);
+	ipl_enable();
 	assert(!critical_inside(CRITICAL_IRQ_LOCK));
 	critical_enter(CRITICAL_IRQ_HANDLER);
 	if (irqn > 7) {
@@ -32,11 +30,7 @@ void irq_handler(pt_regs_t *regs) {
 	/* Send reset signal to master. (As well as slave, if necessary). */
 	out8(NON_SPEC_EOI, PIC1_COMMAND);
 
-++irqHandlerIns;
-
 	irq_dispatch(irqn);
-
-++irqHandlerOuts;
 
 	interrupt_enable(irqn);
 	critical_leave(CRITICAL_IRQ_HANDLER);
