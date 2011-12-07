c3badc57b0f0851ded9752b0ec64bac121825a31
Anton Bondarev <anton.bondarev2310@gmail.com>
2011-12-07 10:28:35 +0000

    Add some Mybuild files and stabs for task's tests

---
+++ src/tests/kernel/task/task_api_test.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ *
+ * @brief
+ *
+ * @date 07.12.2011
+ * @author Anton Bondarev
+ */
+
+#include <embox/test.h>
+#include <kernel/task.h>
+
+EMBOX_TEST_SUITE("test for task API");
+
+TEST_CASE("create and destroy task") {
+}
