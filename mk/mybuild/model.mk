815dc8967e15414e7848ec309ab6f51d92adc1c9
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-12-08 15:25:00 +0000

    #364: containment class

--- mk/mybuild/model.mk
+++ mk/mybuild/model.mk
@@ -11,10 +11,33 @@ __mybuild_model_mk := 1
 include mk/core/define.mk
 include mk/core/object.mk
 
+define class-containment
+	$(field container_property,$1)
+	$(setter container_property,$(error $0 is read-only))
+
+	$(field contents_property,$2)
+	$(setter contents_property,$(error $0 is read-only))
+
+	$(field container)
+	$(setter container,
+		$(or $(eq $1,$(get container)),
+			$(foreach c,$(get container),
+					$(set- $c.$(get contents_property),$(this)))
+			$(if $1,$(set+ $1.$(get contents_property),$(this)))
+			$1
+		)
+	)
+
+	$(field contents)
+	$(setter contents,
+		$(foreach 1,$1,$(error NIY))
+	)
+endef
+
 # Constructor args:
 #   1. Resource.
 define class-my_object
-	$(field resource)
+	$(field resource,$(new containment))
 	$(setter resource,
 		$(or $(eq $1,$(get resource)),
 			$(foreach r,$(get resource),$(set- $r.objects,$(this)))
@@ -25,6 +48,13 @@ define class-my_object
 	$(set resource,$1)
 
 	$(field container)
+	$(setter container,
+		$(or $(eq $1,$(get container)),
+			$(foreach c,$(get container),$(set- $c.contents,$(this)))
+			$(if $1,$(set+ $1.contents,$(this)))
+			$1
+		)
+	)
 	$(field contents)
 
 	# Returns a list of referenced objects.
