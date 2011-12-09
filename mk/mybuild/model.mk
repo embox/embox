1a6b97710a8dd868d20789dafa9e9ad666409710
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-12-09 11:24:34 +0000

    #364: containment helper

--- mk/mybuild/model.mk
+++ mk/mybuild/model.mk
@@ -12,19 +12,20 @@ include mk/core/define.mk
 include mk/core/object.mk
 
 # Constructor args:
-#   1. Name of property in container.
 #   1. Name of property in each contained object.
-define class-containment
-	$(field property_in_container,$(or $(singleword $1),
-			$(error 'property_in_container' must be a singleword: '$1')))
-	$(setter property_in_container,$(error $0 is read-only))
-
-	$(field property_in_contents,$(or $(singleword $2),
-			$(error 'property_in_contents' must be a singleword: '$2')))
+#   2. Name of property in container.
+define class-containment_helper
+	$(field property_in_contents,$(or $(singleword $1),
+			$(error 'property_in_contents' must be a singleword: '$1')))
 	$(setter property_in_contents,$(error $0 is read-only))
 
-	$(field container)
-	$(setter container,
+	$(field property_in_container,$(or $(singleword $2),
+			$(error 'property_in_container' must be a singleword: '$2')))
+	$(setter property_in_container,$(error $0 is read-only))
+
+	# 1. Contained (child) node.
+	# 2. New container (parent).
+	$(method set_container_of,
 		$(or $(eq $1,$(get container)),
 			$(and $(foreach p,$(get property_in_contents),
 				$(foreach c,$(get container),$(set- $c.$p,$(this)))
@@ -34,20 +35,26 @@ define class-containment
 		)
 	)
 
-	$(field contents)
-	$(setter contents,
+	$(setter set_contents,
 		$(and $(foreach p,$(get property_in_container),
 			$(foreach c,$(get contents),$(set- $c.$p,$(this)))
 			$(foreach 1,$1,$(set+ $1.$p,$(this)))
 		),)
 		$1
 	)
+
+	$(method set)
 endef
 
 # Constructor args:
 #   1. Resource.
 define class-my_object
-	$(field resource)$(new containment,objects,resource)
+	$(field __resource)
+	$(new containment,objects,resource)
+
+	$(field __contents)
+	$(field __container)
+	$(new containment,contents,container)
 
 	$(field container)
 	$(setter container,
@@ -57,7 +64,6 @@ define class-my_object
 			$1
 		)
 	)
-	$(field contents)
 
 	# Returns a list of referenced objects.
 	$(method get_references,
