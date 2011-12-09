fc43b951d7cebdb3e00d65374ac059ba2c1d389f
Gleb Efimov <sena1or@gmail.com>
2011-12-09 15:57:01 +0000

    It's finally work! (#355)

--- src/example/mem/malloc.c
+++ src/example/mem/malloc.c
@@ -6,6 +6,7 @@
  * @author	Gleb Efimov
  *
  */
+#include <stdio.h>
 #include <stdlib.h>
 #include <types.h>
 #include <framework/example/self.h>
@@ -16,7 +17,7 @@
  */
 EMBOX_EXAMPLE(run);
 
-#define MEM_SIZE 0x1000000
+#define MEM_SIZE 0x10000
 #define NUMBER_OF_TESTS 100
 
 #define CHECK(cfs, sz) \
@@ -32,24 +33,26 @@ char* current_free_space;
 char memory[MEM_SIZE];
 
 void* memory_allocate(int size);
+void  memory_free(void *address);
+void memory_init(void);
 
 static int run(int argc,char **argv){
 	struct memory_descriptor *md;
 	int i, temp;
 	void *succes, *address;
 	current_free_space = memory;
-	md = (void *)memory;
-	md->available = 1;
-	md->size = sizeof(memory);
+	memory_init();
 	for (i = 0; i < NUMBER_OF_TESTS; i++) {
 		succes = memory_allocate(temp = rand()%10000);
 		if (succes == NULL) {
-			printf("Memory allocation error on the addition #%d size of block: %d",i,temp);
+			printf("Memory allocation error on the addition %d size of block: %d",i,temp);
 		}
 	}
-	address = (void *)memory;
+	address = memory;
 	for (i = 0; i < NUMBER_OF_TESTS; i++) {
-		memory_free(*address);
+		if (rand()%2 == 0) memory_free(address);
+		md = address - sizeof(struct memory_descriptor);
+		address += md->size;
 	}
 	return 0;
 }
@@ -58,13 +61,18 @@ void* memory_allocate(int size) {
 	struct memory_descriptor *current_md = (void *)current_free_space;
 	while ((current_md->available == 0) & (current_md->size <= size + sizeof(struct memory_descriptor))) {
 		current_md += current_md->size;
-		if (current_md > memory + sizeof(memory)) return NULL;
+		if ((void *)current_md > (void *)(memory + sizeof(memory))) return NULL;
 	}
 	if (CHECK(current_free_space, size))
 	{
+		int all_block = current_md->size + sizeof(struct memory_descriptor);
+		struct memory_descriptor *temp;
 		current_md->available = 0;
 		current_md->size = size + sizeof(struct memory_descriptor);
 		current_free_space += current_md->size;
+		temp = (void *)current_free_space;
+		temp->available = 1;
+		temp->size = all_block - current_md->size;
 		return ((void *)(current_md + sizeof(struct memory_descriptor)));
 	}
 	return NULL;
@@ -73,5 +81,12 @@ void* memory_allocate(int size) {
 void memory_free(void *address) {
 	struct memory_descriptor *md = address - sizeof(struct memory_descriptor);
 	md->available = 1;
-	current_free_space = md;
+	current_free_space = (char *)md;
+}
+void memory_init(void) {
+	struct memory_descriptor *md;
+	md = (void *)memory;
+	md->available = 1;
+	md->size = sizeof(memory);
+
 }
