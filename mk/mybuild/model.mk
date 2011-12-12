0714ca6f0559d2fb7dd33b4c3d822b751553afdc
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-12-12 16:40:11 +0000

    #364: Remove containment helpers, add node implementation

--- mk/mybuild/model.mk
+++ mk/mybuild/model.mk
@@ -13,6 +13,127 @@ include mk/core/object.mk
 
 include mk/mybuild/scoping.mk
 
+# A node can have a parent and children.
+define class-node
+	$(method get_parent_node)
+	$(method set_parent_node)
+
+	$(method get_child_nodes)
+	$(method set_child_nodes)
+
+endef
+
+# Abstract node implementation.
+define class-abstract_node
+	$(field parent_node/node)
+	$(field child_nodes/node)
+
+	$(method get_parent_node,$(get parent_node))
+	$(method set_parent_node,
+		$(if $(eq $1,$(get parent_node)),
+			,# Do nothing.
+			$(assert $(if $1,$(singleword $1)),
+					Assigning more than one parent: '$1')
+			$(foreach old,$(get parent_node),$(set- old->child_nodes,$(this)))
+			$(set parent_node,$1)
+			$(foreach new,$1,$(set+ new->child_nodes,$(this)))
+		)
+	)
+
+	$(method get_child_nodes,$(get child_nodes))
+	$(method set_child_nodes,
+		$(foreach old,$(filter-out $1,$(get child_nodes)),
+				$(invoke old->set_parent_node,))
+		$(foreach new,$(filter-out $(get child_nodes),$1),
+				$(invoke this->set_parent_node,$(this)))
+	)
+
+endef
+
+define class-named
+	$(method get_name)
+	$(method set_name)
+endef
+
+# Constructor args:
+#   1. Name.
+define class-abstract_named
+	$(field name,$(value 1))
+
+	$(method get_name,$(get name))
+	$(method set_name,$(set name,$1))
+endef
+
+define class-resource
+	$(field nodes)
+	$(field issues)
+endef
+
+# Constructor args:
+#   1. Resource.
+define class-list
+	$(super node,$1)
+
+	# Adds the given elements to this list.
+	#   1. Elements to add.
+	# Return:
+	#   Element that were newly added.
+	$(method add,
+		$(and $(foreach element,$1,
+			$(invoke node_containment_helper->set_container_of,
+					$(element),$(this))
+		),)
+	)
+
+	# Removes the given elements from this list.
+	# Objects that don't belong to this list are not touched.
+	#   1. Elements to remove.
+	# Return:
+	#   Element that were actually removed.
+	$(method remove,
+		$(and $(foreach element,$(filter $(get children),$1),
+			$(invoke node_containment_helper->set_container_of,
+					$(element),)
+		),)
+	)
+
+	$(method get_elements,
+		$(get children)
+	)
+endef
+
+# Constructor args:
+#   1. Package name.
+define class-model
+	$(super abstract_node)
+	$(super abstract_named,$1)
+
+	$(field imports)
+	$(field modules/module)
+
+	$(method set_modules,
+		$(set modules,$1)
+	)
+endef
+
+# Constructor args:
+#   1. Module name.
+define class-module
+	$(super abstract_node)
+	$(super abstract_named,$1)
+
+	$(field modifiers)
+	$(field super_module_ref)
+
+	$(field depends_refs)
+
+	$(field requires_refs)
+	$(field provides_refs)
+
+	$(field sources)
+	$(field objects)
+endef
+
 # Constructor args:
 #   1. (optional) value.
 define class-string
@@ -58,119 +179,6 @@ define class-module_ref
 	$(method get_link_type,module)
 endef
 
-# Constructor args:
-#   1. Module name.
-define class-module
-	$(field name,$1)
-
-	$(field modifiers)
-	$(field super_module_ref)
-
-	$(field depends_refs)
-
-	$(field requires_refs)
-	$(field provides_refs)
-
-	$(field sources)
-	$(field objects)
-endef
-
-# Constructor args:
-#   1. Name of property in each contained object.
-#   2. Name of property in container.
-define class-containment_helper
-	$(field prop_in_contents,$(or $(singleword $1),
-			$(error 'prop_in_contents' must be a singleword: '$1')))
-	$(setter prop_in_contents,$(error $0 is read-only))
-
-	$(field prop_in_container,$(or $(singleword $2),
-			$(error 'prop_in_container' must be a singleword: '$2')))
-	$(setter prop_in_container,$(error $0 is read-only))
-
-	# 1. Contained (child) node.
-	# 2. New container (parent).
-	$(method set_container_of,
-		$(foreach parent,$(get prop_in_contents),
-				$(foreach children,$(get prop_in_container),
-			$(or $(eq $2,$(get $1.$(parent))),
-				$(foreach old,$(get $1.$(parent)),$(set- old->$(children),$1))
-				$(set $1.$(parent),$2)
-				$(foreach new,$2,$(set+ new->$(children),$1))
-			)
-		))
-	)
-
-endef
-
-create_containment_helper = $(new containment_helper,$1,$2)
-
-# Constructor args:
-#   1. Resource.
-define class-node
-	$(field resource)
-	$(invoke resource_containment_helper->set_container_of,$(this),$1)
-
-	$(field children)
-	$(field parent)
-
-	# Returns a list of referenced objects.
-	$(method get_references,
-		$(sort $(foreach f,$(invoke get_reference_features),$(get $f)))
-	)
-
-	# Implementation have to return a list of names of fields corresponding
-	# to reference features.
-	$(method get_reference_features)# Abstract.
-
-endef
-
-define class-resource
-	$(field nodes)
-	$(field issues)
-endef
-
-# Constructor args:
-#   1. Resource.
-define class-list
-	$(super node,$1)
-
-	# Adds the given elements to this list.
-	#   1. Elements to add.
-	# Return:
-	#   Element that were newly added.
-	$(method add,
-		$(and $(foreach element,$1,
-			$(invoke node_containment_helper->set_container_of,
-					$(element),$(this))
-		),)
-	)
-
-	# Removes the given elements from this list.
-	# Objects that don't belong to this list are not touched.
-	#   1. Elements to remove.
-	# Return:
-	#   Element that were actually removed.
-	$(method remove,
-		$(and $(foreach element,$(filter $(get children),$1),
-			$(invoke node_containment_helper->set_container_of,
-					$(element),)
-		),)
-	)
-
-	$(method get_elements,
-		$(get children)
-	)
-endef
-
-# Constructor args:
-#   1. Resource.
-define class-model
-	$(super node,$1)
-endef
-
 $(def_all)
-
-resource_containment_helper := $(call create_containment_helper,resource,nodes)
-node_containment_helper := $(call create_containment_helper,parent,children)
 
 endif # __mybuild_model_mk
