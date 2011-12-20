46019b987d14d53f63522561f5902c1592a5366a
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2011-12-20 14:18:46 +0000

    Add base interfaces for Node and Reference

---
+++ mk/model/model.mk
@@ -0,0 +1,60 @@
+#
+# Model framework core interfaces.
+#
+#   Date: Dec 20, 2011
+# Author: Eldar Abusalimov
+#
+
+ifndef __model_model_mk
+__model_model_mk := 1
+
+include mk/core/define.mk
+include mk/core/object.mk
+
+#
+# Interface of all modelled objects.
+#
+define class-Node
+
+	# Returns meta class of the instance.
+	$(method getMetaClass)
+
+	# 1. Meta reference.
+	$(method getNodeReference)
+
+	# 1. Meta reference.
+	# 2. New references.
+	$(method setNodeReference)
+
+	# 1. Meta reference.
+	# 2. What to add.
+	$(method addNodeReference)
+
+	# 1. Meta reference.
+	# 2. What to remove.
+	$(method removeNodeReference)
+
+	# 1. Meta reference.
+	$(method clearNodeReference)
+
+endef
+
+#
+# Named proxy of referenced node.
+#
+define class-Reference
+
+	# Returns meta reference represented by this proxy.
+	$(method getMetaReference)
+
+	# Returns a textual representation of the reference.
+	$(method getReferenceString)
+
+	# Returns a textual representation of the reference.
+	$(method setReferenceString)
+
+endef
+
+$(def_all)
+
+endif # __model_model_mk
