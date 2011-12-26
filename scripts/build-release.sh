925b6271dad114f18ffb27c89dcc69d0893353d0
Anton Bondarev <anton.bondarev2310@gmail.com>
2011-12-26 13:46:06 +0000

    build-release script

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -3,6 +3,10 @@
 PRGNAM=embox
 VERSION=0.2.4
 
+pushd ../.. > /dev/null
+
 rm -fr `find $PRGNAM-$VERSION -name ".svn"`
 tar cf - $PRGNAM-$VERSION | gzip -f9 > $PRGNAM-$VERSION.tgz
 tar cf - $PRGNAM-$VERSION | xz -f > $PRGNAM-$VERSION.txz
+
+popd > /dev/null
