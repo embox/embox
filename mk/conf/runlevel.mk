e52eefed936667dfd84c419323e687700278c06c
Anton Bondarev <anton.bondarev2310@gmail.com>
2012-01-20 16:07:38 +0000

    mybuild: runlevel setup

--- mk/conf/runlevel.mk
+++ mk/conf/runlevel.mk
@@ -1,11 +1,27 @@
 ifndef __conf_runlevel_mk
 __conf_runlevel_mk := 1
 
-#param $1 runlevel object
-#param $2 module object
-define runlevel_setup
+include mk/conf/roots.mk
 
+#param $1 module object
+define get_runlevel_name
+	embox.framework.level_$(or $(RUNLEVEL-$1),2)
 endef
+
+#param $1 module object
+define runlevel_setup
+	$(foreach r,$(call find_mod,$(call get_runlevel_name,$1)),
+		$(info $r)
+	)
+endef
+
+define runlevels_setup
+	$(info $(\n)$(\n))
+	$(foreach m,$1,
+		$(call runlevel_setup,$m)
+	)
+endef
+
 $(def_all)
 
 endif #__conf_runlevel_mk
