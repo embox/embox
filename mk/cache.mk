9967cd039e0893a09aa76b3b9b82847283341558
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-01-25 12:29:20 +0000

    proper escaping of hashes in cache

--- mk/cache.mk
+++ mk/cache.mk
@@ -19,6 +19,9 @@ __cache_new_line := $(__cache_new_line)
 
 __cache_hash := \#
 
+__cache_escape_variable_name = \
+	$(subst $(__cache_hash),$$(__cache_hash),$(subst $$,$$$$,$1))
+
 # Arg 1: variable name.
 __cache_construct_variable_definition = \
 	$(__cache_construct_$(if $(findstring $(__cache_hash), \
@@ -29,22 +32,23 @@ __cache_construct_variable_definition = \
 			,verbose,regular)_definition_for_$(flavor $1)_variable)
 
 __cache_construct_regular_definition_for_recursive_variable = \
-	$(subst $$,$$$$,$1) = $(value $1)
+	$(__cache_escape_variable_name) = $(value $1)
 __cache_construct_regular_definition_for_simple_variable = \
-	$(subst $$,$$$$,$1 := $(value $1))
+	$(call __cache_escape_variable_name,$1 := $(value $1))
 
 __cache_construct_verbose_definition_for_recursive_variable = \
-	define $(subst $$,$$$$,$1)$(__cache_new_line)$(value \
+	define $(__cache_escape_variable_name)$(__cache_new_line)$(value \
 		$1)$(__cache_new_line)endef
 __cache_construct_verbose_definition_for_simple_variable = \
 	$(__cache_construct_verbose_definition_for_recursive_variable) \
-	$(__cache_new_line)$(subst $$,$$$$,$1) := $$(value $(subst $$,$$$$,$1))
+	$(__cache_new_line)$(__cache_escape_variable_name) := \
+		$$(value $(__cache_escape_variable_name))
 
 # No args.
 __cache_print_all_new_variable_definitions = \
-	$(and $(foreach 1,$(filter-out \
+	$(and $(foreach 1,$(sort $(filter-out \
 			$(__cache_preinclude_variables), \
-			$(__cache_postinclude_variables)), \
+			$(__cache_postinclude_variables))), \
 		$(info $(__cache_construct_variable_definition))),)
 
 __cache_preinclude_variables := $(.VARIABLES)
