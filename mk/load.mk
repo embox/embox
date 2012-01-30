40e79d26f50f10828d731e711b8622c4abaef155
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-01-30 14:52:32 +0000

    Improve load.mk: using target specific variables instead of plain ones
    Also add mk/.cache to .gitignore.

--- mk/load.mk
+++ mk/load.mk
@@ -7,38 +7,37 @@ CACHE_DIR := mk/.cache
 
 # Core scripts: def & obj.
 mk_core := $(CACHE_DIR)/mk_core.mk
-$(mk_core)/includes := \
+$(mk_core) : includes := \
 	mk/core/string.mk \
 	mk/core/define.mk \
 	mk/core/object.mk
-$(mk_core)/uses :=
 
 # GOLD parser engine.
 mk_gold := $(CACHE_DIR)/mk_gold.mk
-$(mk_gold)/includes := \
+$(mk_gold) : includes := \
 	mk/gold/engine.mk
-$(mk_gold)/uses := \
+$(mk_gold) : uses := \
 	$(mk_core)
 
 ifeq (0,1) ###
 # Tiny version of EMF Ecore.
 mk_model := $(CACHE_DIR)/mk_model.mk
-$(mk_model)/includes := \
+$(mk_model) : includes := \
 	mk/model/model.mk     \
 	mk/model/factory.mk   \
 	mk/model/metamodel.mk
-$(mk_model)/uses := \
+$(mk_model) : uses := \
 	$(mk_core)
 
 # Mybuild itself.
 mk_mybuild := $(CACHE_DIR)/mk_mybuild.mk
-$(mk_mybuild)/includes := \
+$(mk_mybuild) : includes := \
 	mk/mybuild/myfile-model.mk     \
 	mk/mybuild/myfile-factory.mk   \
 	mk/mybuild/myfile-metamodel.mk \
 	mk/mybuild/myfile-resource.mk  \
 	mk/mybuild/myfile-parser.mk
-$(mk_mybuild)/uses := \
+$(mk_mybuild) : uses := \
 	$(mk_core) \
 	$(mk_gold) \
 	$(mk_model)
@@ -52,11 +51,11 @@ all_mk_targets := \
 else ###
 
 mk_mybuild := $(CACHE_DIR)/mk_mybuild.mk
-$(mk_mybuild)/includes := \
+$(mk_mybuild) : includes := \
 	mk/mybuild/model.mk     \
 	mk/mybuild/myfile.mk     \
 	mk/mybuild/resource.mk
-$(mk_mybuild)/uses := \
+$(mk_mybuild) : uses := \
 	$(mk_core) \
 	$(mk_gold)
 
@@ -67,16 +66,20 @@ all_mk_targets := \
 
 endif ###
 
+# Defaults.
+includes :=
+uses :=
+
 all : $(all_mk_targets)
 	$(MAKE) -f mk/core/common.mk MAKEFILES=$(mk_mybuild)
 
 .SECONDEXPANSION:
-$(all_mk_targets) : $$($$@/includes)
-$(all_mk_targets) : $$($$@/uses)
+$(all_mk_targets) : $$(includes)
+$(all_mk_targets) : $$(uses)
 $(all_mk_targets) : mk/load.mk
 $(all_mk_targets) : mk/cache.mk
 	@echo Preparing $(@F)...
 	@mkdir -p $(@D)
 	@$(MAKE) -f mk/cache.mk \
-		CACHE_INCLUDES='$(value $@/includes)' \
-		CACHE_USES='$(value $@/uses)' > $@
+		CACHE_INCLUDES='$(includes)' \
+		CACHE_USES='$(uses)' > $@
