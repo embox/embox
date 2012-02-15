e2f73cf7751cdd7b8a13681b4146050d265ff4de
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-02-15 12:14:34 +0000

    reorder some code in mk/load.mk, add header comment

--- mk/load.mk
+++ mk/load.mk
@@ -1,21 +1,12 @@
 #
+# First stage launcher: prepares necessary Make scripts
+# and runs the second stage (see mk/load2.mk).
+#
 #   Date: Jan 25, 2012
 # Author: Eldar Abusalimov
 #
 
 MK_CACHE_DIR := mk/.cache/mk
-
-# Default values which we are about to override with target-specific ones.
-export CACHE_INCLUDES :=
-export CACHE_REQUIRES :=
-
-# Because inclusions of cached scripts may occur in a non-linear order
-# we need to specify a unique allocation scope to prevent possible collisions.
-# (Generally speaking the order of caching is defined by the dependence DAG of
-# the cache. Thus if script A requires B and C, which are cached
-# independently and allocate the same resource, then a collision would happen
-# after including both of them at the same time.)
-export ALLOC_SCOPE
 
 # Core scripts: def & obj.
 export mk_core_def := $(MK_CACHE_DIR)/mk_core_def.mk
@@ -65,7 +56,7 @@ $(mk_mybuild) : CACHE_REQUIRES := \
 	$(mk_model)
 $(mk_mybuild) : ALLOC_SCOPE := e
 
-all_mk_scripts := \
+export all_mk_scripts := \
 	$(mk_core_def) \
 	$(mk_core_obj) \
 	$(mk_gold_engine) \
@@ -85,7 +76,7 @@ $(mk_mybuild) : CACHE_REQUIRES := \
 	$(mk_gold_engine)
 $(mk_mybuild) : ALLOC_SCOPE := e
 
-all_mk_scripts := \
+export all_mk_scripts := \
 	$(mk_core_def) \
 	$(mk_core_obj) \
 	$(mk_gold_engine) \
@@ -93,15 +84,27 @@ all_mk_scripts := \
 
 endif ###
 
-export all_mk_scripts
+# To get our scripts work properly an 'ALLOC_SCOPE' variable should be defined.
+#
+# Different scopes are necessary to prevent possible collisions, because
+# inclusions of cached scripts may occur in a non-linear order. Generally
+# speaking, the order of caching is defined by the dependence DAG of the cache.
+# Thus if script A requires B and C, which are cached independently and
+# allocate the same resource, then a collision would happen after including
+# both of them at the same time.
+export ALLOC_SCOPE ?=
 
 $(MAKECMDGOALS) : $(all_mk_scripts)
 	@$(MAKE) -f mk/load2.mk $@
 
--include $(all_mk_scripts:%=%.d)
-
 .DELETE_ON_ERROR:
+
+# Default values which are overridden with target-specific ones.
+$(all_mk_scripts) : export CACHE_INCLUDES ?=
+$(all_mk_scripts) : export CACHE_REQUIRES ?=
+
 .SECONDEXPANSION:
+
 $(all_mk_scripts) : $$(CACHE_INCLUDES)
 $(all_mk_scripts) : $$(CACHE_REQUIRES)
 $(all_mk_scripts) : mk/load.mk
@@ -110,3 +113,5 @@ $(all_mk_scripts) :
 	@echo ' MKGEN $(@F)'
 	@mkdir -p $(@D) && \
 		$(MAKE) -f mk/script/mk-cache.mk CACHE_DEP_TARGET='$@' > $@
+
+-include $(all_mk_scripts:%=%.d)
