fc9bee747768cafb5efe44e226a242e5ce3b5716
Alex Kalmuk <alexkalmuk@gmail.com>
2012-03-02 17:53:26 +0000

    pnet: net_entry

--- src/pnet/net_entry.c
+++ src/pnet/net_entry.c
@@ -7,28 +7,33 @@
  */
 
 #include <errno.h>
-
 #include <pnet/prior_path.h>
-
 #include <net/skbuff.h>
 #include <net/netdevice.h>
-
 #include <pnet/core.h>
 #include <pnet/node.h>
 #include <pnet/repo.h>
-#if 0
-int pnet_entry(struct net_packet *pack) {
+
+#include <linux/interrupt.h>
+#include <embox/unit.h>
+#include <linux/init.h>
+#include <pnet/match.h>
+#include <pnet/pnet_pack.h>
+
+EMBOX_UNIT_INIT(unit_init);
+
+int pnet_entry(struct pnet_pack *pack) {
 	net_device_t *dev = pnet_get_net_device(pack->node);
 	pack->skbuf->dev = dev;
 
 	netif_rx(pack->skbuf);
 
-	pnet_pack_free(pack);
+	pnet_pack_destroy(pack);
 
 	return 0;
 }
 
-int netif_rx(struct sk_buff *skb) {
+int pnetif_rx(struct sk_buff *skb) {
 	net_device_t *dev;
 
 	if (NULL == skb) {
@@ -47,15 +52,55 @@ int netif_rx(struct sk_buff *skb) {
 	netif_rx_schedule(dev);
 	return NET_RX_SUCCESS;
 }
-#endif
+#if 0
 int netif_receive_skb(sk_buff_t *skb) {
-//	struct pnet_pack *pack;
+	struct pnet_pack *pack;
 	net_node_t node;
 
 	node = pnet_get_dev_by_device(skb->dev);
-	// TODO pnet pack = pnet_pack_alloc_skb(node->rx_dfault, skb);
+	//pnet pack = pnet_pack_alloc_skb(node->rx_dfault, skb);
 //	pack = NULL;
-//	pnet_rx_thread_add(pack);
+	pnet_rx_thread_add(skb, node);
 
+	return 0;
+}
+#endif
+
+void pnetif_rx_schedule(void) {
+	raise_softirq(PNET_RX_SOFTIRQ);
+}
+
+static int is_empty_pnet(void) {
+	return 1;
+}
+
+static int is_empty_net(void) {
+	return 0;
+}
+
+static int match_skb(struct sk_buff *skb, net_node_t node) {
+	return 0;
+}
+
+static void pnet_rx_action(struct softirq_action *action) {
+	net_node_t node;
+	struct sk_buff *skb;
+
+	skb = skb_dequeue(NULL);
+	while(!is_empty_pnet()) {
+		//match(pack);
+	}
+	while(!is_empty_net()) {
+		node = pnet_get_dev_by_device(skb->dev);
+		if(!match_skb(skb, node)) {
+			netif_rx_schedule(skb->dev);
+		}
+	}
+
+
+}
+
+static int unit_init(void) {
+	open_softirq(PNET_RX_SOFTIRQ, pnet_rx_action, NULL);
 	return 0;
 }
