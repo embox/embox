a97815387ec1783c5e158ac0ca2472036015e4c7
Anton Kozlov <drakon.mega@gmail.com>
2012-03-20 14:22:12 +0000

    mybuild translation of old config files to new

--- mk/load3.mk
+++ mk/load3.mk
@@ -9,9 +9,17 @@ export CONFIGFILES_CACHE_DIR := $(MYBUILD_CACHE_DIR)/config
 
 CONFIG_PATH := conf/
 
+CONFIG_GENERATED := $(CONFIG_PATH)/genConf.config
+
+$(CONFIG_GENERATED) :
+	mkdir -p $(@D)
+	$(HOSTCPP) -P -undef -nostdinc $(HOSTCC_CPPFLAGS) \
+		-D__MODS_CONF__ mk/confmacro2.S | sed 's/\\t/\t/' > $@
+
 CONFIGFILES := \
 	$(shell find $(CONFIG_PATH) -depth \
-		-name \*.config -print)
+		-name \*.config -print) \
+	$(CONFIG_GENERATED)
 
 override CONFIGFILES := $(firstword $(CONFIGFILES))
 
@@ -26,7 +34,13 @@ $(MAKECMDGOALS) : $(configfiles_linked_mk)
 
 .DELETE_ON_ERROR:
 
-.PHONY : $(configfiles_linked_mk)
+.PHONY : $(configfiles_linked_mk) $(CONFIG_GENERATED)
+
+HOSTCPP = gcc -E
+
+HOSTCC_CPPFLAGS := -I $(CONFIG_PATH)
+
+$(configfiles_mk) : $(CONFIGFILES)
 
 $(configfiles_mk) : mk/load3.mk
 $(configfiles_mk) : mk/script/mk-persist.mk
