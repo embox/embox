1af43424428b580a3c3e09f95d3a66753e13dbd0
Andrey Gazukin <gazukin.andrey@gmail.com>
2012-03-21 09:11:54 +0000

    Add final hashtable test

--- src/tests/util/hashtable_test.c
+++ src/tests/util/hashtable_test.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <util/array.h>
 #include <util/hashtable.h>
 
 EMBOX_TEST_SUITE("util/hashtable test");
@@ -17,6 +18,9 @@ EMBOX_TEST_SUITE("util/hashtable test");
 struct ht_element {
 	int number;
 };
+
+static struct ht_element el[3] = { {1}, {2}, {3}};
+static const char *key[3] = {"first", "secnd", "third" };
 
 static size_t get_hash(void *key) {
 	char *key_str = key;
@@ -30,43 +34,36 @@ static int cmp_keys(void *key1,void *key2) {
 
 TEST_CASE("Add single element to hashtable") {
 	struct hashtable *ht;
-	struct ht_element el = { 1 };
 	struct ht_element *ht_value;
 
 	ht = hashtable_create(0x10, get_hash, cmp_keys);
-	hashtable_put(ht, "first", &el);
-	ht_value = (struct ht_element *) hashtable_get(ht, "first");
+	hashtable_put(ht, (void *)key[0], &el[0]);
+	ht_value = (struct ht_element *) hashtable_get(ht, (void *)key[0]);
 	hashtable_destroy(ht);
 
-	test_assert_equal(ht_value, &el);
+	test_assert_equal(ht_value, &el[0]);
 }
 
 TEST_CASE("Add three elements to hashtable") {
 	struct hashtable *ht;
-	struct ht_element el[3] = { {1}, {2}, {3}};
-
 	struct ht_element *ht_value[3];
-	const char *key[3] = {"first", "secnd", "third" };
+	int i, j;
 
 	ht = hashtable_create(0x30, get_hash, cmp_keys);
-	for(int i=0; i<3; i++)	{
+
+	for(i = 0; i < ARRAY_SIZE(el); i++)	{
 		hashtable_put(ht, (void *)key[i], &el[i]);
 	}
 
-	for(int i=0; i<3; i++)	{
-		if(NULL != hashtable_get(ht, (void *)key[i])){
-			ht_value[i] = (struct ht_element *) hashtable_get(ht, (void *)key[i]);
-			/*printf ("%d\n",(int) ht_value[i]->number);*/
-		}
-		else {
-			/*printf ("NULL");*/
-		}
+	for(i = 0; i < ARRAY_SIZE(ht_value); i++)	{
+		ht_value[i] = (struct ht_element *) hashtable_get(ht, (void *)key[i]);
+		test_assert_not_null(ht_value[i]);
 	}
 
 	hashtable_destroy(ht);
 
-	for(int i=0; i<3; i++)	{
-		for(int j=0; j<3; j++)	{
+	for(i = 0; i < ARRAY_SIZE(el); i++)	{
+		for(j = 0; j < ARRAY_SIZE(ht_value); j++)	{
 			if (i == j){
 				test_assert_equal(ht_value[i], &el[j]);
 			}
