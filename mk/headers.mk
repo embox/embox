1db3a3ed6a9f6f727a07676c845a8784fe9f6216
Anton Kozlov <drakon.mega@gmail.com>
2012-03-23 16:18:33 +0000

    mybuild module headers

--- mk/headers.mk
+++ mk/headers.mk
@@ -3,5 +3,10 @@
 HEADERS_BUILD := \
   $(patsubst %,$(OBJ_DIR)/mods/%.h,$(subst .,/,$(foreach i,$(MODS_ENABLE_OBJ),$(call get,$(call get,$i,type),qualifiedName))))
 
-$(HEADERS_BUILD): mk/image.mk $(AUTOCONF_DIR)/mods.mk
-	@$(MKDIR) $(@D) && printf "%b" '$(__header_gen)' > $@
+configfiles_model_mk := $(MYBUILD_CACHE_DIR)/configfiles-model.mk
+
+$(HEADERS_BUILD): mk/image.mk $(configfiles_model_mk)
+	@$(MKDIR) $(@D) && printf "%b" '$(__header_gen)' > $@.tmp
+	@diff -q $@ $@.tmp &>/dev/null; \
+		if [ ! 0 -eq $$? ]; then mv $@.tmp $@; echo Module header $@; \
+			else rm $@.tmp; fi
