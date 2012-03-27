67ed3e8478de54816a9f4e1dcafc6c092963862e
Anton Kozlov <drakon.mega@gmail.com>
2012-03-27 11:23:06 +0000

    mybuild header gen logic bug (oops)

--- mk/headers.mk
+++ mk/headers.mk
@@ -7,4 +7,4 @@ HEADERS_BUILD_TMP := $(HEADERS_BUILD:%=%.tmp)
 
 $(HEADERS_BUILD_TMP) : mk/image.mk $(build_model_mk)
 	@$(MKDIR) $(@D) && printf "%b" '$(call __header_gen,$(subst .tmp,,$@))' > $@
-	@diff -q $@ $(subst .tmp,,$@) &>/dev/null || (mv $@ $(subst .tmp,,$@); echo Module header $@)
+	@diff -q $@ $(subst .tmp,,$@) &>/dev/null || (cp $@ $(subst .tmp,,$@); echo Module header $@)
