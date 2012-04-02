2ca7ddc70ad7ce40924df964b6043800814be364
Anton Kozlov <drakon.mega@gmail.com>
2012-04-02 17:02:04 +0000

    forgotten files added (oops)

---
+++ src/drivers/diag/mem_diag.c
@@ -0,0 +1,48 @@
+/**
+ * @file
+ *
+ * @brief
+ *
+ * @date 17.03.2012
+ * @author Anton Kozlov
+ */
+
+#include <kernel/diag.h>
+#include <string.h>
+
+#define BUF_LEN 0x100
+
+static char diag_buf[BUF_LEN + 1]; /* for trailing \0 */
+static int diag_buf_head = 0;
+
+char *diag_get_first_part(void) {
+	return diag_buf + diag_buf_head + 1;
+}
+
+char *diag_get_last_part(void) {
+	return diag_buf;
+}
+
+void diag_init(void) {
+	diag_buf_head = 0;
+	memset(diag_buf, 0, BUF_LEN + 1);
+}
+
+char diag_getc(void) {
+	return 0;
+
+}
+
+void diag_putc(char ch) {
+	if (diag_buf_head == BUF_LEN) {
+		diag_buf_head = 0;
+	}
+
+	diag_buf[diag_buf_head] = ch;
+	diag_buf[++diag_buf_head] = '\0';
+
+}
+
+int diag_has_symbol(void) {
+	return 0;
+}
---
+++ src/drivers/interrupt/no_interrupts.h
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * @brief No interrupts
+ *
+ * @date 24.03.2012
+ * @author Anton Kozlov
+ */
+
+#ifndef DRIVERS_INTERRUPT_NO_INTERRUPTS
+#define DRIVERS_INTERRUPT_NO_INTERRUPTS
+
+#define __INTERRUPT_NRS_TOTAL 32
+
+#ifndef __ASSEMBLER__
+
+typedef unsigned char __interrupt_nr_t;
+typedef unsigned long __interrupt_mask_t;
+
+#endif /*__ASSEMBLER__*/
+
+#endif /* DRIVERS_INTERRUPT_NO_INTERRUPTS */
+
