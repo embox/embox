3eafc90cb98302c3e87ed0d40a2ef65a0f0ddc3a
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-04-03 11:25:50 +0000

    protect test_emit with IPL, however this does not fix the issue
    (#323 related)

---
+++ src/framework/test/emit.c
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * @brief TODO documentation for emit.c -- Eldar Abusalimov
+ *
+ * @date Apr 2, 2012
+ * @author Eldar Abusalimov
+ */
+
+#include "emit_impl.h"
+
+#include <hal/ipl.h>
+
+void test_emit_into(struct test_emit_buffer *b, char ch) {
+	ipl_t ipl = ipl_save();
+	{
+		if (test_emit_buffer_overflown(b)) {
+			goto out;
+		}
+
+		if (!test_emit_buffer_full(b)) {
+			*b->ptr++ = ch;
+			*b->ptr = '\0';
+		} else {
+			/* do overflow */
+			b->ptr++;
+		}
+	}
+	out: ipl_restore(ipl);
+}
+
