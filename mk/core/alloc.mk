683a45ef542f418b4c732e97ae19786a17cd6679
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-04-12 16:49:14 +0000

    Rewrite mk/core/alloc.mk using more efficient increment operation

--- mk/core/alloc.mk
+++ mk/core/alloc.mk
@@ -9,15 +9,31 @@ __core_alloc_mk := 1
 ALLOC_SCOPE ?=
 
 alloc = \
-	$(foreach cnt,$(words x \
-		$(or $(value __alloc_cnt_$(ALLOC_SCOPE)-$1), \
-			${eval __alloc_cnt_$(ALLOC_SCOPE)-$1 :=})) \
-		,${eval __alloc_cnt_$(ALLOC_SCOPE)-$1 += $(cnt)}$(cnt)$(ALLOC_SCOPE))
+	$(call __alloc,__alloc_cnt_$(ALLOC_SCOPE)-$1)$(ALLOC_SCOPE)
+__alloc = \
+	${eval $1 := $(call inc,$(value $1))}$(subst .,,$($1))
 
 alloc_last = \
-	$(foreach cnt,$(lastword $(value __alloc_cnt_$(ALLOC_SCOPE)-$1)) \
-		,$(cnt)$(ALLOC_SCOPE))
+	$(subst .,,$(value __alloc_cnt_$(ALLOC_SCOPE)-$1))$(ALLOC_SCOPE)
 
 __cache_volatile += __alloc_cnt_%
+
+# Increments the given numeber.
+#   1. Decimal with periods between digits: 42='.4.2'
+inc = \
+	$(call __inc$(suffix .0$1),$(basename $1))
+
+__inc.0 = $1.1
+__inc.1 = $1.2
+__inc.2 = $1.3
+__inc.3 = $1.4
+__inc.4 = $1.5
+__inc.5 = $1.6
+__inc.6 = $1.7
+__inc.7 = $1.8
+__inc.8 = $1.9
+__inc.9 = $(call inc,$1).0
+
+to_dec = $(subst .,,$1)
 
 endif # __core_alloc_mk
