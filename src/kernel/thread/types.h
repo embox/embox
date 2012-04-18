faa661b2177492c9411e1e92aad479c3ac734df9
Anton Bulychev <iantonishko@gmail.com>
2012-04-18 21:51:01 +0000

    Comment

--- src/kernel/thread/types.h
+++ src/kernel/thread/types.h
@@ -69,7 +69,7 @@ struct thread {
 	struct task      *task;          /**< Task belong to. */
 	struct list_head  task_link;     /**< Link in list holding task threads. */
 
-	clock_t           running_time;  /**< Running time of thread. */
+	clock_t           running_time;  /**< Running time of thread in clocks. */
 };
 
 #endif /* KERNEL_THREAD_TYPES_H_ */
