9f3687ae964f26e301fc13caa0b782e3c570f986
Ilia Vaprol <ki.stfu@gmail.com>
2012-04-23 07:41:46 +0000

    cmd: httpd

--- src/cmds/httpd.c
+++ src/cmds/httpd.c
@@ -17,9 +17,11 @@
 
 EMBOX_CMD(exec);
 
+
 #define MAX_FNAME     32
 #define BUFF_SZ       512
 #define DEFAULT_PAGE  "index.html"
+
 
 /* HTTP Methods */
 enum http_method {
@@ -59,17 +61,9 @@ struct client_info {
 	char req_file[MAX_FNAME]; /* client want open this page */
 	char buff[BUFF_SZ];       /* client's buffer (may contains more than one piece of data) */
 	char *data;               /* pointer to current chunk */
-	size_t len;               /* size of data */
 	char *next_data;          /* pointer to next piece of data in buffer */
 	size_t next_len;          /* length of the next chunk */
 };
-
-/* Methods table */
-#define METHOD_INFO_SET(name) {(#name " "), sizeof (#name " "), HTTP_METHOD_##name }
-const struct { char *n; size_t l; enum http_method m; } methods_info[] = {
-	METHOD_INFO_SET(GET), METHOD_INFO_SET(POST)
-};
-#undef METHOD_INFO_SET
 
 /* Status code */
 static const char *http_stat_str[HTTP_STAT_MAX] = {
@@ -82,7 +76,7 @@ static const char *http_stat_str[HTTP_STAT_MAX] = {
 	[HTTP_STAT_414] = "414 Request-URI Too Long",
 };
 
-static int get_next_chunk(struct client_info *info) {
+static char * get_next_line(struct client_info *info) {
 	int res;
 	size_t i, len;
 	char *chunk;
@@ -90,20 +84,18 @@ static int get_next_chunk(struct client_info *info) {
 	len = info->next_len;
 	chunk = info->next_data;
 	/* if no more data, return NULL */
-	if (len == 0) {
-		info->len = 0;
-		return 0;
+	if ((chunk != NULL) && (len == 0)) {
+		return NULL;
 	}
 
 	/* try to get next chunk (if we find end-line then exit) */
 	for (i = 0; i < len; ++i) {
-		if (chunk[i] == '\n') {
-			/* chunk[i] = '\0'; */
-			info->next_data = chunk + (i + 1);
-			info->next_len = len - (i + 1);
+		if (chunk[i] == '\r') {
+			chunk[i] = '\0';
+			info->next_data = chunk + (i + 2);
+			info->next_len = len - (i + 2);
 			info->data = chunk;
-			info->len = (i + 1);
-			return 0;
+			return chunk;
 		}
 	}
 	/* need to recive more data (end-line not found): */
@@ -113,109 +105,101 @@ static int get_next_chunk(struct client_info *info) {
 	res = recvfrom(info->sock, chunk, sizeof info->buff - len, 0,
 			(struct sockaddr *)&info->addr, &info->addr_len);
 	if (res <= 0) {
-		/* chunk[len] = '\0'; */
-		info->next_len = 0;
-		info->data = chunk;
-		info->len = len;
-		return 0;
+		return NULL;
 	}
 
 	/* find chunks again */
 	/* i == len there */
 	for (len += res; i < len; ++i) {
-		if (chunk[i] == '\n') {
-			/* chunk[i] = '\0'; */
-			info->next_data = chunk + (i + 1);
-			info->next_len = len - (i + 1);
+		if (chunk[i] == '\r') {
+			chunk[i] = '\0';
+			info->next_data = chunk + (i + 2);
+			info->next_len = len - (i + 2);
 			info->data = chunk;
-			info->len = (i + 1);
-			return 0;
+			return chunk;
 		}
 	}
 
 	/* error: string is too long */
 	printf("Error.. string is too long\n");
 	info->next_len = 0;
-	info->data = chunk;
-	info->len = len;
 
-	return -1;
-}
-
-static enum http_method parse_method(char *buff, size_t len) {
-	size_t i;
-
-	for (i = 0; i < sizeof methods_info / sizeof methods_info[0]; ++i) {
-		if ((len >= methods_info[i].l)
-				&& (memcmp(buff, methods_info[i].n, methods_info[i].l) == 0)) {
-			return methods_info[i].m;
-		}
-	}
-
-	return HTTP_METHOD_UNKNOWN;
+	return NULL;
 }
 
 static int http_hnd_title(struct client_info *info) {
-	size_t i, len;
-	char *data;
+	char *method, *file, *param, *version;
 
-	printf("http_req_hdr: ");
-	write(1, info->data, info->len);
+	printf("http_req_hdr: %s\n", info->data);
 
-	len = info->len;
-	data = info->data;
-	switch ((info->method = parse_method(data, len))) {
-	default: /*  unknown or unsupported */
-		return HTTP_STAT_405;
-	case HTTP_METHOD_GET:
-		/* parse file */
-		for (i = 4; i < len; ++i) {
-			if ((data[i] == ' ') || (data[i] == '?')) {
-				if (strncmp(data + 4, "/ ", 2) == 0) {
-					strcpy(info->req_file, DEFAULT_PAGE);
-				}
-				else {
-					strncpy(info->req_file, data + 5, i - 5);
-				}
-				return HTTP_RET_HNDOPS;
-			}
-		}
-		return HTTP_STAT_414;
+	method = info->data;
+
+	file = strchr(method, ' ');
+	if (file == NULL) {
+		return HTTP_RET_ABORT; /* bad request */
 	}
+
+	*file = '\0', file = file + 1;
+
+	if (strcmp(method, "GET") == 0) {
+		info->method = HTTP_METHOD_GET;
+
+		version = strchr(file, ' ');
+		if (version == NULL) {
+			return HTTP_RET_ABORT; /* bad request */
+		}
+
+		version = '\0', version = version + 1;
+
+		param = strchr(file, '?');
+		if (param != NULL) {
+			*param = '\0', param = param + 1;
+		}
+
+		if (strcmp(file, "/") == 0) {
+			strcpy(info->req_file, DEFAULT_PAGE);
+		}
+		else {
+			strncpy(info->req_file, file, sizeof info->req_file);
+		}
+	}
+	else if (strcmp(method, "POST") == 0) {
+		info->method = HTTP_METHOD_POST;
+	}
+	else {
+		info->method = HTTP_METHOD_UNKNOWN;
+		return HTTP_STAT_405; /* method unknown or unsupported */
+	}
+
+	return HTTP_RET_HNDOPS;
 }
 
 static int http_hnd_ops(struct client_info *info) {
-	size_t i, len;
-	char opt[32], *data;
+	char *ops, *param;
 
-	data = info->data;
-	len = info->len;
+	printf("http_req_ops: %s\n", info->data);
 
-	for (i = 0; i < len; ++i) {
-		if ((data[i] == ':') && (data[i] == '\r')) {
-			strncpy(opt, data, i);
-			break;
-		}
+	ops = info->data;
+	if (strcmp(ops, "") == 0) {
+		return HTTP_RET_ENDHDR;
 	}
-	if (i == len) {
+
+	param = strchr(ops, ':');
+	if (param == NULL) {
 		return HTTP_STAT_400;
 	}
 
-	printf("http_req_ops: ");
-	write(1, info->data, info->len);
+	*param = '\0', param = param + 2;
 
-	if (strcmp(data, "") == 0) {
-		return HTTP_RET_ENDHDR;
-	}
-	else if (strcmp(data, "Host") == 0) { }
-	else if (strcmp(data, "Connection") == 0) { }
-	else if (strcmp(data, "User-Agent") == 0) { }
-	else if (strcmp(data, "Accept") == 0) { }
-	else if (strcmp(data, "Accept-Encoding") == 0) { }
-	else if (strcmp(data, "Accept-Language") == 0) { }
-	else if (strcmp(data, "Accept-Charset") == 0) { }
+	if (strcmp(ops, "Host") == 0) { }
+	else if (strcmp(ops, "Connection") == 0) { }
+	else if (strcmp(ops, "User-Agent") == 0) { }
+	else if (strcmp(ops, "Accept") == 0) { }
+	else if (strcmp(ops, "Accept-Encoding") == 0) { }
+	else if (strcmp(ops, "Accept-Language") == 0) { }
+	else if (strcmp(ops, "Accept-Charset") == 0) { }
 	else {
-		printf("httpd warning: unknown options");
+		printf("httpd warning: unknown options: ops='%s' param='%s'\n", ops, param);
 	}
 
 	return HTTP_RET_OK;
@@ -226,7 +210,7 @@ static int process_request(struct client_info *info) {
 	int (*http_header_hnd)(struct client_info *);
 
 	http_header_hnd = http_hnd_title; /* set first heandler of data */
-	while (get_next_chunk(info) == -1) {
+	while (get_next_line(info) == 0) {
 		res = http_header_hnd(info);
 		switch (res) {
 		default:
@@ -241,7 +225,7 @@ static int process_request(struct client_info *info) {
 		}
 	}
 
-	return HTTP_STAT_413;
+	return (http_header_hnd == http_hnd_title ? HTTP_STAT_414 : HTTP_STAT_413);
 }
 
 static int http_req_get(struct client_info *info) {
@@ -300,7 +284,6 @@ static int http_req_get(struct client_info *info) {
 static int process_response(struct client_info *info) {
 	switch (info->method) {
 	default:
-		assert(0);
 		return HTTP_STAT_405;
 	case HTTP_METHOD_GET:
 		return http_req_get(info);
@@ -330,7 +313,7 @@ process_again:
 		/* 2. set ops */
 		curr += sprintf(curr, "Content-Type: %s\r\n", "text/html");
 		/* Send file */
-		printf("RESPONSE:\n %s", ci.buff);
+		printf("RESPONSE:\n%s", ci.buff);
 		break;
 	case HTTP_RET_OK:
 		hnd = process_response;
@@ -345,7 +328,7 @@ process_again:
 	return;
 }
 
-int exec(int argc, char **argv) {
+static int exec(int argc, char **argv) {
 	int res, host;
 	socklen_t addr_len;
 	struct sockaddr_in addr;
