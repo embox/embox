c83cf07534ea1f49b47c45f300fe8e0560dc23f7
Anton Bondarev <anton.bondarev2310@gmail.com>
2012-05-03 13:09:37 +0000

    Fix build

--- src/compat/posix/fs.c
+++ src/compat/posix/fs.c
@@ -12,7 +12,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#include <fs/fs.h>
+//#include <fs/fs.h>
 #include <fcntl.h>
 
 int remove(const char *pathname) {
@@ -27,9 +27,12 @@ int rmdir(const char *pathname) {
 	return 0;
 }
 
+int mkdir(const char *path, mode_t __mode) {
+	return 0;
+}
+
 int creat(const char *pathname, mode_t mode) {
 	return 0;
-
 }
 
 int fstat(const char *path, stat_t *buf) {
