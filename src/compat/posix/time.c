8f33b2956d7cf8d49d6b3d57d182685d85f7a888
Alex Kalmuk <alexkalmuk@gmail.com>
2012-05-12 12:56:40 +0000

    time: Move time.c to POSIX

--- src/compat/posix/time.c
+++ src/compat/posix/time.c
@@ -1,16 +1,41 @@
 /**
  * @file
  *
- * @brief
+ * @brief ISO C99 Standard: 7.23 Date and time.
  *
  * @date 12.05.2012
+ * @author Ilia Vaprol
  * @author Anton Bondarev
+ *         - clock()
  */
 
 #include <time.h>
 #include <hal/clock.h>
-
+#include <kernel/clock_source.h>
 
 clock_t clock(void) {
 	return clock_sys_ticks();
+}
+
+static useconds_t time_usec(void) {
+	return clock_source_clock_to_usec(NULL, clock());
+}
+
+struct timeval * get_timeval(struct timeval *tv) {
+	useconds_t usec;
+
+	usec = time_usec();
+	tv->tv_sec = usec / MICROSEC_PER_SEC;
+	tv->tv_usec = usec % MICROSEC_PER_SEC;
+	return tv;
+}
+
+time_t time(time_t *t) {
+	time_t sec;
+
+	sec = time_usec() / MICROSEC_PER_SEC;
+	if (t) {
+		*t = sec;
+	}
+	return sec;
 }
--- src/include/kernel/ktime.h
+++ src/include/kernel/ktime.h
@@ -9,11 +9,14 @@
 #define KERNEL_KTIME_H_
 
 #include <stdint.h>
+#include <kernel/clock_source.h>
 
 /* Parameters used to convert the time specific values */
 #define MSEC_PER_SEC    1000L
 #define USEC_PER_MSEC   1000L
 #define NSEC_PER_USEC   1000L
+#define USEC_PER_SEC    1000000L
+#define NSEC_PER_SEC    1000000000L
 
 /* nanosecond-resolution time format */
 typedef uint64_t ns_t;
@@ -27,7 +30,7 @@ static inline uint64_t ns_to_cycles(uint64_t ns, uint32_t khz) {
 	return (ns * khz + 999999) / 1000000;
 }
 
-static inline ns_t cycles_to_ns(uint64_t cls, uint32_t khz) {
+static inline ns_t cycles_to_ns(cycle_t cls, uint32_t khz) {
 	return (cls * 1000000) / khz;
 }
 
