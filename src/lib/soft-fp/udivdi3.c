464d0b647954050147cbf3865a0d2c2af1f81720
Anton Bondarev <anton.bondarev2310@gmail.com>
2012-05-16 09:33:04 +0000

    lib: 64 bit divide function (__divdi3 and so on)

---
+++ src/lib/soft-fp/udivdi3.c
@@ -0,0 +1,33 @@
+/**
+ * @file
+ *
+ * @brief
+ *
+ * @date 16.05.2012
+ * @author Anton Bondarev
+ */
+#include <types.h>
+
+uint64_t __udivdi3(uint64_t num, uint64_t den) {
+	uint64_t result = 0;
+	int steps;
+	int i;
+
+	if (0 == den) {
+		return 0;
+	}
+	while (0x8000000000000000 != (den & 0x8000000000000000)) {
+		den <<= 1;
+		steps++;
+	}
+
+	for (i = 0; i <= steps; i++) {
+		result <<= 1;
+		if (num >= den) {
+			result += 1;
+			num -= den;
+		}
+		den >>= 1;
+	}
+	return result;
+}
