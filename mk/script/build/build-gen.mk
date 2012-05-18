2c48991c8a3837024a41f4f69ef38e859966198c
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-05-18 15:36:57 +0000

    Use true PHONY target in the build-gen script
    seems to work fine (at least for make -n)

--- mk/script/build/build-gen.mk
+++ mk/script/build/build-gen.mk
@@ -23,24 +23,33 @@ rule_notouch = \
 rule_notouch_stdout = \
 	$(call rule_notouch,$1 > $$TMPFILE,$2)
 
-build_modules := \
-	$(call get,$(build_model),modules)
-build_sources := \
-	$(call get,$(build_modules),sources)
+#
+# Global artifacts.
+#
 
-all .PHONY : $(build_model:%=build-runtime-inject-c/%)
-all .PHONY : $(build_modules:%=module/%)
-all .PHONY : $(build_sources:%=source/%)
+build_model_targets := \
+	$(build_model:%=build-runtime-inject-c/%)
 
-$(build_model:%=build-runtime-inject-c/%) : build-runtime-inject-c/% :
+all .PHONY : $(build_model_targets)
+
+$(build_model_targets) : build-runtime-inject-c/% :
 	@$(call rule_notouch_stdout, \
 		$(MAKE) -f mk/script/depsinject.mk,$(SRC_DIR)/depsinject.c)
 
-$(build_modules:%=module/%) : module/% : module-h/%
+#
+# Per-module artifacts.
+#
 
-module-h/% :
+build_modules := \
+	$(call get,$(build_model),modules)
+module_targets := \
+	$(build_modules:%=module-h/%)
+
+all .PHONY : $(module_targets)
+
+$(module_targets) : module-h/% :
 	@$(call rule_notouch_stdout, \
-		$(MAKE) -f mk/script/h-module.mk,$(call module_h,$*))
+		$(MAKE) -f mk/script/h-genmodule.mk,$(call module_h,$*))
 module_h = \
 	$(call fqn_to_h,$(call get,$(call get,$1,type),qualifiedName))
 
@@ -49,18 +58,26 @@ fqn_to_h = $(SRC_DIR)/include/module/$(fqn_to_dir).h
 fqn_to_dir = $(subst .,/,$1)#< 'qualified.name' -> 'qualified/name'
 fqn_to_id = $(subst .,__,$1)#< 'qualified.name' -> 'qualified__name'
 
-$(build_sources:%=source-/%) : source-/% : $$(source_prerequisites)
-source_prerequisites = \
-	$(foreach f,$(call get,$*,fileName), \
-		$(if $(filter %.c %.S,$f),source-cmd-cc/$*)  \
-		$(if $(filter %.lds.S,$f),source-cmd-cpp/$*))
+#
+# Per-source artifacts.
+#
 
-source-cmd-cc/% :
+build_sources := \
+	$(call get,$(build_modules),sources)
+source_targets := \
+	$(foreach s,$(build_sources), \
+		$(foreach f,$(call get,$s,fileName), \
+			$(if $(filter %.c %.S,$f),source-cmd-cc/$s)  \
+			$(if $(filter %.lds.S,$f),source-cmd-cpp/$s)))
+
+all .PHONY : $(source_targets)
+
+$(filter source-cmd-cc/%,$(source_targets)) : source-cmd-cc/% :
 	@$(foreach f,$(call get,$*,fileFullName),$(call rule_notouch_stdout, \
 		echo '$(call flags,$f) -o $(basename $f).o -c',$f.cmd))
 source-cmd-cc/% : flags = \
 	$(CPPFLAGS) $($(if $(filter %.c,$1),C,AS)FLAGS)
 
-source-cmd-cpp/% :
+$(filter source-cmd-cpp/%,$(source_targets)) : source-cmd-cpp/% :
 
 
