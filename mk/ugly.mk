f199e7615ca4f2fe343286dcc7eab8cb9984fea1
Anton Kozlov <drakon.mega@gmail.com>
2012-05-28 14:17:43 +0000

    mybuild: library modules sorted in their dependencies order in build

--- mk/ugly.mk
+++ mk/ugly.mk
@@ -79,8 +79,42 @@ define filter_abstract_modules
 					$(get s->qualifiedName))))
 endef
 
-filter_static_modules = \
-	$(strip $(foreach m,$1,$(if $(get $(get m->type).isStatic),$m)))
+
+# Performs topological sort of library modules.
+# Anonymous function performs sorting,
+#	1 arg: Black, visited, sorted vertexes, consists of topological sorted vertexes.
+#	2 arg: Grey vertexes, considering on this step.
+#	3 arg: White vertexes, have not reach those.
+# On each step:
+#	If there is no grey or white vertexes
+#		 print out $1. Exit.
+#	Else
+#		If there is grey vertexes,
+#			take first.
+#			If one has white vertexes as depends,
+#				move them in front of grey vertexes.
+#			Else
+#				If one has grey vertexes as depends,
+#					move it to end of grey vertexes list.
+#				Else move one in front of black vertexes.
+#		Else
+#			move first of white vertexes to grey vertexes.
+#
+#	Take next step.
+#
+define filter_static_modules
+	$(with ,,$(strip $(foreach m,$1,$(if $(get $(get m->type).isStatic),$m))),
+		$(if $(or $2,$3),
+			$(if $2,
+				$(if $(filter $(get $(firstword $2).depends),$3),
+					$(call $0,$1,$(filter $(get $(firstword $2).depends),$3) $2,
+						$(filter-out $(get $(firstword $2).depends),$3)),
+					$(if $(filter $(get $(firstword $2).depends),$2),
+						$(call $0,$1,$(wordlist 2,$(words $2),$2) $(firstword $2),$3),
+						$(call $0,$(firstword $2) $1,$(wordlist 2,$(words $2),$2),$3))),
+				$(call $0,$1,$(firstword $3),$(wordlist 2,$(words $3),$3))),
+			$1))
+endef
 
 ROOTFS_LABEL := mybuild.lang.InitFS
 
