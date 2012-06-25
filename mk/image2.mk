4086f3b4d921a7e50eb2bdfb97a594eb8e370373
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-06-25 14:39:39 +0000

    almost all except for initfs.cpio generation works well

--- mk/image2.mk
+++ mk/image2.mk
@@ -3,6 +3,23 @@
 # Author: Eldar Abusalimov
 #
 
+.PHONY : all image
+all : image
+
+include mk/core/common.mk
+
+include $(MKGEN_DIR)/build.mk
+include mk/flags.mk # It must be included after a user-defined config.
+
+.SECONDEXPANSION:
+include $(MKGEN_DIR)/include.mk
+
+.SECONDARY:
+.DELETE_ON_ERROR:
+
+TARGET ?= embox$(if $(value PLATFORM),-$(PLATFORM))
+TARGET := $(TARGET)$(if $(value LOCALVERSION),-$(LOCALVERSION))
+
 IMAGE       = $(BIN_DIR)/image
 IMAGE_DIS   = $(IMAGE).dis
 IMAGE_BIN   = $(IMAGE).bin
@@ -11,33 +28,24 @@ IMAGE_SIZE  = $(IMAGE).size
 IMAGE_PIGGY = $(IMAGE).piggy
 
 image: $(IMAGE)
-image: $(IMAGE_DIS)
-image: $(IMAGE_BIN)
-image: $(IMAGE_SREC)
-image: $(IMAGE_SIZE)
-image: $(IMAGE_PIGGY)
+image: $(IMAGE_DIS) $(IMAGE_BIN) $(IMAGE_SREC) $(IMAGE_SIZE) $(IMAGE_PIGGY)
 
-CC      = $(CROSS_COMPILE)gcc
-CPP     = $(CC) -E
-CXX     = $(CROSS_COMPILE)g++
-AR      = $(CROSS_COMPILE)ar
-AS      = $(CROSS_COMPILE)as
-LD      = $(CROSS_COMPILE)ld
-OBJDUMP = $(CROSS_COMPILE)objdump
-OBJCOPY = $(CROSS_COMPILE)objcopy
-SIZE    = $(CROSS_COMPILE)size
+CROSS_COMPILE ?=
 
-include mk/flags.mk
-include $(MKGEN_DIR)/config.mk
-
-.SECONDEXPANSION:
-include $(MKGEN_DIR)/include.mk
-
+CC      := $(CROSS_COMPILE)gcc
+CPP     := $(CC) -E
+CXX     := $(CROSS_COMPILE)g++
+AR      := $(CROSS_COMPILE)ar
+AS      := $(CROSS_COMPILE)as
+LD      := $(CROSS_COMPILE)ld
+OBJDUMP := $(CROSS_COMPILE)objdump
+OBJCOPY := $(CROSS_COMPILE)objcopy
+SIZE    := $(CROSS_COMPILE)size
 
 # This must be expanded in a secondary expansion context.
 # 1. Pattern.
 target_rulemk = $(patsubst $(abspath $1),$(MKGEN_DIR)/%.rule.mk,$(abspath $@))
-common_prereqs := mk/image2.mk $(MKGEN_DIR)/config.mk
+common_prereqs := mk/image2.mk $(MKGEN_DIR)/build.mk
 
 VPATH = $(SRCGEN_DIR)
 
@@ -47,30 +55,33 @@ VPATH = $(SRCGEN_DIR)
 cc_prerequisites    = $(call target_rulemk,$(OBJ_DIR)/%.o) $(common_prereqs)
 
 $(OBJ_DIR)/%.o : $(ROOT_DIR)/%.c | $$(@D)/.
-	$(CC) $(CFLAGS) $(CPPFLAGS) -c -o $@ $<
+	$(CC) $(CFLAGS) $(CPPFLAGS) $(flags) -c -o $@ $<
 
 $(OBJ_DIR)/%.o : $(ROOT_DIR)/%.S | $$(@D)/.
-	$(CC) $(ASFLAGS) $(CPPFLAGS) -c -o $@ $<
+	$(CC) $(ASFLAGS) $(CPPFLAGS) $(flags) -c -o $@ $<
 
 $(OBJ_DIR)/%.o : $(ROOT_DIR)/%.cpp | $$(@D)/.
-	$(CC) $(CXXFLAGS) $(CPPFLAGS) -c -o $@ $<
+	$(CC) $(CXXFLAGS) $(CPPFLAGS) $(flags) -c -o $@ $<
 $(OBJ_DIR)/%.o : $(ROOT_DIR)/%.cxx | $$(@D)/.
-	$(CC) $(CXXFLAGS) $(CPPFLAGS) -c -o $@ $<
+	$(CC) $(CXXFLAGS) $(CPPFLAGS) $(flags) -c -o $@ $<
 
 cpp_prerequisites   = $(call target_rulemk,$(OBJ_DIR)/%)   $(common_prereqs)
 $(OBJ_DIR)/%.lds : $(ROOT_DIR)/%.lds.S | $$(@D)/.
-	$(CPP) -P -undef $(CPPFLAGS) -imacros $(AUTOCONF_DIR)/config.lds.h \
+	$(CPP) -P -undef $(CPPFLAGS) -imacros $(SRCGEN_DIR)/config.lds.h \
 		-MMD -MT $@ -MF $@.d -o $@ $<
 
 fmt_line = $(addprefix \$(\n)$(\t)$(\t),$1)
 
 ar_prerequisites    = $(call target_rulemk,$(OBJ_DIR)/%.a) $(ar_objs)
-$(OBJ_DIR)/%.a :
+$(OBJ_DIR)/%.a : | $$(@D)/.
 	$(AR) $(ARFLAGS) $@ $(call fmt_line,$(ar_objs))
 
-image_prerequisites = $(call target_rulemk,$(BIN_DIR)/%) $(ld_objs) $(ld_libs)
+image_prerequisites = $(call target_rulemk,$(BIN_DIR)/%) \
+	$(ld_scripts) $(ld_objs) $(ld_libs)
 $(IMAGE): | $$(@D)/.
-	$(LD) $(LDFLAGS) $(call fmt_line,$(ld_objs)) $(call fmt_line,$(ld_objs)) \
+	$(LD) $(LDFLAGS) $(call fmt_line,$(ld_scripts:%=-T%)) \
+	$(call fmt_line,$(ld_objs)) \
+	$(call fmt_line,$(ld_libs)) \
 	-Map $@.map \
 	-o   $@
 
