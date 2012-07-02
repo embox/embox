c57dcd338f14ecc88751a25f081f6d4b318ad2f9
Anton Kozlov <drakon.mega@gmail.com>
2012-07-02 09:09:36 +0000

    stm32 clock driver compiles

--- src/drivers/clock/systick_timer.c
+++ src/drivers/clock/systick_timer.c
@@ -31,24 +31,20 @@
 #define SCB_SHP_BASE ((unsigned int *) 0xe000ed18)
 #define SCB_SHP_PERIF_N 8
 
-static struct clock_source systick;
-
 void clock_handler(void) {
 	clock_tick_handler(SYSTICK_IRQ, NULL);
 }
 
-void clock_init(void) {
-	systick.flags = 1;
-	systick.resolution = 1000;
-	clock_source_register(&systick);
+static int this_init(void) {
+	return 0;
 }
 
-void clock_setup(useconds_t usec) {
+static int this_config(enum device_config cfg, void *param) {
 	int ticks_per_10ms = REG_LOAD(SYSTICK_CALIB) & 0xffffff;
 
 	REG_STORE(SYSTICK_CTRL, 0);
 
-	REG_STORE(SYSTICK_RELOAD, (ticks_per_10ms * usec)/10000 - 1);
+	REG_STORE(SYSTICK_RELOAD, ticks_per_10ms /10 - 1);
 
 	REG_STORE(SYSTICK_VAL, 0);
 
@@ -57,4 +53,28 @@ void clock_setup(useconds_t usec) {
 	REG_STORE(SYSTICK_CTRL, SYSTICK_ENABLE | SYSTICK_TICKINT |
 			SYSTICK_CLOCKINIT);
 
+	return 0;
 }
+
+static struct time_event_device this_event = {
+	.init = this_init,
+	.jiffies = 0,
+	.config = this_config ,
+	.resolution = 10,
+};
+
+
+static struct time_counter_device this_counter = {
+	.read = NULL,
+	.resolution = 1000000,
+};
+
+static struct clock_source this_clock_source = {
+	.name = "system_tick",
+	.event_device = &this_event,
+	.counter_device = &this_counter,
+	.read = clock_source_read,
+};
+
+CLOCK_SOURCE(&this_clock_source);
+
