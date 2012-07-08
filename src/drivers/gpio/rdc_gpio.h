e04ce13deab41db869b841d8cc253c84dcf8a4ba
Anton Bondarev <anton.bondarev2310@gmail.com>
2012-07-08 13:09:46 +0000

    bifferboard: rdc_gpio.h file

---
+++ src/drivers/gpio/rdc_gpio.h
@@ -0,0 +1,17 @@
+/**
+ * @file
+ *
+ * @brief
+ *
+ * @date 08.07.2012
+ * @author Anton Bondarev
+ */
+
+#ifndef RDC_GPIO_H_
+#define RDC_GPIO_H_
+
+typedef volatile unsigned long __gpio_mask_t;
+
+//typedef volatile unsigned int stm32_reg_t;
+
+#endif /* RDC_GPIO_H_ */
