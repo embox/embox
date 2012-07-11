54fc8979df75118f885ea0b3b251e336b7378c65
Ilia Vaprol <ki.stfu@gmail.com>
2012-07-11 14:42:11 +0000

    c++: Add new_heandler; new with bad_alloc exception

--- src/lib/c++/new.cpp
+++ src/lib/c++/new.cpp
@@ -7,15 +7,56 @@
 
 #include <new>
 
-const std::nothrow_t std::nothrow;
+const std::nothrow_t std::nothrow = { };
+
+static std::new_handler __new_handler = 0;
+
+std::new_handler std::set_new_handler(std::new_handler handler) throw() {
+	std::new_handler prev_handler = __new_handler;
+	__new_handler = handler;
+	return prev_handler;
+}
 
 // Implementation of new and delete operators for single object
 void* operator new(std::size_t size) throw(std::bad_alloc) {
-	return std::malloc(size);
+	void *ptr;
+
+	if (size == 0) { /* std::malloc(0) is not predictable */
+		size = 1;
+	}
+
+	while ((ptr = std::malloc(size)) == 0) {
+		std::new_handler handler = __new_handler;
+		if (handler == 0) {
+			throw std::bad_alloc();
+		}
+		handler();
+	}
+
+	return ptr;
 }
 
 void* operator new(std::size_t size, const std::nothrow_t& nothrow_const) throw() {
-	return 0;
+	void *ptr;
+
+	if (size == 0) { /* std::malloc(0) is not predictable */
+		size = 1;
+	}
+
+	while ((ptr = std::malloc(size)) == 0) {
+		std::new_handler handler = __new_handler;
+		if (handler == 0) {
+			return 0;
+		}
+		try {
+			handler();
+		}
+		catch (const std::bad_alloc&) {
+			return 0;
+		}
+	}
+
+	return ptr;
 }
 
 void operator delete(void* ptr) throw() {
