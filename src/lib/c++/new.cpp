c007d0bff691f0381d156e66d3efb97690ce2454
Ilia Vaprol <ki.stfu@gmail.com>
2012-07-11 10:37:46 +0000

    c++: more new operators; add nothrow constant

--- src/lib/c++/new.cpp
+++ src/lib/c++/new.cpp
@@ -7,21 +7,40 @@
 
 #include <new>
 
-void * operator new(std::size_t sz) {
-	return std::malloc(sz);
+const std::nothrow_t std::nothrow;
+
+// Implementation of new and delete operators for single object
+void* operator new(std::size_t size) {
+	return std::malloc(size);
 }
 
-void * operator new[](std::size_t sz) {
-//	return ::operator new(sz);
-	return std::malloc(sz);
+void* operator new(std::size_t size, const std::nothrow_t& nothrow_const) throw() {
+	return 0;
 }
 
-void operator delete(void* ptr) {
+void operator delete(void* ptr) throw() {
 	std::free(ptr);
 }
 
-void operator delete[](void* ptr) {
-//	return ::operator delete(ptr);
-	return std::free(ptr);
+void operator delete(void* ptr, const std::nothrow_t* nothrow_const) throw() {
+	::operator delete(ptr);
+}
+
+
+// Forwarding functions for array of objects
+void* operator new[](std::size_t size) {
+	return ::operator new(size);
+}
+
+void* operator new[](std::size_t size, const std::nothrow_t& nothrow_const) throw() {
+	return ::operator new(size, nothrow_const);
+}
+
+void operator delete[](void* ptr) throw() {
+	::operator delete(ptr);
+}
+
+void operator delete[](void* ptr, const std::nothrow_t* nothrow_const) throw() {
+	::operator delete(ptr, nothrow_const);
 }
 
