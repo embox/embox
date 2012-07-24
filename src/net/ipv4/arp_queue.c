1077a3d7c816639c569b11fdefabe97cc18d825b
Ilia Vaprol <ki.stfu@gmail.com>
2012-07-24 14:23:33 +0000

    net: Simplify arp_queue

--- src/net/ipv4/arp_queue.c
+++ src/net/ipv4/arp_queue.c
@@ -19,7 +19,6 @@
 #include <kernel/time/time_types.h>
 #include <kernel/time/ktime.h>
 #include <framework/mod/options.h>
-#include <util/list.h>
 #include <assert.h>
 
 EMBOX_UNIT_INIT(arp_queue_init);
@@ -27,7 +26,6 @@ EMBOX_UNIT_INIT(arp_queue_init);
 /**
  * Declaration module constants
  */
-#define ARP_QUEUE_AMOUNT_HOST     OPTION_GET(NUMBER, amount_host)
 #define ARP_QUEUE_AMOUNT_PACKAGE  OPTION_GET(NUMBER, amount_package)
 #define ARP_QUEUE_HTABLE_WIDTH    OPTION_GET(NUMBER, htable_width)
 #define ARP_QUEUE_PACKAGE_TIMEOUT OPTION_GET(NUMBER, package_timeout)
@@ -37,31 +35,22 @@ EMBOX_UNIT_INIT(arp_queue_init);
  * Packing for a sk_buff
  */
 struct arp_queue_item {
-	struct list_link lnk; /* for linking */
-	struct sk_buff *skb;  /* outgoing package */
-	uint32_t was_posted;  /* time in which he was queued */
-};
-
-/**
- * Wrapper for entity
- */
-struct arp_queue {
-	in_addr_t dest_ip; /* IP address of destanation host */
-	struct list items; /* list of pending packages (struct arp_queue_item) */
+	in_addr_t dest_ip;   /* IP address of destanation host */
+	struct sk_buff *skb; /* outgoing package */
+	uint32_t was_posted; /* time in which he was queued */
 };
 
 
 /**
  * Hashtable of awaiting packages
  * Structure:
- * 	 ip_addr => slist
+ * 	 ip_addr => arp_queue_item
  * ip_addr -- destonation IP address
- * slist   -- queue a waiting pakages
+ * arp_queue_item -- waiting pakage
  */
 static struct hashtable *arp_queue_table;
 
 /* Allocators */
-POOL_DEF(arp_queue_pool, struct arp_queue, ARP_QUEUE_AMOUNT_HOST);
 POOL_DEF(arp_queue_item_pool, struct arp_queue_item, ARP_QUEUE_AMOUNT_PACKAGE);
 
 /* Get time in milliseconds */
@@ -73,7 +62,6 @@ void arp_queue_process(struct sk_buff *arp_skb) {
 	int res;
 	uint32_t now, lifetime;
 	in_addr_t resolved_ip;
-	struct arp_queue *queue;
 	struct arp_queue_item *waiting_item;
 	struct event *sock_ready;
 
@@ -83,87 +71,62 @@ void arp_queue_process(struct sk_buff *arp_skb) {
 	now = get_msec();
 	resolved_ip = arp_skb->nh.arph->ar_sip;
 
-	queue = hashtable_get(arp_queue_table, &resolved_ip);
-	if (queue == NULL) {
-		return; /* no waiting packages for this host */
-	}
-
-	while (!list_is_empty(&queue->items)) {
-		waiting_item = list_first_element(&queue->items, struct arp_queue_item, lnk);
-		assert(waiting_item != NULL);
+	while ((waiting_item = hashtable_get(arp_queue_table, &resolved_ip)) != NULL) {
 		assert(waiting_item->skb != NULL);
+
 		/* calculate experience */
 		lifetime = now - waiting_item->was_posted;
 		if (lifetime >= ARP_QUEUE_PACKAGE_TIMEOUT) {
-			skb_free(waiting_item->skb); /* time is up! drop this package */
+			goto free_skb_and_item; /* time is up! drop this package */
 		}
-		else {
-			/* try to rebuild */
-			assert(waiting_item->skb->dev != NULL);
-			assert(waiting_item->skb->dev->header_ops != NULL);
-			assert(waiting_item->skb->dev->header_ops->rebuild != NULL);
-			res = waiting_item->skb->dev->header_ops->rebuild(waiting_item->skb);
-			if (res < 0) {
-				continue; /* we still have time, let's try later */
-			}
-			/* save final event */
-			assert(waiting_item->skb->sk != NULL);
-			sock_ready = &waiting_item->skb->sk->sock_is_ready;
-			/* try to xmit */
-			if (dev_queue_xmit(waiting_item->skb) == ENOERR) {
-				event_fire(sock_ready);
-			}
+
+		/* try to rebuild */
+		assert(waiting_item->skb->dev != NULL);
+		assert(waiting_item->skb->dev->header_ops != NULL);
+		assert(waiting_item->skb->dev->header_ops->rebuild != NULL);
+		res = waiting_item->skb->dev->header_ops->rebuild(waiting_item->skb);
+		if (res < 0) {
+			goto free_skb_and_item; /* XXX it's not normal */
 		}
+
+		/* save final event */
+		assert(waiting_item->skb->sk != NULL);
+		sock_ready = &waiting_item->skb->sk->sock_is_ready;
+
+		/* try to xmit */
+		if (dev_queue_xmit(waiting_item->skb) == ENOERR) {
+			event_fire(sock_ready);
+		}
+
 		/* free resourse */
-		list_unlink_element(waiting_item, lnk);
+		goto free_item;
+
+free_skb_and_item:
+		skb_free(waiting_item->skb);
+free_item:
+		hashtable_del(arp_queue_table, &resolved_ip);
 		pool_free(&arp_queue_item_pool, waiting_item);
 	}
-
-	res = hashtable_del(arp_queue_table, &resolved_ip);
-	assert(res == ENOERR);
-
-	pool_free(&arp_queue_pool, queue);
 }
 
 int arp_queue_add(struct sk_buff *skb) {
 	int res;
-	in_addr_t dest_ip;
-	struct arp_queue *queue;
 	struct arp_queue_item *waiting_item;
 
-	dest_ip = skb->nh.iph->daddr;
-
-	/* get packages queue corresponding this destonation host */
-	queue = hashtable_get(arp_queue_table, &dest_ip);
-	if (queue == NULL) {
-		/* create new queue if not this queue not found */
-		queue = (struct arp_queue *)pool_alloc(&arp_queue_pool);
-		if (queue == NULL) {
-			return -ENOMEM;
-		}
-		/* initialize queue */
-		queue->dest_ip = dest_ip;
-		list_init(&queue->items);
-		/* put to arp_queue_table */
-		res = hashtable_put(arp_queue_table, &queue->dest_ip, queue);
-		if (res < 0) {
-			pool_free(&arp_queue_pool, queue);
-			return res;
-		}
-	}
-
-	/* allocate new node */
 	waiting_item = (struct arp_queue_item *)pool_alloc(&arp_queue_item_pool);
 	if (waiting_item == NULL) {
-		return -ENOMEM; /* XXX We do not free `queue` even if it was just created */
+		return -ENOMEM;
 	}
 
 	waiting_item->was_posted = get_msec();
 	waiting_item->skb = skb;
-	list_link_init(&waiting_item->lnk);
+	waiting_item->dest_ip = skb->nh.iph->daddr;
 
-	/* and push to queue */
-	list_add_last_element(waiting_item, &queue->items, lnk);
+	res = hashtable_put(arp_queue_table, (void *)&waiting_item->dest_ip, (void *)waiting_item);
+	if (res < 0) {
+		pool_free(&arp_queue_item_pool, waiting_item);
+		return res;
+	}
 
 	return ENOERR;
 }
