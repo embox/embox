eea312c4c1652ca61b54ffeba30ead37544bbadd
Ilia Vaprol <ki.stfu@gmail.com>
2012-08-08 11:36:45 +0000

    stdio: make previous commit more pleasent

--- src/drivers/virtual/diag_device.c
+++ src/drivers/virtual/diag_device.c
@@ -14,7 +14,6 @@
 #include <fs/ioctl.h>
 
 static int nonblocking = 0;
-static int last_is_cr = 0;  /* Last symbol is '\r' (Carriage Return) */
 
 static void *diag_open(struct file_desc *desc, const char *mode);
 static int diag_close(struct file_desc *desc);
@@ -50,44 +49,50 @@ static int diag_close(struct file_desc *desc) {
 	return 0;
 }
 
+/**
+ * This used for save current new ch to buff
+ * and it's replace sequances \r\n, \r, \n to \n on the fly
+ * @return non-zero if symbol was saved, zero otherwise
+ */
+static int save_to_buf(char *save_to, char ch) {
+	static int last_is_cr = 0;
+
+	if (last_is_cr && (ch == '\n')) {
+		last_is_cr = 0;
+		return 0; /* resolve \r\n case */
+	}
+
+	last_is_cr = (ch == '\r');
+	ch = ((ch == '\r') ? '\n' : ch); /* resolve \r case */
+
+	/* Save to buffer */
+	*save_to = ch;
+
+	return 1;
+}
+
 static size_t diag_read(void *buf, size_t size, size_t count, void *file) {
-	char ch, *ch_buf = (char *) buf;
+	char *ch_buf = (char *) buf;
 	int n = count * size;
 
 	if (nonblocking) {
-		int i = 0;
-		while (i < n) {
+		for (int i = 0; i < n; ++i) {
 			if (!diag_has_symbol()) {
 				if (0 == i) {
 					return -EAGAIN;
 				}
 				return i;
 			}
-			/* Replace sequances \r\n, \r, \n to \n */
-			ch = diag_getc();
-			if (last_is_cr && (ch == '\n')) {
-				last_is_cr = 0;
-				continue; /* resolve \r\n case */
+			if (save_to_buf(ch_buf, diag_getc())) {
+				ch_buf++;
 			}
-			last_is_cr = (ch == '\r');
-			ch = ((ch == '\r') ? '\n' : ch); /* resolve \r case */
-			/* Save to buffer */
-			*(ch_buf++) = ch;
-			i++;
 		}
 	} else {
 		int i = n;
-		while (i --) {
-			/* Replace sequances \r\n, \r, \n to \n */
-			ch = diag_getc();
-			if (last_is_cr && (ch == '\n')) {
-				last_is_cr = 0;
-				continue; /* resolve \r\n case */
+		while (i--) {
+			if (save_to_buf(ch_buf, diag_getc())) {
+				ch_buf++;
 			}
-			last_is_cr = (ch == '\r');
-			ch = ((ch == '\r') ? '\n' : ch); /* resolve \r case */
-			/* Save to buffer */
-			*(ch_buf++) = ch;
 		}
 	}
 
