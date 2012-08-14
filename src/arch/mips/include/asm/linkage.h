040d6a5bd1347728535ba97216759e369203bda4
Anton Bondarev <anton.bondarev2310@gmail.com>
2012-08-14 14:04:13 +0000

    mips: exception's code refactoring

--- src/arch/mips/include/asm/linkage.h
+++ src/arch/mips/include/asm/linkage.h
@@ -8,51 +8,16 @@
 #ifndef MIPS_LINKAGE_H_
 #define MIPS_LINKAGE_H_
 
+#include <asm/asm.h>
+
 #ifdef __ASSEMBLER__
-
-
-
-#define PTR_ADD     add
-#define PTR_ADDU    addu
-#define PTR_ADDI    addi
-#define PTR_ADDIU   addiu
-#define PTR_SUB     sub
-#define PTR_SUBU    subu
-#define PTR_L       lw
-#define PTR_S       sw
-#define PTR_LA      la
-#define PTR_LI      li
-#define PTR_SLL     sll
-#define PTR_SLLV    sllv
-#define PTR_SRL     srl
-#define PTR_SRLV    srlv
-#define PTR_SRA     sra
-#define PTR_SRAV    srav
-
-#define LONG_ADD    add
-#define LONG_ADDU   addu
-#define LONG_ADDI   addi
-#define LONG_ADDIU  addiu
-#define LONG_SUB    sub
-#define LONG_SUBU   subu
-#define LONG_L      lw
-#define LONG_S      sw
-#define LONG_SLL    sll
-#define LONG_SLLV   sllv
-#define LONG_SRL    srl
-#define LONG_SRLV   srlv
-#define LONG_SRA    sra
-#define LONG_SRAV   srav
-
-#define LONG        .word
-
 
 #define C_ENTRY(name) .globl name; .align 2; name
 
 /*
  * LEAF - declare leaf routine
  */
-#define LEAF(symbol)                        \
+#define LEAF(symbol)                       \
 	.globl symbol;                         \
 	.align 2;                              \
 	.type  symbol, @function;              \
@@ -62,19 +27,19 @@ symbol:    .frame  $sp, 0, $ra
 /*
  * NESTED - declare nested routine entry point
  */
-#define NESTED(symbol, framesize, rpc)   \
+#define NESTED(symbol, framesize, rpc)    \
 	.globl symbol;                        \
 	.align 2;                             \
-	.type  symbol, @function;              \
-	.ent   symbol, 0;                       \
+	.type  symbol, @function;             \
+	.ent   symbol, 0;                     \
 symbol:    .frame $sp, framesize, rpc
 
 
 /*
  * END - mark end of function
  */
-#define END(function)                       \
-	.end  function;                         \
+#define END(function)                     \
+	.end  function;                       \
 	.size function, .-function
 
 #endif /* __ASEEMBLER__ */
