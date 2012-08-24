9c5ec3d8b687c42bf66b90267f79d170d798fedd
Anton Bulychev <iantonishko@gmail.com>
2012-08-24 11:17:37 +0000

    backtrace: SPARC implementation

---
+++ src/arch/sparc/profiler/backtrace/stackframe.c
@@ -0,0 +1,42 @@
+/**
+ * @file
+ * @brief TODO: brief
+ *
+ * @date 16 Aug, 2012
+ * @author Bulychev Anton
+ */
+
+#include <stdio.h>
+#include <asm/stack.h>
+
+struct stackframe {
+	void* fp;
+	void* pc;
+};
+
+// TODO: __builtin_extract_return_address!!!
+void stackframe_set_current(struct stackframe* f) {
+	f->fp = __builtin_frame_address(0);
+	f->pc = __builtin_return_address(0);
+}
+
+// TODO: May be add type of previous: through interruption or not?
+// TODO: __builtin_extract_return_address!!!
+int stackframe_set_prev(struct stackframe *f) {
+	extern void irq_handler_call_pointer(void);
+	extern void exception_handler_call_pointer(void);
+
+	struct stack_frame* sf = f->fp;
+	f->fp = sf->reg_window.fp;
+	if (f->fp == NULL) {
+		f->pc = NULL;
+		return 0;
+	}
+	f->pc = sf->reg_window.ret_pc;
+
+	return 1;
+}
+
+void stackframe_print(struct stackframe* f) {
+	printk("fp = 0x%p, pc = 0x%p  \n", f->fp, f->pc);
+}
