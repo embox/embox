e44bea3996c6ce327f7c036750cbb43ba7cd1bc5
Anton Bondarev <anton.bondarev2310@gmail.com>
2012-08-28 14:56:44 +0000

    Clean linker script for x86 architecture

---
+++ src/arch/x86/include/asm/linkage.h
@@ -0,0 +1,17 @@
+/*
+ * @file
+ *
+ * @date Aug 28, 2012
+ * @author: Anton Bondarev
+ */
+
+#ifndef LINKAGE_H_
+#define LINKAGE_H_
+
+#ifdef __ASSEMBLER__
+
+#define C_ENTRY(name) .globl name; .align 2; name
+
+#endif
+
+#endif /* LINKAGE_H_ */
