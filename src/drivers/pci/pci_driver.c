b36815918d811e5397acc7daf52224569d8c2812
Anton Kozlov <drakon.mega@gmail.com>
2012-09-13 12:00:32 +0000

    pci_driver initialize all driver dependencies before init call

--- src/drivers/pci/pci_driver.c
+++ src/drivers/pci/pci_driver.c
@@ -4,7 +4,9 @@
  * @date Sep 6, 2012
  * @author: Anton Bondarev
  */
+
 #include <drivers/pci.h>
+#include <framework/mod/api.h>
 
 ARRAY_SPREAD_DEF(const struct pci_driver *,
 		__pci_driver_registry);
@@ -22,13 +24,23 @@ static const struct pci_driver * pci_driver_find(uint16_t ven_id, uint16_t dev_i
 
 int pci_driver_load(struct pci_slot_dev *dev) {
 	const struct pci_driver *drv;
+	const struct mod *dep;
 
 	assert(dev != NULL);
 
 	drv = pci_driver_find(dev->vendor, dev->device);
+
 	if(NULL == drv) {
 		return -1;
 	}
+
+	/* Enable mod (and dependencies) without cyclic detection error generating.
+	 * This introduced since some driver can be inited before pci_driver_load, then next lines
+	 * just ensure that other dependecies of driver are satisfied */
+	mod_foreach_requires(dep, drv->mod) {
+		mod_enable_rec_safe(dep, true);
+	}
+
 	drv->init(dev);
 
 	return 0;
