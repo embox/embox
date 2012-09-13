78b9a57a719085c2cd66ebdcbee028b3827fb5ce
Anton Kozlov <drakon.mega@gmail.com>
2012-09-13 10:50:46 +0000

    pci_driver macros: fixed wrong expansion

--- src/include/drivers/pci_driver.h
+++ src/include/drivers/pci_driver.h
@@ -17,16 +17,21 @@ struct pci_driver {
 	uint16_t dev_id;
 };
 
-#define PCI_DRIVER(drv_name,init_func,vid,did)      \
+#define __PCI_DRIVER(drv_name,init_func,vid,did,driver_struct_nm)      \
 		extern const struct pci_driver *__pci_driver_registry[]; \
 		static int init_func(struct pci_slot_dev *pci_dev);      \
-		static const struct pci_driver drv_ ## vid ## _ ## pid = { \
+		static const struct pci_driver driver_struct_nm = { \
 			.init = init_func,                                   \
 			.name = "" drv_name,                                 \
 			.ven_id = vid,                                       \
 			.dev_id = did                                        \
 		};                                                       \
-		ARRAY_SPREAD_ADD(__pci_driver_registry, &drv_ ## vid ## _ ## pid)
+		ARRAY_SPREAD_ADD(__pci_driver_registry, &driver_struct_nm)
+
+/* Expands vid and did if it is macros */
+#define _PCI_DRIVER(drv_name,init_func,vid,did) __PCI_DRIVER(drv_name,init_func,vid,did,drv_ ## vid ## _ ## did)
+
+#define PCI_DRIVER(drv_name,init_func,vid,did) _PCI_DRIVER(drv_name,init_func,vid,did)
 
 extern int pci_driver_load(struct pci_slot_dev *dev);
 
