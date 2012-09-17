fe97423a90922221f373f611f969ed873a002b98
Anton Bulychev <iantonishko@gmail.com>
2012-09-17 14:18:07 +0000

    sched: priority based scheduler uses SCHED_TICK_INTERVAL from options

--- src/kernel/thread/sched_strategy/priority_based.c
+++ src/kernel/thread/sched_strategy/priority_based.c
@@ -20,14 +20,13 @@
 #include <util/array.h>
 #include <util/prioq.h>
 
+#include <framework/mod/options.h>
+
 #include <kernel/thread/api.h>
 #include <kernel/thread/sched.h>
 #include <kernel/thread/sched_strategy.h>
 #include <kernel/thread/state.h>
 #include <kernel/time/timer.h>
-
-/** Interval, what scheduler_tick is called in. */
-#define SCHED_TICK_INTERVAL 100
 
 static void sched_tick(sys_timer_t *timer, void *param);
 
@@ -57,7 +56,7 @@ void runq_init(struct runq *rq, struct thread *current, struct thread *idle) {
 
 	/* Initializing tick timer. */
 	/* TODO: Error if not set timer and timer close. */
-	if (timer_set(&tick_timer, TIMER_PERIODIC, SCHED_TICK_INTERVAL, sched_tick, NULL)) {
+	if (timer_set(&tick_timer, TIMER_PERIODIC, OPTION_GET(NUMBER, SCHED_TICK_INTERVAL), sched_tick, NULL)) {
 		return ;
 	}
 }
