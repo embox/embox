13bcfee02d7bd2f30b801d412c5fd20e4dec7b44
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-09-20 21:01:04 +0000

    oops, forgotten implementation file

--- src/util/bit_impl.h
+++ src/util/bit_impl.h
@@ -9,16 +9,16 @@
 #ifndef UTIL_BIT_IMPL_H_
 #define UTIL_BIT_IMPL_H_
 
+#include <assert.h>
+
 #define __bit_lsb_mask(n) ((0x1ul << (n)) - 1) /* n rightmost bits set. */
 #define __bit_msb_mask(n) (~(~0x0ul >> (n)))   /* n leftmost bits set. */
 
-/* Find first set. */
-static inline int bit_ffs(unsigned long x) {
-	int nr = 1;
+/* Count trailing zeroes. */
+static inline int bit_ctz(unsigned long x) {
+	int nr = 0;
 
-	if (!x) {
-		return 0;
-	}
+	assert(x);
 
 	/* Hope that compiler optimizes out the sizeof check. */
 	if (sizeof(x) == 8 && !(x & __bit_lsb_mask(32))) {
@@ -48,35 +48,33 @@ static inline int bit_ffs(unsigned long x) {
 	return nr;
 }
 
-/* Find last set. */
-static inline int bit_fls(unsigned long x) {
-	int nr = sizeof(x) * 8;
+/* Count leading zeroes. */
+static inline int bit_clz(unsigned long x) {
+	int nr = 0;
 
-	if (!x) {
-		return 0;
-	}
+	assert(x);
 
 	if (sizeof(x) == 8 && !(x & __bit_msb_mask(32))) {
 		x <<= sizeof(x) * 8 / 2; /* see bit_ffs */
-		nr -= 32;
+		nr += 32;
 	}
 	if (!(x & __bit_msb_mask(16))) {
 		x <<= 16;
-		nr -= 16;
+		nr += 16;
 	}
 	if (!(x & __bit_msb_mask(8))) {
 		x <<= 8;
-		nr -= 8;
+		nr += 8;
 	}
 	if (!(x & __bit_msb_mask(4))) {
 		x <<= 4;
-		nr -= 4;
+		nr += 4;
 	}
 	if (!(x & __bit_msb_mask(2))) {
 		x <<= 2;
-		nr -= 2;
+		nr += 2;
 	}
-	nr -= !(x & __bit_msb_mask(1));
+	nr += !(x & __bit_msb_mask(1));
 
 	return nr;
 }
