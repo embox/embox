a4ee846403af26af4df82b910f6a5f171e3646aa
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-09-20 21:00:33 +0000

    Add clz/ctz (count leading/trailing zeroes) functions
    Redefine ffs and flz through these functions.

--- src/include/util/bit.h
+++ src/include/util/bit.h
@@ -12,19 +12,50 @@
 #include __impl_x(util/bit_impl.h)
 
 /**
- * Find first (least significant) set bit.
+ * Count trailing (least significant) zero bits.
+ *
+ * @param x
+ *   Must not be 0.
+ * @return
+ *   The number of trailing zero bits in @c x.
+ *
  * @code
- *  bit_ffs(0x00000000ul) =  0
- *  bit_ffs(0x00000001ul) =  1
- *  bit_ffs(0xfffffffful) =  1
- *  bit_ffs(0x80000000ul) = 32
+ *  bit_ctz(0x00000001ul) =  0
+ *  bit_ctz(0xfffffffful) =  0
+ *  bit_ctz(0x80000000ul) = 31
+ *  bit_ctz(0) // error, undefined
  * @endcode
+ */
+extern int bit_ctz(unsigned long x);
+
+/**
+ * Find first (least significant) set bit.
+ *
  * @param x
  * @return
  *   One plus the index of the least significant set bit of @c x,
  *   or zero in case when @c x is zero too.
  */
-extern int bit_ffs(unsigned long x);
+static inline int bit_ffs(unsigned long x) {
+	return x ? (bit_ctz(x) + 1) : 0;
+}
+
+/**
+ * Count leading (most significant) zero bits.
+ *
+ * @param x
+ *   Must not be 0.
+ * @return
+ *   The number of leading zero bits in @c x.
+ *
+ * @code
+ *  bit_clz(0x00000001ul) = 31 // assuming the word is 32-bit width
+ *  bit_clz(0xfffffffful) =  0
+ *  bit_clz(0x80000000ul) =  0
+ *  bit_clz(0) // error, undefined
+ * @endcode
+ */
+extern int bit_clz(unsigned long x);
 
 /**
  * Find last (most significant) set bit.
@@ -39,6 +70,8 @@ extern int bit_ffs(unsigned long x);
  *   One plus the index of the most significant set bit of @c x,
  *   or zero if @c x is zero.
  */
-extern int bit_fls(unsigned long x);
+static inline int bit_fls(unsigned long x) {
+	return x ? (sizeof(x) * 8 - bit_clz(x)) : 0;
+}
 
 #endif /* UTIL_BIT_H_ */
