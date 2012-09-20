f01091264d247760b38c4989f93ff637f425c6be
Anton Kozlov <drakon.mega@gmail.com>
2012-09-20 08:34:30 +0000

    arm: Move to arm-none-eabi compiler, add necessary ARM eabi functions
    + forgotten start_script for stm added

--- src/arch/arm/lib/arm_eabi.c
+++ src/arch/arm/lib/arm_eabi.c
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief implementaton of arm eabi specs for long long operations
+ * @brief implementaton of ARM EABI
  *
  * @author  Anton Kozlov
  * @date    02.07.2012
@@ -12,6 +12,9 @@ extern uint64_t __udivdi3(uint64_t num, uint64_t den);
 extern uint64_t __umoddi3(uint64_t num, uint64_t den);
 extern uint64_t __divdi3(int64_t num, int64_t den);
 extern uint64_t __moddi3(int64_t num, int64_t den);
+
+extern int __divsi3(int op1, int op2);
+unsigned int __udivsi3(unsigned int op1, unsigned int op2);
 
 void __aeabi_uldivmod(uint64_t u, uint64_t v) {
 	uint64_t res;
@@ -43,6 +46,40 @@ void __aeabi_ldivmod(int64_t u, int64_t v) {
 		asm volatile(""
 			 : "+r"(r0), "+r"(r1), "+r"(r2),"+r"(r3)  // output
 			 : "r"(r0), "r"(r1), "r"(r2), "r"(r3));   // input
+		return ;//r0;
+	}
+}
+
+int __aeabi_idiv(int num, int denom) {
+	return __divsi3(num, denom);
+}
+
+unsigned int __aeabi_uidiv(unsigned int num, unsigned int denom) {
+	return __udivsi3(num, denom);
+}
+
+void __aeabi_uidivmod(unsigned int num, unsigned int denom) {
+	uint32_t res = __udivsi3(num, denom);
+	uint32_t mod = num - res * denom;
+	{
+		register uint32_t r0 __asm__ ("r0") = res;
+		register uint32_t r1 __asm__ ("r1") = mod;
+		asm volatile(""
+			 : "+r"(r0), "+r"(r1)
+			 : "r"(r0), "r"(r1));   // input
+		return ;//r0;
+	}
+}
+
+void __aeabi_idivmod(int num, int denom) {
+	int32_t res = __divsi3(num, denom);
+	int32_t mod = num - res * denom;
+	{
+		register int32_t r0 __asm__ ("r0") = res;
+		register int32_t r1 __asm__ ("r1") = mod;
+		asm volatile(""
+			 : "+r"(r0), "+r"(r1)
+			 : "r"(r0), "r"(r1));   // input
 		return ;//r0;
 	}
 }
