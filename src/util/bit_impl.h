a6e38c2ea8011108a6d9a229722ce6b107e33b6f
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2012-09-21 01:04:59 +0000

    Rewrite bit counting without using conditional branches

--- src/util/bit_impl.h
+++ src/util/bit_impl.h
@@ -11,39 +11,29 @@
 
 #include <assert.h>
 
-#define __bit_lsb_mask(n) ((0x1ul << (n)) - 1) /* n rightmost bits set. */
-#define __bit_msb_mask(n) (~(~0x0ul >> (n)))   /* n leftmost bits set. */
+#define __bit_lsb_mask(n) ((0x1ul << (n)) - 1) /* n rightmost bits. */
+#define __bit_msb_mask(n) (~(~0x0ul >> (n)))   /* n leftmost bits. */
 
 /* Count trailing zeroes. */
 static inline int bit_ctz(unsigned long x) {
 	int nr = 0;
+	int sh;
 
 	assert(x);
 
 	/* Hope that compiler optimizes out the sizeof check. */
-	if (sizeof(x) == 8 && !(x & __bit_lsb_mask(32))) {
+	if (sizeof(x) == 8) {
 		/* Suppress "shift count >= width of type" error in case
 		 * when sizeof(x) is NOT 8, i.e. when it is a dead code anyway. */
-		x >>= sizeof(x) * 8 / 2; /* guaranteed to be 32 */
-		nr += 32;
+		sh = !(x & __bit_lsb_mask(sizeof(x)*8/2)) << 5;
+		nr += sh; x >>= sh;
 	}
-	if (!(x & __bit_lsb_mask(16))) {
-		x >>= 16;
-		nr += 16;
-	}
-	if (!(x & __bit_lsb_mask(8))) {
-		x >>= 8;
-		nr += 8;
-	}
-	if (!(x & __bit_lsb_mask(4))) {
-		x >>= 4;
-		nr += 4;
-	}
-	if (!(x & __bit_lsb_mask(2))) {
-		x >>= 2;
-		nr += 2;
-	}
-	nr += !(x & __bit_lsb_mask(1));
+
+	sh = !(x & __bit_lsb_mask(1 << 4)) << 4; nr += sh; x >>= sh;
+	sh = !(x & __bit_lsb_mask(1 << 3)) << 3; nr += sh; x >>= sh;
+	sh = !(x & __bit_lsb_mask(1 << 2)) << 2; nr += sh; x >>= sh;
+	sh = !(x & __bit_lsb_mask(1 << 1)) << 1; nr += sh; x >>= sh;
+	sh = !(x & __bit_lsb_mask(1 << 0)) << 0; nr += sh;
 
 	return nr;
 }
@@ -51,30 +41,20 @@ static inline int bit_ctz(unsigned long x) {
 /* Count leading zeroes. */
 static inline int bit_clz(unsigned long x) {
 	int nr = 0;
+	int sh;
 
 	assert(x);
 
-	if (sizeof(x) == 8 && !(x & __bit_msb_mask(32))) {
-		x <<= sizeof(x) * 8 / 2; /* see bit_ffs */
-		nr += 32;
+	if (sizeof(x) == 8) {
+		sh = !(x & __bit_msb_mask(sizeof(x)*8/2)) << 5; /* see bit_ffs */
+		nr += sh; x <<= sh;
 	}
-	if (!(x & __bit_msb_mask(16))) {
-		x <<= 16;
-		nr += 16;
-	}
-	if (!(x & __bit_msb_mask(8))) {
-		x <<= 8;
-		nr += 8;
-	}
-	if (!(x & __bit_msb_mask(4))) {
-		x <<= 4;
-		nr += 4;
-	}
-	if (!(x & __bit_msb_mask(2))) {
-		x <<= 2;
-		nr += 2;
-	}
-	nr += !(x & __bit_msb_mask(1));
+
+	sh = !(x & __bit_msb_mask(1 << 4)) << 4; nr += sh; x <<= sh;
+	sh = !(x & __bit_msb_mask(1 << 3)) << 3; nr += sh; x <<= sh;
+	sh = !(x & __bit_msb_mask(1 << 2)) << 2; nr += sh; x <<= sh;
+	sh = !(x & __bit_msb_mask(1 << 1)) << 1; nr += sh; x <<= sh;
+	sh = !(x & __bit_msb_mask(1 << 0)) << 0; nr += sh;
 
 	return nr;
 }
