2a99fa845e0fcad48ca4cce2f7c4fae158696043
Anton Kozlov <drakon.mega@gmail.com>
2012-10-01 14:21:43 +0000

    mem & wmem cmds adresses > 0x80000000 fixed

--- src/cmds/mem.c
+++ src/cmds/mem.c
@@ -15,13 +15,13 @@
 
 EMBOX_CMD(exec);
 
-#define DEFAULT_LENGTH 0x100
+#define DEFAULT_LENGTH 0x4
 
 static void print_usage(void) {
 	printf("Usage: mem [-h] -a addr [-n bytes]\n");
 }
 
-static int parse_option(char *optarg, int opt, long int *number) {
+static int parse_option(char *optarg, int opt, long unsigned int *number) {
 	char *endptr;
 
 	if ((optarg == NULL) || (*optarg == '\0')) {
@@ -30,7 +30,7 @@ static int parse_option(char *optarg, int opt, long int *number) {
 		return -1;
 	}
 
-	*number = strtol(optarg, &endptr, 0);
+	*number = strtoul(optarg, &endptr, 0);
 	if (*endptr != '\0') {
 		printf("wmem -%c: invalid option: %s\n", opt, optarg);
 		print_usage();
@@ -43,21 +43,21 @@ static int parse_option(char *optarg, int opt, long int *number) {
 static int exec(int argc, char **argv) {
 	bool a_flag = false;
 	int opt, i;
-	unsigned int *address;
+	unsigned long int *address;
 	size_t length = DEFAULT_LENGTH;
 
 	getopt_init();
 	while (-1 != (opt = getopt(argc, argv, "a:n:h"))) {
 		switch (opt) {
 		case 'a':
-			if (0 != parse_option(optarg, opt, (long int *) &address)) {
+			if (0 != parse_option(optarg, opt, (unsigned long int *) &address)) {
 				return -1;
 			}
 			a_flag = true;
 			break;
 
 		case 'n':
-			if (0 != parse_option(optarg, opt, (long int *) &length)) {
+			if (0 != parse_option(optarg, opt, (unsigned long int *) &length)) {
 				return -1;
 			}
 			break;
@@ -77,15 +77,15 @@ static int exec(int argc, char **argv) {
 		return -1;
 	}
 
-	address = (unsigned int *) ((int) address & ~(sizeof(address) - 1));
-	length = (length + sizeof(address) - 1) / sizeof(address);
+	/*address = (unsigned int *) ((int) address & ~(sizeof(address) - 1));*/
+	/*length = (length + sizeof(address) - 1) / sizeof(address);*/
 	i = 0;
 	while (length--) {
 		if (i-- == 0) {
 			i = 3;
 			printf("\n0x%08x:\t", (unsigned int) address);
 		}
-		printf("0x%08x  ", *address++);
+		printf("0x%08lx  ", *address++);
 	}
 	printf("\n");
 
--- src/cmds/wmem.c
+++ src/cmds/wmem.c
@@ -22,7 +22,7 @@ static void print_usage(void) {
 	printf("Usage: wmem [-h] -a addr -v value\n");
 }
 
-static int parse_option(char *optarg, int opt, long int *number) {
+static int parse_option(char *optarg, int opt, long unsigned int *number) {
 	char *endptr;
 
 	if ((optarg == NULL) || (*optarg == '\0')) {
@@ -31,7 +31,7 @@ static int parse_option(char *optarg, int opt, long int *number) {
 		return -1;
 	}
 
-	*number = strtol(optarg, &endptr, 0);
+	*number = strtoul(optarg, &endptr, 0);
 	if (*endptr != '\0') {
 		printf("wmem -%c: invalid option: %s\n", opt, optarg);
 		print_usage();
@@ -51,14 +51,14 @@ static int exec(int argc, char **argv) {
 	while (-1 != (opt = getopt(argc, argv, "a:v:h"))) {
 		switch (opt) {
 		case 'a':
-			if (0 != parse_option(optarg, opt, (long int *) &address)) {
+			if (0 != parse_option(optarg, opt, (unsigned long int *) &address)) {
 				return -1;
 			}
 			a_flag = true;
 			break;
 
 		case 'v':
-			if (0 != parse_option(optarg, opt, (long int *) &value)) {
+			if (0 != parse_option(optarg, opt, (unsigned long int *) &value)) {
 				return -1;
 			}
 			v_flag = true;
