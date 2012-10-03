8adbc77f6a001d27dac383738e2a9c1aaca7dc9f
IvantsovSE <ivantsovse@gmail.com>
2012-10-03 13:38:15 +0000

    edit uname code

--- src/cmds/uname.c
+++ src/cmds/uname.c
@@ -14,7 +14,7 @@
 EMBOX_CMD(exec);
 
 static void print_usage(void) {
-	printf("Usage: uname [OPTION]...\n"); //[-h -a -r -s]\n");
+	printf("Usage: uname [OPTION]...\n");
 	printf("Print certain system information\n\n");
 	printf("\t-a\tprint all information\n");
 	printf("\t-s\tprint the kernel name\n");
@@ -30,10 +30,10 @@ static int exec(int argc, char **argv) {
 			print_usage();
 			return 0;
 		case 'a':
-			printf("Embox 0.3.2 %12s %9s", __DATE__, __TIME__);
+			printf("Embox %s %12s %9s", EMBOX_REVISION, __DATE__, __TIME__);
 			return 0;
 		case 'r':
-			printf("0.3.2");
+			printf("%s", EMBOX_REVISION);
 			return 0;
 		case 's':
 			printf("Embox");
@@ -43,11 +43,6 @@ static int exec(int argc, char **argv) {
 		}
 	}
 	printf("Usage: uname [-hars]\n");
-/*	printf("OS: Embox\n");
 
-	printf("Date: %12s\n", __DATE__);
-	printf("Time: %9s\n", __TIME__);
-	printf("Compiler: %s\n", __VERSION__);
-*/
 	return 0;
 }
