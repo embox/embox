1daa95511e0b1f4696b29275c5edea24155e30ce
IvantsovSE <ivantsovse@gmail.com>
2012-10-03 13:32:50 +0000

    Add options to uname command

--- src/cmds/uname.c
+++ src/cmds/uname.c
@@ -1,43 +1,53 @@
-/*
- * uname.c
+/**
+ * @file
  *
- *  Created on: Oct 1, 2012
- *      Author: Sergey
+ * @date Oct 1, 2012
+ * @author Sergey Ivantsov
  */
 
 
 #include <embox/cmd.h>
 #include <getopt.h>
 #include <stdio.h>
+#include <revision.h>
 
 EMBOX_CMD(exec);
 
 static void print_usage(void) {
-	printf("Usage: uname [-a]\n");
+	printf("Usage: uname [OPTION]...\n"); //[-h -a -r -s]\n");
+	printf("Print certain system information\n\n");
+	printf("\t-a\tprint all information\n");
+	printf("\t-s\tprint the kernel name\n");
+	printf("\t-r\tprint the kernel release\n");
 }
 
 static int exec(int argc, char **argv) {
-#ifdef CONFIG_SVN_REV
-	unsigned int rev = CONFIG_SVN_REV;
-#endif
 	int opt;
 	getopt_init();
-	while (-1 != (opt = getopt(argc, argv, "a"))) {
+	while (-1 != (opt = getopt(argc, argv, "hars"))) {
 		switch(opt) {
-		case 'a':
+		case 'h':
 			print_usage();
+			return 0;
+		case 'a':
+			printf("Embox 0.3.2 %12s %9s", __DATE__, __TIME__);
+			return 0;
+		case 'r':
+			printf("0.3.2");
+			return 0;
+		case 's':
+			printf("Embox");
 			return 0;
 		default:
 			return 0;
 		}
 	}
-	printf("OS: Embox");
-    printf("\n");
+	printf("Usage: uname [-hars]\n");
+/*	printf("OS: Embox\n");
+
 	printf("Date: %12s\n", __DATE__);
 	printf("Time: %9s\n", __TIME__);
 	printf("Compiler: %s\n", __VERSION__);
-#ifdef CONFIG_SVN_REV
-	printf("Revision: r%d\n", rev);
-#endif
+*/
 	return 0;
 }
