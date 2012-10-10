82d8d78239b845926518fcbd5c2b11cc3ee9ed7a
Anton Bulychev <iantonishko@gmail.com>
2012-10-10 15:30:01 +0000

    sys_exit syscall

--- src/arch/x86/kernel/syscall_table.c
+++ src/arch/x86/kernel/syscall_table.c
@@ -8,9 +8,15 @@
 
 #include <types.h>
 #include <unistd.h>
-
+#include <kernel/task.h>
+#include <kernel/irq.h>
 
 #define SYSCALL_NRS_TOTAL 5
+
+long sys_exit(int error_code) {
+	ipl_enable();
+	task_exit(NULL);
+}
 
 size_t sys_write(int fd, const void *buf, size_t nbyte) {
 	return write(fd, buf, nbyte);
@@ -18,7 +24,7 @@ size_t sys_write(int fd, const void *buf, size_t nbyte) {
 
 void *SYSCALL_TABLE[SYSCALL_NRS_TOTAL] = {
 	NULL,
-	NULL,
+	sys_exit,
 	NULL,
 	NULL,
 	sys_write
