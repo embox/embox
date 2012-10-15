f895471e56f51932ab4b37b2b1e936fb66193344
Alexandr Chernakov <alexandr.chernakov@gmail.com>
2012-10-15 12:20:15 +0000

    xwnd: .cproject corruption fixed

--- src/cmds/xwnd.c
+++ src/cmds/xwnd.c
@@ -13,58 +13,68 @@
 #include <xwnd/xwnd.h>
 #include <xwnd/bmp.h>
 #include <asm/io.h>
+#include <drivers/keyboard.h>
+#include <kernel/time/timer.h>
 
 void xwnd_draw_horiz_line (unsigned x, unsigned y, unsigned l, unsigned c) {
-	int i;
-	for (i = 0; i <= l; i++) {
-		vesa_put_pixel(x + i, y, c);
-	}
+        int i;
+        for (i = 0; i <= l; i++) {
+                vesa_put_pixel(x + i, y, c);
+        }
 }
 
 void xwnd_draw_vert_line (unsigned x, unsigned y, unsigned l, unsigned c) {
-	int i;
-	for (i = 0; i <= l; i++) {
-		vesa_put_pixel(x, y + i, c);
-	}
+        int i;
+        for (i = 0; i <= l; i++) {
+                vesa_put_pixel(x, y + i, c);
+        }
 }
 
 void xwnd_draw_window (struct xwnd_window * wnd) {
-	xwnd_draw_vert_line  (wnd->x, wnd->y, wnd->ht, 2);
-	xwnd_draw_horiz_line (wnd->x, wnd->y, wnd->wd, 2);
-	xwnd_draw_vert_line  (wnd->x + wnd->wd, wnd->y, wnd->ht, 2);
-	xwnd_draw_horiz_line (wnd->x, wnd->y + wnd->ht, wnd->wd, 2);
+        xwnd_draw_vert_line  (wnd->x, wnd->y, wnd->ht, 2);
+        xwnd_draw_horiz_line (wnd->x, wnd->y, wnd->wd, 2);
+        xwnd_draw_vert_line  (wnd->x + wnd->wd, wnd->y, wnd->ht, 2);
+        xwnd_draw_horiz_line (wnd->x, wnd->y + wnd->ht, wnd->wd, 2);
 }
 
 EMBOX_CMD(exec);
 
 static int exec (int argc, char ** argv) {
-	struct xwnd_bmp_image * img;
+        struct xwnd_bmp_image * img;
 
-	if (argc > 1) {
-		img = xwnd_bmp_load(argv[1]);
-		if (!img) {
-			printf ("Failed to load bmp, error code %d\n", xwnd_bmp_get_errno());
-			return 1;
-		}
+        if (argc > 1) {
+                img = xwnd_bmp_load(argv[1]);
+                if (!img) {
+                        printf ("Failed to load bmp, error code %d\n", xwnd_bmp_get_errno());
+                        return 1;
+                }
 
-		if (argc == 2) {
-			vesa_init_mode(VESA_MODE_DEFAULT);
-			vesa_clear_screen();
-			xwnd_bmp_draw(img);
-			xwnd_bmp_unload(img);
-			//vesa_quit_mode();
-			return 0;
-		}
-		if (argc > 2)
-		{
-			printf("w/h: %d/%d, bpp: %d, ERR: %d\n", img->width, img->height, img->bpp, xwnd_bmp_get_errno());
-			xwnd_bmp_unload(img);
-			return 0;
-		}
-	}
-	else {
-		printf("Need more arguments\n");
-		return 1;
-	}
-	return 1;
+                if (argc == 2) {
+                        vesa_init_mode(VESA_MODE_DEFAULT);
+                        vesa_clear_screen();
+                        xwnd_bmp_draw(img);
+                        xwnd_bmp_unload(img);
+                        while (1) {
+                                if (!keyboard_has_symbol()) {
+                                        usleep(100);
+                                }
+                                else if ('q' == keyboard_getc()) {
+                                        break;
+                                }
+                        }
+                        vesa_quit_mode();
+                        return 0;
+                }
+                if (argc > 2)
+                {
+                        printf("w/h: %d/%d, bpp: %d, ERR: %d\n", img->width, img->height, img->bpp, xwnd_bmp_get_errno());
+                        xwnd_bmp_unload(img);
+                        return 0;
+                }
+        }
+        else {
+                printf("Need more arguments\n");
+                return 1;
+        }
+        return 1;
 }
