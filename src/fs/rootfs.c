5d4fb928ce978b21d40e9aa202d3f912b9d3369f
Anton Bondarev <anton.bondarev2310@gmail.com>
2012-10-16 15:57:04 +0000

    rootfs not FS driver now

--- src/fs/rootfs.c
+++ src/fs/rootfs.c
@@ -19,25 +19,10 @@ EMBOX_UNIT_INIT(unit_init);
 
 static node_t *root_node;
 
-static int rootfs_init(void * par) {
-	return 0;
-}
 
 node_t *rootfs_get_node(void) {
 	assert(NULL != root_node);
 	return root_node;
-}
-
-static int rootfs_format(void *par) {
-	return 0;
-}
-
-static int rootfs_create(void *params) {
-	return 0;
-}
-
-static int rootfs_delete(const char *file_name) {
-	return 0;
 }
 
 static int rootfs_mount(void *par) {
@@ -51,28 +36,11 @@ static int rootfs_mount(void *par) {
 	return 0;
 }
 
-static fsop_desc_t rootfs_fsop = {
-	rootfs_init,
-	rootfs_format,
-	rootfs_mount,
-	rootfs_create,
-	rootfs_delete
-};
-
-static const fs_drv_t rootfs_drv = {
-	"rootfs",
-	NULL,
-	&rootfs_fsop
-};
-
-DECLARE_FILE_SYSTEM_DRIVER(rootfs_drv);
-
 static int unit_init(void) {
 	root_node = alloc_node("/");
 	if (root_node == NULL) {
 		return -ENOMEM;
 	}
-	root_node->fs_type = (struct fs_drv *)&rootfs_drv;
 	rootfs_mount(NULL);
 	return 0;
 }
