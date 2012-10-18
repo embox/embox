a4d6e22be3f3bc129bba2fc7401468fab873e7a0
Anton Kozlov <drakon.mega@gmail.com>
2012-10-18 16:52:20 +0000

    contex frame interface extended

--- src/arch/x86/kernel/context.c
+++ src/arch/x86/kernel/context.c
@@ -8,6 +8,8 @@
 #include <types.h>
 #include <hal/context.h>
 
+#include <string.h>
+
 void context_init(struct context *ctx, bool privileged) {
 }
 
@@ -20,12 +22,25 @@ void context_set_entry(struct context *ctx, void(*pc)(void)) {
 	ctx->eip = (uint32_t) pc;
 }
 
-void context_enter_frame(struct context *ctx, void (*pc)(void)) {
+void context_enter_frame(struct context *ctx, void *pc) {
 	void *stack = (uint32_t *) ctx->esp;
 
 	stack -= sizeof(uint32_t);
 	*(uint32_t *) stack = ctx->eip;
 
+	stack -= sizeof(uint32_t);
+	*(uint32_t *) stack = ctx->ebp;
+
+	ctx->ebp = (unsigned int) stack;
+
 	ctx->esp = ((uint32_t) stack);
 	context_set_entry(ctx, pc);
 }
+
+void context_push_stack(struct context *ctx, void *arg, size_t n) {
+	ctx->esp -= n;
+
+	memcpy((void *) ctx->esp, arg, n);
+
+}
+
