185214ce79322d2ca08d002e10ca2e2a1c56163d
Nikolay Korotkiy <sikmir@gmail.com>
2012-10-19 19:39:11 +0000

    Fix example cmd

--- src/cmds/example.c
+++ src/cmds/example.c
@@ -1,37 +1,35 @@
 /**
  * @file
- *
- * @brief
+ * @brief Example runner
  *
  * @date 20.08.2011
  * @author Anton Bondarev
- *
  */
+
 #include <stdio.h>
 #include <getopt.h>
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
-
 #include <framework/example/api.h>
 #include <embox/cmd.h>
 
-
 EMBOX_CMD(exec);
 
-static void print_usage(void) {
-	printf("Usage: example [-h] [-n <number> | <example_name>]");
+static void print_usage(const char *cmd) {
+	printf("Usage: %s [-h] [-n <number> | <example_name>]", cmd);
 }
 
 static void print_examples(void) {
 	const struct example *example;
-	int i = 0;
+	size_t i = 0;
 
 	example_foreach(example) {
-		if(0 == strcmp("", example_path(example))) {
+		if (0 == strcmp("", example_path(example))) {
 			printf("%3d. %s\n", ++i, example_name(example));
 		} else {
-			printf("%3d. %s.%s\n", ++i, example_path(example), example_name(example));
+			printf("%3d. %s.%s\n", ++i,
+				example_path(example), example_name(example));
 		}
 	}
 	printf("\nTotal examples: %d\n", i);
@@ -41,20 +39,20 @@ static int exec(int argc, char **argv) {
 	int opt, number;
 	const struct example *example;
 
-	number = 0;
+	number = -1;
 	getopt_init();
 	while (-1 != (opt = getopt(argc, argv, "n:h"))) {
 		switch (opt) {
-		case '?':
-		case 'h':
-			print_usage();
-			return ENOERR;
 		case 'n':
 			if (sscanf(optarg, "%d", &number) != 1) {
 				printf("Invalid a number of example\n");
 				return -EINVAL;
 			}
 			break;
+		case 'h':
+		case '?':
+			print_usage(argv[0]);
+			return ENOERR;
 		default:
 			return -EINVAL;
 		}
@@ -65,13 +63,12 @@ static int exec(int argc, char **argv) {
 		return ENOERR;
 	}
 
-	if (number == 0) {
+	if (number == -1) {
 		example = example_lookup(argv[1]);
 		if (example == NULL) {
 			printf("Example \"%s\" not found\n", argv[1]);
 		}
-	}
-	else {
+	} else {
 		example_foreach(example) {
 			if (--number == 0) {
 				break;
