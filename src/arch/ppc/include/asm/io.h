3a2c962562a03bd6443831809d36542c585ba553
Anton Bondarev <anton.bondarev2310@gmail.com>
2012-10-19 14:08:36 +0000

    ppc: diag output work on qemu for virtex-ml507 platform

---
+++ src/arch/ppc/include/asm/io.h
@@ -0,0 +1,26 @@
+/*
+ * @file
+ *
+ * @date Oct 18, 2012
+ * @author: Anton Bondarev
+ */
+
+#ifndef PPC_IO_H_
+#define PPC_IO_H_
+
+#include <types.h>
+
+//FIXME BAD BAD BAD (this is for only serial diag 8250 on PPC architecture)
+
+#define IO_OFFSET    0x83e01003
+
+static inline void out8(uint8_t v, uint32_t a) {
+	*((volatile uint8_t *)(((a - 0x3f8) << 2) + IO_OFFSET)) = (uint8_t)(v);
+}
+
+static inline uint8_t in8(uint32_t a) {
+	return *(volatile uint8_t *)(((a - 0x3f8) << 2) + IO_OFFSET);
+}
+
+
+#endif /* PPC_IO_H_ */
