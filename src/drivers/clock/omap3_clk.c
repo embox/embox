7e2769359cd1084cd533cda654d5a0b73a68ccbf
Anton Kozlov <drakon.mega@gmail.com>
2012-10-24 11:10:01 +0000

    omap3: clk shoot one time

--- src/drivers/clock/omap3_clk.c
+++ src/drivers/clock/omap3_clk.c
@@ -19,14 +19,57 @@
 
 EMBOX_UNIT_INIT(omap3_clk_init);
 
+#define GPTIMER1_BASE 0x48318000
+#define GPTIMER1_IRQ 37
+
+#define GPTIMER_POS_INC (GPTIMER1_BASE + 0x48)
+#define GPTIMER_NEG_INC (GPTIMER1_BASE + 0x4c)
+#define GPTIMER_TWPS    (GPTIMER1_BASE + 0x34)
+#define GPTIMER_TLDR    (GPTIMER1_BASE + 0x2c)
+#define GPTIMER_TCRR    (GPTIMER1_BASE + 0x28)
+#define GPTIMER_TCLR    (GPTIMER1_BASE + 0x24)
+#define GPTIMER_TWER    (GPTIMER1_BASE + 0x20)
+#define GPTIMER_TIER    (GPTIMER1_BASE + 0x1c)
+#define GPTIMER_TISTAT  (GPTIMER1_BASE + 0x14)
+#define GPTIMER_CFG     (GPTIMER1_BASE + 0x10)
+
+#define GPTIMER_TCLR_START	(1 << 0)
+#define GPTIMER_TCLR_AUTORELOAD (1 << 1)
+
+#define GPTIMER_TIER_OVERFLOW   (1 << 1)
+
+#define CM_FCLKEN_WKUP  0x48004C00
+#define CM_ICLKEN_WKUP  0x48004C10
+
 extern int prom_printf(const char *, ...);
 static irq_return_t clock_handler(unsigned int irq_nr, void *data) {
-	prom_printf("clock_handler\n");
+	prom_printf("\t\tclock_handler\n");
 	clock_tick_handler(irq_nr, data);
 	return IRQ_HANDLED;
 }
 
 static int omap3_clk_config(struct time_dev_conf *conf) {
+
+	REG_STORE(CM_FCLKEN_WKUP, 1);
+	REG_STORE(CM_ICLKEN_WKUP, 1);
+
+	REG_STORE(GPTIMER_CFG, 0x2);
+
+	while (0 == REG_LOAD(GPTIMER_TISTAT));
+
+#if 0
+	REG_STORE(GPTIMER_POS_INC, 232000);
+	REG_STORE(GPTIMER_NEG_INC, -768000);
+#endif
+
+	REG_STORE(GPTIMER_TCRR, 0xffffffe0);
+	REG_STORE(GPTIMER_TLDR, 0xffffffe0);
+
+	REG_STORE(GPTIMER_TCLR, GPTIMER_TCLR_START | GPTIMER_TCLR_AUTORELOAD);
+
+	REG_STORE(GPTIMER_TIER, GPTIMER_TIER_OVERFLOW);
+	/*REG_STORE(GPTIMER_TWER, GPTIMER_TIER_OVERFLOW);*/
+
 	return 0;
 }
 
@@ -55,6 +98,6 @@ static struct clock_source omap3_clk_clock_source = {
 
 static int omap3_clk_init(void) {
 	clock_source_register(&omap3_clk_clock_source);
-	return irq_attach(1/*SYSTICK_IRQ*/, clock_handler, 0, NULL, "omap3_clk");
+	return irq_attach(GPTIMER1_IRQ, clock_handler, 0, NULL, "omap3_clk");
 }
 
