ca97ed7d037e7c82a42f90fcab92fad760e768be
Anton Bondarev <anton.bondarev2310@gmail.com>
2012-10-24 12:01:59 +0000

    Extract header from 8250 drivers (diag and serial)
    Delete unused header serial.h.

---
+++ src/include/drivers/serial/8250.h
@@ -0,0 +1,85 @@
+/**
+ * @file
+ *
+ * @date Oct 23, 2012
+ * @author: Anton Bondarev
+ */
+
+#ifndef SERIAL_8250_H_
+#define SERIAL_8250_H_
+
+
+/**
+ * UART registers
+ *           +---------------------+---------------------+
+ *           |      DLAB = 0       |       DLAB = 1      |
+ * +---------+---------+-----------+---------+-----------+
+ * |I/O port |  Read   |  Write    | Read    |Write      |
+ * +-------------------+-----------+---------+-----------+
+ * |base + 0 |  RBR    |  THR      |                     |
+ * |         |receiver |transmitter|DLL divisor latch LSB|
+ * |         | buffer  | holding   |                     |
+ * +---------+---------+-----------+---------------------+
+ * |base + 1 |  IER    |   IER     |                     |
+ * |         |interrupt|interrupt  |DLM divisor latch MSB|
+ * |         | enable  | enable    |                     |
+ * +---------+---------+-----------+---------+-----------+
+ * |base + 2 |  IIR    |  FCR      |  IIR    |  FCR      |
+ * |         |interrupt|  FIFO     |interrupt|  FIFO     |
+ * |         |identif. | control   |identif. | control   |
+ * +---------+---------+-----------+---------+-----------+
+ * |base + 3 |            LCR line control               |
+ * +---------+-------------------------------------------+
+ * |base + 4 |            MCR modem control              |
+ * +---------+---------+-----------+---------------------+
+ * |base + 5 |  LSR    |    -      |  LSR    |    -      |
+ * |         |  line   | factory   |  line   | factory   |
+ * |         | status  |   test    | status  |   test    |
+ * +---------+---------+-----------+---------------------+
+ * |base + 6 |  MSR    |    -      |  MSR    |    -      |
+ * |         |  modem  |   not     |  modem  |   not     |
+ * |         | status  |  used     | status  |  used     |
+ * +---------+---------+-----------+---------------------+
+ * |base + 7 |            SCR scratch                    |
+ * +---------+---------+-----------+---------------------+
+ */
+
+/* The offsets of UART registers */
+#define UART_TX             0x0
+#define UART_RX             0x0
+#define UART_DLL            0x0
+#define UART_IER            0x1
+#define UART_DLH            0x1
+#define UART_IIR            0x2
+#define UART_FCR            0x2
+#define UART_LCR            0x3
+#define UART_MCR            0x4
+#define UART_LSR            0x5
+#define UART_MSR            0x6
+#define UART_SR             0x7
+
+#define UART_DATA_READY     0x01
+#define UART_EMPTY_TX       0x20
+#define UART_ENABLE_FIFO    0xC7
+#define UART_ENABLE_MODEM   0x0B
+/* Divisor latch access bit */
+#define UART_DLAB           0x80
+
+/* The type of word length */
+#define UART_5BITS_WORD     0x00
+#define UART_6BITS_WORD     0x01
+#define UART_7BITS_WORD     0x02
+#define UART_8BITS_WORD     0x03
+
+/* The type of parity */
+#define UART_NO_PARITY      0x00
+#define UART_ODD_PARITY     0x08
+#define UART_EVEN_PARITY    0x18
+
+/* The type of the length of stop bit */
+#define UART_1_STOP_BIT     0x00
+#define UART_2_STOP_BITS    0x04
+
+#define DIVISOR(baud) (115200 / baud)
+
+#endif /* SERIAL_8250_H_ */
