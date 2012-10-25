95670d443a20e3fe095ced1fa8c677a2c8bc9825
Anton Kozlov <drakon.mega@gmail.com>
2012-10-25 12:00:56 +0000

    omap: context switch reworked to complete sometime

--- src/tests/hal/context/context_switch_test.c
+++ src/tests/hal/context/context_switch_test.c
@@ -9,29 +9,38 @@
 #include <hal/context.h>
 #include <embox/test.h>
 #include <stdio.h>
-
+#include <prom/prom_printf.h>
 #define STACK_SZ 0x10000
 
 EMBOX_TEST(run);
+
+#define TRACE(msg)
 
 static struct context entry_context, infinite_context, redundant_context;
 static char entry_stack[STACK_SZ];
 static char infinite_stack[STACK_SZ];
 
+static unsigned int mask = 0x55555555;
+
 static void entry(void) {
 	TRACE("entry begin\n");
-	while (true) {
-		TRACE("E");
+	while (mask) {
+		test_assert(mask & 1);
+		mask >>= 1;
+
 		context_switch(&entry_context, &infinite_context);
 	}
 	TRACE("entry end (should not be reached)\n");
 }
 
 static void infinite(void) {
-	while (true) {
-		TRACE("?");
+	while (mask) {
+		test_assert(0 == (mask & 1));
+		mask >>= 1;
+
 		context_switch(&infinite_context, &entry_context);
 	}
+	context_switch(&infinite_context, &redundant_context);
 }
 
 static int run(void) {
