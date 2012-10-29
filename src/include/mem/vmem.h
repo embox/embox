809629e3fc2b9e026f45d0f1900f477430492d6f
Anton Bulychev <iantonishko@gmail.com>
2012-10-29 11:56:38 +0000

    Add vmem_page_set_flags

--- src/include/mem/vmem.h
+++ src/include/mem/vmem.h
@@ -24,6 +24,8 @@ struct task_vmem_data {
 
 extern int vmem_map_region(mmu_ctx_t ctx, mmu_paddr_t phy_addr, mmu_vaddr_t virt_addr, size_t reg_size, vmem_page_flags_t flags);
 extern int vmem_create_space(mmu_ctx_t ctx, mmu_vaddr_t virt_addr, size_t reg_size, vmem_page_flags_t flags);
+extern int vmem_page_set_flags(mmu_ctx_t ctx, mmu_vaddr_t virt_addr, vmem_page_flags_t flags);
+
 //extern void vmem_create_virtual_space(mmu_ctx_t ctx);
 
 #endif /* MEM_VMEM_H_ */
