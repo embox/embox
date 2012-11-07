3f155caae03ca1f2a7e3bdc2236bc859b0ed0aaf
Anton Bulychev <iantonishko@gmail.com>
2012-11-07 09:45:09 +0000

    Work on syscalls

--- src/include/kernel/syscall_caller.h
+++ src/include/kernel/syscall_caller.h
@@ -11,4 +11,12 @@
 
 #include <module/embox/arch/syscall_caller.h>
 
+#define SYSCALL0     __SYSCALL0
+#define SYSCALL1     __SYSCALL1
+#define SYSCALL2     __SYSCALL2
+#define SYSCALL3     __SYSCALL3
+#define SYSCALL4     __SYSCALL4
+#define SYSCALL5     __SYSCALL5
+#define SYSCALL6     __SYSCALL6
+
 #endif /* KERNEL_SYSCALL_CALLER_H_ */
