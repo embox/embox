f53966acbcdcf2f48d9be57b02420df11648cacd
Anton Bulychev <iantonishko@gmail.com>
2012-11-16 12:00:39 +0000

    Add syscall example

--- src/kernel/syscall/syscall_table.c
+++ src/kernel/syscall/syscall_table.c
@@ -8,30 +8,44 @@
 
 #include <kernel/syscall.h>
 
-#define SYSCALL_NRS_TOTAL 6
+#define SYSCALL_NRS_TOTAL 8
 
-static int sys_0(void) {
+int sys_0(void) {
 	return 0;
 }
 
-static int sys_1(int arg1) {
+int sys_1(int arg1) {
 	return arg1;
 }
 
-static int sys_2(int arg1, int arg2) {
+int sys_2(int arg1, int arg2) {
 	return arg2;
 }
 
-static int sys_3(int arg1, int arg2, int arg3) {
+int sys_3(int arg1, int arg2, int arg3) {
 	return arg3;
 }
 
-static int sys_4(int arg1, int arg2, int arg3, int arg4) {
+int sys_4(int arg1, int arg2, int arg3, int arg4) {
 	return arg4;
 }
 
-static int sys_5(int arg1, int arg2, int arg3, int arg4, int arg5) {
+int sys_5(int arg1, int arg2, int arg3, int arg4, int arg5) {
 	return arg5;
+}
+
+#include <hal/ipl.h>
+#include <kernel/thread/api.h>
+
+clock_t sys_thread_running_time(void) {
+	ipl_enable();
+	return thread_get_running_time(thread_self());
+}
+
+int sys_thread_exit(void *r) {
+	ipl_enable();
+	thread_exit(r);
+	return 0;
 }
 
 void *const SYSCALL_TABLE[SYSCALL_NRS_TOTAL] = {
@@ -41,4 +55,6 @@ void *const SYSCALL_TABLE[SYSCALL_NRS_TOTAL] = {
 	sys_3,
 	sys_4,
 	sys_5,
+	sys_thread_running_time,
+	sys_thread_exit,
 };
