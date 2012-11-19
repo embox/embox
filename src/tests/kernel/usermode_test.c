72bdf96399f708e78b70e7c747ca3282ed965151
Anton Bulychev <iantonishko@gmail.com>
2012-11-19 22:51:38 +0000

    usermode: Add simple usermode test

--- src/tests/kernel/usermode_test.c
+++ src/tests/kernel/usermode_test.c
@@ -9,15 +9,22 @@
 #include <embox/test.h>
 
 #include <kernel/usermode.h>
+#include <kernel/syscall_caller.h>
 
 EMBOX_TEST_SUITE("usermode test");
 
-void usermode_function(void) {
+SYSCALL1(7,int,syscall_thread_exit,int,exitcode);
 
+void usermode_function(void) {
+	for (int i = 0; i < 100000; i++) {
+
+	}
+
+	syscall_thread_exit(0);
 }
 
 TEST_CASE("usermode") {
 	struct thread *t;
-	create_usermode_thread(&t, 0, usermode_function, (void *)0x20000000UL);
+	create_usermode_thread(&t, 0, usermode_function, (void *)0x200000UL);
 	thread_join(t, NULL);
 }
