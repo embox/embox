1820493203963a24228d1eafbb81d93bb8d4e72f
Ilia Vaprol <ki.stfu@gmail.com>
2012-11-22 11:08:04 +0000

    ppc: clock works (but with bad precision)

--- src/drivers/clock/ppc_clk.c
+++ src/drivers/clock/ppc_clk.c
@@ -13,10 +13,13 @@
 #include <hal/system.h>
 #include <kernel/irq.h>
 #include <kernel/time/clock_source.h>
+#include <asm/psr.h>
 
 #include <embox/unit.h>
 
 EMBOX_UNIT_INIT(ppc_clk_init);
+
+#define PPCCLK_IRQ 10
 
 static irq_return_t clock_handler(unsigned int irq_nr, void *data) {
 	__set_tsr(__get_tsr() & ~TSR_DIS);
@@ -29,19 +32,25 @@ static int ppc_clk_config(struct time_dev_conf *conf) {
 }
 
 static cycle_t ppc_clk_read(void) {
-	return 0;
+	uint32_t l, u, tmp;
+	tmp = __get_tbu();
+	do {
+		u = tmp;
+		l = __get_tbl();
+		tmp = __get_tbu();
+	} while (tmp != u);
+	return ((uint64_t)u << 32) | l;
 }
 
 static struct time_event_device ppc_clk_event = {
 	.config = ppc_clk_config,
-	.resolution = 100000000,
-	.irq_nr = 10/*GPTIMER1_IRQ*/,
+	.resolution = 1000000,
+	.irq_nr = PPCCLK_IRQ,
 };
-
 
 static struct time_counter_device ppc_clk_counter = {
 	.read = ppc_clk_read,
-//	.resolution = SYS_CLOCK / CLOCK_DIVIDER,
+	.resolution = 1000000000,
 };
 
 static struct clock_source ppc_clk_clock_source = {
@@ -52,10 +61,10 @@ static struct clock_source ppc_clk_clock_source = {
 };
 
 static int ppc_clk_init(void) {
-	__set_dec(1000000);
-	__set_decar(1000000);
+	__set_dec(1000);
+	__set_decar(1000);
     __set_tcr(TCR_DIE | TCR_ARE);
 	clock_source_register(&ppc_clk_clock_source);
-	return irq_attach(10/*GPTIMER1_IRQ*/, clock_handler, 0, NULL, "ppc_clk");
+	return irq_attach(PPCCLK_IRQ, clock_handler, 0, NULL, "ppc_clk");
 }
 
