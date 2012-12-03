9f117fb1c482b9ea59cc9cd29c1ea43f562f6a5b
Anton Bulychev <iantonishko@gmail.com>
2012-12-03 18:46:16 +0000

    Create generic MMU for RISC architectures

--- src/arch/sparc/mmu/mmu.c
+++ src/arch/sparc/mmu/mmu.c
@@ -10,13 +10,8 @@
 
 #include <hal/mmu.h>
 #include <asm/asi.h>
-
 #include <asm/mmu_consts.h>
 
-static mmu_pgd_t *context_table[0x100]  __attribute__((aligned(MMU_PAGE_SIZE)));
-static int ctx_counter = 0;
-
-/* ------------------ Managing MMU registers ----------------*/
 static inline void mmu_set_mmureg(unsigned long addr_reg,
 				unsigned long regval) {
 	__asm__ __volatile__(
@@ -94,38 +89,12 @@ static inline void mmu_on(void) {
  * swap instruction.  This insures the mmu and the cpu are in sync
  * with respect to ref/mod bits in the page tables.
  */
-static inline unsigned long mmu_set_val(void *addr, unsigned long value) {
+void mmu_set_val(void *addr, unsigned long value) {
 	__asm__ __volatile__(
 		"swap [%2], %0"
 		: "=&r" (value)
 		: "0" (value), "r" (addr)
 	);
-	return value;
-}
-
-/* ------------------------ Useful defines ----------------------- */
-
-#define mmu_set_ptd_entry(addr,ptr,flags)                                \
-	mmu_set_val((void *)(addr),                                          \
-			    ((((unsigned long)(ptr))>>4) & MMU_PTD_PMASK) | (flags))
-
-#define mmu_set_pte_entry(addr,ptr,flags)                                \
-	mmu_set_val((void *)(addr),                                          \
-			    ((((unsigned long)(ptr))>>4) & MMU_PTE_PMASK) | (flags))
-
-#define mmu_get_ptd_ptr(entry) \
-	((((unsigned long)(entry)) & MMU_PTD_PMASK) << 4)
-
-#define mmu_get_pte_ptr(entry) \
-	((((unsigned long)(entry)) & MMU_PTE_PMASK) << 4)
-
-
-/* ------------------ Functions which are external -------------- */
-
-mmu_ctx_t mmu_create_context(mmu_pgd_t *pgd) {
-	mmu_ctx_t ctx = (mmu_ctx_t) (++ctx_counter);
-	mmu_set_ptd_entry(&context_table[ctx], pgd, MMU_ET_PTD);
-	return ctx;
 }
 
 void mmu_set_context(mmu_ctx_t ctx) {
@@ -133,74 +102,14 @@ void mmu_set_context(mmu_ctx_t ctx) {
 	//mmu_flush_tlb_all();
 }
 
-mmu_pgd_t *mmu_get_root(mmu_ctx_t ctx) {
-	return (mmu_pgd_t *) mmu_get_ptd_ptr(context_table[ctx]);
-}
-
-/* Set functions */
-
-void mmu_pgd_set(mmu_pgd_t *pgd, mmu_pmd_t *pmd) {
-	mmu_set_ptd_entry(pgd, pmd, MMU_ET_PTD);
-}
-
-void mmu_pmd_set(mmu_pmd_t *pmd, mmu_pte_t *pte) {
-	mmu_set_ptd_entry(pmd, pte, MMU_ET_PTD);
-}
-
-void mmu_pte_set(mmu_pte_t *pte, mmu_paddr_t addr) {
-	mmu_set_pte_entry(pte, addr, MMU_PAGE_SOMEFLAG | MMU_ET_PTE);
-}
-
-/* Value functions */
-
-mmu_pmd_t *mmu_pgd_value(mmu_pgd_t *pgd) {
-	return (mmu_pmd_t *) mmu_get_ptd_ptr(*pgd);
-}
-
-mmu_pte_t *mmu_pmd_value(mmu_pmd_t * pmd) {
-	return (mmu_pte_t *) mmu_get_ptd_ptr(*pmd);
-}
-
-mmu_paddr_t mmu_pte_value(mmu_pte_t *pte) {
-	return (mmu_paddr_t) mmu_get_pte_ptr(*pte);
-}
-
-/* Present functions */
-
-int mmu_pgd_present(mmu_pgd_t *pgd) {
-	return ((unsigned int) *pgd) & MMU_ET_PRESENT;
-}
-
-int mmu_pmd_present(mmu_pmd_t *pmd) {
-	return ((unsigned int) *pmd) & MMU_ET_PRESENT;
-}
-
-int mmu_pte_present(mmu_pte_t *pte) {
-	return ((unsigned int) *pte) & MMU_ET_PRESENT;
-}
-
-/* Unset functions */
-void mmu_pgd_unset(mmu_pgd_t *pgd) {
-	mmu_set_ptd_entry(pgd, 0, 0);
-}
-
-void mmu_pmd_unset(mmu_pmd_t *pmd) {
-	mmu_set_ptd_entry(pmd, 0, 0);
-}
-
-void mmu_pte_unset(mmu_pte_t *pte) {
-	mmu_set_pte_entry(pte, 0, 0);
-}
-
-
-static uint32_t boot_pgd[0x100] __attribute__((aligned(0x1000)));
+static uint32_t boot_pgd[0x100] __attribute__((aligned(MMU_PAGE_SIZE)));
 
 #define PAGE_16MB    0x1000000UL
 
 void mmu_enable(void) {
-	mmu_set_ptd_entry(&context_table[0], boot_pgd, MMU_ET_PTD);
+	mmu_set_ptd_entry(&context_table[0], boot_pgd);
 
-	for (int i=0; i<0x100; i++) {
+	for (unsigned int i = 0; i < 0x100; i++) {
 		mmu_pte_set(&boot_pgd[i], i * PAGE_16MB);
 		mmu_pte_set_writable(&boot_pgd[i], 1);
 		mmu_pte_set_usermode(&boot_pgd[i], 0);
@@ -208,31 +117,4 @@ void mmu_enable(void) {
 	}
 
 	mmu_on();
-}
-
-/*
- * Page Table flags
- */
-void mmu_pte_set_writable(mmu_pte_t *pte, int val) {
-	if (val) {
-		*pte = *pte | MMU_PAGE_WRITABLE;
-	} else {
-		*pte = *pte & (~MMU_PAGE_WRITABLE);
-	}
-}
-
-void mmu_pte_set_usermode(mmu_pte_t *pte, int val) {
-	if (val) {
-		*pte = *pte & (~MMU_PAGE_SUPERVISOR);
-	} else {
-		*pte = *pte | MMU_PAGE_SUPERVISOR;
-	}
-}
-
-void mmu_pte_set_cacheable(mmu_pte_t *pte, int val) {
-	if (val) {
-		*pte = *pte | MMU_PAGE_CACHEABLE;
-	} else {
-		*pte = *pte & (~MMU_PAGE_CACHEABLE);
-	}
 }
