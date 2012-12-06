43f4ecd7a50411dc187d3917e6508e5948999eeb
Vita Loginova <vita.loginova@gmail.com>
2012-12-06 12:00:24 +0000

    servd: request parser test fix

--- src/tests/net/parser/request_parser_test.c
+++ src/tests/net/parser/request_parser_test.c
@@ -19,30 +19,28 @@ TEST_CASE("Parse first string") {
 	char *test_get2 = "HEAD /first_folder/second/index.php?p1+p2&p3=54; HTTP/1.1\r\nHost: 10.10.111.1:80";
 	char *test_get3 = "head /first_folder/second/index.php?p1+p2&p3=54; HTTP/1.1\r\nHost: 10.10.111.1:80";
 	char *test_get4 = "HEAD  HTTP/1.1\nHost: 10.10.111.1:80";
-	http_request *parsed_request;
+	http_request parsed_request;
 
-	parsed_request = parse_http(test_get);
-	test_assert_str_equal(parsed_request->method, "GET");
-	test_assert_str_equal(parsed_request->proto, "HTTP/1.1");
-	test_assert_str_equal(parsed_request->parsed_url->host, "wsdf-365.ru");
-	test_assert_str_equal(parsed_request->parsed_url->path, "first_folder/second/index.html");
-	test_assert_str_equal(parsed_request->parsed_url->fragment, "frag01");
-	free_http_request(parsed_request);
+	parse_http(test_get, &parsed_request);
+	test_assert_str_equal(parsed_request.method, "GET");
+	test_assert_str_equal(parsed_request.proto, "HTTP/1.1");
+	test_assert_str_equal(parsed_request.parsed_url->host, "wsdf-365.ru");
+	test_assert_str_equal(parsed_request.parsed_url->path, "first_folder/second/index.html");
+	test_assert_str_equal(parsed_request.parsed_url->fragment, "frag01");
+	free_http_request(&parsed_request);
 
-	parsed_request = parse_http(test_get2);
-	test_assert_str_equal(parsed_request->method, "HEAD");
-	test_assert_str_equal(parsed_request->proto, "HTTP/1.1");
-	test_assert_str_equal(parsed_request->parsed_url->host, "10.10.111.1");
-	test_assert_str_equal(parsed_request->parsed_url->path, "first_folder/second/index.php");
-	test_assert_str_equal(parsed_request->parsed_url->query, "p1+p2&p3=54;");
-	test_assert_str_equal(parsed_request->parsed_url->port, "80");
-	free_http_request(parsed_request);
+	parse_http(test_get2, &parsed_request);
+	test_assert_str_equal(parsed_request.method, "HEAD");
+	test_assert_str_equal(parsed_request.proto, "HTTP/1.1");
+	test_assert_str_equal(parsed_request.parsed_url->host, "10.10.111.1");
+	test_assert_str_equal(parsed_request.parsed_url->path, "first_folder/second/index.php");
+	test_assert_str_equal(parsed_request.parsed_url->query, "p1+p2&p3=54;");
+	test_assert_str_equal(parsed_request.parsed_url->port, "80");
+	free_http_request(&parsed_request);
 
-	parsed_request = parse_http(test_get3);
-	test_assert_null(parsed_request);
-	free_http_request(parsed_request);
+	test_assert_true(0 > parse_http(test_get3, &parsed_request));
+	free_http_request(&parsed_request);
 
-	parsed_request = parse_http(test_get4);
-	test_assert_null(parsed_request);
-	free_http_request(parsed_request);
+	test_assert_true(0 > parse_http(test_get4, &parsed_request));
+	free_http_request(&parsed_request);
 }
