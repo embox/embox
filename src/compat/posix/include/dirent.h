a3f07b5443b9288a85edc82162b12ac07298e5dc
Anton Bondarev <anton.bondarev2310@gmail.com>
2012-12-07 11:37:35 +0000

    fill dirent.h header

--- src/compat/posix/include/dirent.h
+++ src/compat/posix/include/dirent.h
@@ -7,5 +7,32 @@
 #ifndef DIRENT_H_
 #define DIRENT_H_
 
+#include <sys/types.h>
+
+
+struct dirent {
+	ino_t  d_ino;      /* File serial number. */
+	char   d_name[40]; /* Name of entry. */
+};
+
+typedef struct DIR {
+	struct dirent;
+} DIR;
+
+
+extern int            closedir(DIR *);
+
+extern DIR           *opendir(const char *);
+
+extern struct dirent *readdir(DIR *);
+
+extern int            readdir_r(DIR *restrict, struct dirent *restrict,
+                   struct dirent **restrict);
+
+extern void           rewinddir(DIR *);
+
+extern void           seekdir(DIR *, long);
+
+extern long           telldir(DIR *);
 
 #endif /* DIRENT_H_ */
