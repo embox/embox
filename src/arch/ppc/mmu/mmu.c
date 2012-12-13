068099738e0ea9900ec2e0dd0a862c1ed1b504dc
Ilia Vaprol <ki.stfu@gmail.com>
2012-12-13 12:11:18 +0000

    Add initial structure for ppc-mmu

---
+++ src/arch/ppc/mmu/mmu.c
@@ -0,0 +1,123 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 13.12.12
+ * @author Ilia Vaprol
+ */
+
+#include <asm/psr.h>
+#include <asm/regs.h>
+#include <hal/mmu.h>
+#include <types.h>
+
+#define MMU_PAGE_PRESENT        (1UL)
+#define MMU_PAGE_WRITABLE       (1UL << 6)
+#define MMU_PAGE_USERMODE       (1UL << 5)
+#define MMU_PAGE_DISABLE_CACHE  (1UL << 4)
+#define MMU_PAGE_4MB            (1UL << 3)
+#define MMU_PMD_FLAG            (MMU_PAGE_WRITABLE | MMU_PAGE_USERMODE)
+
+void mmu_on(void) {
+	__set_msr(__get_msr() | MSR_IS | MSR_DS);
+}
+
+void mmu_set_context(mmu_ctx_t ctx) {
+}
+
+mmu_ctx_t mmu_create_context(mmu_pgd_t *pgd) {
+	return 0;
+#if 0
+	mmu_ctx_t ctx = (mmu_ctx_t) (++ctx_counter);
+	ctx_table[ctx] = pgd;
+	return ctx;
+#endif
+}
+
+mmu_pgd_t *mmu_get_root(mmu_ctx_t ctx) {
+	return 0; /*ctx_table[ctx];*/
+}
+
+/* Present functions */
+
+int mmu_pgd_present(mmu_pgd_t *pgd) {
+	return 1;
+}
+
+int mmu_pmd_present(mmu_pmd_t *pmd) {
+	return ((uint32_t) *pmd & MMU_PAGE_PRESENT);
+}
+
+int mmu_pte_present(mmu_pte_t *pte) {
+	return ((uint32_t) *pte & MMU_PAGE_PRESENT);
+}
+
+/* Set functions */
+
+void mmu_pgd_set(mmu_pgd_t *pgd, mmu_pmd_t *pmd) {
+	return ;
+}
+
+void mmu_pmd_set(mmu_pmd_t *pmd, mmu_pmd_t *pte) {
+	*pmd = (mmu_pmd_t) ((((uint32_t) pte) & (~MMU_PAGE_MASK))
+			| MMU_PMD_FLAG | MMU_PAGE_PRESENT);
+}
+
+void mmu_pte_set(mmu_pgd_t *pte, mmu_paddr_t addr) {
+	*pte = (mmu_pte_t) ((((uint32_t) addr) & (~MMU_PAGE_MASK))
+			| MMU_PAGE_PRESENT);
+}
+
+/* Value functions */
+
+mmu_pmd_t *mmu_pgd_value(mmu_pgd_t *pgd) {
+	return (mmu_pmd_t *) pgd;
+}
+
+mmu_pte_t *mmu_pmd_value(mmu_pmd_t *pmd) {
+	return (mmu_pte_t *) ((*pmd) & (~MMU_PAGE_MASK));
+}
+
+mmu_paddr_t mmu_pte_value(mmu_pte_t *pte) {
+	return (mmu_paddr_t) ((*pte) & (~MMU_PAGE_MASK));
+}
+
+/* Unset functions */
+
+void mmu_pgd_unset(mmu_pgd_t *pgd) {
+	return ;
+}
+
+void mmu_pmd_unset(mmu_pmd_t *pmd) {
+	*pmd = 0;
+}
+
+void mmu_pte_unset(mmu_pgd_t *pte) {
+	*pte = 0;
+}
+
+/* Page Table flags */
+
+void mmu_pte_set_writable(mmu_pte_t *pte, int val) {
+	if (val) {
+		*pte = *pte | MMU_PAGE_WRITABLE;
+	} else {
+		*pte = *pte & (~MMU_PAGE_WRITABLE);
+	}
+}
+
+void mmu_pte_set_usermode(mmu_pte_t *pte, int val) {
+	if (val) {
+		*pte = *pte | MMU_PAGE_USERMODE;
+	} else {
+		*pte = *pte & (~MMU_PAGE_USERMODE);
+	}
+}
+
+void mmu_pte_set_cacheable(mmu_pte_t *pte, int val) {
+	if (val) {
+		*pte = *pte & (~MMU_PAGE_DISABLE_CACHE);
+	} else {
+		*pte = *pte | MMU_PAGE_DISABLE_CACHE;
+	}
+
---
+++ src/arch/ppc/mmu/mmu.h
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 13.12.12
+ * @author Ilia Vaprol
+ */
+
+#ifndef PPC_MMU_H_
+#define PPC_MMU_H_
+
+#include <types.h>
+
+#define __MMU_PGD_SHIFT         22
+#define __MMU_PMD_SHIFT         22
+#define __MMU_PTE_SHIFT         12
+
+typedef uint32_t __mmu_paddr_t;
+typedef uint32_t __mmu_vaddr_t;
+
+typedef uint32_t __mmu_ctx_t;
+
+typedef uint32_t __mmu_pgd_t;
+typedef uint32_t __mmu_pmd_t;
+typedef uint32_t __mmu_pte_t;
+
+#endif /* PPC_MMU_H_ */
