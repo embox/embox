862707e23e6ce751d87224e179111ac690fe303a
Felix Sulima <fsulima@gmail.com>
2012-12-28 12:39:14 +0000

    java: CLDC hello world application added

---
+++ third-party/phoneme/hello/src/embox/Hello.java
@@ -0,0 +1,22 @@
+package embox;
+
+public class Hello {
+	public static void main(final String args[]) {
+		System.out.println("Hello, world!");
+		if((null!=args) && (args.length!=0)) {
+			Thread thread = new Thread(new Runnable() {
+				public void run() {
+					for(int i=0; i<args.length; i++) {
+						System.out.println(">>> args[" + i + "]=" + args[i]);
+					}
+				}
+			});
+			thread.start();
+			try {
+				thread.join();
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		}
+	}
+}
