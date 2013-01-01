9d092c0d01405b64abbea2bf957fbde56d8312a9
Ilia Vaprol <ki.stfu@gmail.com>
2013-01-01 14:34:44 +0000

    Fix copysign; add info about endianness to powerpc

--- src/lib/math/copysign.c
+++ src/lib/math/copysign.c
@@ -9,22 +9,13 @@
 #include <math.h>
 
 double copysign(double x, double y) {
-    if (((x < 0) && (y > 0)) || ((x > 0) && (y < 0))) {
-        return -x;
-    }
-    return x;
+    return signbit(x) ^ signbit(y) ? -x : x;
 }
 
 float copysignf(float x, float y) {
-    if (((x < 0) && (y > 0)) || ((x > 0) && (y < 0))) {
-        return -x;
-    }
-    return x;
+    return signbitf(x) ^ signbitf(y) ? -x : x;
 }
 
 long double copysignl(long double x, long double y) {
-    if (((x < 0) && (y > 0)) || ((x > 0) && (y < 0))) {
-        return -x;
-    }
-    return x;
+    return signbitl(x) ^ signbitl(y) ? -x : x;
 }
