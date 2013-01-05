4206a630fda4878640b102701d9fc0ef552f90d1
Ilia Vaprol <ki.stfu@gmail.com>
2013-01-05 19:38:18 +0000

    templates: Add math_test; fix spaces

--- src/lib/math/signbit.c
+++ src/lib/math/signbit.c
@@ -10,19 +10,19 @@
 #include <math.h>
 
 int signbit(double x) {
-    union ieee754_double ieee_x;
-    ieee_x.val = x;
-    return ieee_x.ieee.negative;
+	union ieee754_double ieee_x;
+	ieee_x.val = x;
+	return ieee_x.ieee.negative;
 }
 
 int signbitf(float x) {
-    union ieee754_single ieee_x;
-    ieee_x.val = x;
-    return ieee_x.ieee.negative;
+	union ieee754_single ieee_x;
+	ieee_x.val = x;
+	return ieee_x.ieee.negative;
 }
 
 int signbitl(long double x) {
-    union ieee754_double ieee_x;
-    ieee_x.val = x; /* FIXME only if size of long double is 64bit */
-    return ieee_x.ieee.negative;
+	union ieee754_double ieee_x;
+	ieee_x.val = x; /* FIXME only if size of long double is 64bit */
+	return ieee_x.ieee.negative;
 }
--- src/tests/math/math_test.c
+++ src/tests/math/math_test.c
@@ -14,23 +14,23 @@ EMBOX_TEST_SUITE("math/common routines's tests");
 static const double values[] = { 0.0, 0.131321312312, 21.3, 2883321.0 };
 
 TEST_CASE("Test for signbit") {
-    size_t i;
+	size_t i;
 
-    /* signbit */
-    for (i = 0; i < sizeof values / sizeof values[0]; ++i) {
-        test_assert_zero(signbit(values[i]));
-        test_assert_not_zero(signbit(-values[i]));
-    }
+	/* signbit */
+	for (i = 0; i < sizeof values / sizeof values[0]; ++i) {
+		test_assert_zero(signbit(values[i]));
+		test_assert_not_zero(signbit(-values[i]));
+	}
 
-    /* signbitf */
-    for (i = 0; i < sizeof values / sizeof values[0]; ++i) {
-        test_assert_zero(signbitf((float)values[i]));
-        test_assert_not_zero(signbitf(-(float)values[i]));
-    }
+	/* signbitf */
+	for (i = 0; i < sizeof values / sizeof values[0]; ++i) {
+		test_assert_zero(signbitf((float)values[i]));
+		test_assert_not_zero(signbitf(-(float)values[i]));
+	}
 
-    /* signbitl */
-    for (i = 0; i < sizeof values / sizeof values[0]; ++i) {
-        test_assert_zero(signbitl((long double)values[i]));
-        test_assert_not_zero(signbitl(-(long double)values[i]));
-    }
+	/* signbitl */
+	for (i = 0; i < sizeof values / sizeof values[0]; ++i) {
+		test_assert_zero(signbitl((long double)values[i]));
+		test_assert_not_zero(signbitl(-(long double)values[i]));
+	}
 }
