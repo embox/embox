be5ca507a2cf9a529cf7e801aa52b0e2114b241a
Alex Kalmuk <alexkalmuk@gmail.com>
2013-01-16 09:54:58 +0000

    Remove mutex from javacall_event_receive

--- third-party/phoneme/javacall_midp_events.c
+++ third-party/phoneme/javacall_midp_events.c
@@ -45,31 +45,27 @@ javacall_result javacall_event_receive(
 	javacall_result res = JAVACALL_OK;
 	int fd_cnt = 0;
 
-	mutex_lock(&java_global_mutex);
-	{
-		FD_ZERO(&readfds);
-		FD_SET(java_pipe[0], &readfds);
+	FD_ZERO(&readfds);
+	FD_SET(java_pipe[0], &readfds);
 
-		timeout.tv_sec = timeToWaitInMillisec / 1000;
-		timeout.tv_usec = (timeToWaitInMillisec % 1000) * 1000;
+	timeout.tv_sec = timeToWaitInMillisec / 1000;
+	timeout.tv_usec = (timeToWaitInMillisec % 1000) * 1000;
 
-		fd_cnt = select(java_pipe[0] + 1, &readfds, NULL, NULL, &timeout);
+	fd_cnt = select(java_pipe[0] + 1, &readfds, NULL, NULL, &timeout);
 
-		if (fd_cnt != 1) {
-			res = JAVACALL_FAIL;
-			goto out;
-		}
-
-		read(java_pipe[0], &actual_size, 4);
-		if (actual_size > binaryBufferMaxLen) {
-			res = JAVACALL_OUT_OF_MEMORY;
-			actual_size = binaryBufferMaxLen;
-		}
-		*outEventLen = read(java_pipe[0], binaryBuffer, actual_size);
+	if (fd_cnt != 1) {
+		res = JAVACALL_FAIL;
+		goto out;
 	}
 
+	read(java_pipe[0], &actual_size, 4);
+	if (actual_size > binaryBufferMaxLen) {
+		res = JAVACALL_OUT_OF_MEMORY;
+		actual_size = binaryBufferMaxLen;
+	}
+	*outEventLen = read(java_pipe[0], binaryBuffer, actual_size);
+
 out:
-	mutex_unlock(&java_global_mutex);
 	return res;
 }
 
