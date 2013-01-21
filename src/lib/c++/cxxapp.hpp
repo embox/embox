7bec49c130953b6abfe1bf1dd768df18610d1323
Felix Sulima <fsulima@gmail.com>
2013-01-21 12:16:45 +0000

    c++: command to run C++ application

---
+++ src/lib/c++/cxxapp.hpp
@@ -0,0 +1,14 @@
+/*
+ * cxxapp.hpp
+ *
+ *  Created on: 21 janv. 2013
+ *      Author: fsulima
+ */
+
+#ifndef CXXAPP_HPP_
+#define CXXAPP_HPP_
+
+/* This function is supposed to be entry point of C++ application */
+extern int main (int argc, char **argv);
+
+#endif /* CXXAPP_HPP_ */
