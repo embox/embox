7c94ca728a3d9c472a4a876789431fe529b02d5e
Anton Bulychev <iantonishko@gmail.com>
2013-02-04 08:00:34 +0000

    smp: Some changes

--- src/drivers/interrupt/lapic.c
+++ src/drivers/interrupt/lapic.c
@@ -64,7 +64,44 @@ int lapic_enable(void) {
 	lapic_errstatus();
 
 #if 1
-	lapic_write(LAPIC_LVT_PCR, 4<<8);
+#define	APIC_APICID	 0x20
+#define	APIC_APICVER	 0x30
+#define	APIC_TASKPRIOR	 0x80
+#define	APIC_EOI	 0x0B0
+#define	APIC_LDR	 0x0D0
+#define	APIC_DFR	 0x0E0
+#define	APIC_SPURIOUS	 0x0F0
+#define	APIC_ESR	 0x280
+#define	APIC_ICRL	 0x300
+#define	APIC_ICRH	 0x310
+#define	APIC_LVT_TMR	 0x320
+#define	APIC_LVT_PERF	 0x340
+#define	APIC_LVT_LINT0	 0x350
+#define	APIC_LVT_LINT1	 0x360
+#define	APIC_LVT_ERR	 0x370
+#define	APIC_TMRINITCNT	 0x380
+#define	APIC_TMRCURRCNT	 0x390
+#define	APIC_TMRDIV	 0x3E0
+#define	APIC_LAST	 0x38F
+#define	APIC_DISABLE	 0x10000
+#define	APIC_SW_ENABLE	 0x100
+#define	APIC_CPUFOCUS	 0x200
+#define	APIC_NMI	 (4<<8)
+#define	TMR_PERIODIC	 0x20000
+#define	TMR_BASEDIV	 (1<<20)
+
+
+	lapic_write(LOCAL_APIC_DEF_ADDR+APIC_DFR, 0xFFFFFFFF);
+
+	val = lapic_read(LOCAL_APIC_DEF_ADDR + APIC_LDR);
+	val &= 0x00FFFFFF;
+	val |= 1;
+	lapic_write(LOCAL_APIC_DEF_ADDR+APIC_LDR, val);
+	lapic_write(LOCAL_APIC_DEF_ADDR+APIC_LVT_TMR, APIC_DISABLE);
+	lapic_write(LOCAL_APIC_DEF_ADDR+APIC_LVT_PERF, APIC_NMI);
+	lapic_write(LOCAL_APIC_DEF_ADDR+APIC_LVT_LINT0, APIC_DISABLE);
+	lapic_write(LOCAL_APIC_DEF_ADDR+APIC_LVT_LINT1, APIC_DISABLE);
+	lapic_write(LOCAL_APIC_DEF_ADDR + APIC_TASKPRIOR, 0);
 #endif
 
     /* Set the spurious interrupt vector register */
