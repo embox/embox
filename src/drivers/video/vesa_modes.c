76202eeb02719ccc1400515ce7ecc6f1d1baec0e
Ilia Vaprol <ki.stfu@gmail.com>
2013-02-04 15:32:23 +0000

    video: Add checking for exceeding the array bounds

--- src/drivers/video/vesa_modes.c
+++ src/drivers/video/vesa_modes.c
@@ -4,8 +4,11 @@
  * @date Jan 11, 2013
  * @author: Anton Bondarev
  */
+
+#include <assert.h>
 #include <types.h>
 #include <drivers/video/vesa.h>
+#include <util/array.h>
 
 static struct vesa_mode_desc vesa_mode_text_descriptions[] = {
 		{40,25,4},     /* VESA_MODE_TEXT_40x25x16_M */
@@ -58,19 +61,22 @@ static struct vesa_mode_desc vesa_mode_svga_descriptions[] = {
 
 
 struct vesa_mode_desc *vesa_mode_get_desc(enum vesa_video_mode mode) {
-	if(mode <= VESA_MODE_MDA_80x25) {
+	if (mode <= VESA_MODE_MDA_80x25) {
 		/* setup text mode */
+		assert(mode < ARRAY_SIZE(vesa_mode_text_descriptions));
 		return &vesa_mode_text_descriptions[mode];
 	}
 
-	if(mode >= VESA_MODE_EGA_320x200x16 && mode <= VESA_MODE_VGA_320x200x256) {
+	if (mode >= VESA_MODE_EGA_320x200x16 && mode <= VESA_MODE_VGA_320x200x256) {
 		/* setup text mode */
+		assert(mode - VESA_MODE_EGA_320x200x16 < ARRAY_SIZE(vesa_mode_vga_descriptions));
 		return &vesa_mode_vga_descriptions[mode - VESA_MODE_EGA_320x200x16];
 	}
 
-	if(mode >= VESA_MODE_SVGA_640x480x256 && mode <= VESA_MODE_SVGA_1280x1024x16M) {
+	if (mode >= VESA_MODE_SVGA_640x480x256 && mode <= VESA_MODE_SVGA_1280x1024x16M) {
+		assert(mode - VESA_MODE_SVGA_640x480x256 < ARRAY_SIZE(vesa_mode_svga_descriptions));
 		return &vesa_mode_svga_descriptions[mode - VESA_MODE_SVGA_640x480x256];
 	}
+
 	return NULL;
 }
-
