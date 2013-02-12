97f13c7f11874451bcffedab71fd0395aa258086
Alex Kalmuk <alexkalmuk@gmail.com>
2013-02-12 15:04:45 +0000

    fixes in dirent.c, rewrite readdir

--- src/compat/posix/dirent.c
+++ src/compat/posix/dirent.c
@@ -17,9 +17,9 @@
 static DLIST_DEFINE(dir_list);
 
 struct dir_info {
-	DIR dir;
-	node_t *node; /* node to increase speed when search directory */
-	struct dlist_head link; /* link to global list of directories */
+	DIR dir;                /**< corresponding directory */
+	node_t *node;           /**< current node of directory */
+	struct dlist_head link; /**< link to global list of dir_info structures */
 };
 
 OBJALLOC_DEF(__dir_pool, struct dir_info, MAX_DIR_QUANTITY);
@@ -28,7 +28,7 @@ DIR *opendir(const char *path) {
 	node_t *node;
 	struct dir_info *d;
 
-	if (NULL == (node = vfs_find_node(path, NULL))) {
+	if (NULL == (node = vfs_lookup(NULL, path))) {
 		SET_ERRNO(EBADF);
 		return NULL;
 	}
@@ -45,6 +45,8 @@ DIR *opendir(const char *path) {
 
 	dlist_head_init(&d->link);
 	dlist_add_prev(&d->link, &dir_list);
+	/* Initialization: set current node's name to empty string and
+	 * current node to current directory */
 	d->node = node;
 	d->dir.current.d_name[0] = '\0';
 
@@ -79,7 +81,7 @@ int closedir(DIR *dir) {
 
 struct dirent *readdir(DIR *dir) {
 	struct dir_info *d;
-	node_t *node;
+	struct node *node;
 	struct tree_link *link;
 
 	if (NULL == (d = get_dirinfo(dir))) {
@@ -87,18 +89,27 @@ struct dirent *readdir(DIR *dir) {
 		return NULL;
 	}
 
-	node = vfs_get_child(dir->current.d_name, d->node);
-	if (NULL == node) {
-		/* we are in directory's node, not in child */
-		node = d->node;
-	}
-
-	link = tree_children_next(&node->tree_link);
-	node = tree_element(link, struct node, tree_link);
-	if (node == d->node) {
-		/* last file in directory was reached on previous readdir()'s call */
+	/* We watched all nodes already. Simply return NULL. */
+	if (NULL == d->node) {
 		return NULL;
 	}
+
+	/* That means we are in directory's node, not in child. So go to first child. */
+	if (d->dir.current.d_name[0] == '\0') {
+		link = tree_children_begin(&d->node->tree_link);
+	} else {
+		/* Otherwise get next child. */
+		link = tree_children_next(&d->node->tree_link);
+	}
+	node = tree_element(link, struct node, tree_link);
+
+	/* If current node is last child */
+	if (&node->tree_link == tree_children_end(node->tree_link.par)) {
+		d->node = NULL;
+	} else {
+		d->node = node;
+	}
+
 	strcpy(dir->current.d_name, node->name);
 
 	return &dir->current;
