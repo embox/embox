7b3afb1a9d37d1faf93f317c7cc1faa364ba4c3c
Ivan Tretyakov <ivan.v.tretyakov@gmail.com>
2013-03-03 21:14:33 +0000

    #185: first version
    should be verified and tests should be written - this time it right
    branch

--- src/kernel/irq.c
+++ src/kernel/irq.c
@@ -28,10 +28,18 @@
 #include <hal/ipl.h>
 #include <mem/objalloc.h>
 #include <profiler/tracing/trace.h>
+#include <util/dlist.h>
 
-struct irq_action {
+struct irq_entry {
 	irq_handler_t handler;
 	void *dev_id;
+	struct dlist_head action_link;
+};
+
+struct irq_action {
+	struct irq_entry entry;
+	struct dlist_head entry_list;
+	int sharing_supported;
 };
 
 OBJALLOC_DEF(irq_actions, struct irq_action, IRQ_NRS_TOTAL);
@@ -51,8 +59,11 @@ int irq_attach(unsigned int irq_nr, irq_handler_t handler, unsigned int flags,
 
 	trace_point("irq attach");
 
-	if (irq_table[irq_nr]) {
-		/* IRQ sharing is not supported for now... */
+	/* Check if irq exists and device support sharing
+	 * and new device also support sharing */
+	if (irq_table[irq_nr] && (! (irq_table[irq_nr]->sharing_supported
+			&& (flags & IF_SHARESUP))) ) {
+		/* IRQ sharing is not supported for this IRQ number */
 		ret = -EBUSY;
 		goto out_unlock;
 	}
@@ -63,12 +74,22 @@ int irq_attach(unsigned int irq_nr, irq_handler_t handler, unsigned int flags,
 		goto out_unlock;
 	}
 
-	action->handler = handler;
-	action->dev_id = dev_id;
+	action->entry.handler = handler;
+	action->sharing_supported = (flags & IF_SHARESUP) ? 1 : 0;
+	action->entry.dev_id = dev_id;
 
-	irq_table[irq_nr] = action;
-
-	irqctrl_enable(irq_nr);
+	if (irq_table[irq_nr]) {
+		/* Add new device to list */
+		dlist_add_next(dlist_head_init(&(action->entry.action_link)),
+				&(irq_table[irq_nr]->entry_list));
+	} else {
+		/* Initialize list if it is first device on this IRQ line */
+		irq_table[irq_nr] = action;
+		dlist_init(&action->entry_list);
+		dlist_add_next(dlist_head_init(&(action->entry.action_link)),
+				&(irq_table[irq_nr]->entry_list));
+		irqctrl_enable(irq_nr);
+	}
 
 	out_unlock: irq_unlock();
 	return ret;
@@ -76,6 +97,7 @@ int irq_attach(unsigned int irq_nr, irq_handler_t handler, unsigned int flags,
 
 int irq_detach(unsigned int irq_nr, void *dev_id) {
 	struct irq_action *action;
+	struct dlist_head *item, *next;
 	int ret = ENOERR;
 
 	if (!irq_nr_valid(irq_nr)) {
@@ -86,25 +108,38 @@ int irq_detach(unsigned int irq_nr, void *dev_id) {
 
 	trace_point("irq detach");
 
-	if (!(action = irq_table[irq_nr]) || action->dev_id != dev_id) {
+	/* Go thru list to determine which IRQ/DEV pair should be detached */
+	if (!(action = irq_table[irq_nr])) {
 		ret = -ENOENT;
 		goto out_unlock;
 	}
 
-	objfree(&irq_actions, action);
-	irq_table[irq_nr] = NULL;
+	dlist_foreach(item, next, &(irq_table[irq_nr]->entry_list)) {
+		action = dlist_entry(item, struct irq_action, entry.action_link);
 
-	irqctrl_disable(irq_nr);
+		if (action->entry.dev_id == dev_id) {
+			dlist_del(&(action->entry.action_link));
+
+			if (dlist_empty(&(irq_table[irq_nr]->entry_list))) {
+				objfree(&irq_actions, action);
+				irq_table[irq_nr] = NULL;
+				irqctrl_disable(irq_nr);
+			} else {
+				objfree(&irq_actions, action);
+			}
+		}
+	}
 
 	out_unlock: irq_unlock();
 	return ret;
 }
 
 void irq_dispatch(unsigned int irq_nr) {
-	struct irq_action *action;
+	struct irq_entry *entry;
 	irq_handler_t handler = NULL;
 	void *dev_id = NULL;
 	ipl_t ipl;
+	struct dlist_head *item, *next;
 	TRACE_BLOCK_DEF(interrupt_tb);
 
 	assert(irq_nr_valid(irq_nr));
@@ -113,21 +148,22 @@ void irq_dispatch(unsigned int irq_nr) {
 
 	trace_block_enter(&interrupt_tb);
 
-	ipl = ipl_save();
-	{
-		if ((action = irq_table[irq_nr])) {
-			handler = action->handler;
-			dev_id = action->dev_id;
+	if (irq_table[irq_nr]) {
+		dlist_foreach(item, next, &(irq_table[irq_nr]->entry_list)) {
+			ipl = ipl_save();
+			{
+				if ((entry =
+						dlist_entry(item, struct irq_entry, action_link))) {
+					handler = entry->handler;
+					dev_id = entry->dev_id;
+				}
+			}
+			ipl_restore(ipl);
+
+			trace_block_leave(&interrupt_tb);
+
+			assert(handler != NULL);
+			handler(irq_nr, dev_id);
 		}
 	}
-	ipl_restore(ipl);
-
-	trace_block_leave(&interrupt_tb);
-
-	if (!action) {
-		return;
-	}
-
-	assert(handler != NULL);
-	handler(irq_nr, dev_id);
 }
