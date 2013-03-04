093c3d5584bd9420b6de02946039db47e251388c
Vita Loginova <vita.loginova@gmail.com>
2013-03-04 12:09:01 +0000

    tiny refactoring

--- src/drivers/tty/tty.c
+++ src/drivers/tty/tty.c
@@ -75,92 +75,99 @@ static inline void inc_line(struct tty *t) {
 	}
 }
 
+static void execute_printable(struct tty *t, char ch) {
+	switch (ch) {
+	case '\n':
+	case '\f': /* Form feed/clear screen*/
+		t->cur_x = 0;
+		inc_line(t);
+		break;
+	case '\r':
+		t->cur_x = 0;
+		break;
+	case '\t':
+		t->cur_x += 4;
+		if (t->cur_x >= t->width) {
+			inc_line(t);
+			t->cur_x -= t->width;
+		}
+		break;
+	case '\b': /* back space */
+		if (t->cur_x != 0)
+			--t->cur_x;
+		break;
+	default:
+		if (t->cur_x >= t->width) {
+			t->cur_x = 0;
+			inc_line(t);
+		}
+		t->ops->putc(t, ch, t->cur_x, t->cur_y);
+		++t->cur_x;
+		break;
+	}
+}
+
+static void execute_escapable(struct tty *t, char ch, short *params) {
+	switch (ch) {
+	case 'f': //Move cursor + blink cursor to location v,h
+	case 'H': //Move cursor to screen location v,h
+		setup_cursor(t, params[1], params[0]);
+		break;
+	case 'm': //color
+		break; //TODO
+	case 'X': //clear n characters
+		t->ops->clear(t, t->cur_x, t->cur_y, params[0], 1);
+		break;
+	case 'K': //Clear line from cursor right
+		switch (params[0]) {
+		default:
+		case 0:
+			t->ops->clear(t, t->cur_x, t->cur_y, t->width - t->cur_x, 1);
+			break;
+		case 1:
+			t->ops->clear(t, 0, t->cur_y, t->cur_x, 1);
+			break;
+		case 2:
+			t->ops->clear(t, 0, t->cur_y, t->width, 1);
+			break;
+		}
+		break;
+	case 'J': //Clear screen from cursor
+		switch (params[0]) {
+		default:
+		case 0:
+			t->ops->clear(t, 0, t->cur_y, t->width, t->height - t->cur_y);
+			break;
+		case 1:
+			t->ops->clear(t, 0, 0, t->width, t->cur_y);
+			break;
+		case 2:
+			t->ops->clear(t, 0, 0, t->width, t->height);
+			break;
+		}
+		break;
+	case 'D':
+		t->cur_x -= params[0];
+		break;
+	}
+}
+
 static void vtparse_callback(struct vtparse *parser, struct vt_token *token) {
 	struct tty *t = (struct tty *) parser->user_data;
+	char ch = parser->token.ch;
+
 	switch (parser->token.action) {
-		case VT_ACTION_PRINT:
-		case VT_ACTION_EXECUTE:
-			switch (parser->token.ch) {
-				case '\n':
-				case '\f': /* Form feed/clear screen*/
-					t->cur_x = 0;
-					inc_line(t);
-					break;
-				case '\r':
-					t->cur_x = 0;
-					break;
-				case '\t':
-					t->cur_x += 4;
-					if (t->cur_x >= t->width) {
-						inc_line(t);
-						t->cur_x -= t->width;
-					}
-					break;
-				case '\b': /* back space */
-					if (t->cur_x != 0)
-						--t->cur_x;
-					break;
-
-				default:
-					if (t->cur_x >= t->width) {
-						t->cur_x = 0;
-						inc_line(t);
-					}
-
-					t->ops->putc(t, parser->token.ch, t->cur_x, t->cur_y);
-					++t->cur_x;
-
-					break;
-				}
-			break;
-		case VT_ACTION_CSI_DISPATCH:
-		case VT_ACTION_ESC_DISPATCH:
-			switch (parser->token.ch) {
-					case 'f':  //Move cursor + blink cursor to location v,h
-					case 'H':  //Move cursor to screen location v,h
-						setup_cursor(t, parser->token.params[1], parser->token.params[0]);
-						break;
-					case 'm':  //color
-						break;  //TODO
-					case 'X':  //clear n characters
-						t->ops->clear(t, t->cur_x, t->cur_y, parser->token.params[0], 1);
-						break;
-					case 'K':  //Clear line from cursor right
-						switch (parser->token.params[0]) {
-						default:
-						case 0:
-							t->ops->clear(t, t->cur_x, t->cur_y, t->width - t->cur_x, 1);
-							break;
-						case 1:
-							t->ops->clear(t, 0, t->cur_y, t->cur_x, 1);
-							break;
-						case 2:
-							t->ops->clear(t, 0, t->cur_y, t->width, 1);
-							break;
-						}
-						break;
-					case 'J':  //Clear screen from cursor
-						switch (parser->token.params[0]) {
-						default:
-						case 0:
-							t->ops->clear(t, 0, t->cur_y, t->width, t->height - t->cur_y);
-							break;
-						case 1:
-							t->ops->clear(t, 0, 0, t->width, t->cur_y);
-							break;
-						case 2:
-							t->ops->clear(t, 0, 0, t->width, t->height);
-							break;
-						}
-						break;
-					case 'D':
-						t->cur_x -= parser->token.params[0];
-						break;
-					}
-			break;
-			default:
-				assert(0);
-				break;
+	case VT_ACTION_PRINT:
+	case VT_ACTION_EXECUTE:
+		execute_printable(t, ch);
+		break;
+	case VT_ACTION_CSI_DISPATCH:
+	case VT_ACTION_ESC_DISPATCH:
+		execute_escapable(t, ch, parser->token.params);
+		break;
+	default:
+		assert(0);
+		break;
 	}
 }
 
@@ -173,7 +180,6 @@ void tty_putc(struct tty *t, char ch) {
 	tty_cursor(t);
 }
 
-
 void tty_init(struct tty *t, uint32_t width, uint32_t height,
 		const struct tty_ops *ops, void *data) {
 	t->cur_x = 0;
@@ -183,15 +189,15 @@ void tty_init(struct tty *t, uint32_t width, uint32_t height,
 	t->ops = ops;
 	t->data = data;
 
+	assert(vtparse_init(&t->parser, NULL) != NULL);
+	t->parser.cb = vtparse_callback;
+	t->parser.user_data = t;
+
 	if (!ops || !ops->init) {
 		return;
 	}
 
 	t->ops->init(t);
-
-	assert(vtparse_init(&t->parser, NULL) != NULL);
-	t->parser.cb = vtparse_callback;
-	t->parser.user_data = t;
 
 	tty_clear(t);
 }
