7096181bef2275f4811fe6b137f34ed263768057
Anton Bulychev <iantonishko@gmail.com>
2013-03-06 14:47:09 +0000

    prioq2 queue is a hybrid of array and prioq now

--- src/kernel/thread/sched_strategy/queue/prioq2.c
+++ src/kernel/thread/sched_strategy/queue/prioq2.c
@@ -7,94 +7,72 @@
  */
 
 #include <util/dlist.h>
+#include <util/prioq.h>
 
 #include <kernel/thread.h>
 #include <kernel/thread/sched_strategy.h>
 
-static inline void queue_init(struct prior_queue *queue);
-static inline void queue_insert(struct prior_queue *queue, struct thread *thread);
-static inline void queue_remove(struct prior_queue *queue, struct thread *thread);
-static inline struct thread *queue_peek(struct prior_queue *queue);
-static inline struct thread *queue_extract(struct prior_queue *queue);
-static inline int queue_empty(struct prior_queue *queue);
-
 void sched_strategy_init(struct sched_strategy_data *s) {
 	dlist_head_init(&s->link);
+	prioq_link_init(&s->pq_link);
 }
+
+/* runq operations */
 
 void runq_queue_init(runq_queue_t *queue) {
-	queue_init(queue);
-}
-
-void runq_queue_insert(runq_queue_t *queue, struct thread *thread) {
-	queue_insert(queue, thread);
-}
-
-void runq_queue_remove(runq_queue_t *queue, struct thread *thread) {
-	queue_remove(queue, thread);
-}
-
-struct thread *runq_queue_extract(runq_queue_t *queue) {
-	return queue_extract(queue);
-}
-
-void sleepq_queue_init(sleepq_queue_t *queue) {
-	queue_init(queue);
-}
-
-void sleepq_queue_insert(sleepq_queue_t *queue, struct thread *thread) {
-	queue_insert(queue, thread);
-}
-
-void sleepq_queue_remove(sleepq_queue_t *queue, struct thread *thread) {
-	queue_remove(queue, thread);
-}
-
-struct thread *sleepq_queue_peek(sleepq_queue_t *queue) {
-	return queue_peek(queue);
-}
-
-int sleepq_queue_empty(sleepq_queue_t *queue) {
-	return queue_empty(queue);
-}
-
-/* QUEUE OPERATIONS */
-
-static inline void queue_init(struct prior_queue *queue) {
 	for (int i = THREAD_PRIORITY_MIN; i <= THREAD_PRIORITY_MAX; i++) {
 		dlist_init(&queue->list[i]);
 	}
 }
 
-static inline void queue_insert(struct prior_queue *queue, struct thread *thread) {
+void runq_queue_insert(runq_queue_t *queue, struct thread *thread) {
 	dlist_add_prev(&thread->sched.link, &queue->list[thread->priority]);
 }
 
-static inline void queue_remove(struct prior_queue *queue, struct thread *thread) {
+void runq_queue_remove(runq_queue_t *queue, struct thread *thread) {
 	dlist_del(&thread->sched.link);
 }
 
-static inline struct thread *queue_peek(struct prior_queue *queue) {
+struct thread *runq_queue_extract(runq_queue_t *queue) {
+	struct thread *thread = NULL;
+
 	for (int i = THREAD_PRIORITY_MAX; i >= THREAD_PRIORITY_MIN; i--) {
 		if (!dlist_empty(&queue->list[i])) {
-			return dlist_entry(queue->list[i].next, struct thread, sched.link);
+			thread = dlist_entry(queue->list[i].next, struct thread, sched.link);
+			runq_queue_remove(queue, thread);
+			break;
 		}
-	}
-
-	return NULL;
-}
-
-static inline struct thread *queue_extract(struct prior_queue *queue) {
-	struct thread *thread = queue_peek(queue);
-
-	if (thread) {
-		queue_remove(queue, thread);
 	}
 
 	return thread;
 }
 
-static inline int queue_empty(struct prior_queue *queue) {
-	return queue_peek(queue) == NULL;
+/* sleepq operations */
+
+static inline int thread_prio_comparator(struct prioq_link *first,
+		struct prioq_link *second) {
+	struct thread *t1 = prioq_element(first, struct thread, sched.pq_link);
+	struct thread *t2 = prioq_element(second, struct thread, sched.pq_link);
+	return t1->priority - t2->priority;
 }
 
+void sleepq_queue_init(sleepq_queue_t *queue) {
+	prioq_init(queue);
+}
+
+void sleepq_queue_insert(sleepq_queue_t *queue, struct thread *thread) {
+	prioq_enqueue(thread, thread_prio_comparator, queue, sched.pq_link);
+}
+
+void sleepq_queue_remove(sleepq_queue_t *queue, struct thread *thread) {
+	prioq_remove(thread, thread_prio_comparator, sched.pq_link);
+}
+
+struct thread *sleepq_queue_peek(sleepq_queue_t *queue) {
+	return prioq_peek(thread_prio_comparator, queue, struct thread,
+			sched.pq_link);
+}
+
+int sleepq_queue_empty(sleepq_queue_t *queue) {
+	return prioq_empty(queue);
+}
--- src/kernel/thread/sched_strategy/queue/prioq2.h
+++ src/kernel/thread/sched_strategy/queue/prioq2.h
@@ -10,19 +10,21 @@
 #define KERNEL_THREAD_QUEUE_PRIOQ_H_
 
 #include <util/dlist.h>
+#include <util/prioq.h>
 
 #include <kernel/thread/sched_priority.h>
 
-struct prior_queue {
+struct runq_queue {
 	struct dlist_head list[THREAD_PRIORITY_TOTAL];
 //	int mask[MASK_LEN];
 };
 
 struct sched_strategy_data {
-	struct dlist_head link;
+	struct dlist_head link;     /* If thread is in runq */
+	struct prioq_link pq_link;  /* If thread is in sleepq */
 };
 
-typedef struct prior_queue runq_queue_t;
-typedef struct prior_queue sleepq_queue_t;
+typedef struct runq_queue runq_queue_t;
+typedef struct prioq sleepq_queue_t;
 
 #endif /* KERNEL_THREAD_QUEUE_PRIOQ_H_ */
