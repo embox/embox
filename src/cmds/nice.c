f65926da2e3f690d42c3f7b7eef913a14c050eb1
Ilia Vaprol <ki.stfu@gmail.com>
2013-03-11 12:24:01 +0000

    cmd: Rewrite nice

--- src/cmds/nice.c
+++ src/cmds/nice.c
@@ -8,55 +8,57 @@
 
 #include <embox/cmd.h>
 #include <errno.h>
-#include <unistd.h>
+#include <sys/resource.h>
 #include <stdio.h>
 #include <framework/cmd/api.h>
+#include <string.h>
 
 EMBOX_CMD(exec);
 
-static int niceness = 0;
-
 static int exec(int argc, char **argv) {
-	int opt, n, i;
+	int incr, ind, prior;
 	const struct cmd *cmd;
 
+	prior = getpriority(PRIO_PROCESS, 0);
+	if (prior == -1) {
+		return -errno;
+	}
+
 	if (argc == 1) {
-		printf("%d\n", niceness);
+		printf("%d\n", prior);
 		return 0;
 	}
 
-	i = 1;
-	getopt_init();
-	while (-1 != (opt = getopt(argc, argv, "hn:"))
-			&& opt != '?') {
-		switch (opt) {
-		default:
-			return -EINVAL;
-		case 'h':
-			printf("Usage: nice [-n <N>] <command> [args]\n");
+	prior = 10;
+	for (ind = 1; ind < argc; ++ind) {
+		if (strcmp(argv[ind], "-h") == 0) {
+			printf("Usage: %s [-n increment] utility [argument...]\n", argv[0]);
 			return 0;
-		case 'n':
-			if (sscanf(optarg, "%d", &n) != 1) {
+		}
+		else if (strcmp(argv[ind], "-n") == 0) {
+			if ((++ind == argc) || (sscanf(argv[ind], "%d", &incr) != 1)) {
 				return -EINVAL;
 			}
-			i += 2;
+			prior += incr;
+		}
+		else {
 			break;
 		}
 	}
 
-	if (i >= argc) {
+	if (ind >= argc) {
 		return -EINVAL;
 	}
 
-	cmd = cmd_lookup(argv[i]);
+	cmd = cmd_lookup(argv[ind]);
 	if (cmd == NULL) {
-		printf("%s: %s: Command not found\n", argv[0], argv[i]);
+		printf("%s: %s: Command not found\n", argv[0], argv[ind]);
 		return -ENOENT;
 	}
 
-	if (nice(n + niceness) == -1) {
+	if (-1 == setpriority(PRIO_PROCESS, 0, prior)) {
 		return -errno;
 	}
 
-	return cmd_exec(cmd, argc - i, argv + i);
+	return cmd_exec(cmd, argc - ind, argv + ind);
 }
