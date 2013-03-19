697f0c9e414508e294b89e2f737e7a9dd0294583
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-03-19 12:16:41 +0000

    oops, revert memchr back, tnx to Vita for the catch

--- src/lib/string/memchr.c
+++ src/lib/string/memchr.c
@@ -9,12 +9,14 @@
 #include <string.h>
 
 void *memchr(const void *s, int c, size_t n) {
+	const unsigned char *src = (const unsigned char *) s;
 	unsigned char d = c;
 
 	while (n--) {
-		if (*s == d)
-			return (void *) s;
-		s++;
+		if (*src == d)
+			return (void *) src;
+		src++;
 	}
+
 	return NULL;
 }
