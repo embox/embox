bcc5da6ff3580168dba8a24ff11095df8e2476bf
Ilia Vaprol <ki.stfu@gmail.com>
2013-03-19 20:47:54 +0000

    lib: Fix fputs and revert getpass

--- src/compat/posix/util/getpass.c
+++ src/compat/posix/util/getpass.c
@@ -8,12 +8,11 @@
 
 #include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 
 char * getpass(const char *prompt) {
 	static char pass[PASS_MAX + 1];
 
-	fwrite(prompt, sizeof(char), strlen(prompt), stdout);
+	fprintf(stdout, "%s", prompt);
 
 	return fgets(&pass[0], PASS_MAX, stdin);
 }
