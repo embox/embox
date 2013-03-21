d2486e690a709e545364bb86d6edaf044849e17a
Felix Sulima <fsulima@gmail.com>
2013-03-21 14:34:15 +0000

    stl: Minor fix

---
+++ third-party/STLport/tree/stlport/stl/config/_embox.h
@@ -0,0 +1,19 @@
+/*
+ * _embox.h
+ *
+ *  Created on: 21 mars 2013
+ *      Author: fsulima
+ */
+
+#ifndef __STL_CONFIG_EMBOX_H_
+#define __STL_CONFIG_EMBOX_H_
+
+#define _STLP_PLATFORM "Embox"
+
+#define _STLP_USE_UNIX_IO
+
+#define _STLP_NO_WCHAR_T
+#define _STLP_NO_CWCHAR
+//#define _STLP_NO_LONG_DOUBLE
+
+#endif /* __STL_CONFIG_EMBOX_H_ */
