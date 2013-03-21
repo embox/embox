b6aa42d06695fbcfd83d0989ce85edfe2bcc9352
Anton Bondarev <anton.bondarev2310@gmail.com>
2013-03-21 12:43:19 +0000

    tty: Add nosched module

---
+++ src/kernel/thread/sched_strategy/nocontext.h
@@ -0,0 +1,15 @@
+/*
+ * @file
+ *
+ * @date Mar 21, 2013
+ * @author: Anton Bondarev
+ */
+
+#ifndef NOCONTEXT_H_
+#define NOCONTEXT_H_
+
+struct context {
+
+};
+
+#endif /* NOCONTEXT_H_ */
---
+++ src/kernel/thread/sched_strategy/nosched.h
@@ -0,0 +1,24 @@
+/*
+ * @file
+ *
+ * @date Mar 21, 2013
+ * @author: Anton Bondarev
+ */
+
+#ifndef NOSCHED_H_
+#define NOSCHED_H_
+
+
+struct runq {
+
+};
+
+struct sleepq {
+
+};
+
+struct sched_strategy_data {
+
+};
+
+#endif /* NOSCHED_H_ */
