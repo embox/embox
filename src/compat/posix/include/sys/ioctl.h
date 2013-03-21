1cf92da492e1a414820e67779f7a4d7376e00848
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-03-21 12:38:43 +0000

    Add IOCTL encoding/decoding macros

--- src/compat/posix/include/sys/ioctl.h
+++ src/compat/posix/include/sys/ioctl.h
@@ -5,6 +5,7 @@
  *
  * @date 14.09.2011
  * @author Anton Bondarev
+ * @author Eldar Abusalimov
  */
 
 #ifndef SYS_IOCTL_H_
@@ -12,20 +13,50 @@
 
 #include <sys/cdefs.h>
 
+/* Encoding IOCTL numbers */
+
+#define _IO(type, nr)         _IOC(_IOC_NONE, type, nr, 0)
+#define _IOR(type, nr, arg)   _IOC(_IOC_READ, type, nr, sizeof(arg))
+#define _IOW(type, nr, arg)   _IOC(_IOC_WRITE, type, nr, sizeof(arg))
+#define _IOWR(type, nr, arg)  _IOC(_IOC_READ|_IOC_WRITE, type, nr, sizeof(arg))
+
+/* Decoding */
+
+#define _IOC_DIR(ioc_nr)     (((ioc_nr) >> _IOC_DIRSHIFT)  & _IOC_DIRMASK)
+#define _IOC_TYPE(ioc_nr)    (((ioc_nr) >> _IOC_TYPESHIFT) & _IOC_TYPEMASK)
+#define _IOC_NR(ioc_nr)      (((ioc_nr) >> _IOC_NRSHIFT)   & _IOC_NRMASK)
+#define _IOC_SIZE(ioc_nr)    (((ioc_nr) >> _IOC_SIZESHIFT) & _IOC_SIZEMASK)
+
+/* Direction bits. */
+
+#define _IOC_NONE   0U
+#define _IOC_WRITE  1U
+#define _IOC_READ   2U
+
+/* internals... */
+
+#define _IOC_NRBITS     8
+#define _IOC_TYPEBITS   8
+#define _IOC_SIZEBITS   14
+#define _IOC_DIRBITS    2
+
+#define _IOC_NRMASK     ((0x1 << _IOC_NRBITS) - 1)
+#define _IOC_TYPEMASK   ((0x1 << _IOC_TYPEBITS) - 1)
+#define _IOC_SIZEMASK   ((0x1 << _IOC_SIZEBITS) - 1)
+#define _IOC_DIRMASK    ((0x1 << _IOC_DIRBITS) - 1)
+
+#define _IOC_NRSHIFT    0
+#define _IOC_TYPESHIFT  (_IOC_NRSHIFT + _IOC_NRBITS)
+#define _IOC_SIZESHIFT  (_IOC_TYPESHIFT + _IOC_TYPEBITS)
+#define _IOC_DIRSHIFT   (_IOC_SIZESHIFT + _IOC_SIZEBITS)
+
+#define _IOC(dir, type, nr, size) \
+	(((dir)  << _IOC_DIRSHIFT)  | \
+	 ((type) << _IOC_TYPESHIFT) | \
+	 ((nr)   << _IOC_NRSHIFT)   | \
+	 ((size) << _IOC_SIZESHIFT))
+
 __BEGIN_DECLS
-
-
-// TODO part of tty_ioctl, not termios -- Eldar
-struct winsize {
-	unsigned short ws_row;
-	unsigned short ws_col;
-	unsigned short ws_xpixel;   /* unused */
-	unsigned short ws_ypixel;   /* unused */
-};
-
-
-/* ioctl number for tty devices */
-#define	TIOCGWINSZ 0x1000
 
 /* Ioctls applicable to any descriptor */
 #define FIONBIO   0x00005421
