48bd54c670dcb4522668ebd30ccc6dac3f0e9e58
Andrey Gazukin <gazukin.andrey@gmail.com>
2013-03-22 06:46:09 +0000

    restore file_system.h

---
+++ src/include/fs/file_system.h
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 26.11.2012
+ * @author Andrey Gazukin
+ */
+
+#ifndef FILE_SYSTEM_H_
+#define FILE_SYSTEM_H_
+
+#include <util/dlist.h>
+#include <sys/stat.h>
+
+struct filesystem {
+	struct dlist_head link;
+	struct fs_driver *drv;    			/* file system driver */
+	struct block_dev *bdev;   			/* block device, where is this file system */
+	void             *fsi;    			/* extended information */
+	void             *rootdir_prev_fs;	/* root directory previous file system info */
+	void             *rootdir_prev_fi;	/* root directory previous file info */
+	char mntfrom[MAX_LENGTH_PATH_NAME];
+	char mntto[MAX_LENGTH_PATH_NAME];
+
+	const struct kfile_operations *file_op;
+};
+
+
+extern struct filesystem *filesystem_alloc(const char *fs_driver);
+extern void filesystem_free(struct filesystem *fs);
+extern int filesystem_get_mount_list(char *buff);
+
+#endif /* FILE_SYSTEM_H_ */
