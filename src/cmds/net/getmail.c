24bc41f6a44a4b301d01d401706727f6cc39cc6b
Ilia Vaprol <ki.stfu@gmail.com>
2013-03-25 17:56:22 +0000

    cmd: Fix bug in getmail

--- src/cmds/net/getmail.c
+++ src/cmds/net/getmail.c
@@ -27,7 +27,11 @@ static int exec(int argc, char **argv) {
 	}
 
 	ret = pop3_open(&p3s, argv[2], POP3_PORT);
-	if ((ret != 0) || !pop3_ok(&p3s)) goto error;
+	if (ret != 0) {
+		printf("%s: pop3_open failure\n", argv[0]);
+		return ret;
+	}
+	else if (!pop3_ok(&p3s)) goto error;
 
 	ret = pop3_user(&p3s, argv[1]);
 	if ((ret != 0) || !pop3_ok(&p3s)) goto error;
@@ -53,7 +57,7 @@ static int exec(int argc, char **argv) {
 error:
 	if (ret == 0) {
 		assert(!pop3_ok(&p3s));
-		printf("server error: %s\n", pop3_status(&p3s));
+		printf("%s: server error: %s\n", argv[0], pop3_status(&p3s));
 	}
 	pop3_close(&p3s);
 	return ret;
