fda6bc925e2b92f148827bfdaa590d479d10d92e
Felix Sulima <fsulima@gmail.com>
2013-03-27 11:00:37 +0000

    0mq: Add dependencies between external modules

---
+++ third-party/zeromq/order.mk
@@ -0,0 +1,4 @@
+
+$(OBJ_DIR)/third-party/zeromq/hwserver.o: $(OBJ_DIR)/third-party/zeromq/../../../../zeromq/lib/libzmq.a
+
+$(OBJ_DIR)/third-party/zeromq/../../../../zeromq/lib/libzmq.a: $(OBJ_DIR)/third-party/STLport/../../../../stlport/embox-lib/libstlportg.a
