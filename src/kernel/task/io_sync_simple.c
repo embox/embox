af1ec6789a2fc1f949d6e60696fd80e3ef094fcf
Anton Kozlov <drakon.mega@gmail.com>
2013-03-28 20:43:01 +0000

    io_sync_simple fixed signatures

--- src/kernel/task/io_sync_simple.c
+++ src/kernel/task/io_sync_simple.c
@@ -8,13 +8,13 @@
 
 #include <kernel/task/io_sync.h>
 
-void idx_io_enable(struct idx_desc *desc, int op) {
+void idx_io_enable(struct idx_desc_data *ddata, int op) {
 	softirq_lock();
-	assert(desc->data);
-	desc->data->io_state.io_ready |= op;
+	assert(ddata);
+	ddata->io_state.io_ready |= op;
 	softirq_unlock();
 }
 
-void idx_io_set_event(struct idx_desc *desc, struct event *event) {
+void idx_io_set_event(struct idx_desc_data *ddata, struct event *event) {
 	return; /* do nothing */
 }
