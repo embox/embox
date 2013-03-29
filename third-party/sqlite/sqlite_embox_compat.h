a3010fa9e69a761e84a3a9390e26f71a2ef57f17
Felix Sulima <fsulima@gmail.com>
2013-03-29 14:11:09 +0000

    sql: sqlite initial import

---
+++ third-party/sqlite/sqlite_embox_compat.h
@@ -0,0 +1,15 @@
+/*
+ * sqlite_embox_compat.h
+ *
+ *  Created on: 25 mars 2013
+ *      Author: fsulima
+ */
+
+#ifndef SQLITE_EMBOX_COMPAT_H_
+#define SQLITE_EMBOX_COMPAT_H_
+
+#ifdef HAVE_POSIX_FALLOCATE
+#undef HAVE_POSIX_FALLOCATE
+#endif
+
+#endif /* SQLITE_EMBOX_COMPAT_H_ */
