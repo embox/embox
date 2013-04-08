9a585786de78139d8a5a8d024fa63aba860a852b
Anton Bondarev <anton.bondarev2310@gmail.com>
2013-04-08 07:08:21 +0000

    Fix MIPS templates

--- src/kernel/thread/sched_none.c
+++ src/kernel/thread/sched_none.c
@@ -7,6 +7,7 @@
 
 struct sleepq;
 struct event;
+struct thread;
 
 void sched_wake_all(struct sleepq *sq) {
 
@@ -17,5 +18,9 @@ int sched_sleep(struct sleepq *sq, unsigned long timeout) {
 }
 
 int sched_sleep_locked(struct sleepq *sq, unsigned long timeout) {
+	return 0;
+}
+
+int sched_tryrun(struct thread *thread) {
 	return 0;
 }
