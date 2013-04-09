51f01884e4e302edb3639ca48a2949ca80f45227
Ilia Vaprol <ki.stfu@gmail.com>
2013-04-09 07:23:41 +0000

    nano: Use standard exit instead hack with setjmp

--- third-party/nano-2.2.6/embox.c
+++ third-party/nano-2.2.6/embox.c
@@ -10,25 +10,8 @@
 #include "embox.h"
 #include <assert.h>
 
-int nano_exit_ret;
-int nano_exited;
-jmp_buf nano_exit_point;
-
 EMBOX_CMD(exec);
 
 static int exec(int argc, char **argv) {
-	int ret;
-
-	nano_exit_ret = -1;
-	nano_exited = 0;
-
-	ret = setjmp(nano_exit_point);
-	if (ret == 0) {
-		return nano_main(argc, argv);
-	}
-
-	assert(ret == NANO_EXIT_JMP_VALUE);
-	assert(nano_exited);
-
-	return nano_exit_ret;
+	return nano_main(argc, argv);
 }
