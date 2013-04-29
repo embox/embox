e4f957190c773cd4749a67b91956c18b438df4c0
Anton Kozlov <drakon.mega@gmail.com>
2013-04-29 14:58:09 +0000

    profiler no_trace updated

---
+++ src/profiler/tracing/no_trace.c
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Kozlov
+ * @date    29.04.2013
+ */
+
+#include <stddef.h>
+
+#include <profiler/tracing/trace.h>
+
+int trace_block_diff(struct __trace_block *tb) {
+	return -1;
+}
+
+int trace_block_get_time(struct __trace_block *tb) {
+	return -1;
+}
+
+int trace_point_get_value(struct __trace_point *tp) {
+	return -1;
+}
+
+struct __trace_point *trace_point_get_by_name(const char *name) {
+	return NULL;
+}
+
+
