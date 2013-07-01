508f476f97f56a7aa4eebbff03e6251ba00f87c1
Ilia Vaprol <ki.stfu@gmail.com>
2013-07-01 23:56:59 +0000

    time: Add NSEC_PER_MSEC constant

--- src/include/kernel/time/time.h
+++ src/include/kernel/time/time.h
@@ -15,8 +15,9 @@
 
 #define MSEC_PER_SEC    1000L
 #define USEC_PER_MSEC   1000L
-#define NSEC_PER_USEC   1000L
 #define USEC_PER_SEC    1000000L
+#define NSEC_PER_USEC   1000L
+#define NSEC_PER_MSEC   1000000L
 #define NSEC_PER_SEC    1000000000L
 
 /**
