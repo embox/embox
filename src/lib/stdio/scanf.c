e212a5bef3885603de86d401b58ef785a113de9d
Anton Kozlov <drakon.mega@gmail.com>
2013-07-16 12:21:44 +0000

    scanf unscaned uppercased ch instead of original, fixed

--- src/lib/stdio/scanf.c
+++ src/lib/stdio/scanf.c
@@ -127,7 +127,7 @@ static int scan_int(char **in, int base, int widht) {
 		dst = 0;
 	}
 
-	for (i = 0; (ch = (int) toupper(scanchar(in))) != EOF; i++) {
+	for (i = 0; (ch = (int) scanchar(in)) != EOF; i++) {
 		if (!(base == 10 ? isdigit(ch) : isxdigit(ch)) || (0 == widht)) {
 			unscanchar(in, ch);
 			/*end conversion*/
--- src/tests/stdio/scanf_test.c
+++ src/tests/stdio/scanf_test.c
@@ -15,10 +15,28 @@ TEST_CASE("sscanf should accept fmt with some placeholders") {
 	const char *data = "1234 5678";
 	int a = 0, b = 0;
 
-	test_assert(sscanf(data, "%d %d", &a, &b) > 0);
+	test_assert_equal(sscanf(data, "%d %d", &a, &b), 2);
 
 	test_assert_equal(a, 1234);
 	test_assert_equal(b, 5678);
+}
+
+TEST_CASE("sscanf should keep case") {
+	int a = 0, b = 0;
+
+	test_assert_equal(sscanf("1234u5678", "%du%d", &a, &b), 2);
+
+	test_assert_equal(a, 1234);
+	test_assert_equal(b, 5678);
+}
+
+TEST_CASE("sscanf should work with hex") {
+	int a = 0, b = 0;
+
+	test_assert_equal(sscanf("123aub678", "%xu%x", &a, &b), 2);
+
+	test_assert_equal(a, 0x123a);
+	test_assert_equal(b, 0xb678);
 }
 
 
