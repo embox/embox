0db756546fc625cdc176cc4bd195420f79669d07
Ilia Vaprol <ki.stfu@gmail.com>
2013-07-19 11:37:45 +0000

    kernel: spinlock used libarch

--- src/include/kernel/spinlock.h
+++ src/include/kernel/spinlock.h
@@ -9,55 +9,29 @@
 #ifndef KERNEL_SPINLOCK_H_
 #define KERNEL_SPINLOCK_H_
 
-#include <kernel/cpu.h>
+#include <linux/compiler.h>
+#include <module/embox/arch/libarch.h>
 #include <sys/types.h>
-#include <module/embox/arch/smp.h>
-
-//typedef unsigned long spinlock_t;
 
 #define SPIN_UNLOCKED 0
 #define SPIN_LOCKED   1
 
-#define SPINLOCK_DEFINE(name) \
-	spinlock_t name = SPIN_UNLOCKED
-
-#define SPINLOCK_DECLARE(name) \
-	extern spinlock_t name
-
-#ifndef NOSMP
-
-#define __barrier() \
-	__asm__ __volatile__("" : : : "memory")
-
-/* TODO: Replace it in arch. It is only for x86. */
 static inline int spin_trylock(spinlock_t *lock) {
-	unsigned long val = SPIN_LOCKED;
-
-	__asm__ __volatile__ (
-			"lock xchgb %0, %1"
-			: "+r" (val), "+m" (*(lock))
-	);
-
-	__barrier();
-
-	return val == SPIN_UNLOCKED;
+#ifdef __HAVE_ARCH_CMPXCHG
+	return cmpxchg(lock, SPIN_UNLOCKED, SPIN_LOCKED);
+#else /* !__HAVE_ARCH_CMPXCHG */
+#warning "cmpxchg arch operation required"
+	return 0;
+#endif /* __HAVE_ARCH_CMPXCHG */
 }
 
 static inline void spin_lock(spinlock_t *lock) {
-	while (!spin_trylock(lock)) ;
+	while (!spin_trylock(lock)) { }
 }
 
 static inline void spin_unlock(spinlock_t *lock) {
 	*lock = SPIN_UNLOCKED;
 	__barrier();
 }
-
-#else /* NOSMP */
-
-#define spin_lock(lock)   do { (void) lock; } while (0)
-
-#define spin_unlock(lock) do { (void) lock; } while (0)
-
-#endif /* !NOSMP */
 
 #endif /* !KERNEL_SPINLOCK_H_ */
