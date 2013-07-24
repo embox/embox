c23d6a6fced6b4473fe377cf9c0854425748e356
Ilia Vaprol <ki.stfu@gmail.com>
2013-07-24 11:45:24 +0000

    Fix build

--- src/arch/generic/onecpu.h
+++ src/arch/generic/onecpu.h
@@ -9,8 +9,12 @@
 #ifndef ARCH_ONECPU_H_
 #define ARCH_ONECPU_H_
 
+#ifndef __ASSEMBLER__
+
 static inline unsigned int cpu_get_id(void) {
 	return 0;
 }
+
+#endif /* !__ASSEMBLER__ */
 
 #endif /* ARCH_ONECPU_H_ */
