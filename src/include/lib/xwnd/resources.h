021a8552f32a123c6abdb03b1b6efcfe0e7e1e51
Ilia Vaprol <ki.stfu@gmail.com>
2013-07-24 12:26:21 +0000

    Remove xwnd headers

--- src/include/lib/xwnd/resources.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIB_XWND_RESOURCES_H_
-#define LIB_XWND_RESOURCES_H_
-
-#define XAPP_REGISTER_RES_TYPE(type) typedef typeof(type) __xwnd_resources_type;
-#define XAPP_RESOURCES(xapp) ((__xwnd_resources_type *)((xapp)->resources))
-#define XAPP_INIT_RESOURCES(xapp,res_inst) ( ((xapp)->resources) = &(res_inst) )
-
-#endif
