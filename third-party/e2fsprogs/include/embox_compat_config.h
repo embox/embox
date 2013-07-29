fc62c8228052c1815d05f06791e4730b209c6ec8
Alex Kalmuk <alexkalmuk@gmail.com>
2013-07-29 15:53:09 +0000

    e2fsprogs: patch config.h.in to protect from AC_INCLUDES_DEFAULT

---
+++ third-party/e2fsprogs/include/embox_compat_config.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @brief Undef some macros generated with AC_INCLUDES_DEFAULT by autoconf
+ *
+ * @author  Alexander Kalmuk
+ * @date    29.07.2013
+ */
+
+#ifndef EMBOX_AC_INCLUDE_DEFAULT_H_
+#define EMBOX_AC_INCLUDE_DEFAULT_H_
+
+#undef HAVE_MEMORY_H
+#undef HAVE_INTTYPES_H
+
+#endif /* EMBOX_AC_INCLUDE_DEFAULT_H_ */
