a6ea150a853b00a530e559d82b7bdcfd07ed2a23
Anton Kozlov <drakon.mega@gmail.com>
2013-08-01 14:51:10 +0000

    usersmode: template changes, script simplified run

--- scripts/emvisor_start.sh
+++ scripts/emvisor_start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+ROOT=$(dirname $0)/../.
+
 VISOR=./emvisor
-EMBOX=./build/base/bin/embox
+EMBOX=${ROOT}/build/base/bin/embox
 TAP=tap77
+
+EMVISOR_MK=${ROOT}/src/arch/usermode86/emvisor.mk
 
 PDOWNSTRM=fdownstream
 PUPSTRM=fupstream
@@ -21,4 +25,7 @@ sudo ip addr change 10.0.2.10 dev $TAP
 sudo ip link set $TAP up
 sudo ip route add 10.0.2.0/24 dev $TAP
 
+[ -x $VISOR ] || make -f $EMVISOR_MK
+
 $VISOR $PDOWNSTRM $PUPSTRM $EMBOX
+
