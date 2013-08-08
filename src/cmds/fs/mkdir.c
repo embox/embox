7161ce9291b43c662cd49fb493a2c5df0a8f7d26
Alex Kalmuk <alexkalmuk@gmail.com>
2013-08-08 15:12:12 +0000

    Remove "/"-prefix in directory name from mkdir

--- src/cmds/fs/mkdir.c
+++ src/cmds/fs/mkdir.c
@@ -37,10 +37,6 @@ static int exec(int argc, char **argv) {
 
 	if (argc > 1) {
 		point = argv[argc - 1];
-		/*TODO relative path*/
-		if(0 != strncmp(point, "/", 1)) {
-			return -EINVAL;
-		}
 		if (-1 == mkdir(point, 0)) {
 			return -errno;
 		}
