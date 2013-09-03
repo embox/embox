2572a532bcebcff9e489334fe0b66a1063ae1842
Anton Kozlov <drakon.mega@gmail.com>
2013-09-03 05:39:52 +0000

    buildfix zrv: module's private -> priv; dropbear's TCP_NODELAY

--- src/cmds/modprobe.c
+++ src/cmds/modprobe.c
@@ -21,13 +21,13 @@ static void print_usage(void) {
 
 static void fake_enable(const struct mod *mod) {
 	const struct mod *dep;
-	int enabled = mod->private->flags & 0x1; // XXX fix later
+	int enabled = mod->priv->flags & 0x1; // XXX fix later
 
 	if (enabled) {
 		return;
 	}
 
-	mod->private->flags |= 0x1;
+	mod->priv->flags |= 0x1;
 	mod_foreach_requires(dep, mod) {
 		fake_enable(dep);
 	}
