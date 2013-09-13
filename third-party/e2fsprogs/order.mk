dc3e21610ed6c11d1a384b1440ab1f91401b1fe5
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-09-13 13:16:12 +0000

    mybuild: Fix e2fsprogs build rules

--- third-party/e2fsprogs/order.mk
+++ third-party/e2fsprogs/order.mk
@@ -1,3 +1,13 @@
+MAKE_E2FS = \
+	$(MAKE) -C $(THIRDPARTY_DIR)/e2fsprogs \
+	MAKEFLAGS= \
+	EMBOX_MAKEFLAGS='$(MAKEFLAGS)' \
+	ROOT_DIR=$(abspath $(ROOT_DIR)) \
+	EMBOX_CFLAGS='$(CFLAGS)' \
+	EMBOX_CXXFLAGS='$(CXXFLAGS)' \
+	EMBOX_LDFLAGS='$(LDFLAGS)' \
+	EMBOX_CPPFLAGS='$(EMBOX_EXPORT_CPPFLAGS)'
+
 MISC_DIR= $(OBJ_DIR)/third-party/e2fsprogs/../../../../e2fsprogs/src/e2fsprogs-build/misc
 E2FSCK_DIR= $(OBJ_DIR)/third-party/e2fsprogs/../../../../e2fsprogs/src/e2fsprogs-build/e2fsck
 EXT2FS_LIB= $(OBJ_DIR)/third-party/e2fsprogs/../../../../e2fsprogs/src/e2fsprogs-build/lib/ext2fs/libext2fs.a
