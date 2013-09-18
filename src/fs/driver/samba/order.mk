521196685685373a55d401784edcb287da827853
Vita Loginova <vita.loginova@gmail.com>
2013-09-18 12:16:46 +0000

    samba: order.mk added

---
+++ src/fs/driver/samba/order.mk
@@ -0,0 +1,7 @@
+
+$(OBJ_DIR)/src/fs/driver/samba/samba.o: $(OBJ_DIR)/third-party/samba/../../../../samba/lib/libsmbclient.a \
+										$(OBJ_DIR)/third-party/samba/../../../../samba/lib/libtevent.a \
+										$(OBJ_DIR)/third-party/samba/../../../../samba/lib/libtalloc.a \
+										$(OBJ_DIR)/third-party/samba/../../../../samba/lib/libtdb.a \
+										$(OBJ_DIR)/third-party/samba/../../../../samba/lib/libwbclient.a
+
