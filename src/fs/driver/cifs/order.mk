c7a41efb0a02613a1f6c6f88c3bfe2fba7086242
Felix Sulima <fsulima@gmail.com>
2013-09-19 08:47:11 +0000

    cifs: samba FS driver renamed to CIFS

--- src/fs/driver/cifs/order.mk
+++ src/fs/driver/cifs/order.mk
@@ -1,5 +1,5 @@
 
-$(OBJ_DIR)/src/fs/driver/samba/samba.o: $(OBJ_DIR)/third-party/samba/../../../../samba/lib/libsmbclient.a \
+$(OBJ_DIR)/src/fs/driver/cifs/cifs.o:	$(OBJ_DIR)/third-party/samba/../../../../samba/lib/libsmbclient.a \
 										$(OBJ_DIR)/third-party/samba/../../../../samba/lib/libtevent.a \
 										$(OBJ_DIR)/third-party/samba/../../../../samba/lib/libtalloc.a \
 										$(OBJ_DIR)/third-party/samba/../../../../samba/lib/libtdb.a \
