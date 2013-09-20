272eb0f2c8e600c27fc6ead0bbd6ca6b98e7bcf3
Felix Sulima <fsulima@gmail.com>
2013-09-20 14:59:30 +0000

    bb: attempt to compile brute force

---
+++ third-party/busybox/include/sys/param.h
@@ -0,0 +1,13 @@
+/*
+ * param.h
+ *
+ *  Created on: 2013-sep-20
+ *      Author: fsulima
+ */
+
+#ifndef PARAM_H_
+#define PARAM_H_
+
+
+
+#endif /* PARAM_H_ */
---
+++ third-party/busybox/include/sys/statfs.h
@@ -0,0 +1,13 @@
+/*
+ * statfs.h
+ *
+ *  Created on: 2013-sep-20
+ *      Author: fsulima
+ */
+
+#ifndef STATFS_H_
+#define STATFS_H_
+
+
+
+#endif /* STATFS_H_ */
