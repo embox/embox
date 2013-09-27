9de0099856908bdf0b0a2137f0d7d45137687f42
Anton Kozlov <drakon.mega@gmail.com>
2013-09-27 14:13:19 +0000

    fdlibm's sqrt added

--- third-party/lib/fdlibm/e_powf.c
+++ third-party/lib/fdlibm/e_powf.c
@@ -153,7 +153,7 @@ float x, y;
 	    if(hy==0x40000000) return x*x; /* y is  2 */
 	    if(hy==0x3fe00000) {	/* y is  0.5 */
 		if(hx>=0)	/* x >= +0 */
-		return sqrt(x);
+		return __ieee754_sqrt(x);
 	    }
 	}
 
