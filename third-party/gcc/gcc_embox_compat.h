82f0d9319911c4372c0e46e9df1e0ab1a6e58b5a
Felix Sulima <fsulima@gmail.com>
2013-09-27 14:02:50 +0000

    libgcc: some build scripts

---
+++ third-party/gcc/gcc_embox_compat.h
@@ -0,0 +1,22 @@
+/*
+ * ntfs-3g_embox_compat.h
+ *
+ *  Created on: 02 juillet 2013
+ *      Author: fsulima
+ */
+
+#ifndef NTFS_EMBOX_COMPAT_H_
+#define NTFS_EMBOX_COMPAT_H_
+
+
+#ifdef linux
+#undef linux
+#endif
+
+#ifdef __linux__
+#undef __linux__
+#endif
+
+
+
+#endif /* NTFS_EMBOX_COMPAT_H_ */
