b40cee35cbf5581cd138aed9ab8727dd2f900b70
Anton Bondarev <anton.bondarev2310@gmail.com>
2013-09-27 14:42:25 +0000

    pci: tlp lib

--- src/drivers/pci/pci_tlp.c
+++ src/drivers/pci/pci_tlp.c
@@ -7,8 +7,11 @@
  */
 #include <stdint.h>
 #include <stddef.h>
+#include <errno.h>
+
+#include <drivers/pci/pci.h>
 #include <drivers/pci/pci_tlp.h>
-#include <endian.h>
+//#include <endian.h>
 
 static inline int tlp_fmt2size(char fmt) {
 	if (TLP_FMT_HEADSIZE & fmt) {
@@ -17,22 +20,70 @@ static inline int tlp_fmt2size(char fmt) {
 	return 3;
 }
 
-#define TLP_FMT_OFFSET  28
-#define TLP_TYPE_OFFSET 24
-int tlp_build_mem_wr(uint32_t *tlp, char frm, uint32_t *buff, size_t data_len) {
+static inline int tlp_size(uint32_t *tlp) {
+	if (tlp[0] & (TLP_FMT_HEADSIZE << TLP_FMT_OFFSET)) {
+		return 4;
+	}
+	return 3;
+}
+
+static inline uint32_t *tlp_to_pci(uint32_t *tlp) {
+	int size = tlp_size(tlp);
+	int cnt;
+
+	for (cnt = 0; cnt != size; ++cnt) {
+		//tlp[cnt] = htobe32(tlp[cnt]);
+	}
+	return tlp;
+}
+
+static inline uint32_t *tlp_from_pci(uint32_t *tlp) {
+	int size = tlp_size(tlp);
+	int cnt;
+
+	for (cnt = 0; cnt != size; ++cnt) {
+		tlp[cnt] = betoh32(tlp[cnt]);
+	}
+	return tlp;
+}
+
+int tlp_build_mem_wr(uint32_t *tlp, struct pci_slot_dev *dev, char bar,
+		uint32_t offset, char fmt, uint32_t *buff, uint16_t len) {
 	int hsize;
+	char fbe, lbe; /* first and last Byte Enable */
 
-
+	if (len > 0xFFF) {
+		return -EINVAL;
+	}
 	hsize = tlp_fmt2size(fmt);
 
-	tlp[0] = (frm << TLP_FMT_OFFSET) | (TLP_TYPE_MRD << TLP_TYPE_OFFSET) |
-			data_len;
-	switch(offset & 3) {
-	case
-	}
-	tlp[1] =
+	tlp[0] = (fmt << TLP_FMT_OFFSET) | (TLP_TYPE_MEM << TLP_TYPE_OFFSET) | len;
 
-	htobe32()
+	switch (offset & 3) {
+	case 1:
+		fbe = 0x7;
+		break;
+	case 2:
+		fbe = 0x3;
+		break;
+	case 3:
+		fbe = 0x1;
+		break;
+	default:
+		fbe = 0xF;
+		break;
+	}
+	lbe = 0;
+
+	tlp[1] = fbe | lbe << 4;
+
+	tlp[2] = dev->bar[bar];
+
+	if (hsize == 4) {
+		tlp[3] = 0;
+	}
+
+
 	return 0;
 }
 
--- src/include/drivers/pci/pci_tlp.h
+++ src/include/drivers/pci/pci_tlp.h
@@ -10,29 +10,25 @@
 #ifndef PCI_TLP_H_
 #define PCI_TLP_H_
 
-#define TLP_TYPE_MRD     0x0   /* Memory Read Request */
+#define TLP_TYPE_MEM     0x0   /* Memory Read/Write Request */
 #define TLP_TYPE_RDLK    0x1   /* Memory Read Lock Request */
-#define TLP_TYPE_MWR     0x0   /* Memory Write Request */
-#define TLP_TYPE_IORD    0x2   /* IO Read Request */
-#define TLP_TYPE_IOWR    0x2   /* IO Write Request */
-#define TLP_TYPE_CFGRD0  0x4   /* Config Type 0 Read Request */
-#define TLP_TYPE_CFGWR0  0x4   /* Config Type 0 Write Request */
-#define TLP_TYPE_CFGRD1  0x5   /* Config Type 1 Read Request */
-#define TLP_TYPE_CFGWR1  0x5   /* Config Type 0 Write Request */
+#define TLP_TYPE_IO      0x2   /* IO Read/Write Request */
+#define TLP_TYPE_CFG0    0x4   /* Config Type 0 Read/Write Request */
+#define TLP_TYPE_CFG1    0x5   /* Config Type 1 Read/Write Request */
 #define TLP_TYPE_CPL     0xA   /* Completion */
-#define TLP_TYPE_CPLD    0xA   /* Completion W/Data */
 #define TLP_TYPE_CPLLK   0xB   /* Completion-Locked */
-#define TLP_TYPE_CPLDLK  0xB   /* Completion W/Data */
+
 
 #define TLP_FMT_HEADSIZE 0x1
 #define TLP_FMT_DATA     0x2
 
-#include <stdint.h>
+#define TLP_FMT_OFFSET  28
+#define TLP_TYPE_OFFSET 24
 
-typedef uint32_t tlp_3dw_t[3];
-typedef uint32_t tlp_4dw_t[4];
 
-extern int tlp_build_mem_wr(char frm, uint32_t *buff, size_t data_len);
+extern int tlp_build_mem_wr(uint32_t *tlp, struct pci_slot_dev *dev, char bar,
+		uint32_t offset, char fmt, uint32_t *buff, uint16_t len);
+
 extern int tlp_build_mem_rd(char frm);
 extern int tlp_build_io_wr(char frm, uint32_t *buff, size_t data_len);
 extern int tlp_build_io_rd(char frm);
