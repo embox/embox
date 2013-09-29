bc24e02d33a57223b6e27df1a89cc357edca65ca
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-09-29 18:57:16 +0000

    lds: Remove obsolete sections from vmem.c (untested)

--- src/mem/vmem/vmem.c
+++ src/mem/vmem/vmem.c
@@ -22,36 +22,40 @@
 #include <kernel/printk.h>
 
 /* Section pointers. */
-extern char _text_vma, _rodata_vma, _bss_vma, _data_vma, _reserve_vma, _stack_vma, _heap_vma;
-extern char _text_len, _rodata_len, _bss_len, _data_len, _reserve_len, _stack_len, _heap_len;
-
-static inline int vmem_map_kernel(mmu_ctx_t ctx);
+extern char _text_vma, _rodata_vma, _bss_vma, _data_vma;
+extern char _text_len, _rodata_len, _bss_len, _data_len;
 
 static inline int vmem_map_kernel(mmu_ctx_t ctx) {
 	int err = 0;
 
 	/* Map sections. */
-	err |= vmem_map_on_itself(ctx, &_text_vma, (size_t) &_text_len, VMEM_PAGE_WRITABLE);
-	err |= vmem_map_on_itself(ctx, &_rodata_vma, (size_t) &_rodata_len, VMEM_PAGE_WRITABLE);
-	err |= vmem_map_on_itself(ctx, &_bss_vma, (size_t) &_bss_len, VMEM_PAGE_WRITABLE);
-	err |= vmem_map_on_itself(ctx, &_data_vma, (size_t) &_data_len, VMEM_PAGE_WRITABLE);
-	err |= vmem_map_on_itself(ctx, &_reserve_vma, (size_t) &_reserve_len, VMEM_PAGE_WRITABLE);
-	err |= vmem_map_on_itself(ctx, &_stack_vma, (size_t) &_stack_len, VMEM_PAGE_WRITABLE);
-	err |= vmem_map_on_itself(ctx, &_heap_vma, (size_t) &_heap_len, VMEM_PAGE_WRITABLE);
+	err |= vmem_map_on_itself(ctx, &_text_vma, (size_t) &_text_len,
+			VMEM_PAGE_WRITABLE);
+	err |= vmem_map_on_itself(ctx, &_rodata_vma, (size_t) &_rodata_len,
+			VMEM_PAGE_WRITABLE);
+	err |= vmem_map_on_itself(ctx, &_bss_vma, (size_t) &_bss_len_with_reserve,
+			VMEM_PAGE_WRITABLE);
+	err |= vmem_map_on_itself(ctx, &_data_vma, (size_t) &_data_len,
+			VMEM_PAGE_WRITABLE);
 
 	/* Map special info. */
-	err |= vmem_map_on_itself(ctx, VIRTUAL_TABLES_START, VIRTUAL_TABLES_LEN, VMEM_PAGE_WRITABLE);
-	err |= vmem_map_on_itself(ctx, VIRTUAL_PAGES_INFO_START, VIRTUAL_PAGES_INFO_LEN, VMEM_PAGE_WRITABLE);
+	err |= vmem_map_on_itself(ctx, VIRTUAL_TABLES_START, VIRTUAL_TABLES_LEN,
+			VMEM_PAGE_WRITABLE);
+	err |= vmem_map_on_itself(ctx, VIRTUAL_PAGES_INFO_START, VIRTUAL_PAGES_INFO_LEN,
+			VMEM_PAGE_WRITABLE);
 
 	// XXX below
 	// for sparc
-	err |= vmem_map_on_itself(ctx, (void *) 0x80000000, (size_t) 0x1000, VMEM_PAGE_WRITABLE);
+	err |= vmem_map_on_itself(ctx, (void *) 0x80000000, (size_t) 0x1000,
+			VMEM_PAGE_WRITABLE);
 
 	// for microblaze
-	err |= vmem_map_on_itself(ctx, (void *) 0x84000000, (size_t) 0x1000, VMEM_PAGE_WRITABLE);
+	err |= vmem_map_on_itself(ctx, (void *) 0x84000000, (size_t) 0x1000,
+			VMEM_PAGE_WRITABLE);
 
 	// mapping x86 video buffer
-	err |= vmem_map_on_itself(ctx, (void *) 0xB8000, (size_t) 0x1000, VMEM_PAGE_WRITABLE);
+	err |= vmem_map_on_itself(ctx, (void *) 0xB8000, (size_t) 0x1000,
+			VMEM_PAGE_WRITABLE);
 
 	return err;
 }
