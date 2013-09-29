1bd21c4100454a9153e87ffed520b31120b9cb6d
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-09-29 21:23:23 +0000

    lds: Fix phymem allocator according to new memory layout
    Add debug output.

--- src/mem/phymem.c
+++ src/mem/phymem.c
@@ -4,8 +4,12 @@
  * @date Oct 5, 2012
  * @author: Anton Bondarev
  */
+
 #include <embox/unit.h>
 
+#include <stdint.h>
+
+#include <kernel/printk.h>
 #include <mem/phymem.h>
 #include <mem/page.h>
 #include <util/binalign.h>
@@ -13,15 +17,21 @@
 EMBOX_UNIT_INIT(phymem_init);
 
 struct page_allocator *__phymem_allocator;
-extern char _mem_begin;
-extern char _mem_length;
-extern char _heap_end;
 
 static int phymem_init(void) {
-	size_t free_start = binalign_bound((size_t)&_heap_end, PAGE_SIZE());
-	size_t free_len = (size_t)&_mem_length - (free_start - (size_t)&_mem_begin);
+	extern char _mem_begin;
+	extern char _mem_length;
+	extern char _reserve_end;
 
-	__phymem_allocator = page_allocator_init((char *)free_start, free_len, PAGE_SIZE());
+	char *mem_start = (char *) binalign_bound((uintptr_t) &_reserve_end,
+			PAGE_SIZE());
+	char *mem_end = &_mem_begin + (size_t) &_mem_length;
+	size_t mem_len = mem_end - mem_start;
+
+	printk("start=0x%08lx, end=0x%08lx, size=%d\n",
+			(uintptr_t) mem_start, (uintptr_t) mem_end, mem_len);
+
+	__phymem_allocator = page_allocator_init(mem_start, mem_len, PAGE_SIZE());
 
 	return 0;
 }
