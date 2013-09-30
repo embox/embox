bfdb0e90412d8178eb5ab110db755e7e73ff4d4a
Felix Sulima <fsulima@gmail.com>
2013-09-30 18:46:14 +0000

    gcc: libgcc is now build (or something far resembling it)

--- third-party/gcc/gcc_embox_compat.h
+++ third-party/gcc/gcc_embox_compat.h
@@ -17,6 +17,15 @@
 #undef __linux__
 #endif
 
+#include <pthread.h>
 
+#define PTHREAD_ONCE_INIT {}
+extern int   pthread_once(pthread_once_t *, void (*)(void));
+extern void *pthread_getspecific(pthread_key_t);
+extern int   pthread_setspecific(pthread_key_t, const void *);
+extern int   pthread_cancel(pthread_t);
+extern int   pthread_cond_timedwait(pthread_cond_t *, pthread_mutex_t *, const struct timespec *);
+extern int   pthread_key_create(pthread_key_t *, void (*)(void *));
+extern int   pthread_key_delete(pthread_key_t);
 
 #endif /* NTFS_EMBOX_COMPAT_H_ */
