43242c2ad3d92c7821cdc377f50322ee119808c2
Denis Deryugin <deryugin.denis@gmail.com>
2013-10-03 16:40:12 +0000

    trace_points: more detailed output implemented

--- src/cmds/trace_points.c
+++ src/cmds/trace_points.c
@@ -4,6 +4,7 @@
  *
  * @date 23.05.2012
  * @author Alina Kramar
+ * @author Denis Deryugin
  */
 
 #include <stdio.h>
@@ -19,20 +20,26 @@ EMBOX_CMD(exec);
 extern struct __trace_point * const __trace_points_array[];
 
 static void print_usage(void) {
-	printf("Usage: trace_point [-h] [-s] [-i <number>] [-d <number>] [-a <number>]\n");
+	printf(	"Usage: trace_point [ARGS]\n%-60s\n%-60s\n%-60s\n%-60s\n%-60s\n",
+			"\t[-h] print usage\n",
+			"\t[-s] all points: (#, NAME, LOCATION, COUNT, ACT)\n",
+			"\t[-i <number>] one point: (#, NAME, LOCATION, COUNT, ACT)\n",
+			"\t[-d <number>] change point activity to FALSE\n",
+			"\t[-a <number>] change point activity to TRUE\n");
 }
 
 static void print_trace_point_stat_with_location(int i) {
 	struct __trace_point *tp;
 	int number = 0;
 
-	printf("%2s %25s %7s %5s\n", "№ ", "Location function", "Count", "Act");
+	printf("%2s %15s %25s %7s %5s\n",
+			"#", "Name", "Location function", "Count", "Act");
 
 	array_nullterm_foreach(tp, __trace_points_array)
 	{
 		if (number++ == i) {
-			printf("%2d %25s %7d %5s\n", i, tp->location.func, tp->count,
-					tp->active ? "yes" : "no");
+			printf("%2d %15s %25s %7d %5s\n", i, tp->name,
+					tp->location.func, tp->count, tp->active ? "yes" : "no");
 			break;
 		}
 	}
@@ -44,13 +51,19 @@ static void print_trace_point_stat(void) {
 	struct __trace_point *tp;
 	int number = 0;
 
-	printf("%2s %7s\n", "№ ", "count");
+
+	printf("%2s %15s %25s %7s %5s\n",
+			"#", "Name", "Location function", "Count", "Act");
+
 
 	array_nullterm_foreach(tp, __trace_points_array)
 	{
-		if (tp->active) {
+		printf("%2d %15s %25s %7d %5s\n", number, tp->name,
+					tp->location.func, tp->count, tp->active ? "yes" : "no");
+
+		/*if (tp->active) {
 			printf("%2d %7d\n", number, tp->count);
-		}
+		}*/
 		number++;
 	}
 
@@ -65,10 +78,13 @@ bool change_point_activity(int index, bool activity) {
 	{
 		if (number++ == index) {
 			tp->active = activity;
+			printf("Success! Trace point #%d now is %sactive.\n",
+					index, (activity)?"":"not ");
 			return true;
 		}
 	}
-
+	printf("Failed to change state: trace point #%d not found!\n",
+			index);
 	return false;
 }
 
@@ -78,7 +94,7 @@ static int exec(int argc, char **argv) {
 
 	if (argc <= 1) {
 		print_usage();
-		return -EINVAL;
+		return 0;
 	}
 
 	getopt_init();
