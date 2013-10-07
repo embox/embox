4884cdaccf07376cfed3f6548eaf0885e40ca199
Ilia Vaprol <ki.stfu@gmail.com>
2013-10-07 14:04:03 +0000

    ti816x: last changes

--- src/drivers/net/ti816x.c
+++ src/drivers/net/ti816x.c
@@ -24,12 +24,13 @@
 #include <net/netdevice.h>
 #include <net/inetdevice.h>
 #include <net/skbuff.h>
+#include <util/binalign.h>
 
 #include <kernel/printk.h>
 
 EMBOX_UNIT_INIT(ti816x_init);
 
-#define PREP_BUF_COUNT 5
+#define PREP_BUF_COUNT 10
 
 static void emac_ctrl_enable_irq(void) {
 	REG_STORE(EMAC_CTRL_BASE + EMAC_R_CMRXTHRESHINTEN, 0xff);
@@ -68,18 +69,27 @@ static void emac_clear_stat_regs(void) {
 	}
 }
 
+#define MACINDEX(x) ((x & 0x1f) << 0)
+#define VALID (1 << 20)
+#define MATCHFILT (1 << 19)
+#define CHANNEL(x) ((x & 0x7) << 16)
 static void emac_set_macaddr(unsigned char (*addr)[6]) {
 	int i;
+	unsigned long mac_hi, mac_lo;
+
+	mac_hi = ((*addr)[3] << 24) | ((*addr)[2] << 16)
+			| ((*addr)[1] << 8) | ((*addr)[0] << 0);
+	mac_lo = ((*addr)[5] << 8) | ((*addr)[4] << 0);
 
 	for (i = 0; i < EMAC_CHANNEL_COUNT; ++i) {
-		REG_STORE(EMAC_BASE + EMAC_R_MACINDEX, i & 0x1f);
-		REG_STORE(EMAC_BASE + EMAC_R_MACADDRHI,
-				((*addr)[2] << 24) | ((*addr)[3] << 16)
-				| ((*addr)[4] << 8) | ((*addr)[5] << 0));
+		REG_STORE(EMAC_BASE + EMAC_R_MACINDEX, MACINDEX(i));
+		REG_STORE(EMAC_BASE + EMAC_R_MACADDRHI, mac_hi);
 		REG_STORE(EMAC_BASE + EMAC_R_MACADDRLO,
-				(0x1 << 20) | (0x1 << 19) | ((i & 0x7) << 16)
-				| ((*addr)[0] << 8) | ((*addr)[1] << 0));
+				mac_lo | VALID | MATCHFILT | CHANNEL(i));
 	}
+
+	REG_STORE(EMAC_BASE + EMAC_R_MACSRCADDRHI, mac_hi);
+	REG_STORE(EMAC_BASE + EMAC_R_MACSRCADDRLO, mac_lo);
 }
 
 static void emac_init_rx_regs(void) {
@@ -120,7 +130,7 @@ static void emac_enable_rxmbp(void) {
 			RXNOCHAIN | RXCSFEN | RXCAFEN | RXBROADEN | RXMULTEN);
 }
 
-#define TXPACE (0x1 << 6)
+//#define TXPACE (0x1 << 6)
 #define GMIIEN (0x1 << 5)
 static void emac_set_macctrl(unsigned long v) {
 	REG_ORIN(EMAC_BASE + EMAC_R_MACCONTROL, v);
@@ -130,12 +140,12 @@ static void emac_set_macctrl(unsigned long v) {
 #define STATMASK (0x1 << 0)
 static void emac_enable_rx_and_tx_irq(void) {
 	/* mask unused channel */
-	//REG_STORE(EMAC_BASE + EMAC_R_TXINTMASKCLEAR, 0xf);
-	//REG_STORE(EMAC_BASE + EMAC_R_RXINTMASKCLEAR, 0xf);
+	REG_STORE(EMAC_BASE + EMAC_R_TXINTMASKCLEAR, 0xe);
+	REG_STORE(EMAC_BASE + EMAC_R_RXINTMASKCLEAR, 0xe);
 
 	/* allow all rx and tx interrupts */
-	REG_STORE(EMAC_BASE + EMAC_R_TXINTMASKSET, 0xf);
-	REG_STORE(EMAC_BASE + EMAC_R_RXINTMASKSET, 0xf);
+	REG_STORE(EMAC_BASE + EMAC_R_TXINTMASKSET, 0x1);
+	REG_STORE(EMAC_BASE + EMAC_R_RXINTMASKSET, 0x1);
 
 	/* allow host error and statistic interrupt */
 	REG_STORE(EMAC_BASE + EMAC_R_MACINTMASKSET, HOSTMASK | STATMASK);
@@ -146,24 +156,27 @@ static void emac_prep_rx_queue(void) {
 	struct sk_buff_data *skb_data;
 	struct emac_desc *desc, *head, *prev;
 
-	head = prev = NULL;
 
-	for (i = 0; i < PREP_BUF_COUNT; ++i) {
+	for (i = 0; i < 8; ++i) {
+	head = prev = NULL;
 		skb_data = skb_data_alloc();
 		assert(skb_data != NULL);
 		desc = (struct emac_desc *)skb_data;
+		assert(binalign_check_bound((uintptr_t)desc, 4));
 		if (head == NULL) head = desc;
 		if (prev != NULL) prev->next = (uintptr_t)desc;
 		desc->next = 0;
+		assert(binalign_check_bound((uintptr_t)desc->next, 4));
 		desc->data = (uintptr_t)(desc + 1);
+		assert(binalign_check_bound((uintptr_t)desc->data, 4));
 		desc->data_off = 0;
 		desc->data_len = skb_max_size() - sizeof *desc;
-		desc->flags = EMAC_DESC_F_SOP | EMAC_DESC_F_EOP;
-		desc->len = desc->data_len;
+		desc->flags = EMAC_DESC_F_OWNER;
+		desc->len = 0;
 		prev = desc;
+	//REG_STORE(EMAC_BASE + EMAC_R_RXHDP(i), (uintptr_t)head);
 	}
 
-	REG_STORE(EMAC_BASE + EMAC_R_RXHDP(0), (uintptr_t)head);
 }
 
 #define RXEN (0x1 << 0)
@@ -182,6 +195,8 @@ static int ti816x_xmit(struct net_device *dev, struct sk_buff *skb) {
 	assert(skb_data != NULL);
 
 	desc = (struct emac_desc *)skb_data;
+	printk("desc %p\n", desc);
+	assert(binalign_check_bound((uintptr_t)desc, 32));
 
 	hdr = (struct emac_hdr *)(desc + 1);
 	memset(&hdr->preamble[0], EMAC_HDR_PRE,
@@ -189,15 +204,17 @@ static int ti816x_xmit(struct net_device *dev, struct sk_buff *skb) {
 	memset(&hdr->sfd, EMAC_HDR_SFD, sizeof hdr->sfd);
 
 	desc->next = 0;
-	desc->data = (uint32_t)hdr;
+	assert(binalign_check_bound(desc->next, 32));
+	desc->data = (uintptr_t)hdr;
+	assert(binalign_check_bound(desc->data, 1));
 	desc->data_off = 0;
-	desc->data_len = sizeof(struct emac_hdr) + skb->len;
+	desc->data_len = max(sizeof(struct emac_hdr) + skb->len, 60);
 	desc->flags = EMAC_DESC_F_SOP | EMAC_DESC_F_EOP | EMAC_DESC_F_OWNER;
-	desc->len = sizeof(struct emac_hdr) + skb->len;
+	desc->len = max(sizeof(struct emac_hdr) + skb->len, 60);
 
 	REG_STORE(EMAC_BASE + EMAC_R_TXHDP(0), (uintptr_t)desc);
 
-	skb_free(skb);
+//	skb_free(skb);
 
 	return 0;
 }
@@ -225,8 +242,23 @@ static const struct net_driver ti816x_ops = {
 };
 
 #include <kernel/printk.h>
+#define HOSTPEND (1 << 26)
 static irq_return_t ti816x_interrupt(unsigned int irq_num, void *dev_id) {
-	printk("!%u", irq_num);
+	unsigned long reg = REG_LOAD(EMAC_BASE + EMAC_R_MACINVECTOR);
+
+	printk("!%u reg %lx\n", irq_num, reg);
+
+	switch (irq_num) {
+	case MACMISC0:
+		printk("cmmiscintstat %lx\n", REG_LOAD(EMAC_CTRL_BASE + EMAC_R_CMMISCINTSTAT));
+		if (reg & HOSTPEND) {
+			printk("resetting\n");
+			REG_STORE(EMAC_BASE + EMAC_R_SOFTRESET, 1);
+			while (REG_LOAD(EMAC_BASE + EMAC_R_SOFTRESET) & 1);
+		}
+		break;
+	}
+
 	return IRQ_HANDLED;
 }
 
@@ -238,6 +270,10 @@ static void ti816x_config(struct net_device *dev) {
 	assert(skb_max_extra_hdr_size() == sizeof(struct emac_desc)
 			+ sizeof(struct emac_hdr));
 
+	/* reset EMAC module */
+	REG_STORE(EMAC_BASE + EMAC_R_SOFTRESET, 1);
+	while (REG_LOAD(EMAC_BASE + EMAC_R_SOFTRESET) & 1);
+
 	/* TODO enable EMAC/MDIO peripheral */
 
 	/* TODO configuration of the interrupt to the CPU */
@@ -245,14 +281,16 @@ static void ti816x_config(struct net_device *dev) {
 	/* disable all the EMAC/MDIO interrupts in the control module */
 	emac_ctrl_disable_irq();
 
+#if 0
 	/* initialization of the EMAC control module */
 	REG_STORE(EMAC_CTRL_BASE + EMAC_R_CMRXINTMAX, 0x4);
 	REG_STORE(EMAC_CTRL_BASE + EMAC_R_CMTXINTMAX, 0x4);
 	REG_STORE(EMAC_CTRL_BASE + EMAC_R_CMINTCTRL, 0x30000 | 0x258);
+#endif
 
 	/* initialization of EMAC and MDIO modules */
-	emac_clear_hdp();
 	emac_clear_ctrl_regs();
+	emac_clear_hdp();
 	emac_clear_stat_regs();
 	emac_set_macaddr(&addr);
 	emac_init_rx_regs();
@@ -260,7 +298,7 @@ static void ti816x_config(struct net_device *dev) {
 	emac_set_rxbufoff(0);
 	emac_clear_and_enable_rxunicast();
 	emac_enable_rxmbp();
-	emac_set_macctrl(TXPACE);
+	//emac_set_macctrl(TXPACE);
 	emac_enable_rx_and_tx_irq();
 	emac_prep_rx_queue();
 	emac_enable_rx_and_tx_dma();
@@ -297,13 +335,10 @@ static int ti816x_init(void) {
 	if (ret < 0) {
 		return ret;
 	}
-#if 0
 	ret = irq_attach(MACMISC0, ti816x_interrupt, 0, nic, "ti816x");
 	if (ret < 0) {
 		return ret;
 	}
-#endif
-
 
 	return inetdev_register_dev(nic);
 }
