6e39cf5a6fe02e3c5d453b36f05ef9905d249e86
Felix Sulima <fsulima@gmail.com>
2013-10-07 14:44:33 +0000

    boost: some more stubs and flags

--- third-party/boost/boost_embox_compat.h
+++ third-party/boost/boost_embox_compat.h
@@ -119,6 +119,12 @@ int pthread_rwlock_wrlock(pthread_rwlock_t *rwlock);
 extern
 int pthread_rwlock_unlock(pthread_rwlock_t *rwlock);
 
+static inline int pthread_key_delete (pthread_key_t __key){
+	DPRINT();
+	return -1;
+}
+
+
 extern
 int swprintf(wchar_t *wcs, size_t maxlen,
 	const wchar_t *format, ...);
@@ -128,5 +134,127 @@ int vswprintf(wchar_t *wcs, size_t maxlen,
 
 extern
 int getpagesize(void);
+
+/* Length of interface name.  */
+#define IF_NAMESIZE	16
+
+#include <netinet/in.h>
+
+struct ip_mreq  {
+	struct in_addr imr_multiaddr;	/* IP multicast address of group */
+	struct in_addr imr_interface;	/* local IP address of interface */
+};
+
+struct ipv6_mreq
+  {
+    /* IPv6 multicast address of group */
+    struct in6_addr ipv6mr_multiaddr;
+    /* local interface */
+    unsigned int ipv6mr_interface;
+  };
+
+
+/* Structure large enough to hold any socket address (with the historical
+   exception of AF_UNIX).  We reserve 128 bytes.  */
+#define __ss_aligntype	unsigned long int
+#define _SS_SIZE	128
+#define _SS_PADSIZE	(_SS_SIZE - (2 * sizeof (__ss_aligntype)))
+
+#define	__SOCKADDR_COMMON(sa_prefix) \
+  sa_family_t sa_prefix##family
+
+#define __SOCKADDR_COMMON_SIZE	(sizeof (unsigned short int))
+
+struct sockaddr_storage
+  {
+    __SOCKADDR_COMMON (ss_);	/* Address family, etc.  */
+    __ss_aligntype __ss_align;	/* Force desired alignment.  */
+    char __ss_padding[_SS_PADSIZE];
+  };
+
+
+struct sockaddr_un {
+    unsigned short sun_family;  /* AF_UNIX */
+    char sun_path[108];
+};
+
+struct addrinfo
+{
+  int ai_flags;			/* Input flags.  */
+  int ai_family;		/* Protocol family for socket.  */
+  int ai_socktype;		/* Socket type.  */
+  int ai_protocol;		/* Protocol for socket.  */
+  socklen_t ai_addrlen;		/* Length of socket address.  */
+  struct sockaddr *ai_addr;	/* Socket address for socket.  */
+  char *ai_canonname;		/* Canonical name for service location.  */
+  struct addrinfo *ai_next;	/* Pointer to next in list.  */
+};
+
+//FIXME
+/* Maximum queue length specifiable by listen.  */
+#define SOMAXCONN	4
+
+#define EAI_AGAIN	2
+#define EAI_BADFLAGS	3
+#define EAI_FAIL	4
+#define EAI_FAMILY	5
+#define EAI_MEMORY	6
+#define EAI_NONAME	7
+#define EAI_SERVICE	9
+#define EAI_SOCKTYPE	10
+
+#define AI_CANONNAME	0x0004
+#define AI_NUMERICHOST	0x0008
+#define AI_PASSIVE	0x0020
+
+#define NI_MAXHOST	1025
+#define NI_MAXSERV	32
+
+static inline
+int getaddrinfo(const char *node, const char *service,
+                       const struct addrinfo *hints,
+                       struct addrinfo **res) {
+	DPRINT();
+	return EAI_FAIL;
+}
+
+extern
+void freeaddrinfo(struct addrinfo *res);
+
+#define NI_DGRAM 1
+#define NI_NUMERICSERV 2
+extern
+int getnameinfo(const struct sockaddr *sa, socklen_t salen,
+                       char *host, size_t hostlen,
+                       char *serv, size_t servlen, int flags);
+
+
+#define SIG_BLOCK 1
+#define SIG_SETMASK 2
+
+typedef int sigset_t;
+
+static inline
+int pthread_sigmask(int how, const sigset_t *set, sigset_t *oldset) {
+	DPRINT();
+	return 0;
+}
+
+static inline
+int sigfillset(sigset_t *set) {
+	DPRINT();
+	*set = -1;
+	return 0;
+}
+
+extern
+int socketpair(int domain, int type, int protocol, int sv[2]);
+extern
+int sockatmark(int sockfd);
+
+extern
+char *if_indextoname(unsigned ifindex, char *ifname);
+extern
+unsigned if_nametoindex(const char *ifname);
 
 #endif /* SAMBA_EMBOX_COMPAT_H_ */
---
+++ third-party/boost/include/wchar.h
@@ -0,0 +1,14 @@
+/*
+ * wchar.h
+ *
+ *  Created on: 2013-okt-07
+ *      Author: fsulima
+ */
+
+#ifndef WCHAR_H_
+#define WCHAR_H_
+
+typedef int wctype_t;
+typedef int wctrans_t;
+
+#endif /* WCHAR_H_ */
