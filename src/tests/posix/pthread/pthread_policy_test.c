8ffa2829a9952fd1e7e9f7cff2b4c1e11cdfab63
Vita Loginova <vita.loginova@gmail.com>
2013-10-10 13:12:41 +0000

    pthread: policy test with new POSIX functions

--- src/tests/posix/pthread/pthread_policy_test.c
+++ src/tests/posix/pthread/pthread_policy_test.c
@@ -7,78 +7,76 @@
 
 #include <pthread.h>
 #include <embox/test.h>
-#include <kernel/thread.h>
-#include <err.h>
-#include <kernel/sched.h>
 
 static pthread_t low, medium, high, low_other;
+pthread_attr_t *low_attr_p, *medium_attr_p, *high_attr_p;
 
 EMBOX_TEST_SUITE("posix/scheduling policy");
 
-TEST_SETUP(setup);
+static void *low_run(void *arg);
+static void *low_other_run(void *arg);
+static void *medium_run(void *arg);
+static void *high_run(void *arg);
 
 TEST_CASE("SCHED_FIFO corectness") {
-	test_assert_zero(thread_launch(low));
-	test_assert_zero(thread_join(low, NULL));
-	test_assert_zero(thread_join(low_other, NULL));
-	test_assert_zero(thread_join(medium, NULL));
-	test_assert_zero(thread_join(high, NULL));
-	test_assert_emitted("abcdefd");
+	sched_priority_t low_p = 200, medium_p = 245, high_p = 250;
+	struct sched_param low_param, medium_param, high_param;
+	pthread_attr_t low_attr, medium_attr, high_attr;
+
+	low_attr_p = &low_attr;
+	medium_attr_p = &medium_attr;
+	high_attr_p = &high_attr;
+
+	low_param.sched_priority = low_p;
+	medium_param.sched_priority = medium_p;
+	high_param.sched_priority = high_p;
+
+	pthread_attr_init(&low_attr);
+	pthread_attr_init(&medium_attr);
+	pthread_attr_init(&high_attr);
+
+	pthread_attr_setschedparam(&low_attr, &low_param);
+	pthread_attr_setschedparam(&medium_attr, &medium_param);
+	pthread_attr_setschedparam(&high_attr, &high_param);
+
+	pthread_attr_setschedpolicy(&low_attr, SCHED_OTHER);
+	pthread_attr_setschedpolicy(&medium_attr, SCHED_FIFO);
+	pthread_attr_setschedpolicy(&high_attr, SCHED_FIFO);
+
+	test_assert_zero(pthread_create(&low, &low_attr, low_run, NULL));
+
+	test_assert_zero(pthread_join(low, NULL));
+	test_assert_zero(pthread_join(low_other, NULL));
+	test_assert_zero(pthread_join(medium, NULL));
+	test_assert_zero(pthread_join(high, NULL));
+	test_assert_emitted("abcdefg");
 }
 
 static void *low_run(void *arg) {
 	test_emit('a');
-	thread_launch(medium);
-	thread_launch(low_other);
-	test_emit('f');
+	test_assert_zero(pthread_create(&medium, medium_attr_p, medium_run, NULL));
+	test_assert_zero(pthread_create(&low_other, low_attr_p, low_other_run, NULL));
+	for (int i = 0; i < 20000000; i++);
+	test_emit('g');
 	return NULL;
 }
 
 static void *low_other_run(void *arg) {
-	for (int i = 0; i < 1000000; i++);
-	test_emit('d');
+	test_emit('f');
 	return NULL;
 }
 
 static void *medium_run(void *arg) {
 	test_emit('b');
-	thread_launch(high);
-	for (int i = 0; i < 1000000; i++);
+	test_assert_zero(pthread_create(&high, high_attr_p, high_run, NULL));
+	for (int i = 0; i < 20000000; i++);
 	test_emit('e');
 	return NULL;
 }
 
 static void *high_run(void *arg) {
 	test_emit('c');
-	for (int i = 0; i < 1000000; i++);
+	for (int i = 0; i < 20000000; i++);
 	test_emit('d');
 	return NULL;
-}
-
-static int setup(void) {
-	sched_priority_t low_p = 200, medium_p = 245, high_p = 250;
-	struct sched_param low_param, medium_param, high_param;
-
-	low_param.sched_priority = low_p;
-	medium_param.sched_priority = medium_p;
-	high_param.sched_priority = high_p;
-
-	low = thread_create(THREAD_FLAG_SUSPENDED, low_run, NULL);
-	test_assert_zero(err(low));
-
-	low_other = thread_create(THREAD_FLAG_SUSPENDED, low_other_run, NULL);
-	test_assert_zero(err(low_other));
-
-	medium = thread_create(THREAD_FLAG_SUSPENDED, medium_run, NULL);
-	test_assert_zero(err(medium));
-
-	high = thread_create(THREAD_FLAG_SUSPENDED, high_run, NULL);
-	test_assert_zero(err(high));
-
-	pthread_setschedparam(low, SCHED_OTHER, &low_param);
-	pthread_setschedparam(low_other, SCHED_OTHER, &low_param);
-	pthread_setschedparam(medium, SCHED_FIFO, &medium_param);
-	pthread_setschedparam(high, SCHED_FIFO, &high_param);
-
-	return 0;
 }
