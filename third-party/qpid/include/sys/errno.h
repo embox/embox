8802e6a930e9c6609ff87bb5a5e64d0e27c53e8d
Felix Sulima <fsulima@gmail.com>
2013-10-11 13:52:54 +0000

    qpid: making progress

---
+++ third-party/qpid/include/sys/errno.h
@@ -0,0 +1,15 @@
+/*
+ * errno.h
+ *
+ *  Created on: 2013-okt-11
+ *      Author: fsulima
+ */
+
+#include <errno.h>
+
+#ifndef ERRNO_H_
+#define ERRNO_H_
+
+
+
+#endif /* ERRNO_H_ */
--- third-party/qpid/qpid_embox_compat.h
+++ third-party/qpid/qpid_embox_compat.h
@@ -21,7 +21,7 @@
 #include <string.h>
 
 #if 1
-#define DPRINT() printf(">>> gcc CALL %s\n", __FUNCTION__)
+#define DPRINT() printf(">>> qpid CALL %s\n", __FUNCTION__)
 #else
 #define DPRINT()
 #endif
@@ -59,6 +59,85 @@ int scandir(const char *dirp, struct dirent ***namelist,
 #define F_ULOCK 2
 extern
 int lockf(int fd, int cmd, off_t len);
+
+using std::sysconf;
+#define _SC_PAGE_SIZE _SC_PAGESIZE
+
+#include <errno.h>
+
+#define MAP_SHARED    0x00
+//#define MAP_PRIVATE   0x01
+//#define PROT_READ     0x10
+#define PROT_WRITE    0x20
+#define MAP_FAILED    ((void*)(-1))
+static inline void  *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off) {
+	// ToDo: implement for InitFS files
+	(void)addr;
+	(void)len;
+	(void)prot;
+	(void)flags;
+	(void)off;
+	printf(">>> mmap(%i)\n",fd);
+	errno = EPERM;
+	return 0;
+}
+
+static inline int munmap(void *addr, size_t size) {
+	(void)size;
+	printf(">>> munmap(%p)\n",addr);
+	errno = EPERM;
+	return -1;
+}
+
+#define MS_ASYNC 1
+static inline
+int msync(void *addr, size_t length, int flags) {
+	(void)addr;
+	(void)length;
+	(void)flags;
+	printf(">>> msync(%p)\n",addr);
+	errno = ENOMEM;
+	return -1;
+}
+
+extern
+int socketpair(int domain, int type, int protocol, int sv[2]);
+
+#include <netinet/in.h>
+
+struct addrinfo
+{
+  int ai_flags;			/* Input flags.  */
+  int ai_family;		/* Protocol family for socket.  */
+  int ai_socktype;		/* Socket type.  */
+  int ai_protocol;		/* Protocol for socket.  */
+  socklen_t ai_addrlen;		/* Length of socket address.  */
+  struct sockaddr *ai_addr;	/* Socket address for socket.  */
+  char *ai_canonname;		/* Canonical name for service location.  */
+  struct addrinfo *ai_next;	/* Pointer to next in list.  */
+};
+
+/* Structure large enough to hold any socket address (with the historical
+   exception of AF_UNIX).  We reserve 128 bytes.  */
+#define __ss_aligntype	unsigned long int
+#define _SS_SIZE	128
+#define _SS_PADSIZE	(_SS_SIZE - (2 * sizeof (__ss_aligntype)))
+
+#define	__SOCKADDR_COMMON(sa_prefix) \
+  sa_family_t sa_prefix##family
+
+#define __SOCKADDR_COMMON_SIZE	(sizeof (unsigned short int))
+
+struct sockaddr_storage
+  {
+    __SOCKADDR_COMMON (ss_);	/* Address family, etc.  */
+    __ss_aligntype __ss_align;	/* Force desired alignment.  */
+    char __ss_padding[_SS_PADSIZE];
+  };
+
+
+#define IPV6_V6ONLY 0
+#define TCP_NODELAY 0
 
 #ifdef __cplusplus
 
