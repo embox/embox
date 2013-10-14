9efef9fb0afa575aa8b35db5714e78747a81568d
Anton Kozlov <drakon.mega@gmail.com>
2013-10-14 15:44:16 +0000

    omap: clk and ti8168 splitted (OMAP was unable to boot)

--- src/drivers/clock/omap3_clk.c
+++ src/drivers/clock/omap3_clk.c
@@ -16,38 +16,7 @@
 
 #include <embox/unit.h>
 
-EMBOX_UNIT_INIT(ti_clk_init);
-
-/* ti8168's timer1 related */
-
-#define TI8168_CLKIN_HZ 27000000
-
-#define CM_TIMER1_CLKSEL 0x48180390
-#define CM_CLKIN 0x2
-
-#define GPTIMER31_01_TIDR 0xfff1301
-#define GPTIMER31_01_MASK 0xfffffff
-
-#define TI8168_GPTIMER1_BASE ((void *) 0x4802e000)
-#define TI8168_GPTIMER1_IRQ 67
-
-#define TI8168_LOAD_VALUE (0xffffffff - (TI8168_CLKIN_HZ / 1000) + 1)
-
-struct gptimer13_1 {
-	uint32_t tidr; 		/* 0x00 */
-	char unused0[12];	/* 0x04 */
-	uint32_t cfg;		/* 0x10 */
-	char unused1[12];	/* 0x14 */
-	uint32_t eoi;		/* 0x20 */
-	uint32_t irqstat_raw;	/* 0x24 */
-	uint32_t irqstat;	/* 0x28 */
-	uint32_t irqenable_set;	/* 0x2c */
-	uint32_t irqenable_clr;	/* 0x30 */
-	uint32_t irqwakeen;	/* 0x34 */
-	uint32_t tclr;		/* 0x38 */
-	uint32_t tcrr;		/* 0x3c */
-	uint32_t tldr;		/* 0x40 */
-} __attribute__((packed));
+EMBOX_UNIT_INIT(omap_clk_init);
 
 /* omap's gptimer */
 
@@ -92,12 +61,12 @@ struct gptimerxx_x {
 #define GPTIMER_TIER_OVERFLOW   (1 << 1)
 #define GPTIMER_TISR_OVERFLOW   (1 << 1)
 
-static cycle_t common_clk_read(void) {
+static cycle_t omap_clk_read(void) {
 	return 0;
 }
 
-static struct time_counter_device common_clk_counter = {
-	.read = common_clk_read,
+static struct time_counter_device omap_clk_counter = {
+	.read = omap_clk_read,
 };
 
 static irq_return_t clock_handler(unsigned int irq_nr, void *data) {
@@ -139,65 +108,11 @@ static struct time_event_device omap3_clk_event = {
 static struct clock_source omap3_clk_clock_source = {
 	.name = "omap3_clk",
 	.event_device = &omap3_clk_event,
-	.counter_device = &common_clk_counter,
+	.counter_device = &omap_clk_counter,
 	.read = clock_source_read,
 };
 
-irq_return_t ti8168_clock_handler(unsigned int irq_nr, void *data) {
-	volatile struct gptimer13_1 *gptimer = TI8168_GPTIMER1_BASE;
-
-	clock_tick_handler(irq_nr, data);
-
-	REG_STORE(&gptimer->irqstat, GPTIMER_TISR_OVERFLOW);
-	REG_STORE(&gptimer->eoi, 0);
-
-	return IRQ_HANDLED;
-}
-
-static int ti8168_clk_config(struct time_dev_conf *conf) {
-	volatile struct gptimer13_1 *gptimer = TI8168_GPTIMER1_BASE;
-
-	REG_STORE(CM_TIMER1_CLKSEL, CM_CLKIN);
-
-	REG_STORE(&gptimer->cfg, 0x01);
-
-	while (REG_LOAD(&gptimer->cfg) & 0x01);
-
-	REG_STORE(&gptimer->tcrr, TI8168_LOAD_VALUE);
-	REG_STORE(&gptimer->tldr, TI8168_LOAD_VALUE);
-
-	REG_STORE(&gptimer->tclr, GPTIMER_TCLR_START | GPTIMER_TCLR_AUTORELOAD);
-
-	REG_STORE(&gptimer->irqenable_set, GPTIMER_TIER_OVERFLOW);
-	REG_STORE(&gptimer->irqwakeen, GPTIMER_TIER_OVERFLOW);
-
-	return 0;
-}
-
-static struct time_event_device ti8168_clk_event = {
-	.config = ti8168_clk_config,
-	.resolution = 1000,
-	.irq_nr = TI8168_GPTIMER1_IRQ,
-};
-
-struct clock_source ti8168_clk_clock_source = {
-	.name = "ti8168",
-	.event_device = &ti8168_clk_event,
-	.counter_device = &common_clk_counter,
-	.read = clock_source_read,
-};
-
-static bool gptimer_detect(uint32_t tidr_val, uint32_t mask, void *base) {
-	return (mask & REG_LOAD(base)) == (mask & tidr_val);
-}
-
-static int ti_clk_init(void) {
-	if (gptimer_detect(GPTIMER31_01_TIDR, GPTIMER31_01_MASK, TI8168_GPTIMER1_BASE)) {
-		printk("TI8168 gptimer detected on %p\n", TI8168_GPTIMER1_BASE);
-		clock_source_register(&ti8168_clk_clock_source);
-		return irq_attach(ti8168_clk_event.irq_nr, ti8168_clock_handler, 0, &ti8168_clk_clock_source, "omap3_clk");
-
-	}
+static int omap_clk_init(void) {
 
 	clock_source_register(&omap3_clk_clock_source);
 	return irq_attach(GPTIMER1_IRQ, clock_handler, 0, &omap3_clk_clock_source, "omap3_clk");
---
+++ src/drivers/clock/ti_8168.c
@@ -0,0 +1,114 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Kozlov
+ * @date    14.10.2013
+ */
+
+#include <drivers/irqctrl.h>
+#include <hal/clock.h>
+#include <hal/reg.h>
+#include <hal/system.h>
+#include <kernel/irq.h>
+#include <kernel/time/clock_source.h>
+#include <kernel/printk.h>
+
+#include <embox/unit.h>
+
+EMBOX_UNIT_INIT(ti8168_clk_init);
+
+#define TI8168_CLKIN_HZ 27000000
+
+#define CM_TIMER1_CLKSEL 0x48180390
+#define CM_CLKIN 0x2
+
+#define GPTIMER31_01_TIDR 0xfff1301
+#define GPTIMER31_01_MASK 0xfffffff
+
+#define TI8168_GPTIMER1_BASE ((void *) 0x4802e000)
+#define TI8168_GPTIMER1_IRQ 67
+
+#define TI8168_LOAD_VALUE (0xffffffff - (TI8168_CLKIN_HZ / 1000) + 1)
+
+/* common register values */
+
+#define GPTIMER_TCLR_START	(1 << 0)
+#define GPTIMER_TCLR_AUTORELOAD (1 << 1)
+
+#define GPTIMER_TIER_OVERFLOW   (1 << 1)
+#define GPTIMER_TISR_OVERFLOW   (1 << 1)
+
+struct gptimer13_1 {
+	uint32_t tidr; 		/* 0x00 */
+	char unused0[12];	/* 0x04 */
+	uint32_t cfg;		/* 0x10 */
+	char unused1[12];	/* 0x14 */
+	uint32_t eoi;		/* 0x20 */
+	uint32_t irqstat_raw;	/* 0x24 */
+	uint32_t irqstat;	/* 0x28 */
+	uint32_t irqenable_set;	/* 0x2c */
+	uint32_t irqenable_clr;	/* 0x30 */
+	uint32_t irqwakeen;	/* 0x34 */
+	uint32_t tclr;		/* 0x38 */
+	uint32_t tcrr;		/* 0x3c */
+	uint32_t tldr;		/* 0x40 */
+} __attribute__((packed));
+
+irq_return_t ti8168_clock_handler(unsigned int irq_nr, void *data) {
+	volatile struct gptimer13_1 *gptimer = TI8168_GPTIMER1_BASE;
+
+	clock_tick_handler(irq_nr, data);
+
+	REG_STORE(&gptimer->irqstat, GPTIMER_TISR_OVERFLOW);
+	REG_STORE(&gptimer->eoi, 0);
+
+	return IRQ_HANDLED;
+}
+
+static int ti8168_clk_config(struct time_dev_conf *conf) {
+	volatile struct gptimer13_1 *gptimer = TI8168_GPTIMER1_BASE;
+
+	REG_STORE(CM_TIMER1_CLKSEL, CM_CLKIN);
+
+	REG_STORE(&gptimer->cfg, 0x01);
+
+	while (REG_LOAD(&gptimer->cfg) & 0x01);
+
+	REG_STORE(&gptimer->tcrr, TI8168_LOAD_VALUE);
+	REG_STORE(&gptimer->tldr, TI8168_LOAD_VALUE);
+
+	REG_STORE(&gptimer->tclr, GPTIMER_TCLR_START | GPTIMER_TCLR_AUTORELOAD);
+
+	REG_STORE(&gptimer->irqenable_set, GPTIMER_TIER_OVERFLOW);
+	REG_STORE(&gptimer->irqwakeen, GPTIMER_TIER_OVERFLOW);
+
+	return 0;
+}
+
+static cycle_t ti8168_clk_read(void) {
+	return 0;
+}
+
+static struct time_counter_device ti8168_clk_counter = {
+	.read = ti8168_clk_read,
+};
+
+static struct time_event_device ti8168_clk_event = {
+	.config = ti8168_clk_config,
+	.resolution = 1000,
+	.irq_nr = TI8168_GPTIMER1_IRQ,
+};
+
+struct clock_source ti8168_clk_clock_source = {
+	.name = "ti8168",
+	.event_device = &ti8168_clk_event,
+	.counter_device = &ti8168_clk_counter,
+	.read = clock_source_read,
+};
+
+static int ti8168_clk_init(void) {
+
+	clock_source_register(&ti8168_clk_clock_source);
+	return irq_attach(ti8168_clk_event.irq_nr, ti8168_clock_handler, 0, &ti8168_clk_clock_source, "omap3_clk");
+}
