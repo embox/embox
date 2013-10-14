2833de2e9894723ded1bd667b73ee93a0599264b
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-10-14 16:03:02 +0000

    signal: (minor) prettify POSIX header

--- src/compat/posix/include/signal.h
+++ src/compat/posix/include/signal.h
@@ -11,85 +11,87 @@
 #define SIGNAL_H_
 
 #include <sys/types.h>
-#include <kernel/task/signal.h>
+// #include <kernel/task/signal.h>
 
-extern int kill(int tid, int sig);
-
-typedef void (*signal_hndfn)(int);
-
-extern signal_hndfn signal(int sig, signal_hndfn fn);
-
-#define SIG_DFL ((signal_hndfn) 0x1)
-#define SIG_IGN ((signal_hndfn) 0x3)
-#define SIG_ERR ((signal_hndfn) 0x5)
-
-extern int sigqueue(int tid, int sig, const union sigval value);
-
-static inline int raise(int sig) {
-	return 0;
-}
+#define SIG_DFL ((__signal_hnd_t) 0x1)
+#define SIG_IGN ((__signal_hnd_t) 0x3)
+#define SIG_ERR ((__signal_hnd_t) 0x5)
 
 /* Signals.  */
-#define	SIGHUP		1	/* Hangup (POSIX).  */
-#define	SIGINT		2	/* Interrupt (ANSI).  */
-#define	SIGQUIT		3	/* Quit (POSIX).  */
-#define	SIGILL		4	/* Illegal instruction (ANSI).  */
-#define	SIGTRAP		5	/* Trace trap (POSIX).  */
-#define	SIGABRT		6	/* Abort (ANSI).  */
-#define	SIGIOT		6	/* IOT trap (4.2 BSD).  */
-#define	SIGBUS		7	/* BUS error (4.2 BSD).  */
-#define	SIGFPE		8	/* Floating-point exception (ANSI).  */
-#define	SIGKILL		9	/* Kill, unblockable (POSIX).  */
-#define	SIGUSR1		10	/* User-defined signal 1 (POSIX).  */
-#define	SIGSEGV		11	/* Segmentation violation (ANSI).  */
-#define	SIGUSR2		12	/* User-defined signal 2 (POSIX).  */
-#define	SIGPIPE		13	/* Broken pipe (POSIX).  */
-#define	SIGALRM		14	/* Alarm clock (POSIX).  */
-#define	SIGTERM		15	/* Termination (ANSI).  */
-#define	SIGSTKFLT	16	/* Stack fault.  */
-#define	SIGCLD		SIGCHLD	/* Same as SIGCHLD (System V).  */
-#define	SIGCHLD		17	/* Child status has changed (POSIX).  */
-#define	SIGCONT		18	/* Continue (POSIX).  */
-#define	SIGSTOP		19	/* Stop, unblockable (POSIX).  */
-#define	SIGTSTP		20	/* Keyboard stop (POSIX).  */
-#define	SIGTTIN		21	/* Background read from tty (POSIX).  */
-#define	SIGTTOU		22	/* Background write to tty (POSIX).  */
-#define	SIGURG		23	/* Urgent condition on socket (4.2 BSD).  */
-#define	SIGXCPU		24	/* CPU limit exceeded (4.2 BSD).  */
-#define	SIGXFSZ		25	/* File size limit exceeded (4.2 BSD).  */
-#define	SIGVTALRM	26	/* Virtual alarm clock (4.2 BSD).  */
-#define	SIGPROF		27	/* Profiling alarm clock (4.2 BSD).  */
-#define	SIGWINCH	28	/* Window size change (4.3 BSD, Sun).  */
-#define	SIGPOLL		SIGIO	/* Pollable event occurred (System V).  */
-#define	SIGIO		29	/* I/O now possible (4.2 BSD).  */
-#define	SIGPWR		30	/* Power failure restart (System V).  */
-#define SIGSYS		31	/* Bad system call.  */
-#define SIGUNUSED	31
+#define SIGHUP      1       /* (POSIX)    Hangup.  */
+#define SIGINT      2       /* (ANSI)     Interrupt.  */
+#define SIGQUIT     3       /* (POSIX)    Quit.  */
+#define SIGILL      4       /* (ANSI)     Illegal instruction.  */
+#define SIGTRAP     5       /* (POSIX)    Trace trap.  */
+#define SIGABRT     6       /* (ANSI)     Abort.  */
+#define SIGIOT      6       /* (4.2 BSD)  IOT trap.  */
+#define SIGBUS      7       /* (4.2 BSD)  BUS error.  */
+#define SIGFPE      8       /* (ANSI)     Floating-point exception.  */
+#define SIGKILL     9       /* (POSIX)    Kill, unblockable.  */
+#define SIGUSR1     10      /* (POSIX)    User-defined signal 1.  */
+#define SIGSEGV     11      /* (ANSI)     Segmentation violation.  */
+#define SIGUSR2     12      /* (POSIX)    User-defined signal 2.  */
+#define SIGPIPE     13      /* (POSIX)    Broken pipe.  */
+#define SIGALRM     14      /* (POSIX)    Alarm clock.  */
+#define SIGTERM     15      /* (ANSI)     Termination.  */
+#define SIGSTKFLT   16      /*            Stack fault.  */
+#define SIGCLD      SIGCHLD /* (System V) Same as SIGCHLD.  */
+#define SIGCHLD     17      /* (POSIX)    Child status has changed.  */
+#define SIGCONT     18      /* (POSIX)    Continue.  */
+#define SIGSTOP     19      /* (POSIX)    Stop, unblockable.  */
+#define SIGTSTP     20      /* (POSIX)    Keyboard stop.  */
+#define SIGTTIN     21      /* (POSIX)    Background read from tty.  */
+#define SIGTTOU     22      /* (POSIX)    Background write to tty.  */
+#define SIGURG      23      /* (4.2 BSD)  Urgent condition on socket.  */
+#define SIGXCPU     24      /* (4.2 BSD)  CPU limit exceeded.  */
+#define SIGXFSZ     25      /* (4.2 BSD)  File size limit exceeded.  */
+#define SIGVTALRM   26      /* (4.2 BSD)  Virtual alarm clock.  */
+#define SIGPROF     27      /* (4.2 BSD)  Profiling alarm clock.  */
+#define SIGWINCH    28      /* (4.3 BSD, Sun) Window size change.  */
+#define SIGPOLL     SIGIO   /* (System V) Pollable event occurred.  */
+#define SIGIO       29      /* (4.2 BSD)  I/O now possible.  */
+#define SIGPWR      30      /* (System V) Power failure restart.  */
+#define SIGSYS      31      /*            Bad system call.  */
+#define SIGUNUSED   31
 
 #define SIGRTMIN    TASK_SIGRTMIN
 #define SIGRTMAX    TASK_SIGRTMAX
 
 typedef int sigset_t;
 
+union sigval {
+	int   sival_int;
+	void *sival_ptr;
+};
+
 typedef struct {
-	int si_signo;
-	int si_code;
-	union sigval si_value;
-	int si_errno;
-	pid_t si_pid;
-	uid_t si_uid;
-	void *si_addr;
-	int si_status;
-	int si_band;
+	int           si_signo;
+	int           si_code;
+	union sigval  si_value;
+	int           si_errno;
+	pid_t         si_pid;
+	uid_t         si_uid;
+	void         *si_addr;
+	int           si_status;
+	int           si_band;
 } siginfo_t;
 
 struct sigaction {
-    void (*sa_handler)(int);
-    void (*sa_sigaction)(int, siginfo_t *, void *);
-    sigset_t sa_mask;
-    int sa_flags;
-    void (*sa_restorer)(void);
+	void     (*sa_handler)(int);
+	void     (*sa_sigaction)(int, siginfo_t *, void *);
+	sigset_t   sa_mask;
+	int        sa_flags;
+	void     (*sa_restorer)(void);
 };
+
+typedef void (*__signal_hnd_t)(int);
+
+extern int kill(int tid, int sig);
+extern __signal_hnd_t signal(int sig, __signal_hnd_t fn);
+extern int sigqueue(int tid, int sig, const union sigval value);
+static inline int raise(int sig) {
+	return 0;
+}
 
 //static inline int sigaction(int sig, const struct sigaction *act,
 		//struct sigaction *oact) { return -1; }
