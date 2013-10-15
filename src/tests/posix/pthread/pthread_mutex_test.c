b7b3251ebc58c2f3080372bba01b201cb06bcf5b
Vita Loginova <vita.loginova@gmail.com>
2013-10-15 11:18:46 +0000

    test for mutex static initializer modified

--- src/tests/posix/pthread/pthread_mutex_test.c
+++ src/tests/posix/pthread/pthread_mutex_test.c
@@ -15,6 +15,7 @@ EMBOX_TEST_SUITE("posix/pthread_mutex api");
 TEST_CASE("Initialize mutex with PTHREAD_MUTEX_INITIALIZER macro") {
 	pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 	test_assert_zero(pthread_mutex_lock(&m));
+	test_assert_not_zero(pthread_mutex_trylock(&m));
 	test_assert_zero(pthread_mutex_unlock(&m));
 }
 
