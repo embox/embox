4333312d6a116c529e69e1fe0b23fb69766ed71f
Ilia Vaprol <ki.stfu@gmail.com>
2013-10-17 12:49:23 +0000

    framework: net: Rename net_pack::handle to rcv_pack

--- src/framework/net/pack/self_impl.h
+++ src/framework/net/pack/self_impl.h
@@ -14,15 +14,15 @@
 
 #include "types.h"
 
-#define __EMBOX_NET_PACK(_name, _type, _handle, _init, _fini)    \
+#define __EMBOX_NET_PACK(_name, _type, _rcv_pack, _init, _fini)  \
 	ARRAY_SPREAD_DECLARE(const struct net_pack,                  \
 			__net_pack_registry);                                \
 	ARRAY_SPREAD_ADD_NAMED(__net_pack_registry,                  \
 			__net_pack_##_name, {                                \
 				.type = _type,                                   \
+				.rcv_pack = _rcv_pack,                           \
 				.init = _init,                                   \
-				.fini = _fini,                                   \
-				.handle = _handle                                \
+				.fini = _fini                                    \
 			});                                                  \
 	extern const struct mod_ops __net_pack_mod_ops;              \
 	MOD_INFO_BIND(&__net_pack_mod_ops, &__net_pack_##_name)
