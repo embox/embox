embox/embox

commit 20ad36dc629ace4d7428159c27fd0e669f7ae810

lds: Use __stack instead of _stack_vma+_stack_len

src/kernel/thread/boot_thread.c
@@ -11,9 +11,15 @@
 #include <kernel/thread/state.h>
 #include <kernel/task.h>
 
+#include <module/embox/kernel/stack.h>
+
+#define STACK_SZ \
+ 	OPTION_MODULE_GET(embox__kernel__stack, NUMBER, stack_size)
+
 static void *boot_stub(void *arg) {
 	return NULL;
 }
+
 /*
  * Initializes thread structure for current thread, adds it to list of threads
  * and to kernel task. Use this ONLY for bootstrap threads.
@@ -26,7 +32,7 @@ struct thread *thread_init_self(void *stack, size_t stack_sz,
 	//thread->stack = stack + sizeof(struct thread);
 	//thread->stack_sz = stack_sz - sizeof(struct thread);
 	thread_stack_set(thread, stack + sizeof(struct thread));
-	thread_stack_set_size(thread, stack_sz + sizeof(struct thread));
+	thread_stack_set_size(thread, stack_sz - sizeof(struct thread));
 
 
 
@@ -49,10 +55,9 @@ struct thread *thread_init_self(void *stack, size_t stack_sz,
 struct thread *boot_thread_create(void) {
 	struct thread *bootstrap;
 	struct task *kernel_task = task_kernel_task();
-	extern char _stack_vma, _stack_len;
+	extern char __stack;
 
-//TODO calculate stack size
-	bootstrap = thread_init_self(&_stack_vma, (uint32_t) &_stack_len,
+	bootstrap = thread_init_self(&__stack, (size_t) STACK_SZ,
 			THREAD_PRIORITY_NORMAL);
 
 	task_add_thread(kernel_task, bootstrap);

src/arch/x86/boot/boot.S
@@ -23,8 +23,7 @@ C_ENTRY(_start):
 	cld
 
 	/* Initialize the stack pointer. */
-	movl    $_stack_vma, %esp  /* load base address for system stack */
-	addl    $_stack_len, %esp  /* setup correct system stack pointer */
+	movl    $__stack, %esp  /* load base address for system stack */
 
 	/* Reset EFLAGS. */
 	pushl   $0

commit adc0b4b8dddcc115b4063b052a60d68183402254

Fix a bug introduced in 20ad36d (alloc boot thread outside its stack)

src/kernel/thread/boot_thread.c
@@ -14,7 +14,7 @@
 #include <module/embox/kernel/stack.h>
 
 #define STACK_SZ \
- 	OPTION_MODULE_GET(embox__kernel__stack, NUMBER, stack_size)
+	((size_t) OPTION_MODULE_GET(embox__kernel__stack, NUMBER, stack_size))
 
 static void *boot_stub(void *arg) {
 	return NULL;
@@ -33,8 +33,6 @@ struct thread *thread_init_self(void *stack, size_t stack_sz,
 	//thread->stack_sz = stack_sz - sizeof(struct thread);
 	thread_stack_set(thread, stack + sizeof(struct thread));
 	thread_stack_set_size(thread, stack_sz - sizeof(struct thread));
-
-
 
 	/* General initialization and task setting up */
 	thread_init(thread, 0, boot_stub, NULL);
@@ -57,7 +55,7 @@ struct thread *boot_thread_create(void) {
 	struct task *kernel_task = task_kernel_task();
 	extern char __stack;
 
-	bootstrap = thread_init_self(&__stack, (size_t) STACK_SZ,
+	bootstrap = thread_init_self(&__stack - STACK_SZ, STACK_SZ,
 			THREAD_PRIORITY_NORMAL);
 
 	thread_register(kernel_task, bootstrap);