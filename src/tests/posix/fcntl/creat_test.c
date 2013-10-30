94ac031f865dc811f834f4e7d024ea6c37930e57
Anton Bondarev <anton.bondarev2310@gmail.com>
2013-10-30 11:02:25 +0000

    idesc: Add some stubs for testing POSIX standard

---
+++ src/tests/posix/fcntl/creat_test.c
@@ -0,0 +1,71 @@
+/**
+ * @file
+ *
+ * @date Oct 30, 2013
+ * @author: Anton Bondarev
+ */
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <drivers/ramdisk.h>
+#include <fs/fsop.h>
+
+#include <embox/test.h>
+
+#include <err.h>
+
+EMBOX_TEST_SUITE("posix/fctl/creat test");
+
+TEST_SETUP_SUITE(setup_suite);
+
+TEST_TEARDOWN_SUITE(teardown_suite);
+
+#define FS_NAME         "vfat"
+#define FS_DEV          "/dev/ram_test"
+#define FS_DIR          "/test"
+#define MKDIR_PERM      0700
+
+#define FS_BLOCKS       124
+
+static int setup_suite(void) {
+	int res;
+
+	res = err(ramdisk_create(FS_DEV, FS_BLOCKS * sysconf(_SC_PAGESIZE)));
+	if (res != 0) {
+		return res;
+	}
+
+	/* format filesystem */
+	if (0 != (res = format(FS_DEV, FS_NAME))) {
+		return res;
+	}
+
+	if (0 != (res = mkdir(FS_DIR, MKDIR_PERM))) {
+		return res;
+	}
+
+	/* mount filesystem */
+	if (0 != (res = mount(FS_DEV, FS_DIR, FS_NAME))) {
+		return res;
+	}
+
+	return 0;
+}
+
+static int teardown_suite(void) {
+	int res;
+
+	if (0 != (res = umount(FS_DIR))) {
+		return res;
+	}
+
+	if (0 != rmdir(FS_DIR)) {
+		return res;
+	}
+
+	if (0 != (res = ramdisk_delete(FS_DEV))) {
+		return res;
+	}
+
+	return 0;
+}
