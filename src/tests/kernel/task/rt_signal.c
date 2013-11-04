1bef1bcd0a3bc3433f11832d4eae0175d50d5c4b
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-11-04 23:22:46 +0000

    signal: Fix rt_signal text to use only POSIX signal interface

--- src/tests/kernel/task/rt_signal.c
+++ src/tests/kernel/task/rt_signal.c
@@ -8,8 +8,7 @@
  */
 
 #include <embox/test.h>
-#include <kernel/task.h>
-#include <kernel/task/rt_signal.h>
+
 #include <signal.h>
 #include <unistd.h>
 
@@ -17,42 +16,49 @@ EMBOX_TEST_SUITE("test for real-time signals");
 
 #define MY_SIGRT (SIGRTMIN + 1)
 
-static int handled_sig_cnt;
-
-static void sig_handler(int sig, union sigval value) {
-	test_emit('0' + (char)value.sival_int);
-	handled_sig_cnt++;
+static void test_sig_handler(int sig, siginfo_t *info, void *ctx) {
+	test_emit('0' + (char) info->si_value.sival_int);
 }
 
-static void *task_hnd(void *data) {
-	handled_sig_cnt=0;
-	task_self_rtsignal_set(MY_SIGRT, sig_handler);
-	while(1);
+static void *test_task_entry(void *data) {
+	struct sigaction act  = { 0 };
+	int err;
+
+	act.sa_sigaction = test_sig_handler;
+
+	err = sigaction(MY_SIGRT, &act, NULL);
+	test_assert_zero(err);
+
+	while(1)
+		;
 	return NULL;
+}
+
+static void test_send_sigval(int tid, int val) {
+	union sigval value;
+	int err;
+
+	value.sival_int = val;
+	err = sigqueue(tid, MY_SIGRT, value);
+	test_assert_zero(err);
 }
 
 TEST_CASE("send 3 rt signals of the same type between two tasks and check"
 		" of handling count and order") {
-	union sigval value;
-	int tid = new_task("", task_hnd, NULL);
+	int tid;
 
+	tid = new_task("", test_task_entry, NULL);
 	test_assert(tid >= 0);
 
 	sleep(0);
 
 	/* Send three real-time signals with the same type. */
 
-	value.sival_int = 1;
-	sigqueue(tid, MY_SIGRT, value);
+	test_send_sigval(tid, 1);
+	test_send_sigval(tid, 2);
+	test_send_sigval(tid, 3);
 
-	value.sival_int = 2;
-	sigqueue(tid, MY_SIGRT, value);
-
-	value.sival_int = 3;
-	sigqueue(tid, MY_SIGRT, value);
-
-	/* Wait until all signals will be handled. */
-	while (handled_sig_cnt != 3) {}
+	sleep(0);
 
 	/* Check order, in witch signals were handled. */
 	test_assert_emitted("123");
