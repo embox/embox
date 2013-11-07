47b645673c9f946ffb19bb53247943e9d3c4cb39
Felix Sulima <fsulima@gmail.com>
2013-11-07 14:19:38 +0000

    qpid: maintain buildability

--- third-party/qpid/qpid_embox_compat.h
+++ third-party/qpid/qpid_embox_compat.h
@@ -148,18 +148,9 @@ typedef unsigned int uint;
 
 #define SIG_SETMASK 2
 
-typedef int sigset_t;
-
 static inline
 int pthread_sigmask(int how, const sigset_t *set, sigset_t *oldset) {
 	DPRINT();
-	return 0;
-}
-
-static inline
-int sigfillset(sigset_t *set) {
-	DPRINT();
-	*set = -1;
 	return 0;
 }
 
