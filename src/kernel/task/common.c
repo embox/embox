dbf3fa1463640560f1a38224811e4eb749b71d48
Anton Bondarev <anton.bondarev2310@gmail.com>
2013-11-12 15:57:26 +0000

    idesc: Split task module for difference files

--- src/kernel/task/common.c
+++ src/kernel/task/common.c
@@ -7,8 +7,6 @@
  * @author Anton Bondarev
  */
 
-#include <embox/unit.h>
-
 #include <errno.h>
 #include <string.h>
 
@@ -19,23 +17,19 @@
 
 #include "common.h"
 
-#include <module/embox/kernel/task/api.h>
-
-EMBOX_UNIT_INIT(kernel_task_init);
-
-#define MAX_RES_SUM_SIZE OPTION_MODULE_GET(embox__kernel__task__api, NUMBER, max_resource_size)
-
-struct kernel_task {
-	struct task task;
-	char resource[MAX_RES_SUM_SIZE];
-};
-
-static struct kernel_task kernel_task;
 
 ARRAY_SPREAD_DEF(const struct task_resource_desc *, task_resource_desc_array);
 ARRAY_SPREAD_DEF(const task_notifing_resource_hnd, task_notifing_resource);
 
 static size_t resource_sum_size = 0;
+
+void resource_sum_size_calc(void) {
+	const struct task_resource_desc *res_desc;
+
+	task_resource_foreach(res_desc) {
+		resource_sum_size += res_desc->resource_size;
+	}
+}
 
 size_t task_resource_sum_size(void) {
 	return resource_sum_size;
@@ -45,9 +39,6 @@ size_t task_size(void) {
 	return sizeof(struct task) + resource_sum_size;
 }
 
-struct task *task_kernel_task(void) {
-	return &kernel_task.task;
-}
 
 struct task *task_init(void *task_n_res_space, size_t size) {
 	struct task *task = (struct task *) task_n_res_space;
@@ -81,30 +72,4 @@ struct task *task_init(void *task_n_res_space, size_t size) {
 
 	return task;
 
-}
-
-static inline void resource_sum_size_calc(void) {
-	const struct task_resource_desc *res_desc;
-
-	task_resource_foreach(res_desc) {
-		resource_sum_size += res_desc->resource_size;
-	}
-}
-
-
-static int kernel_task_init(void) {
-	struct task *task;
-	resource_sum_size_calc();
-
-	task = task_kernel_task();
-
-	if (!task_init(task, sizeof(kernel_task))) {
-		return -ENOMEM;
-	}
-
-	dlist_init(&task->task_link); /* it's out list handler */
-
-	strncpy(task->task_name, "kernel", sizeof(task_kernel_task()->task_name) - 1);
-
-	return 0;
 }
