af00f3bbeeda679372dab675bb296052a3e49eea
Andrey Kokorev <anix97@gmail.com>
2013-11-12 20:47:02 +0000

    Qemu launching script improved

--- scripts/qemu/win_launcher/launch.bat
+++ scripts/qemu/win_launcher/launch.bat
@@ -1,1 +1,16 @@
-start "name" /D"c:\Program Files\qemu\" qemu.exe -kernel d:/projects/embox/build/base/bin/embox -net nic,model=ne2k_pci,macaddr=AA:BB:CC:DD:EE:02 -serial tcp:127.0.0.1:12345,server
+@echo off
+if "%~1"=="" (
+	echo.
+	echo Usage: launch.bat [path_to_embox_bin]
+	echo [path_to_embox_bin] should not end with "embox.bin", just "embox"!
+ 	echo Qemu will be listening for incoming connection at 127.0.0.1:12345
+	echo.
+	echo Example: launch.bat d:/embox/build/base/bin/embox
+	goto :eof
+)
+
+::If you want to start it from explorer, remove the code above
+::and replace "%~1" with path to embox.bin at the next line
+start "name" /D"%programfiles%\qemu\" qemu.exe -kernel %~1 -net nic,model=ne2k_pci,macaddr=AA:BB:CC:DD:EE:02 -serial tcp:127.0.0.1:12345,server
+
+
