1f679d5bd4115f92265111111e81e221b1151e86
Felix Sulima <fsulima@gmail.com>
2013-11-13 16:46:21 +0000

    qt+qpid: skeleton for redirecting stdout to Qt component

--- third-party/qt/examples/qpidhello/mainwindow.cpp
+++ third-party/qt/examples/qpidhello/mainwindow.cpp
@@ -4,33 +4,45 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#include <QScrollBar>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     int flags;
 
-    oldstd[0] = dup(1);
-    oldstd[1] = dup(2);
     pipe(pipefd);
     flags = fcntl(pipefd[0], F_GETFL, 0);
     fcntl(pipefd[0], F_SETFL, flags | O_NONBLOCK);
     flags = fcntl(pipefd[1], F_GETFL, 0);
     fcntl(pipefd[1], F_SETFL, flags | O_NONBLOCK);
-    ::close(STDOUT_FILENO);
-    dup(pipefd[0]);
-    ::close(STDERR_FILENO);
-    dup(pipefd[0]);
 
-    notifier.reset(new QSocketNotifier(pipefd[1], QSocketNotifier::Read));
+    oldstd[0] = dup(STDOUT_FILENO);
+    oldstd[1] = dup(STDERR_FILENO);
+    ::close(STDOUT_FILENO);
+    dup(pipefd[1]);
+    ::close(STDERR_FILENO);
+    dup(pipefd[1]);
+
+    notifier.reset(new QSocketNotifier(pipefd[0], QSocketNotifier::Read));
     connect(notifier.data(), SIGNAL(activated(int)), this, SLOT(onStdout()));
-    notifier.data()->setEnabled(true);
 
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
+    ::close(STDOUT_FILENO);
+    dup(oldstd[0]);
+    ::close(STDERR_FILENO);
+    dup(oldstd[1]);
+    ::close(oldstd[0]);
+    ::close(oldstd[1]);
+
+    ::close(pipefd[0]);
+    ::close(pipefd[1]);
+
     delete ui;
 }
 
@@ -40,9 +52,17 @@ void MainWindow::onStdout()
     ssize_t read;
 
     do {
-        read = ::read(pipefd[1], buf, 256);
+        read = ::read(pipefd[0], buf, 256);
         ui->plainTextEdit->setPlainText(ui->plainTextEdit->toPlainText().append(QByteArray(buf,read)));
     } while (read>0);
+
+    QScrollBar *sb = ui->plainTextEdit->verticalScrollBar();
+    sb->setSliderPosition(sb->maximum());
+}
+
+void MainWindow::on_pushButton_pressed()
+{
+    on_lineEdit_returnPressed();
 }
 
 void MainWindow::on_lineEdit_returnPressed()
@@ -51,7 +71,3 @@ void MainWindow::on_lineEdit_returnPressed()
     fflush(stdout);
 }
 
-void MainWindow::on_pushButton_pressed()
-{
-    on_lineEdit_returnPressed();
-}
