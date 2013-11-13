d9778cefe7c3f8aa60e020095c9702727b370573
Felix Sulima <fsulima@gmail.com>
2013-11-13 14:24:10 +0000

    qt+qpid: hello app: minor changes

--- third-party/qt/examples/qpidhello/mainwindow.cpp
+++ third-party/qt/examples/qpidhello/mainwindow.cpp
@@ -17,10 +17,14 @@ MainWindow::MainWindow(QWidget *parent) :
     fcntl(pipefd[0], F_SETFL, flags | O_NONBLOCK);
     flags = fcntl(pipefd[1], F_GETFL, 0);
     fcntl(pipefd[1], F_SETFL, flags | O_NONBLOCK);
-    ::close(1);
+    ::close(STDOUT_FILENO);
     dup(pipefd[0]);
-    ::close(2);
+    ::close(STDERR_FILENO);
     dup(pipefd[0]);
+
+    notifier.reset(new QSocketNotifier(pipefd[1], QSocketNotifier::Read));
+    connect(notifier.data(), SIGNAL(activated(int)), this, SLOT(onStdout()));
+    notifier.data()->setEnabled(true);
 
     ui->setupUi(this);
 }
@@ -28,4 +32,26 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::onStdout()
+{
+    char buf[256];
+    ssize_t read;
+
+    do {
+        read = ::read(pipefd[1], buf, 256);
+        ui->plainTextEdit->setPlainText(ui->plainTextEdit->toPlainText().append(QByteArray(buf,read)));
+    } while (read>0);
+}
+
+void MainWindow::on_lineEdit_returnPressed()
+{
+    printf("qpidapp:>>> %s\n", ui->lineEdit->text().toAscii().data());
+    fflush(stdout);
+}
+
+void MainWindow::on_pushButton_pressed()
+{
+    on_lineEdit_returnPressed();
 }
--- third-party/qt/examples/qpidhello/mainwindow.h
+++ third-party/qt/examples/qpidhello/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSocketNotifier>
 
 namespace Ui {
 class MainWindow;
@@ -19,6 +20,12 @@ private:
     Ui::MainWindow *ui;
     int pipefd[2];
     int oldstd[2];
+    QScopedPointer<QSocketNotifier> notifier;
+
+private slots:
+    void onStdout();
+    void on_lineEdit_returnPressed();
+    void on_pushButton_pressed();
 };
 
 #endif // MAINWINDOW_H
