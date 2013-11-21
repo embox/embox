e3ac5263567e63fb82e96a22f8b0224821bea424
Ilia Vaprol <ki.stfu@gmail.com>
2013-11-21 16:13:36 +0000

    arm: cortexa8: Enable insn/data caching and program flow prediction

--- src/arch/arm/include/asm/modes.h
+++ src/arch/arm/include/asm/modes.h
@@ -23,4 +23,22 @@
 #define I_BIT 0x80
 #define F_BIT 0x40
 
+/**
+ * c1, Control Register
+ * read: MRC p15, 0, <Rd>, c1, c0, 0
+ * write: MCR p15, 0, <Rd>, c1, c0, 0
+ */
+#define CR_M 0x0001 /* MMU enabled */
+#define CR_A 0x0002 /* strict alignment fault checking enabled */
+#define CR_C 0x0004 /* data caching enabled */
+#define CR_Z 0x0800 /* program flow prediction enabled */
+#define CR_I 0x1000 /* instruction caching enabled */
+
+/**
+ * c1, Auxiliary Control Register
+ * read: MRC p15, 0, <Rd>, c1, c0, 1
+ * write: MCR p15, 0, <Rd>, c1, c0, 1
+ */
+#define ACR_L2EN 0x2 /* L2 cache enabled */
+
 #endif /* ARM_MODES_H_ */
