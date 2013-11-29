bef01c9baf302475664b7f0fe93371ac04a3ad31
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-11-29 01:18:56 +0000

    sched: cherry-pick f6c1a34

--- mk/flags.mk
+++ mk/flags.mk
@@ -57,21 +57,26 @@ cppflags := $(CPPFLAGS)
 override CPPFLAGS  = $(call cppflags_fn,) $(cppflags)
 EMBOX_EXPORT_CPPFLAGS := $(call cppflags_fn,$(abspath $(ROOT_DIR))/)
 
+override COMMON_FLAGS := -pipe
+override COMMON_FLAGS += --debug-prefix-map=$$(pwd)=
+override COMMON_FLAGS += --debug-prefix-map=./=
+
 # Assembler flags
 asflags := $(CFLAGS)
-override ASFLAGS  = -pipe
+override ASFLAGS = $(COMMON_FLAGS)
 override ASFLAGS += $(asflags)
 
+override COMMON_CCFLAGS := $(COMMON_FLAGS)
+override COMMON_CCFLAGS += -fno-strict-aliasing -fno-common
+override COMMON_CCFLAGS += -Wall -Werror
+override COMMON_CCFLAGS += -Wundef -Wno-trigraphs -Wno-char-subscripts
+override COMMON_CCFLAGS += -Wformat -Wno-format-zero-length #-Wformat-nonliteral
 
 cxxflags := $(CXXFLAGS)
-override CXXFLAGS = -pipe
+override CXXFLAGS = $(COMMON_CCFLAGS)
 #override CXXFLAGS += -fno-rtti
 #override CXXFLAGS += -fno-exceptions
 #override CXXFLAGS += -fno-threadsafe-statics
-override CXXFLAGS += -fno-strict-aliasing -fno-common
-override CXXFLAGS += -Wall -Werror
-override CXXFLAGS += -Wundef -Wno-trigraphs -Wno-char-subscripts
-override CXXFLAGS += -Wformat -Wformat-nonliteral
 override CXXFLAGS += -I$(SRC_DIR)/include/c++
 override CXXFLAGS += -I$(ROOT_DIR)/build/gcc/include
 #	C++ has build-in type bool
@@ -80,14 +85,9 @@ override CXXFLAGS += $(cxxflags)
 
 # Compiler flags
 cflags := $(CFLAGS)
-override CFLAGS  = -std=gnu99
+override CFLAGS  = $(COMMON_CCFLAGS)
+override CFLAGS += -std=gnu99
 #override CFLAGS += -fexceptions
-override CFLAGS += -fno-strict-aliasing -fno-common
-override CFLAGS += -Wall -Werror
-override CFLAGS += -Wstrict-prototypes -Wdeclaration-after-statement
-override CFLAGS += -Wundef -Wno-trigraphs -Wno-char-subscripts
-override CFLAGS += -Wformat -Wno-format-zero-length #-Wformat-nonliteral
-override CFLAGS += -pipe
 override CFLAGS += $(cflags)
 
 # Linker flags
