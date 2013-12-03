a10f13fde2ed0bb8a336f4a3e095c2337befb4db
Anton Kozlov <drakon.mega@gmail.com>
2013-12-03 13:51:17 +0000

    idesc: f*xattr through idesc_*xattr

--- src/compat/posix/fs/xattr.c
+++ src/compat/posix/fs/xattr.c
@@ -11,6 +11,7 @@
 
 #include <fs/perm.h>
 #include <security/security.h>
+#include <fs/index_descriptor.h>
 
 #include <sys/xattr.h>
 
@@ -29,32 +30,6 @@ int getxattr(const char *path, const char *name, char *value, size_t size) {
 	}
 
 	if (0 > (res = kfile_xattr_get(node, name, value, size))) {
-		SET_ERRNO(-res);
-		return -1;
-	}
-
-	return res;
-}
-
-int fgetxattr(int fd, const char *name, void *value, size_t size) {
-	struct file_desc *file;
-	int res;
-
-	file = file_desc_get(fd);
-
-	if (!file) {
-		SET_ERRNO(EBADF);
-		return -1;
-	}
-
-	assert(file->node);
-
-	if (0 > (res = security_xattr_get(file->node, name, value, size))) {
-		SET_ERRNO(EACCES);
-		return -1;
-	}
-
-	if (0 > (res = kfile_xattr_get(file->node, name, value, size))) {
 		SET_ERRNO(-res);
 		return -1;
 	}
@@ -85,35 +60,6 @@ int setxattr(const char *path, const char *name, const char *value, size_t size,
 	return res;
 }
 
-int fsetxattr(int fd, const char *name, const char *value, size_t size, int flags) {
-	struct file_desc *file;
-	int res;
-
-	file = file_desc_get(fd);
-
-
-	if (!file) {
-		SET_ERRNO(EBADF);
-		return -1;
-	}
-
-	assert(file->node);
-
-	if (0 > (res = security_xattr_set(file->node, name, value, size, flags))) {
-		SET_ERRNO(EACCES);
-		return -1;
-	}
-
-
-	if (0 > (res = kfile_xattr_set(file->node, name, value, size, flags))) {
-		SET_ERRNO(-res);
-		return -1;
-	}
-
-	return res;
-}
-
-
 int listxattr(const char *path, char *list, size_t size) {
 	int res;
 	struct node *node;
@@ -136,27 +82,70 @@ int listxattr(const char *path, char *list, size_t size) {
 	return res;
 }
 
-int flistxattr(int fd, char *list, size_t size) {
-	struct file_desc *file;
+int fsetxattr(int fd, const char *name, const char *value, size_t size, int flags) {
+	struct idesc *idesc;
 	int res;
 
-	file = file_desc_get(fd);
+	idesc = index_descriptor_get(fd);
 
-	if (!file) {
+	if (!idesc) {
 		SET_ERRNO(EBADF);
 		return -1;
 	}
 
-	assert(file->node);
-
-	if (0 > (res = security_xattr_list(file->node, list, size))) {
+	if (0 > (res = security_xattr_idesc_set(idesc, name, value, size, flags))) {
 		SET_ERRNO(EACCES);
 		return -1;
 	}
 
-	if (0 > (res = kfile_xattr_list(file->node, list, size))) {
-		SET_ERRNO(-res);
+	if (0 > (res = idesc_setxattr(idesc, name, value, size, flags))) {
+		return SET_ERRNO(-res);
+	}
+
+	return res;
+}
+
+int fgetxattr(int fd, const char *name, void *value, size_t size) {
+	struct idesc *idesc;
+	int res;
+
+	idesc = index_descriptor_get(fd);
+
+	if (!idesc) {
+		SET_ERRNO(EBADF);
 		return -1;
+	}
+
+	if (0 > (res = security_xattr_idesc_get(idesc, name, value, size))) {
+		SET_ERRNO(EACCES);
+		return -1;
+	}
+
+	if (0 > (res = idesc_getxattr(idesc, name, value, size))) {
+		return SET_ERRNO(-res);
+	}
+
+	return res;
+}
+
+int flistxattr(int fd, char *list, size_t size) {
+	struct idesc *idesc;
+	int res;
+
+	idesc = index_descriptor_get(fd);
+
+	if (!idesc) {
+		SET_ERRNO(EBADF);
+		return -1;
+	}
+
+	if (0 > (res = security_xattr_idesc_list(idesc, list, size))) {
+		SET_ERRNO(EACCES);
+		return -1;
+	}
+
+	if (0 > (res = idesc_listxattr(idesc, list, size))) {
+		return SET_ERRNO(-res);
 	}
 
 	return res;
