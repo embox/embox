f384aa06399060123ea76202ecec78c23eb707c8
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-12-05 11:00:50 +0000

    sched: Rewrite bkl using spinlocks

--- src/kernel/cpu/bkl.c
+++ src/kernel/cpu/bkl.c
@@ -15,40 +15,10 @@
 
 static spinlock_t bkl = SPIN_UNLOCKED;
 
-static unsigned int owner_id = -1;
-static unsigned int nested = 0;
-
 void bkl_lock(void) {
-	ipl_t ipl;
-	int locked;
-	unsigned int cpu_id;
-
-	locked = 0;
-	cpu_id = cpu_get_id();
-
-	do {
-		ipl = spin_lock_ipl(&bkl);
-		{
-			if ((nested == 0) || (cpu_id == owner_id)) {
-				owner_id = cpu_id;
-				++nested;
-
-				locked = 1;
-			}
-		}
-		spin_unlock_ipl(&bkl, ipl);
-	} while (!locked);
+	spin_lock_no_preempt(&bkl);
 }
 
 void bkl_unlock(void) {
-	ipl_t ipl;
-
-	ipl = spin_lock_ipl(&bkl);
-	{
-		assert(nested && (cpu_get_id() == owner_id));
-		--nested;
-		if (!nested)
-			owner_id = -1;
-	}
-	spin_unlock_ipl(&bkl, ipl);
+	spin_unlock_no_preempt(&bkl);
 }
