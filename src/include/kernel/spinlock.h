13dd3360192bdfef411d47d0231e8e58718bc9ca
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-12-05 09:12:44 +0000

    spinlock: Add conditional locking macros

--- src/include/kernel/spinlock.h
+++ src/include/kernel/spinlock.h
@@ -19,6 +19,7 @@
 #include <module/embox/arch/libarch.h>
 
 #include <util/lang.h>
+#include <util/macro.h>
 
 #define SPIN_UNLOCKED 0
 #define SPIN_LOCKED   1
@@ -112,6 +113,53 @@ static inline void spin_unlock_ipl_enable(spinlock_t *lock) {
 	ipl_enable();  /* implies optimization barrier */
 	sched_unlock();
 }
+
+/**
+ * Spin until either @a lock is acquired or @a cond becomes @c false.
+ * @return @a cond value. In case of a non-zero value the spin is locked.
+ * Unlocking it in the latter case is up to the client.
+ */
+#define SPIN_LOCK_COND(lock, cond) \
+	({                                       \
+		spinlock_t *__lock = (lock);         \
+		typeof(cond) __cond;                 \
+		                                     \
+		do {                                 \
+			__cond = (cond);                 \
+			if (!__cond)                     \
+				break;                       \
+		} while (!spin_trylock(__lock));     \
+		                                     \
+		if (__cond) {                        \
+			/* just been locked */           \
+			__cond = (cond);                 \
+			if (!__cond)                     \
+				spin_unlock(__lock);         \
+		}                                    \
+		                                     \
+		__cond;                              \
+	})
+
+/**
+ * 'if' statement based on #SPIN_LOCK_COND(). True branch gets executed with
+ * a non-zero value of @a cond and spin locked. False branch gets no locks.
+ * No explicit unlocking is required.
+ * Making 'return' stmt inside the block will leave the spin locked.
+ * Also 'break'/'continue' will not work as expected.
+ */
+#define spin_protected_if(lock, cond) \
+	__spin_protected_if(lock, cond, \
+		MACRO_GUARD(__done), \
+		MACRO_GUARD(__lock), \
+		MACRO_GUARD(__cond))
+
+#define __spin_protected_if(lock, cond, __done, __lock, __cond) \
+	for (int __done = 0;                              !__done; ) \
+	for (spinlock_t *__lock = (lock);                 !__done; ) \
+	for (int __cond = !!SPIN_LOCK_COND(__lock, cond); !__done;   \
+			({ if (__cond) spin_unlock(__lock); }))              \
+	while (!__done && (__done = 1)) /* break/continue control this loop */  \
+		if (__cond)
 
 #define SPIN_PROTECTED_DO(lock, expr) \
 	__lang_surround(expr,             \
