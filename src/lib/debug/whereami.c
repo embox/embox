f97833d995c1f51df5eadeede573aeb2664e6729
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-12-06 15:57:52 +0000

    traceback: output thread run function, cleanup code

--- src/lib/debug/whereami.c
+++ src/lib/debug/whereami.c
@@ -36,45 +36,93 @@ static void *bt_buff[MAX_DEPTH];
 	({ ptrdiff_t __sz = sz; assert(__sz >= 0); \
 		__sz ? min(snprintf(p, __sz, fmt, ##__VA_ARGS__), __sz-1) : 0; })
 
+static inline int tb_decimal_width(int i) {
+	char buff[11];
+	return snprintf(buff, sizeof(buff), "%d", i);
+}
+
+static size_t tb_snprint_symbol(char *buff, size_t buff_sz, void *addr) {
+	char *p = buff;
+	char *end = buff + buff_sz;
+	const struct symbol *sym;
+	const struct location *loc;
+
+	size_t lineno_len;  /* including a colon */
+	const char *fname;
+	size_t fname_len;
+	int fname_padding;
+
+	p += tb_safe_snprintf(p, end-p, "%p", addr);
+
+	sym = symbol_lookup(addr);
+	if (!sym)
+		goto out;
+
+	p += tb_safe_snprintf(p, end-p, " <%s+%#tx> ",
+		sym->name, (char *) addr - (char *) sym->addr);
+
+	loc = &sym->loc;
+	if (!loc->file)
+		goto out;
+
+	lineno_len = tb_decimal_width(loc->line);
+	if (end-p <= strlen(ELLI) + lineno_len)
+		goto out;
+
+	fname = loc->file;
+	fname_len = strlen(fname);
+	fname_padding = (end-p) - (fname_len + lineno_len + 1);
+
+	if (fname_padding < 0) {
+		fname += strlen(ELLI) - fname_padding;
+		p += tb_safe_snprintf(p, end-p, "%s", ELLI);
+	} else {
+		for (int i = 0; i < fname_padding; ++i)
+			p += tb_safe_snprintf(p, end-p, " ");
+	}
+
+	p += tb_safe_snprintf(p, end-p, "%s:%d", fname, loc->line);
+
+out:
+	return end-p;
+}
+
 static size_t tb_snprint_stack_frame(char *buff, size_t buff_sz,
-		int nr, void *addr, const struct symbol *sym) {
+		int nr, void *addr) {
 	char *p = buff;
 	char *end = buff + buff_sz;
 
-	p += tb_safe_snprintf(p, end-p, "%3d %p", nr, addr);
+	p += tb_safe_snprintf(p, end-p, "%3d ", nr);
+	return tb_snprint_symbol(p, end-p, addr);
+}
 
-	if (sym) {
-		const struct location *loc = &sym->loc;
-		ptrdiff_t offset = (char *) addr - (char *) sym->addr;
-		p += tb_safe_snprintf(p, end-p, " <%s+%#tx> ",
-			(offset < 0 ? "__unknown__" : sym->name), offset);
+static size_t tb_snprint_thread_run(char *buff, size_t buff_sz,
+		struct thread *t) {
+	char *p = buff;
+	char *end = buff + buff_sz;
 
-		if (loc->file) {
-			const char *fname = loc->file;
-			size_t lineno_len = 1;  /* including a colon */
-			int lineno_div = loc->line;
-			do {
-				++lineno_len;
-			} while ((lineno_div /= 10));
+	p += tb_safe_snprintf(p, end-p, "run ");
+	return tb_snprint_symbol(p, end-p, t->run);
+}
 
-			if (end-p > strlen(ELLI) + lineno_len) {
-				size_t fname_len = strlen(fname);
-				int padding = (end-p) - (fname_len + lineno_len + 1);
+static size_t tb_snprint_thread_state(char *buff, size_t buff_sz,
+		struct thread *t) {
+	char *p = buff;
+	char *end = buff + buff_sz;
+	int is_current = (t == thread_self());
 
-				if (padding < 0) {
-					fname += strlen(ELLI) - padding;
-					p += tb_safe_snprintf(p, end-p, "%s", ELLI);
-				} else {
-					for (int i = 0; i < padding; ++i)
-						p += tb_safe_snprintf(p, end-p, " ");
-				}
+	p += tb_safe_snprintf(p, end-p,
+		" === thread %d (task %d) %c%c%c%c ",
+		t->id, t->task->tid,
+		is_current      ? '*' : ' ',
+		sched_active(t) ? 'A' : ' ',
+		t->ready        ? 'R' : ' ',
+		t->waiting      ? 'W' : ' ');
 
-				p += tb_safe_snprintf(p, end-p, "%s:%d", fname, loc->line);
-			}
-		}
-	}
+	memset(p, '=', end-p-1);
+	*(end-1) = '\0';
 
-	return end-p;
+	return buff_sz;
 }
 
 static void tb_dump_thread_stack(struct thread *t) {
@@ -87,22 +135,20 @@ static void tb_dump_thread_stack(struct thread *t) {
 	size = backtrace_context(&bt_buff[0], ARRAY_SIZE(bt_buff), ctx);
 	limit = MAX_ROWS ? min(size, MAX_ROWS + 1) : size;
 
-	printk("\n\n thread %d (task %d) %c%c%c%c\n",
-		t->id, t->task->tid,
-		is_current      ? '*' : ' ',
-		sched_active(t) ? 'A' : ' ',
-		t->ready        ? 'R' : ' ',
-		t->waiting      ? 'W' : ' ');
-	printk("Backtrace:\n"
-			"    pc         func + offset\n"
-			"    ---------- ------------------------\n"
-			);
+	tb_snprint_thread_state(buff, sizeof(buff), t);
+	printk("\n\n%s\n", buff);
+
+	printk("    pc         func + offset\n");
+	printk("    ---------- ------------------------\n");
 
 	for (int i = is_current ? 2 : 0; i < limit; ++i) {
 		tb_snprint_stack_frame(buff, sizeof(buff),
-			size-i, bt_buff[i], symbol_lookup(bt_buff[i]));
+			size-i, bt_buff[i]);
 		printk("%s\n", buff);
 	}
+
+	tb_snprint_thread_run(buff, sizeof(buff), t);
+	printk("\n%s\n", buff);
 }
 
 void whereami(void) {
