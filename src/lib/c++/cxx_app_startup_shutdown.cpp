ed3361f235d2b2aafb3ff9be399f17fb48d0d462
Felix Sulima <fsulima@gmail.com>
2013-12-16 13:05:29 +0000

    c++: calling getopt_init on c++ app startup

--- src/lib/c++/cxx_app_startup_shutdown.cpp
+++ src/lib/c++/cxx_app_startup_shutdown.cpp
@@ -8,11 +8,13 @@
 #include "cxx_app_start.h"
 #include "cxx_app_startup_termination.h"
 #include "cxxapp.hpp"
+#include <unistd.h>
 
 int cxx_app_start(int argc, char **argv) {
 	int ret;
 
 	cxx_app_startup();
+	getopt_init();
 	ret = main(argc,argv);
 	cxx_app_termination();
 
