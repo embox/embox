318e76d32010893139e1142d59d6c81e2c92fa15
Roman Kurbatov <roman.kurbatov.1992@gmail.com>
2013-12-17 02:03:52 +0000

    acpi: copied requried components of ACPICA

---
+++ src/drivers/acpi/acpica/osl/osemboxxf.c
@@ -0,0 +1,9 @@
+/**
+ * @file
+ * @brief Embox OSL for ACPICA.
+ *
+ * @date 17.12.13
+ * @author Roman Kurbatov
+ */
+
+/* There will be definitions of Embox OSL functions here. */
---
+++ src/include/drivers/acpi/platform/acembox.h
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @brief Embox specific defines for ACPICA.
+ *
+ * @date 17.12.13
+ * @author Roman Kurbatov
+ */
+
+#ifndef DRIVERS_ACPI_ACEMBOX_H_
+#define DRIVERS_ACPI_ACEMBOX_H_
+
+#include <drivers/acpi/platform/acgcc.h>
+
+/* May be we should place some Embox specific defines here. */
+
+#endif /* DRIVERS_ACPI_ACEMBOX_H_ */
