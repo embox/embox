d17f1dba4817aa33fa6896e0162b5d93e02ed13c
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2013-12-24 17:16:30 +0000

    serial: don't flush RX buffer in dev_uart_write

--- src/drivers/serial/fsnode.c
+++ src/drivers/serial/fsnode.c
@@ -72,17 +72,12 @@ static size_t dev_uart_write(struct file_desc *desc, void *buff, size_t size) {
 	struct uart *uart_dev = desc->file_info;
 	struct tty *tty = &uart_dev->tty;
 	size_t written, left = size;
-	int ch;
 
 	do {
 		written = tty_write(tty, buff, left);
 
-		while (-1 != (ch = tty_out_getc(tty))) {
-			uart_putc(uart_dev, ch);
-		}
-
 		left -= written;
-		buff = (void *)((char *)buff + written);
+		buff = (char *) buff + written;
 	} while (left != 0);
 
 	return size;
