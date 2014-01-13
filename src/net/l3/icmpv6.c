ece01d8788fac03e29fa24a368bcb661fdcd3a9a
Ilia Vaprol <ki.stfu@gmail.com>
2014-01-13 23:00:08 +0000

    net: icmpv6: Comment a little

--- src/net/l3/icmpv6.c
+++ src/net/l3/icmpv6.c
@@ -1,6 +1,8 @@
 /**
  * @file
- * @brief
+ * @brief Internet Control Message Protocol (ICMPv6)
+ * for the Internet Protocol Version 6 (IPv6) Specification
+ * @details RFC 4443
  *
  * @date 17.05.13
  * @author Ilia Vaprol
@@ -11,6 +13,7 @@
 #include <embox/net/proto.h>
 #include <net/l3/icmpv6.h>
 #include <net/lib/icmpv6.h>
+#include <net/lib/ipv6.h>
 #include <net/skbuff.h>
 #include <net/util/checksum.h>
 #include <arpa/inet.h>
@@ -19,7 +22,6 @@
 #include <net/inetdevice.h>
 #include <string.h>
 #include <net/l3/ipv6.h>
-#include <net/lib/ipv6.h>
 #include <errno.h>
 #include <util/binalign.h>
 #include <net/neighbour.h>
@@ -29,24 +31,26 @@ EMBOX_NET_PROTO(ETH_P_IPV6, IPPROTO_ICMPV6, icmp6_rcv, NULL);
 int icmp6_send(struct sk_buff *skb, uint8_t type, uint8_t code,
 		const void *body, size_t body_sz) {
 	struct in_device *in_dev;
+	struct ip6hdr *ip6h;
 	struct in6_addr src_ip6, dst_ip6;
 
 	assert(skb != NULL);
-	assert(skb->nh.ip6h != NULL);
-
 	in_dev = inetdev_get_by_dev(skb->dev);
 	assert(in_dev != NULL);
 
+	ip6h = ip6_hdr(skb);
+	assert(ip6h != NULL);
+
 	memcpy(&src_ip6, &in_dev->ifa6_address, sizeof src_ip6);
-	memcpy(&dst_ip6, &skb->nh.ip6h->saddr, sizeof dst_ip6);
-	ip6_build(skb->nh.ip6h, ip6_data_length(skb->nh.ip6h),
+	memcpy(&dst_ip6, &ip6h->saddr, sizeof dst_ip6);
+	ip6_build(ip6h, ip6_data_length(ip6h),
 			IPPROTO_ICMPV6, 255, &src_ip6, &dst_ip6);
 
-	icmp6_build(skb->h.icmp6h, type, code, body, body_sz);
-	icmp6_set_check_field(skb->h.icmp6h, skb->nh.ip6h);
+	icmp6_build(icmp6_hdr(skb), type, code, body, body_sz);
+	icmp6_set_check_field(icmp6_hdr(skb), ip6h);
 
 	if (ip6_out_ops == NULL) {
-		return -ENOSYS;
+		return -ENOSYS; /* error: not implemented */
 	}
 
 	assert(ip6_out_ops->snd_pack != NULL);
@@ -60,11 +64,11 @@ static int icmp6_hnd_echo_request(const struct icmp6hdr *icmp6h,
 
 	if (icmp6h->code != 0) {
 		skb_free(skb);
-		return 0;
+		return 0; /* error: bad code */
 	}
 
-	if (sizeof *icmp6h + sizeof *echo_req > ntohs(
-				skb->nh.ip6h->payload_len)) {
+	if (sizeof *icmp6h + sizeof *echo_req > ip6_data_length(
+				ip6_hdr(skb))) {
 		skb_free(skb);
 		return 0; /* error: invalid length */
 	}
@@ -88,7 +92,7 @@ static int ndp_hnd_neighbor_solicit(const struct icmp6hdr *icmp6h,
 		char __ops_ll_addr_storage[MAX_ADDR_LEN];
 	} __attribute__((packed)) nbr_advert;
 
-	if (skb->nh.ip6h->hop_limit != 255) {
+	if (ip6_hdr(skb)->hop_limit != 255) {
 		skb_free(skb);
 		return 0; /* error: not neighbour */
 	}
@@ -98,7 +102,7 @@ static int ndp_hnd_neighbor_solicit(const struct icmp6hdr *icmp6h,
 		return 0; /* error: bad code */
 	}
 
-	len = ntohs(skb->nh.ip6h->payload_len);
+	len = ip6_data_length(ip6_hdr(skb));
 	if (sizeof *icmp6h + sizeof *nbr_solicit > len) {
 		skb_free(skb);
 		return 0; /* error: invalid length */
@@ -129,8 +133,8 @@ static int ndp_hnd_neighbor_solicit(const struct icmp6hdr *icmp6h,
 				skb_free(skb);
 				return 0; /* error: invalid length */
 			}
-			(void)neighbour_add(ETH_P_IPV6, &skb->nh.ip6h->saddr,
-					sizeof skb->nh.ip6h->saddr, in_dev->dev,
+			(void)neighbour_add(ETH_P_IPV6, &ip6_hdr(skb)->saddr,
+					sizeof ip6_hdr(skb)->saddr, in_dev->dev,
 					ARPG_HRD_ETHERNET, ops->ll_addr,
 					ops->hdr.len * 8 - sizeof ops->hdr, 0);
 			break;
@@ -146,6 +150,7 @@ static int ndp_hnd_neighbor_solicit(const struct icmp6hdr *icmp6h,
 	nbr_advert.ops.hdr.type = NDP_TARGET_LL_ADDR;
 	nbr_advert.ops.hdr.len = binalign_bound(sizeof nbr_advert.ops
 			+ in_dev->dev->addr_len, 8) / 8;
+	assert(in_dev->dev->addr_len <= sizeof __ops_ll_addr_storage);
 	memcpy(nbr_advert.ops.ll_addr, &in_dev->dev->dev_addr[0],
 			in_dev->dev->addr_len);
 
@@ -161,7 +166,7 @@ static int ndp_hnd_neighbor_advert(const struct icmp6hdr *icmp6h,
 	size_t len;
 	const struct ndpoptions_ll_addr *ops;
 
-	if (skb->nh.ip6h->hop_limit != 255) {
+	if (ip6_hdr(skb)->hop_limit != 255) {
 		skb_free(skb);
 		return 0; /* error: not neighbour */
 	}
@@ -171,7 +176,7 @@ static int ndp_hnd_neighbor_advert(const struct icmp6hdr *icmp6h,
 		return 0; /* error: bad code */
 	}
 
-	len = ntohs(skb->nh.ip6h->payload_len);
+	len = ip6_data_length(ip6_hdr(skb));
 	if (sizeof *icmp6h + sizeof *nbr_advert > len) {
 		skb_free(skb);
 		return 0; /* error: invalid length */
@@ -211,20 +216,30 @@ static int ndp_hnd_neighbor_advert(const struct icmp6hdr *icmp6h,
 
 static int icmp6_rcv(struct sk_buff *skb) {
 	struct icmp6hdr *icmp6h;
+	uint16_t old_check;
 
-	if (sizeof *icmp6h > ntohs(skb->nh.ip6h->payload_len)) {
+	if (sizeof *icmp6h > ip6_data_length(ip6_hdr(skb))) {
 		skb_free(skb);
 		return 0; /* error: invalid length */
 	}
 
 	icmp6h = icmp6_hdr(skb);
+	assert(icmp6h != NULL);
+
+	old_check = icmp6h->check;
+	icmp6_set_check_field(icmp6h, ip6_hdr(skb));
+	if (old_check != icmp6h->check) {
+		skb_free(skb);
+		return 0; /* error: bad checksum */
+	}
+
 	switch (icmp6h->type) {
 	default:
-		/*printk("icmp6_rcv: unknown type: %hhu\n", icmp6h->type);*/
-		break;
+		//printk("icmp6_rcv: unknown type: %hhu\n", icmp6h->type);
+		break; /* error: unknown type */
 	case ICMP6_ECHO_REQUEST:
-		return icmp6_hnd_echo_request(icmp6h, &icmp6h->body[0].echo,
-				skb);
+		return icmp6_hnd_echo_request(icmp6h,
+				&icmp6h->body[0].echo, skb);
 	case ICMP6_ECHO_REPLY:
 		break;
 	case NDP_NEIGHBOR_SOLICIT:
