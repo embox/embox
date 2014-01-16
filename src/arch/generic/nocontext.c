5c907ae3f0c49c487742f116ad3e80febc353fff
Anton Kozlov <drakon.mega@gmail.com>
2014-01-16 10:40:10 +0000

    arch generic nocontext extended

---
+++ src/arch/generic/nocontext.c
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Kozlov
+ * @date    16.01.2014
+ */
+
+#include <hal/context.h>
+
+void context_switch(struct context *prev, struct context *next) {
+
+}
