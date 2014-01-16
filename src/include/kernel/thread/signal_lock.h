d787eaf8ab8763ab58a5cfd4364a057d3f52c591
Anton Kozlov <drakon.mega@gmail.com>
2014-01-16 08:26:49 +0000

    thread sig lock to API

--- src/include/kernel/thread/signal_lock.h
+++ src/include/kernel/thread/signal_lock.h
@@ -9,40 +9,9 @@
 #ifndef THREAD_SIGNAL_LOCK_H_
 #define THREAD_SIGNAL_LOCK_H_
 
-#include <assert.h>
-#include <kernel/thread.h>
-#include <kernel/sched.h>
-#include <kernel/critical.h>
+static inline void threadsig_lock(void);
+static inline void threadsig_unlock(void);
 
-static inline void threadsig_lock(void) {
-	struct thread *t = thread_self();
-
-	assert(t);
-	assert(t->siglock >= 0);
-
-	++t->siglock;
-
-	assert(t->siglock > 0); /* overflow */
-}
-
-static inline void threadsig_unlock(void) {
-	struct thread *t = thread_self();
-
-	assert(t);
-	assert(t->siglock > 0);
-
-	--t->siglock;
-
-	if (!t->siglock) {
-		/* No signal could be handled in sched_lock and harder */
-		if (critical_allows(CRITICAL_SCHED_LOCK)) {
-			thread_signal_handle();
-		} else {
-			/* Ensure that signal will be handled after *_unlock()
- 			 * of all locks (sched_lock and harder) */
-			sched_post_switch();
-		}
-	}
-}
+#include <module/embox/kernel/thread/signal/lock_api.h>
 
 #endif /* THREAD_SIGNAL_LOCK_H_ */
