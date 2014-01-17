dbfa337167ec061013449f237186507a54b1478c
Anton Bondarev <anton.bondarev2310@gmail.com>
2014-01-17 15:31:07 +0000

    iperf porting

--- third-party/iperf/iperf_embox_compat.h
+++ third-party/iperf/iperf_embox_compat.h
@@ -9,6 +9,7 @@
 #ifndef IPERF_EMBOX_COMPAT_H_
 #define IPERF_EMBOX_COMPAT_H_
 
+#include <sys/types.h>
 #include <sys/time.h>
 
 #define ITIMER_REAL 0
@@ -17,6 +18,19 @@ static inline int setitimer(int which, const struct itimerval *new_value,\
 		struct itimerval *old_value) {
 	return -1;
 }
+//#include <syslog.h>
+#define LOG_DAEMON    100
 
+//#include <unistd.h>
+static inline pid_t setsid(void) {
+	return -1;
+}
+
+//#include <libstd.h>
+static inline int atexit(void (*function)(void)) {
+	return -1;
+}
+
+#define HAVE_GETTIMEOFDAY
 
 #endif /* IPERF_EMBOX_COMPAT_H_ */
