c34c7522ce28ad21a395a3341b775517418634b4
Anton Kozlov <drakon.mega@gmail.com>
2014-01-17 13:40:56 +0000

    thread signal_lock DECLS added

--- src/include/kernel/thread/signal_lock.h
+++ src/include/kernel/thread/signal_lock.h
@@ -9,8 +9,14 @@
 #ifndef THREAD_SIGNAL_LOCK_H_
 #define THREAD_SIGNAL_LOCK_H_
 
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
 extern void threadsig_lock(void);
 
 extern void threadsig_unlock(void);
+
+__END_DECLS
 
 #endif /* THREAD_SIGNAL_LOCK_H_ */
