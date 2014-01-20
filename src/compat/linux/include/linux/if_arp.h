fbfe097ce6fb0262b1d49cb7b64f8978dd9588fc
Ilia Vaprol <ki.stfu@gmail.com>
2014-01-20 22:24:01 +0000

    net: Add linux/if_arp linux wrapper

---
+++ src/compat/linux/include/linux/if_arp.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @brief linux wrapper
+ *
+ * @date 21.01.14
+ * @author Ilia Vaprol
+ */
+
+#ifndef COMPAT_LINUX_LINUX_IF_ARP_H_
+#define COMPAT_LINUX_LINUX_IF_ARP_H_
+
+#include <net/l3/arp.h>
+#include <net/l3/rarp.h>
+
+#endif /* COMPAT_LINUX_LINUX_IF_ARP_H_ */
