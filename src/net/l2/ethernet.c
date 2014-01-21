f67e37a31a0e410fc18954c278e90e141043dc0d
Ilia Vaprol <ki.stfu@gmail.com>
2014-01-21 06:46:08 +0000

    net: l2/ethernet: Minor changes

--- src/net/l2/ethernet.c
+++ src/net/l2/ethernet.c
@@ -24,13 +24,10 @@ static int ethernet_build_hdr(struct sk_buff *skb,
 		const struct net_header_info *hdr_info) {
 	struct ethhdr *ethh;
 
-	if ((skb == NULL) || (hdr_info == NULL)) {
-		return -EINVAL;
-	}
+	assert(skb != NULL);
+	assert(hdr_info != NULL);
 
-	ethh = skb->mac.ethh;
-	assert(ethh != NULL);
-
+	ethh = eth_hdr(skb);
 	ethh->h_proto = htons(hdr_info->type);
 
 	assert(skb->dev != NULL);
@@ -52,12 +49,10 @@ static int ethernet_parse_hdr(struct sk_buff *skb,
 		struct net_header_info *out_hdr_info) {
 	const struct ethhdr *ethh;
 
-	if ((skb == NULL) || (out_hdr_info == NULL)) {
-		return -EINVAL;
-	}
+	assert(skb != NULL);
+	assert(out_hdr_info != NULL);
 
-	ethh = skb->mac.ethh;
-	assert(ethh != NULL);
+	ethh = eth_hdr(skb);
 
 	memset(out_hdr_info, 0, sizeof *out_hdr_info);
 	out_hdr_info->type = ntohs(ethh->h_proto);
@@ -67,31 +62,15 @@ static int ethernet_parse_hdr(struct sk_buff *skb,
 	return 0;
 }
 
-static int ethernet_check_addr(const void *addr) {
-	if (addr == NULL) {
-		return -EINVAL;
-	}
-
-	if (!is_valid_ether_addr(addr)) {
-		return -EINVAL;
-	}
-
-	return 0;
-}
-
 static int ethernet_check_mtu(int mtu) {
-	if ((mtu < ETH_ZLEN) || (mtu > ETH_FRAME_LEN)) {
-		return -EINVAL;
-	}
-
-	return 0;
+	return (mtu >= ETH_ZLEN) && (mtu <= ETH_FRAME_LEN);
 }
 
 const struct net_device_ops ethernet_ops = {
-	.build_hdr = &ethernet_build_hdr,
-	.parse_hdr = &ethernet_parse_hdr,
-	.check_addr = &ethernet_check_addr,
-	.check_mtu = &ethernet_check_mtu
+	.build_hdr = ethernet_build_hdr,
+	.parse_hdr = ethernet_parse_hdr,
+	.check_addr = is_valid_ether_addr,
+	.check_mtu = ethernet_check_mtu
 };
 
 static int ethernet_setup(struct net_device *dev) {
@@ -99,10 +78,7 @@ static int ethernet_setup(struct net_device *dev) {
 	int ret;
 	char name_fmt[IFNAMSIZ];
 
-	if (dev == NULL) {
-		return -EINVAL;
-	}
-
+	assert(dev != NULL);
 	strcpy(&name_fmt[0], &dev->name[0]);
 	ret = snprintf(&dev->name[0], ARRAY_SIZE(dev->name),
 			&name_fmt[0], eth_id);
@@ -114,7 +90,7 @@ static int ethernet_setup(struct net_device *dev) {
 	}
 	++eth_id;
 
-	memset(&dev->broadcast[0], 0xFF, ETH_ALEN);
+	memset(&dev->broadcast[0], 0xff, ETH_ALEN);
 	dev->type = ARP_HRD_ETHERNET;
 	dev->hdr_len = ETH_HEADER_SIZE;
 	dev->addr_len = ETH_ALEN;
