d91ecbf00b77bd6ae40e112484343eebf94aa286
Vita Loginova <vita.loginova@gmail.com>
2014-02-18 13:39:43 +0000

    useradd: home directory, shell, password, comment and group options

--- src/cmds/user/useradd.c
+++ src/cmds/user/useradd.c
@@ -13,6 +13,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pwd.h>
+#include <grp.h>
+#include <ctype.h>
 
 #define BUF_LEN 64
 
@@ -31,7 +33,43 @@ static void get_default_pwd(struct passwd *result, char *name) {
 	result->pw_shell = "/bin/false";
 }
 
-static int create(char *name) {
+static int set_options(struct passwd *result, char *home, char *shell,
+		char *gecos, char *group) {
+	if (0 != strcmp(home, "")) {
+		result->pw_dir = home;
+	}
+
+	if (0 != strcmp(shell, "")) {
+		result->pw_shell = shell;
+	}
+
+	if (0 != strcmp(gecos, "")) {
+		result->pw_gecos = gecos;
+	}
+
+	if (0 != strcmp(group, "")) {
+		struct group _group, *_group_res;
+		char buf[80];
+		int res = 0;
+
+		if (isdigit(group[0])){
+			res = getgrgid_r(atoi(group), &_group, buf, 80, &_group_res);
+		} else {
+			res = getgrnam_r(group, &_group, buf, 80, &_group_res);
+		}
+		if (!res && _group_res != NULL) {
+			result->pw_gid = _group_res->gr_gid;
+		} else {
+			//todo message
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+static int create_user(char *name, char *home, char *shell, char *pswd,
+		char *gecos, char *group) {
 	struct passwd pwd;
 	FILE *pswdf, *sdwf;
 	char buf[40];
@@ -46,6 +84,11 @@ static int create(char *name) {
 	}
 
 	get_default_pwd(&pwd, name);
+	if (0 != set_options(&pwd, home, shell, gecos, group)) {
+		fclose(pswdf);
+		fclose(sdwf);
+		return -1;
+	}
 
 	fwrite(pwd.pw_name, sizeof(char), strlen(pwd.pw_name), pswdf);
 	fputc(':', pswdf);
@@ -73,7 +116,7 @@ static int create(char *name) {
 
 	fwrite(pwd.pw_name, sizeof(char), strlen(pwd.pw_name), sdwf);
 	fputc(':', sdwf);
-	//password
+	fwrite(pswd, sizeof(char), strlen(pswd), sdwf);
 	fwrite(":::::::", sizeof(char), 7, sdwf);
 	fputc('\n', sdwf);
 
@@ -83,44 +126,63 @@ static int create(char *name) {
 	return 0;
 }
 
-static int create_user(char* name) {
+static int is_user_exists(char *name) {
 	char pwdbuf[BUF_LEN];
 	struct passwd pwd, *result;
 
 	if ((getpwnam_r(name, &pwd, pwdbuf, BUF_LEN, &result)) ||
 			result) {
-		//user exists
-		return -1;
+		return 1;
 	}
 
-	return create(name);
+	return 0;
 }
 
 static int useradd(int argc, char **argv) {
-	char *name;
+	char name[15], home[20] = "", shell[20] = "", pswd[15] = "",
+			gecos[15] = "", group[15] = "";
 	int opt;
 
 	if (argc != 1) {
-
 		getopt_init();
-
-		while (-1 != (opt = getopt(argc, argv, "pc:"))) {
+		while (-1 != (opt = getopt(argc, argv, "d:s:p:c:g:"))) {
 			switch(opt) {
+			case 'd':
+				//todo: isdir
+				strcpy(home, optarg);
+				break;
+			case 's':
+				//todo: isshell
+				strcpy(shell, optarg);
+				break;
+			case 'p':
+				strcpy(pswd, optarg);
+				break;
+			case 'c':
+				strcpy(gecos, optarg);
+				break;
+			case 'g':
+				strcpy(group, optarg);
+				break;
 			default:
 				break;
 			}
 		}
 
 		if (optind >= argc) {
-			//help
+			//todo: help
 			return 0;
 		}
 
-		name = argv[optind];
+		strcpy(name, argv[optind]);
 
-		return create_user(name);
+		if (is_user_exists(name)) {
+			//todo message
+			return -1;
+		}
+		return create_user(name, home, shell, pswd, gecos, group);
 	}
 
-	//help
+	//todo: help
 	return 0;
 }
