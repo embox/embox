33528f126f97f2ad12ad6600205c36883ad814bb
Anton Kozlov <drakon.mega@gmail.com>
2014-02-25 10:54:18 +0000

    ti81 PCI rd/wr config

--- src/drivers/pci/pci_chip/ti81xx.c
+++ src/drivers/pci/pci_chip/ti81xx.c
@@ -9,6 +9,8 @@
 #include <stdint.h>
 #include <hal/reg.h>
 
+#include <drivers/pci/pci_utils.h>
+
 #include <embox/unit.h>
 EMBOX_UNIT_INIT(ti81xx_pci_init);
 
@@ -45,12 +47,117 @@ EMBOX_UNIT_INIT(ti81xx_pci_init);
 #define TI81_CM_DEF_PCI_CLKCTRL_STANBY        0x00040000
 
 #define TI81_PCI_REGION0 0x51000000
-#define TI81_PCI_CMD_STATUS_OFF		      (TI81_PCI_REGION0 + 0x4)
+#define TI81_PCI_CMD_STATUS		      (TI81_PCI_REGION0 + 0x4)
 #define TI81_PCI_CMD_STATUS_LTSSM_EN 	      0x00000001
+#define TI81_PCI_CFG_SETUP                    (TI81_PCI_REGION0 + 0x8)
+
+#define TI81_PCI_REGION0_REMOTE_CFG (TI81_PCI_REGION0 + 0x2000)
 
 #define TI81_PCI_REGION1 0x20000000
 
 #include <kernel/printk.h>
+#define PCI_CONFIG_CMD(bus, dev_fn, where) \
+		(0x80000000 | (bus << 16) | (dev_fn << 8) | (where & ~3))
+
+#define PCI_TYPE0 0
+#define PCI_TYPE1 1
+
+int pci_check_type(void) {
+#if 0
+	int pci_type = 0;
+	uint32_t tmp;
+
+	out8(PCI_CONFIG_ADDRESS + 0x3, 0);
+	out8(PCI_CONFIG_ADDRESS, 0);
+	out8(PCI_CONFIG_ADDRESS + 0x2, 0);
+	if (in8(PCI_CONFIG_ADDRESS) == 0 && in8(PCI_CONFIG_ADDRESS + 0x2) == 0) {
+		pci_type = 2;
+	} else {
+		out8(PCI_CONFIG_ADDRESS + 0x3, 1);
+		tmp = in32(PCI_CONFIG_ADDRESS);
+		out32(PCI_CONFIG_ADDRESS, 0x80000000);
+		if (in32(PCI_CONFIG_ADDRESS) == 0x80000000) {
+			pci_type = 1;
+		}
+		out32(PCI_CONFIG_ADDRESS, tmp);
+	}
+	return pci_type;
+#endif
+	return 0;
+}
+
+static void ti81xx_cfg_setup(int pci_type, uint32_t bus, uint32_t dev, uint32_t fn) {
+
+}
+
+static inline uint32_t ti81_pci_config_read(uint32_t bus, uint32_t dev_fn,
+				uint32_t where, int size) {
+	uint32_t reg;
+
+	ti81xx_cfg_setup(PCI_TYPE0, bus, dev_fn >> 3, dev_fn & 0x7);
+
+	reg = REG_LOAD(TI81_PCI_REGION0_REMOTE_CFG + (where & ~3));
+
+	if (size == 1) {
+		return (reg >> (8 * (where & 3))) & 0xff;
+	} else if (size == 2) {
+		return (reg >> (8 * (where & 3))) & 0xffff;
+	}
+
+	return reg;
+}
+
+static inline void ti81_pci_config_write(uint32_t bus, uint32_t dev_fn,
+				uint32_t where, int size, uint32_t value) {
+	void *ptr;
+
+	ti81xx_cfg_setup(PCI_TYPE0, bus, dev_fn >> 3, dev_fn & 0x7);
+
+	ptr = (void *) (TI81_PCI_REGION0_REMOTE_CFG + where);
+	if (size == 1) {
+		* (volatile uint8_t *) ptr = value;
+	} else if (size == 2) {
+		* (volatile uint16_t *) ptr = value;
+	} else {
+		* (volatile uint32_t *) ptr = value;
+	}
+}
+
+uint32_t pci_read_config8(uint32_t bus, uint32_t dev_fn,
+				uint32_t where, uint8_t *value) {
+	*value = ti81_pci_config_read(bus, dev_fn, where, 1);
+	return PCIUTILS_SUCCESS;
+}
+
+uint32_t pci_read_config16(uint32_t bus, uint32_t dev_fn,
+				uint32_t where, uint16_t *value) {
+	*value = ti81_pci_config_read(bus, dev_fn, where, 2);
+	return PCIUTILS_SUCCESS;
+}
+
+uint32_t pci_read_config32(uint32_t bus, uint32_t dev_fn,
+				uint32_t where, uint32_t *value) {
+	*value = ti81_pci_config_read(bus, dev_fn, where, 4);
+	return PCIUTILS_SUCCESS;
+}
+
+uint32_t pci_write_config8(uint32_t bus, uint32_t dev_fn,
+				uint32_t where, uint8_t value) {
+	ti81_pci_config_write(bus, dev_fn, where, 1, value);
+	return PCIUTILS_SUCCESS;
+}
+
+uint32_t pci_write_config16(uint32_t bus, uint32_t dev_fn,
+				uint32_t where, uint16_t value) {
+	ti81_pci_config_write(bus, dev_fn, where, 2, value);
+	return PCIUTILS_SUCCESS;
+}
+
+uint32_t pci_write_config32(uint32_t bus, uint32_t dev_fn,
+		uint32_t where,	uint32_t value) {
+	ti81_pci_config_write(bus, dev_fn, where, 4, value);
+	return PCIUTILS_SUCCESS;
+}
 
 static void ti81xx_pci_clk_enable(void) {
 	unsigned long reg;
@@ -90,9 +197,11 @@ static int ti81xx_pci_init(void) {
 	}
 	REG_STORE(TI81_CM_DEF_CLKSTCTRL, TI81_CM_DEF_CLKSTCTRL_CLKTRCTRL_WKUP);
 
-	REG_ANDIN(TI81_PCI_CMD_STATUS_OFF, ~TI81_PCI_CMD_STATUS_LTSSM_EN);
+	REG_ANDIN(TI81_PCI_CMD_STATUS, ~TI81_PCI_CMD_STATUS_LTSSM_EN);
 
-	/*printk("%s: cmd_status=%08lx\n", __func__, REG_LOAD(TI81_PCI_CMD_STATUS_OFF));*/
+	/* Configure core registers ? */
+	REG_ORIN(TI81_PCI_CMD_STATUS, TI81_PCI_CMD_STATUS_LTSSM_EN);
+	printk("%s: cmd_status=%08lx\n", __func__, REG_LOAD(TI81_PCI_CMD_STATUS));
 
 	return 0;
 }
