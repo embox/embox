839912d54f363c2d3f44abff854a7764e3d1cc8e
Anton Kozlov <drakon.mega@gmail.com>
2014-02-27 09:25:55 +0000

    zeromq build fixed after qt-pro merge

--- third-party/zeromq/order.mk
+++
@@ -1,4 +0,0 @@
-
-$(OBJ_DIR)/third-party/zeromq/hwserver.o: $(OBJ_DIR)/third-party/zeromq/../../../../zeromq/lib/libzmq.a
-
-$(OBJ_DIR)/third-party/zeromq/../../../../zeromq/lib/libzmq.a: $(OBJ_DIR)/third-party/STLport/../../../../stlport/embox-lib/libstlportg.a
