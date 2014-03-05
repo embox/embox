075f3a8f9f400073c18f571cd10307397bc7c9b5
Alex Kalmuk <alexkalmuk@gmail.com>
2014-03-05 11:19:03 +0000

    taskmem: forgotten header

---
+++ src/include/mem/heap_bm.h
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @brief Boundary markers memory allocation algorithm
+ *
+ * @date 04.02.14
+ * @author Alexander Kalmuk
+ */
+
+#ifndef MEM_HEAP_BM_H_
+#define MEM_HEAP_BM_H_
+
+#include <sys/types.h>
+
+extern void bm_init(void *segment, size_t size);
+extern void *bm_memalign(void *segment, size_t boundary, size_t size);
+extern void bm_free(void *segment, void *ptr);
+extern void *bm_realloc(void *segment, void *ptr, size_t size);
+
+#endif /* MEM_HEAP_BM_H_ */
