a2a6ef2c2bce5a172434df83d99dbc9c348433db
Anton Kozlov <drakon.mega@gmail.com>
2014-03-13 13:35:58 +0000

    runtime-mod: bootable embuild_light, no rodata shirnking at all

--- src/framework/mod/embuild_light_impl.h
+++ src/framework/mod/embuild_light_impl.h
@@ -9,11 +9,50 @@
 #ifndef FRAMEWORK_MOD_EMBUILD_LIGHT_IMPL_H_
 #define FRAMEWORK_MOD_EMBUILD_LIGHT_IMPL_H_
 
+#include <util/array.h>
+
 #include <framework/mod/decls.h>
 #include <framework/mod/types.h>
 #include <framework/mod/options.h>
 
-#define __MOD_DEF(mod_nm, package_nm, mod_name)
+#define MOD_PACKAGE_DEF(package_nm, package_name) \
+	static const struct mod_package __MOD_PACKAGE(package_nm) = { \
+		.name = package_name,                                \
+	}
+
+#define __MOD_DEF(mod_nm, package_nm, mod_name) \
+	extern const struct mod_info __MOD_INFO(mod_nm)               \
+			__attribute__ ((weak));                               \
+	extern const struct mod_cmd __MOD_CMD(mod_nm)                 \
+			__attribute__ ((weak));                               \
+	extern const struct mod_app __MOD_APP(mod_nm)                 \
+			__attribute__ ((weak));                               \
+	extern const struct mod_label __MOD_LABEL(mod_nm)             \
+			__attribute__ ((weak));                               \
+	static const struct mod_package __MOD_PACKAGE(package_nm);    \
+	ARRAY_SPREAD_DEF_TERMINATED(static const struct mod *,        \
+			__MOD_REQUIRES(mod_nm), NULL);                        \
+	ARRAY_SPREAD_DEF_TERMINATED(static const struct mod *,        \
+			__MOD_PROVIDES(mod_nm), NULL);                        \
+	ARRAY_SPREAD_DEF_TERMINATED(static const struct mod *,        \
+			__MOD_AFTER_DEPS(mod_nm), NULL);                      \
+	ARRAY_SPREAD_DEF_TERMINATED(static const struct mod *,        \
+			__MOD_CONTENTS(mod_nm), NULL);                        \
+	ARRAY_SPREAD_DEF_TERMINATED(static const struct mod_member *, \
+			__MOD_MEMBERS(mod_nm), NULL);                         \
+	static struct __mod_private __MOD_PRIVATE(mod_nm);            \
+	const struct mod __MOD(mod_nm) = {            \
+		.priv       = &__MOD_PRIVATE(mod_nm),     \
+		.info       = &__MOD_INFO(mod_nm),        \
+		.cmd        = &__MOD_CMD(mod_nm),         \
+		.app        = &__MOD_APP(mod_nm),         \
+		.name       = mod_name,                   \
+		.members    = __MOD_MEMBERS(mod_nm),      \
+		.package    = &__MOD_PACKAGE(package_nm), \
+	};                                            \
+	ARRAY_SPREAD_DECLARE(const struct mod *,      \
+			__mod_registry);                      \
+	ARRAY_SPREAD_ADD(__mod_registry, &__MOD(mod_nm)) // TODO don't like it. -- Eldar
 
 /**
  * Cmd-specific definitions.
@@ -52,7 +91,6 @@
 #define MOD_DEP_DEF(mod_nm, dep_nm)
 #define MOD_CONTENTS_DEF(mod_nm, content_nm)
 #define MOD_AFTER_DEP_DEF(mod_nm, dep_nm)
-#define MOD_PACKAGE_DEF(package_nm, package_name)
 #define MOD_LABEL_DEF(mod_nm)
 #define MOD_SEC_LABEL_DEF(mod_nm)
 
