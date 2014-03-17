efb7fbd8f6294dd3420924080a478634d4a734da
Ilia Vaprol <ki.stfu@gmail.com>
2014-03-17 21:14:17 +0000

    extbld-porting: Add stubs (utime, execl/execlp/execvp, getdtablesize)
    Disable __fpending.

--- third-party/tar/tar_embox_compat.h
+++ third-party/tar/tar_embox_compat.h
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <utime.h>
 
 extern ssize_t readlink(const char *path, char *buf,
 		       size_t bufsize);
@@ -33,6 +34,34 @@ static inline void funlockfile(FILE *file) {
 
 static inline void rewinddir(DIR *dirp) {
 	printf(">>> %s %p\n", __func__, dirp);
+}
+
+static inline int utime(const char *path,
+		const struct utimbuf *times) {
+	printf(">>> %s %s %p\n", __func__, path, times);
+	return SET_ERRNO(ENOSYS);
+}
+
+static inline int execl(const char *path, const char *arg0,
+		... /*, (char *)0 */) {
+	printf(">>> %s %s %s\n", __func__, path, arg0);
+	return SET_ERRNO(ENOSYS);
+}
+
+static inline int execlp(const char *file, const char *arg0,
+		... /*, (char *)0 */) {
+	printf(">>> %s %s %s\n", __func__, file, arg0);
+	return SET_ERRNO(ENOSYS);
+}
+
+static inline int execvp(const char *file, char *const argv[]) {
+	printf(">>> %s %s %s\n", __func__, file, argv[0]);
+	return SET_ERRNO(ENOSYS);
+}
+
+static inline int getdtablesize(void) {
+	printf(">>> %s\n", __func__);
+	return 10;
 }
 
 #endif /* TAR_EMBOX_COMPAT_H_ */
