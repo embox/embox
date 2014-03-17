e73a503caf3d6a34c750b9021a566e67b5c9a46c
Ilia Vaprol <ki.stfu@gmail.com>
2014-03-17 16:18:01 +0000

    extbld-porting: tar: Add more stubs

--- third-party/tar/tar_embox_compat.h
+++ third-party/tar/tar_embox_compat.h
@@ -4,21 +4,35 @@
 #include <stddef.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <stdio.h>
+#include <dirent.h>
 
 extern ssize_t readlink(const char *path, char *buf,
 		       size_t bufsize);
-//static inline ssize_t readlink(const char *path, char *buf,
-//		       size_t bufsize) {
-//	(void)path; (void)buf; (void)bufsize;
-//	return SET_ERRNO(ENOSYS);
-//}
 
 extern ssize_t readlinkat(int fd, const char *path,
 		       char *buf, size_t bufsize);
-//static inline ssize_t readlinkat(int fd, const char *path,
-//		       char *buf, size_t bufsize) {
-//	(void)fd; (void)path; (void)buf; (void)bufsize;
-//	return SET_ERRNO(ENOSYS);
-//}
+
+static inline int WIFEXITED(int status) {
+	printf(">>> %s %d\n", __func__, status);
+	return 0;
+}
+
+static inline void flockfile(FILE *file) {
+	printf(">>> %s %p\n", __func__, file);
+}
+
+static inline int ftrylockfile(FILE *file) {
+	printf(">>> %s %p\n", __func__, file);
+	return 0;
+}
+
+static inline void funlockfile(FILE *file) {
+	printf(">>> %s %p\n", __func__, file);
+}
+
+static inline void rewinddir(DIR *dirp) {
+	printf(">>> %s %p\n", __func__, dirp);
+}
 
 #endif /* TAR_EMBOX_COMPAT_H_ */
