3285679dd1af0a7515c47704c2994fbacaa1bdbd
Ilia Vaprol <ki.stfu@gmail.com>
2014-03-22 00:46:26 +0000

    extbld-porting: Add SECTION_REGION to all .lds

--- mk/image2.mk
+++ mk/image2.mk
@@ -197,22 +197,22 @@ GPATH := $(OBJ_DIR:$(ROOT_DIR)/%=%)
 VPATH += $(GPATH)
 
 embox_o = $(OBJ_DIR)/embox.o
+image_lds = $(OBJ_DIR)/mk/image.lds
 
 $(embox_o): ldflags_all = $(LDFLAGS) \
 		$(call fmt_line,$(call ld_scripts_flag,$(ld_scripts)))
-$(embox_o): $$(common_prereqs)
-	$(LD) -r $(ldflags_all) \
+$(embox_o): $(image_lds) $$(common_prereqs)
+	$(LD) -r -T $(image_lds) $(ldflags_all) \
 		$(call fmt_line,$(ld_objs)) \
 		--start-group \
 		$(call fmt_line,$(ld_libs)) \
 		--end-group \
-	--cref -Map $@.map \
-	-o $@
+		--cref -Map $@.map \
+		-o $@
 
 __define_image_rules = $(eval $(value __image_rule))
 $(call __define_image_rules,$(embox_o))
 
-image_lds = $(OBJ_DIR)/mk/image.lds
 $(image_lds) : $$(common_prereqs)
 $(image_lds) : flags_before :=
 $(image_lds) : flags = \
