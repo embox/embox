42fe2be4b3a5d4171298b1c440704f642d25567c
Anton Kozlov <drakon.mega@gmail.com>
2014-04-02 10:44:06 +0000

    extbld-porting: flags cleanup, link with embox.o
    (which is much faster)

--- mk/extbld/toolchain.mk
+++ mk/extbld/toolchain.mk
@@ -5,6 +5,8 @@ include $(MKGEN_DIR)/build.mk
 include mk/flags.mk
 include $(SRCGEN_DIR)/image.rule.mk
 
+
+rootdir := $(abspath $(ROOT_DIR))
 EMBOX_IMPORTED_CPPFLAGS :=
 EMBOX_IMPORTED_CPPFLAGS += -I$(shell pwd)/include
 EMBOX_IMPORTED_CPPFLAGS += $(filter -I%,$(EMBOX_EXPORT_CPPFLAGS))
@@ -60,44 +62,31 @@ endif
 
 EMBOX_IMPORTED_LDFLAGS_FULL :=
 EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--relax
+EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,-T,$(rootdir)/build/base/obj/mk/image.lds
 EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=__symbol_table=0,--defsym=__symbol_table_size=0
-#EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,-T,$(ROOT_DIR)/build/base/obj/mk/image.lds
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_ram_base=0,--defsym=_ram_size=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_traps_text_start=0,--defsym=_traps_text_end=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_text_vma=0,--defsym=_text_lma=0,--defsym=_text_len=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_rodata_vma=0,--defsym=_rodata_lma=0,--defsym=_rodata_len=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_data_vma=0,--defsym=_data_lma=0,--defsym=_data_len=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_bss_vma=0,--defsym=_bss_lma=0,--defsym=_bss_len=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_ctors_start=0,--defsym=_ctors_end=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_dtors_start=0,--defsym=_dtors_end=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=__EH_FRAME_BEGIN__=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_bss_end=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_reserve_end=0
-EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--defsym=_bss_len=0,--defsym=_bss_len_with_reserve=0
+EMBOX_IMPORTED_LDFLAGS_FULL += $(rootdir)/build/base/obj/embox.o
+EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--start-group
+EMBOX_IMPORTED_LDFLAGS_FULL += $(__image_ld_libs1:.%=-Wl,$(rootdir)%)
+EMBOX_IMPORTED_LDFLAGS_FULL += -Wl,--end-group
+
+# We are not including user ld scripts in flags, since it is not required to do
+# working object, only compilation errors are matter. If required, place it below
+# --relax flag and perfrom 2 phase link at arch-unknown-embox, first with user lds,
+# next with image.lds
+#EMBOX_IMPORTED_LDFLAGS_FULL += $(__image_ld_scripts1:.%=-Wl,-T,$(rootdir)%)
 
 EMBOX_IMPORTED_MAKEFLAGS :=
 ifneq (,$(filter -j,$(MAKEFLAGS)))
 EMBOX_IMPORTED_MAKEFLAGS += -j $(shell nproc)
 endif
 
-rootdir := $(abspath $(ROOT_DIR))
-
 $(EMBOX_GCC_ENV): $(MKGEN_DIR)/build.mk mk/flags.mk $(SRCGEN_DIR)/image.rule.mk
-$(EMBOX_GCC_ENV): _ld_objs    = $(__image_ld_objs1:.%=-Wl,$(rootdir)%)
-$(EMBOX_GCC_ENV): _ld_libs    = $(__image_ld_libs1:.%=-Wl,$(rootdir)%)
-$(EMBOX_GCC_ENV): _ld_scripts = $(__image_ld_scripts1:.%=-Wl,-T,$(rootdir)%)
 $(EMBOX_GCC_ENV): | $(dir $(EMBOX_GCC_ENV))
-	@echo "EMBOX_CROSS_COMPILE='$(CROSS_COMPILE)'"               >> $@
-	@echo "EMBOX_IMPORTED_CPPFLAGS='$(EMBOX_IMPORTED_CPPFLAGS)'" >> $@
-	@echo "EMBOX_IMPORTED_CFLAGS='$(EMBOX_IMPORTED_CFLAGS)'"     >> $@
-	@echo "EMBOX_IMPORTED_CXXFLAGS='$(EMBOX_IMPORTED_CXXFLAGS)'" >> $@
-	@echo "EMBOX_IMPORTED_LDFLAGS='$(EMBOX_IMPORTED_LDFLAGS)'"   >> $@
-	@echo "EMBOX_IMPORTED_LDFLAGS_FULL='$(EMBOX_IMPORTED_LDFLAGS_FULL) \
-		$(_ld_scripts) \
-		$(_ld_objs) \
-		-Wl,--start-group \
-		$(_ld_libs) \
-		-Wl,--end-group \
-		'"   >> $@
+	@echo "EMBOX_CROSS_COMPILE='$(CROSS_COMPILE)'"                       >> $@
+	@echo "EMBOX_IMPORTED_CPPFLAGS='$(EMBOX_IMPORTED_CPPFLAGS)'"         >> $@
+	@echo "EMBOX_IMPORTED_CFLAGS='$(EMBOX_IMPORTED_CFLAGS)'"             >> $@
+	@echo "EMBOX_IMPORTED_CXXFLAGS='$(EMBOX_IMPORTED_CXXFLAGS)'"         >> $@
+	@echo "EMBOX_IMPORTED_LDFLAGS='$(EMBOX_IMPORTED_LDFLAGS)'"           >> $@
+	@echo "EMBOX_IMPORTED_LDFLAGS_FULL='$(EMBOX_IMPORTED_LDFLAGS_FULL)'" >> $@
 
 $$(image_prerequisites):
