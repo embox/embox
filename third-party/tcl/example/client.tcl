2e37d0b43f7e91a4aa7dbe664efb1baace4b40a7
Alex Kalmuk <alexkalmuk@gmail.com>
2014-04-04 12:17:51 +0000

    tcl: client improved

--- third-party/tcl/example/client.tcl
+++ third-party/tcl/example/client.tcl
@@ -1,7 +1,32 @@
-set server "10.0.2.16"
+set server "192.168.0.210"
 set port   12345
+set prompt ">>"
 
+proc print_data_from_sock {sock} {
+	# waiting while some data will be avaliable
+	gets $sock buf
+
+	fconfigure $sock -blocking 0
+	while 1 {
+		puts $buf
+		if {[gets $sock buf] < 0} break
+	}
+	fconfigure $sock -blocking 1
+}
+
+puts "connecting to $server"
 set sock [socket $server $port]
-puts $sock "hello"
-flush $sock
+puts "connected to server"
+fconfigure $sock -buffering none
+fconfigure stdout -buffering none
+
+puts "enter commands:"
+puts -nonewline $prompt
+flush stdout
+
+while {[gets stdin cmd] >= 0} {
+	puts $sock $cmd
+	print_data_from_sock $sock
+	puts -nonewline $prompt
+}
 
