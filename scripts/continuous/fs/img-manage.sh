328d24d5e7d7eca37f74c458bfbb500240ba1317
Anton Kozlov <drakon.mega@gmail.com>
2014-04-08 16:27:38 +0000

    autotest-fs: img_manage is responsible for ro/rw decision
    run could test NFS (in theory, haven't tested yet)

--- scripts/continuous/fs/img-manage.sh
+++ scripts/continuous/fs/img-manage.sh
@@ -1,20 +1,16 @@
+#!/bin/sh
 
 FS=$1
-BASE_DIR=$PWD
-WORK_DIR=$PWD
+IMG=$2
+ACTION=$3
+BASE_DIR=$(dirname $0)
+WORK_DIR=$BASE_DIR
 SUDO=sudo
-
-shift
 
 MOUNT_POINT=$WORK_DIR/mountpoint
 
-IMG_RO_CONTENT=$BASE_DIR/img-ro
-IMG_RW_CONTENT=$BASE_DIR/img-rw
-IMG_RW_GOLD=$BASE_DIR/img-rw-gold
-
-IMG=$FS.img
-
-writeable_fs=([fat]=1)
+build_support="vfat ext2 ext3 ext4 iso9660 jffs2"
+mount_support="vfat ext2 ext3 ext4"
 
 do_mount() {
 	$SUDO mount $IMG -t $FS $MOUNT_POINT
@@ -24,37 +20,72 @@ do_unmount() {
 	sudo umount $MOUNT_POINT
 }
 
-fs_iswriteable() {
-	[ ! -z ${writeable_fs[$FS]} ]
+is_inlist() {
+	echo $2 | grep $1 >/dev/null
+}
+
+mk_mountpoint() {
+	[ -d $MOUNT_POINT ] || mkdir $MOUNT_POINT
+}
+
+build_dir() {
+	if [ ! -d $1 ]; then
+		$SUDO mkdir -p $1
+	fi
+
+	$SUDO rm $1/* 2>/dev/null || true
+	$SUDO cp -R $1/* $2
 }
 
 build() {
+	content=$1
+
+	case $FS in
+		jffs2)
+			mkfs.jffs2 -p 0x10000 -r $content -o $IMG
+			return 0
+			;;
+		iso9660)
+			mkisofs -o $IMG $content
+			return 0
+			;;
+	esac
+
 	dd if=/dev/zero bs=1M count=8 of=$IMG
-	mkfs.$FS $IMG
 
-	if fs_iswriteable; then
-		content=$IMG_RW_CONTENT
-	else
-		content=$IMG_RO_CONTENT
-	fi
+	case $FS in
+		ext?)
+			mkfs.$FS -F -b 1024 $IMG
+			;;
+		*)
+			mkfs.$FS $IMG
+			;;
+	esac
 
+	mk_mountpoint
 	do_mount
 
-	$SUDO cp -R $content/* $MOUNT_POINT
+	build_dir $content $MOUNT_POINT
 
 	do_unmount
 }
 
+check_dir() {
+	diff -q -r $1 $2
+	ret=$?
+	echo img-manage: diff code $ret
+	return $ret
+}
+
 check() {
+	content=$1
+
+	is_inlist $FS "$mount_support" || ( echo "Warning $FS check is not supported";  return 0 )
+
+	mk_mountpoint
 	do_mount
 
-	if fs_iswriteable; then
-		content=$IMG_RW_GOLD
-	else
-		content=$IMG_RO_CONTENT
-	fi
-
-	diff -q -r $content $MOUNT_POINT
+	check_dir $content $MOUNT_POINT
 	ret=$?
 
 	do_unmount
@@ -62,4 +93,10 @@ check() {
 	exit $ret
 }
 
-eval $@
+case $ACTION in
+	build_dir) 	build_dir $4 $2 ;;
+	build) 		build $4 ;;
+	check_dir) 	check_dir $4 $2;;
+	check) 		check $4 ;;
+	write_content)	write_content $4 ;;
+esac
--- scripts/continuous/fs/run.sh
+++ scripts/continuous/fs/run.sh
@@ -1,42 +1,125 @@
+#!/bin/sh
+
+#FS_TEST_RO="iso9660 jffs2"
+#FS_TEST_RW="vfat ext2 ext3 ext4"
+#FS_TEST_RO=""
+#FS_TEST_RW="ext2 "
+FS_TEST_NETWORK="nfs"
+
+FS_TEST_NFS_ROOT="/var/nfs_root/fs_test"
 
 ROOT_DIR=.
 BASE_DIR=$(dirname $0)
 
-
-IMGS=$(ls $BASE_DIR/*.img)
 START_SCRIPT=$ROOT_DIR/conf/start_script.inc
+CONT_BASE=$ROOT_DIR/scripts/continuous
+CONT_FS_MANAGE=$CONT_BASE/fs/img-manage.sh
+CONT_RUN=$CONT_BASE/run.sh
 
-start_script_store() {
-	cp $START_SCRIPT $START_SCRIPT.old
-}
+IMG_RO_CONTENT=$BASE_DIR/img-ro
+IMG_RW_CONTENT=$BASE_DIR/img-rw
+IMG_RW_GOLD=$BASE_DIR/img-rw-gold
 
-start_script_restore() {
-	mv $START_SCRIPT.old $START_SCRIPT
+posted_ret=0
+check_post_exit() {
+	ret=$?
+	if [ 0 -ne $ret ]; then
+		posted_ret=$ret
+	fi
 }
 
 check_exit() {
-	ret=$?
-	if [ 0 -ne $ret ]; then
-		start_script_restore
+	check_post_exit
+	if [ 0 -ne $posted_ret ]; then
 		exit $ret
 	fi
 }
 
-for i in $IMGS; do
+run_qemu_fs() {
+	FS=$1
+	IMG=$2
+
 	cp $START_SCRIPT $START_SCRIPT.old
 
-	echo \"mount -t $(basename ${i%.img}) /dev/hda /mnt/fs_test\", >> $START_SCRIPT
+	img_mount=
+	QEMU_MOUNT_HD="\"mount -t $FS /dev/hda /mnt/fs_test\","
+	case $FS in
+		vfat | ext2 | ext3 | ext4 | qnx6)
+			img_mount="$QEMU_MOUNT_HD"
+			;;
+		jffs2)
+			img_mount="\"mkfs -t $FS /dev/hda\",$QEMU_MOUNT_HD"
+			;;
+		iso9660)
+			img_mount="\"mount -t $FS /dev/cd0 /mnt/fs_test\","
+			;;
+		cifs | nfs)
+			img_mount="\"mount -t $FS 10.0.2.10:$IMG /mnt/fs_test\","
+			;;
+	esac
+
+	echo $img_mount >> $START_SCRIPT
 	echo \"test -t fs_test\", >> $START_SCRIPT
 	echo \"umount /mnt/fs_test\", >> $START_SCRIPT
 
 	make &> /dev/null
-	check_exit
 
-	#$BASE_DIR/../run.sh x86/test_fs "-hda $i"
+	img_run=
+	case $FS in
+		vfat | ext2 | ext3 | ext4 | qnx6 | jffs2)
+			img_run="-hda $IMG"
+			;;
+		iso9660)
+			img_run="-cdrom $IMG"
+			;;
+		cifs | nfs)
+			;;
+	esac
 
-	./scripts/qemu/auto_qemu -hda $i
-	check_exit
+	$CONT_RUN x86/test_fs "$img_run"
+	#./scripts/qemu/auto_qemu $img_run
+	check_post_exit
 
-	start_script_restore
+	mv $START_SCRIPT.old $START_SCRIPT
+}
+
+for f in $FS_TEST_RO; do
+	img=$BASE_DIR/$f.img
+
+	$CONT_FS_MANAGE $f $img build $IMG_RO_CONTENT
+
+	run_qemu_fs $f $img
+	check_post_exit
 done
 
+for f in $FS_TEST_RW; do
+	img=$BASE_DIR/$f.img
+	img_work=$img.work
+
+	$CONT_FS_MANAGE $f $img build $IMG_RW_CONTENT
+
+	cp $img $img_work
+
+	run_qemu_fs $f $img_work
+	check_post_exit
+
+	$CONT_FS_MANAGE $f $img_work check $IMG_RW_GOLD
+	check_post_exit
+
+	rm $img_work
+done
+
+for f in $FS_TEST_NETWORK; do
+	case $f in
+		nfs)
+			$CONT_FS_MANAGE $f $FS_TEST_NFS_ROOT build_dir $IMG_RW_CONTENT
+			run_qemu_fs $f $FS_TEST_NFS_ROOT
+			check_post_exit
+
+			$CONT_FS_MANAGE $f $FS_TEST_NFS_ROOT check_dir $IMG_RW_GOLD
+			check_post_exit
+			;;
+	esac
+done
+
+exit $posted_ret
