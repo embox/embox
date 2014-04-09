1b85ba1a8ed441bea66174ddf8d15c41d4da1ccb
Ilia Vaprol <ki.stfu@gmail.com>
2014-04-09 10:36:44 +0000

    extbld-porting: iperf: Add extern C++ for stubs

--- third-party/iperf/include/stdlib.h
+++ third-party/iperf/include/stdlib.h
@@ -1,9 +1,15 @@
 #ifndef IPERF_STDLIB_H_
 #define IPERF_STDLIB_H_
 
+#include_next <stdlib.h>
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
 extern int atexit(void (*func)(void));
 
-#include_next <stdlib.h>
+__END_DECLS
 
 #endif /* IPERF_STDLIB_H_ */
 
--- third-party/iperf/include/sys/time.h
+++ third-party/iperf/include/sys/time.h
@@ -3,9 +3,15 @@
 
 #include_next <sys/time.h>
 
+#include <sys/cdefs.h>
+
 #define ITIMER_REAL 0
+
+__BEGIN_DECLS
 
 extern int setitimer(int which, const struct itimerval *value,
 		struct itimerval *ovalue);
+
+__END_DECLS
 
 #endif /* IPERF_SYS_TIME_H_ */
