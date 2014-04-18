ebbecc14d25a56760e3f681ecbfeabe999d0bee4
Denis Deryugin <deryugin.denis@gmail.com>
2014-04-18 16:00:37 +0000

    profile: deleteing unnecessary funcion enter_condition()

--- src/profiler/tracing/__cyg_profile.c
+++ src/profiler/tracing/__cyg_profile.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include <profiler/tracing/trace.h>
 
-static inline int enter_condition() {
-	return 1;
-	/* TODO: remove this funcion */
-	/*return !critical_inside(CRITICAL_IRQ_LOCK) && !critical_inside(CRITICAL_IRQ_HANDLER) &&
-			!critical_inside(CRITICAL_SOFTIRQ_LOCK)  && !critical_inside(CRITICAL_SOFTIRQ_HANDLER) &&
-			!critical_inside(CRITICAL_SCHED_LOCK);
-*/}
-
 void __cyg_profile_func_enter(void *func, void *caller) {
 	/* This function is for instrument profiling. It is being called just before
 	 * every call of instrumented funcion.
@@ -16,8 +8,7 @@ void __cyg_profile_func_enter(void *func, void *caller) {
 	 */
 	if (cyg_profiling) {
 		cyg_profiling = false;
-		if (enter_condition())
-			trace_block_func_enter(func);
+		trace_block_func_enter(func);
 		cyg_profiling = true;
 	}
 }
@@ -29,8 +20,7 @@ void __cyg_profile_func_exit(void *func, void *caller) {
 	 */
 	 if (cyg_profiling) {
 		cyg_profiling = false;
-		if (enter_condition())
-			trace_block_func_exit(func);
+		trace_block_func_exit(func);
 		cyg_profiling = true;
 	}
 }
