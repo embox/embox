7138554cb8edac7e0cb3cfd471de5cbe922a9d16
Ilia Vaprol <ki.stfu@gmail.com>
2014-04-24 14:29:58 +0000

    task: Clean task_heap resource

--- src/kernel/task/resource/task_heap.c
+++ src/kernel/task/resource/task_heap.c
@@ -15,23 +15,13 @@
 
 #include <util/dlist.h>
 
-
-
 TASK_RESOURCE_DEF(task_heap_desc, struct task_heap);
 
 extern int mspace_init(struct dlist_head *mspace);
 extern int mspace_fini(struct dlist_head *mspace);
 
-
-struct task_heap *task_heap_get(const struct task *task);
-
 static void task_heap_init(const struct task *task, void *task_heap) {
 	mspace_init(&task_heap_get(task)->mm);
-}
-
-static int task_heap_inherit(const struct task *task, const struct task *parent) {
-	mspace_init(&task_heap_get(task)->mm);
-	return 0;
 }
 
 static void task_heap_deinit(const struct task *task) {
@@ -42,7 +32,6 @@ static size_t task_heap_offset;
 
 static const struct task_resource_desc task_heap_desc = {
 	.init = task_heap_init,
-	.inherit = task_heap_inherit,
 	.deinit = task_heap_deinit,
 	.resource_size = sizeof(struct task_heap),
 	.resource_offset = &task_heap_offset
