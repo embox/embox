7f92e64497f2180212b6d8b4d15e8db3000229c7
Kirill Smirnov <lich@math.spbu.ru>
2014-04-29 12:15:51 +0000

    There is one HBA with two ports on ti8168
    Power on HBA.

--- src/drivers/ahci/ti8168.c
+++ src/drivers/ahci/ti8168.c
@@ -1,19 +1,17 @@
 #include <embox/unit.h>
 #include <drivers/ahci.h>
+#include <hal/reg.h>
 
 EMBOX_UNIT_INIT(ahci_ti8168_init);
 
-static struct ahci_hba ti8168_hba[] = {
-	{
-		.base_addr = 0x4A140000,
-	},
-	{
-		.base_addr = 0x4A0AE000,
-	},
+static struct ahci_hba ti8168_hba= {
+	.base_addr = 0x4A140000,
 };
 
 int ahci_ti8168_init(void) {
-	register_ahci_hba(&ti8168_hba[0]);
-	register_ahci_hba(&ti8168_hba[1]);
+	/* Power on device */
+	REG_ORIN(0x48180000 + 0x560, 2);
+
+	register_ahci_hba(&ti8168_hba);
 	return 0;
 }
