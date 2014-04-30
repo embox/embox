af285c2695358bec76559e7353525969dd51eaba
Vita Loginova <vita.loginova@gmail.com>
2014-04-30 13:39:06 +0000

    fs: dcache: cache most recent directories

--- src/fs/syslib/dcache.c
+++ src/fs/syslib/dcache.c
@@ -5,6 +5,7 @@
  * @author Vita Loginova
  */
 #include <util/hashtable.h>
+#include <util/dlist.h>
 #include <errno.h>
 #include <embox/unit.h>
 #include <string.h>
@@ -20,47 +21,78 @@
 
 EMBOX_UNIT_INIT(dcache_init);
 
-POOL_DEF(dcache_path_pool, struct path, DCACHE_TABLE_SIZE);
-
-struct hashtable *dcache_table = NULL;
-
 //todo may be localname + mount_table link
-struct dcache {
+struct dkey {
 	char fullpath[DCACHE_MAX_NAME_SIZE];
 };
 
+struct dvalue {
+	struct dkey key;
+	struct path path;
+	struct dlist_head link;
+};
+
+POOL_DEF(dcache_path_pool, struct dvalue, DCACHE_TABLE_SIZE);
+
+static struct hashtable *dcache_table = NULL;
+static struct dlist_head values;
+
+static void delete(struct dvalue *dvalue) {
+	hashtable_del(dcache_table, &dvalue->key);
+	dlist_del(&dvalue->link);
+	pool_free(&dcache_path_pool, dvalue);
+}
+
+static void dvalue_init(struct dvalue *dvalue, struct path *path, const char *fullpath) {
+	strcpy(dvalue->key.fullpath, fullpath);
+	dvalue->path = *path;
+	dlist_head_init(&dvalue->link);
+}
+
+static void __dcache_add(struct dvalue *dvalue) {
+	dlist_add_next(&dvalue->link, &values);
+	hashtable_put(dcache_table, &dvalue->key, dvalue);
+}
+
 int dcache_add(const char *fullpath, struct path *path) {
-	struct dcache dcache;
-	struct path *dpath;
+	struct dvalue *dvalue;
 
-	*dcache.fullpath = *fullpath;
-
-	if(NULL == (dpath = pool_alloc(&dcache_path_pool))) {
+	if (*fullpath == '\0') {
 		return -1;
 	}
 
-	*dpath = *path;
+	if (NULL == (dvalue = pool_alloc(&dcache_path_pool))) {
+		struct dvalue *last;
 
-	hashtable_put(dcache_table, &dcache, dpath);
+		last = dlist_entry(values.prev, struct dvalue, link);
+		delete(last);
+		dvalue = pool_alloc(&dcache_path_pool);
+
+		assert(NULL != dvalue);
+	}
+
+	dvalue_init(dvalue, path, fullpath);
+	__dcache_add(dvalue);
 
 	return 0;
 }
 
 struct path *dcache_get(const char *fullpath) {
-	struct dcache dcache;
+	struct dkey dkey;
+	struct dvalue *dvalue;
 
-	if (fullpath == '\0') {
-		return 0;
+	if (*fullpath == '\0') {
+		return NULL;
 	}
 
-	*dcache.fullpath = *fullpath;
-	return hashtable_get(dcache_table, &dcache);
+	strcpy(dkey.fullpath, fullpath);
+	dvalue = hashtable_get(dcache_table, &dkey);
+	return dvalue ? &dvalue->path : NULL;
 }
 
-
-static size_t dcache_hash(struct dcache *dcache) {
+static size_t dcache_hash(struct dkey *dkey) {
 	size_t hash;
-	char *path = &dcache->fullpath[0];
+	char *path = dkey->fullpath;
 
 	hash = 0;
 	while (*path != '\0') {
@@ -70,17 +102,20 @@ static size_t dcache_hash(struct dcache *dcache) {
 	return hash;
 }
 
-static int dcache_cmp(struct dcache *key1, struct dcache *key2) {
-	return strcmp(&key1->fullpath[0], &key1->fullpath[0]);
+static int dcache_cmp(struct dkey *key1, struct dkey *key2) {
+	return strcmp(key1->fullpath, key2->fullpath);
 }
 
 static int dcache_init(void) {
 	dcache_table = hashtable_create(DCACHE_TABLE_SIZE,
-				(get_hash_ft)&dcache_hash, (ht_cmp_ft)&dcache_cmp);
-		if (dcache_table == NULL) {
-			return -ENOMEM;
-		}
+			(get_hash_ft) &dcache_hash, (ht_cmp_ft) &dcache_cmp);
 
-		return 0;
+	if (dcache_table == NULL) {
+		return -ENOMEM;
+	}
+
+	dlist_init(&values);
+
+	return 0;
 }
 
