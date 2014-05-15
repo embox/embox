6db19d9fcfc010097be54d6e901a4133d9ddea2c
Anton Kozlov <drakon.mega@gmail.com>
2014-05-15 13:00:01 +0000

    exec vfork-aware implementation

--- src/compat/posix/proc/exec.c
+++ src/compat/posix/proc/exec.c
@@ -6,22 +6,41 @@
  * @date 13.09.2011
  * @author Anton Bondarev
  * @author Anton Kozlov
- * 	- shell invokation
+ * 	- shell invocation
  */
 
 #include <string.h>
-#include <cmd/shell.h>
 #include <unistd.h>
+#include <kernel/thread.h>
+#include <kernel/sched.h>
+#include <hal/context.h>
+#include <cmd/shell.h>
 
-int execv(const char *path, char *const argv[]) {
+extern void vfork_release_parent(void);
+
+#define EXEC_LEN 16
+static char exec_path[EXEC_LEN];
+
+#if 0
+#define EXEC_ARGC 8
+static int  exec_argc;
+static char exec_argv[EXEC_ARGC][EXEC_LEN];
+#endif
+
+static void exec_trampoline(void) {
 	const struct shell *sh;
 	const char *default_shells[] = { "/bin/sh", "sh", NULL };
 	const char **shellp;
+	const char *path = exec_path;
 	int ecode;
+
+	sched_unlock();
+
+	vfork_release_parent();
 
 	sh = shell_lookup(path);
 	if (!sh) {
-		for (shellp = default_shells; shellp != NULL; shellp++) {
+		for (shellp = default_shells; *shellp != NULL; shellp++) {
 			if (!strcmp(*shellp, path)) {
 				sh = shell_lookup("tish");
 				break;
@@ -33,6 +52,23 @@ int execv(const char *path, char *const argv[]) {
 	ecode = sh ? shell_run(sh) : shell_exec(shell_any(), path);
 
 	_exit(ecode);
+}
+
+int execv(const char *path, char *const argv[]) {
+	struct context oldctx;
+	struct thread *t;
+
+	strncpy(exec_path, path, sizeof(exec_path) - 1);
+
+	sched_lock();
+	t = thread_self();
+
+	context_init(&t->context, true);
+	context_set_entry(&t->context, exec_trampoline);
+	context_set_stack(&t->context,
+			thread_stack_get(t) + thread_stack_get_size(t));
+
+	context_switch(&oldctx, &t->context);
 
 	return 0;
 }
