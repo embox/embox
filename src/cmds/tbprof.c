4ed66202c22fdae31462324a43b98768d4b547c0
Denis Deryugin <deryugin.denis@gmail.com>
2014-05-17 18:04:45 +0000

    profile: Work on tbprof

--- src/cmds/tbprof.c
+++ src/cmds/tbprof.c
@@ -16,6 +16,8 @@
 
 EMBOX_CMD(tbprof_main);
 
+static int run_count = 1;
+
 void initialize_hashtable() {
 	trace_block_hashtable_init();
 }
@@ -40,8 +42,8 @@ void run_cmd(const struct cmd *cmd, int argc, char *argv[], FILE *out) {
 	/*trace_block_hashtable_init();*/
 	printf("Executing command\n");
 	begin = clock();
-	while (true) {
-		printf ("TBPROF RUN #%d\n", run++);
+	for (run = 0; run < run_count; run++) {
+		printf ("TBPROF RUN #%d\n", run);
 		set_profiling_mode(CYG_PROFILING);
 		res = cmd_exec(cmd, argc, argv);
 		set_profiling_mode(DISABLED);
@@ -58,13 +60,24 @@ void run_cmd(const struct cmd *cmd, int argc, char *argv[], FILE *out) {
 static int tbprof_main (int argc, char *argv[]) {
 	/* parse params */
 	const struct cmd *c_cmd;
-	int c_argc = argc, opt;
+	int c_argc = argc, opt, argnum = 1;
 	FILE *out = NULL;
 
 	getopt_init();
 
-	while (-1 != (opt = getopt(argc, argv, "ed"))) {
+	while (-1 != (opt = getopt(argc, argv, "chn:ed"))) {
+		argnum++;
 		switch (opt) {
+			case 'c':
+				/* execute command */
+				c_cmd = cmd_lookup(argv[argnum]);
+				run_cmd(c_cmd, c_argc - argnum, argv + argnum, out);
+				return 0;
+				break;
+			case 'n':
+				sscanf(optarg, "%d", &run_count);
+				argnum++;
+				break;
 			case 'e':
 				initialize_hashtable();
 				set_profiling_mode(CYG_PROFILING);
@@ -76,12 +89,11 @@ static int tbprof_main (int argc, char *argv[]) {
 				printf("cyg_profiling disabled.\n");
 				return 0;
 				break;
+			case 'h':
+				printf("Example: tbprof [-n count] -c [cmd]\nUse man tbprof for more info.\n");
+				break;
 		}
 	}
-
-
-	c_cmd = cmd_lookup(argv[1]);
-	run_cmd(c_cmd, c_argc - 1, argv + 1, out);
-
+	printf("Wrong arguments. tbprof -h for usage.\n");
 	return 0;
 }
