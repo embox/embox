309cdfb58d5ebd3515c96e73781f2451fe090c90
Ilia Vaprol <ki.stfu@gmail.com>
2014-05-27 08:18:39 +0000

    phoneme: javacall_cldc_dir.c: Use dirent instead of VFS nodes

--- third-party/phoneme/javacall_cldc_dir.c
+++ third-party/phoneme/javacall_cldc_dir.c
@@ -7,21 +7,12 @@
  */
 
 #include <javacall_dir.h>
-#include <mem/objalloc.h>
 #include <string.h>
-#include <fs/vfs.h>
+#include <dirent.h>
 #include "embox_java_compat.h"
 
-struct java_dir {
-	node_t *dir;
-	node_t *cur_file;
-};
-
-OBJALLOC_DEF(__dir_pool, struct java_dir, 64);
-
 javacall_handle javacall_dir_open(const javacall_utf16* path, int pathLen) {
-	node_t *node;
-	struct java_dir *jdir;
+	DIR *dir;
 	javacall_int32 utf8NameLen;
 	unsigned char *utf8Name;
 
@@ -29,53 +20,33 @@ javacall_handle javacall_dir_open(const javacall_utf16* path, int pathLen) {
 		return NULL;
 	}
 
-    node = vfs_lookup(NULL, (const char *) utf8Name);
-    jdir = objalloc(&__dir_pool);
-    if (NULL == jdir) {
-    	return NULL;
-    }
-    jdir->dir = node;
-    jdir->cur_file = NULL;
+	dir = opendir((const char *) utf8Name);
 
     free(utf8Name);
 
-    return node;
+	return dir;
 }
 
 /* FIXME O(n^2). It may be implement in O(n) */
 javacall_utf16* javacall_dir_get_next(javacall_handle handle, int* /*OUT*/ outFilenameLength) {
-	node_t *item;
 	javacall_utf16* name;
 	javacall_int32 namelen;
-	int get_this_item = 0;
-	struct java_dir *jdir = (struct java_dir *)handle;
+	struct dirent *dent;
 
-	/* FIXME look for basename. See javacall_dir.h */
-	tree_foreach_children(item, (&jdir->dir->tree_link), tree_link) {
-		if (NULL == jdir->cur_file) {
-			break;
-		}
-		if (get_this_item) {
-			break;
-		}
-		if (jdir->cur_file == item) {
-			/* and get next file */
-			get_this_item = 1;
-		}
+	dent = readdir((DIR *)handle);
+	if (dent == NULL) {
+		return NULL;
 	}
 
-	jdir->cur_file = item;
-
 	/* FIXME freeing of memory will be doing in dir_close */
-	utf8_to_utf16(&name, &namelen, (const unsigned char *)item->name, strlen(item->name));
+	utf8_to_utf16(&name, &namelen, (const unsigned char *)dent->d_name, strlen(dent->d_name));
 	*outFilenameLength = namelen;
 
 	return name;
 }
 
 void javacall_dir_close(javacall_handle handle) {
-	/* do freeing of memory allocated in javacall_dir_get_next */
-	objfree(&__dir_pool, handle);
+	closedir((DIR *)handle);
 }
 
 javacall_int64 javacall_dir_get_free_space_for_java(void){
