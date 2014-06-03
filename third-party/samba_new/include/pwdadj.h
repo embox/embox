ecaccef515682ae8ec29ff140c410e2f680c2ca9
Alex Kalmuk <alexkalmuk@gmail.com>
2014-06-03 11:49:33 +0000

    samba_new builded (but not installed)

---
+++ third-party/samba_new/include/pwdadj.h
@@ -0,0 +1,13 @@
+/*
+ * pwdadj1.h
+ *
+ *  Created on: 2014 6 3
+ *      Author: alexander
+ */
+
+#ifndef PWDADJ1_H_
+#define PWDADJ1_H_
+
+
+
+#endif /* PWDADJ1_H_ */
---
+++ third-party/samba_new/include/sys/label.h
@@ -0,0 +1,13 @@
+/*
+ * label.h
+ *
+ *  Created on: 2014 6 3
+ *      Author: alexander
+ */
+
+#ifndef LABEL_H_
+#define LABEL_H_
+
+
+
+#endif /* LABEL_H_ */
