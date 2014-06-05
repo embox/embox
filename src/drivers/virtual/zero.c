7e7a3480bdd5b5897c0310d3e4f542dc95b6f8c1
Anton Bondarev <anton.bondarev2310@gmail.com>
2014-06-05 14:54:04 +0000

    /dev/zero can write

--- src/drivers/virtual/zero.c
+++ src/drivers/virtual/zero.c
@@ -32,10 +32,15 @@ static size_t zero_read(struct file_desc *desc, void *buf, size_t size) {
 	return size;
 }
 
+static size_t zero_write(struct file_desc *desc, void *buf, size_t size) {
+	return size;
+}
+
 static const struct kfile_operations zero_ops = {
 		.open = zero_open,
 		.close = zero_close,
-		.read = zero_read
+		.read = zero_read,
+		.write = zero_write
 };
 
 static int zero_init(void) {
