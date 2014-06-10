a31b10460e1971ce936c9ed05355526d1abbe052
Alex Kalmuk <alexkalmuk@gmail.com>
2014-06-10 13:41:21 +0000

    Work on ntfs-3g

---
+++ third-party/ntfs-3g-new/include/getopt.h
@@ -0,0 +1,13 @@
+/*
+ * getopt.h
+ *
+ *  Created on: 3 juil. 2013
+ *      Author: fsulima
+ */
+
+#ifndef GETOPT_H_
+#define GETOPT_H_
+
+
+
+#endif /* GETOPT_H_ */
---
+++ third-party/ntfs-3g-new/include/regex.h
@@ -0,0 +1,13 @@
+/*
+ * regex.h
+ *
+ *  Created on: 3 juil. 2013
+ *      Author: fsulima
+ */
+
+#ifndef REGEX_H_
+#define REGEX_H_
+
+
+
+#endif /* REGEX_H_ */
