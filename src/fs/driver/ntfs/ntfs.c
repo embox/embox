4392a02a92f6d97cb3708fd1d34c7e3cbd113631
Alex Kalmuk <alexkalmuk@gmail.com>
2014-06-11 09:09:17 +0000

    NTFS fixed mount

--- src/fs/driver/ntfs/ntfs.c
+++ src/fs/driver/ntfs/ntfs.c
@@ -577,7 +577,7 @@ struct ntfs_bdev_desc {
 static int ntfs_device_bdev_io_open(struct ntfs_device *dev, int flags)
 {
 	int err;
-	node_t *dev_node;
+	node_t *dev_node, *dev_folder;
 
 	if (NDevOpen(dev)) {
 		errno = EBUSY;
@@ -591,7 +591,9 @@ static int ntfs_device_bdev_io_open(struct ntfs_device *dev, int flags)
 	if (!dev->d_private)
 		return -1;
 
-	dev_node = vfs_subtree_lookup_child(vfs_subtree_lookup_child(0,"dev"),dev->d_name);
+	dev_folder = vfs_subtree_lookup_child(vfs_get_root(),"dev");
+	dev_node = vfs_subtree_lookup_child(dev_folder, dev->d_name);
+
 	if (dev_node) {
 		((struct ntfs_bdev_desc*)dev->d_private)->dev = dev_node->nas->fi->privdata;
 		((struct ntfs_bdev_desc*)dev->d_private)->pos = 0;
