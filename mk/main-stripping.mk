ca2d51f9f402d9a7ce69153daa6972b74c5e1d97
Anton Kozlov <drakon.mega@gmail.com>
2014-06-24 13:23:11 +0000

    mk main-stripping.sh added

--- mk/main-stripping.mk
+++ mk/main-stripping.mk
@@ -6,31 +6,5 @@ ifeq ($(FILE_APP),)
 $(error FILE_APP is not set.)
 endif
 
-MODULE_ID :=$(TARGET_APP)
-TARGET_OBJ:=$(FILE_APP)
-SOURCE_OBJ:=$(subst strip.,,$(TARGET_OBJ))
-
-OBJCOPY := $(EMBOX_CROSS_COMPILE)objcopy
-OBJDUMP := $(EMBOX_CROSS_COMPILE)objdump
-CC := $(EMBOX_CROSS_COMPILE)gcc
-LD := $(EMBOX_CROSS_COMPILE)ld
-
-CMD_WRAPPER_SRC:=$(abspath $(ROOT_DIR))/mk/script/application_template.c
-
-redefd_main_obj=$<.redef.o
-cmd_wrapper_obj=$<.cmd.o
-localize_symbols=$<.lsyms.txt
-main_rename_name=main_$(MODULE_ID)
-
-$(TARGET_OBJ): $(SOURCE_OBJ)
-	$(OBJDUMP) -t $< | grep " g " | tr -s ' ' | cut -d \  -f 5 | \
-		grep -v main > $(localize_symbols)
-	$(OBJCOPY) --localize-symbols=$(localize_symbols) \
-		--redefine-sym=main=$(main_rename_name) \
-		$< $(redefd_main_obj)
-	$(CC) -DMAIN_ROUTING_NAME=$(main_rename_name) \
-		-D__EMBUILD_MOD__=$(MODULE_ID) \
-		$(EMBOX_CFLAGS) \
-		$(EMBOX_CPPFLAGS) \
-		-c -o $(cmd_wrapper_obj) $(CMD_WRAPPER_SRC)
-	$(LD) -r $(EMBOX_LDFLAGS) $(redefd_main_obj) $(cmd_wrapper_obj) -o $@
+$(FILE_APP) : $(subst strip.,,$(FILE_APP))
+	$(ROOT_DIR)/mk/main-stripping.sh $(TARGET_APP) $< $@
