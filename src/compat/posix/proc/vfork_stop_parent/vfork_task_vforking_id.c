11d10872a958d571ce99b47aa7471e1d4add8a16
Anton Bondarev <anton.bondarev2310@gmail.com>
2014-07-05 12:12:06 +0000

    vfork: Move different version of 'vfork' into separate folder

---
+++ src/compat/posix/proc/vfork_stop_parent/vfork_task_vforking_id.c
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Kozlov
+ * @date    26.06.2014
+ */
+
+#include <kernel/task/task_vforking.h>
+
+struct task *task_vforking(struct task *task) {
+	return task;
+}
