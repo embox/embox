eec5e6d956a9b44048c56750ed7159b58e07f990
Anton Kozlov <drakon.mega@gmail.com>
2014-07-07 21:09:26 +0000

    security smac_user_dynamic sets db's mode and owner

--- src/security/smac/smac_user_dynamic_init.c
+++ src/security/smac/smac_user_dynamic_init.c
@@ -14,7 +14,23 @@ EMBOX_UNIT_INIT(smac_user_dynamic_init_init);
 #define SMAC_USER_INIT_DB "/smac_users.init"
 #define SMAC_USER_TARGET_DB "/smac_users"
 
-static int smac_user_dynamic_init_init(void) {
 
-	return system("cp " SMAC_USER_INIT_DB " " SMAC_USER_TARGET_DB);
+static int smac_user_dynamic_init_init(void) {
+	const char *cmds[] = {
+		"cp " SMAC_USER_INIT_DB " " SMAC_USER_TARGET_DB,
+		"chown 1 " SMAC_USER_TARGET_DB,
+		"chmod u=rw,g=,o= " SMAC_USER_TARGET_DB,
+		NULL,
+	};
+	const char **cmd_p;
+	int res;
+
+	for (cmd_p = cmds; *cmd_p != NULL; cmd_p++) {
+		res = system(*cmd_p);
+		if (res) {
+			return res;
+		}
+	}
+
+	return 0;
 }
