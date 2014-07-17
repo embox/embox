8ffeff332e8774ce214f4d4f6d5e0e43deca8f5b
Anton Kozlov <drakon.mega@gmail.com>
2014-07-17 12:46:03 +0000

    libc clearerr signature fixed

--- src/compat/libc/include/stdio.h
+++ src/compat/libc/include/stdio.h
@@ -204,7 +204,7 @@ extern FILE *stderr;
 extern int fileno(FILE *stream);
 
 //TODO: stub
-extern int clearerr(FILE *stream);
+extern void clearerr(FILE *stream);
 extern int feof(FILE *stream);
 extern int ferror(FILE *stream);
 extern int fflush(FILE *fp);
