bc4eb9afd25e3b6b06fc1d838f148256bf81c74c
Anton Kozlov <drakon.mega@gmail.com>
2014-07-17 10:28:02 +0000

    posix: sched some functions implemented

--- src/compat/posix/sched_posix.c
+++ src/compat/posix/sched_posix.c
@@ -13,3 +13,15 @@ int sched_yield(void) {
 	sched_post_switch();
 	return 0;
 }
+
+int sched_get_priority_min(int policy) {
+	return SCHED_PRIORITY_MIN;
+}
+
+int sched_get_priority_max(int policy) {
+	return SCHED_PRIORITY_MAX;
+}
+
+int sched_setscheduler(pid_t pid, int policy, const struct sched_param *param) {
+	return 0;
+}
