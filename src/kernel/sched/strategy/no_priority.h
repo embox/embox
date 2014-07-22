6892bc3e54769a02986a77b47fcca175c44ae0cd
Anton Bondarev <anton.bondarev2310@gmail.com>
2014-07-22 16:27:35 +0000

    Fix build after merge

--- src/kernel/sched/strategy/no_priority.h
+++ src/kernel/sched/strategy/no_priority.h
@@ -40,4 +40,14 @@ static inline sched_priority_t thread_priority_get(struct thread *t) {
 static inline int thread_priority_set(struct thread *t, sched_priority_t new_priority) {
 	return 0;
 }
+
+static inline sched_priority_t thread_priority_inherit(struct thread *t,
+		sched_priority_t priority) {
+	return 0;
+}
+
+static inline sched_priority_t thread_priority_reverse(struct thread *t) {
+	return 0;
+}
+
 #endif /* STRATEGY_NO_PRIORITY_H_ */
