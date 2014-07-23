e75a8de8335715e75ad72cdf2bfc7e50387d1cd0
Anton Kozlov <drakon.mega@gmail.com>
2014-07-23 14:40:14 +0000

    cmd true added

---
+++ src/cmds/true.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Kozlov
+ * @date    22.07.2014
+ */
+
+#include <embox/cmd.h>
+
+EMBOX_CMD(true_main);
+
+static int true_main(int argc, char *argv[]) {
+	return 0;
+}
+
