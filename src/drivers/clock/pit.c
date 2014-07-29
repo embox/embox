081e36ec704f67527c1dc63021a949dffda8d0d7
Anton Kozlov <drakon.mega@gmail.com>
2014-07-29 13:59:56 +0000

    PIT slowdown factor removed

--- src/drivers/clock/pit.c
+++ src/drivers/clock/pit.c
@@ -25,9 +25,8 @@
 
 #define INPUT_CLOCK        1193182L /* clock tick rate, Hz */
 #define IRQ_NR             OPTION_GET(NUMBER,irq_num)
-#define SLOWDOWN_FACTOR    OPTION_GET(NUMBER,slowdown_factor)
 
-#define PIT_HZ (1000 / SLOWDOWN_FACTOR)
+#define PIT_HZ 1000
 
 #define PIT_LOAD ((INPUT_CLOCK + PIT_HZ / 2) / PIT_HZ)
 static_assert(PIT_LOAD < 0x10000);
@@ -118,7 +117,7 @@ static struct time_event_device pit_event_device = {
 
 static struct time_counter_device pit_counter_device = {
 	.read = i8253_read,
-	.cycle_hz = INPUT_CLOCK * SLOWDOWN_FACTOR,
+	.cycle_hz = INPUT_CLOCK;
 };
 
 static struct clock_source pit_clock_source = {
