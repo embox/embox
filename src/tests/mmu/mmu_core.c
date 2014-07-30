21cba35e84b21e9dd5a61fca3742282659b53f0c
Alex Kalmuk <alexkalmuk@gmail.com>
2014-07-30 14:57:26 +0000

    mmu: MMU test fixed

--- src/tests/mmu/mmu_core.c
+++ src/tests/mmu/mmu_core.c
@@ -24,6 +24,8 @@ static volatile int exception_flag;
 static char page[VMEM_PAGE_SIZE] __attribute__((aligned(VMEM_PAGE_SIZE)));
 //static char PAGE2[VMEM_PAGE_SIZE];
 
+static mmu_paddr_t paddr = (mmu_paddr_t) page;
+
 /*mapping to address, assuming there is no mem at here */
 #define BIGADDR 0xf0010000
 
@@ -31,20 +33,19 @@ static char page[VMEM_PAGE_SIZE] __attribute__((aligned(VMEM_PAGE_SIZE)));
  * used to read-n-check */
 #define UNIQ_VAL 0x12345678
 
-/* Big area able to cover text + data + stack*/
-#define REGION_SIZE 0xa000000
-
 /* MMU data access exception handler */
 static inline int dfault_handler(uint32_t trap_nr, void *data) {
 	vmem_page_set_flags((mmu_ctx_t) ctx, BIGADDR, VMEM_PAGE_WRITABLE);
 	exception_flag = 1;
+
+	test_assert_not_equal(*((volatile uint32_t *) paddr), UNIQ_VAL);
+
 	/* repeat instruction */
 	return 1;
 }
 
 TEST_CASE("Writing to read-only memory should cause exception."
 		 " Exception handler should allow write.") {
-	mmu_paddr_t paddr = (mmu_paddr_t) page;
 	mmu_vaddr_t vaddr = (mmu_vaddr_t) BIGADDR;
 	exception_flag = 0;
 
@@ -92,8 +93,9 @@ static ipl_t ipl;
 static int mmu_case_setup(void) {
 	ipl = ipl_save();
 
+	vmem_off();
 	test_assert_zero(vmem_create_context(&ctx));
-	test_assert_zero(vmem_map_on_itself(ctx, (void *) 0, REGION_SIZE, VMEM_PAGE_WRITABLE));
+	test_assert_zero(vmem_init_context(&ctx));
 	vmem_set_context(ctx);
 	vmem_on();
 
