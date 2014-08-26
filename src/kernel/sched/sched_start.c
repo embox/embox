b40c0e4fdba7d93688cd3862b7bff7c62be386f8
Anton Kozlov <drakon.mega@gmail.com>
2014-08-26 16:17:57 +0000

    sched: boot_thread file removed

--- src/kernel/sched/sched_start.c
+++ src/kernel/sched/sched_start.c
@@ -7,14 +7,38 @@
  */
 
 #include <assert.h>
+#include <kernel/panic.h>
 #include <embox/unit.h>
 #include <kernel/sched.h>
 #include <kernel/thread.h>
 
+#include <module/embox/kernel/stack.h>
+#include <kernel/task/kernel_task.h>
+#include <kernel/thread.h>
+#include <kernel/task.h>
+
+extern struct thread *idle_thread_create(void);
+
+#define STACK_SZ \
+	((size_t) OPTION_MODULE_GET(embox__kernel__stack, NUMBER, stack_size))
+
 EMBOX_UNIT_INIT(sched_start_init);
 
-extern struct thread * boot_thread_create(void);
-extern struct thread * idle_thread_create(void);
+static void *boot_stub(void *arg) {
+	panic("Entering boot_stub");
+}
+
+static struct thread *boot_thread_create(void) {
+	struct thread *bootstrap;
+	extern char _stack_top;
+
+	bootstrap = thread_init_stack(&_stack_top - STACK_SZ, STACK_SZ,
+			THREAD_PRIORITY_NORMAL, boot_stub, NULL);
+
+	task_set_main(task_kernel_task(), bootstrap);
+
+	return bootstrap;
+}
 
 static int sched_start_init(void) {
 	struct thread *current;
