c596086ee9768f37c6c8b12896be3644aae34796
Anton Kozlov <drakon.mega@gmail.com>
2014-08-27 15:26:28 +0000

    sched: stm32_vl builded with sched

--- platform/stm32vl/tests/blinking_led/blinking_led.c
+++ platform/stm32vl/tests/blinking_led/blinking_led.c
@@ -7,54 +7,35 @@
  */
 
 #include <unistd.h>
-#include <hal/reg.h>
-#include <kernel/time/timer.h>
 #include <drivers/gpio.h>
 #include <embox/test.h>
-
 
 EMBOX_TEST(blinking_led);
 
 #define LED_BLUE (1 << 8) /* port C, pin 8 */
 #define LED_GREEN (1 << 9) /* port C, pin 9 */
 
-static inline void led1_on(void) {
-	gpio_set_level(GPIO_C, LED_BLUE, 0x1);
+static inline void led1_level(unsigned int level) {
+	gpio_set_level(GPIO_C, LED_BLUE, level);
 }
-
-static inline void led1_off(void) {
-	gpio_set_level(GPIO_C, LED_BLUE, 0);
-}
-
-static void timer_hdn(sys_timer_t *tmd, void *param) {
-	static int state;
-	volatile int *count = (volatile int *) param;
-
-	state ^= 1;
-	*count -= 1;
-
-	if (state) {
-		led1_on();
-		return;
-	}
-
-	led1_off();
-}
-
 
 static int blinking_led(void) {
-	sys_timer_t tmr;
-	volatile int count = 10;
+	int count = 5;
+	int state;
 
 	gpio_settings(GPIO_C, LED_BLUE | LED_GREEN, GPIO_MODE_OUTPUT);
 	gpio_set_level(GPIO_C, LED_BLUE, 0x1);
 	gpio_set_level(GPIO_C, LED_GREEN, 0);
 
-	timer_init_msec(&tmr, TIMER_PERIODIC, 1000, timer_hdn, (void *) &count);
-
-	while (count) { } ;
-
-	timer_close(&tmr);
+	state = 0;
+	while (count) {
+		state = !state;
+		led1_level(state);
+		if (!state) {
+			count--;
+		}
+		sleep(1);
+	}
 	gpio_set_level(GPIO_C, LED_GREEN, 0);
 
 	return 0;
--- src/arch/generic/nocontext.c
+++ src/arch/generic/nocontext.c
@@ -6,8 +6,15 @@
  * @date    16.01.2014
  */
 
+#include <kernel/panic.h>
+
 #include <hal/context.h>
 
-void context_switch(struct context *prev, struct context *next) {
+void context_init(struct context *ctx, unsigned int flags,
+		void (*routine_fn)(void), void *sp) {
 
+}
+
+void context_switch(struct context *prev, struct context *next) {
+	panic("%s", __func__);
 }
