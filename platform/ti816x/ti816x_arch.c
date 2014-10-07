b81aa6564ad414895e03b1ccce15fa12bbc6e885
Anton Kozlov <drakon.mega@gmail.com>
2014-10-07 07:52:40 +0000

    embox.arch.arm.arch implementation deleted
    should be in platform packages

--- platform/ti816x/ti816x_arch.c
+++ platform/ti816x/ti816x_arch.c
@@ -2,21 +2,24 @@
  * @file
  * @brief
  *
- * @date 21.06.10
- * @author Anton Kozlov
+ * @author  Anton Kozlov
+ * @date    13.08.2014
  */
 
+#include <hal/reg.h>
 #include <hal/arch.h>
-#include <platform_shutdown.h>
 
 void arch_init(void) {
 
 }
 
 void arch_idle(void) {
+
 }
 
 void arch_shutdown(arch_shutdown_mode_t mode) {
-	platform_shutdown(mode);
+	if (mode == ARCH_SHUTDOWN_MODE_REBOOT) {
+		REG_STORE(0x481800a0, 0x2);
+	}
 	while (1);
 }
