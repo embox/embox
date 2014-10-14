50e6dcaeb87230dd150dfd801afc840e3ff8e173
Alex Kalmuk <alexkalmuk@gmail.com>
2014-10-14 09:38:12 +0000

    Add test on big ping forwarding, refactoring

--- scripts/continuous/net/forwarding/test_ping_forwarding.sh
+++ scripts/continuous/net/forwarding/test_ping_forwarding.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ue
+
 ROOT_DIR=.
 BASE_DIR=$ROOT_DIR
 
@@ -11,6 +13,9 @@ TAP_DEV=tap0
 
 EMBOX1_START_SCRIPT=$TEST_DIR/embox1_start_script
 EMBOX2_START_SCRIPT=$TEST_DIR/embox2_start_script
+
+QEMU1_PID_FILE=$BASE_DIR/qemu_bg1.pid
+QEMU2_PID_FILE=$BASE_DIR/qemu_bg2.pid
 
 QEMU1_ETH0=10.0.2.16
 QEMU1_ETH1=172.16.0.16
@@ -26,14 +31,11 @@ build_embox() {
 
 	echo "building embox with $embox_start_script"
 	cat $embox_start_script > $ROOT_DIR/conf/start_script.inc
-	make -j9
+	make -j9 > /dev/null 2>&1
 	mv $ROOT_DIR/build/base/bin/embox $2
 }
 
-test_ping_forwarding() {
-	qemu1_pid_file=$BASE_DIR/qemu_bg1.pid
-	qemu2_pid_file=$BASE_DIR/qemu_bg2.pid
-
+test_suite_setup() {
 	build_embox $EMBOX1_START_SCRIPT $EMBOX1_KERNEL
 	build_embox $EMBOX2_START_SCRIPT $EMBOX2_KERNEL
 
@@ -42,27 +44,44 @@ test_ping_forwarding() {
 		 "-net nic,vlan=0,model=e1000,macaddr=AA:BB:CC:DD:EE:12
 			-net tap,vlan=0,script=./scripts/qemu/start_script,downscript=./scripts/qemu/stop_script
 			-net nic,model=e1000,vlan=1,macaddr=AA:BB:CC:DD:EE:22 -net socket,vlan=1,listen=:12345" \
-		 $qemu1_pid_file
+		 $QEMU1_PID_FILE
 
 	AUTOQEMU_NICS="" AUTOQEMU_NICS_CONFIG="" KERNEL=$EMBOX2_KERNEL $CONT_RUN generic/qemu_bg \
 		 "-net nic,model=e1000,vlan=3,macaddr=AA:BB:CC:DD:EE:23
 			-net socket,vlan=3,connect=:12345" \
-		 $qemu2_pid_file
+		 $QEMU2_PID_FILE
 
 	sudo route add default gw $QEMU1_ETH0 dev $TAP_DEV
+}
 
-	ping -I $TAP_DEV -c 4 $QEMU2_ETH0
+test_suite_teardown() {
+	$CONT_RUN generic/qemu_bg_kill "" $QEMU1_PID_FILE
+	$CONT_RUN generic/qemu_bg_kill "" $QEMU2_PID_FILE
+	rm $EMBOX1_KERNEL $EMBOX2_KERNEL
+}
+
+test_retcode() {
 	if [ 0 -ne $? ]; then
 		test_code=1
 	fi
-
-	$CONT_RUN generic/qemu_bg_kill "" $qemu1_pid_file
-	$CONT_RUN generic/qemu_bg_kill "" $qemu2_pid_file
-	rm $EMBOX1_KERNEL $EMBOX2_KERNEL
-
-	return $test_code
 }
 
-test_ping_forwarding
+test_case_ping_forwarding() {
+	echo "Test case \"embox should forward ping\""
+	ping -I $TAP_DEV -c 4 $QEMU2_ETH0
+	test_retcode
+}
+
+test_case_big_ping_forwarding() {
+	echo "Test case \"embox should forward big ping\""
+	ping -I $TAP_DEV -c 4 -s 16384 $QEMU2_ETH0
+	test_retcode
+}
+
+test_suite_setup
+	test_case_ping_forwarding
+	test_case_big_ping_forwarding
+test_suite_teardown
+
 exit $test_code
 
