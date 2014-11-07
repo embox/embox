5492fc1d81edb66496b792982c06f26b59461099
Anton Kozlov <drakon.mega@gmail.com>
2014-11-07 10:58:34 +0000

    stm32f4_iocontrol led_driver cmd serialize state

--- platform/stm32f4_iocontrol/cmds/led_driver.c
+++ platform/stm32f4_iocontrol/cmds/led_driver.c
@@ -9,8 +9,24 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <stdio.h>
 
 #include "libleddrv.h"
+
+static int cldrv_serialize(void) {
+	unsigned char led_states[LEDDRV_LED_N];
+	int i;
+
+	leddrv_getstates(led_states);
+
+
+	printf("[");
+	for (i = 0; i < LEDDRV_LED_N - 1; ++i) {
+		printf("%d,", !!led_states[i]);
+	}
+	printf("%d]", led_states[LEDDRV_LED_N - 1]);
+	return 0;
+}
 
 int main(int argc, char *argv[]) {
 	const char *action = argv[1];
@@ -22,6 +38,8 @@ int main(int argc, char *argv[]) {
 			return leddrv_set(atoi(argv[2]));
 		} else if (0 == strcmp(action, "clr")) {
 			return leddrv_clr(atoi(argv[2]));
+		} else if (0 == strcmp(action, "serialize_states")) {
+			return cldrv_serialize();
 		}
 	}
 
