985a65d61143aa3222a56f8c499afb5883d3e077
Anton Bondarev <anton.bondarev2310@gmail.com>
2014-11-28 12:11:52 +0000

    Enable base address setting for mem_diag module

--- src/drivers/diag/mem_diag.c
+++ src/drivers/diag/mem_diag.c
@@ -13,7 +13,12 @@
 
 #define BUF_LEN OPTION_GET(NUMBER,buffer_len)
 
+#if OPTION_GET(NUMBER, buffer_addr)
+static char *const diag_buf = (char *const)OPTION_GET(NUMBER, buffer_addr);
+#else
 static char diag_buf[BUF_LEN];
+#endif
+
 static int diag_buf_head = 0;
 
 static void diag_mem_putc(const struct diag *diag, char ch) {
