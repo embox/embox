187085d81a12ca02e54ccf9bb508c869014055b2
Anton Kozlov <drakon.mega@gmail.com>
2014-12-05 14:09:02 +0000

    Andrey Golikov patch: libpcre extbld, [sg]etrlimit

--- src/compat/posix/include/sys/resource.h
+++ src/compat/posix/include/sys/resource.h
@@ -11,9 +11,21 @@
 
 #include <sys/types.h>
 
+#define RLIMIT_STACK 0
+#define RLIMIT_CORE  1
+
 #define PRIO_PROCESS 0
 #define PRIO_PGRP    1
 #define PRIO_USER    2
+
+typedef unsigned int rlim_t;
+struct rlimit {
+               rlim_t rlim_cur;  /* Soft limit */
+               rlim_t rlim_max;  /* Hard limit (ceiling for rlim_cur) */
+};
+
+extern int getrlimit(int resource, struct rlimit *rlp);
+extern int setrlimit(int resource, const struct rlimit *rlp);
 
 int getpriority(int which, id_t who);
 int setpriority(int which, id_t who, int value);
