ecbe38ca21a4cb191b175ff19c82861a46b5bfb6
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2015-01-25 23:30:19 +0000

    mybuild: (minor) Silence the output of `which`

--- mk/variables.mk
+++ mk/variables.mk
@@ -56,6 +56,6 @@ export MD5    := $(shell for i in md5 md5sum; do type $$i >/dev/null 2>&1 && ech
 export CPIO   := $(shell for i in gcpio cpio; do type $$i >/dev/null 2>&1 && echo $$i && break; done)
 export AWK    := $(shell for i in gawk awk nawk mawk; do type $$i >/dev/null 2>&1 && echo $$i && break; done)
 export TSORT  := tsort
-export TAC    := $(if $(shell which tac),tac,tail -rq)
+export TAC    := $(if $(shell which tac 2>/dev/null),tac,tail -rq)
 export SEQ    := seq -w
 export ECHO   := $(shell which echo) #force using feature-rich echo instead of shell builtin
