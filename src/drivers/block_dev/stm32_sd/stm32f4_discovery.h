64fb676be1f4ccc8a5916d41428e687334aecd66
Denis Deryugin <deryugin.denis@gmail.com>
2015-01-26 14:37:56 +0000

    stm32: Add block device interface for SD card
    Use SDIO interface from STM32F4xx_Ethernet_Example/Utilities.

---
+++ src/drivers/block_dev/stm32_sd/stm32f4_discovery.h
@@ -0,0 +1,114 @@
+/**
+  ******************************************************************************
+  * @file    stm32f4_discovery.h
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    28-October-2011
+  * @brief   This file contains definitions for STM32F4-Discovery Kit's Leds and
+  *          push-button hardware resources.
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; Portions COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************
+  */
+/**
+  ******************************************************************************
+  * <h2><center>&copy; Portions COPYRIGHT 2012 Embest Tech. Co., Ltd.</center></h2>
+  * @file    stm32f4_discovery.h
+  * @author  CMP Team
+  * @version V1.0.0
+  * @date    28-December-2012
+  * @brief   This file contains definitions for STM32F4-Discovery Kit's Leds and
+  *          push-button hardware resources.
+  *          Modified to support the STM32F4DISCOVERY, STM32F4DIS-BB, STM32F4DIS-CAM
+  *          and STM32F4DIS-LCD modules.
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, Embest SHALL NOT BE HELD LIABLE FOR ANY DIRECT, INDIRECT
+  * OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE CONTENT
+  * OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING INFORMATION
+  * CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32F4_DISCOVERY_H
+#define __STM32F4_DISCOVERY_H
+
+/* Includes ------------------------------------------------------------------*/
+ #include "stm32f4xx.h"
+
+/**
+  * @brief  SD FLASH SDIO Interface
+  */
+#define SD_DETECT_PIN                    GPIO_Pin_15                 /* PB.15 */
+#define SD_DETECT_GPIO_PORT              GPIOB                       /* GPIOB */
+#define SD_DETECT_GPIO_CLK               RCC_AHB1Periph_GPIOB
+
+#define SDIO_FIFO_ADDRESS                ((uint32_t)0x40012C80)
+/**
+  * @brief  SDIO Intialization Frequency (400KHz max)
+  */
+#define SDIO_INIT_CLK_DIV                ((uint8_t)0x76)
+/**
+  * @brief  SDIO Data Transfer Frequency (25MHz max)
+  */
+#define SDIO_TRANSFER_CLK_DIV            ((uint8_t)0x1)
+
+#define SD_SDIO_DMA                   DMA2
+#define SD_SDIO_DMA_CLK               RCC_AHB1Periph_DMA2
+
+#define SD_SDIO_DMA_STREAM3	          3
+//#define SD_SDIO_DMA_STREAM6           6
+
+#ifdef SD_SDIO_DMA_STREAM3
+ #define SD_SDIO_DMA_STREAM            DMA2_Stream3
+ #define SD_SDIO_DMA_CHANNEL           DMA_Channel_4
+ #define SD_SDIO_DMA_FLAG_FEIF         DMA_FLAG_FEIF3
+ #define SD_SDIO_DMA_FLAG_DMEIF        DMA_FLAG_DMEIF3
+ #define SD_SDIO_DMA_FLAG_TEIF         DMA_FLAG_TEIF3
+ #define SD_SDIO_DMA_FLAG_HTIF         DMA_FLAG_HTIF3
+ #define SD_SDIO_DMA_FLAG_TCIF         DMA_FLAG_TCIF3
+ #define SD_SDIO_DMA_IRQn              DMA2_Stream3_IRQn
+ #define SD_SDIO_DMA_IRQHANDLER        DMA2_Stream3_IRQHandler
+#elif defined SD_SDIO_DMA_STREAM6
+ #define SD_SDIO_DMA_STREAM            DMA2_Stream6
+ #define SD_SDIO_DMA_CHANNEL           DMA_Channel_4
+ #define SD_SDIO_DMA_FLAG_FEIF         DMA_FLAG_FEIF6
+ #define SD_SDIO_DMA_FLAG_DMEIF        DMA_FLAG_DMEIF6
+ #define SD_SDIO_DMA_FLAG_TEIF         DMA_FLAG_TEIF6
+ #define SD_SDIO_DMA_FLAG_HTIF         DMA_FLAG_HTIF6
+ #define SD_SDIO_DMA_FLAG_TCIF         DMA_FLAG_TCIF6
+ #define SD_SDIO_DMA_IRQn              DMA2_Stream6_IRQn
+ #define SD_SDIO_DMA_IRQHANDLER        DMA2_Stream6_IRQHandler
+#endif /* SD_SDIO_DMA_STREAM3 */
+
+/** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_Exported_Macros
+  * @{
+  */
+/**
+  * @}
+  */
+
+
+/** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_Exported_Functions
+  * @{
+  */
+void SD_LowLevel_DeInit(void);
+void SD_LowLevel_Init(void);
+void SD_LowLevel_DMA_TxConfig(uint32_t *BufferSRC, uint32_t BufferSize);
+void SD_LowLevel_DMA_RxConfig(uint32_t *BufferDST, uint32_t BufferSize);
+
+#endif /* __STM32F4_DISCOVERY_H */
+
+/*********** Portions COPYRIGHT 2012 Embest Tech. Co., Ltd.*****END OF FILE****/
---
+++ src/drivers/block_dev/stm32_sd/stm32f4_sd.c
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * @brief SD-Card driver for STM32F4-Discovery using SDIO.
+ *
+ * @author  Denis Deryugin
+ * @date    26 Jan 2015
+ */
+
+#include <embox/block_dev.h>
+#include "stm32f4_discovery_sdio_sd.h"
+
+static int stm32f4_sd_init(void *arg);
+static int stm32f4_sd_ioctl(struct block_dev *bdev, int cmd, void *buf, size_t size);
+static int stm32f4_sd_read(struct block_dev *bdev, char *buf, size_t count, blkno_t blkno);
+static int stm32f4_sd_write(struct block_dev *bdev, char *buf, size_t count, blkno_t blkno);
+
+block_dev_driver_t stm32f4_sd_driver = {
+	.name = "stm32f4 sd card",
+	.ioctl = stm32f4_sd_ioctl,
+	.read = stm32f4_sd_read,
+	.write = stm32f4_sd_write,
+};
+
+EMBOX_BLOCK_DEV("stm32f4 sd card", &stm32f4_sd_driver, stm32f4_sd_init);
+
+static int stm32f4_sd_init(void *arg) {
+	return -SD_Init();
+}
+
+static int stm32f4_sd_ioctl(struct block_dev *bdev, int cmd, void *buf, size_t size) {
+	return 0;
+}
+
+static int stm32f4_sd_read(struct block_dev *bdev, char *buf, size_t count, blkno_t blkno) {
+	return -SD_ReadBlock((uint8_t*) buf, blkno, SECTOR_SIZE);
+}
+
+static int stm32f4_sd_write(struct block_dev *bdev, char *buf, size_t count, blkno_t blkno) {
+	return -SD_WriteBlock((uint8_t*) buf, blkno, SECTOR_SIZE);
+}
+
