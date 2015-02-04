bd1b572baabe092b5f92343ec523ade0ad9363e6
Anton Kozlov <drakon.mega@gmail.com>
2015-02-04 23:41:15 +0000

    output heap section can be defined in lds

--- src/include/asm-generic/embox.lds.h
+++ src/include/asm-generic/embox.lds.h
@@ -22,12 +22,13 @@
 
 #define MEMORY_REGION(name) \
 	name : ORIGIN = LDS_REGION_BASE_##name, LENGTH = LDS_REGION_SIZE_##name
-
 #define HAS_MEMORY_REGION(name) \
 	defined(LDS_REGION_BASE_##name)
 
 #define SECTION_REGION(section) \
 	> LDS_SECTION_VMA_##section AT> LDS_SECTION_LMA_##section
+#define HAS_SECTION_REGION(section) \
+	defined(LDS_SECTION_VMA_##section)
 
 #define DEFAULT_TEXT_ALIGNMENT 16
 #define DEFAULT_DATA_ALIGNMENT 32
