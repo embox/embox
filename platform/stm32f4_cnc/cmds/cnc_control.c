b66086a26db28ca8f2945938e63c55e983bea3be
Anton Kozlov <drakon.mega@gmail.com>
2015-02-14 22:56:29 +0000

    cnc web clients notified when control started

--- platform/stm32f4_cnc/cmds/cnc_control.c
+++ platform/stm32f4_cnc/cmds/cnc_control.c
@@ -3,11 +3,32 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
+#include "cnc_ipc.h"
+
+static int cnc_g_ipc_out_fd = -1;
+static void cnc_ctrl_ipc_init(void) {
+	const char *cnc_out = getenv(CNCIPC_OUT);
+	if (cnc_out) {
+		cnc_g_ipc_out_fd = atoi(cnc_out);
+	}
+}
+
+static void cnc_status_update(const char *str) {
+	if (cnc_g_ipc_out_fd != -1) {
+		write(cnc_g_ipc_out_fd, str, strlen(str));
+	}
+}
 
 int main(int argc, char *argv[]) {
 	int cnc_program_file;
 	char line_buff[20];
+
+	cnc_ctrl_ipc_init();
+
+	cnc_status_update("cnc_control runned!");
 
 	if (argc < 2) {
 		return -EINVAL;
--- platform/stm32f4_cnc/cmds/live_status.c
+++ platform/stm32f4_cnc/cmds/live_status.c
@@ -1,24 +1,67 @@
 
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <poll.h>
 
-#include <stm32f4xx_gpio.h>
+#include "cnc_ipc.h"
+
+#define SSE_HEADER "data: \""
+#define SSE_HEADER_LEN strlen(SSE_HEADER)
+#define SSE_FOOTER "\"\n\n"
+#define SSE_FOOTER_LEN strlen(SSE_FOOTER)
+#define SSE_SERVICE_LEN (SSE_HEADER_LEN + SSE_FOOTER_LEN)
+
+static int cnc_ipc_get_input_fd(void) {
+	char *cnc_in;
+	if (!(cnc_in = getenv(CNCIPC_IN))) {
+		printf("Error: IPC not found");
+		return 0;
+	}
+
+	return atoi(cnc_in);
+}
 
 int main(int argc, char *argv[]) {
+	const int sock = STDOUT_FILENO;
+	const int ipc_in = cnc_ipc_get_input_fd();
 	char buf[128];
-	int count = 0;
-	int err;
+	struct pollfd fds[2];
+	bool sock_conn, ipc_has_data;
+	int buf_written;
 
-	err = printf("HTTP/1.1 200 OK\r\n"
+	fds[0].fd = ipc_in;
+	fds[0].events = POLLIN | POLLERR;
+	fds[1].fd = sock;
+	fds[1].events = POLLERR;
+
+
+	buf_written = snprintf(buf, sizeof(buf),
+		"HTTP/1.1 200 OK\r\n"
 		"Content-Type: text/event-stream\r\n"
 		"Cache-Control: no-cache\r\n"
 		"Connection: keep-alive\r\n"
 		"\r\n");
 
-	while (err > 0) {
-		int nwrite = snprintf(buf, sizeof(buf), "data: \"(%d) live!\"\n\n", ++count);
-		err = write(STDOUT_FILENO, buf, nwrite);
-		sleep(1);
+	ipc_has_data = false;
+	sock_conn = 0 < write(sock, buf, buf_written);
+	while (sock_conn) {
+		if (poll(fds, 2, -1) > 0) {
+			sock_conn = !(fds[1].revents & POLLERR);
+			ipc_has_data = fds[0].revents & POLLIN;
+		}
+
+		if (sock_conn && ipc_has_data) {
+			char *buf_data = buf + SSE_HEADER_LEN;
+			strcpy(buf, SSE_HEADER);
+			buf_written = read(ipc_in, buf_data, sizeof(buf) - SSE_SERVICE_LEN);
+			assert(buf_written > 0);
+			strcpy(buf_data + buf_written, SSE_FOOTER);
+			sock_conn = 0 < write(sock, buf, buf_written + SSE_SERVICE_LEN);
+		}
 	}
 
 	return 0;
