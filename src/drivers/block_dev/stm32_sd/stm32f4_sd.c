ab123488c845e432bb1766181cbfdcb5ea2b6d85
Denis Deryugin <deryugin.denis@gmail.com>
2015-02-20 11:49:40 +0000

    stm32: sdcard: Remove mutex from read and write operations

--- src/drivers/block_dev/stm32_sd/stm32f4_sd.c
+++ src/drivers/block_dev/stm32_sd/stm32f4_sd.c
@@ -11,7 +11,6 @@
 #include <embox/block_dev.h>
 #include "stm32f4_discovery_sdio_sd.h"
 #include <kernel/time/ktime.h>
-#include <kernel/thread/sync/mutex.h>
 
 static int stm32f4_sd_init(void *arg);
 static int stm32f4_sd_ioctl(struct block_dev *bdev, int cmd, void *buf, size_t size);
@@ -29,13 +28,10 @@ block_dev_driver_t stm32f4_sd_driver = {
 
 EMBOX_BLOCK_DEV(STM32F4_SD_DEVNAME, &stm32f4_sd_driver, stm32f4_sd_init);
 
-static struct mutex stm32f4_sd_mutex;
-
 static int stm32f4_sd_init(void *arg) {
 	block_dev_t *bdev;
 	int res;
 	if (block_dev_lookup(STM32F4_SD_DEVNAME)) {
-		mutex_init(&stm32f4_sd_mutex);
 		res = SD_Init();
 		if (res != SD_OK) {
 			return -res;
@@ -68,19 +64,15 @@ static int stm32f4_sd_ioctl(struct block_dev *bdev, int cmd, void *buf, size_t s
 
 static int stm32f4_sd_read(struct block_dev *bdev, char *buf, size_t count, blkno_t blkno) {
 	int res;
-	mutex_lock(&stm32f4_sd_mutex);
 	while (SD_GetTransferState() != SD_TRANSFER_OK);
 	res = SD_ReadBlock((uint8_t*) buf, blkno * SECTOR_SIZE, SECTOR_SIZE) ? -1 : SECTOR_SIZE;
-	while (SD_GetTransferState() != SD_TRANSFER_OK);
-	mutex_unlock(&stm32f4_sd_mutex);
 	return res;
 }
 
 static int stm32f4_sd_write(struct block_dev *bdev, char *buf, size_t count, blkno_t blkno) {
 	int res;
-	mutex_lock(&stm32f4_sd_mutex);
+	while (SD_GetTransferState() != SD_TRANSFER_OK);
 	res = SD_WriteBlock((uint8_t*) buf, blkno * SECTOR_SIZE, SECTOR_SIZE) ? -1 : SECTOR_SIZE;
-	mutex_unlock(&stm32f4_sd_mutex);
 	return res;
 }
 
