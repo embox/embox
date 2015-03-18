31d5145c9f686aa9c6c51a06caf84d02c71cba9a
Anton Kozlov <drakon.mega@gmail.com>
2015-03-18 18:21:05 +0000

    Make openlibm disable switch (via abstract module)

--- src/compat/libc/include/math.h
+++ src/compat/libc/include/math.h
@@ -9,22 +9,6 @@
 #ifndef MATH_H_
 #define MATH_H_
 
-#include <sys/cdefs.h>
-
-#define __BSD_VISIBLE 1
-
 #include <module/third_party/lib/OpenLibm.h>
-
-static inline int finite(double x) {
-	return isfinite(x);
-}
-
-static inline int finitef(float x) {
-	return isfinite(x);
-}
-
-static inline int finitel(long double x) {
-	return isfinite(x);
-}
 
 #endif /* MATH_H_ */
