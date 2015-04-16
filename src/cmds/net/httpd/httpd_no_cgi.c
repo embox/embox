20fdb539e4c9762a4fae899bf2accdbad76ea444
Anton Kozlov <drakon.mega@gmail.com>
2015-04-16 23:26:02 +0300

    Make httpd cgi optional

--- src/cmds/net/httpd/httpd_no_cgi.c
+++ src/cmds/net/httpd/httpd_no_cgi.c
@@ -6,30 +6,12 @@
  * @date    15.04.2015
  */
 
-#include <errno.h>
-#include <stdio.h>
-
 #include "httpd.h"
 
-int httpd_send_response_cgi(const struct client_info *cinfo, const struct http_req *hreq) {
-	FILE *skf;
-
-	skf = fdopen(cinfo->ci_sock, "rw");
-	if (!skf) {
-		HTTPD_ERROR("can't allocate FILE for socket\n");
-		return -ENOMEM;
-	}
-
-	fprintf(skf,
-		"HTTP/1.1 %d %s\r\n"
-		"Content-Type: %s\r\n"
-		"Connection: close\r\n"
-		"\r\n"
-		"%s", 200, "OK", "text/plain",
-		"Sorry, CGI support is disabled");
-
-	fclose(skf);
-
+pid_t httpd_try_respond_script(const struct client_info *cinfo, const struct http_req *hreq) {
 	return 0;
 }
 
+pid_t httpd_try_respond_cmd(const struct client_info *cinfo, const struct http_req *hreq) {
+	return 0;
+}
