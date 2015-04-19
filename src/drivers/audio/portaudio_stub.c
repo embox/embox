3b730ac0895f3409c1460f666da9bcf959b18ce8
Anton Kozlov <drakon.mega@gmail.com>
2015-04-19 13:25:15 +0300

    Fix portaudio_stub: openstream ret is valid stream
    Also, make debug output more descriptive.

--- src/drivers/audio/portaudio_stub.c
+++ src/drivers/audio/portaudio_stub.c
@@ -18,6 +18,8 @@
 		printk("%s: " fmt "\n", __func__, ##__VA_ARGS__); \
 	} while (0)
 
+#define PA_STUB_VALID_STREAM ((void *) 0xff0)
+
 PaError Pa_Initialize(void) {
 	D("");
 	return paNoError;
@@ -33,14 +35,14 @@ PaDeviceIndex Pa_GetDeviceCount(void) { return 1; }
 PaDeviceIndex Pa_GetDefaultOutputDevice(void) { return 0; }
 
 const char * Pa_GetErrorText(PaError errorCode) {
-	D("%d", errorCode);
+	D("errcode=%d", errorCode);
 	return "Pa_GetErrorText not implemented";
 }
 
 const PaDeviceInfo * Pa_GetDeviceInfo(PaDeviceIndex device) {
 	static const PaDeviceInfo info = {
 		.structVersion = 1,
-		.name = "stm32f4_audio",
+		.name = "portaudio_stub",
 		.hostApi = 0,
 		.maxInputChannels = 1,
 		.maxOutputChannels = 1,
@@ -52,12 +54,12 @@ const PaDeviceInfo * Pa_GetDeviceInfo(PaDeviceIndex device) {
 	};
 	const PaDeviceInfo *pa_info = device == 0 ? &info : NULL;
 
-	D("%d = %p", device, pa_info);
+	D("index=%d info=%p", device, pa_info);
 	return pa_info;
 }
 
 const PaHostApiInfo * Pa_GetHostApiInfo(PaHostApiIndex hostApi) {
-	D("%d = NULL", hostApi);
+	D("api_index=%d info=%p", hostApi, NULL);
 	return NULL;
 }
 
@@ -70,7 +72,7 @@ const PaStreamInfo * Pa_GetStreamInfo(PaStream *stream) {
 	};
 	PaStreamInfo *pa_info = stream != NULL ? &info : NULL;
 
-	D("%p = %p", stream, pa_info);
+	D("stream=%p info=%p", stream, pa_info);
 	return pa_info;
 }
 
@@ -81,38 +83,34 @@ PaError Pa_OpenStream(PaStream** stream,
 		PaStreamFlags streamFlags, PaStreamCallback *streamCallback,
 		void *userData) {
 	assert(stream != NULL);
-	assert(inputParameters == NULL);
-	assert(outputParameters != NULL
-			&& outputParameters->device == 0
-			&& outputParameters->channelCount == 1
-			&& (outputParameters->sampleFormat == paInt16)
-			&& outputParameters->hostApiSpecificStreamInfo == 0);
 	assert(streamFlags == paNoFlag || streamFlags == paClipOff);
 	assert(streamCallback != NULL);
 
-	D("%p %p %p %f %lu %lu %p %p", stream, inputParameters, outputParameters,
-			sampleRate, framesPerBuffer, streamFlags, streamCallback, userData);
+	D("stream=%p in_param=%p out_param=%p samplerate=%f"
+			" frames/buffer=%lu flags=%lu callback=%p data=%p",
+			stream, inputParameters, outputParameters, sampleRate,
+			framesPerBuffer, streamFlags, streamCallback, userData);
 
-	*stream = NULL;
+	*stream = PA_STUB_VALID_STREAM;
 	return paNoError;
 }
 
 PaError Pa_CloseStream(PaStream *stream) {
-	D("%p", stream);
+	D("stream=%p", stream);
 	return paNoError;
 }
 
 PaError Pa_StartStream(PaStream *stream) {
-	D("%p", stream);
+	D("stream=%p", stream);
 	return paNoError;
 }
 
 PaError Pa_StopStream(PaStream *stream) {
-	D("%p", stream);
+	D("stream=%p", stream);
 	return paNoError;
 }
 
 void Pa_Sleep(long msec) {
-	D("%ld", msec);
+	D("msec=%ld", msec);
 	usleep(msec * 1000);
 }
