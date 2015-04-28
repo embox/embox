6f977ebbb27db1d6c23a5bd29835693ad66074c9
Alex Kalmuk <alexkalmuk@gmail.com>
2015-04-28 12:53:29 +0400

    usb-net: Add functions to set device configuration
    Every usb device may have many configurations.
    So we need to choose appropriate configuration before we start doing
    something with that one.

--- src/drivers/usb/class/usb_class_cdc.c
+++ src/drivers/usb/class/usb_class_cdc.c
@@ -1,6 +1,8 @@
 /**
  * @file
  * @brief
+ *    For more details about configurations and interfaces see
+ *    usb specification 1.1 chapter 9.
  *
  * @author  Alex Kalmuk
  * @date    05.04.2014
@@ -22,12 +24,22 @@ POOL_DEF(cdc_classes, struct usb_class_cdc, USB_CDC_MAX_DEVS);
 struct usb_desc_configuration config_desc;
 struct usb_desc_interface interface_desc;
 
+/* FIXME store all static data in pool */
+
+/* Storage for device configuration*/
+static char cdc_conf[16];
+
 #define FUNCTIONAL_DESCS_TOTAL_SIZE    (3 * 5 + 4)
 #define CONFIG_DESC_OFFSET             0
 #define INTERFACE_DESC_OFFSET          (sizeof (struct usb_desc_configuration))
 #define ENDPOINT_DESC_OFFSET           \
 	(INTERFACE_DESC_OFFSET + sizeof (struct usb_desc_interface) + \
 			FUNCTIONAL_DESCS_TOTAL_SIZE)
+
+static void cdc_get_conf_len(struct usb_request *req, void *arg);
+static void cdc_get_conf_by_idx(struct usb_dev *dev, struct usb_class_cdc *cdc,
+		size_t idx, size_t len, usb_request_notify_hnd_t cb);
+static void cdc_get_conf_content(struct usb_request *req, void *arg);
 
 static void usb_class_cdc_get_conf_hnd(struct usb_request *req, void *arg) {
 	struct usb_dev *dev = req->endp->dev;
@@ -47,26 +59,56 @@ static void usb_class_cdc_get_conf_hnd(struct usb_request *req, void *arg) {
 	printk("%s: out\n", __func__);
 }
 
-static int usb_class_cdc_get_conf(struct usb_class *cls, struct usb_dev *dev) {
-	size_t conf_size;
-	struct usb_class_cdc *cdc = usb2cdcdata(dev);
+static void cdc_get_conf_len(struct usb_request *req, void *arg) {
+	struct usb_dev *dev = req->endp->dev;
 
-	conf_size = sizeof(struct usb_desc_configuration)
-					+ sizeof(struct usb_desc_interface)
-					+ FUNCTIONAL_DESCS_TOTAL_SIZE
-					+ (dev->endp_n - 1) * sizeof(struct usb_desc_endpoint);
-
-	cdc->getconf = sysmalloc(conf_size);
-
-	usb_endp_control(dev->endpoints[0], cls->get_conf_hnd, NULL,
+	usb_endp_control(dev->endpoints[0], cdc_get_conf_content, NULL,
 		USB_DEV_REQ_TYPE_RD
 			| USB_DEV_REQ_TYPE_STD
 			| USB_DEV_REQ_TYPE_DEV,
 		USB_DEV_REQ_GET_DESC,
-		USB_DESC_TYPE_CONFIG << 8,
+		(USB_DESC_TYPE_CONFIG << 8) | 1,
 		dev->c_config,
-		conf_size,
+		9,
+		cdc_conf);
+}
+
+static void cdc_get_conf_by_idx(struct usb_dev *dev, struct usb_class_cdc *cdc,
+		size_t idx, size_t len, usb_request_notify_hnd_t cb) {
+	usb_endp_control(dev->endpoints[0], usb_class_cdc_get_conf_hnd, NULL,
+		USB_DEV_REQ_TYPE_RD
+			| USB_DEV_REQ_TYPE_STD
+			| USB_DEV_REQ_TYPE_DEV,
+		USB_DEV_REQ_GET_DESC,
+		idx,
+		dev->c_config,
+		len,
 		cdc->getconf);
+}
+
+static void cdc_get_conf_content(struct usb_request *req, void *arg) {
+	struct usb_dev *dev = req->endp->dev;
+	struct usb_class_cdc *cdc = usb2cdcdata(dev);
+	struct usb_desc_configuration *c;
+
+	c = (struct usb_desc_configuration*) cdc_conf;
+
+	cdc->getconf = sysmalloc(c->w_total_length);
+
+	cdc_get_conf_by_idx(dev, cdc, (USB_DESC_TYPE_CONFIG << 8) | 1,
+			c->w_total_length, usb_class_cdc_get_conf_hnd);
+}
+
+static int usb_class_cdc_get_conf(struct usb_class *cls, struct usb_dev *dev) {
+	usb_endp_control(dev->endpoints[0], cdc_get_conf_len, NULL,
+		USB_DEV_REQ_TYPE_WR
+			| USB_DEV_REQ_TYPE_STD
+			| USB_DEV_REQ_TYPE_DEV,
+		USB_DEV_REQ_SET_CONF,
+		1,
+		0,
+		0,
+		NULL);
 
 	return 0;
 }
