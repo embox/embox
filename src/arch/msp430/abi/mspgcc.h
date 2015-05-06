09479c08af0c358c9d1ceab729531e3c1a5fc805
Anton Kozlov <drakon.mega@gmail.com>
2015-05-06 18:18:22 +0300

    msp430: Introduce ABI abstraction

---
+++ src/arch/msp430/abi/mspgcc.h
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @brief msp430 mspgcc abi
+ *
+ * @author  Anton Kozlov
+ * @date    06.05.2015
+ */
+
+#ifndef MSP430_EABI_H_
+#define MSP430_EABI_H_
+
+#define MSP430_SARG1 r15
+#define MSP430_SARG2 r14
+#define MSP430_SARG3 r13
+#define MSP430_SARG4 r12
+
+#define MSP430_SRET  r15
+
+#endif /* MSP430_EABI_H_ */
+
