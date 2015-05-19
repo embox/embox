c413b2d44d27ec0b273416685fc63c92d0e1c75c
Alex Kalmuk <alexkalmuk@gmail.com>
2015-05-19 23:23:14 +0300

    stm32f3discovery-port: Build stm32f3 libraries
    - HAL library
    - BSP (gyro, accelereometer, ...) library

---
+++ third-party/bsp/st_f3/arch.c
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Kozlov
+ * @date    30.10.2014
+ */
+
+#include <assert.h>
+#include <stdint.h>
+#include <system_stm32f3xx.h>
+#include <framework/mod/options.h>
+#include <module/embox/arch/system.h>
+#include <hal/arch.h>
+
+void arch_init(void) {
+	static_assert(OPTION_MODULE_GET(embox__arch__system, NUMBER, core_freq) == 144000000);
+	SystemInit();
+}
+
+void arch_idle(void) {
+
+}
+
+void arch_shutdown(arch_shutdown_mode_t mode) {
+	while (1);
+}
