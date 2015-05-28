afb8cd1145a81df42b22e5b574c0bd3e2ce2510f
Deryugin <deryugin.denis@gmail.com>
2015-05-28 15:57:57 +0300

    dvfs: Implement chdir POSIX call

--- src/compat/posix/fs/dvfs/chdir.c
+++ src/compat/posix/fs/dvfs/chdir.c
@@ -6,6 +6,25 @@
  * @date 3 Apr 2015
  */
 
+#include <errno.h>
+
+#include <fs/dvfs.h>
+#include <kernel/task/resource/vfs.h>
+
+/**
+ * @brief POSIX-compatible function changing process working directory
+ *
+ * @param path Path to new working directory
+ *
+ * @return On success, 0 returned, otherwise -1 returned and ERRNO is set
+ *         appropriately.
+ */
 int chdir(const char *path) {
-	return 0;
+	struct lookup l;
+	dvfs_lookup(path, &l);
+	if (l.item != NULL) {
+		task_fs()->pwd = l.item;
+		return 0;
+	} else
+		return SET_ERRNO(ENOENT);
 }
