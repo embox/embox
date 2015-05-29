3f6ac38e73ac4111572890f74c4775006eb9a488
Deryugin <deryugin.denis@gmail.com>
2015-05-29 16:20:49 +0300

    libdl: Remove old code from header
    We have VCS for this purpose after all

--- src/include/lib/libdl.h
+++ src/include/lib/libdl.h
@@ -34,43 +34,4 @@ extern int dl_proceed_all_relocations(Elf32_Objlist *list, dl_data *data);
 extern int dl_relocate_rel(dl_data *data, Elf32_Obj *obj, Elf32_Shdr *relsh,
 		Elf32_Rel *rel, Elf32_Sym *sym, const char *sym_names);
 
-#if 0
-typedef struct {
-	Elf32_Obj *obj;
-	Elf32_Sym *sym;
-} dl_globsym;
-
-typedef struct dl_element_struct {
-	Elf32_Obj                 *obj;
-	FILE                      *fd;
-	struct dl_element_struct  *next;
-} dl_element;
-
-typedef struct {
-	dl_element *element_list;
-
-	dl_globsym *globsym_table;
-	int globsym_count;
-} dl_data;
-
-
-extern int elf_relocate(FILE *fd, Elf32_Obj *obj, dl_data *data);
-
-extern void dl_initilize_data(dl_data *data);
-extern int dl_add_file(dl_data *data, const char *filename);
-extern void dl_finilize_data(dl_data *data);
-extern int dl_fetch_global_symbols(dl_data *data);
-
-extern int dl_load_element(dl_element *element, const char *filename);
-extern void dl_finilize_element(dl_element *element);
-
-extern int dl_fetch_global_symbols(dl_data *data);
-extern dl_globsym *dl_find_global_symbol(dl_data *data, const char *name);
-
-extern Elf32_Addr dl_get_global_symbol_addr(dl_globsym *globsym);
-
-extern int dl_add_element(dl_element *element, FILE *fd, Elf32_Obj *obj);
-
-#endif
-
 #endif /* LIB_DL_H_ */
