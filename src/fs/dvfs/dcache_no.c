6566e0278082294b97e7cd12eb4b4479abfee100
Denis Deryugin <deryugin.denis@gmail.com>
2015-06-09 17:23:12 +0300

    vfs: Implement stubs for trirvial cache

--- src/fs/dvfs/dcache_no.c
+++ src/fs/dvfs/dcache_no.c
@@ -6,4 +6,20 @@
  * @date 2015-06-09
  */
 
+#include <fs/dvfs.h>
 
+struct dentry *dvfs_cache_lookup(const char *path, struct dentry *base) {
+	return NULL;
+}
+
+struct dentry *dvfs_cache_get(char *path) {
+	return NULL;
+}
+
+int dvfs_cache_del(struct dentry *dentry) {
+	return 0;
+}
+
+int dvfs_cache_add(struct dentry *dentry) {
+	return 0;
+}
