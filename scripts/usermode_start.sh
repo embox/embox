8d96cef4e66aac572ad45cc81be4d3f164d3e77f
Anton Kozlov <drakon.mega@gmail.com>
2015-06-19 16:15:05 +0300

    Make usermode tap adapter optional

--- scripts/usermode_start.sh
+++ scripts/usermode_start.sh
@@ -5,7 +5,7 @@ ROOT=$(dirname $0)/../.
 
 EMBOX=${ROOT}/build/base/bin/embox
 
-TAP=tap77
+[ -z "$TAP" ] && TAP=tap77
 
 rm_tap() {
 	$SUDO ip tuntap del dev $TAP mode tap
@@ -19,6 +19,7 @@ fi
 
 $SUDO ip tuntap add dev $TAP mode tap
 $SUDO $ROOT/scripts/qemu/start_script $TAP
+export EMBOX_USERMODE_TAP_NAME=$TAP
 
 if [ $USERMODE_START_OUTPUT ]; then
 	$EMBOX $TAP >$USERMODE_START_OUTPUT 2>$USERMODE_START_OUTPUT
