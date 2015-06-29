72f3f8b0fd29ed4e959c27dd5484fc8cf52d6fbc
Vita Loginova <vita.loginova@gmail.com>
2015-06-29 20:48:57 +0300

    logger: Comment log.h and logging.h

--- src/util/log.h
+++ src/util/log.h
@@ -1,6 +1,7 @@
 /**
  * @file
- * @brief
+ * @brief Describes implicit logger for modules. To use implicit logger
+ * add log_level option in a mybuild module description of the specific module.
  *
  * @date 18.06.15
  * @author Vita Loginova
@@ -12,19 +13,39 @@
 #include <framework/mod/self.h>
 #include <util/logging.h>
 
+/**
+ * Logger structure binding a specific module and logging params.
+ */
 struct logger {
 	const struct mod *mod;
 	struct logging logging;
 };
 
+/**
+ * Declares logger of the module from which macro is called.
+ */
 #define LOG_DECLARE_LOGGER() \
 	extern struct logger mod_logger __attribute__ ((weak))
 
+/**
+ * Logs a formatted message in a way specified by the module logger. A
+ * resulting message has a following format:
+ * "level: function: message"
+ *
+ * @param level Message logging level, see #log_level
+ * @param fmt   printf-like format of the message
+ */
 #define log_logp(level, fmt, ...) \
 	if (!&mod_logger) \
 		logging_raw(&mod_logger.logging, level, "%s: %s: " fmt "\n", \
 					log_levels[level-1], __func__, ## __VA_ARGS__)
 
+/**
+ * Logs a raw message in a way specified by the module logger.
+ *
+ * @param level Message logging level, see #log_level
+ * @param fmt   printf-like format of the message
+ */
 #define log_raw(level, fmt, ...) \
 	if (!&mod_logger) \
 		logging_raw(&mod_logger.logging, level, fmt, ## __VA_ARGS__)
--- src/util/logging.h
+++ src/util/logging.h
@@ -9,6 +9,9 @@
 #ifndef UTIL_LOGGING_H_
 #define UTIL_LOGGING_H_
 
+/**
+ * Logging level, decreasing by fatality.
+ */
 enum log_level{
 	LOG_NONE=0,
 	LOG_ERROR,
@@ -17,12 +20,27 @@ enum log_level{
 	LOG_DEBUG
 };
 
+/**
+ * Logging params
+ */
 struct logging {
-	int level;
+	int level; /**< Filtering log level */
 };
 
+
+/**
+ * Array mapping a log_level on its text view.
+ * log_level - 1 -> "level"
+ */
 extern char *log_levels[];
 
+/**
+ * Logs message in a way specified by @p logging.
+ *
+ * @param logging Logging params
+ * @param level   Level of the message
+ * @param fmt     printf-like format of the message
+ */
 extern void logging_raw(struct logging *logging, int level,
 	const char* fmt, ...);
 
