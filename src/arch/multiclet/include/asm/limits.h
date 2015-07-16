c5a9a588f2194183ddcaa16b4fd20a23c30fd235
Anton Bondarev <anton.bondarev2310@gmail.com>
2015-07-16 19:39:25 +0300

    Add limits.h for multiclet arch

---
+++ src/arch/multiclet/include/asm/limits.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ *
+ * @data 16 jule 2015
+ * @author: Anton Bondarev
+ */
+
+#ifndef SRC_ARCH_MULTICLET_INCLUDE_ASM_LIMITS_H_
+#define SRC_ARCH_MULTICLET_INCLUDE_ASM_LIMITS_H_
+
+#include <asm-generic/limits32.h>
+
+#endif /* SRC_ARCH_MULTICLET_INCLUDE_ASM_LIMITS_H_ */
