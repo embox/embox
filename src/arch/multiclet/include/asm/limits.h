8012be32cc9bf8a20fb5f5f1f72f47a1e0688168
Anton Kozlov <drakon.mega@gmail.com>
2015-07-16 23:48:37 +0300

    Change multiclet limits to set long long = long

--- src/arch/multiclet/include/asm/limits.h
+++ src/arch/multiclet/include/asm/limits.h
@@ -10,4 +10,13 @@
 
 #include <asm-generic/limits32.h>
 
+#undef LLONG_MAX
+#define LLONG_MAX LONG_MAX
+
+#undef LLONG_MIN
+#define LLONG_MIN LONG_MIN
+
+#undef ULLONG_MAX
+#define ULLONG_MAX ULONG_MAX
+
 #endif /* SRC_ARCH_MULTICLET_INCLUDE_ASM_LIMITS_H_ */
