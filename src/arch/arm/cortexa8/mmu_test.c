597f412c5ada58081ce40745c1a5ccc503020265
Denis Deryugin <deryugin.denis@gmail.com>
2015-08-28 15:44:32 +0300

    cortexa8: Data abort handler could be changed now

--- src/arch/arm/cortexa8/mmu_test.c
+++ src/arch/arm/cortexa8/mmu_test.c
@@ -6,8 +6,20 @@
  * @date 2015-08-18
  */
 
+#include <string.h>
+
 #include <hal/test/traps_core.h>
 
-void set_fault_handler(enum fault_type type, fault_handler_t handler) {
+extern void *data_abt_handler_addr;
+extern void *data_abt_handler;
 
+void set_fault_handler(enum fault_type type, fault_handler_t handler) {
+	switch (type) {
+		case MMU_DATA_MISS:
+			if (handler == NULL)
+				handler = data_abt_handler;
+			memcpy(&data_abt_handler_addr, &handler, sizeof(uintptr_t));
+		default:
+			return;
+	}
 }
