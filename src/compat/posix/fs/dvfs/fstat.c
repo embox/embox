33e42243e6a184ee686ceabb6c9762ea6fedb4e2
Denis Deryugin <deryugin.denis@gmail.com>
2015-09-10 16:44:53 +0300

    posix/dvfs: Add stubs for fstat() and ioctl()

---
+++ src/compat/posix/fs/dvfs/fstat.c
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version 0.1
+ * @date 2015-09-10
+ */
+
+#include <sys/stat.h>
+
+int fstat(int fd, struct stat *buff) {
+	return 0;
+}
