358f45227fd577f963b27a1a47e3726585856646
Anton Bondarev <anton.bondarev2310@gmail.com>
2015-09-20 22:02:49 +0400

    drivers: Add mapping to raspi's clock

--- src/drivers/clock/raspi_systick.c
+++ src/drivers/clock/raspi_systick.c
@@ -15,6 +15,10 @@
 #include <hal/system.h>
 #include <kernel/irq.h>
 #include <kernel/time/clock_source.h>
+#include <sys/mman.h>
+#include <hal/mmu.h>
+#include <mem/vmem.h>
+#include <util/binalign.h>
 
 #include <embox/unit.h>
 
@@ -69,6 +73,15 @@ static irq_return_t clock_handler(unsigned int irq_nr, void *data) {
 }
 
 static int this_init(void) {
+	/* Map one vmem page to handle this device if mmu is used */
+	mmap_device_memory(
+			(void*) ((uintptr_t) BCM2835_SYSTEM_TIMER_BASE & ~MMU_PAGE_MASK),
+			PROT_READ | PROT_WRITE | PROT_NOCACHE,
+			binalign_bound(sizeof(struct raspi_timer_regs), MMU_PAGE_SIZE),
+			VMEM_PAGE_WRITABLE,
+			((uintptr_t) BCM2835_SYSTEM_TIMER_BASE & ~MMU_PAGE_MASK)
+			);
+
 	clock_source_register(&this_clock_source);
 	irq_attach(SYSTICK_IRQ, clock_handler, 0, &this_clock_source,
 		"Raspberry PI systick timer");
