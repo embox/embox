1ae1940294901272dbc0eeec49a25689fc6a22de
Denis Deryugin <deryugin.denis@gmail.com>
2015-10-08 17:25:32 +0300

    dvfs: Add format() function to fs driver structure
    Also, implement format function for vfat FS

--- src/fs/dvfs/dvfs.h
+++ src/fs/dvfs/dvfs.h
@@ -122,6 +122,7 @@ struct file_operations {
 
 struct dumb_fs_driver {
 	const char name[FS_NAME_LEN];
+	int (*format)(void *dev);
 	int (*fill_sb)(struct super_block *sb, struct block_dev *dev);
 	int (*mount_end)(struct super_block *sb);
 };
