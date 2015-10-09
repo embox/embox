7068d161f48baf898ba5f78c38b34dd5b23780b2
Denis Deryugin <deryugin.denis@gmail.com>
2015-10-09 13:28:33 +0300

    vmem_device_memory: Add Dox

--- src/mem/vmem/vmem_device_memory.c
+++ src/mem/vmem/vmem_device_memory.c
@@ -29,11 +29,24 @@ static struct emmap *self_mmap(void) {
 	}
 }
 
+/**
+ * @brief Map specific memory address to access device
+ *
+ * @param addr Virtual address to where you want to map the object
+ * @param len Number of bytes to be mapped
+ * @param prot Access flags for region (PROT_EXEC/PROT_NOCACHE/etc)
+ * @param flags MAP_FIXED/etc
+ * @param physical Physical address of memory that's to be mapped
+ *
+ * @return Pointer to allocated vmem or NULL if failed
+ */
 void *mmap_device_memory(void *addr,
                            size_t len,
                            int prot,
                            int flags,
                            uint64_t physical){
+	/* TODO handle addr=NULL, region should be mapped to any addr
+	 * TODO handle flags anyhow */
 	struct emmap *emmap = self_mmap();
 	struct marea *marea;
 
