4ee4d5a6f68caebc04af3f8b00d34aebdb480e79
Anton Bondarev <anton.bondarev2310@gmail.com>
2015-10-28 19:26:00 +0300

    dfvs: Fix block_dev idesc operations

--- src/drivers/block_dev/block_dev_idesc_dvfs.c
+++ src/drivers/block_dev/block_dev_idesc_dvfs.c
@@ -18,19 +18,26 @@ static void bdev_idesc_close(struct idesc *desc) {
 }
 
 static ssize_t bdev_idesc_read(struct idesc *desc, void *buf, size_t size) {
+	struct file *file;
 	struct block_dev *bdev;
 
-	bdev = desc->f_inode->i_data;
-	return bdev->driver->read(bdev, buf, size, desc->pos / bdev->block_size);
+	file = (struct file *)desc;
+
+	bdev = file->f_inode->i_data;
+	return bdev->driver->read(bdev, buf, size, file->pos / bdev->block_size);
 }
 
 static ssize_t bdev_idesc_write(struct idesc *desc, const void *buf, size_t size) {
+	struct file *file;
 	struct block_dev *bdev;
 
-	bdev = desc->f_inode->i_data;
+	file = (struct file *)desc;
+
+	bdev = file->f_inode->i_data;
 	assert(bdev->driver);
 	assert(bdev->driver->write);
-	return bdev->driver->write(bdev, buf, size, desc->pos / bdev->block_size);
+
+	return bdev->driver->write(bdev, (void *)buf, size, file->pos / bdev->block_size);
 }
 
 static struct idesc_ops idesc_bdev_ops = {
