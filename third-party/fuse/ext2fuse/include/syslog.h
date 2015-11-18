5044165ce7fa8586bb6f95f6837c56b26e228326
Alex Kalmuk <alexkalmuk@gmail.com>
2015-11-18 18:59:53 +0300

    ext2fuse: Add ext2fuse command

--- third-party/fuse/ext2fuse/include/syslog.h
+++ third-party/fuse/ext2fuse/include/syslog.h
@@ -8,6 +8,13 @@
 #ifndef SYSLOG_H_
 #define SYSLOG_H_
 
+#define LOG_CRIT    2   /* critical conditions */
+#define LOG_ERR     3   /* error conditions */
+#define LOG_WARNING 4   /* warning conditions */
+#define LOG_NOTICE  5   /* normal but significant condition */
+#define LOG_INFO    6   /* informational */
+#define LOG_DEBUG   7   /* debug-level messages */
+
 extern void  syslog(int prio, const char *format, ...);
 
 #endif /* SYSLOG_H_ */
