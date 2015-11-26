bee530a94bc3dbae600c9b51cebc99ce496ee1e0
Anton Bondarev <anton.bondarev2310@gmail.com>
2015-11-26 14:46:40 +0300

    compat: Make struct dirent linux compatible

--- src/compat/posix/include/dirent.h
+++ src/compat/posix/include/dirent.h
@@ -17,6 +17,12 @@ __BEGIN_DECLS
 struct dirent {
 	ino_t  d_ino;                    /* File serial number. */
 	char   d_name[DIRENT_DNAME_LEN]; /* Name of entry. */
+
+	/*only for linux compatible */
+    off_t          d_off;       /* not an offset; see NOTES */
+    unsigned short d_reclen;    /* length of this record */
+	unsigned char  d_type;      /* type of file; not supported
+	                            by all filesystem types */
 };
 
 typedef struct DIR_struct DIR;
