b2ce4188967fadb323f4a2328cd8b33926941ee3
Denis Deryugin <deryugin.denis@gmail.com>
2015-11-27 17:33:26 +0300

    ac97: Add initial implementation with stubs

---
+++ src/drivers/audio/ac97_pa.c
@@ -0,0 +1,82 @@
+/**
+ * @file
+ * @brief Portaudio driver for AC97 controller
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version 0.1
+ * @date 2015-11-27
+ */
+
+#include <unistd.h>
+
+#include <drivers/audio/portaudio.h>
+
+PaError Pa_Initialize(void) {
+	return paNoError;
+}
+
+PaError Pa_Terminate(void) {
+	return paNoError;
+}
+
+PaHostApiIndex Pa_GetHostApiCount(void) {
+	return 1;
+}
+
+PaHostApiIndex Pa_GetDefaultHostApi(void) {
+	return 0;
+}
+
+PaDeviceIndex Pa_GetDeviceCount(void) {
+	return 1;
+}
+
+PaDeviceIndex Pa_GetDefaultInputDevice(void) {
+	return 0;
+}
+
+PaDeviceIndex Pa_GetDefaultOutputDevice(void) {
+	return 0;
+}
+
+PaError Pa_OpenStream(PaStream** stream,
+                       const PaStreamParameters *inputParameters,
+                       const PaStreamParameters *outputParameters,
+                       double sampleRate,
+                       unsigned long framesPerBuffer,
+                       PaStreamFlags streamFlags,
+                       PaStreamCallback *streamCallback,
+                       void *userData) {
+	return paNoError;
+}
+
+PaError Pa_StartStream(PaStream *stream) {
+	return paNoError;
+}
+
+PaError Pa_StopStream(PaStream *stream) {
+	return paNoError;
+}
+
+PaError Pa_CloseStream(PaStream *stream) {
+	return paNoError;
+}
+
+void Pa_Sleep(long msec) {
+	usleep(msec * 1000);
+}
+
+const PaDeviceInfo *Pa_GetDeviceInfo(PaDeviceIndex device) {
+	return 0;
+}
+
+const PaHostApiInfo *Pa_GetHostApiInfo(PaHostApiIndex hostApi) {
+	return 0;
+}
+
+const PaStreamInfo *Pa_GetStreamInfo(PaStream *stream) {
+	return 0;
+}
+
+const char *Pa_GetErrorText(PaError errorCode) {
+	return "Pa_GetErrorText not implemented";
+}
