cf66a20fcadfdef44ceb300e3098080fe636698b
Anton Bondarev <anton.bondarev2310@gmail.com>
2015-12-02 13:48:27 +0300

    drivers: Work on stm_usart driver
    Move stm32_usart to separate folder.
    Extract common headers to make the driver compatible for all stm32fx
    series.

---
+++ src/drivers/serial/stm32_usart/stm32_usart_conf_f4.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ *
+ * @data 30.11.2015
+ * @author: Anton Bondarev
+ */
+
+#ifndef SRC_DRIVERS_SERIAL_STM32_USART_STM32_USART_CONF_F4_H_
+#define SRC_DRIVERS_SERIAL_STM32_USART_STM32_USART_CONF_F4_H_
+
+#include <stm32f4xx_usart.h>
+#include <stm32f4xx_rcc.h>
+#include <stm32f4xx_gpio.h>
+
+#endif /* SRC_DRIVERS_SERIAL_STM32_USART_STM32_USART_CONF_F4_H_ */
---
+++ src/drivers/serial/stm32_usart/stm32_usart_conf_f7.h
@@ -0,0 +1,16 @@
+/**
+ * @file
+ *
+ * @data 30 нояб. 2015 г.
+ * @author: Anton Bondarev
+ */
+
+#ifndef SRC_DRIVERS_SERIAL_STM32_USART_STM32_USART_CONF_F7_H_
+#define SRC_DRIVERS_SERIAL_STM32_USART_STM32_USART_CONF_F7_H_
+
+#include <stm32f7xx_hal_dma.h>
+#include <stm32f7xx_hal_usart.h>
+#include <stm32f7xx_hal_rcc.h>
+#include <stm32f7xx_hal_gpio.h>
+
+#endif /* SRC_DRIVERS_SERIAL_STM32_USART_STM32_USART_CONF_F7_H_ */
