544f4d007c7d4b2967217da8892f95d1403cc0e2
Anton Bondarev <anton.bondarev2310@gmail.com>
2015-12-04 18:29:50 +0300

    driver: Work on stm_usart_cude driver
    Modufy f4 and f7 version

--- src/drivers/serial/stm32_usart/stm32_usart_conf_f4.h
+++ src/drivers/serial/stm32_usart/stm32_usart_conf_f4.h
@@ -8,48 +8,31 @@
 #ifndef SRC_DRIVERS_SERIAL_STM32_USART_STM32_USART_CONF_F4_H_
 #define SRC_DRIVERS_SERIAL_STM32_USART_STM32_USART_CONF_F4_H_
 
-#include <stm32f4xx_hal_dma.h>
-#include <stm32f4xx_hal_usart.h>
-#include <stm32f4xx_hal_rcc.h>
-#include <stm32f4xx_hal_gpio.h>
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_usart.h"
+#include "stm32f4xx_hal_uart.h"
+#include "stm32f4_discovery.h"
 
 #define MODOPS_USARTX OPTION_GET(NUMBER, usartx)
 
-#if MODOPS_USARTX == 6
-#define EVAL_COM                        USART6
-#define EVAL_COM_CLK                    RCC_APB2Periph_USART6
-#define EVAL_COM_CLK_CMD                RCC_APB2PeriphClockCmd
-#define EVAL_COM_TX_PIN                 GPIO_Pin_6
-#define EVAL_COM_TX_GPIO_PORT           GPIOC
-#define EVAL_COM_TX_GPIO_CLK            RCC_AHB1Periph_GPIOC
-#define EVAL_COM_TX_SOURCE              GPIO_PinSource6
-#define EVAL_COM_TX_AF                  GPIO_AF_USART6
-#define EVAL_COM_RX_PIN                 GPIO_Pin_7
-#define EVAL_COM_RX_GPIO_PORT           GPIOC
-#define EVAL_COM_RX_GPIO_CLK            RCC_AHB1Periph_GPIOC
-#define EVAL_COM_RX_SOURCE              GPIO_PinSource7
-#define EVAL_COM_RX_AF                  GPIO_AF_USART6
-/* In Embox we assume that the lower external irq number is 0,
- * but in the cortexm3 it is -15 */
-#define EVAL_COM_IRQn                   (USART6_IRQn + 16)
-#elif MODOPS_USARTX == 2
-#define EVAL_COM                        USART2
-#define EVAL_COM_CLK                    RCC_APB1Periph_USART2
-#define EVAL_COM_CLK_CMD                RCC_APB1PeriphClockCmd
-#define EVAL_COM_TX_PIN                 GPIO_Pin_5
-#define EVAL_COM_TX_GPIO_PORT           GPIOD
-#define EVAL_COM_TX_GPIO_CLK            RCC_AHB1Periph_GPIOD
-#define EVAL_COM_TX_SOURCE              GPIO_PinSource5
-#define EVAL_COM_TX_AF                  GPIO_AF_USART2
-#define EVAL_COM_RX_PIN                 GPIO_Pin_6
-#define EVAL_COM_RX_GPIO_PORT           GPIOD
-#define EVAL_COM_RX_GPIO_CLK            RCC_AHB1Periph_GPIOD
-#define EVAL_COM_RX_SOURCE              GPIO_PinSource6
-#define EVAL_COM_RX_AF                  GPIO_AF_USART2
-#define EVAL_COM_IRQn                   (USART2_IRQn + 16)
-#else
-#error Unsupported USARTx
-#endif
+#define USARTx                           USART2
+#define USARTx_CLK_ENABLE()              __HAL_RCC_USART2_CLK_ENABLE();
+#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
 
+#define USARTx_FORCE_RESET()             __HAL_RCC_USART2_FORCE_RESET()
+#define USARTx_RELEASE_RESET()           __HAL_RCC_USART2_RELEASE_RESET()
+
+/* Definition for USARTx Pins */
+#define USARTx_TX_PIN                    GPIO_PIN_2
+#define USARTx_TX_GPIO_PORT              GPIOA
+#define USARTx_TX_AF                     GPIO_AF7_USART2
+#define USARTx_RX_PIN                    GPIO_PIN_3
+#define USARTx_RX_GPIO_PORT              GPIOA
+#define USARTx_RX_AF                     GPIO_AF7_USART2
+
+/* Definition for USARTx's NVIC */
+#define USARTx_IRQn                      USART2_IRQn
+#define USARTx_IRQHandler                USART2_IRQHandler
 
 #endif /* SRC_DRIVERS_SERIAL_STM32_USART_STM32_USART_CONF_F4_H_ */
