43df80aaac40375c23a5c927cff6102cd7f15ad2
Anton Bondarev <anton.bondarev2310@gmail.com>
2015-12-09 18:52:37 +0300

    stm32: Work on flash driver

--- src/drivers/flash/stm32_flash.h
+++ src/drivers/flash/stm32_flash.h
@@ -8,9 +8,7 @@
 #ifndef SRC_DRIVERS_FLASH_STM32_FLASH_H_
 #define SRC_DRIVERS_FLASH_STM32_FLASH_H_
 
-#include <framework/mod/options.h>
-
-#define STM32F3_FLASH_PAGE_SIZE     OPTION_GET(NUMBER,page_size)
-#define STM32F3_FLASH_FLASH_SIZE    OPTION_GET(NUMBER,flash_size)
+#include <drivers/block_dev/flash/stm32flash_conf.h>
+#include <stm32f4xx_flash.h>
 
 #endif /* SRC_DRIVERS_FLASH_STM32_FLASH_H_ */
--- src/drivers/flash/stm32_flash_cube.c
+++ src/drivers/flash/stm32_flash_cube.c
@@ -13,49 +13,41 @@
 #include <util/math.h>
 
 #include <drivers/block_dev/flash/flash_dev.h>
-#include <fs/node.h>
 #include <hal/reg.h>
 #include <hal/mem.h>
 #include <kernel/printk.h>
 
 #include <drivers/block_dev/flash/stm32flash.h>
 
-#define STM32_FIRST_BLOCK_OFFSET 1
-#define STM32_ERR_MASK 0x1f3
+#define STM32_FIRST_BLOCK_OFFSET   0x1
+#define STM32_ERR_MASK             0x1F3
 
-static const struct flash_dev_drv stm32_flash_drv;
-static const struct flash_dev stm32_flash = {
+extern char _flash_start, _flash_end;
+
+static const struct flash_dev_drv stm32flash_drv;
+static const struct flash_dev stm32flash = {
 	.bdev = NULL,
-	.drv = &stm32_flash_drv,
+	.drv = &stm32flash_drv,
 	.flags = 0,
-	.start = 0x08004000,
-	.end   = 0x08010000,
+	.start = (uint32_t) &_flash_start,
+	.end   = (uint32_t) &_flash_end,
 	.num_block_infos = 1,
 	.block_info = {
-		.block_size = 0x004000,
-		.blocks = 3
+		.block_size = STM32_FLASH_PAGE_SIZE,
+		.blocks = STM32_FLASH_FLASH_SIZE / STM32_FLASH_PAGE_SIZE
 	},
 };
-
-static inline void stm32_flash_set_program_size(void) {
-	/* setting word 32-bit access */
-	REG_STORE(&FLASH->CR, (REG_LOAD(&FLASH->CR) & CR_PSIZE_MASK) | FLASH_PSIZE_WORD);
-}
 
 static inline int stm32_flash_check_range(struct flash_dev *dev, unsigned long base, size_t len) {
 	return dev->start + base + len <= dev->end;
 }
 
 static inline int stm32_flash_check_align(unsigned long base, size_t len) {
-	return ((uintptr_t) base & STM32_FLASH_WORD_ALIGN ) == 0 && ((uintptr_t) len  & STM32_FLASH_WORD_ALIGN) == 0;
-}
-static inline void stm32_flash_wait(void) {
-	while (REG_LOAD(&FLASH->SR) & FLASH_FLAG_BSY) {
-		/* nop */
-	}
+	return ((uintptr_t) base & STM32_FLASH_WORD_ALIGN ) == 0 &&
+			((uintptr_t) len  & STM32_FLASH_WORD_ALIGN) == 0;
 }
 
-static inline int stm32_flash_check_block(struct flash_dev *dev, uint32_t block) {
+static inline int stm32f3_flash_check_block(struct flash_dev *dev, uint32_t block) {
 	unsigned int n_block, i;
 
 	n_block = 0;
@@ -66,39 +58,40 @@ static inline int stm32_flash_check_block(struct flash_dev *dev, uint32_t block)
 	return block < n_block;
 }
 
-static int stm32_flash_erase_block(struct flash_dev *dev, uint32_t block) {
-	unsigned long sr;
-	int err;
 
-	if (!stm32_flash_check_block(dev, block)) {
+static inline void stm32_flash_set_program_size(void) {
+	/* setting word 32-bit access */
+	REG_STORE(&FLASH->CR, (REG_LOAD(&FLASH->CR) & CR_PSIZE_MASK) | FLASH_PSIZE_WORD);
+}
+
+static inline void stm32_flash_wait(void) {
+	while (REG_LOAD(&FLASH->SR) & FLASH_FLAG_BSY) {
+		/* nop */
+	}
+}
+
+static int stm32_flash_erase_block(struct flash_dev *dev, uint32_t block) {
+	int err;
+	uint32_t page_err;
+	FLASH_EraseInitTypeDef erase_struct;
+
+	if (!stm32f3_flash_check_block(dev, block)) {
 		return -EINVAL;
 	}
 
-	FLASH_Unlock();
-
+	HAL_FLASH_Unlock();
 	stm32_flash_wait();
 
-	/* clear all errors */
-	REG_STORE(&FLASH->SR, STM32_ERR_MASK);
+	erase_struct = (FLASH_EraseInitTypeDef) {
+		.TypeErase = FLASH_TYPEERASE_SECTORS,
+		.Sector = block,
+		.NbSectors = 1,
+	};
 
-	stm32_flash_set_program_size();
-
-	REG_STORE(&FLASH->CR,
-			(REG_LOAD(&FLASH->CR) & 0xffffff07)
-			| FLASH_CR_SER
-			| ((STM32_FIRST_BLOCK_OFFSET + block) << 3));
-
-	sr = REG_LOAD(&FLASH->SR);
-	if (!(sr & STM32_ERR_MASK)) {
-		REG_ORIN(&FLASH->CR, FLASH_CR_STRT);
-		stm32_flash_wait();
-		err = 0;
-	} else {
-		printk("%s: EIO sr=%08lx\n", __func__, sr);
-		err = -EIO;
-	}
-
-	FLASH_Lock();
+	err = HAL_FLASHEx_Erase(&erase_struct, &page_err);
+	/* TODO check errcode */
+	/* Lock to prevent unwanted operations with flash memory */
+	HAL_FLASH_Lock();
 
 	return err;
 }
@@ -119,7 +112,7 @@ static int stm32_flash_program(struct flash_dev *dev, uint32_t base, const void*
 	int err;
 
 	if (!stm32_flash_check_align(base, len)
-			|| ((uintptr_t) data & STM32_FLASH_WORD_ALIGN) != 0) {
+			|| ((uintptr_t) data & 0x3) != 0) {
 		return -EINVAL;
 	}
 
@@ -127,7 +120,7 @@ static int stm32_flash_program(struct flash_dev *dev, uint32_t base, const void*
 		return -EFBIG;
 	}
 
-	FLASH_Unlock();
+	HAL_FLASH_Unlock();
 
 	stm32_flash_wait();
 
@@ -148,7 +141,7 @@ static int stm32_flash_program(struct flash_dev *dev, uint32_t base, const void*
 		err = -EIO;
 	}
 
-	FLASH_Lock();
+	HAL_FLASH_Lock();
 
 	return err;
 }
@@ -164,4 +157,3 @@ static const struct flash_dev_drv stm32_flash_drv = {
 	.flash_program = stm32_flash_program,
 	.flash_copy = stm32_flash_copy,
 };
-
