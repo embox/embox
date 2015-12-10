8858cb192ae8b331c6a15177375ba143d613032d
Denis Deryugin <deryugin.denis@gmail.com>
2015-12-10 14:07:50 +0300

    fs: Initial implement of file format detect

---
+++ src/fs/file_format.c
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * @brief Detect file format by header/etc
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version 0.1
+ * @date 2015-12-10
+ */
+
+#include <fs/file_format.h>
+#include <unistd.h>
+
+enum fformat raw_get_file_format(uint8_t *file) {
+	return TEXT_FILE; /* Stub */
+}
+
+enum fformat posix_get_file_format(int fd) {
+	uint8_t buf[FILE_HEADER_LEN];
+	read(fd, buf, FILE_HEADER_LEN);
+	return raw_get_file_format(buf);
+}
+
+enum fformat libc_get_file_format(FILE *file) {
+	uint8_t buf[FILE_HEADER_LEN];
+	fread(buf, 1, FILE_HEADER_LEN, file);
+	return raw_get_file_format(buf);
+}
