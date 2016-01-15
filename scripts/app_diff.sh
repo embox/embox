163027490e3fba242ec43b143b3ce1e7d01cc11c
Alex Kalmuk <alexkalmuk@gmail.com>
2016-01-15 12:00:06 +0300

    scripts: Fix diff script a little

--- scripts/app_diff.sh
+++ scripts/app_diff.sh
@@ -16,5 +16,5 @@ SRC_APP_DIR=$EMBOX_DIR/$3 #third-party/fuse/fuse
 cd $EXTBLD_APP_DIR
 mkdir -p $APP_NAME-orig
 tar xzf $EMBOX_DIR/download/${APP_NAME}.tar.gz -C $APP_NAME-orig --strip-components=1
-diff -aur -x configure $APP_NAME-orig $APP_NAME | grep -v "Only in" | tee $SRC_APP_DIR/patch.txt
+diff -aur -x configure $APP_NAME-orig $APP_NAME | grep -v "^Only in" | tee $SRC_APP_DIR/patch.txt
 rm -rf $APP_NAME-orig
