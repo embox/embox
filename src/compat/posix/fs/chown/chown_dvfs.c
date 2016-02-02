97e797226e8203bcf8b7a4a1178431139c83c0b4
Anton Bondarev <anton.bondarev2310@gmail.com>
2016-02-02 17:04:13 +0300

    fs: posix: Implemante chown() syscall

---
+++ src/compat/posix/fs/chown/chown_dvfs.c
@@ -0,0 +1,31 @@
+/**
+ * @file
+ *
+ * @data 01.02.2016
+ * @author: Anton Bondarev
+ */
+
+#include <errno.h>
+#include <unistd.h>
+
+#include <fs/dvfs.h>
+
+int chown(const char *path, uid_t owner, gid_t group) {
+	struct lookup lookup;
+	int res;
+
+	if (!path) {
+		return SET_ERRNO(EFAULT);
+	}
+
+	res = dvfs_lookup(path, &lookup);
+	if (res) {
+		return SET_ERRNO(ENOENT);
+	}
+	assert(lookup.item);
+	assert(lookup.item->d_inode);
+	lookup.item->d_inode->i_owner_id = owner;
+	lookup.item->d_inode->i_owner_id = group;
+
+	return 0;
+}
