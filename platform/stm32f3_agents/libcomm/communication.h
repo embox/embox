6f82695f3eec117fdcf6f4aa34ef9c697e65b577
Deryugin <deryugin.denis@gmail.com>
2016-02-02 12:31:13 +0300

    libcomm: Remove UART implementation
    Now Cube implementation is used

--- platform/stm32f3_agents/libcomm/communication.h
+++ platform/stm32f3_agents/libcomm/communication.h
@@ -6,6 +6,13 @@
  * @date 2015-11-02
  */
 
+#ifndef LIBCOMM_UART_H_
+#define LIBCOMM_UART_H_
+
+#include <stdint.h>
+#include "stm32f3xx_hal.h"
+#include "stm32f3_discovery.h"
+
 #define UART_BUFF_SZ     16
 #define UART_BAUD_RATE   9600
 /**
@@ -17,3 +24,5 @@ extern int comm_init(void);
 
 extern int send_byte(uint8_t b, int i_num);
 extern uint8_t get_byte(int i_num);
+
+#endif /* LIBCOMM_UART_H_ */
