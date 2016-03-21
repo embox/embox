a3058d75195cf740363fcfca3eb3d132062c33a9
gzoom13 <golikov.andrei@gmail.com>
2016-03-21 23:04:34 +0300

    Added xen_diag stub

--- src/drivers/diag/xen_diag.c
+++ src/drivers/diag/xen_diag.c
@@ -1,2 +1,21 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 17.03.2012
+ * @author Anton Kozlov
+ */
+
 #include <drivers/diag.h>
 
+static int diag_xen_init(const struct diag *diag) {
+	return 0;
+}
+
+static void diag_xen_putc(const struct diag *diag, char ch) {
+}
+
+DIAG_OPS_DECLARE(
+	.init = diag_xen_init,
+	.putc = diag_xen_putc,
+);
