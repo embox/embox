a988df2f74151154ed43cc72c162e15a8c922d47
Deryugin <deryugin.denis@gmail.com>
2016-04-11 15:11:41 +0300

    (drivers) lan91c111: Add I/O-related macros
    Add offsets for banks, add macros for 16-bit register store and load,
    add function for bank select

--- src/drivers/net/lan91c111/lan91c111.c
+++ src/drivers/net/lan91c111/lan91c111.c
@@ -8,6 +8,61 @@
 #include <net/netdevice.h>
 #include <embox/unit.h>
 
+/* Internal I/O space mapping */
+
+#define BANK_BASE_ADDR 0x00000000 /* TODO */
+
+/* Bank 0 */
+#define BANK_TCR        (BANK_BASE_ADDR + 0x0)
+#define BANK_EPH_STATUS (BANK_BASE_ADDR + 0x2)
+#define BANK_RCR        (BANK_BASE_ADDR + 0x4)
+#define BANK_COUNTER    (BANK_BASE_ADDR + 0x6)
+#define BANK_MIR        (BANK_BASE_ADDR + 0x8)
+#define BANK_RPCR       (BANK_BASE_ADDR + 0xA)
+/*      BANK_RESERVED   (BANK_BASE_ADDR + 0xC) */
+#define BANK_BANK       (BANK_BASE_ADDR + 0xE) /* Common for all banks */
+
+/* Bank 1 */
+#define BANK_CONFIG          (BANK_BASE_ADDR + 0x0)
+#define BANK_BASE            (BANK_BASE_ADDR + 0x2)
+#define BANK_IA01            (BANK_BASE_ADDR + 0x4)
+#define BANK_IA23            (BANK_BASE_ADDR + 0x6)
+#define BANK_IA35            (BANK_BASE_ADDR + 0x8)
+#define BANK_GENERAL_PURPOSE (BANK_BASE_ADDR + 0xA)
+#define BANK_CONTROL         (BANK_BASE_ADDR + 0xC)
+/*      BANK_BANK            (BANK_BASE_ADDR + 0xE) -- already defined above */
+
+/* Bank 2 */
+#define BANK_MMU_CMD    (BANK_BASE_ADDR + 0x0)
+#define BANK_PNR        (BANK_BASE_ADDR + 0x2)
+#define BANK_FIFO_PORTS (BANK_BASE_ADDR + 0x4)
+#define BANK_POINTER    (BANK_BASE_ADDR + 0x6)
+#define BANK_DATA       (BANK_BASE_ADDR + 0x8)
+/*      BANK_DATA       (BANK_BASE_ADDR + 0xA) -- another 2 bytes */
+#define BANK_INTERRUPT  (BANK_BASE_ADDR + 0xC)
+/*      BANK_BANK       (BANK_BASE_ADDR + 0xE) -- already defined above */
+
+/* Bank 3 */
+#define BANK_MT01     (BANK_BASE_ADDR + 0x0)
+#define BANK_MT23     (BANK_BASE_ADDR + 0x2)
+#define BANK_MT45     (BANK_BASE_ADDR + 0x4)
+#define BANK_MT67     (BANK_BASE_ADDR + 0x6)
+#define BANK_MGMT     (BANK_BASE_ADDR + 0x8)
+#define BANK_REVISION (BANK_BASE_ADDR + 0xA)
+#define BANK_RCV      (BANK_BASE_ADDR + 0xC)
+/*      BANK_BANK     (BANK_BASE_ADDR + 0xE) -- already defined above */
+
+#define REG16_LOAD(addr, val) \
+	*((volatile uint16_t *)(addr))
+
+#define REG16_STORE(addr, val) \
+	do { *((volatile uint16_t *)(addr)) = (val); } while (0)
+
+static void _set_bank(int n) {
+	assert(0 <= n && n <= 3);
+	REG16_STORE(BANK_BANK, (uint16_t) n);
+}
+
 EMBOX_UNIT_INIT(lan91c111_init);
 static int lan91c111_xmit(struct net_device *dev, struct sk_buff *skb) {
 	skb_free(skb);
@@ -29,5 +84,7 @@ static const struct net_driver lan91c111_drv_ops = {
 };
 
 static int lan91c111_init(void) {
+	_set_bank(0); /* Just to use it */
+
 	return 0;
 }
