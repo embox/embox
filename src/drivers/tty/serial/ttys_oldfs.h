9e5448d6b6287a46f62270e56ce6bbca82abf4ed
Anton Bondarev <anton.bondarev2310@gmail.com>
2016-04-21 20:56:07 +0400

    drivers: Extract TTYS_DEF macro to drivers/ttys.h

---
+++ src/drivers/tty/serial/ttys_oldfs.h
@@ -0,0 +1,16 @@
+/**
+ * @file
+ *
+ * @date 21.04.2016
+ * @author Anton Bondarev
+ */
+
+#ifndef TTYS_H_
+#define TTYS_H_
+
+#include <drivers/char_dev.h>
+#define TTYS_DEF(name, uart) \
+		extern const struct kfile_operations ttys_fops; \
+		CHAR_DEV_DEF(name, (struct file_operations *)&ttys_fops, NULL, uart)
+
+#endif /* TTYS_H_ */
