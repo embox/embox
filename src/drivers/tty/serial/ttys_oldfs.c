79e4765a051fd1837b35247bfc24c266ea4cdc64
Anton Bondarev <anton.bondarev2310@gmail.com>
2016-04-23 17:36:42 +0400

    drivers: Add ttys oldfs stubs

--- src/drivers/tty/serial/ttys_oldfs.c
+++ src/drivers/tty/serial/ttys_oldfs.c
@@ -18,10 +18,20 @@
 #include "idesc_serial.h"
 #include <fs/file_operation.h>
 #include <fs/node.h>
+#include <fs/file_desc.h>
 
 
 static int uart_fsop_open(struct node *node, struct file_desc *file_desc, int flags)  {
-	return -1;
+	struct uart *uart;
+	uart = uart_dev_lookup(node->name);
+	if (!uart) {
+		return -ENOENT;
+	}
+	file_desc->file_info = uart;
+	idesc_serial_create(uart, flags);
+
+	//uart_open(uart);
+	return 0;
 }
 
 static int uart_fsop_close(struct file_desc *desc) {
@@ -29,11 +39,32 @@ static int uart_fsop_close(struct file_desc *desc) {
 }
 
 static size_t uart_fsop_read(struct file_desc *desc, void *buf, size_t size) {
+	struct uart *uart;
+	int i;
+	char *b;
 
-	return 0;
+	b = buf;
+	uart = desc->file_info;
+	for(i = 0; i < size; i ++) {
+		while(!uart->uart_ops->uart_hasrx(uart)) {
+		}
+		b[i] = uart->uart_ops->uart_getc(uart);
+	}
+
+	return i;
 }
 
 static size_t uart_fsop_write(struct file_desc *desc, void *buf, size_t size) {
+	struct uart *uart;
+	int i;
+	char *b;
+
+	b = buf;
+	uart = desc->file_info;
+
+	for(i = 0; i < size; i ++) {
+		uart->uart_ops->uart_putc(uart, b[i]);
+	}
 	return 0;
 }
 
@@ -43,3 +74,7 @@ const struct kfile_operations ttys_fops = {
 	.read = uart_fsop_read,
 	.write = uart_fsop_write
 };
+
+int ttys_register(const char*name, void *dev_info) {
+	return char_dev_register(name, &ttys_fops);
+}
--- src/drivers/tty/serial/ttys_oldfs.h
+++ src/drivers/tty/serial/ttys_oldfs.h
@@ -9,8 +9,9 @@
 #define TTYS_H_
 
 #include <drivers/char_dev.h>
+#if 0
 #define TTYS_DEF(name, uart) \
 		extern const struct kfile_operations ttys_fops; \
-		CHAR_DEV_DEF(name, (struct file_operations *)&ttys_fops, NULL, uart)
-
+		CHAR_DEV_DEF(name, (struct kfile_operations *)&ttys_fops, NULL, uart)
+#endif
 #endif /* TTYS_H_ */
