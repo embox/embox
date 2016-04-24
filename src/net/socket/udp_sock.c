6e488b5aa3f838b6cee82aa1541bd45ca00a0cd0
denis-chusovitin <chusovitin.den@gmail.com>
2016-04-24 16:02:33 +0300

    upd_sendmsg supports multiple msg buffers now

--- src/net/socket/udp_sock.c
+++ src/net/socket/udp_sock.c
@@ -25,6 +25,8 @@
 
 #include <util/dlist.h>
 
+#include <stdlib.h>
+
 static const struct sock_proto_ops udp_sock_ops_struct;
 const struct sock_proto_ops *const udp_sock_ops
 		= &udp_sock_ops_struct;
@@ -38,6 +40,9 @@ static int udp_sendmsg(struct sock *sk, struct msghdr *msg, int flags) {
 	struct sk_buff *skb;
 	const struct sockaddr_in *to;
 	const struct sockaddr *sockaddr;
+	int i, size;
+	void* buf;
+	void* buf_;
 
 	assert(sk);
 	assert(sk->o_ops);
@@ -46,8 +51,13 @@ static int udp_sendmsg(struct sock *sk, struct msghdr *msg, int flags) {
 	assert(msg->msg_iov);
 	assert(msg->msg_iov->iov_base);
 
-	data_len = msg->msg_iov->iov_len;
+	data_len = 0;
+	for (i = 0; i < msg->msg_iovlen; i++) {
+		data_len += msg->msg_iov[i].iov_len;
+	}
+
 	total_len = actual_len = UDP_HEADER_SIZE + data_len;
+
 	skb = NULL;
 	sockaddr = (const struct sockaddr *)msg->msg_name;
 
@@ -74,12 +84,19 @@ static int udp_sendmsg(struct sock *sk, struct msghdr *msg, int flags) {
 
 	udp_build(skb->h.uh, sock_inet_get_src_port(sk), to->sin_port, total_len);
 
-	memcpy(skb->h.uh + 1, msg->msg_iov->iov_base, data_len);
+	size = sizeof(msg->msg_iov[i].iov_base[0]);
+	buf = buf_ = malloc(data_len);
+	for (i = 0; i < msg->msg_iovlen; i++) {
+		memcpy(buf_, msg->msg_iov[i].iov_base, msg->msg_iov[i].iov_len);
+		buf_ += msg->msg_iov[i].iov_len * size;
+	}
+	memcpy(skb->h.uh + 1, buf, data_len);
 
 	udp4_set_check_field(skb->h.uh, skb->nh.iph);
 
 	assert(sk->o_ops->snd_pack);
 	ret = sk->o_ops->snd_pack(skb);
+
 	if (0 > ret) {
 		return ret;
 	}
