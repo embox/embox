85051d8257f4317be97c95355cffc4ec70cbd26b
denis-chusovitin <chusovitin.den@gmail.com>
2016-04-30 11:27:38 +0300

    Fix udp_sendmsg

--- src/net/socket/udp_sock.c
+++ src/net/socket/udp_sock.c
@@ -35,14 +35,12 @@ EMBOX_NET_SOCK(AF_INET, SOCK_DGRAM, IPPROTO_UDP, 1,
 		udp_sock_ops_struct);
 
 static int udp_sendmsg(struct sock *sk, struct msghdr *msg, int flags) {
-	int ret;
-	size_t data_len, total_len, actual_len;
+	size_t data_len, data_len_, actual_len, skb_len, iov_len;
 	struct sk_buff *skb;
 	const struct sockaddr_in *to;
 	const struct sockaddr *sockaddr;
-	int i, size;
-	void* buf;
-	void* buf_;
+	int i, size, ret;
+	unsigned char *udp_header;
 
 	assert(sk);
 	assert(sk->o_ops);
@@ -55,51 +53,61 @@ static int udp_sendmsg(struct sock *sk, struct msghdr *msg, int flags) {
 	for (i = 0; i < msg->msg_iovlen; i++) {
 		data_len += msg->msg_iov[i].iov_len;
 	}
+	data_len_ = data_len;
 
-	total_len = actual_len = UDP_HEADER_SIZE + data_len;
+	actual_len = UDP_HEADER_SIZE + data_len;
 
 	skb = NULL;
 	sockaddr = (const struct sockaddr *)msg->msg_name;
 
-	ret = sk->o_ops->make_pack(sk, sockaddr, &actual_len, &skb);
-	if (ret != 0) {
-		return ret;
+	while (data_len_ > 0) {
+		ret = sk->o_ops->make_pack(sk, sockaddr, &actual_len, &skb);
+
+		if (ret != 0) {
+			return ret;
+		}
+
+		if (msg->msg_name != NULL) {
+			to = (const struct sockaddr_in *)msg->msg_name;
+		} else {
+			to = (const struct sockaddr_in *)&to_inet_sock(sk)->dst_in;
+		}
+
+		assert(skb);
+		assert(skb->h.uh);
+
+		udp_header = (unsigned char *) (skb->h.uh + 1);
+		size = sizeof(msg->msg_iov[i].iov_base[0]);
+
+		iov_len = i = skb_len = 0;
+
+		for (i = 0; i < msg->msg_iovlen; i++) {
+			iov_len = msg->msg_iov[i].iov_len * size;
+
+			if (skb_len + iov_len <= actual_len) {
+				memcpy(udp_header + skb_len, msg->msg_iov[i].iov_base, iov_len);
+				skb_len += iov_len;
+			}
+			else {
+				break;
+			}
+		}
+
+		udp_build(skb->h.uh, sock_inet_get_src_port(sk), to->sin_port,
+				skb_len + UDP_HEADER_SIZE);
+
+		data_len_ -= skb_len;
+		actual_len = data_len_ + UDP_HEADER_SIZE;
+		udp4_set_check_field(skb->h.uh, skb->nh.iph);
+
+		assert(sk->o_ops->snd_pack);
+		ret = sk->o_ops->snd_pack(skb);
+
+		if (0 > ret) {
+			return ret;
+		}
 	}
 
-#if 0
-	if (actual_len < total_len) {
-		skb_free(skb);
-		return -EMSGSIZE;
-	}
-#endif
-
-	if (msg->msg_name != NULL) {
-		to = (const struct sockaddr_in *)msg->msg_name;
-	} else {
-		to = (const struct sockaddr_in *)&to_inet_sock(sk)->dst_in;
-	}
-
-	assert(skb);
-	assert(skb->h.uh);
-
-	udp_build(skb->h.uh, sock_inet_get_src_port(sk), to->sin_port, total_len);
-
-	size = sizeof(msg->msg_iov[i].iov_base[0]);
-	buf = buf_ = malloc(data_len);
-	for (i = 0; i < msg->msg_iovlen; i++) {
-		memcpy(buf_, msg->msg_iov[i].iov_base, msg->msg_iov[i].iov_len);
-		buf_ += msg->msg_iov[i].iov_len * size;
-	}
-	memcpy(skb->h.uh + 1, buf, data_len);
-
-	udp4_set_check_field(skb->h.uh, skb->nh.iph);
-
-	assert(sk->o_ops->snd_pack);
-	ret = sk->o_ops->snd_pack(skb);
-
-	if (0 > ret) {
-		return ret;
-	}
 	return data_len;
 }
 
