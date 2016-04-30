398bf87fc4720ce0c2f7c742962bfb3ec2a3b32f
denis-chusovitin <chusovitin.den@gmail.com>
2016-04-30 11:27:38 +0300

    Fix udp_sendmsg

--- src/net/socket/ksocket.c
+++ src/net/socket/ksocket.c
@@ -230,7 +230,6 @@ int ksendmsg(struct sock *sk, struct msghdr *msg, int flags) {
 	assert(sk);
 	assert(msg);
 	assert(msg->msg_iov);
-	//assert(msg->msg_iovlen == 1); // FIXME add support of scatter gather
 
 	assert(sk->f_ops);
 	assert(sk->f_ops->sendmsg);
@@ -280,7 +279,6 @@ int krecvmsg(struct sock *sk, struct msghdr *msg, int flags) {
 	assert(sk);
 	assert(msg);
 	assert(msg->msg_iov);
-	//assert(msg->msg_iovlen == 1); // FIXME add support of scatter gather
 
 //	if (msg->msg_iov->iov_len == 0) {
 //		return 0;
