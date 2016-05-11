2b9e472fbabdf098e8a4adcea81fd5ebf053b358
Deryugin <deryugin.denis@gmail.com>
2016-05-11 18:46:14 +0300

    hal/reg: Add support for 8/16/32-bit ops

--- src/include/hal/reg.h
+++ src/include/hal/reg.h
@@ -11,6 +11,7 @@
 #define HAL_REG_H_
 
 #include <asm/hal/reg.h>
+#include <stdint.h>
 
 #ifndef __REG_STORE
 #define __REG_STORE(addr, value) \
@@ -36,5 +37,23 @@
 #define REG_LOAD(addr)         __REG_LOAD(addr)
 #define REG_ORIN(addr, mask)   __REG_ORIN(addr, mask)
 #define REG_ANDIN(addr, mask)  __REG_ANDIN(addr, mask)
+
+#define REG32_LOAD(addr) \
+	*((volatile uint32_t *)(addr))
+
+#define REG32_STORE(addr, val) \
+	do { *((volatile uint32_t *)(addr)) = (val); } while (0)
+
+#define REG16_LOAD(addr) \
+	*((volatile uint16_t *)(addr))
+
+#define REG16_STORE(addr, val) \
+	do { *((volatile uint16_t *)(addr)) = (val); } while (0)
+
+#define REG8_LOAD(addr) \
+	*((volatile uint8_t *)(addr))
+
+#define REG8_STORE(addr, val) \
+	do { *((volatile uint8_t *)(addr)) = (val); } while (0)
 
 #endif /* HAL_REG_H_ */
