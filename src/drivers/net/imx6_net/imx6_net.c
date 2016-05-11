e69f1fd58b3922d9c647a8dea06bffa78ad3c009
Deryugin <deryugin.denis@gmail.com>
2016-05-11 18:50:13 +0300

    (drivers) imx6_net: Implement macaddr_set()

--- src/drivers/net/imx6_net/imx6_net.c
+++ src/drivers/net/imx6_net/imx6_net.c
@@ -8,6 +8,8 @@
 
 #include <errno.h>
 
+#include <hal/reg.h>
+
 #include <net/inetdevice.h>
 #include <net/l0/net_entry.h>
 #include <net/l2/ethernet.h>
@@ -15,6 +17,13 @@
 #include <net/skbuff.h>
 
 #include <embox/unit.h>
+
+#include <framework/mod/options.h>
+
+#define NIC_BASE OPTION_GET(NUMBER, base_addr)
+
+#define MAC_LOW (NIC_BASE + 0xE4)
+#define MAC_HI  (NIC_BASE + 0xE8)
 
 EMBOX_UNIT_INIT(imx6_net_init);
 static int imx6_net_xmit(struct net_device *dev, struct sk_buff *skb) {
@@ -27,6 +36,20 @@ static int imx6_net_open(struct net_device *dev) {
 }
 
 static int imx6_net_set_macaddr(struct net_device *dev, const void *addr) {
+	uint8_t *mac = (uint8_t *) addr;
+	uint32_t mac_hi, mac_lo;
+
+	assert(dev);
+	assert(addr);
+
+        assert(mac);
+
+	mac_hi  = (mac[5] << 16) | (mac[4] << 8) | (mac[3] << 0);
+	mac_lo  = (mac[2] << 16) | (mac[1] << 8) | (mac[0] << 0);
+
+	REG32_STORE(MAC_LOW, mac_lo);
+	REG32_STORE(MAC_HI, mac_hi);
+
 	return 0;
 }
 
