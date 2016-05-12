ab1c5a7e655e6a61f902408c142d65e134f2b8ec
Deryugin <deryugin.denis@gmail.com>
2016-05-12 09:56:47 +0300

    (drivers) imx6_net: Add structure for buf desc
    Also add all flags

--- src/drivers/net/imx6_net/imx6_net.c
+++ src/drivers/net/imx6_net/imx6_net.c
@@ -8,6 +8,7 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <string.h>
 
 #include <hal/reg.h>
 
@@ -23,6 +24,7 @@
 
 #define NIC_BASE OPTION_GET(NUMBER, base_addr)
 
+#define ENET_TDAR (NIC_BASE + 0x0014)
 #define ENET_ECR  (NIC_BASE + 0x0024)
 #define ENET_RCR  (NIC_BASE + 0x0084)
 #define ENET_TCR  (NIC_BASE + 0x00C4)
@@ -43,12 +45,92 @@
 #define RX_BUF_FRAMES 256
 #define RX_BUF_LEN    (FRAME_LEN * TX_BUF_FRAMES)
 
-static uint8_t _tx_buf[TX_BUF_LEN] __attribute__ ((aligned(16)));
-static uint8_t _rx_buf[RX_BUF_LEN] __attribute__ ((aligned(16)));
+/* Descriptor flags */
+/* TX */
+/* Flags 1 */
+#define FLAG_R      (1 << 15)
+#define FLAG_TO1    (1 << 14)
+#define FLAG_W      (1 << 13)
+#define FLAG_TO2    (1 << 12)
+#define FLAG_L      (1 << 11)
+#define FLAG_TC     (1 << 10)
+/* Flags 2 */
+#define FLAG_TSE    (1 << 8)
+#define FLAG_OE     (1 << 9)
+#define FLAG_LCE    (1 << 10)
+#define FLAG_FE     (1 << 11)
+#define FLAG_EE     (1 << 12)
+#define FLAG_UE     (1 << 13)
+#define FLAG_TXE    (1 << 15)
+#define FLAG_IINS   (1 << 27)
+#define FLAG_PINS   (1 << 28)
+#define FLAG_TS     (1 << 29)
+#define FLAG_INT_TX (1 << 30)
+/* Flags 3 */
+#define FLAG_BDU    (1 << 15)
 
-EMBOX_UNIT_INIT(imx6_net_init);
+/* RX */
+/* Flags 1 */
+#define FLAG_TR     (1 << 0)
+#define FLAG_OV     (1 << 1)
+#define FLAG_CR     (1 << 2)
+#define FLAG_NO     (1 << 4)
+#define FLAG_LG     (1 << 5)
+#define FLAG_MC     (1 << 6)
+#define FLAG_BC     (1 << 7)
+#define FLAG_M      (1 << 8)
+#define FLAG_RO2    (1 << 12)
+#define FLAG_RO1    (1 << 14)
+#define FLAG_E      (1 << 15)
+/* Flags 2 */
+#define FLAG_FRAG   (1 << 0)
+#define FLAG_IPV6   (1 << 1)
+#define FLAG_VLAN   (1 << 2)
+#define FLAG_PCR    (1 << 4)
+#define FLAG_ICE    (1 << 5)
+#define FLAG_INT_RX (1 << 23)
+#define FLAG_UC     (1 << 24)
+#define FLAG_CE     (1 << 25)
+#define FLAG_PE     (1 << 26)
+#define FLAG_ME     (1 << 31)
+
+struct imx6_buf_desc {
+	uint16_t len;
+	uint16_t flags1;
+	uint32_t data_pointer;
+	uint32_t flags2;
+	uint16_t payload_checksum; /* Used only for RX */
+	uint8_t  protocol;         /* Used only for RX */
+	uint8_t  header_len;       /* Used only for RX */
+	uint16_t pad1;             /* Unused */
+	uint16_t flags3;           /* Used only for RX */
+	uint32_t timestamp;
+	uint16_t pad2[4];          /* Unused */
+} __attribute__ ((aligned(16)));
+
+static struct imx6_buf_desc _tx_desc_ring[TX_BUF_FRAMES];
+static struct imx6_buf_desc _rx_desc_ring[RX_BUF_FRAMES];
+
+static uint8_t _tx_buf[TX_BUF_LEN] __attribute__ ((aligned(16)));
+//static uint8_t _rx_buf[RX_BUF_LEN] __attribute__ ((aligned(16)));
+
 static int imx6_net_xmit(struct net_device *dev, struct sk_buff *skb) {
+	uint8_t *data;
+
+	assert(dev);
+	assert(skb);
+
+	data = (uint8_t*) skb_data_cast_in(skb->data);
+
+	if (!data)
+		return -1;
+
 	skb_free(skb);
+
+	memcpy(&_tx_buf[0], data, skb->len);
+
+	REG32_STORE(ENET_TDAR, 0);
+
 	return 0;
 }
 
@@ -83,6 +165,7 @@ static const struct net_driver imx6_net_drv_ops = {
 	.set_macaddr = imx6_net_set_macaddr
 };
 
+EMBOX_UNIT_INIT(imx6_net_init);
 static int imx6_net_init(void) {
 	struct net_device *nic;
 
@@ -95,11 +178,11 @@ static int imx6_net_init(void) {
 	REG32_STORE(ENET_ECR, RESET);
 
 	/* Setup TX and RX buffers */
-	assert((((uint32_t) &_tx_buf[0]) & 0xF) == 0);
-	REG32_STORE(ENET_TDSR, ((uint32_t) &_tx_buf[0]));
+	assert((((uint32_t) &_tx_desc_ring[0]) & 0xF) == 0);
+	REG32_STORE(ENET_TDSR, ((uint32_t) &_tx_desc_ring[0]));
 
-	assert((((uint32_t) &_rx_buf[0]) & 0xF) == 0);
-	REG32_STORE(ENET_RDSR, ((uint32_t) &_rx_buf[0]));
+	assert((((uint32_t) &_rx_desc_ring[0]) & 0xF) == 0);
+	REG32_STORE(ENET_RDSR, ((uint32_t) &_rx_desc_ring[0]));
 
 	assert((RX_BUF_LEN & 0xF) == 0);
 	REG32_STORE(ENET_MRBR, RX_BUF_LEN);
