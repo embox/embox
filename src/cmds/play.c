15f0c9a920a5f6ed6c7b43656eb896d63bae6e0b
Deryugin <deryugindenis@gmail.com>
2016-05-28 22:01:49 +0300

    (cmds) play: Implement callback for presudo-sinus

--- src/cmds/play.c
+++ src/cmds/play.c
@@ -18,11 +18,53 @@ static void print_usage(void) {
 	printf("Usage: play [WAVAUDIOFILE]\n");
 }
 
-int paCallback(const void *inputBuffer, void *outputBuffer,
+double _sin(double x) {
+	if (x > 0) {
+		while (x > 3.14)
+			x -= 3.14;
+	} else {
+		while (x < -3.14)
+			x += 3.14;
+	}
+	return x - x * x * x / 6. + x * x * x * x * x / 120.;
+}
+
+static int _sin_w = 10;
+static int _sin_h = 30000;
+
+int sinCallback(const void *inputBuffer, void *outputBuffer,
 		unsigned long framesPerBuffer,
 		const PaStreamCallbackTimeInfo* timeInfo,
 		PaStreamCallbackFlags statusFlags,
 		void *userData) {
+	FILE *file;
+	uint16_t *data;
+
+	data = outputBuffer;
+
+	for (int i = 0; i < framesPerBuffer; i++) {
+		double x = (i % _sin_w) / _sin_w * 3.14;
+		*data++ = (int) (_sin(x) * _sin_h); /* Left channel  */
+		*data++ = (int) (_sin(x) * _sin_h); /* Right channel */
+	}
+
+	return 0;
+}
+
+int fdCallback(const void *inputBuffer, void *outputBuffer,
+		unsigned long framesPerBuffer,
+		const PaStreamCallbackTimeInfo* timeInfo,
+		PaStreamCallbackFlags statusFlags,
+		void *userData) {
+        uint32_t read_bytes;
+	FILE *file;
+
+	file = userData;
+
+	read_bytes = framesPerBuffer * 2 * 2; /* Stereo 16-bit */
+
+	fread(outputBuffer, 1, read_bytes, file);
+
 	return 0;
 }
 
@@ -30,7 +72,7 @@ int main(int argc, char **argv) {
 	int opt;
 	int err;
 	FILE *fd;
-	static uint8_t snd_buf[128 * 1024];
+	static uint8_t fmt_buf[128];
 	int chan_n;
 	int sample_rate;
 	int bits_per_sample;
@@ -60,24 +102,24 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
-	fread(snd_buf, 128, 1024, fd);
-	if (raw_get_file_format(snd_buf) != RIFF_FILE) {
+	fread(fmt_buf, 1, 44, fd);
+	if (raw_get_file_format(fmt_buf) != RIFF_FILE) {
 		printf("%s is not a RIFF audio file\n", argv[argc - 1]);
 		return 0;
 	}
 
-	chan_n          = *((uint16_t*) &snd_buf[22]);
-	sample_rate     = *((uint32_t*) &snd_buf[24]);
-	bits_per_sample = *((uint16_t*) &snd_buf[34]);
+	chan_n          = *((uint16_t*) &fmt_buf[22]);
+	sample_rate     = *((uint32_t*) &fmt_buf[24]);
+	bits_per_sample = *((uint16_t*) &fmt_buf[34]);
 
-	printf("File size:             %d bytes\n", *((uint32_t*) &snd_buf[4]));
-	printf("File type header:      %c%c%c%c\n", snd_buf[8], snd_buf[9], snd_buf[10], snd_buf[11]);
-	printf("Length of format data: %d\n", *((uint32_t*) &snd_buf[16]));
-	printf("Type format:           %d\n", *((uint16_t*) &snd_buf[20]));
+	printf("File size:             %d bytes\n", *((uint32_t*) &fmt_buf[4]));
+	printf("File type header:      %c%c%c%c\n", fmt_buf[8], snd_buf[9], snd_buf[10], snd_buf[11]);
+	printf("Length of format data: %d\n", *((uint32_t*) &fmt_buf[16]));
+	printf("Type format:           %d\n", *((uint16_t*) &fmt_buf[20]));
 	printf("Number of channels:    %d\n", chan_n);
 	printf("Sample rate:           %d\n", sample_rate);
 	printf("Bits per sample:       %d\n", bits_per_sample);
-	printf("Size of data section:  %d\n", *((uint32_t*) &snd_buf[40]));
+	printf("Size of data section:  %d\n", *((uint32_t*) &fmt_buf[40]));
 
 	/* Initialize PA */
 	if (paNoError != (err = Pa_Initialize())) {
@@ -99,8 +141,8 @@ int main(int argc, char **argv) {
 			sample_rate,
 			256,
 			0,
-			paCallback,
-			&snd_buf[44]);
+			fdCallback,
+			fd);
 
 	if (err != paNoError) {
 		printf("Portaudio error: could not open stream!\n");
