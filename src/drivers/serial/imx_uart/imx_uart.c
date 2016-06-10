0692a02f39209b338ade87eb4bdbec4bbeb4b3ee
Deryugin <deryugin.denis@gmail.com>
2016-06-10 15:35:37 +0300

    (drivers) imx_uart: Temporary flag set disable
    Leave it to uboot for a while...

--- src/drivers/serial/imx_uart/imx_uart.c
+++ src/drivers/serial/imx_uart/imx_uart.c
@@ -13,7 +13,6 @@
 #define UART_BASE OPTION_GET(NUMBER, base_addr)
 #define UART(x)   (*(volatile unsigned long *)(UART_BASE + (x)))
 
-#define USR2       0x98
 #define USR2_RDR   1
 #define USR2_TXFE (1 << 14)
 
@@ -21,14 +20,22 @@
 #define TXR  0x40
 #define UCR1 0x80
 #define UCR2 0x84
+#define UCR3 0x88
+#define UCR4 0x8C
+#define UFCR 0x90
+#define USR2 0x98
+#define UBIR 0xA4
+#define UBMR 0xA8
 
 #define UCR1_UARTEN (1 << 0)
 #define UCR2_TXEN   (1 << 2)
 #define UCR2_RXEN   (1 << 1)
 
 static int imxuart_diag_init(const struct diag *diag) {
-	UART(UCR1)  = UCR1_UARTEN;
-	UART(UCR2) |= UCR2_RXEN | UCR2_TXEN;
+	//UART(UCR1) = UCR1_UARTEN;
+	//UART(UCR2) = 0x2127;//|= UCR2_RXEN | UCR2_TXEN;
+	//UART(UCR3) = 0x0704;
+	//UART(UCR4) = 0x7C00;
 	return 0;
 }
 
