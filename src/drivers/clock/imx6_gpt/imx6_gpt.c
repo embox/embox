65f63b4647aaf3652efb256abf398f4f5abbd9f9
Deryugin <deryugin.denis@gmail.com>
2016-06-12 16:22:56 +0300

    (drivers) imx6_gpt: Implement GPT init
    Some values are hardcoded, this would be fixed

--- src/drivers/clock/imx6_gpt/imx6_gpt.c
+++ src/drivers/clock/imx6_gpt/imx6_gpt.c
@@ -17,6 +17,8 @@
 #define GPT_BASE OPTION_GET(NUMBER, base_addr)
 #define GPT_IRQ  OPTION_GET(NUMBER, irq_nr)
 
+#define GPT_PRESCALER 0 /* 2^0 */
+
 #define GPT_CR   (GPT_BASE + 0x00) /* Control register */
 #define GPT_PR   (GPT_BASE + 0x04) /* Prescaler register */
 #define GPT_SR   (GPT_BASE + 0x08) /* Status register */
@@ -28,12 +30,49 @@
 #define GPT_ICR2 (GPT_BASE + 0x20) /* Input capture register 2 */
 #define GPT_CNT  (GPT_BASE + 0x24) /* Counter register */
 
+#define GPT_CR_EN     (1 <<  0)
+#define GPT_CR_ENMOD  (1 <<  1)
+#define GPT_CR_DBGEN  (1 <<  2)
+#define GPT_CR_WAITEN (1 <<  3)
+#define GPT_CR_DOZEEN (1 <<  4)
+#define GPT_CR_STOPEN (1 <<  5)
+#define GPT_CR_FRR    (1 <<  9)
+#define GPT_CR_SWR    (1 << 15)
+#define GPT_CR_FO1    (1 << 29)
+#define GPT_CR_FO2    (1 << 30)
+#define GPT_CR_FO3    (1 << 31)
+
+#define GPT_CR_CLKSRC_OFFT  6
+#define GPT_CR_IM1_OFFT    16
+#define GPT_CR_IM2_OFFT    18
+#define GPT_CR_OM1_OFFT    20
+#define GPT_CR_OM2_OFFT    23
+#define GPT_CR_OM3_OFFT    26
+
+#define GPT_IR_OF1IE (1 << 0)
+
+#define CLKSRC_PERIPH 1
+
 static struct clock_source imx6_gpt_clock_source;
 static irq_return_t clock_handler(unsigned int irq_nr, void *data) {
 	return IRQ_HANDLED;
 }
 
 static int imx6_gpt_init(void) {
+	uint32_t t;
+	REG32_STORE(GPT_CR, GPT_CR_SWR);
+
+	REG32_STORE(GPT_PR, GPT_PRESCALER);
+
+	REG32_STORE(GPT_OCR1, 1024); /* XXX */
+	/* Generate interrupt on OutputCompare1 overflow */
+	REG32_STORE(GPT_IR, GPT_IR_OF1IE);
+
+	t  = GPT_CR_EN;
+	t |= CLKSRC_PERIPH << GPT_CR_CLKSRC_OFFT;
+
+	REG32_STORE(GPT_CR, t);
+
 	return irq_attach(GPT_IRQ,
 	                  clock_handler,
 	                  0,
