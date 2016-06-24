8e249325150307e1caac22ef4187b90f0d51652c
Anton Bondarev <anton.bondarev2310@gmail.com>
2016-06-24 19:28:04 +0300

    drivers: audio: Make 'intel82801aa' as audio_dev

--- src/drivers/audio/intel82801aa.c
+++ src/drivers/audio/intel82801aa.c
@@ -9,17 +9,38 @@
 
 #include <unistd.h>
 
+#include <util/log.h>
+
 #include <asm/io.h>
 #include <drivers/pci/pci.h>
 #include <drivers/pci/pci_chip/pci_utils.h>
 #include <drivers/pci/pci_driver.h>
 #include <drivers/audio/portaudio.h>
+#include <drivers/audio/audio_dev.h>
 #include <drivers/audio/ac97.h>
 #include <kernel/irq.h>
-#include <kernel/printk.h>
-#include <mem/misc/pool.h>
 
-/* You may want to look at "Inte 82801AA (ICH) & Intel 82801AB(ICH0)
+
+
+struct intel_ac_hw_dev {
+	uint32_t base_addr;
+	uint32_t ctrl;
+	uint32_t sctrl;
+};
+
+struct intel_ac_dev_priv {
+	struct intel_ac_hw_dev *hw_dev;
+	int devid;
+	uint8_t *out_buf;
+	uint32_t out_buf_len;
+	uint8_t *in_buf;
+
+	uint32_t cur_buff_offset;
+};
+
+static struct intel_ac_hw_dev intel_ac_hw_dev;
+
+/* You may want to look at "Intel 82801AA (ICH) & Intel 82801AB(ICH0)
  * IO Controller Hub AC ’97 Programmer’s Reference Manual" for more
  * technical details */
 
@@ -58,24 +79,10 @@ static struct intel_ac_buff_desc pcm_out_buff_list[INTEL_AC_BUFFER_SZ];
 
 /* Some of this stuff probably shoud be placed into
  * separate module */
-#define MAX_BUF_LEN (INTEL_AC_SAMPLE_SZ * INTEL_AC_BUFFER_SZ)
-struct pa_strm {
-	int started;
-	int paused;
-	int completed;
-	int sample_format;
-	PaStreamCallback *callback;
-	void *callback_data;
-	size_t chan_buf_len;
-	uint8_t in_buf[MAX_BUF_LEN];
-	uint8_t out_buf[MAX_BUF_LEN];
-};
-
-#define STREAM_POOL_SZ 16
-POOL_DEF(pa_stream_pool, struct pa_strm, STREAM_POOL_SZ);
+#define INTEL_AC_MAX_BUF_LEN (INTEL_AC_SAMPLE_SZ * INTEL_AC_BUFFER_SZ)
 
 static uint32_t audio_base;
-
+#if 0
 static int intel_ac_buf_init(int n, PaStream *stream) {
 	pcm_out_buff_list[n] = (struct intel_ac_buff_desc) {
 		.pointer = (uint32_t)((struct pa_strm *)stream)->callback_data + 2 * 0xFF00 * n,
@@ -85,6 +92,7 @@ static int intel_ac_buf_init(int n, PaStream *stream) {
 	};
 	return 0;
 }
+#endif
 
 /* Intel Corporation 82801AA AC'97 Audio Controller,
  * provided by QEMU with flag -soundhw ac97 */
@@ -92,13 +100,16 @@ static int intel_ac_buf_init(int n, PaStream *stream) {
 #define INTEL_AC_PID 0x2415
 
 static irq_return_t iac_interrupt(unsigned int irq_num, void *dev_id) {
-	printk("New irq\n");
+	log_debug("irq_num = %d", irq_num);
+
 	return IRQ_HANDLED;
 }
 
 static int intel_ac_init(struct pci_slot_dev *pci_dev) {
 	int err;
 	assert(pci_dev);
+
+	pci_set_master(pci_dev);
 
 	audio_base = pci_dev->bar[1] & 0xFF00;
 	if ((err = ac97_init(pci_dev)))
@@ -112,86 +123,12 @@ static int intel_ac_init(struct pci_slot_dev *pci_dev) {
 
 PCI_DRIVER("Intel Corporation 82801AA AC'97 Audio Controller", intel_ac_init, INTEL_AC_VID, INTEL_AC_PID);
 
-/**
- * @brief Configure AC97 codec
- * @note This function does nothing, everything was done on intel_ac_init
- * @return PortAudio error code
- */
-PaError Pa_Initialize(void) {
-	return paNoError;
-}
 
-PaError Pa_Terminate(void) {
-	return paNoError;
-}
-
-PaHostApiIndex Pa_GetHostApiCount(void) {
-	return 1;
-}
-PaHostApiIndex Pa_GetDefaultHostApi(void) {
-	return 0;
-}
-
-PaDeviceIndex Pa_GetDeviceCount(void) {
-	return 1;
-}
-
-PaDeviceIndex Pa_GetDefaultInputDevice(void) {
-	return 0;
-}
-
-PaDeviceIndex Pa_GetDefaultOutputDevice(void) {
-	return 0;
-}
-
-/**
- * @brief Allocate and configure PaStream w/ given parameters
- * @note Now works only for audio output
- *
- * @param stream
- * @param inputParameters
- * @param outputParameters
- * @param sampleRate
- * @param framesPerBuffer
- * @param streamFlags
- * @param streamCallback
- * @param userData
- *
- * @return Error number
- */
-PaError Pa_OpenStream(PaStream** stream,
-                       const PaStreamParameters *inputParameters,
-                       const PaStreamParameters *outputParameters,
-                       double sampleRate,
-                       unsigned long framesPerBuffer,
-                       PaStreamFlags streamFlags,
-                       PaStreamCallback *streamCallback,
-                       void *userData) {
-	struct pa_strm *strm = pool_alloc(&pa_stream_pool);
-
-	if (!strm)
-		return paInternalError;
-
-	assert(outputParameters);
-
-	strm->started = 0;
-	strm->paused = 0;
-	strm->completed = 0;
-	strm->sample_format = outputParameters->sampleFormat;
-	strm->chan_buf_len = (MAX_BUF_LEN / framesPerBuffer) * framesPerBuffer;
-	strm->callback = streamCallback;
-	strm->callback_data = userData;
-
-	*stream = strm;
-
-	return paNoError;
-}
-
-PaError Pa_StartStream(PaStream *stream) {
+static void intel_ac_dev_start(struct audio_dev *dev) {
 	uint8_t tmp;
-	int i;
 
 	out32((uint32_t)&pcm_out_buff_list, audio_base + INTEL_AC_PO_BUF);
+#if 0
 	/* Setup buffers, currently just zeroes */
 	for (i = 0; i < INTEL_AC_BUFFER_SZ; i++) {
 		intel_ac_buf_init(i, stream);
@@ -200,7 +137,7 @@ PaError Pa_StartStream(PaStream *stream) {
 	for (i = 0; i < MAX_BUF_LEN; i++) {
 		((struct pa_strm*)stream)->out_buf[i] = 0xff;
 	}
-
+#endif
 	/* Setup Last Valid Index */
 	out8(INTEL_AC_BUFFER_SZ - 1, audio_base + INTEL_AC_PO_LVI);
 
@@ -209,37 +146,58 @@ PaError Pa_StartStream(PaStream *stream) {
 	tmp |= 0xf;
 	out8(tmp, audio_base + INTEL_AC_PO_CR);
 
-	return paNoError;
 }
 
-PaError Pa_StopStream(PaStream *stream) {
+static void intel_ac_dev_pause(struct audio_dev *dev) {
 	out8(0x0, audio_base + INTEL_AC_PO_CR);
-
-	return paNoError;
 }
 
-PaError Pa_CloseStream(PaStream *stream) {
-	pool_free(&pa_stream_pool, stream);
+static void intel_ac_dev_resume(struct audio_dev *dev) {
 
-	return paNoError;
 }
 
-void Pa_Sleep(long msec) {
-	usleep(msec * 1000);
+static void intel_ac_dev_stop(struct audio_dev *dev) {
+	out8(0x0, audio_base + INTEL_AC_PO_CR);
 }
 
-const PaDeviceInfo *Pa_GetDeviceInfo(PaDeviceIndex device) {
-	return 0;
-}
+static const struct audio_dev_ops intel_ac_dev_ops = {
+	.ad_ops_start = intel_ac_dev_start,
+	.ad_ops_pause = intel_ac_dev_pause,
+	.ad_ops_resume = intel_ac_dev_resume,
+	.ad_ops_stop = intel_ac_dev_stop,
+};
 
-const PaHostApiInfo *Pa_GetHostApiInfo(PaHostApiIndex hostApi) {
-	return 0;
-}
+static uint8_t dac1_out_buf[INTEL_AC_MAX_BUF_LEN] __attribute__ ((aligned(0x1000)));
 
-const PaStreamInfo *Pa_GetStreamInfo(PaStream *stream) {
-	return 0;
-}
+static struct intel_ac_dev_priv intel_ac_dac1 = {
+	.hw_dev = &intel_ac_hw_dev,
+	.devid  = 0,
+	.out_buf = dac1_out_buf,
+	.out_buf_len = sizeof(dac1_out_buf)
+};
 
-const char *Pa_GetErrorText(PaError errorCode) {
-	return "Pa_GetErrorText not implemented";
+static struct intel_ac_dev_priv intel_ac_dac2 = {
+	.hw_dev = &intel_ac_hw_dev,
+	.devid  = 1
+};
+
+static struct intel_ac_dev_priv intel_ac_adc1 = {
+	.hw_dev = &intel_ac_hw_dev,
+	.devid  = 2
+};
+
+AUDIO_DEV_DEF("intel_ac_dac1", (struct audio_dev_ops *)&intel_ac_dev_ops, &intel_ac_dac1);
+AUDIO_DEV_DEF("intel_ac_dac2", (struct audio_dev_ops *)&intel_ac_dev_ops, &intel_ac_dac2);
+AUDIO_DEV_DEF("intel_ac_adc1", (struct audio_dev_ops *)&intel_ac_dev_ops, &intel_ac_adc1);
+
+
+uint8_t *audio_dev_get_out_cur_ptr(struct audio_dev *audio_dev) {
+	struct intel_ac_dev_priv *priv;
+
+	priv = audio_dev->ad_priv;
+
+	priv->cur_buff_offset += audio_dev->samples_per_buffer *
+			audio_dev->num_of_chan * 2;
+	priv->cur_buff_offset %= priv->out_buf_len;
+	return &priv->out_buf[priv->cur_buff_offset];
 }
