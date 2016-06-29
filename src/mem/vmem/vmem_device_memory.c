631778ae08b4dc2f16d0ae113765f444977f6383
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2016-06-29 13:11:32 +0300

    mem: Free up physical memory in munmap

--- src/mem/vmem/vmem_device_memory.c
+++ src/mem/vmem/vmem_device_memory.c
@@ -12,6 +12,7 @@
 #include <kernel/task/resource/mmap.h>
 #include <mem/mapping/marea.h>
 #include <mem/mmap.h>
+#include <mem/phymem.h>
 
 extern int mmap_kernel_inited(void);
 
@@ -76,6 +77,11 @@ int munmap(void *addr, size_t size) {
 
 	if (mmap_kernel_inited()) {
 		mmap_do_marea_unmap(emmap, marea);
+	}
+
+	if (marea->is_allocated) {
+		int pages = (size + MMU_PAGE_SIZE - 1) / MMU_PAGE_SIZE;
+		phymem_free((void *)(marea->start), pages);
 	}
 
 	marea_destroy(marea);
