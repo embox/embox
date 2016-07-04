2dfa05ceda7ff2498393c655bf57e45d92c7c19a
Anton Bondarev <anton.bondarev2310@gmail.com>
2016-07-04 18:40:26 +0300

    drivers: Add ttyS0 for imx_uart

--- src/drivers/serial/imx_uart/imx_uart.c
+++ src/drivers/serial/imx_uart/imx_uart.c
@@ -6,12 +6,16 @@
  */
 
 #include <drivers/diag.h>
+#include <drivers/serial/uart_device.h>
+#include <drivers/serial/diag_serial.h>
+#include <embox/unit.h>
 #include <framework/mod/options.h>
-#include <hal/reg.h>
-#include <kernel/printk.h>
+
+EMBOX_UNIT_INIT(uart_init);
 
 #define UART_BASE OPTION_GET(NUMBER, base_addr)
 #define UART(x)   (*(volatile unsigned long *)(UART_BASE + (x)))
+#define IRQ_NUM OPTION_GET(NUMBER, irq_num)
 
 #define USR2_RDR   1
 #define USR2_TXFE (1 << 14)
@@ -31,7 +35,7 @@
 #define UCR2_TXEN   (1 << 2)
 #define UCR2_RXEN   (1 << 1)
 
-static int imxuart_diag_init(const struct diag *diag) {
+static int imxuart_setup(struct uart *dev, const struct uart_params *params) {
 	//UART(UCR1) = UCR1_UARTEN;
 	//UART(UCR2) = 0x2127;//|= UCR2_RXEN | UCR2_TXEN;
 	//UART(UCR3) = 0x0704;
@@ -39,24 +43,60 @@ static int imxuart_diag_init(const struct diag *diag) {
 	return 0;
 }
 
-static int imxuart_diag_hasrx(const struct diag *diag) {
+static int imxuart_has_symbol(struct uart *dev) {
 	return UART(USR2) & USR2_RDR;
 }
 
-static char imxuart_diag_getc(const struct diag *diag) {
+static int imxuart_getc(struct uart *dev) {
 	return (char)UART(RXR);
 }
 
-static void imxuart_diag_putc(const struct diag *diag, char ch) {
+static int imxuart_putc(struct uart *dev, int ch) {
 	while (!(UART(USR2) & USR2_TXFE)) {
 	}
 
 	UART(TXR) = ch;
+
+	return 0;
 }
 
-DIAG_OPS_DECLARE(
-	.init = imxuart_diag_init,
-	.putc = imxuart_diag_putc,
-	.getc = imxuart_diag_getc,
-	.kbhit = imxuart_diag_hasrx,
-);
+static const struct uart_ops imxuart_uart_ops = {
+		.uart_getc = imxuart_getc,
+		.uart_putc = imxuart_putc,
+		.uart_hasrx = imxuart_has_symbol,
+		.uart_setup = imxuart_setup,
+};
+
+static struct uart uart0 = {
+		.uart_ops = &imxuart_uart_ops,
+		.irq_num = IRQ_NUM,
+		.base_addr = UART_BASE,
+};
+
+static const struct uart_params uart_defparams = {
+		.baud_rate = OPTION_GET(NUMBER,baud_rate),
+		.parity = 0,
+		.n_stop = 1,
+		.n_bits = 8,
+		.irq = true,
+};
+
+static const struct uart_params uart_diag_params = {
+		.baud_rate = OPTION_GET(NUMBER,baud_rate),
+		.parity = 0,
+		.n_stop = 1,
+		.n_bits = 8,
+		.irq = false,
+};
+
+const struct uart_diag DIAG_IMPL_NAME(__EMBUILD_MOD__) = {
+		.diag = {
+			.ops = &uart_diag_ops,
+		},
+		.uart = &uart0,
+		.params = &uart_diag_params,
+};
+
+static int uart_init(void) {
+	return uart_register(&uart0, &uart_defparams);
+}
