49b4af5252e33053634b99e2832ad8cd415f637c
Anton Bondarev <anton.bondarev2310@gmail.com>
2016-07-22 18:40:05 +0300

    drivers: Work on imx6_net driver

--- src/drivers/net/imx6_net/imx6_net.c
+++ src/drivers/net/imx6_net/imx6_net.c
@@ -38,10 +38,29 @@ struct fec_priv {
 
 	int _cur_rx;
 	int _dirty_tx;
-	int _cur_tx;
 };
 
 static struct fec_priv fec_priv;
+
+#define DEBUG 1
+#if DEBUG
+#include <kernel/printk.h>
+/* Debugging routines */
+static inline void show_packet(uint8_t *raw, int size, char *title) {
+	int i;
+
+	printk("\nPACKET(%d) %s:", size, title);
+	for (i = 0; i < size; i++) {
+		if (!(i % 16)) {
+			printk("\n");
+		}
+		printk(" %02hhX", *(raw + i));
+	}
+	printk("\n.\n");
+}
+#else
+#define show_packet(raw, size,title)
+#endif
 
 static void _reg_dump(void) {
 	log_debug("ENET_EIR  %10x", REG32_LOAD(ENET_EIR ));
@@ -66,64 +85,37 @@ static void _reg_dump(void) {
 
 static uint32_t _uboot_regs[0x200];
 static void _mem_dump(void) {
-	//_uboot_regs[ENET_EIR  >> 2] = REG32_LOAD(ENET_EIR );
-	//_uboot_regs[ENET_EIMR >> 2] = REG32_LOAD(ENET_EIMR);
-	//_uboot_regs[ENET_RDAR >> 2] = REG32_LOAD(ENET_RDAR);
-	//_uboot_regs[ENET_TDAR >> 2] = REG32_LOAD(ENET_TDAR);
 	_uboot_regs[0] = REG32_LOAD(ENET_ECR );
 	_uboot_regs[1] = REG32_LOAD(ENET_MSCR);
 	_uboot_regs[2] = REG32_LOAD(ENET_RCR );
-	//_uboot_regs[ENET_TCR  >> 2] = REG32_LOAD(ENET_TCR );
-	//_uboot_regs[MAC_LOW   >> 2] = REG32_LOAD(MAC_LOW  );
-	//_uboot_regs[MAC_HI    >> 2] = REG32_LOAD(MAC_HI   );
-	//_uboot_regs[ENET_IAUR >> 2] = REG32_LOAD(ENET_IAUR);
-	//_uboot_regs[ENET_IALR >> 2] = REG32_LOAD(ENET_IALR);
-	//_uboot_regs[ENET_GAUR >> 2] = REG32_LOAD(ENET_GAUR);
-	//_uboot_regs[ENET_GALR >> 2] = REG32_LOAD(ENET_GALR);
-	//_uboot_regs[ENET_TFWR >> 2] = REG32_LOAD(ENET_TFWR);
-	//_uboot_regs[ENET_RDSR >> 2] = REG32_LOAD(ENET_RDSR);
-	//_uboot_regs[ENET_TDSR >> 2] = REG32_LOAD(ENET_TDSR);
-	//_uboot_regs[ENET_MRBR >> 2] = REG32_LOAD(ENET_MRBR);
 }
 
 static void _mem_restore(void) {
-	//REG32_LOAD(ENET_EIR ) = _uboot_regs[ENET_EIR  >> 2];
-	//REG32_LOAD(ENET_EIMR) = _uboot_regs[ENET_EIMR >> 2];
-	//REG32_LOAD(ENET_RDAR) = _uboot_regs[ENET_RDAR >> 2];
-	//REG32_LOAD(ENET_TDAR) = _uboot_regs[ENET_TDAR >> 2];
 	REG32_STORE(ENET_ECR , _uboot_regs[0] & ~ETHEREN);
 	REG32_STORE(ENET_MSCR, _uboot_regs[1]);
 	REG32_STORE(ENET_RCR , _uboot_regs[2]);
-	//REG32_LOAD(ENET_TCR ) = _uboot_regs[ENET_TCR  >> 2];
-	//REG32_LOAD(MAC_LOW  ) = _uboot_regs[MAC_LOW   >> 2];
-	//REG32_LOAD(MAC_HI   ) = _uboot_regs[MAC_HI    >> 2];
-	//REG32_LOAD(ENET_IAUR) = _uboot_regs[ENET_IAUR >> 2];
-	//REG32_LOAD(ENET_IALR) = _uboot_regs[ENET_IALR >> 2];
-	//REG32_LOAD(ENET_GAUR) = _uboot_regs[ENET_GAUR >> 2];
-	//REG32_LOAD(ENET_GALR) = _uboot_regs[ENET_GALR >> 2];
-	//REG32_LOAD(ENET_TFWR) = _uboot_regs[ENET_TFWR >> 2];
-	//REG32_LOAD(ENET_RDSR) = _uboot_regs[ENET_RDSR >> 2];
-	//REG32_LOAD(ENET_TDSR) = _uboot_regs[ENET_TDSR >> 2];
-	//REG32_LOAD(ENET_MRBR) = _uboot_regs[ENET_MRBR >> 2];
 }
 
-static void emac_set_macaddr(unsigned char (*_macaddr)[6]) {
+static void emac_set_macaddr(unsigned char _macaddr[6]) {
 	uint32_t mac_hi, mac_lo;
+	const uint8_t *tmp = _macaddr;
 
 	REG32_STORE(ENET_IAUR, 0);
 	REG32_STORE(ENET_IALR, 0);
 	REG32_STORE(ENET_GAUR, 0);
 	REG32_STORE(ENET_GALR, 0);
 
-	mac_hi  = (*_macaddr[5] << 16) |
-	          (*_macaddr[4] << 24);
-	mac_lo  = (*_macaddr[3] <<  0) |
-	          (*_macaddr[2] <<  8) |
-	          (*_macaddr[1] << 16) |
-	          (*_macaddr[0] << 24);
+	log_debug("addr = %x:%x:%x:%x:%x:%x", tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5]);
+
+	mac_hi  = (_macaddr[5] << 16) |
+	          (_macaddr[4] << 24);
+	mac_lo  = (_macaddr[3] <<  0) |
+	          (_macaddr[2] <<  8) |
+	          (_macaddr[1] << 16) |
+	          (_macaddr[0] << 24);
 
 	REG32_STORE(MAC_LOW, mac_lo);
-	REG32_STORE(MAC_HI, mac_hi);
+	REG32_STORE(MAC_HI, mac_hi | 0x8808);
 }
 
 static struct imx6_buf_desc _tx_desc_ring[TX_BUF_FRAMES] __attribute__ ((aligned(0x10)));
@@ -131,7 +123,6 @@ static struct imx6_buf_desc _rx_desc_ring[RX_BUF_FRAMES] __attribute__ ((aligned
 
 static int _cur_rx = 0;
 static int _dirty_tx = 0;
-static int _cur_tx = 0;
 
 static uint8_t _tx_buf[TX_BUF_FRAMES][2048] __attribute__ ((aligned(0x10)));
 static uint8_t _rx_buf[RX_BUF_FRAMES][2048] __attribute__ ((aligned(0x10)));
@@ -141,13 +132,15 @@ extern void dcache_flush(const void *p, size_t size);
 
 static void fec_tbd_init(struct fec_priv *fec)
 {
-	unsigned size = 2 * sizeof(struct imx6_buf_desc);
+	unsigned size = TX_BUF_FRAMES * sizeof(struct imx6_buf_desc);
 
 	memset(fec->tbd_base, 0, size);
-	fec->tbd_base[0].flags1 = 0;
-	fec->tbd_base[1].flags1 = FLAG_W;
+
+	fec->tbd_base[TX_BUF_FRAMES - 1].flags1 = FLAG_W;
+
 	fec->tbd_index = 0;
 	dcache_flush(fec->tbd_base, size);
+
 	REG32_STORE(ENET_TDSR, ((uint32_t) fec->tbd_base));
 }
 
@@ -182,106 +175,117 @@ static void fec_rbd_init(struct fec_priv *fec, int count, int dsize) {
 static int imx6_net_xmit(struct net_device *dev, struct sk_buff *skb) {
 	uint8_t *data;
 	struct imx6_buf_desc *desc;
-
-	log_debug("Transmitting packet %2d", _cur_tx);
-
+	int res;
 	ipl_t sp;
+	struct fec_priv *priv;
+	int cur_tx_desc;
 
 	assert(dev);
 	assert(skb);
 
+	res = 0;
+
+	data = (uint8_t*) skb_data_cast_in(skb->data);
+
+	if (!data) {
+		log_error("No skb data!\n");
+		res = -1;
+		goto out;
+
+	}
+	show_packet(data, skb->len, "tx");
+
+	priv = dev->priv;
+
 	sp = ipl_save();
 	{
-		//REG32_STORE(ENET_TCR, (1 << 2));
-		data = (uint8_t*) skb_data_cast_in(skb->data);
+		cur_tx_desc = priv->tbd_index;
+		log_debug("Transmitting packet %2d", cur_tx_desc);
 
-		if (!data) {
-			log_error("No skb data!\n");
-			ipl_restore(sp);
-			return -1;
+		memcpy(&_tx_buf[cur_tx_desc][0], data, skb->len);
+		dcache_flush(&_tx_buf[cur_tx_desc][0], skb->len);
+		//dcache_flush(data, skb->len);
+
+		desc = &_tx_desc_ring[cur_tx_desc];
+		dcache_inval(desc, sizeof(struct imx6_buf_desc));
+		if (desc->flags1 & FLAG_R) {
+			log_error("tx desc still busy");
+
 		}
-		memset(&_tx_buf[_cur_tx][0], 0, 2048);
-		memcpy(&_tx_buf[_cur_tx][0], data, skb->len);
-		dcache_flush(&_tx_buf[_cur_tx][0], skb->len);
-
-		desc = &_tx_desc_ring[_cur_tx];
-		desc->data_pointer = (uint32_t) &_tx_buf[_cur_tx][0];
-		desc->len          = skb->len;
-		desc->flags1       = FLAG_L | FLAG_TC;
-
-		skb_free(skb);
-
-		if (_cur_tx == TX_BUF_FRAMES - 1)
-			desc->flags1 |= FLAG_W;
-
-		desc->flags1 |= FLAG_R;
-
+		desc->data_pointer = (uint32_t)&_tx_buf[cur_tx_desc][0];
+		//desc->data_pointer = (uint32_t)data;
+		//desc->len          = skb->len;
+		desc->len          = 256;
+		desc->flags1       |= FLAG_L | FLAG_TC | FLAG_R;
 		dcache_flush(desc, sizeof(struct imx6_buf_desc));
+		log_debug("desc = %x", desc);
+		log_debug("->data_pointer = %x", desc->data_pointer);
+		log_debug("->len = %x", desc->len);
+		log_debug("->flags1 = %x", desc->flags1);
 
-		REG32_STORE(ENET_TDAR, 0xFFFFFFFF);
+		REG32_LOAD(desc + sizeof(*desc) - 4);
+
+		REG32_STORE(ENET_TDAR, 1 << 24);
 
 		int timeout = 0xFF;
-		while(timeout--) {
-			if (!(REG32_LOAD(ENET_TDAR)))
+		while(--timeout) {
+			if (!(REG32_LOAD(ENET_TDAR))) {
 				break;
+			}
+			log_debug("ENET_TDAR not zero");
 		}
 
-		if (timeout == 0)
-			log_debug("TX timeout...");
+		log_debug("TX timeout %d", timeout);
+		if (timeout == 0) {
+			log_debug("TX timeout ENET_TDAR is not zero...");
+		}
 
-		//REG32_STORE(ENET_TCR, (1 << 2) | 1);
+		timeout = 0xFFFFFF;
+		while(--timeout) {
+			dcache_inval(desc, sizeof(struct imx6_buf_desc));
+			if (!(desc->flags1 & FLAG_R)) {
+				log_debug("************\n->data_pointer = %x", desc->data_pointer);
+				log_debug("->len = %x", desc->len);
+				log_debug("->flags1 = %x", desc->flags1);
+				break;
+			}
+		}
+		if (timeout == 0) {
+			log_debug("TX timeout bit READY still set...");
+		}
+		log_debug("TX timeout %d", timeout);
+
+		priv->tbd_index = (priv->tbd_index + 1) % TX_BUF_FRAMES;
 	}
 	ipl_restore(sp);
 
-	_cur_tx = (_cur_tx + 1) % TX_BUF_FRAMES;
+out:
+	skb_free(skb);
 
-	return 0;
+	return res;
 }
 
 static void _init_buffers(void) {
 	int i;
 
-#if 0
-	struct imx6_buf_desc *desc;
-
-	memset(&_tx_desc_ring[0], 0,
-	        TX_BUF_FRAMES * sizeof(struct imx6_buf_desc));
-	memset(&_rx_desc_ring[0], 0,
-	        RX_BUF_FRAMES * sizeof(struct imx6_buf_desc));
-
-	/* Mark last buffer (i.e. set wrap flag) */
-	_rx_desc_ring[RX_BUF_FRAMES - 1].flags1 = FLAG_W;
-	_tx_desc_ring[TX_BUF_FRAMES - 1].flags1 = FLAG_W;
-#endif
 	for (i = 0; i < RX_BUF_FRAMES; i++) {
 		_rx_desc_ring[i].data_pointer = (uint32_t)&_rx_buf[i][0];
 	}
+#if 0
 	for (i = 0; i < TX_BUF_FRAMES; i++) {
 		_tx_desc_ring[i].data_pointer = (uint32_t)& _tx_buf[i][0];
 	}
-#if 0
-	dcache_flush(&_tx_desc_ring[0],
-	              TX_BUF_FRAMES * sizeof(struct imx6_buf_desc));
-	dcache_flush(&_rx_desc_ring[0],
-	              RX_BUF_FRAMES * sizeof(struct imx6_buf_desc));
-
-	assert((((uint32_t) &_rx_desc_ring[0]) & 0xF) == 0);
-	REG32_STORE(ENET_RDSR, ((uint32_t) &_rx_desc_ring[0]));
-
-	assert((((uint32_t) &_tx_desc_ring[0]) & 0xF) == 0);
-	REG32_STORE(ENET_TDSR, ((uint32_t) &_tx_desc_ring[0]));
-
-	assert((RX_BUF_LEN & 0xF) == 0);
-	REG32_STORE(ENET_MRBR, 0x600);
 #endif
 }
 
 static void _reset(struct net_device *dev) {
 	int cnt = 0;
 
-	log_debug("ENET dump...\n");
+	log_debug("ENET dump uboot...\n");
+	_reg_dump();
+
 	_mem_dump();
-	log_debug("ENET reset...\n");
+
 	REG32_STORE(ENET_ECR, RESET);
 	while(REG32_LOAD(ENET_ECR) & RESET){
 		if (cnt ++ > 100000) {
@@ -289,15 +293,13 @@ static void _reset(struct net_device *dev) {
 			break;
 		}
 	}
-	log_debug("ENET restore");
 	//REG32_STORE(ENET_ECR, 0xF0000100);
+
 	_mem_restore();
-	log_debug("ENET restore end");
+
 	_init_buffers();
-	log_debug("fec_rbd_init");
 	fec_rbd_init(dev->priv, RX_BUF_FRAMES, 0x600);
-	REG32_STORE(ENET_RDAR, (1 << 24));
-	log_debug("fec_tbd_init");
+
 	fec_tbd_init(dev->priv);
 
 
@@ -310,17 +312,18 @@ static void _reset(struct net_device *dev) {
 	 * Receive Frame Interrupt
 	 * Receive Buffer Interrupt
 	 */
-	REG32_STORE(ENET_EIMR, 0xFFFFFFFF);
+	REG32_STORE(ENET_EIMR, 0x00000000);
 	/*
 	 * Clear FEC-Lite interrupt event register(IEVENT)
 	 */
-	REG32_STORE(ENET_EIR, 0xFFFFFFFF);
+	REG32_STORE(ENET_EIR, 0xffc00000);
 
 	/* Full-Duplex Enable */
 	 REG32_STORE(ENET_TCR, (1 << 2));
 
 	 /* Transmit FIFO Write 64 bytes */
-	REG32_STORE(ENET_TFWR, 0x0);
+	REG32_STORE(ENET_TFWR, 0x100);
+	//REG32_STORE(ENET_TFWR, 0x0);
 
 	//uint32_t t = 0x08000124;
 	/* MAX_FL frame length*/
@@ -335,8 +338,10 @@ static void _reset(struct net_device *dev) {
 	 */
 	REG32_STORE(ENET_MRBR, 0x600);
 
+	REG32_STORE(ENET_ECR, REG32_LOAD(ENET_ECR) | ETHEREN); /* Note: should be last ENET-related init step */
 
-	REG32_STORE(ENET_RCR, REG32_LOAD(ENET_ECR) | ETHEREN); /* Note: should be last ENET-related init step */
+	REG32_STORE(ENET_RDAR, (1 << 24));
+	log_debug("ENET dump embox...\n");
 	_reg_dump();
 }
 
@@ -350,7 +355,11 @@ static int imx6_net_set_macaddr(struct net_device *dev, const void *addr) {
 	assert(dev);
 	assert(addr);
 
-	emac_set_macaddr((unsigned char (*)[6])addr);
+	const uint8_t *tmp = addr;
+	log_debug("addr = %x:%x:%x:%x:%x:%x", tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5]);
+	_reset(dev);
+	emac_set_macaddr((unsigned char *)addr);
+	_reg_dump();
 
 	return 0;
 }
