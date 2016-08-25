09c8cec8adb55ee44f779946db0f86dff067ba86
Denis Deryugin <deryugin.denis@gmail.com>
2016-08-25 19:55:21 +0300

    arm/mmu: Implement small page mode

---
+++ src/arch/arm/mmu/mmu_common.c
@@ -0,0 +1,117 @@
+/**
+ * @file mmu.c
+ * @brief
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version
+ * @date 2015-08-18
+ */
+
+#include <string.h>
+
+#include <asm/hal/mmu.h>
+#include <asm/regs.h>
+#include <embox/unit.h>
+#include <hal/mmu.h>
+#include <mem/vmem.h>
+
+#include <framework/mod/options.h>
+#include <kernel/printk.h>
+
+EMBOX_UNIT_INIT(mmu_init);
+
+#define DOMAIN_ACCESS OPTION_GET(NUMBER, domain_access)
+#define CTX_NUMBER    32 /* TODO: make it related to number of tasks */
+
+/**
+ * @brief Fill translation table and so on
+ * @note Assume MMU is off right now
+ *
+ * @return
+ */
+static int mmu_init(void) {
+	__asm__ __volatile__ (
+		/* setup c3, Domain Access Control Register */
+#if DOMAIN_ACCESS == 1
+		"mov r0, #0x55\n\t" /* Client for all domains */
+#elif DOMAIN_ACCESS == 3
+		"mov r0, #0xff\n\t" /* Manager for all domains */
+#else
+#error Given domain access level is not supported
+#endif
+		"orr r0, r0, lsl #8\n\t"
+		"orr r0, r0, lsl #16\n\t"
+		"mcr p15, 0, r0, c3, c0, 0\n\t"
+		: :
+	);
+
+	return 0;
+}
+
+/**
+* @brief Turn MMU on
+*
+* @note Set flag CR_M at c1, the control register
+*/
+void mmu_on(void) {
+#ifndef NOMMU
+	__asm__ __volatile__ (
+		"mrc p15, 0, r0, c1, c0, 0\n\t"
+		"orr r0, r0, %[flag]\n\t" /* enabling MMU */
+		"mcr p15, 0, r0, c1, c0, 0"
+		: : [flag] "I" (CR_M)
+	);
+#endif
+}
+
+/**
+* @brief Turn MMU off
+*
+* @note Clear flag CR_M at c1, the control register
+*/
+void mmu_off(void) {
+#ifndef NOMMU
+	__asm__ __volatile__ (
+		"mrc p15, 0, r0, c1, c0, 0\n\t"
+		"bic r0, r0, %[flag]\n\t"
+		"mcr p15, 0, r0, c1, c0, 0"
+		: : [flag] "I" (CR_M)
+	);
+#endif
+}
+
+void mmu_flush_tlb(void) {
+	/* NIY */
+}
+
+mmu_vaddr_t mmu_get_fault_address(void) {
+	mmu_vaddr_t val;
+
+	__asm__ __volatile__ (
+		"mrc p15, 0, %[out], c6, c0, 0" : [out] "=r" (val) :
+	);
+
+	return val;
+}
+
+mmu_ctx_t mmu_create_context(mmu_pgd_t *pgd) {
+	return (mmu_ctx_t) pgd;
+}
+
+void mmu_set_context(mmu_ctx_t ctx) {
+	__asm__ __volatile__ (
+		"mcr p15, 0, %[addr], c2, c0, 0\n\t"
+		: [addr] "=r" (ctx) :
+	);
+}
+
+/**
+ * @brief Get address of first translation level
+ * @note XXX We have the same ctx for all tasks
+ *
+ * @param ctx
+ *
+ * @return Pointer to translation table
+ */
+mmu_pgd_t *mmu_get_root(mmu_ctx_t ctx) {
+	return (void*) ctx;
+}
