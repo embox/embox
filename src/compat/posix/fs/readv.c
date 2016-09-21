d73c4c2a0c55abedcbc9333b1b779acef0868b58
Anton Bondarev <anton.bondarev2310@gmail.com>
2016-09-21 17:01:40 +0300

    compat: Rewrite readv()

--- src/compat/posix/fs/readv.c
+++ src/compat/posix/fs/readv.c
@@ -6,26 +6,34 @@
  * @author Denis Chusovitin
  */
 
-#include "vec_common.h"
+#include <errno.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+
+#include <fs/index_descriptor.h>
+#include <fs/idesc.h>
+#include <kernel/task/resource/idesc_table.h>
 
 ssize_t readv(int fd, const struct iovec *iov, int iovcnt) {
 	int ret;
-	struct msghdr msg;
-	struct sock *sk;
+	struct idesc *idesc;
 
 	if (iovcnt < 0) {
 		return SET_ERRNO(EINVAL);
 	}
 
-	socket_idesc_check(fd, sk);
+	if (!idesc_index_valid(fd)
+			|| (NULL == (idesc = index_descriptor_get(fd)))
+			|| (!(idesc->idesc_amode & S_IROTH))) {
+		return SET_ERRNO(EBADF);
+	}
 
-	msg.msg_name = NULL;
-	msg.msg_namelen = 0;
-	msg.msg_iov = (struct iovec*)iov;
-	msg.msg_iovlen = iovcnt;
-	msg.msg_flags = 0;
+	assert(idesc->idesc_ops);
+	assert(idesc->idesc_ops->id_readv);
 
-	ret = krecvmsg(sk, &msg, sk->idesc.idesc_flags);
+	ret = idesc->idesc_ops->id_readv(idesc, iov, iovcnt);
 
 	if (ret < 0) {
 		return SET_ERRNO(-ret);
