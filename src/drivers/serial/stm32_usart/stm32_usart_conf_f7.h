f128bb3ca93a69c74fd70ddb2283707ee4477391
Alex Kalmuk <alexkalmuk@gmail.com>
2016-12-13 17:02:34 +0300

    stm32f7: Enable usart

--- src/drivers/serial/stm32_usart/stm32_usart_conf_f7.h
+++ src/drivers/serial/stm32_usart/stm32_usart_conf_f7.h
@@ -58,6 +58,27 @@
 /* Definition for USARTx's NVIC */
 #define USARTx_IRQn                      USART2_IRQn + 16
 #define USARTx_IRQHandler                USART2_IRQHandler
+
+#elif MODOPS_USARTX == 1
+#define USARTx                           USART1
+#define USARTx_CLK_ENABLE()              __HAL_RCC_USART1_CLK_ENABLE();
+#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOB_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
+
+#define USARTx_FORCE_RESET()             __HAL_RCC_USART1_FORCE_RESET()
+#define USARTx_RELEASE_RESET()           __HAL_RCC_USART1_RELEASE_RESET()
+
+/* Definition for USARTx Pins */
+#define USARTx_TX_PIN                    GPIO_PIN_9
+#define USARTx_TX_GPIO_PORT              GPIOA
+#define USARTx_TX_AF                     GPIO_AF7_USART1
+#define USARTx_RX_PIN                    GPIO_PIN_7
+#define USARTx_RX_GPIO_PORT              GPIOB
+#define USARTx_RX_AF                     GPIO_AF7_USART1
+
+/* Definition for USARTx's NVIC */
+#define USARTx_IRQn                      USART1_IRQn + 16
+#define USARTx_IRQHandler                USART1_IRQHandler
 #else
 #error Unsupported USARTx
 #endif
