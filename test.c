99378aa840c9b1fe80a1754505c4b4029de51607
Anton Kozlov <drakon.mega@gmail.com>
2017-01-07 17:08:23 +0300

    Very WIP

---
+++ test.c
@@ -0,0 +1,8 @@
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx(aaaaaaaaaa, aaaaaaaa, aaaaaaaaaaaaaaa, aaaaaaa, aaaaaaaaaaaaaaaaaaaaaaaaaa);
+
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx(aaaaaaaaaa, aaaaaaaa, aaaaaaaaaaaaaaa, aaaaaaa, aaaaaaaaaaaaaaaaaaaaaaaaaaa);
+
+int schedee_init(struct schedee *schedee, int priority,
+		struct schedee *(*process)(
+				struct schedee *prev, struct schedee *next)) {
+}
