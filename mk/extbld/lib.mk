183395fd5c02d6238291c0cf3fefa81d8f382278
kakadu <kakadu.hafanana@gmail.com>
2017-01-09 21:06:41 +0300

    More quite unzipping

--- mk/extbld/lib.mk
+++ mk/extbld/lib.mk
@@ -78,7 +78,7 @@ EXTRACT  := $(BUILD_DIR)/.extracted
 extract : $(EXTRACT)
 $(EXTRACT): | $(DOWNLOAD_DIR) $(BUILD_DIR)
 	$(foreach i,$(sources_extract),\
-		$(if $(filter %zip,$i),unzip $(DOWNLOAD_DIR)/$i -d $(BUILD_DIR),\
+		$(if $(filter %zip,$i),unzip -q $(DOWNLOAD_DIR)/$i -d $(BUILD_DIR),\
 			mkdir -p $(BUILD_DIR) && ( cd $(BUILD_DIR); tar -xf $(DOWNLOAD_DIR)/$i) );)
 	COPY_FILES="$(addprefix $(DOWNLOAD_DIR)/, \
 			$(call targets_git,$(sources_git)) \
