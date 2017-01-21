a504bf842aa10d09cb156348cbe11628521ce067
Anton Bondarev <anton.bondarev2310@gmail.com>
2017-01-21 16:53:56 +0300

    drivers: Start working on dwapb_gpio driver

---
+++ src/drivers/gpio/dwapb/dwapb_gpio.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ *
+ * @date 20.01.2017
+ * @author Anton Bondarev
+ */
+#include <drivers/gpio.h>
+
+
+void gpio_set_level(struct gpio *gpio, gpio_mask_t mask, char level) {
+
+}
+
+gpio_mask_t gpio_get_level(struct gpio *gpio, gpio_mask_t mask) {
+	return 0;
+}
---
+++ src/drivers/gpio/dwapb/dwapb_gpio.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ *
+ * @date 21.01.2017
+ * @author Anton Bondarev
+ */
+
+#ifndef DWAPB_GPIO_H_
+#define DWAPB_GPIO_H_
+
+#include <stdint.h>
+
+typedef uint32_t __gpio_mask_t;
+
+#endif /* DWAPB_GPIO_H_ */
