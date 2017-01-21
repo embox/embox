afeb3a936b23900382f26633d68bb99d97661ccc
Anton Bondarev <anton.bondarev2310@gmail.com>
2017-01-21 18:36:03 +0300

    drivers: Work on dwapb_gpio driver

--- src/drivers/gpio/dwapb/dwapb_gpio.c
+++ src/drivers/gpio/dwapb/dwapb_gpio.c
@@ -4,8 +4,17 @@
  * @date 20.01.2017
  * @author Anton Bondarev
  */
+#include <stddef.h>
 #include <drivers/gpio.h>
 
+
+struct gpio *gpio_by_num(int num_port) {
+	return NULL;
+}
+
+int gpio_settings(struct gpio *gpio, gpio_mask_t mask, int mode) {
+	return 0;
+}
 
 void gpio_set_level(struct gpio *gpio, gpio_mask_t mask, char level) {
 
