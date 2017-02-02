5e35b101a65b6fcb8fb9ef4b699c5020c6551411
Anton Bondarev <anton.bondarev2310@gmail.com>
2017-02-02 19:29:25 +0300

    scripts: Update app_diff.sh
    Working on zip archives

--- scripts/app_diff.sh
+++ scripts/app_diff.sh
@@ -18,3 +18,10 @@ mkdir -p $APP_NAME-orig
 tar xzf $EMBOX_DIR/download/${APP_NAME}.tar.gz -C $APP_NAME-orig --strip-components=1
 diff -aur -x configure $APP_NAME-orig $APP_NAME | grep -v "^Only in" | tee $SRC_APP_DIR/patch.txt
 rm -rf $APP_NAME-orig
+
+#ORIGIN_DIR=../$APP_NAME-orig
+#cd $EXTBLD_APP_DIR
+#mkdir -p $ORIGIN_DIR
+#unzip $EMBOX_DIR/download/${APP_NAME}.zip -d $ORIGIN_DIR
+#diff -aur -x configure $ORIGIN_DIR/$APP_NAME $APP_NAME | grep -v "^Only in" | tee $SRC_APP_DIR/patch.txt
+#rm -rf $ORIGIN_DIR
