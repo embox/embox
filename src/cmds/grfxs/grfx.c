24e851b374f77b03694c4ea4cc222522e2257b50
AleksandraButrova <aleksandra.butrova@gmail.com>
2017-03-01 12:00:48 +0300

    cmd: Add definitions from nuklear.

--- src/cmds/grfxs/grfx.c
+++ src/cmds/grfxs/grfx.c
@@ -1,11 +1,9 @@
-//include embox.cmd.grfxs.grfx
-
 #include <stdio.h>
 
-
-#define NK_INTERN static inline
-#define NK_API extern
-
+/* includes for nuklear*/
+#define NK_PRIVATE
+#define NK_API
+#define NK_INTERN
 #define NK_INCLUDE_FIXED_TYPES
 #define NK_INCLUDE_STANDARD_IO
 #define NK_INCLUDE_DEFAULT_ALLOCATOR
@@ -23,9 +21,11 @@
 #include <drivers/video/fb.h>
 #include <drivers/input/input_dev.h>
 
+
+/* callbacks */
 static void inpevent(struct vc *vc, struct input_event *ev)
 {
-    printf("\nI do not want do anything ._.\n");
+    printf("\nWhat are doing with graphic window??	\n");
 }
 
 static void visd(struct vc *vc, struct fb_info *fbinfo)
@@ -37,7 +37,6 @@ static void visd(struct vc *vc, struct fb_info *fbinfo)
 	 	return;
 	}
 
-    
     /* portable */
     struct fb_fillrect rect;
     rect.dx = 200;
@@ -70,6 +69,8 @@ const struct vc_callbacks thiscbs = {
 
 
 int main(int argc, char *argv[]) {
+
+	/* register callbacks */
 	struct vc this_vc = {
 		.name = "simple app",
 		.callbacks = &thiscbs,
@@ -77,70 +78,34 @@ int main(int argc, char *argv[]) {
 
 	mpx_register_vc(&this_vc);
 
-	while(1) { }
-#if 0
-
-	struct nk_font_atlas atlas;
-	//const void *img;
-	//int w, h;
+	/* initial items for nuklear */
+    struct nk_font_atlas atlas;
+	int w, h;
 	struct nk_font *font;
 	struct nk_context ctx;
 	
 	
 	nk_font_atlas_init_default(&atlas);
 	nk_font_atlas_begin(&atlas);
-	font = nk_font_atlas_add_from_file(&atlas, "Roboto-Black.ttf", 13, 0);
-	//img = nk_font_atlas_bake(&atlas, &w, &h, NK_FONT_ATLAS_RGBA32);
-	//nk_handle texture;
-	//nk_font_atlas_end(&atlas, nk_handle_id(texture), 0);
+	font = nk_font_atlas_add_default(&atlas, 13, 0);
+	nk_font_atlas_bake(&atlas, &w, &h, NK_FONT_ATLAS_RGBA32);
+	nk_font_atlas_end(&atlas, nk_handle_id(0), 0);
 	
+	nk_init_default(&ctx, &font->handle);
 	
-    nk_init_default(&ctx, &font->handle);
+	/* start of work with nuklear buffers */
+	struct nk_buffer dev_cmds;
+	
+	nk_buffer_init_default(&dev_cmds);
+
+	while(1) {}
+
+	/* end of work */
     nk_font_atlas_clear(&atlas);
+    nk_free(&ctx);
     
-    
-    
+	/* checking working command */
+	printf("\nJob's done! :)\n\n");
 
-
-	// //Start of example
-	
-	// nk_init_fixed(&ctx, calloc(1, 1024), 1024, &NK_INCLUDE_DEFAULT_FONT);
-
-	// enum {EASY, HARD};
-	// int op = EASY;
-	// float value = 0.6f;
-	// //int i =  20;
-
-	// if (nk_begin(&ctx, "Show", nk_rect(50, 50, 220, 220),
-	// 	NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_CLOSABLE)) {
-	// 	/* fixed widget pixel width */
-	// 	nk_layout_row_static(&ctx, 30, 80, 1);
-	// 	if (nk_button_label(&ctx, "button")) {
-	// 		/* event handling */
-	// 	}
-
-	// 	/* fixed widget window ratio width */
-	// 	nk_layout_row_dynamic(&ctx, 30, 2);
-	// 	if (nk_option_label(&ctx, "easy", op == EASY)) op = EASY;
-	// 	if (nk_option_label(&ctx, "hard", op == HARD)) op = HARD;
-
-	// 	/* custom widget pixel width */
-	// 	nk_layout_row_begin(&ctx, NK_STATIC, 30, 2);
-	// 	{
-	// 		nk_layout_row_push(&ctx, 50);
-	// 		nk_label(&ctx, "Volume:", NK_TEXT_LEFT);
-	// 		nk_layout_row_push(&ctx, 110);
-	// 		nk_slider_float(&ctx, 0, &value, 1.0f, 0.1f);
-	// 	}
-	// 	nk_layout_row_end(&ctx);
-	// }
-	// nk_end(&ctx);
-	// // End of example
-
-
-#endif
-
-    printf("\nThere is Sasha's command! :)\n\n");
 	return 0;
 }
-
