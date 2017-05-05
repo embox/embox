957a6e6053af4b81efab97bea2188a47c883a616
Deryugin <deryugin.denis@gmail.com>
2017-05-05 15:32:33 +0300

    armv7-debug: Expand test for a breakpoint attempt

--- src/cmds/armv7a_debug.c
+++ src/cmds/armv7a_debug.c
@@ -12,42 +12,54 @@
 #include <asm/cp14.h>
 #include <asm/cp15.h>
 
+#define TIMEOUT 0xffffff
+
+void _test_func(void) {
+	printf("This is a test function\n");
+}
+
 void _reg_dump(char *s) {
-	uint32_t val;
 
 	printf("-------------------------\n");
-	printf("DEBUG REGS %s\n", s);
+	printf("ARM debug registers %s\n", s);
 
-	val = cp14_get_dbgdidr();
-	printf("DIDR = %x\n", val);
+	printf("SCR  = 0x%08x\n", cp15_get_scr());
+	printf("SDER = 0x%08x\n", cp15_get_sder());
 
-	val = cp15_get_scr();
-	printf("SCR  = 0x%08x\n", val);
-
-	val = cp15_get_sder();
-	printf("SDER = 0x%08x\n", val);
-
-	val = cp14_get_dbgbvr(0);
-	printf("BVR  = 0x%08x\n", val);
-
-	val = cp14_get_dbgbcr(0);
-	printf("BCR  = 0x%08x\n", val);
+	printf("DIDR = 0x%08x\n", cp14_get_dbgdidr());
+	printf("DSCR = 0x%08x\n", cp14_get_dbgdscr());
+	printf("BVR  = 0x%08x\n", cp14_get_dbgbvr(0));
+	printf("BCR  = 0x%08x\n", cp14_get_dbgbcr(0));
 
 	printf("-------------------------\n");
 }
 
 int main(int argc, char *argv[]) {
+	uint32_t val;
+
 	_reg_dump("before");
+
 	cp15_set_sder(3);
 
-	printf("1\n");
-	printf("2\n");
+	val = cp14_get_dbgdscr();
 
-	uint32_t val = 1 << 15;
-	asm volatile("MCR p14,0,%[val],c0,c2,2" : : [val] "r" (val));
+	val |= (1 >> 13) | (1 << 14);
+	val = (val & (~(3 << 20))) | (1 << 20);
 
-	cp14_set_dbgbvr(0, 0);
-	cp14_set_dbgbcr((1 << 0) | (4 << 20), 0);
+	cp14_set_dbgdscr(val);
+
+	val = TIMEOUT;
+	printf("wait..\n");
+	while(val--);
+
+	cp14_set_dbgbvr((uint32_t) _test_func, 0);
+	cp14_set_dbgbcr((0xf << 5) | (1 << 0) | (0 << 20) | 2 | 4 | (1 << 13), 0);
+
+	val = TIMEOUT;
+	printf("wait..\n");
+	while(val--);
+
+	_test_func(); /* This should trigger the breakpoint */
 
 	_reg_dump("after");
 
