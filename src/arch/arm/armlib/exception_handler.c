10af83efcc7dda9b0e6a20427baf2534e5f798f6
Alex Kalmuk <alexkalmuk@gmail.com>
2017-05-17 18:58:45 +0300

    arm: Add dirty version of exception_handler for a while

--- src/arch/arm/armlib/exception_handler.c
+++ src/arch/arm/armlib/exception_handler.c
@@ -8,14 +8,37 @@
 #include <util/log.h>
 
 #include <kernel/panic.h>
+#include <kernel/printk.h>
+
+#include <asm/cp15.h>
+
+uint32_t GLOBAL_LR1 = 0;
+uint32_t GLOBAL_SP1 = 0;
+uint32_t GLOBAL_CPSR1 = 0;
+uint32_t GLOBAL_SPSR1 = 0;
+
+uint32_t GLOBAL_LR2 = 0;
+uint32_t GLOBAL_SP2 = 0;
+uint32_t GLOBAL_CPSR2 = 0;
+uint32_t GLOBAL_SPSR2 = 0;
+
+void mon_swi_handler(void) {
+	while (1)
+		;
+}
+
+void mon_func(void) {
+}
 
 #define ARM_GDB_BAD_INSTRUCTION    0xe1212374 /* bkpt 0x1234 */
 void arm_exception_handler(unsigned int *regs) {
+	uint32_t val;
+
 	if ((*(uint32_t*)(regs[15])) == ARM_GDB_BAD_INSTRUCTION) {
 		log_debug("BKPT happened");
 		regs[15] += 4;
 	} else {
-		panic("EXCEPTION:\n"
+		printk("EXCEPTION:\n"
 				"r0=%08x r1=%08x r2=%08x r3=%08x\n"
 				"r4=%08x r5=%08x r6=%08x r7=%08x\n"
 				"r8=%08x r9=%08x r10=%08x r11=%08x\n"
@@ -26,6 +49,14 @@ void arm_exception_handler(unsigned int *regs) {
 				regs[10], regs[11], regs[12], regs[13],
 				regs[14], regs[15],
 				regs[0], regs[1]);
-		while(1);
+		printk("REGS* = %p\n", regs);
+		val = get_cpsr();
+		printk("[SMC] cpsr = %x\n", val);
+		printk("[SMC] mbvar = %x\n", cp15_get_mvbar());
+		printk("[SMC] vbar = %x\n",  cp15_get_vbar());
+
+		cp15_set_scr(0x1);
+		printk(">>>>>>>>>>>>>>> NS=1 updated \n");
+		//while(1);
 	}
 }
