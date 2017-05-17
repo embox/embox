2f7c269dcd3deb4876546e98b728fcbd69426078
Alex Kalmuk <alexkalmuk@gmail.com>
2017-05-17 18:56:58 +0300

    arm: Extend armv7a_debug command

--- src/cmds/armv7a_debug.c
+++ src/cmds/armv7a_debug.c
@@ -11,17 +11,40 @@
 #include <stdio.h>
 #include <asm/cp14.h>
 #include <asm/cp15.h>
+#include <asm/hal/reg.h>
 
 #define TIMEOUT 0xffffff
+
+extern uint32_t GLOBAL_LR1;
+extern uint32_t GLOBAL_SP1;
+extern uint32_t GLOBAL_CPSR1;
+extern uint32_t GLOBAL_SPSR1;
+
+extern uint32_t GLOBAL_LR2;
+extern uint32_t GLOBAL_SP2;
+extern uint32_t GLOBAL_CPSR2;
+extern uint32_t GLOBAL_SPSR2;
 
 void _test_func(void) {
 	printf("This is a test function\n");
 }
 
 void _reg_dump(char *s) {
-
 	printf("-------------------------\n");
 	printf("ARM debug registers %s\n", s);
+
+	printf("GLOBAL_LR1 = %x\n", GLOBAL_LR1);
+	printf("GLOBAL_SP1 = %x\n", GLOBAL_SP1);
+	printf("GLOBAL_CPSR1 = %x\n", GLOBAL_CPSR1);
+	printf("GLOBAL_SPSR1 = %x\n", GLOBAL_SPSR1);
+
+	printf("\n");
+
+	printf("GLOBAL_LR2 = %x\n", GLOBAL_LR2);
+	printf("GLOBAL_SP2 = %x\n", GLOBAL_SP2);
+	printf("GLOBAL_CPSR2 = %x\n", GLOBAL_CPSR2);
+	printf("GLOBAL_SPSR2 = %x\n", GLOBAL_SPSR2);
+
 
 	printf("SCR  = 0x%08x\n", cp15_get_scr());
 	printf("SDER = 0x%08x\n", cp15_get_sder());
@@ -34,18 +57,22 @@ void _reg_dump(char *s) {
 	printf("-------------------------\n");
 }
 
+extern void exit_monitor_mode(void);
+
 int main(int argc, char *argv[]) {
 	uint32_t val;
 
+	val = get_cpsr();
+	printf("cpsr = %x\n", val);
+
+	__asm__ __volatile__ (
+		"dmb\n\t"
+		"isb\n\t"
+		"SMC #0" : : :);
+
 	_reg_dump("before");
 
-	cp15_set_sder(3);
-
-	val = cp14_get_dbgdscr();
-
-	val |= (1 >> 13) | (1 << 14);
-	val = (val & (~(3 << 20))) | (1 << 20);
-
+	val = 1 << 14;
 	cp14_set_dbgdscr(val);
 
 	val = TIMEOUT;
@@ -53,7 +80,9 @@ int main(int argc, char *argv[]) {
 	while(val--);
 
 	cp14_set_dbgbvr((uint32_t) _test_func, 0);
-	cp14_set_dbgbcr((0xf << 5) | (1 << 0) | (0 << 20) | 2 | 4 | (1 << 13), 0);
+
+	cp14_set_dbgbcr((1 << 0) | (0 << 20), 0);
+
 
 	val = TIMEOUT;
 	printf("wait..\n");
