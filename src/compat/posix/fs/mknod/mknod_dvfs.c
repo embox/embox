7c37992a6053ab0dd40def3eb51343a7a1767332
Anton Bondarev <anton.bondarev2310@gmail.com>
2017-06-03 16:35:16 +0300

    compat: Add default implementations of mknod()

---
+++ src/compat/posix/fs/mknod/mknod_dvfs.c
@@ -0,0 +1,15 @@
+/**
+ * @file
+ *
+ * @date 14.04.2017
+ * @author Anton Bondarev
+ */
+
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+int mknod(const char *pathname, mode_t mode, dev_t dev) {
+	return 0;
+}
---
+++ src/compat/posix/fs/mknod/mknod_oldfs.c
@@ -0,0 +1,21 @@
+/**
+ * @file
+ *
+ * @date 14.04.2017
+ * @author Anton Bondarev
+ */
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fs/vfs.h>
+
+int mknod(const char *pathname, mode_t mode, dev_t dev) {
+	struct path root, child;
+
+	vfs_get_root_path(&root);
+	vfs_create(&root, pathname, mode, &child);
+
+	SETERNO(ENOSYS);
+
+	return -1;
+}
