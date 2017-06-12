3b93e079e1e32ec1fb92388c90315cfd2d4f6a0f
Sharganov <sharganov.artem@yandex.ru>
2017-06-12 18:23:55 +0300

    modified forkexec command

--- src/cmds/forkexec.c
+++ src/cmds/forkexec.c
@@ -14,14 +14,46 @@
 #include <stdint.h>
 
 #include <kernel/task.h>
+#include "lib/libelf.h"
+#include <errno.h>
 
-static void *new_task_entry(void *file);
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <util/math.h>
+#include <sys/mman.h>
+
+#include <lib/libelf.h>
+#include <kernel/usermode.h>
+#include <mem/mmap.h>
+#include <kernel/task.h>
+#include <kernel/task/resource/mmap.h>
+
+//static void *new_task_entry(void *file);
 
 int main(int argc, char **argv) {
-	char *filename;
+    /*char *filename;
 	int pid;
+*/
+    if(argc < 1) return -1;
 
-	filename = malloc(strlen(argv[argc-1]));
+    Elf32_Ehdr header;
+    int elf_file;
+    int err;
+
+    elf_file = open(argv[argc - 1], O_RDONLY);
+
+    if ((err = elf_read_header(elf_file, &header)) < 0) {
+        close(elf_file);
+        return err;
+    }
+    void (*functionPtr)();
+    functionPtr = (void*) header.e_entry;
+    (*functionPtr)();
+
+    /*filename = malloc(strlen(argv[argc-1]));
 	strcpy(filename, argv[argc - 1]);
 	pid = new_task(filename, new_task_entry, filename);
 
@@ -32,8 +64,11 @@ int main(int argc, char **argv) {
 	free(filename);
 
 	return pid > 0 ? 0 : pid;
-}
+    */
 
+    return 0;
+}
+/*
 extern int execve_syscall(const char *filename, char *const argv[], char *const envp[]);
 
 static void *new_task_entry(void *filename) {
@@ -44,3 +79,4 @@ static void *new_task_entry(void *filename) {
 
 	return NULL;
 }
+*/
