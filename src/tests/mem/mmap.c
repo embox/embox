7664066c259c495252e83255f09be6d35ba78d51
Anna Maryina <maryina.anna.va@gmail.com>
2017-06-17 18:05:47 +0300

    mmap() child exit test

--- src/tests/mem/mmap.c
+++ src/tests/mem/mmap.c
@@ -21,3 +21,32 @@ TEST_CASE("request zero-length") {
 	test_assert_null(ptr);
 	test_assert_equal(errno, EINVAL);
 }
+
+static void run_wait_task(void *(*fn)(void *), void *arg) {
+	pid_t p;
+
+	p = new_task("", fn, NULL);
+
+	while (-EINTR == task_waitpid(p));
+}
+
+static void *mmap_test_child(void *arg) {
+	int allocated = 0;
+	while (1) {
+		void *ptr = mmap(NULL, PAGE_SIZE(), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+		if (!ptr)
+			break;
+		if (!allocated && ptr) {
+			allocated = 1;
+			test_emit('a');
+		}
+	}
+	test_emit('b');
+	_exit(0);
+}
+
+TEST_CASE("unmap after exit") {
+	run_wait_task(mmap_test_child, NULL);
+	run_wait_task(mmap_test_child, NULL);
+	test_assert_emitted("abab");
+}
