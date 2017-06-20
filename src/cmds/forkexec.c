f59efed0f20c2cdfad0f0c5b38e02eda10316da8
Sharganov <sharganov.artem@yandex.ru>
2017-06-20 04:01:19 +0300

    Basic implementation of user code execution

--- src/cmds/forkexec.c
+++ src/cmds/forkexec.c
@@ -39,20 +39,62 @@ int main(int argc, char **argv) {
 */
     if(argc < 1) return -1;
 
+    // need sto read segments from file
     Elf32_Ehdr header;
     int elf_file;
     int err;
 
     elf_file = open(argv[argc - 1], O_RDONLY);
 
+    // read elf file header
     if ((err = elf_read_header(elf_file, &header)) < 0) {
         close(elf_file);
         return err;
     }
-    void (*functionPtr)();
-    functionPtr = (void*) header.e_entry;
-    (*functionPtr)();
 
+    Elf32_Shdr * section_header_table = malloc(sizeof(Elf32_Shdr)*header.e_shnum);
+
+
+    // read section header table
+    if((err = elf_read_sh_table(elf_file, &header, section_header_table)) < 0)
+    {
+        close(elf_file);
+        return err;
+    }
+
+    // read .shstrtab section
+    Elf32_Shdr *names_section = malloc(sizeof(Elf32_Shdr));
+    ///TODO change parametrs \index and name to elf_read_header....
+    elf_read_section_by_index(elf_file, &header, names_section, header.e_shstrndx);
+
+    // array of section names, string are presented as null-terminated
+    char * names = malloc(names_section->sh_size);
+    elf_read_section(elf_file, names_section, names);
+
+    // text section. Used to iterate a table
+    Elf32_Shdr *text_section = section_header_table;
+
+    int i = 0;
+
+    while(strcmp(names + text_section->sh_name, ".text") != 0)
+    {
+        text_section++;
+        i++;
+        // iterate through all table and didn't find .text section
+        if(i == header.e_shnum) return -1;
+    }
+
+    void * instructions = malloc(text_section->sh_size);
+    elf_read_section(elf_file, text_section, instructions);
+
+    printf("Code address : %p \n", instructions);
+
+    int (*functionPtr)();
+
+    functionPtr = instructions;
+    printf("Result : %d", functionPtr());
+
+    /*
     /*filename = malloc(strlen(argv[argc-1]));
 	strcpy(filename, argv[argc - 1]);
 	pid = new_task(filename, new_task_entry, filename);
--- src/lib/elf/elf.c
+++ src/lib/elf/elf.c
@@ -47,6 +47,39 @@ int elf_read_ph_table(int fd, Elf32_Ehdr *header, Elf32_Phdr *ph_table) {
 	return ENOERR;
 }
 
+int elf_read_sh_table(int fd, Elf32_Ehdr * header, Elf32_Shdr *sh_table)
+{
+    size_t size = header->e_shnum * header->e_shentsize;
+
+    lseek(fd, header->e_shoff, SEEK_SET);
+
+    if(size != read(fd, sh_table, size))
+    {
+        printf("ERROR");
+        return -EBADF;
+    }
+    return ENOERR;
+}
+
+int elf_read_section(int fd, Elf32_Shdr *sh, void * addr)
+{
+    lseek(fd, sh->sh_offset, SEEK_SET);
+    if (sh->sh_size != read(fd, addr, sh->sh_size)) {
+        return -EBADF;
+    }
+    return ENOERR;
+}
+
+int elf_read_section_by_index(int fd, Elf32_Ehdr *header, Elf32_Shdr *s_header, unsigned short index)
+{
+    int offset = header->e_shoff + index * sizeof(Elf32_Shdr);
+    lseek(fd, offset, SEEK_SET);
+    if(sizeof(Elf32_Shdr) != read(fd, s_header, sizeof(Elf32_Shdr)))
+        return -EBADF;
+
+    return ENOERR;
+}
+
 int elf_read_segment(int fd, Elf32_Phdr *ph, void *addr) {
 	lseek(fd, ph->p_offset, SEEK_SET);
 	if (ph->p_filesz != read(fd, addr, ph->p_filesz)) {
