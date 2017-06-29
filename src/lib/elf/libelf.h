473826dc0e44f1d8c83fb5cb6174030af5161ce1
Sharganov <sharganov.artem@yandex.ru>
2017-06-29 21:58:53 +0300

    Script for building app for Embox. Clean up

--- src/lib/elf/libelf.h
+++ src/lib/elf/libelf.h
@@ -13,8 +13,14 @@
 #include <lib/elf_types.h>
 
 extern int elf_read_header(int fd, Elf32_Ehdr *header);
+
+int elf_read_sh_table(int fd, Elf32_Ehdr *header, Elf32_Shdr *sh_table);
+int elf_read_section(int fd, Elf32_Shdr *sh, void *addr);
+int elf_read_section_by_index(int fd, Elf32_Ehdr *header, Elf32_Shdr *s_header, unsigned short index);
+
 extern int elf_read_ph_table(int fd, Elf32_Ehdr *header, Elf32_Phdr *ph_table);
 extern int elf_read_segment(int fd, Elf32_Phdr *ph, void *addr);
+
 extern int elf_read_interp(int fd, Elf32_Phdr *ph, char *interp);
 
 #endif /* LIB_ELF_H_ */
