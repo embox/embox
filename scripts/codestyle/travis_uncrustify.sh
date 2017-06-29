701439e189fa59167697d271dbc0c633ce288caa
alex <a.e.chebykin@gmail.com>
2017-06-29 18:08:02 +0300

    Uncrustify: updated travis script

--- scripts/codestyle/travis_uncrustify.sh
+++ scripts/codestyle/travis_uncrustify.sh
@@ -1,4 +1,5 @@
-if $TRAVIS_PULL_REQUEST then
+if $TRAVIS_PULL_REQUEST; then
 	./run_uncrustify.sh -v $(git rev-parse HEAD~1) $TRAVIS_PULL_REQUEST_SHA
 else
 	./run_uncrustify.sh -v $(git rev-parse HEAD~1) $(git rev-parse HEAD)
+fi
