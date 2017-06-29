61c4584cc20c5c667ce83a72e0f5293a5d3e23db
alex <a.e.chebykin@gmail.com>
2017-06-29 20:01:16 +0300

    Uncrustify: scripts updates: fixed problem with paths

--- scripts/codestyle/travis_uncrustify.sh
+++ scripts/codestyle/travis_uncrustify.sh
@@ -1,5 +1,6 @@
-if $TRAVIS_PULL_REQUEST; then
-	./run_uncrustify.sh -v $(git rev-parse HEAD~1) $TRAVIS_PULL_REQUEST_SHA
+echo $PWD
+if ! $TRAVIS_PULL_REQUEST; then
+	bash ./run_uncrustify.sh -v $(git rev-parse HEAD~1) $(git rev-parse HEAD)
 else
-	./run_uncrustify.sh -v $(git rev-parse HEAD~1) $(git rev-parse HEAD)
+	bash ./run_uncrustify.sh -v $(git rev-parse HEAD~1) $TRAVIS_PULL_REQUEST_SHA
 fi
