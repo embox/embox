7914b71c0cff9fb6c9a82e76b708e2b92e199e02
Guzel Garifullina <GiseleGarifullina@gmail.com>
2017-07-03 18:03:58 +0300

    Division error with output (uncleaned version)

--- src/drivers/interrupt/xen.c
+++ src/drivers/interrupt/xen.c
@@ -11,6 +11,10 @@
 #include <kernel/printk.h>
 
 #include <drivers/irqctrl.h>
+#include <xen/event.h>
+
+extern shared_info_t xen_shared_info;
+extern void do_divide_error(void);
 
 EMBOX_UNIT_INIT(xen_int_init);
 
@@ -22,9 +26,14 @@ void irqctrl_enable(unsigned int irq) {
 }
 
 void irqctrl_disable(unsigned int irq) {
+	printk("in disable\n");
 }
 
 void irqctrl_force(unsigned int irq) {
+	printk("xen force interrupt %d\n", irq);
+	int x = 0;
+	int y = 100 / x; /* make div-by-zero execption */
+	printk("%d\n", y); /* avoid optimizing out by compiler */
 }
 
 void interrupt_handle(void) {
