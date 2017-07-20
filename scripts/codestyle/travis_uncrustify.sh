3b7b52da1d92bb561f4833dfb73531678dd30d8a
Anton Kozlov <drakon.mega@gmail.com>
2017-07-20 14:43:25 +0300

    Travis codestyle check changes
    Not PR checks are not fatal
    Uncrustify runs on default os

--- scripts/codestyle/travis_uncrustify.sh
+++ scripts/codestyle/travis_uncrustify.sh
@@ -1,5 +1,18 @@
-if ! $TRAVIS_PULL_REQUEST; then
-	bash ./run_uncrustify.sh -v $(git rev-parse HEAD~1) $(git rev-parse HEAD)
-else
-	bash ./run_uncrustify.sh -v $(git rev-parse HEAD~1) $TRAVIS_PULL_REQUEST_SHA
+#!/bin/sh
+
+git fetch origin master:master
+git fetch --depth=1000 # unlikely the branch will have more than 1000 commits
+
+revs="$(git rev-parse --short master)...$(git rev-parse --short HEAD)"
+
+echo Checking $revs
+
+$(dirname $0)/run_uncrustify.sh $revs
+result=$?
+
+if [ $result != 0 ] && [ x"$TRAVIS_PULL_REQUEST" = xfalse ]; then
+	echo "Codestyle issues are not fatal for not Pull-Request jobs"
+	result=0
 fi
+
+exit $result
