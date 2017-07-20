7689cea31887a1b36f971a448c70879171bf1416
Anton Kozlov <drakon.mega@gmail.com>
2017-07-21 01:16:23 +0300

    Change message wording and other appearance

--- scripts/codestyle/travis_uncrustify.sh
+++ scripts/codestyle/travis_uncrustify.sh
@@ -5,13 +5,13 @@ git fetch --depth=1000 # unlikely the branch will have more than 1000 commits
 
 revs="$(git rev-parse --short master)...$(git rev-parse --short HEAD)"
 
-echo Checking $revs
+echo Checking $revs for code style conformance
 
 $(dirname $0)/run_uncrustify.sh $revs
 result=$?
 
 if [ $result != 0 ] && [ x"$TRAVIS_PULL_REQUEST" = xfalse ]; then
-	echo "Codestyle issues are not fatal for not Pull-Request jobs"
+	echo "Ignoring code style violations (non-PR job)"
 	result=0
 fi
 
