e4c7da1c1373e84a33e1b7f930759042e4b39cc6
Deryugin <deryugin.denis@gmail.com>
2017-12-20 17:09:10 +0300

    e2k: Add atomic add

--- src/arch/e2k/include/asm/io.h
+++ src/arch/e2k/include/asm/io.h
@@ -11,11 +11,44 @@
 
 #include <stdint.h>
 
+#define WMB_AFTER_ATOMIC	".word 0x00008001\n" \
+				".word 0x30000084\n"
+
+#define __e2k_atomic32_add(__val, __addr) \
+({ \
+	int __rval; \
+	asm volatile ("\n1:" \
+			  "\n\tldw,0 %[addr], %[rval], mas=0x7" \
+			  "\n\tadds %[rval], %[val], %[rval]" \
+			  "\n\t{"\
+			  "\n\tstw,2 %[addr], %[rval], mas=0x2" \
+			  "\n\tibranch 1b ? %%MLOCK" \
+			  "\n\t}" \
+			  WMB_AFTER_ATOMIC \
+			  : [rval] "=&r" (__rval), [addr] "+m" (*(__addr)) \
+			  : [val] "ir" (__val) \
+			  : "memory"); \
+	__rval; \
+})
+
+static void e2k_wait_all(void) {
+	_Pragma ("no_asm_inline")
+	asm volatile ("wait \ttrap = %0, ma_c = %1, fl_c = %2, ld_c = %3, "
+			"st_c = %4, all_e = %5, all_c = %6"
+			: : "i" (0), "i" (1), "i" (1), "i" (0), "i" (0), "i" (1), "i" (1) : "memory");
+}
+
+static void wmb() {
+	asm volatile (	".word 0x00008001\n"
+			".word 0x30000084\n"
+			::: "memory");
+}
+
 static inline void e2k_write8(uint8_t val, void *addr) {
        asm volatile ("stb,2\t0x0, [%0] %2, %1" :
 		: "r" (addr), "r" (val), "i" (6) : "memory");
 }
-#if 0
+
 static inline void e2k_write16(uint16_t val, void *addr) {
        asm volatile ("sth,2\t0x0, [%0] %2, %1" :
 		: "r" (addr), "r" (val), "i" (6) : "memory");
@@ -58,5 +91,5 @@ static inline uint64_t e2k_read64(void *addr) {
 		"=r" (res) : "r" (addr), "i" (6));
 	return res;
 }
-#endif
+
 #endif /* E2K_IO_H_ */
