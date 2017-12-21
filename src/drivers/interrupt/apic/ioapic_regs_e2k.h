f38ee192822a5bb55f876bdc3cff4b34a7b2bf7e
Deryugin <deryugin.denis@gmail.com>
2017-12-21 14:47:16 +0300

    apic: Port driver for e2k

---
+++ src/drivers/interrupt/apic/ioapic_regs_e2k.h
@@ -0,0 +1,28 @@
+/**
+ * @file ioapic_regs_e2k.h
+ * @brief
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version
+ * @date 21.12.2017
+ */
+
+#ifndef IOAPIC_REGS_E2K_H_
+#define IOAPIC_REGS_E2K_H_
+
+#include <stdint.h>
+
+#define IOAPIC_DEF_ADDR	          0xFEC00000
+#define IOREGSEL                  (IOAPIC_DEF_ADDR + 0x00)
+#define IOREGWIN                  (IOAPIC_DEF_ADDR + 0x10)
+
+static inline uint32_t ioapic_read(uint8_t reg) {
+	e2k_write32(reg, IOREGSEL);
+	return e2k_read32(IOREGWIN);
+}
+
+static inline void ioapic_write(uint8_t reg, uint32_t val) {
+	e2k_write32(reg, IOREGSEL);
+	e2k_write32(val, IOREGWIN);
+}
+
+#endif /* IOAPIC_REGS_E2K_H_ */
---
+++ src/drivers/interrupt/apic/lapic_regs_e2k.h
@@ -0,0 +1,22 @@
+/**
+ * @file lapic_regs_e2k.h
+ * @brief
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version
+ * @date 21.12.2017
+ */
+
+#ifndef LAPIC_REGS_E2K_H_
+#define LAPIC_REGS_E2K_H_
+
+#include <asm/io.h>
+
+static inline uint32_t lapic_read(uint32_t reg) {
+	return e2k_read32((void *) reg);
+}
+
+static inline void lapic_write(uint32_t reg, uint32_t value) {
+	e2k_write32(value, (void *) reg);
+}
+
+#endif /* LAPIC_REGS_E2K_H_ */
