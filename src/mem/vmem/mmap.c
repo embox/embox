2ef10d22c9d81fc5d1f003e4768c4a7407e81ac7
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-01-19 18:18:35 +0300

    mmap: Add logging

--- src/mem/vmem/mmap.c
+++ src/mem/vmem/mmap.c
@@ -1,3 +1,5 @@
+#include <util/log.h>
+
 #include <errno.h>
 #include <stddef.h>
 #include <sys/mman.h>
@@ -6,6 +8,7 @@
 extern void *mmap_userspace_add(void *addr, size_t len, int prot);
 
 void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off) {
+	log_debug("addr=%p, len=%d, prot=%d, flags=%d, fd=%d, off=%d", addr, len, prot, flags, fd, off);
 	if (len == 0) {
 		SET_ERRNO(EINVAL);
 		return MAP_FAILED;
