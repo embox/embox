7b37d8b54f314e2303c772609ce634dd70e9fdf4
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-03-15 17:53:56 +0300

    arch: e2k: Start work on e2k interrupts

---
+++ src/arch/e2k/e2k_syscall.c
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date Mar 13, 2018
+ * @author Anton Bondarev
+ */
+
+#include <stdint.h>
+
+unsigned long cpuSyscall(int number, uint64_t arg1, uint64_t arg2,
+			 uint64_t arg3, uint64_t arg4)
+{
+	return 0;
+}
