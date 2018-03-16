c6f62cad19a9aa9499bd62bc98e64a2474a93b3f
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-03-16 18:57:49 +0300

    arch: e2k: Add coping trap table to 0 address

--- src/arch/e2k/entry.c
+++ src/arch/e2k/entry.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include <asm/io.h>
 
 static int last_cpuid = 0;
@@ -5,6 +7,7 @@ static int last_cpuid = 0;
 __attribute__ ((__section__(".e2k_entry")))
 void e2k_entry(void) {
 	int cpuid;
+	extern char *_t_entry;
 
 	e2k_wait_all();
 
@@ -14,6 +17,9 @@ void e2k_entry(void) {
 		/* XXX currently we support only single core */
 		while(1);
 	}
+
+	/* copy of trap table */
+	memcpy((void*)0, &_t_entry, 0x1800);
 
 	kernel_start();
 }
