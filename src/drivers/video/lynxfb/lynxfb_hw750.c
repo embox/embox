745f2b793b1efee446c6f0b4a9a4bbfbed470b51
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-03-30 13:45:58 +0300

    drivers: lynxfb: Start working lynxfb_hw750

---
+++ src/drivers/video/lynxfb/lynxfb_hw750.c
@@ -0,0 +1,20 @@
+/**
+ * @file
+ *
+ * @date Mar 29, 2018
+ * @author Anton Bondarev
+ */
+
+
+int lynxfb_hw750_map(struct lynx_share *share, struct pci_dev *pdev) {
+	return 0;
+}
+
+int lynxfb_hw750_inithw(struct lynx_share *share, struct pci_dev *pdev) {
+	return 0;
+}
+
+/* 	chip specific g_option configuration routine */
+void lynxfb_hw750_setup(struct lynx_share *share, char *src) {
+
+}
