4077d1f1358bf70e4891d04d894d8e9e6a01c062
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-03-31 16:31:44 +0300

    drivers: video: Work on lynxfb

--- src/drivers/video/lynxfb/lynxfb.c
+++ src/drivers/video/lynxfb/lynxfb.c
@@ -4,6 +4,7 @@
  * @date Mar 21, 2018
  * @author Anton Bondarev
  */
+#include <util/log.h>
 
 #include <errno.h>
 #include <string.h>
@@ -19,8 +20,15 @@
 
 #include <framework/mod/options.h>
 
+#include "lynxfb.h"
+#include "lynxfb_hw750.h"
+
 
 PCI_DRIVER("lynxfb", lynxfb_init, PCI_VENDOR_ID_SMI, PCI_DEV_ID_LYNX_SE);
+
+
+static struct sm750_share lynxfb_share;
+
 
 static int lynxfb_set_var(struct fb_info *info, const struct fb_var_screeninfo *var) {
 	return 0;
@@ -38,10 +46,37 @@ static const struct fb_ops lynxfb_ops = {
 	.fb_get_var = lynxfb_get_var,
 };
 
+static void lynxfb_hw_init(struct pci_slot_dev *pci_dev) {
+#if 0
+	int g_specId;
+	size_t spec_offset;
+
+	switch (pci_dev->device) {
+	case PCI_DEV_ID_LYNX_EXP:
+	case PCI_DEV_ID_LYNX_SE:
+		g_specId = LYNXFB_SPC_SM750;
+		/* though offset of share in sm750_share is 0,
+		 *we use this marcro as the same */
+		spec_offset = offsetof(struct sm750_share, share);
+		break;
+	default:
+		break;
+	}
+#endif
+
+	lynxfb_share.share.devid = pci_dev->device;
+	lynxfb_share.share.revid = pci_dev->rev;
+
+	lynxfb_hw750_setup(&lynxfb_share);
+
+}
+
 static int lynxfb_init(struct pci_slot_dev *pci_dev) {
 	char *mmap_base = (char *)(pci_dev->bar[0] & ~0xf); /* FIXME */
 	size_t mmap_len = binalign_bound(2048 * 2048 * 32 / 8, PAGE_SIZE());
 	struct fb_info *info;
+
+	lynxfb_hw_init(pci_dev);
 
 	if (MAP_FAILED == mmap_device_memory(mmap_base,
 				mmap_len,
--- src/drivers/video/lynxfb/lynxfb.h
+++ src/drivers/video/lynxfb/lynxfb.h
@@ -18,9 +18,10 @@
 
 struct fb_info;
 
-/* 	lynx_share stands for a presentation of two frame buffer
-	that use one smi adaptor , it is similar to a basic class of C++
-*/
+/**
+ * lynx_share stands for a presentation of two frame buffer
+ *  that use one smi adaptor , it is similar to a basic class of C++
+ */
 struct lynx_share {
 	/* common members */
 	uint16_t devid;
@@ -35,8 +36,75 @@ struct lynx_share {
 	size_t vidreg_size;
 
 	/* function pointers */
-	void (*suspend) (struct lynx_share *);
-	void (*resume) (struct lynx_share *);
+	void (*suspend)(struct lynx_share *);
+	void (*resume)(struct lynx_share *);
+};
+
+struct lynxfb_crtc {
+	unsigned char *vCursor; /*virtual address of cursor */
+	unsigned char *vScreen; /*virtual address of on_screen */
+	int oCursor; /*cursor address offset in vidmem */
+	int oScreen; /*onscreen address offset in vidmem */
+	int channel; /* which channel this crtc stands for */
+	int pallete_was_updated;
+	size_t vidmem_size; /* this view's video memory max size */
+
+	/* below attributes belong to info->fix, their value depends on specific adaptor */
+	uint16_t line_pad; /* padding information:0, 1, 2, 4, 8, 16, ... */
+	uint16_t xpanstep;
+	uint16_t ypanstep;
+	uint16_t ywrapstep;
+
+	void *priv;
+
+	int (*proc_setMode)(struct lynxfb_crtc *, struct fb_var_screeninfo *,
+			struct fb_fix_screeninfo *);
+	int (*proc_checkMode)(struct lynxfb_crtc *, struct fb_var_screeninfo *);
+	int (*proc_setColReg)(struct lynxfb_crtc *, uint16_t, uint16_t, uint16_t,
+			uint16_t);
+	void (*clear)(struct lynxfb_crtc *);
+	/* pan display */
+	int (*proc_panDisplay)(struct lynxfb_crtc *,
+			const struct fb_var_screeninfo *, const struct fb_info *);
+	/* cursor information */
+	//struct lynx_cursor cursor;
+};
+
+struct lynxfb_output {
+	int dpms;
+	int paths;
+	/*
+	 * which paths(s) this output stands for, for sm750:
+	 * paths=1:means output for panel paths
+	 * paths=2:means output for crt paths
+	 * paths=3:means output for both panel and crt paths
+	 */
+
+	int *channel;
+	/*
+	 * which channel these outputs linked with, for sm750:
+	 * channel=0 means primary channel
+	 * channel=1 means secondary channel
+	 * output->channel ==> &crtc->channel
+	 */
+	void *priv;
+
+	int (*proc_setMode)(struct lynxfb_output *, struct fb_var_screeninfo *,
+			struct fb_fix_screeninfo *);
+
+	int (*proc_checkMode)(struct lynxfb_output *, struct fb_var_screeninfo *);
+	int (*proc_setBLANK)(struct lynxfb_output *, int);
+	void (*clear)(struct lynxfb_output *);
+};
+
+struct lynxfb_par {
+	/* either 0 or 1 for dual head adaptor, 0 is the older one registered */
+	int index;
+	unsigned int pseudo_palette[256];
+	struct lynxfb_crtc crtc;
+	struct lynxfb_output output;
+	struct fb_info *info;
+	struct lynx_share *share;
 };
 
 #endif /* SRC_DRIVERS_VIDEO_LYNXFB_LYNXFB_H_ */
