871ffd02ea283e65ec3057a7c9878c0209c240ba
miha2733 <somthing>
2018-04-04 12:28:09 +0300

    [WIP] Sending an irq

--- src/drivers/interrupt/xen.c
+++ src/drivers/interrupt/xen.c
@@ -6,18 +6,40 @@
  * @author
  */
 
+#include <stdint.h>
 #include <kernel/irq.h>
+#include <kernel/panic.h>
 #include <embox/unit.h>
 #include <kernel/printk.h>
+#include <xen_hypercall-x86_32.h>
+#include <xen/xen.h>
+#include <string.h>
 
 #include <drivers/irqctrl.h>
 #include <xen/event.h>
 
-int test_irqctrl_div_zero;
+extern shared_info_t xen_shared_info;
 
 EMBOX_UNIT_INIT(xen_int_init);
 
+int port;
+
 static int xen_int_init(void) {
+	evtchn_alloc_unbound_t op_sender;
+	op_sender.dom = DOMID_SELF;
+	op_sender.remote_dom = DOMID_SELF;
+	if (HYPERVISOR_event_channel_op (EVTCHNOP_alloc_unbound, &op_sender) != 0)
+	{
+		return -1;
+	}
+	port = op_sender.port;
+	evtchn_bind_interdomain_t op_recipient;
+	op_recipient.remote_dom = DOMID_SELF;
+	op_recipient.remote_port = op_sender.port;
+	if ( HYPERVISOR_event_channel_op ( EVTCHNOP_bind_interdomain, &op_recipient) != 0 )
+	{
+		return -1;
+	}
 	return 0;
 }
 
@@ -29,14 +51,11 @@ void irqctrl_disable(unsigned int irq) {
 }
 
 void irqctrl_force(unsigned int irq) {
-	printk("xen force interrupt %d\n", irq);
-	int x = 0;
-	test_irqctrl_div_zero = irq;
-	__asm__ __volatile("" ::: "memory");
-	int y = 100 / x; /* make div-by-zero execption */
-	__asm__ __volatile("" ::: "memory");
-	test_irqctrl_div_zero = 0;
-	printk("%d\n", y); /* avoid optimizing out by compiler */
+	struct evtchn_send event;
+	event.port = port;
+	if (HYPERVISOR_event_channel_op(EVTCHNOP_send, &event) != 0) {
+		panic("ops");
+	};
 }
 
 void interrupt_handle(void) {
