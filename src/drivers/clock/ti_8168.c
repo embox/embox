88eb71495e1d5b67e25fdb4551311fc87015297c
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-04-18 18:18:15 +0300

    drivers: Improve dm816x timer

--- src/drivers/clock/ti_8168.c
+++ src/drivers/clock/ti_8168.c
@@ -14,26 +14,30 @@
 #include <kernel/time/clock_source.h>
 #include <kernel/printk.h>
 
+#include <drivers/common/memory.h>
+#include <hal/mmu.h>
+#include <mem/vmem.h>
+
 #include <embox/unit.h>
 
 EMBOX_UNIT_INIT(ti8168_clk_init);
 
-#define TI8168_CLKIN_HZ 27000000
+#define TI8168_CLKIN_HZ      27000000
 
-#define CM_TIMER1_CLKSEL 0x48180390
-#define CM_CLKIN 0x2
+#define CM_TIMER1_CLKSEL     0x48180390
+#define CM_CLKIN             0x2
 
-#define GPTIMER31_01_TIDR 0xfff1301
-#define GPTIMER31_01_MASK 0xfffffff
+#define GPTIMER31_01_TIDR    0xfff1301
+#define GPTIMER31_01_MASK    0xfffffff
 
 #define TI8168_GPTIMER1_BASE ((void *) 0x4802e000)
-#define TI8168_GPTIMER1_IRQ 67
+#define TI8168_GPTIMER1_IRQ  67
 
-#define TI8168_LOAD_VALUE (0xffffffff - (TI8168_CLKIN_HZ / 1000) + 1)
+#define TI8168_LOAD_VALUE    (0xffffffff - (TI8168_CLKIN_HZ / 1000) + 1)
 
 /* common register values */
 
-#define GPTIMER_TCLR_START	(1 << 0)
+#define GPTIMER_TCLR_START	    (1 << 0)
 #define GPTIMER_TCLR_AUTORELOAD (1 << 1)
 
 #define GPTIMER_TIER_OVERFLOW   (1 << 1)
@@ -41,9 +45,9 @@ EMBOX_UNIT_INIT(ti8168_clk_init);
 
 struct gptimer13_1 {
 	uint32_t tidr; 		/* 0x00 */
-	char unused0[12];	/* 0x04 */
+	uint32_t unused0[3];	/* 0x04 */
 	uint32_t cfg;		/* 0x10 */
-	char unused1[12];	/* 0x14 */
+	uint32_t unused1[3];	/* 0x14 */
 	uint32_t eoi;		/* 0x20 */
 	uint32_t irqstat_raw;	/* 0x24 */
 	uint32_t irqstat;	/* 0x28 */
@@ -53,9 +57,9 @@ struct gptimer13_1 {
 	uint32_t tclr;		/* 0x38 */
 	uint32_t tcrr;		/* 0x3c */
 	uint32_t tldr;		/* 0x40 */
-} __attribute__((packed));
+} __attribute__((packed)) __attribute__((aligned(sizeof(uint32_t))));
 
-irq_return_t ti8168_clock_handler(unsigned int irq_nr, void *data) {
+static irq_return_t ti8168_clock_handler(unsigned int irq_nr, void *data) {
 	volatile struct gptimer13_1 *gptimer = TI8168_GPTIMER1_BASE;
 
 	clock_tick_handler(irq_nr, data);
@@ -114,3 +118,17 @@ static int ti8168_clk_init(void) {
 	clock_source_register(&ti8168_clk_clock_source);
 	return irq_attach(ti8168_clk_event.irq_nr, ti8168_clock_handler, 0, &ti8168_clk_clock_source, "omap3_clk");
 }
+
+static struct periph_memory_desc dm816x_timer_mem = {
+	.start = (uint32_t) TI8168_GPTIMER1_BASE,
+	.len   = 0x1000,
+};
+
+PERIPH_MEMORY_DEFINE(dm816x_timer_mem);
+
+static struct periph_memory_desc dm816x_timer_cm_mem = {
+	.start = (uint32_t) 0x48180000,
+	.len   = 0x1000,
+};
+
+PERIPH_MEMORY_DEFINE(dm816x_timer_cm_mem);
