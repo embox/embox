771661ad8e7ed6a18b799de57e9a365565c162af
Irina Len <ia.len@mail.ru>
2018-05-19 09:11:07 +0300

    Implement xen timer
    Co-authored-by: Andrey Chernov <a.o.chernov@mail.ru>

--- src/drivers/clock/xen.c
+++ src/drivers/clock/xen.c
@@ -8,11 +8,11 @@
 #include <hal/clock.h>
 #include <kernel/time/clock_source.h>
 #include <kernel/irq.h>
+#include <kernel/panic.h>
+#include <xen/event_channel.h>
+#include <xen_hypercall-x86_32.h>
 
 #include <embox/unit.h>
-
-// FIXME
-#define CLOCK_IRQ 0
 
 EMBOX_UNIT_INIT(xen_clock_init);
 
@@ -24,7 +24,7 @@ static struct time_event_device xen_event_device = {
 	.name = "xen event device",
 	.config = integratorcp_clock_setup,
 	.event_hz = 1000,
-	.irq_nr = CLOCK_IRQ
+	.irq_nr = -1
 };
 
 static cycle_t xen_tcd_read(void) {
@@ -44,12 +44,24 @@ static struct clock_source xen_cs = {
 };
 
 static irq_return_t clock_handler(unsigned int irq_nr, void *dev_id) {
-	clock_tick_handler(irq_nr, dev_id);
+	for (int i = 0; i < 10; i++) {
+		clock_tick_handler(irq_nr, dev_id);
+	}
 	return IRQ_HANDLED;
 }
 
-static int xen_clock_init(void) {
-	clock_source_register(&xen_cs);
 
-	return irq_attach(CLOCK_IRQ, clock_handler, 0, &xen_cs, "xen clock irq");
+static int xen_clock_init(void) {
+
+	clock_source_register(&xen_cs);
+	evtchn_bind_virq_t op;
+
+	op.virq = VIRQ_TIMER;
+	op.vcpu = 0;
+	if (HYPERVISOR_event_channel_op(EVTCHNOP_bind_virq, &op) != 0) {
+		panic("Error has happened during timer initialization.\n");
+	}
+	xen_event_device.irq_nr = op.port;
+
+	return irq_attach(op.port, clock_handler, 0, &xen_cs, "xen clock irq");
 }
