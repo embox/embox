5a10dd5d74c34ff66647f578ed53031ae8e3c931
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-06-08 19:50:17 +0300

    tests: Fix kernel/task/select_test

--- src/tests/kernel/task/select_test.c
+++ src/tests/kernel/task/select_test.c
@@ -7,62 +7,76 @@
  * @author Alexander Kalmuk
  */
 
-#include <embox/test.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/select.h>
+
+#include <util/math.h>
+
 #include <kernel/task.h>
 #include <kernel/sched.h>
-#include <sys/select.h>
-#include <math.h>
+
+#include <embox/test.h>
+
 
 EMBOX_TEST_SUITE("select test");
 
-static int pipefd1[2], pipefd2[2];
-static int active_desc_cnt;
-static fd_set readfds;
-static bool select_hnd_exist = true;
+struct task_args {
+	int pipefd1[2];
+	int pipefd2[2];
+	int active_desc_cnt;
+	fd_set readfds;
+	bool select_hnd_exist;
+};
 
 static void *select_hnd(void *arg) {
-	int nfds = max(pipefd1[0], pipefd2[0]) + 1;
+	struct task_args *volatile args = arg;
+	int nfds;
 
-	FD_ZERO(&readfds);
-	FD_SET(pipefd1[0], &readfds);
-	FD_SET(pipefd2[0], &readfds);
+	args = arg;
+	nfds = max(args->pipefd1[0], args->pipefd2[0]) + 1;
+
+	FD_ZERO(&args->readfds);
+	FD_SET(args->pipefd1[0], &args->readfds);
+	FD_SET(args->pipefd2[0], &args->readfds);
 
 	/* Select will change active_desc_cnt and readfds. */
-	active_desc_cnt = select(nfds, &readfds, NULL, NULL, NULL);
+	args->active_desc_cnt = select(nfds, &args->readfds, NULL, NULL, NULL);
 
-	select_hnd_exist = false;
+	args->select_hnd_exist = false;
 
 	return NULL;
 }
 
 TEST_CASE("select on pipe ends") {
+	struct task_args volatile args;
+
+	args.select_hnd_exist = true;
 	/* Create two pipes */
-	assert(pipe(pipefd1) >= 0);
-	assert(pipe(pipefd2) >= 0);
+	test_assert(pipe((int *)args.pipefd1) >= 0);
+	test_assert(pipe((int *)args.pipefd2) >= 0);
 
 	/* Create new task and sleep for 5 seconds to catch on sleeping in select
 	 * in created task */
-	new_task("", select_hnd, &readfds);
+	new_task("", select_hnd, (void*)&args);
 
 	/* Used to switch to select_hnd */
 	sleep(0);
 
 	/* Write something in pipe. In other task we will wake up and check
 	 * witch ends of pipe are active */
-	write(pipefd1[1], "a", 1);
+	write(args.pipefd1[1], "a", 1);
 
-	while (select_hnd_exist) {}
+	while (args.select_hnd_exist) {}
 
 	/* Assertions after select */
-	test_assert(active_desc_cnt == 1);
-	test_assert(FD_ISSET(pipefd1[0], &readfds));
-	test_assert(!FD_ISSET(pipefd2[0], &readfds));
+	test_assert(args.active_desc_cnt == 1);
+	test_assert(FD_ISSET(args.pipefd1[0], &args.readfds));
+	test_assert(!FD_ISSET(args.pipefd2[0], &args.readfds));
 
 	/* close pipes */
-	close(pipefd1[0]);
-	close(pipefd1[1]);
-	close(pipefd2[0]);
-	close(pipefd2[1]);
+	close(args.pipefd1[0]);
+	close(args.pipefd1[1]);
+	close(args.pipefd2[0]);
+	close(args.pipefd2[1]);
 }
