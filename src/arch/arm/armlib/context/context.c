722148a33694c1f4afc55ee379bf5c297e91da2a
Alex Kalmuk <alexkalmuk@gmail.com>
2018-06-18 18:40:02 +0300

    arm: Assert SP aligment in context_init

--- src/arch/arm/armlib/context/context.c
+++ src/arch/arm/armlib/context/context.c
@@ -9,17 +9,27 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <assert.h>
 #include <hal/context.h>
 
 #include <asm/modes.h>
 
 #include <arm/fpu.h>
 
+/* In the RVCT v2.0 and above, all generated code and C library code
+ * will maintain eight-byte stack alignment on external interfaces. */
+#define ARM_SP_ALIGNMENT 8
+
 void context_init(struct context *ctx, unsigned int flags,
 		void (*routine_fn)(void), void *sp) {
 	ctx->lr = (uint32_t) routine_fn;
 	ctx->sp = (uint32_t) sp;
 	ctx->cpsr = ARM_MODE_SYS;
+
+	assertf(((uint32_t) sp % ARM_SP_ALIGNMENT) == 0,
+		"Stack pointer is not aligned to 8 bytes.\n"
+		"Firstly please make sure the thread stack size is aligned to 8 bytes"
+	);
 
 	if (flags & CONTEXT_IRQDISABLE) {
 		ctx->cpsr |= I_BIT | F_BIT;
