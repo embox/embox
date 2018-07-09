d6450d1638d63ba484d8deba935f4202992de6f1
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-07-09 18:44:23 +0300

    drivers: Update am85c30 serial driver

--- src/drivers/serial/e2k/am85c30.h
+++ src/drivers/serial/e2k/am85c30.h
@@ -10,42 +10,70 @@
 
 /* AM85C30 WRITE Registers */
 
-#define		AM85C30_WR0 		0x00
-#define 	AM85C30_WR1		0x01
-#define 	AM85C30_WR2		0x02
-#define 	AM85C30_WR3		0x03
-#define 	AM85C30_WR4		0x04
-#define 	AM85C30_WR5		0x05
-#define 	AM85C30_WR6		0x06
-#define 	AM85C30_WR7		0x07
-#define 	AM85C30_WR8		0x08
-#define 	AM85C30_WR9		0x09
-#define 	AM85C30_WR10		0x0a
-#define 	AM85C30_WR11		0x0b
-#define 	AM85C30_WR12		0x0c
-#define 	AM85C30_WR13		0x0d
-#define 	AM85C30_WR14		0x0e
-#define 	AM85C30_WR15		0x0f
+#define AM85C30_WR0      0x00
+#define AM85C30_WR1      0x01
+#define AM85C30_WR2      0x02
+#define AM85C30_WR3      0x03
+#define AM85C30_WR4      0x04
+#define AM85C30_WR5      0x05
+#define AM85C30_WR6      0x06
+#define AM85C30_WR7      0x07
+#define AM85C30_WR8      0x08
+#define AM85C30_WR9      0x09
+#define AM85C30_WR10     0x0a
+#define AM85C30_WR11     0x0b
+#define AM85C30_WR12     0x0c
+#define AM85C30_WR13     0x0d
+#define AM85C30_WR14     0x0e
+#define AM85C30_WR15     0x0f
 
 /* READ (Status) Registers */
 
-#define		AM85C30_RR0		0x00
-#define 	AM85C30_RR1		0x01
-#define		AM85C30_RR2		0x02
-#define		AM85C30_RR3		0x03
-#define		AM85C30_RR8		0x08
-#define		AM85C30_RR10		0x0a
-#define		AM85C30_RR12		0x0c
-#define		AM85C30_RR13		0x0d
+#define AM85C30_RR0      0x00
+#define AM85C30_RR1      0x01
+#define AM85C30_RR2      0x02
+#define AM85C30_RR3      0x03
+#define AM85C30_RR8      0x08
+#define AM85C30_RR10     0x0a
+#define AM85C30_RR12     0x0c
+#define AM85C30_RR13     0x0d
 
-#define		AM85C30_D0		(0x01 << 0)
-#define		AM85C30_D1		(0x01 << 1)
-#define		AM85C30_D2		(0x01 << 2)
-#define		AM85C30_D3		(0x01 << 3)
-#define		AM85C30_D4		(0x01 << 4)
-#define		AM85C30_D5		(0x01 << 5)
-#define		AM85C30_D6		(0x01 << 6)
-#define		AM85C30_D7		(0x01 << 7)
+#define AM85C30_D0       (0x01 << 0) /* Rx Character Available */
+#define AM85C30_D1       (0x01 << 1)
+#define AM85C30_D2       (0x01 << 2) /* Tx Buffer empty */
+#define AM85C30_D3       (0x01 << 3)
+#define AM85C30_D4       (0x01 << 4)
+#define AM85C30_D5       (0x01 << 5)
+#define AM85C30_D6       (0x01 << 6)
+#define AM85C30_D7       (0x01 << 7)
+
+#if 0
+#define	NULLCODE	0	/* Null Code */
+#define	POINT_HIGH	0x8	/* Select upper half of registers */
+#define	RES_EXT_INT	0x10	/* Reset Ext. Status Interrupts */
+#define	SEND_ABORT	0x18	/* HDLC Abort */
+#define	RES_RxINT_FC	0x20	/* Reset RxINT on First Character */
+#define	RES_Tx_P	0x28	/* Reset TxINT Pending */
+#define	ERR_RES		0x30	/* Error Reset */
+#define	RES_H_IUS	0x38	/* Reset highest IUS */
+#endif
+
+/* Read Register 1 */
+#define AM85C30_ALL_SNT   0x1  /* All sent */
+/* Residue Data for 8 Rx bits/char programmed */
+#define AM85C30_RES3      0x8  /* 0/3 */
+#define AM85C30_RES4      0x4  /* 0/4 */
+#define AM85C30_RES5      0xc  /* 0/5 */
+#define AM85C30_RES6      0x2  /* 0/6 */
+#define AM85C30_RES7      0xa  /* 0/7 */
+#define AM85C30_RES8      0x6  /* 0/8 */
+#define AM85C30_RES18     0xe  /* 1/8 */
+#define AM85C30_RES28     0x0  /* 2/8 */
+/* Special Rx Condition Interrupts */
+#define AM85C30_PAR_ERR   0x10 /* Parity error */
+#define AM85C30_Rx_OVR    0x20 /* Rx Overrun Error */
+#define AM85C30_CRC_ERR   0x40 /* CRC/Framing Error */
+#define AM85C30_END_FR    0x80 /* End of Frame (SDLC) */
 
 /* WR0 */
 /*  	D2,D1,D0
