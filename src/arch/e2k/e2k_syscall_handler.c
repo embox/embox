b772580bb736aa1da7137aefe1b72a880ec4a8c4
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-08-02 19:34:31 +0300

    e2k: Rename e2k_syscall.c->e2k_syscall_handler.c

--- src/arch/e2k/e2k_syscall_handler.c
+++ src/arch/e2k/e2k_syscall_handler.c
@@ -8,8 +8,12 @@
 
 #include <stdint.h>
 
+#include <kernel/printk.h>
+
 unsigned long cpuSyscall(int number, uint64_t arg1, uint64_t arg2,
 			 uint64_t arg3, uint64_t arg4)
 {
+	printk("hhhhhhhh syscall %d (%x, %x, %x, %x)\n", arg1, arg2, arg3, arg4);
+	//while (1);
 	return 0;
 }
