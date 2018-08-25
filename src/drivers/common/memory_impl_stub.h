e6f27817a8170a34f4d8d836607a455a7935a6ee
Alex Kalmuk <alexkalmuk@gmail.com>
2018-08-26 02:16:30 +0300

    mem: Add stub impl for periph_memory

---
+++ src/drivers/common/memory_impl_stub.h
@@ -0,0 +1,21 @@
+/**
+ * @file memory.h
+ * @brief
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version
+ * @date 2016-08-11
+ */
+
+#ifndef _DRIVERS_COMMON_MEMORY_H
+#define _DRIVERS_COMMON_MEMORY_H
+
+#include <stdint.h>
+
+struct periph_memory_desc {
+	uint32_t start;
+	uint32_t len;
+};
+
+#define PERIPH_MEMORY_DEFINE(_mem_desc)
+
+#endif /* _DRIVERS_COMMON_MEMORY_H */
