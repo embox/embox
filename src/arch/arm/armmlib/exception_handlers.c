2200d186567c10203e0d775a00921621f5e2c9b7
Alex Kalmuk <alexkalmuk@gmail.com>
2018-08-30 00:05:00 +0300

    cortex-m: Improve info on exception occurance

--- src/arch/arm/armmlib/exception_handlers.c
+++ src/arch/arm/armmlib/exception_handlers.c
@@ -10,16 +10,20 @@
 #include <kernel/panic.h>
 #include <kernel/printk.h>
 #include <hal/context.h>
+#include <hal/reg.h>
 
-#define NMI        2
-#define HARD_FAULT 3
+/* SCB - System Control Block */
+#define SCB_BASE 0xe000ed00
+#define SCB_CONF_FAULT_STATUS (SCB_BASE + 0x28)
+#define SCB_HARD_FAULT_STATUS  (SCB_BASE + 0x2C)
 
-static void nmi_handler(void) {
-	panic("\n%s\n", __func__);
-}
+static void print_fault_status(void) {
+	uint32_t conf_faults = REG_LOAD(SCB_CONF_FAULT_STATUS);
 
-static void hard_fault(void) {
-	panic("\n%s\n", __func__);
+	printk("Memory Management Fault Status register = %x\n", conf_faults & 0xf);
+	printk("Bus Fault Status register = %x\n", (conf_faults >> 8) & 0xf);
+	printk("Usage Fault Status register = %x\n", (conf_faults >> 16) & 0xff);
+	printk("Hard Fault Status register = %lx\n", REG_LOAD(SCB_HARD_FAULT_STATUS));
 }
 
 /* Print exception info and return */
@@ -27,20 +31,13 @@ void exc_default_handler(struct exc_saved_base_ctx *ctx, int xpsr) {
 	int exp_nr = xpsr & 0xFF;
 
 	printk("\nEXCEPTION (0x%x):\n"
-			"r0=%08x r1=%08x r2=%08x r3=%08x\n"
-			"r12=%08x lr=%08x pc=%08x xPSR=%08x\n",
+			"Exception saved context:\n"
+			"  r0=%08x r1=%08x r2=%08x r3=%08x\n"
+			"  r12=%08x lr=%08x pc=%08x xPSR=%08x\n",
 			exp_nr,
 			ctx->r[0], ctx->r[1], ctx->r[2], ctx->r[3],
 			ctx->r[4], ctx->lr, ctx->pc, ctx->psr);
 
-	switch (exp_nr) {
-	case NMI:
-		nmi_handler();
-		break;
-	case HARD_FAULT:
-		hard_fault();
-		break;
-	default:
-		break;
-	}
+	print_fault_status();
+	panic("\n%s\n", __func__);
 }
