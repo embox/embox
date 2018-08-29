62e466edfdc66d9a9a9e51210fb8823c998af888
Alex Kalmuk <alexkalmuk@gmail.com>
2018-08-30 00:07:56 +0300

    third_party: Add arch.h with speciefic platform definitions

---
+++ third-party/bsp/stmf4cube/arch.h
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Alexamder Kalmuk
+ * @date    29.08.2018
+ */
+
+#ifndef STM32F4_ARCH_H_
+#define STM32F4_ARCH_H_
+
+/* Derive it from core_cm4.h */
+#define NVIC_PRIO_BITS 4
+
+#define IRQ_PRIO_SHIFT NVIC_PRIO_BITS
+
+#endif /* STM32F4_ARCH_H_ */
