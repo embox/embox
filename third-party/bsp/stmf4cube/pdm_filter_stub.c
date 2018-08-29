b612df775fe720c879ef2d2b28f49f7ae83b9c5d
Alex Kalmuk <alexkalmuk@gmail.com>
2018-08-29 23:59:45 +0300

    stm32f4: Add stub for PDM sound filter

---
+++ third-party/bsp/stmf4cube/pdm_filter_stub.c
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Alexander Kalmuk
+ * @date    24.08.2018
+ */
+
+#include <assert.h>
+
+#include <stm32f4_discovery.h>
+
+#include <pdm_filter.h>
+
+void PDM_Filter_Init(PDMFilter_InitStruct * Filter) {
+	assert(0);
+}
+
+int32_t PDM_Filter_64_MSB(uint8_t* data, uint16_t* dataOut,
+		uint16_t MicGain, PDMFilter_InitStruct * Filter) {
+	assert(0);
+	return -1;
+}
+
+int32_t PDM_Filter_64_LSB(uint8_t* data, uint16_t* dataOut,
+		uint16_t MicGain, PDMFilter_InitStruct * Filter) {
+	assert(0);
+	return -1;
+}
