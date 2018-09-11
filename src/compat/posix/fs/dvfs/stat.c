3a1d9cc939f55345d4ed7899fb39e002cb64fd2c
Deryugin <deryugin.denis@gmail.com>
2018-09-11 17:34:45 +0300

    dvfs/stat: Use fstat instead of stub

--- src/compat/posix/fs/dvfs/stat.c
+++ src/compat/posix/fs/dvfs/stat.c
@@ -6,31 +6,23 @@
  */
 #include <errno.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 #include <fs/dvfs.h>
 
 int stat(const char *path, struct stat *buf) {
-	struct lookup l;
-	int res = dvfs_lookup(path, &l);
-	struct dentry *d = l.item;
+	int fd, ret;
 
-	if (res < 0) {
-		errno = -res;
-		return -1;
-	}
+	assert(path);
+	assert(buf);
 
-	assert(d);
+	fd = open(path, O_RDONLY); /* Actually, flag should be smth O_PATH */
 
-	*buf = (struct stat) {
-		.st_dev     = (d && d->d_sb && d->d_sb->bdev) ? d->d_sb->bdev->id : 0,
-		.st_ino     = d->d_inode->i_no,
-		.st_nlink   = 1,
-		.st_size    = (d && d->d_inode) ? d->d_inode->length : 0,
-		.st_blksize = 512,
-		.st_mode    = d->flags & (S_IFMT | S_IRWXA),
-	};
+	ret = fstat(fd, buf);
 
-	return 0;
+	close(fd);
+
+	return ret;
 }
 
 int lstat(const char *path, struct stat *buf) {
