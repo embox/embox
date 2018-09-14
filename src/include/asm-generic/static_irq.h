aa8e1074dc3e5d8aa97919c6b64c34b4bcfdf0da
Alex Kalmuk <alexkalmuk@gmail.com>
2018-09-14 16:44:19 +0300

    Improve static irqs designing irq table as a separate section

--- src/include/asm-generic/static_irq.h
+++ src/include/asm-generic/static_irq.h
@@ -14,27 +14,16 @@
 
 #ifdef STATIC_IRQ_EXTENTION
 
-#define SIRQ_TABLE_ENTRY(n) \
-	. = n * 4; \
-	*(.trap_table.routine_##n);
-
-/* Currently, we allow to define handlers starting from
- * SVCall, please take a look at exception_table.S and
- * http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0553a/CIHIGCIF.html */
 #define STATIC_IRQ_TABLE \
-	SIRQ_TABLE_ENTRY(11) \
-	SIRQ_TABLE_ENTRY(12) \
-	SIRQ_TABLE_ENTRY(13) \
-	SIRQ_TABLE_ENTRY(14) \
-	SIRQ_TABLE_ENTRY(15) \
-	SIRQ_TABLE_ENTRY(63) \
-	SIRQ_TABLE_ENTRY(77)
+	__static_irq_table_start = .; \
+	*(.static_irq_table*); \
+	__static_irq_table_end = .;
 
 #define ARM_M_IRQ_HANDLER_VAR_NAME(irq_num) \
 	arm_m_irq_handler##irq_num
 
 #define ARM_M_IRQ_HANDLER_SECTION_STR(irq_num) \
-	".trap_table.routine_"#irq_num
+	".static_irq_table.routine_"#irq_num
 
 #define ARM_M_IRQ_HANDLER_DEF(irq_num, irq_handler) \
 	void * ARM_M_IRQ_HANDLER_VAR_NAME(irq_num) \
