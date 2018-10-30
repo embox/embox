93d26a07a96d704e1bfe7c55813e2589bdf10d5e
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-10-30 17:43:42 +0300

    compat: Clean stdbool.h

--- src/compat/libc/include/stdbool.h
+++ src/compat/libc/include/stdbool.h
@@ -10,37 +10,22 @@
 #ifndef STDBOOL_H_
 #define STDBOOL_H_
 
-#ifndef __cplusplus
+ /* Don't define bool, true, and false in C++, except as a GNU extension. */
+ #ifndef __cplusplus
+ #define bool _Bool
+ #define true 1
+ #define false 0
+ #elif defined(__GNUC__) && !defined(__STRICT_ANSI__)
+ /* Define _Bool as a GNU extension. */
+ #define _Bool bool
+ #if __cplusplus < 201103L
+ /* For C++98, define bool, false, true as a GNU extension. */
+ #define bool  bool
+ #define false false
+ #define true  true
+ #endif
+ #endif
 
-#if defined(__STDC__) && defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
-//typedef _Bool bool;
-#define bool _Bool
-
-#else
-/**
- * The is no reason to follow strict ABI in kernel code and to define boolean
- * type as 1-byte _Bool. Make it native word sized.
- *
- * TODO (see below) -- Eldar
- * May be I'm not right and it would be better to introduce something like
- * bool_t to avoid confusion with bool defined by C99 standard.
- */
-typedef int bool;
-#endif
-
-#define false 0
-#define true  1
-
-#else /* __cplusplus */
-
-/* Supporting <stdbool.h> in C++ is a GCC extension.  */
-#define _Bool   bool
-#define bool    bool
-#define false   false
-#define true    true
-
-#endif /* __cplusplus */
-
-#define __bool_true_false_are_defined 1
+ #define __bool_true_false_are_defined 1
 
 #endif /* STDBOOL_H_ */
