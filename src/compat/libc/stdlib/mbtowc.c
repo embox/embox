3860257958d67ee45845efad4737417fcd29e64f
filipp <filipp.chubukov@mail.ru>
2018-12-06 16:44:09 +0300

    add: mbtowc function

---
+++ src/compat/libc/stdlib/mbtowc.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @brief mbtowc function.
+ *
+ * @see stdlib.h
+ *
+ * @date 01.11.18
+ * @author Chubukov Filipp
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+int mbtowc(wchar_t *wcstring, char *mbstring, size_t max) {
+	return 0;
+}
