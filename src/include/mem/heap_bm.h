26c828c8b78c2a926292ae4543455d4bdc8bbed8
Alex Kalmuk <alexkalmuk@gmail.com>
2019-01-09 17:47:19 +0300

    heap_bm: Maintain count of allocated objects in heap_bm

--- src/include/mem/heap_bm.h
+++ src/include/mem/heap_bm.h
@@ -11,8 +11,9 @@
 
 #include <sys/types.h>
 
-extern void bm_init(void *segment, size_t size);
-extern void *bm_memalign(void *segment, size_t boundary, size_t size);
-extern void bm_free(void *segment, void *ptr);
+extern void bm_init(void *heap, size_t size);
+extern void *bm_memalign(void *heap, size_t boundary, size_t size);
+extern void bm_free(void *heap, void *ptr);
+extern int bm_heap_is_empty(void *heap);
 
 #endif /* MEM_HEAP_BM_H_ */
