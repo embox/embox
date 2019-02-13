b0288a4bdd3903ca69502597684d0756e9e5b6c0
Alex Kalmuk <alexkalmuk@gmail.com>
2019-02-13 19:25:11 +0300

    travis: Add build.sh instead of weird travis_wait
    Previously, we added travis_wait since some builds take more
    than 10 minutes. Then, after redirection build log to file, we
    just tail the log after build finished. This solution leads to
    some weird behaviour of travis_wait + docker. So, we decided
    just to call build.sh script with echoing something every 5 minutes.

---
+++ scripts/continuous/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Since travis_wait doesn't work with docker well somewhy,
+# just use bell which prints something to stdout every 5 minutes.
+function bell() {
+	i=0
+	while true :
+	do
+		sleep 5m
+		i=$(($i+5))
+		echo "CI: Still building (minutes=$i)"
+	done
+}
+
+bell &
+bell_pid=$!
+
+make &> build.log
+RETVAL=$?
+tail -c 3M build.log
+
+# https://github.com/travis-ci/travis-ci/issues/6018 -- Prevert log truncation
+# on error. There are also many issues which refer to this one.
+sleep 5
+
+kill $bell_pid
+exit $RETVAL
