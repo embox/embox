d16ad14862e063ec475438302f8c677a04b3313f
Alex Kalmuk <alexkalmuk@gmail.com>
2019-02-18 22:35:15 +0300

    net: Fix sock_hash

--- src/net/socket/sock_hash.c
+++ src/net/socket/sock_hash.c
@@ -6,18 +6,28 @@
  */
 #include <net/sock.h>
 #include <util/dlist.h>
+#include <hal/ipl.h>
 
 void sock_hash(struct sock *sk) {
+	ipl_t ipl;
+
 	assert(sk != NULL);
 	assert(sk->p_ops != NULL);
 	assert(dlist_empty_entry(sk, lnk));
 
+	/* TODO Probably, it's better to use spinlock here */
+	ipl = ipl_save();
 	dlist_add_prev_entry(sk, sk->p_ops->sock_list, lnk);
+	ipl_restore(ipl);
 }
 
 void sock_unhash(struct sock *sk) {
+	ipl_t ipl;
+
 	assert(sk != NULL);
 	assert(!dlist_empty_entry(sk, lnk));
 
+	ipl = ipl_save();
 	dlist_del_init_entry(sk, lnk);
+	ipl_restore(ipl);
 }
