4b4d0f6b918f6f074d35b77aa53586592c939935
nastya-nizharadze <naniti@bk.ru>
2019-02-22 23:52:14 +0300

    add net driver stub for xen

---
+++ src/drivers/net/xen_pv_net/xen_pv_net.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @brief Xen PV net driver
+ *
+ * @author
+ * @date
+ */
+
+#include <errno.h>
+#include <embox/unit.h>
+
+EMBOX_UNIT_INIT(xennet_init);
+
+static int xennet_init(void) {
+    return 0;
+}
