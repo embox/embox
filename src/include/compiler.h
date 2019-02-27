a51907c0afa12bd838bc9f1f48e2d96354a8b7cf
Anton Bondarev <anton.bondarev2310@gmail.com>
2019-02-27 11:29:48 +0300

    utils: Fix _NORETURN macro for c++11

--- src/include/compiler.h
+++ src/include/compiler.h
@@ -9,17 +9,17 @@
 #ifndef COMPILER_H_
 #define COMPILER_H_
 
-#ifdef __GNUC__
-
+#if defined(__GNUC__) && !defined(__cplusplus)
 #define _NORETURN __attribute__ ((noreturn))
+#else /* __GNUC__ */
+#define _NORETURN
+#endif /* __GNUC__ */
+
+#ifdef __GNUC__
 #define _PRINTF_FORMAT(format_i, arg_start_i) \
 	__attribute__ ((format (printf, format_i, arg_start_i)))
-
 #else /* __GNUC__ */
-
-#define _NORETURN
 #define _PRINTF_FORMAT(format_i, arg_start_i)
-
 #endif /* __GNUC__ */
 
 #if (!defined(__STDC__) || !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L) && \
