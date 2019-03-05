1def36a8d405ecdead594a0a4cd591d61015dd3e
Mark Kovalev <restonich@gmail.com>
2019-03-05 20:45:52 +0300

    drivers/net/xen_pv_net: xenstore info

--- src/drivers/net/xen_pv_net/xen_pv_net.c
+++ src/drivers/net/xen_pv_net/xen_pv_net.c
@@ -13,66 +13,67 @@
 #include <xenstore.h>
 #include <kernel/printk.h>
 
-#define MAX_KEY_LENGTH 		64
-#define MAX_VALUE_LENGTH 	64
+#define XS_MAX_KEY_LENGTH 		256
+#define XS_MAX_VALUE_LENGTH 	1024
+#define XS_MAX_NAME_LENGTH 		16
+#define XS_MAX_DOMID_LENGTH 	4
+
+#define XS_LS_NOT_RECURSIVE		0
+#define XS_LS_RECURSIVE			1
 
 EMBOX_UNIT_INIT(xennet_init);
 
-static int xennet_init(void) {
-	char key[MAX_KEY_LENGTH], value[MAX_VALUE_LENGTH],
-		 domid[8], values[128];
+int xenstore_ls_val(char *ls_key, int flag) {
+	char ls_value[XS_MAX_VALUE_LENGTH];
+	char key[XS_MAX_KEY_LENGTH], value[XS_MAX_VALUE_LENGTH];
 	int r;
+	char *output;
 
-	memset(&key, 0, MAX_KEY_LENGTH);
-	memset(&value, 0, MAX_VALUE_LENGTH);
-	strcpy(key, "vm");
-	xenstore_read(key, value, MAX_VALUE_LENGTH);
-	printk("\n\n"
-		   "--- KEY:%s ---\n"
-		   "--- VALUE:%s ---\n\n",
-		   key, value);
+	memset(&ls_value, 0, XS_MAX_VALUE_LENGTH);
+	r = xenstore_ls(ls_key, ls_value, XS_MAX_VALUE_LENGTH);
 
-	memset(&key, 0, MAX_KEY_LENGTH);
-	memset(&value, 0, MAX_VALUE_LENGTH);
-	strcpy(key, "name");
-	xenstore_read(key, value, MAX_VALUE_LENGTH);
-	printk("\n\n"
-		   "--- KEY:%s ---\n"
-		   "--- VALUE:%s ---\n\n",
-		   key, value);
+	if (r <= 0)
+		return r;
 
-	memset(&key, 0, MAX_KEY_LENGTH);
-	memset(&value, 0, MAX_VALUE_LENGTH);
-	strcpy(key, "domid");
-	xenstore_read(key, value, MAX_VALUE_LENGTH);
-	printk("\n\n"
-		   "--- KEY:%s ---\n"
-		   "--- VALUE:%s ---\n\n",
-		   key, value);
-	strcpy(domid, value);
+	output = ls_value;
+	while(*output != 0) {
+		memset(&key, 0, XS_MAX_KEY_LENGTH);
+		memset(&value, 0, XS_MAX_VALUE_LENGTH);
+		sprintf(key, "%s/%s", ls_key, output);
+		xenstore_read(key, value, XS_MAX_VALUE_LENGTH);
+		printk("%s = \"%s\"\n", key, value);
 
-	memset(&key, 0, MAX_KEY_LENGTH);
-	memset(&value, 0, MAX_VALUE_LENGTH);
-	strcpy(key, "memory/static-max");
-	xenstore_read(key, value, MAX_VALUE_LENGTH);
-	printk("\n\n"
-		   "--- KEY:%s ---\n"
-		   "--- VALUE:%s ---\n\n",
-		   key, value);
+		if (flag == XS_LS_RECURSIVE)
+			xenstore_ls_val(key, XS_LS_RECURSIVE);
 
-	memset(&key, 0, MAX_KEY_LENGTH);
-	memset(&values, 0, 128);
-	sprintf(key, "console");
-	r = xenstore_ls(key, values, MAX_VALUE_LENGTH);
-	if (r < 0) {
-		printk("\n\nERROR\n\n");
-		return 0;
+		output += strlen(output) + 1;
 	}
-	printk("\n\n"
-		   "--- KEY:%s ---\n"
-		   "--- VALUE:\n%s ---\n\n",
-		   key, values);
+	return r;
+}
 
+static void xenstore_info() {
+	char key[XS_MAX_KEY_LENGTH];
+	char name[XS_MAX_NAME_LENGTH], domid[XS_MAX_DOMID_LENGTH];
+
+	printk("\n --- XenStore Info Begin ---\n");
+	memset(&name, 0, XS_MAX_NAME_LENGTH);
+	xenstore_read("name", name, XS_MAX_NAME_LENGTH);
+	printk("\nname = %s\n", name);
+
+	memset(&domid, 0, XS_MAX_DOMID_LENGTH);
+	xenstore_read("domid", domid, XS_MAX_DOMID_LENGTH);
+	printk("\ndomid = %s\n", domid);
+
+	printk("\nDomain Home Path contents:\n");
+	memset(&key, 0, XS_MAX_KEY_LENGTH);
+	sprintf(key, "/local/domain/%s", domid);
+	xenstore_ls_val(key, XS_LS_RECURSIVE);
+
+	printk("\n --- XenStore Info End ---\n");
+}
+
+static int xennet_init(void) {
+	xenstore_info();
 
     return 0;
 }
