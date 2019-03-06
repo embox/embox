a2945eecdf64c5e902d0cb18c3ecf5510145bb6b
Mark Kovalev <restonich@gmail.com>
2019-03-06 17:30:51 +0300

    testing/mqtt_env: improvement of server and client

--- src/cmds/testing/mqtt_env/client.c
+++ src/cmds/testing/mqtt_env/client.c
@@ -1,92 +1,102 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
+// #include <arpa/inet.h>
+// #include <errno.h>
+#include <netdb.h> 
+// #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
-#define BUF_SIZE 500
+#define BUF_SIZE 	500
+#define TEST_PORT	"1337"
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     struct addrinfo hints;
-    struct addrinfo *result, *rp;
-    int sfd, s, j;
+    struct addrinfo *result, *res_it;
+    int sfd, s, i;
     size_t len;
     ssize_t nread;
-    char buf[BUF_SIZE];
+    char buf[BUF_SIZE], msg[BUF_SIZE];
+	/* for logging */
+	FILE *fd = fopen("log/client.log", "a");
+	char timestamp[BUF_SIZE];
+	time_t timer;
+	struct tm *cur_time;
 
-   if (argc < 3) {
-        fprintf(stderr, "Usage: %s host port msg...\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	/* Obtain address(es) matching host/port */
 
-   /* Obtain address(es) matching host/port */
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+	hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+	hints.ai_flags = 0;
+	hints.ai_protocol = 0;          /* Any protocol */
 
-   memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
-    hints.ai_flags = 0;
-    hints.ai_protocol = 0;          /* Any protocol */
+	s = getaddrinfo("10.0.2.16", TEST_PORT, &hints, &result);
+	if (s != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+		exit(EXIT_FAILURE);
+	}
 
-   s = getaddrinfo(argv[1], argv[2], &hints, &result);
-    if (s != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-        exit(EXIT_FAILURE);
-    }
+	/* getaddrinfo() returns a list of address structures.
+		Try each address until we successfully connect(2).
+		If socket(2) (or connect(2)) fails, we (close the socket
+		and) try the next address. */
 
-   /* getaddrinfo() returns a list of address structures.
-       Try each address until we successfully connect(2).
-       If socket(2) (or connect(2)) fails, we (close the socket
-       and) try the next address. */
+	for (res_it = result; res_it != NULL; res_it = res_it->ai_next) {
+		sfd = socket(res_it->ai_family, res_it->ai_socktype,
+					 res_it->ai_protocol);
+		if (sfd == -1)
+			continue;
 
-   for (rp = result; rp != NULL; rp = rp->ai_next) {
-        sfd = socket(rp->ai_family, rp->ai_socktype,
-                     rp->ai_protocol);
-        if (sfd == -1)
-            continue;
+		if (connect(sfd, res_it->ai_addr, res_it->ai_addrlen) != -1)
+			break;                  /* Success */
 
-       if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
-            break;                  /* Success */
+		close(sfd);
+	}
 
-       close(sfd);
-    }
+	if (res_it == NULL) {               /* No address succeeded */
+		fprintf(stderr, "Could not connect\n");
+		exit(EXIT_FAILURE);
+	}
 
-   if (rp == NULL) {               /* No address succeeded */
-        fprintf(stderr, "Could not connect\n");
-        exit(EXIT_FAILURE);
-    }
+	freeaddrinfo(result);           /* No longer needed */
 
-   freeaddrinfo(result);           /* No longer needed */
+	/* Send remaining command-line arguments as separate
+		datagrams, and read responses from server */
 
-   /* Send remaining command-line arguments as separate
-       datagrams, and read responses from server */
+	for (i = atoi(argv[1]); i <= 20; i+=2) {
+		sprintf(msg, "ping%i", i);
+		len = strlen(msg) + 1;
+		timer = time(NULL);
+		cur_time = localtime(&timer);
+		strftime(timestamp, BUF_SIZE, "%d-%m-%Y %I:%M:%S %p", cur_time);
 
-   for (j = 3; j < argc; j++) {
-        len = strlen(argv[j]) + 1;
-                /* +1 for terminating null byte */
+		if (len + 1 > BUF_SIZE) {
+			fprintf(stderr, "Message too long: %d\n", i);
+			continue;
+		}
+		nread = write(sfd, msg, len);
+		if (nread != len) {
+			fprintf(stderr, "partial/failed write\n");
+			exit(EXIT_FAILURE);
+		}
+		fprintf(fd, "[%s] Sent %ld bytes to server. Message: %s\n",
+					 timestamp, (long) nread, msg);
 
-       if (len + 1 > BUF_SIZE) {
-            fprintf(stderr,
-                    "Ignoring long message in argument %d\n", j);
-            continue;
-        }
+		nread = read(sfd, buf, BUF_SIZE);
+		if (nread == -1) {
+			perror("read");
+			exit(EXIT_FAILURE);
+		}
 
-       if (write(sfd, argv[j], len) != len) {
-            fprintf(stderr, "partial/failed write\n");
-            exit(EXIT_FAILURE);
-        }
+		printf("Received %ld bytes: %s\n", (long) nread, buf);
 
-       nread = read(sfd, buf, BUF_SIZE);
-        if (nread == -1) {
-            perror("read");
-            exit(EXIT_FAILURE);
-        }
+		fprintf(fd, "[%s] Recieved %ld bytes from server. Message: %s\n",
+					 timestamp, (long) nread, msg);
+	}
 
-       printf("Received %ld bytes: %s\n", (long) nread, buf);
-    }
-
-   exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
--- src/cmds/testing/mqtt_env/server.c
+++ src/cmds/testing/mqtt_env/server.c
@@ -1,13 +1,17 @@
-#include <sys/types.h>
+// #include <arpa/inet.h>
+// #include <errno.h>
+#include <netdb.h> 
+// #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <netdb.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
 #define BUF_SIZE 	500
-#define TEST_PORT	1337
+#define TEST_PORT	"1337"
 
 int main(int argc, char *argv[]) {
 	/* connection and socket listening */
@@ -16,17 +20,25 @@ int main(int argc, char *argv[]) {
     int sockfd;
 	/* message recieving */
     struct sockaddr_storage peer_addr;
-    socklen_t peer_addr_len;
+    socklen_t peer_addr_len = sizeof(struct sockaddr_storage);
     ssize_t nread;
-    char buf[BUF_SIZE];
+    char buf[BUF_SIZE], host[NI_MAXHOST], service[NI_MAXSERV];
 	/* function return check */
 	int r;
+	/* for logging */
+	FILE *fd = fopen("log/server.log", "a");
+	char timestamp[BUF_SIZE];
+	time_t timer;
+	struct tm *cur_time;
+	// char hostbuffer[BUF_SIZE];
+	// char *this_ip;
+	// struct hostent *host_entry;
 
 	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_INET;    /* Allow IPv4 or IPv6 */
-	hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
-	hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
-	hints.ai_protocol = 0;          /* Any protocol */
+	hints.ai_family = AF_INET;		/* Allow IPv4 or IPv6 */
+	hints.ai_socktype = SOCK_DGRAM;	/* Datagram socket */
+	hints.ai_flags = AI_PASSIVE;	/* For wildcard IP address */
+	hints.ai_protocol = 0;			/* Any protocol */
 
 	r = getaddrinfo(NULL, TEST_PORT, &hints, &result);
 	if (r != 0) {
@@ -41,41 +53,59 @@ int main(int argc, char *argv[]) {
 			continue;
 
 		if (bind(sockfd, res_it->ai_addr, res_it->ai_addrlen) == 0)
-			break;                  /* Success */
+			break;		/* Success */
 
 		close(sockfd);
 	}
 
-	if (res_it == NULL) {               /* No address succeeded */
+	if (res_it == NULL) {		/* No address succeeded */
 		fprintf(stderr, "Could not bind\n");
 		exit(EXIT_FAILURE);
 	}
 
-	freeaddrinfo(result);           /* No longer needed */
+	freeaddrinfo(result);
 
-	/* Read datagrams and echo them back to sender */
+	// gethostname(hostbuffer, BUF_SIZE);
+	// host_entry = gethostbyname(hostbuffer);
+	// // To convert an Internet network
+	// // address into ASCII string
+	// this_ip = inet_ntoa(*((struct in_addr*) host_entry->h_addr_list[0]));
 
-	for (;;) {
-		peer_addr_len = sizeof(struct sockaddr_storage);
+	while (1) {
 		nread = recvfrom(sockfd, buf, BUF_SIZE, 0,
-				(struct sockaddr *) &peer_addr, &peer_addr_len);
-		if (nread == -1)
-			continue;               /* Ignore failed request */
+						 (struct sockaddr *) &peer_addr, &peer_addr_len);
 
-		char host[NI_MAXHOST], service[NI_MAXSERV];
+		timer = time(NULL);
+		cur_time = localtime(&timer);
+		strftime(timestamp, BUF_SIZE, "%d-%m-%Y %I:%M:%S %p", cur_time);
+		
+		if (nread == -1) {
+			fprintf(fd, "[%s] Failed request.", timestamp);
+			continue;
+		}
 
 		r = getnameinfo((struct sockaddr *) &peer_addr,
 						peer_addr_len, host, NI_MAXHOST,
 						service, NI_MAXSERV, NI_NUMERICSERV);
-		if (r == 0)
+
+		if (r == 0) {
 			printf("Received %ld bytes from %s:%s\n",
-					(long) nread, host, service);
-		else
+				   (long) nread, host, service);
+			
+			fprintf(fd, "[%s] Received %ld bytes from %s:%s. Message: %s\n",
+						timestamp, (long) nread, host, service, buf);
+		} else {
 			fprintf(stderr, "getnameinfo: %s\n", gai_strerror(r));
 
-		if (sendto(sockfd, buf, nread, 0,
-					(struct sockaddr *) &peer_addr,
-					peer_addr_len) != nread)
+			fprintf(fd, "[%s] getnameinfo: %s\n", timestamp, gai_strerror(r));
+		}
+
+		if (nread != sendto(sockfd, buf, nread, 0, 
+							(struct sockaddr *) &peer_addr, peer_addr_len))
+		{
 			fprintf(stderr, "Error sending response\n");
+
+			fprintf(fd, "[%s] Failed to send response.", timestamp);
+		}
 	}
 }
