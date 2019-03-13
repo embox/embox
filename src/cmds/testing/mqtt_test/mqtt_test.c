90853f856c824570d892b10cdf71d68cc1a5899c
Mark Kovalev <restonich@gmail.com>
2019-03-13 14:18:38 +0300

    cmds/testing: add mqtt_test module

---
+++ src/cmds/testing/mqtt_test/mqtt_test.c
@@ -0,0 +1,35 @@
+#include "mqtt_test.h"
+
+int sock_connect(const char *nodename, const char *servname,
+				struct addrinfo *hints) {
+    struct addrinfo *result, *res_it;
+    int sockfd;
+	int r;
+
+	r = getaddrinfo(nodename, servname, hints, &result);
+	if (r != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
+		return (r < 0)? r : -r;
+	}
+
+	for (res_it = result; res_it != NULL; res_it = res_it->ai_next) {
+		sockfd = socket(res_it->ai_family, res_it->ai_socktype,
+						res_it->ai_protocol);
+		if (sockfd == -1)
+			continue;
+
+		if (0 == bind(sockfd, res_it->ai_addr, res_it->ai_addrlen))
+			break;		/* Success */
+
+		close(sockfd);
+	}
+
+	if (res_it == NULL) {		/* No address succeeded */
+		fprintf(stderr, "Could not bind\n");
+		return -ENOENT;
+	}
+
+	freeaddrinfo(result);
+
+	return sockfd;
+}
