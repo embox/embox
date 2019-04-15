ad68a1f17e2afb6654fe09ae34f5f2ee28338c82
Alex Kalmuk <alexkalmuk@gmail.com>
2019-04-16 00:28:04 +0300

    tests: cxx: Fix memory test

--- src/tests/c++/memory.cpp
+++ src/tests/c++/memory.cpp
@@ -28,9 +28,13 @@ EMBOX_TEST_SUITE_EXT("c++ memory test", NULL, NULL, case_setup, NULL);
 namespace {
 
 class Base {
+protected:
+	int field;
 public:
 	Base() { ++base_ctor; }
+	Base(int i) : field(i) { ++base_ctor; }
 	~Base() { ++base_dtor; }
+	int get_field(void) { return field; }
 };
 
 TEST_CASE("Class can allocated on stack") {
@@ -43,7 +47,7 @@ TEST_CASE("Class can allocated on stack") {
 	test_assert_equal(base_dtor, 1);
 }
 
-TEST_CASE("Class can allocated one stack using placement new") {
+TEST_CASE("Class can be allocated on stack using placement new") {
 	char storage[sizeof(Base)];
 	Base *base_ptr = new(storage) Base();
 	test_assert_equal(base_ctor, 1);
@@ -53,7 +57,7 @@ TEST_CASE("Class can allocated one stack using placement new") {
 	test_assert_equal(base_dtor, 1);
 }
 
-TEST_CASE("Class can allocated on heap using nothrow new") {
+TEST_CASE("Class can be allocated in heap using nothrow new") {
 	Base *base_ptr = new(std::nothrow) Base();
 	test_assert_equal(base_ctor, 1);
 	test_assert_equal(base_dtor, 0);
@@ -62,10 +66,20 @@ TEST_CASE("Class can allocated on heap using nothrow new") {
 	test_assert_equal(base_dtor, 1);
 }
 
-TEST_CASE("Class can allocated on heap using throwing new") {
+TEST_CASE("Class can be allocated in heap using throwing new") {
 	Base *base_ptr = new Base();
 	test_assert_equal(base_ctor, 1);
 	test_assert_equal(base_dtor, 0);
+	delete base_ptr;
+	test_assert_equal(base_ctor, 1);
+	test_assert_equal(base_dtor, 1);
+}
+
+TEST_CASE("Class can be allocated with class field initialization") {
+	Base *base_ptr = new Base(7);
+	test_assert_equal(base_ctor, 1);
+	test_assert_equal(base_dtor, 0);
+	test_assert_equal(base_ptr->get_field(), 7);
 	delete base_ptr;
 	test_assert_equal(base_ctor, 1);
 	test_assert_equal(base_dtor, 1);
