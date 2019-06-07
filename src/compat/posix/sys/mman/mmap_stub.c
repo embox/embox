84eab0078d2b7f8d8e997774385ce2b616ca9014
Anton Bondarev <anton.bondarev2310@gmail.com>
2019-06-07 19:09:31 +0300

    compat: Add idesc_map to mmap_stub if fd > 0

--- src/compat/posix/sys/mman/mmap_stub.c
+++ src/compat/posix/sys/mman/mmap_stub.c
@@ -12,7 +12,14 @@
 
 #include <util/log.h>
 
+#include <module/embox/fs/syslib/idesc_mmap_api.h>
+
 void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off) {
+	if (fd > 0) {
+		/* Call device-specific handler.
+		 * Working with plain files is not supported for now */
+		return idesc_mmap(addr, len, prot, flags, fd, off);
+	}
 	return addr;
 }
 
