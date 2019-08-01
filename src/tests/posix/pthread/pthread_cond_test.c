aaf79ab061b0c799a082337e4dd9b3634f0594a0
nastya-nizharadze <naniti@bk.ru>
2019-08-01 13:38:20 +0300

    test: pthread: fix tests

--- src/tests/posix/pthread/pthread_cond_test.c
+++ src/tests/posix/pthread/pthread_cond_test.c
@@ -7,6 +7,7 @@
 #include <pthread.h>
 #include <time.h>
 #include <embox/test.h>
+#include <kernel/time/time.h>
 
 EMBOX_TEST_SUITE("posix/pthread_cond_api");
 
@@ -16,13 +17,17 @@ static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 TEST_CASE("Initialize pthread_cond with PTHREAD_COND_INITIALIZER macro") {
 	int ret;
 	struct timespec timeout;
+	struct timespec relative_time;
 
 	timeout.tv_sec = 0;
 	timeout.tv_nsec = 10000000;
+
+	clock_gettime(CLOCK_REALTIME, &relative_time);
+	timeout = timespec_add(timeout, relative_time);
 
 	pthread_mutex_lock(&mut);
 	ret = pthread_cond_timedwait(&cond, &mut, &timeout);
 	pthread_mutex_unlock(&mut);
 
-	test_assert (ret == ETIMEDOUT);
+	test_assert(ret == ETIMEDOUT);
 }
