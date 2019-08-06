47cb8d841569225b3e8e33945313709f964bd932
Anton Bondarev <anton.bondarev2310@gmail.com>
2019-08-06 17:49:21 +0300

    drivers: Add TTYS_DEF macro to oldfs

--- src/drivers/tty/serial/ttys_oldfs.h
+++ src/drivers/tty/serial/ttys_oldfs.h
@@ -10,13 +10,18 @@
 
 #include <fs/idesc.h>
 #include <drivers/tty.h>
+#include <drivers/char_dev.h>
 
 struct uart;
-
 struct tty_uart {
 	struct idesc idesc;
 	struct tty tty;
 	struct uart *uart;
 };
+
+extern struct idesc *uart_cdev_open(struct dev_module *cdev, void *priv);
+
+#define TTYS_DEF(name, uart) \
+		CHAR_DEV_DEF(name, uart_cdev_open, NULL, NULL, uart)
 
 #endif /* TTYS_H_ */
