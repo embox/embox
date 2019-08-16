c402094bff198388bf80e1c848557f3d97fc5836
Deryugin <deryugin.denis@gmail.com>
2019-08-16 18:40:23 +0300

    dvfs: Fix lookup for single dot

--- src/fs/dvfs/dvfs_dentry.c
+++ src/fs/dvfs/dvfs_dentry.c
@@ -120,8 +120,9 @@ int dvfs_path_walk(const char *path, struct dentry *parent, struct lookup *looku
 	if (strlen(buff) > 1 && path_is_double_dot(buff))
 		return dvfs_path_walk(path + 2, parent->parent, lookup);
 
-	if (strlen(buff) > 1 && path_is_single_dot(buff))
-		return dvfs_path_walk(path + 2, parent, lookup);
+	if (path_is_single_dot(buff)) {
+		return dvfs_path_walk(path + 1, parent, lookup);
+	}
 
 	/* TODO use cache instead */
 	assert(parent->d_sb);
