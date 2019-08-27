30920e7985b96f6aa95fd75302e6cf9661330162
Anton Bondarev <anton.bondarev2310@gmail.com>
2019-08-27 18:16:14 +0300

    usb: Clean usb

--- src/include/drivers/usb/usb_desc.h
+++ src/include/drivers/usb/usb_desc.h
@@ -26,71 +26,72 @@
 #define USB_DESC_ENDP_ADDR_IN           0x80
 
 struct usb_control_header {
-	uint8_t		bm_request_type;
-	uint8_t 	b_request;
-	uint16_t 	w_value;
-	uint16_t 	w_index;
-	uint16_t	w_length;
+	uint8_t   bm_request_type;
+	uint8_t   b_request;
+	uint16_t  w_value;
+	uint16_t  w_index;
+	uint16_t  w_length;
 } __attribute__((packed));
 
 struct usb_desc_device {
-	uint8_t		b_length;
-	uint8_t 	b_desc_type;
-	uint16_t	bcd_usb;
-	uint8_t 	b_dev_class;
-	uint8_t 	b_dev_subclass;
-	uint8_t 	b_dev_protocol;
-	uint8_t 	b_max_packet_size;
-	uint16_t	id_vendor;
-	uint16_t	id_product;
-	uint16_t	bcd_device;
-	uint8_t 	i_manufacter;
-	uint8_t 	i_product;
-	uint8_t		i_serial_number;
-	uint8_t		i_num_configurations;
+	uint8_t  b_length;
+	uint8_t  b_desc_type;
+	uint16_t bcd_usb;
+	uint8_t  b_dev_class;
+	uint8_t  b_dev_subclass;
+	uint8_t  b_dev_protocol;
+	uint8_t  b_max_packet_size;
+	uint16_t id_vendor;
+	uint16_t id_product;
+	uint16_t bcd_device;
+	uint8_t  i_manufacter;
+	uint8_t  i_product;
+	uint8_t  i_serial_number;
+	uint8_t  i_num_configurations;
 } __attribute__((packed));
 
 struct usb_desc_configuration {
-	uint8_t		b_length;
-	uint8_t 	b_desc_type;
-	uint16_t	w_total_length;
-	uint8_t 	b_num_interfaces;
-	uint8_t 	b_configuration_value;
-	uint8_t		i_configuration;
-	uint8_t 	bm_attributes;
-	uint8_t 	b_max_power;
+	uint8_t  b_length;
+	uint8_t  b_desc_type;
+	uint16_t w_total_length;
+	uint8_t  b_num_interfaces;
+	uint8_t  b_configuration_value;
+	uint8_t  i_configuration;
+	uint8_t  bm_attributes;
+	uint8_t  b_max_power;
 } __attribute__((packed));
 
 struct usb_desc_interface {
-	uint8_t		b_length;
-	uint8_t 	b_desc_type;
-	uint8_t 	b_interface_number;
-	uint8_t 	b_alternate_setting;
-	uint8_t		b_num_endpoints;
-	uint8_t		b_interface_class;
-	uint8_t		b_interface_subclass;
-	uint8_t		b_interface_protocol;
-	uint8_t 	i_interface;
+	uint8_t b_length;
+	uint8_t b_desc_type;
+	uint8_t b_interface_number;
+	uint8_t b_alternate_setting;
+	uint8_t b_num_endpoints;
+	uint8_t b_interface_class;
+	uint8_t b_interface_subclass;
+	uint8_t b_interface_protocol;
+	uint8_t i_interface;
 }__attribute__((packed));
 
 struct usb_desc_endpoint {
-	uint8_t		b_length;
-	uint8_t 	b_desc_type;
-	uint8_t 	b_endpoint_address;
-	uint8_t 	bm_attributes;
-	uint16_t	w_max_packet_size;
-	uint8_t		b_interval;
+	uint8_t  b_length;
+	uint8_t  b_desc_type;
+	uint8_t  b_endpoint_address;
+	uint8_t  bm_attributes;
+	uint16_t w_max_packet_size;
+	uint8_t  b_interval;
 }__attribute__((packed));
 
 struct usb_desc_hub {
-    uint8_t  b_desc_length;
-    uint8_t  b_desc_type;
-    uint8_t  b_nbr_ports;
-    uint16_t w_hub_characteristics;
-    uint8_t  b_pwr_on_2_pwr_good;
-    uint8_t  b_hub_contr_current;
-    uint8_t  var_data[]; /* Variable length field; 64 should be the maximum possible
-                      length (255 ports => 2 x 32 bytes of data) */
+	uint8_t  b_desc_length;
+	uint8_t  b_desc_type;
+	uint8_t  b_nbr_ports;
+	uint16_t w_hub_characteristics;
+	uint8_t  b_pwr_on_2_pwr_good;
+	uint8_t  b_hub_contr_current;
+	 /* Variable length field; 64 should be the maximum possible
+	  * length (255 ports => 2 x 32 bytes of data) */
+	uint8_t  var_data[];
 }__attribute__((packed));
 
 #endif /* DRIVERS_USB_DESC_H_ */
