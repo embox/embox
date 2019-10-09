09037ca47a0a3e75a477454531bd1d4132c681f6
Anton Bondarev <anton.bondarev2310@gmail.com>
2019-10-09 17:01:40 +0300

    drivers: Add some definitions for DMA direction

--- src/compat/linux/include/asm-generic/dma-mapping.h
+++ src/compat/linux/include/asm-generic/dma-mapping.h
@@ -19,6 +19,17 @@
 
 struct device;
 
+/*
+ * These definitions mirror those in pci.h, so they can be used
+ * interchangeably with their PCI_ counterparts.
+ */
+enum dma_data_direction {
+	DMA_BIDIRECTIONAL = 0,
+	DMA_TO_DEVICE = 1,
+	DMA_FROM_DEVICE = 2,
+	DMA_NONE = 3,
+};
+
 /**
  * Allocate strong-ordered memory
  */
