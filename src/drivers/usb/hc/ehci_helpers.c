481ddaf8250d732492351b5748b78a8d1f33d4e0
Anton Bondarev <anton.bondarev2310@gmail.com>
2019-11-28 16:39:30 +0300

    minor: ehci: Clean ehci_helpers.c

--- src/drivers/usb/hc/ehci_helpers.c
+++ src/drivers/usb/hc/ehci_helpers.c
@@ -24,9 +24,9 @@ void ehci_caps_dump(struct ehci_hcd *ehci) {
 	}
 
 	log_debug("EHCI caps registers:");
-	log_debug("hc_capsbase = %08"PRIX32, ehci->ehci_caps->hc_capbase);
-	log_debug("hcs_params  = %08"PRIX32, ehci->ehci_caps->hcs_params);
-	log_debug("hcc_params  = %08"PRIX32, ehci->ehci_caps->hcc_params);
+	log_debug("hc_capsbase = %08"PRIX32, ehci_read(ehci, &ehci->ehci_caps->hc_capbase));
+	log_debug("hcs_params  = %08"PRIX32, ehci_read(ehci, &ehci->ehci_caps->hcs_params));
+	log_debug("hcc_params  = %08"PRIX32, ehci_read(ehci, &ehci->ehci_caps->hcc_params));
 	log_debug("portroute   = [%02"PRIX8" %02"PRIX8" %02"PRIX8" %02"PRIX8
 			" %02"PRIX8" %02"PRIX8" %02"PRIX8" %02"PRIX8"]",
 			ehci->ehci_caps->portroute[0],
@@ -47,22 +47,22 @@ void ehci_regs_dump(struct ehci_hcd *ehci) {
 		return;
 	}
 
-	if (ehci->ehci_caps == NULL) {
-		log_debug("Can't dump registers: ehci_caps is NULL!");
+	if (ehci->ehci_regs == NULL) {
+		log_debug("Can't dump registers: ehci_regs is NULL!");
 		return;
 	}
 
 	/* Note: reserved fields and port status are not printed */
 	log_debug("EHCI operational registers:");
-	log_debug("command         = %08"PRIX32, ehci->ehci_regs->command);
-	log_debug("status          = %08"PRIX32, ehci->ehci_regs->status);
-	log_debug("intr_enable     = %08"PRIX32, ehci->ehci_regs->intr_enable);
-	log_debug("frame_index     = %08"PRIX32, ehci->ehci_regs->frame_index);
-	log_debug("segment         = %08"PRIX32, ehci->ehci_regs->segment);
-	log_debug("frame_list      = %08"PRIX32, ehci->ehci_regs->frame_list);
-	log_debug("async_next      = %08"PRIX32, ehci->ehci_regs->async_next);
-	log_debug("txfill_tuning   = %08"PRIX32, ehci->ehci_regs->txfill_tuning);
-	log_debug("configured_flag = %08"PRIX32, ehci->ehci_regs->configured_flag);
+	log_debug("command         = %08"PRIX32, ehci_read(ehci, &ehci->ehci_regs->command));
+	log_debug("status          = %08"PRIX32, ehci_read(ehci, &ehci->ehci_regs->status));
+	log_debug("intr_enable     = %08"PRIX32, ehci_read(ehci, &ehci->ehci_regs->intr_enable));
+	log_debug("frame_index     = %08"PRIX32, ehci_read(ehci, &ehci->ehci_regs->frame_index));
+	log_debug("segment         = %08"PRIX32, ehci_read(ehci, &ehci->ehci_regs->segment));
+	log_debug("frame_list      = %08"PRIX32, ehci_read(ehci, &ehci->ehci_regs->frame_list));
+	log_debug("async_next      = %08"PRIX32, ehci_read(ehci, &ehci->ehci_regs->async_next));
+	log_debug("txfill_tuning   = %08"PRIX32, ehci_read(ehci, &ehci->ehci_regs->txfill_tuning));
+	log_debug("configured_flag = %08"PRIX32, ehci_read(ehci, &ehci->ehci_regs->configured_flag));
 
 	i = EHCI_HCS_N_PORTS(ehci_read(ehci, &ehci->ehci_caps->hcs_params));
 	while (i--) {
