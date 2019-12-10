6b5b32a3b9bc3113101af26e249e160367966c02
Anton Bondarev <anton.bondarev2310@gmail.com>
2019-12-10 13:38:46 +0300

    dvfs: Use common file_operations structure

--- src/fs/dvfs/dvfs.h
+++ src/fs/dvfs/dvfs.h
@@ -13,6 +13,7 @@
 #include <fs/file_desc.h>
 #include <fs/file_system.h>
 #include <util/dlist.h>
+#include <fs/file_operation.h>
 
 /*****************
  New VFS prototype
@@ -115,22 +116,6 @@ struct dentry {
 
 struct dentry_operations {
 
-};
-
-/* NOTE ON FILE OPEN
- *
- * Basically,  in  regular  file  systems  file  open  driver  function  should
- * just  return  the same  idesc  that  was  passed as second  parameter.  This
- * feature  is  required for device-dependent operations, otherwise just return
- * the second argument.
- */
-
-struct file_operations {
-	struct idesc *(*open)(struct inode *node, struct idesc *desc);
-	int    (*close)(struct file_desc *desc);
-	size_t (*read)(struct file_desc *desc, void *buf, size_t size);
-	size_t (*write)(struct file_desc *desc, void *buf, size_t size);
-	int    (*ioctl)(struct file_desc *desc, int request, void *data);
 };
 
 struct dumb_fs_driver {
