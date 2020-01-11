d4b20b24364643d1a3b0bbb76f2073a310122e97
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-01-11 14:40:47 +0300

    e2k: Update endians definitions in asm/hal/arch.h

--- src/arch/e2k/include/asm/hal/arch.h
+++ src/arch/e2k/include/asm/hal/arch.h
@@ -8,11 +8,10 @@
 #define __ORDER_LITTLE_ENDIAN__ _LITTLE_ENDIAN
 #endif
 
-#define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
+#define __BYTE_ORDER__       __ORDER_LITTLE_ENDIAN__
 #define __FLOAT_WORD_ORDER__ __ORDER_LITTLE_ENDIAN__
 
 #define __ORDER_BIG_ENDIAN__ 0
 #endif
 
-#define __BYTE_ORDER __ORDER_LITTLE_ENDIAN__
 #define __PLATFORM_ARCH "e2k"
