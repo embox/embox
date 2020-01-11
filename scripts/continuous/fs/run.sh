ea637dc09b13eac61cb7607318c3ea7be68246f3
Anton Kozlov <drakon.mega@gmail.com>
2020-01-11 10:45:25 +0300

    Update .travis.yml

--- scripts/continuous/fs/run.sh
+++ scripts/continuous/fs/run.sh
@@ -7,7 +7,7 @@ FS_TEST_RW="ext2 ext3 ntfs vfat"
 FS_TEST_NETWORK="nfs cifs"
 
 FS_TEST_NFS_ROOT="/var/nfs_test"
-FS_TEST_NFS_PREPARE="sudo /etc/init.d/nfs-kernel-server restart"
+FS_TEST_NFS_PREPARE="sudo modprobe nfsd && sudo /etc/init.d/nfs-kernel-server restart"
 
 FS_TEST_CIFS_SHARE="/Public"
 FS_TEST_CIFS_PATH="/var/cifs_test"
