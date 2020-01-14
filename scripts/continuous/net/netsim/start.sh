09f2197ccfd2130a4f8f8a51f3bd0fca996d6a11
Anton Kozlov <drakon.mega@gmail.com>
2020-01-15 00:37:55 +0300

    WIP

--- scripts/continuous/net/netsim/start.sh
+++ scripts/continuous/net/netsim/start.sh
@@ -3,13 +3,15 @@
 IP=$1
 
 B=$(dirname $0)
-NETSIM=./build/netsim
+BUILD=./build
+NETSIM=$BUILD/netsim
 
 SUDO=
 if [ $(id -u) != 0 ]; then
 	SUDO=sudo
 fi
 
+mkdir -p $BUILD
 if ! [ -f $NETSIM ]; then
 	gcc -o $NETSIM $B/netsim.c
 fi
