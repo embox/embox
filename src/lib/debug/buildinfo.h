f672961b48456dcc7089ab32d1adc3b0d5c2fcee
Deryugin <deryugin.denis@gmail.com>
2020-01-20 17:06:22 +0300

    lib/debug: Add function for build date

---
+++ src/lib/debug/buildinfo.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version
+ * @date 20.01.2020
+ */
+
+#ifndef BUILDINFO_H
+#define BUILDINFO_H
+
+extern char *buildinfo_date(void);
+
+#endif /* BUILDINFO_H */
