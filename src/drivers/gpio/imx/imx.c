94eb4e47daba8d65767210c017cf3f48d6cd1b77
Alex Kalmuk <alexkalmuk@gmail.com>
2020-02-13 15:49:20 +0300

    imx: gpio: Fix gpio_setup_mode

--- src/drivers/gpio/imx/imx.c
+++ src/drivers/gpio/imx/imx.c
@@ -60,7 +60,7 @@ static int imx_gpio_setup_mode(unsigned char port, gpio_mask_t mask, int mode) {
 	tmp = REG32_LOAD(GPIO_GDIR(port));
 	tmp &= ~mask;
 	tmp |= val;
-	REG32_CLEAR(GPIO_GDIR(port), tmp);
+	REG32_STORE(GPIO_GDIR(port), tmp);
 
 	return 0;
 }
