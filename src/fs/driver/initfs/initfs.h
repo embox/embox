d6bb6a78b587ff3bdac7ea7ca69efdda58d8de9b
Deryugin <deryugin.denis@gmail.com>
2020-03-03 13:15:27 +0300

    fs/drivers/initfs: Work on unification
    Use the same iterate handler

--- src/fs/driver/initfs/initfs.h
+++ src/fs/driver/initfs/initfs.h
@@ -11,19 +11,21 @@
 #include <stddef.h>
 
 struct initfs_file_info {
-	int start_pos;
-};
-
-/**
-* @brief Should be used as inode->i_data, but only for directories
-*/
-struct initfs_dir_info {
 	int    start_pos;
+	struct cpio_entry *entry;
 
+	/* Following fields are used just for directories */
 	char  *path;
 	size_t path_len;
 	char  *name;
 	size_t name_len;
 };
+
+extern struct initfs_file_info *initfs_file_alloc(void);
+extern void initfs_file_free(struct initfs_file_info *fi);
+extern int initfs_iterate(struct inode *next, char *name,
+		struct inode *parent, struct dir_ctx *ctx);
+extern int initfs_fill_inode(struct inode *node, char *cpio,
+		struct cpio_entry *entry);
 
 #endif /* SRC_FS_DRIVER_INITFS_INITFS_H_ */
