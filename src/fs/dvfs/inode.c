9d1a2b6c1eb3914e62e1dffa31d21c09d5d7ff6a
Deryugin <deryugin.denis@gmail.com>
2020-03-03 12:22:56 +0300

    fs/inode: Add handlers for size/mtime/ctime access

--- src/fs/dvfs/inode.c
+++ src/fs/dvfs/inode.c
@@ -10,10 +10,34 @@
 
 #include <fs/inode.h>
 
+void *inode_priv(const struct inode *node) {
+	return node->i_data;
+}
+
 void inode_priv_set(struct inode *node, void *priv) {
 	node->i_data = priv;
 }
 
-void *inode_priv(const struct inode *node) {
-	return node->i_data;
+size_t inode_size(const struct inode *node) {
+	return node->length;
+}
+
+void inode_size_set(struct inode *node, size_t sz) {
+	node->length = sz;
+}
+
+unsigned inode_ctime(const struct inode *node) {
+	return node->ctime;
+}
+
+void inode_ctime_set(struct inode *node, unsigned ctime) {
+	node->ctime = ctime;
+}
+
+unsigned inode_mtime(const struct inode *node) {
+	return node->mtime;
+}
+
+void inode_mtime_set(struct inode *node, unsigned mtime) {
+	node->mtime = mtime;
 }
