3c314595386e92a3b58f6a538d6129d9805b5eb1
sobhaniiest <sobhaniiest@gmail.com>
2020-03-03 16:39:12 +0530

    x86/qemu: multiple argumets for cat command

--- src/cmds/fs/cat.c
+++ src/cmds/fs/cat.c
@@ -8,9 +8,9 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-
 
 static void print_usage(void) {
 	printf("Usage: cat [OPTION]... [FILE]...\n");
@@ -21,13 +21,22 @@ int main(int argc, char **argv) {
 	int number = 0, line = 0, new_line = 1;
 	FILE *fd;
 	char buff;
+	int no_of_arg;
+	char *file_names[argc];
+	size_t arg_len;
+
+	for (no_of_arg = 0; no_of_arg < argc; no_of_arg++) {
+		arg_len = strlen(argv[no_of_arg]);
+		file_names[no_of_arg] = (char *)malloc (sizeof(char) * arg_len);
+		strcpy (file_names[no_of_arg], argv[no_of_arg]);
+	}
 
 	if (argc < 2) {
 		print_usage();
 		return -EINVAL;
 	}
 
-	while (-1 != (opt = getopt(argc - 1, argv, "nh"))) {
+	while (-1 != (opt = getopt(argc, argv, "nh"))) {
 		switch (opt) {
 		case 'n':
 			number = 1;
@@ -43,26 +52,36 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	if (NULL == (fd = fopen(argv[argc - 1], "r"))) {
-		printf("Can't open file %s\n", argv[argc - 1]);
-		return -errno;
-	}
+	for (no_of_arg = 1; no_of_arg < argc; no_of_arg++) {
+		if (strcmp ("-n", file_names[no_of_arg]) != 0) {
+			if (NULL == (fd = fopen(file_names[no_of_arg], "r"))) {
+				printf("Can't open file %s\n", file_names[no_of_arg]);
+				return -errno;
+			}
 
-	while (fread(&buff, 1, 1, fd) == 1) {
-		if (new_line && number) {
-			printf("\t%d %c", line++, buff);
-		} else {
-			printf("%c", buff);
+			while (fread(&buff, 1, 1, fd) == 1) {
+				if (new_line && number) {
+					printf("\t%d %c", line++, buff);
+				} else {
+					printf("%c", buff);
+				}
+				new_line = (buff == '\n') ? 1 : 0;
+			}
+
+			if (buff != '\n') {
+				printf("\n");
+			}
+
+			fflush(stdout);
+
+			fclose(fd);
 		}
-		new_line = (buff == '\n') ? 1 : 0;
 	}
 
-	if (buff != '\n') {
-		printf("\n");
+	for (no_of_arg = 0; no_of_arg < argc; no_of_arg++) {
+		char *file_name = file_names[no_of_arg];
+		free (file_name);
 	}
 
-	fflush(stdout);
-
-	fclose(fd);
 	return 0;
 }
