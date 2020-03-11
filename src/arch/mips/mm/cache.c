c14a62da6974978bd3f59e64f4bf959130a80d1c
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-03-11 17:32:55 +0300

    mips: Add stubs for cache and mem barriers for MIPS

---
+++ src/arch/mips/mm/cache.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ *
+ * @date Mar 11, 2020
+ * @Author Anton Bondarev
+ */
+
+#include <stddef.h>
+
+void dcache_inval(const void *p, size_t size) {
+	/* FIXME cache*/
+}
+
+void dcache_flush(const void *p, size_t size) {
+	/* FIXME cache*/
+}
