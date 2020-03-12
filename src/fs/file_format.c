121b3f77e670ff7f53396ac4857d2bd47c5882cf
Yogesh Singh <yogesh.singh.ams5@gmail.com>
2020-03-13 01:36:17 +0530

    Extended XML file format: removed version check

--- src/fs/file_format.c
+++ src/fs/file_format.c
@@ -15,9 +15,7 @@ enum fformat raw_get_file_format(uint8_t *file) {
 	/* signature of PNG files */
 	uint8_t png_signature[8] = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a};
 	/* signature of ELF files */
-	uint8_t elf_signature[4] = {0x7f, 0x45, 0x4c, 0x46};
-	/* signature of XML files */
-	uint8_t xml_signature[21] = {0x3c, 0x3f, 0x78, 0x6d, 0x6c, 0x20, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x3d, 0x22, 0x31, 0x2e, 0x30, 0x22, 0x3f, 0x3e};
+	uint8_t elf_signature[4] = {0x7f, 'E', 'L', 'F'};
 	/* Is it a RIFF file? */
 	if (!memcmp(file, "RIFF", 4)) {
 		/* TODO check file consistency */
@@ -31,7 +29,7 @@ enum fformat raw_get_file_format(uint8_t *file) {
 		return GIF_FILE;
 	} else if (!memcmp(file, elf_signature, sizeof(elf_signature))) {
 		return ELF_FILE;
-	} else if (!memcmp(file, xml_signature, sizeof(xml_signature))) {
+	} else if (!memcmp(file, "<?xml", 5)) {
 		return XML_FILE;
 	}
 
