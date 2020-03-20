ee3d1e7f371df48048a49a23cef6712e0b2bd429
Sha Ahammed Roze <shahahamedroze@gmail.com>
2020-03-20 17:49:54 +0530

    code-style 2.0

--- src/tests/stdlib/memcmp_test.c
+++ src/tests/stdlib/memcmp_test.c
@@ -16,16 +16,16 @@ TEST_CASE("Test that buf1 > buf2") {
     char buf1[] = "SWgaOtP43df2";
     char buf2[] = "SWGaOTP12df0";
  
-    n = memcmp(buf1,buf2,sizeof(buf1));
+    n = memcmp(buf1, buf2, sizeof(buf1));
     test_assert(n > 0)
 }
 
-TEST_CASE("Test that buf3 < buf4") {
+TEST_CASE("Test that buf2 < buf4") {
     int n;
-    char buf3[] = "Hello";
+    char buf2[] = "Hello";
     char buf4[] = "Welcome";
  
-    n = memcmp(buf3,buf4,sizeof(buf3));
+    n = memcmp(buf2, buf4, sizeof(buf2));
     test_assert(n < 0);
 }
 
@@ -34,6 +34,6 @@ TEST_CASE("Test's that two arrays are equal") {
     char buf4[] = "Welcome";
     char buf5[] = "Welcome";
  
-    n = memcmp(buf4,buf5,sizeof(buf4));
+    n = memcmp(buf4, buf5, sizeof(buf4));
     test_assert(n == 0);
 }
