49d840d187da28ea828bc2104809ad191fbdc772
Sha Ahammed Roze <shahahamedroze@gmail.com>
2020-03-20 17:35:12 +0530

    updated
    updated to comply with code style, moved all test strings to TEST_CASEs

--- src/tests/stdlib/memcmp_test.c
+++ src/tests/stdlib/memcmp_test.c
@@ -11,35 +11,29 @@
 
  EMBOX_TEST_SUITE("Test suite for memcmp()");
 
- /**
- * Test arrays
- * buf1 > buf2
- * buf4 > buf3
- * buf5 = buf4
- */
-
-char buf1[] = "SWgaOtP43df2";
-char buf2[] = "SWGaOTP12df0";
-char buf3[] = "Hello";
-char buf4[] = "Welcome";
-char buf5[] = "Welcome";
-
-int n;
-
-TEST_CASE("Test that buf1 > buf2")
-{
-   n = memcmp(buf1,buf2,sizeof(buf1));
-  test_assert(n > 0)
+TEST_CASE("Test that buf1 > buf2") {
+    int n;
+    char buf1[] = "SWgaOtP43df2";
+    char buf2[] = "SWGaOTP12df0";
+ 
+    n = memcmp(buf1,buf2,sizeof(buf1));
+    test_assert(n > 0)
 }
 
-TEST_CASE("Test that buf3 < buf4")
-{
-   n = memcmp(buf3,buf4,sizeof(buf3));
-  test_assert(n < 0);
+TEST_CASE("Test that buf3 < buf4") {
+    int n;
+    char buf3[] = "Hello";
+    char buf4[] = "Welcome";
+ 
+    n = memcmp(buf3,buf4,sizeof(buf3));
+    test_assert(n < 0);
 }
 
-TEST_CASE("Test's that two arrays are equal")
-{
-  int n = memcmp(buf4,buf5,sizeof(buf4));
-  test_assert(n == 0);
+TEST_CASE("Test's that two arrays are equal") {
+    int n;
+    char buf4[] = "Welcome";
+    char buf5[] = "Welcome";
+ 
+    n = memcmp(buf4,buf5,sizeof(buf4));
+    test_assert(n == 0);
 }
