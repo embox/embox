19ac653f4513c37a45dddc00716bc8f693a9a594
crobinette7 <crobinette7@gatech.edu>
2020-03-22 17:28:02 -0400

    Add test cases for strcpy and strncpy

---
+++ src/tests/stdlib/strcpy.c
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * 
+ * @date Mar 22, 2020
+ * @author: Calen Robinette
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <embox/test.h>
+
+EMBOX_TEST_SUITE("test suite for strcpy()");
+
+TEST_CASE("duplicate full string with strcpy()") {
+    char src[] = "string";
+    char dst[sizeof(src) + 1];
+
+    strcpy(dst, src);
+    test_assert_not_null(dst);
+    test_assert_zero(strcmp(dst, src));
+}
+
+TEST_CASE("duplicate empty string with strcpy()") {
+    char src[] = "";
+    char dst[sizeof(src) + 1];
+
+    strcpy(dst, src);
+    test_assert_not_null(dst);
+    test_assert_zero(strcmp(dst, src));
+}
