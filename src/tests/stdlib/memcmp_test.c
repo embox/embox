e1670d770f1fe9110d1f6d736e0e30b9b5b1e28f
Sha Ahammed Roze <shahahamedroze@gmail.com>
2020-03-24 18:35:40 +0530

    small changes to names of strings

--- src/tests/stdlib/memcmp_test.c
+++ src/tests/stdlib/memcmp_test.c
@@ -11,7 +11,7 @@
 
  EMBOX_TEST_SUITE("Test suite for memcmp()");
 
-TEST_CASE("Test that buf1 > buf2") {
+TEST_CASE("Test that 'SWgaOtP43df2' > 'SWGaOTP12df0' ") {
     int n;
     char buf1[] = "SWgaOtP43df2";
     char buf2[] = "SWGaOTP12df0";
@@ -20,20 +20,20 @@ TEST_CASE("Test that buf1 > buf2") {
     test_assert(n > 0);
 }
 
-TEST_CASE("Test that buf2 < buf4") {
+TEST_CASE("Test that 'Hello' < 'Welcome'") {
     int n;
-    char buf2[] = "Hello";
-    char buf4[] = "Welcome";
+    char buf1[] = "Hello";
+    char buf2[] = "Welcome";
  
-    n = memcmp(buf2, buf4, sizeof(buf2));
+    n = memcmp(buf1, buf2, sizeof(buf1));
     test_assert(n < 0);
 }
 
-TEST_CASE("Test's that two arrays are equal") {
+TEST_CASE("Test's that 'Welcome' and 'Welcome' are equal") {
     int n;
-    char buf4[] = "Welcome";
-    char buf5[] = "Welcome";
+    char buf1[] = "Welcome";
+    char buf2[] = "Welcome";
  
-    n = memcmp(buf4, buf5, sizeof(buf4));
+    n = memcmp(buf1, buf2, sizeof(buf1));
     test_assert(n == 0);
 }
