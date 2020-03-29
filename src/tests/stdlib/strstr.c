909729751716ee54ddbe1f3e5b8172fd36650b81
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-03-29 20:04:52 +0300

    tests: Fix compilation strcasestr & strstr

--- src/tests/stdlib/strstr.c
+++ src/tests/stdlib/strstr.c
@@ -12,8 +12,8 @@
 EMBOX_TEST_SUITE("strstr test suite");
 
 TEST_CASE("strstr with no needle in the haystack (no match)") {
-	test_assert_zero(strstr("the quick brown fox", "wolf"))
-	test_assert_zero(strstr("the slow green turtle", "hare"))
+	test_assert_zero(strstr("the quick brown fox", "wolf"));
+	test_assert_zero(strstr("the slow green turtle", "hare"));
 
 }
 
@@ -21,12 +21,12 @@ TEST_CASE("strstr with multiple needles in the haystack, returns pointer of firs
 	char *multiple_words_quick = "the quick brown fox ran quicker than a quick hare";
 	char *multiple_words_slow = "the turtle was slower than a slow snail";
 
-	test_assert(strstr(multiple_words_quick, "quick") == multiple_words_quick + 4)
-	test_assert(strstr(multiple_words_slow, "slow") == multiple_words_slow + 15)
+	test_assert(strstr(multiple_words_quick, "quick") == multiple_words_quick + 4);
+	test_assert(strstr(multiple_words_slow, "slow") == multiple_words_slow + 15);
 }
 
 TEST_CASE("strstr with a needle in the haystack (match of first occurrence)") {
-	test_assert(strstr("the quick brown fox", "fox") != 0)
-	test_assert(strstr("the slow green turtle", "turtle") != 0)
+	test_assert(strstr("the quick brown fox", "fox") != 0);
+	test_assert(strstr("the slow green turtle", "turtle") != 0);
 }
 
