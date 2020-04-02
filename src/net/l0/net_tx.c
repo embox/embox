b636cd0b9fa96c976a6a448042175b4d1aa2fd37
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-04-02 16:44:45 +0300

    net: Rework net_tx

--- src/net/l0/net_tx.c
+++ src/net/l0/net_tx.c
@@ -12,7 +12,6 @@
 #include <errno.h>
 #include <arpa/inet.h>
 #include <stddef.h>
-#include <util/array.h>
 
 #include <net/l0/net_crypt.h>
 #include <net/l0/net_tx.h>
@@ -22,46 +21,8 @@
 #include <net/socket/packet.h>
 #include <net/l2/ethernet.h>
 
-static int nt_build_hdr(struct sk_buff *skb, struct net_header_info *hdr_info) {
-	int ret;
-	unsigned char dst_haddr[MAX_ADDR_LEN];
-	struct net_device *dev = skb->dev;
-
-	assert(dev != NULL);
-
-	if (hdr_info == NULL) {
-		return 0; /* ok: already built */
-	}
-
-	/* resolve hw address */
-	if (hdr_info->src_hw == NULL) {
-		hdr_info->src_hw = &dev->dev_addr[0];
-	}
-
-	if (hdr_info->dst_hw == NULL) {
-		if (hdr_info->dst_p != NULL) {
-			ret = neighbour_get_haddr(hdr_info->type,
-					hdr_info->dst_p, dev, dev->type,
-					ARRAY_SIZE(dst_haddr), &dst_haddr[0]);
-			if (ret == 0) {
-				hdr_info->dst_hw = &dst_haddr[0];
-			} else if (ret == -ENOENT && (dev->flags & IFF_NOARP)) {
-				hdr_info->dst_hw = NULL;
-			} else {
-				return ret;
-			}
-		} else {
-			hdr_info->dst_hw = &dev->broadcast[0];
-		}
-	}
-
-	/* try to build */
-	assert(dev->ops != NULL);
-	assert(dev->ops->build_hdr != NULL);
-	return dev->ops->build_hdr(skb, hdr_info);
-}
-
 extern int netif_tx(struct net_device *dev,  struct sk_buff *skb);
+
 int net_tx_direct(struct sk_buff *skb) {
 	struct net_device *dev;
 
@@ -99,6 +60,7 @@ int net_tx_direct(struct sk_buff *skb) {
 
 int net_tx(struct sk_buff *skb, struct net_header_info *hdr_info) {
 	int ret;
+	unsigned char dst_haddr[MAX_ADDR_LEN];
 	struct net_device *dev;
 
 	assert(skb != NULL);
@@ -106,15 +68,29 @@ int net_tx(struct sk_buff *skb, struct net_header_info *hdr_info) {
 	dev = skb->dev;
 	assert(dev != NULL);
 
-	if ((0 != nt_build_hdr(skb, hdr_info))) {
-		assert(hdr_info != NULL);
-		ret = neighbour_send_after_resolve(hdr_info->type,
-				hdr_info->dst_p, hdr_info->p_len,
-				dev, skb);
+	if (hdr_info->dst_p == NULL || (dev->flags & IFF_NOARP)) {
+		/* it's local we don't need resolve hardware address */
+		hdr_info->dst_hw = &dev->broadcast[0];
+	} else {
+		ret = neighbour_get_haddr(hdr_info->type,
+				hdr_info->dst_p, dev, dev->type,
+				sizeof(dst_haddr), &dst_haddr[0]);
 		if (ret != 0) {
-			log_debug("neighbour_send_after_resolve = %d", ret);
+			ret = neighbour_send_after_resolve(hdr_info->type,
+					hdr_info->dst_p, hdr_info->p_len, dev, skb);
+			if (ret != 0) {
+					log_debug("neighbour_send_after_resolve = %d", ret);
+			}
+			return ret;
 		}
+		hdr_info->dst_hw = &dst_haddr[0];
+	}
 
+	/* try to build */
+	assert(dev->ops != NULL);
+	assert(dev->ops->build_hdr != NULL);
+	ret = dev->ops->build_hdr(skb, hdr_info);
+	if (ret != 0) {
 		return ret;
 	}
 
