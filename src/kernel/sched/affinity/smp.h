3f1871244da54ae0d132823f654364bb43d2f43a
Alex Kalmuk <alexkalmuk@gmail.com>
2020-04-06 14:03:39 +0300

    sched: affinity: Add static init

--- src/kernel/sched/affinity/smp.h
+++ src/kernel/sched/affinity/smp.h
@@ -12,4 +12,10 @@ struct affinity {
 	int mask;
 };
 
+/** Default schedee affinity mask */
+#define SCHEDEE_AFFINITY_NONE         ((unsigned int)-1)
+
+#define __SCHED_AFFINITY_INIT() \
+	{ .mask = SCHEDEE_AFFINITY_NONE }
+
 #endif /* SMP_AFFINITY_H_ */
