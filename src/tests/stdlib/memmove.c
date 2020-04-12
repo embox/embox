dde690c076e9e50e36f0ce7ec7225295e89561e8
KOUAM Ledoux <kouamdoux@gmail.com>
2020-04-12 05:56:08 +0200

    memmove test suite

--- src/tests/stdlib/memmove.c
+++ src/tests/stdlib/memmove.c
@@ -10,7 +10,6 @@ TEST_CASE("move full data")
     char src[] = "string" ;
     char dest[strlen(src)+1] ;
 
-    test_assert_not_null(dest);
     memmove(dest,src,strlen(src));
     test_assert_equal(*dest,*src);
 }
@@ -29,7 +28,6 @@ TEST_CASE("move array")
     char src[] = {'s','t','r','i','n','g'} ;
     char dest[strlen(src)+1] ;
 
-    test_assert_not_null(dest);
     memmove(dest,src,strlen(src));
     test_assert_equal(*dest,*src);
 }
@@ -39,7 +37,6 @@ TEST_CASE("Move array with null value in the middle")
     char src[] = {'s','t','r','\0','i','n','g'} ;
     char dest[strlen(src)+1] ;
 
-    test_assert_not_null(dest);
     memmove(dest,src,strlen(src));
     test_assert_equal(*dest,*src);
 }
@@ -49,7 +46,6 @@ TEST_CASE("If memory is not allocated enough")
     char src[] = {'s','t','r','\0','i','n','g'} ;
     char dest[2] ;
 
-    test_assert_not_null(dest);
     memmove(dest,src,strlen(src));
     test_assert_equal(*dest,*src);
 }
