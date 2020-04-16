e8ce5e6c212b0cfaeddfc69ac19857bc41861121
al-kostryukov <al-kostryukov@yandex.ru>
2020-04-16 12:49:15 +0300

    feat: first attempt to do syscall from hello.c

--- mk/script/loadable-build.mk
+++ mk/script/loadable-build.mk
@@ -3,7 +3,7 @@ L_CFLAGS = -nostdlib -emain -fpie -N
 
 $(LOADABLE_DIR)/$(DST) : | $(LOADABLE_DIR)
 $(LOADABLE_DIR)/$(DST) : $(SRC)
-	$(EMBOX_CROSS_COMPILE)gcc $(EMBOX_CFLAGS) $(L_CFLAGS) -o $@ $<
+	$(EMBOX_CROSS_COMPILE)gcc $(L_CFLAGS) -o $@ $<
 
 $(LOADABLE_DIR) :
 	mkdir -p $@
