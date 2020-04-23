3a8cf7bb5c53bd233ca178d56d26d89e7035f3e2
DmiitriyJarosh <dimajarosh@gmail.com>
2020-04-23 10:34:40 +0300

    Added lkl kernel start to lkl task

--- src/kernel/task/lkl_task.c
+++ src/kernel/task/lkl_task.c
@@ -1,9 +1,26 @@
+#include <stdio.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic warning "-Wundef"
+#include <lkl_host.h>
+#include <lkl.h>
+#pragma GCC diagnostic pop
 #include <embox/unit.h>
+
 
 EMBOX_UNIT_INIT(lkl_task_init);
 
-static int lkl_task_init(void) {
-	printk("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+extern __trap_handler __exception_table[0x20];
 
-	return 0;
+static int lkl_task_init(void) {
+	printf("LKL TASK\n");
+	long ret;
+	char cmdline[64];
+
+	snprintf(cmdline, sizeof(cmdline), "mem=64M loglevel=8"); // loglevel=8 is from tests
+    ret = lkl_start_kernel(&lkl_host_ops, cmdline);
+
+	// set lkl handlers for syscalls
+	__exception_table[exception_nr] = handler;
+	return ret;
 }
