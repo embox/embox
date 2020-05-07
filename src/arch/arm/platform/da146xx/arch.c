d4ef1da8ce01bdc60140c9b5b830cd2bbdc9c7d0
Alex Kalmuk <alexkalmuk@gmail.com>
2020-05-07 10:00:06 +0300

    da146xx: Add gpregs_base as option to arch_init

--- src/arch/arm/platform/da146xx/arch.c
+++ src/arch/arm/platform/da146xx/arch.c
@@ -6,13 +6,16 @@
  */
 #include <hal/arch.h>
 #include <hal/reg.h>
+#include <framework/mod/options.h>
 
-#define GPREG_BASE            0x50003300
+#define GPREG_BASE           OPTION_GET(NUMBER,gpregs_base)
+
 #define GPREG_SET_FREEZE_REG (GPREG_BASE + 0x0)
+# define GPREG_SET_FREEZE_SYS_WDOG (1 << 3)
 
 void arch_init(void) {
 	/* Disable watchdog. It was enabled by bootloader. */
-	REG16_STORE(GPREG_SET_FREEZE_REG, (1 << 3));
+	REG16_STORE(GPREG_SET_FREEZE_REG, GPREG_SET_FREEZE_SYS_WDOG);
 }
 
 void arch_idle(void) {
