20f3d02aa6dce89317772c92f2ff3523180bf44d
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-05-15 12:26:34 +0300

    drivers: Make uart3 availeble for stm32f4_usart

--- src/drivers/serial/stm32_usart/hal_msp_f4.c
+++ src/drivers/serial/stm32_usart/hal_msp_f4.c
@@ -10,98 +10,147 @@
 #include <drivers/serial/stm_usart.h>
 
 void USART_TX_GPIO_CLK_ENABLE(void *usart_base) {
-	if (usart_base ==  USART2)
+	if (usart_base ==  USART2) {
 		__HAL_RCC_GPIOA_CLK_ENABLE();
-	else if (usart_base == USART6)	
+	}
+	else if (usart_base ==  USART3) {
+		__HAL_RCC_GPIOD_CLK_ENABLE();
+	}
+	else if (usart_base == USART6) {
 		__HAL_RCC_GPIOC_CLK_ENABLE();
-	else
+	}
+	else {
 		/* Not supported */
 		assert(0);
+	}
 }
 
 void USART_RX_GPIO_CLK_ENABLE(void *usart_base) {
-	if (usart_base ==  USART2)
+	if (usart_base ==  USART2) {
 		__HAL_RCC_GPIOA_CLK_ENABLE();
-	else if (usart_base ==  USART6)
+	}
+	else if (usart_base ==  USART3) {
+		__HAL_RCC_GPIOD_CLK_ENABLE();
+	}
+	else if (usart_base == USART6) {
 		__HAL_RCC_GPIOC_CLK_ENABLE();
-	else
+	}
+	else {
 		/* Not supported */
 		assert(0);
+	}
 }
 
 void USART_CLK_ENABLE(void *usart_base) {
-	if (usart_base ==  USART2)
+	if (usart_base ==  USART2) {
 		__HAL_RCC_USART2_CLK_ENABLE();
-	else if (usart_base ==  USART6)
+	}
+	else if (usart_base ==  USART3) {
+		__HAL_RCC_USART3_CLK_ENABLE();
+	}
+	else if (usart_base ==  USART6) {
 		__HAL_RCC_USART6_CLK_ENABLE();
-	else	
+	}
+	else {
 		/* Not supported */
 		assert(0);
+	}
 }
 
 uint16_t USART_RX_PIN(void *usart_base) {
-	if (usart_base ==  USART2)
+	if (usart_base ==  USART2) {
 		return GPIO_PIN_3;
-	else if (usart_base ==  USART6)
+	}
+	else if (usart_base ==  USART3) {
+			return GPIO_PIN_9;
+	}
+	else if (usart_base ==  USART6) {
 		return GPIO_PIN_7;
-	else
-		/* Not supported */
-		assert(0);
+	}
+	/* Not supported */
+	assert(0);
+
 	return 0;
 }
 
 uint16_t USART_TX_PIN(void *usart_base) {
-	if (usart_base ==  USART2)
+	if (usart_base ==  USART2) {
 		return GPIO_PIN_2;
-	else if (usart_base ==  USART6)
+	}
+	else if (usart_base ==  USART3) {
+		return GPIO_PIN_8;
+	}
+	else if (usart_base ==  USART6) {
 		return GPIO_PIN_6;
-	else
-		/* Not supported */
-		assert(0);
+	}
+
+	/* Not supported */
+	assert(0);
+
 	return 0;
 }
 
 uint8_t USART_TX_AF(void *usart_base) {
-	if (usart_base ==  USART2)
+	if (usart_base ==  USART2) {
 		return GPIO_AF7_USART2;
-	else if (usart_base ==  USART6)
+	}
+	else if (usart_base ==  USART3) {
+		return GPIO_AF7_USART3;
+	}
+	else if (usart_base ==  USART6) {
 		return GPIO_AF8_USART6;
-	else		
-		/* Not supported */
-		assert(0);
+	}
+
+	/* Not supported */
+	assert(0);
 	return 0;
 }
 
 uint8_t USART_RX_AF(void *usart_base) {
-	if (usart_base ==  USART2)
+	if (usart_base ==  USART2) {
 		return GPIO_AF7_USART2;
-	else if (usart_base ==  USART6)
+	}
+	else if (usart_base ==  USART3) {
+		return GPIO_AF7_USART3;
+	}
+	else if (usart_base ==  USART6) {
 		return GPIO_AF8_USART6;
-	else		
-		/* Not supported */
-		assert(0);
+	}
+
+	/* Not supported */
+	assert(0);
 	return 0;
 }
 
 GPIO_TypeDef *USART_RX_GPIO_PORT(void *usart_base) {
-	if (usart_base ==  USART2)
+	if (usart_base ==  USART2) {
 		return GPIOA;
-	if (usart_base ==  USART6)
+	}
+	if (usart_base ==  USART3) {
+		return GPIOD;
+	}
+	if (usart_base ==  USART6) {
 		return GPIOC;
-	else		
-		/* Not supported */
-		assert(0);
+	}
+	/* Not supported */
+	assert(0);
+
 	return 0;
 }
 
 GPIO_TypeDef *USART_TX_GPIO_PORT(void *usart_base) {
-	if (usart_base ==  USART2)
+	if (usart_base ==  USART2) {
 		return GPIOA;
-	if (usart_base ==  USART6)
+	}
+	if (usart_base ==  USART3) {
+		return GPIOD;
+	}
+	if (usart_base ==  USART6) {
 		return GPIOC;
-	else		
-		/* Not supported */
-		assert(0);
+	}
+	/* Not supported */
+	assert(0);
+
 	return 0;
 }
 
--- src/drivers/serial/stm32_usart/stm32_usart_conf_f4.h
+++ src/drivers/serial/stm32_usart/stm32_usart_conf_f4.h
@@ -58,6 +58,28 @@
 /* Definition for USARTx's NVIC */
 #define USARTx_IRQn                      USART2_IRQn + 16
 #define USARTx_IRQHandler                USART2_IRQHandler
+
+#elif MODOPS_USARTX == 3
+#define USARTx                           USART3
+#define USARTx_CLK_ENABLE()              __HAL_RCC_USART3_CLK_ENABLE()
+#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+
+#define USARTx_FORCE_RESET()             __HAL_RCC_USART3_FORCE_RESET()
+#define USARTx_RELEASE_RESET()           __HAL_RCC_USART3_RELEASE_RESET()
+
+/* Definition for USARTx Pins */
+#define USARTx_TX_PIN                    GPIO_PIN_8
+#define USARTx_TX_GPIO_PORT              GPIOD
+#define USARTx_TX_AF                     GPIO_AF7_USART3
+#define USARTx_RX_PIN                    GPIO_PIN_9
+#define USARTx_RX_GPIO_PORT              GPIOD
+#define USARTx_RX_AF                     GPIO_AF7_USART3
+
+/* Definition for USARTx's NVIC */
+#define USARTx_IRQn                      USART3_IRQn + 16
+#define USARTx_IRQHandler                USART3_IRQHandler
+
 #else
 #error Unsupported USARTx
 #endif
