6e4d538d80e6fcae39c3557ef52e870a76b08e84
Alex Kalmuk <alexkalmuk@gmail.com>
2020-05-25 20:25:34 +0300

    cortex-m: Simplify interrupt handling

--- src/drivers/interrupt/cortexm/cortexm_nvic.h
+++ src/drivers/interrupt/cortexm/cortexm_nvic.h
@@ -11,4 +11,13 @@
 #include <module/embox/driver/interrupt/cortexm_nvic.h>
 #define __IRQCTRL_IRQS_TOTAL OPTION_MODULE_GET(embox__driver__interrupt__cortexm_nvic,NUMBER,irq_table_size)
 
+/* TODO It's an architectural part, move to src/arch/arm. */
+/* SCB_BASE is used in stm32 cube, so define _SCB_BASE instead. */
+#define _SCB_BASE              0xe000ed00
+#define SCB_ICSR              (_SCB_BASE + 0x04)
+# define SCB_ICSR_RETTOBASE   (1 << 11)
+# define SCB_ICSR_PENDSVSET   (1 << 28)
+#define SCB_VTOR              (_SCB_BASE + 0x08)
+# define SCB_VTOR_IN_RAM      (1 << 29)
+
 #endif /* IRQCTRL_NVIC_IMPL_H_ */
