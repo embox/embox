01ab04833a495b389267179bd0086b178b6e1297
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-06-01 20:09:47 +0300

    adar: Add audio/nucleo_f429zi_audio

---
+++ platform/adar7251/drivers/audio/nucleo_f429zi_audio.c
@@ -0,0 +1,97 @@
+/*
+ * nucleo_f429zi_audio.c
+ *
+ *  Created on: Jun 1, 2020
+ *      Author: anton
+ */
+
+//#include <stm32f4xx.h>
+#include <stm32f4xx_hal.h>
+//#include <stm32f4xx_hal_sai.h>
+
+SAI_HandleTypeDef hsai_BlockA1;
+DMA_HandleTypeDef hdma_sai1_a;
+
+/**
+  * @brief SAI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SAI1_Init(void)
+{
+
+  /* USER CODE BEGIN SAI1_Init 0 */
+
+  /* USER CODE END SAI1_Init 0 */
+
+  /* USER CODE BEGIN SAI1_Init 1 */
+
+  /* USER CODE END SAI1_Init 1 */
+  hsai_BlockA1.Instance = SAI1_Block_A;
+  hsai_BlockA1.Init.Protocol = SAI_FREE_PROTOCOL;
+  hsai_BlockA1.Init.AudioMode = SAI_MODESLAVE_RX;
+  hsai_BlockA1.Init.DataSize = SAI_DATASIZE_32;
+  hsai_BlockA1.Init.FirstBit = SAI_FIRSTBIT_MSB;
+  hsai_BlockA1.Init.ClockStrobing = SAI_CLOCKSTROBING_FALLINGEDGE;
+  hsai_BlockA1.Init.Synchro = SAI_ASYNCHRONOUS;
+  hsai_BlockA1.Init.OutputDrive = SAI_OUTPUTDRIVE_DISABLE;
+  hsai_BlockA1.Init.FIFOThreshold = SAI_FIFOTHRESHOLD_1QF;
+  hsai_BlockA1.FrameInit.FrameLength = 64;
+  hsai_BlockA1.FrameInit.ActiveFrameLength = 1;
+  hsai_BlockA1.FrameInit.FSDefinition = SAI_FS_STARTFRAME;
+  hsai_BlockA1.FrameInit.FSPolarity = SAI_FS_ACTIVE_LOW;
+  hsai_BlockA1.FrameInit.FSOffset = SAI_FS_FIRSTBIT;
+  hsai_BlockA1.SlotInit.FirstBitOffset = 0;
+  hsai_BlockA1.SlotInit.SlotSize = SAI_SLOTSIZE_32B;
+  hsai_BlockA1.SlotInit.SlotNumber = 1;
+  hsai_BlockA1.SlotInit.SlotActive = 0x00000000;
+  if (HAL_SAI_Init(&hsai_BlockA1) != HAL_OK)
+  {
+   // Error_Handler();
+  }
+  /* USER CODE BEGIN SAI1_Init 2 */
+
+  /* USER CODE END SAI1_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA2_Stream1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+}
+
+int sai_init(void) {
+	  /* Initialize all configured peripherals */
+	  MX_GPIO_Init();
+	  MX_DMA_Init();
+	  MX_SAI1_Init();
+	  return 0;
+}
---
+++ platform/adar7251/drivers/audio/nucleo_f429zi_audio.h
@@ -0,0 +1,15 @@
+/*
+ * nucleo_f429zi_audio.h
+ *
+ *  Created on: Jun 1, 2020
+ *      Author: anton
+ */
+
+#ifndef PLATFORM_ADAR7251_DRIVERS_AUDIO_NUCLEO_F429ZI_AUDIO_H_
+#define PLATFORM_ADAR7251_DRIVERS_AUDIO_NUCLEO_F429ZI_AUDIO_H_
+
+
+extern int sai_init(void);
+
+
+#endif /* PLATFORM_ADAR7251_DRIVERS_AUDIO_NUCLEO_F429ZI_AUDIO_H_ */
