17c58c947c0c22e7d3c0e7e4d0790f76863bcaff
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-06-02 20:39:32 +0300

    adar7251/drivers/audio

---
+++ platform/adar7251/drivers/audio/cube_callbacks.c
@@ -0,0 +1,54 @@
+/*
+ * cube_fixups.c
+ *
+ *  Created on: Jun 3, 2020
+ *      Author: anton
+ */
+
+#include <util/log.h>
+
+#include <stm32f4xx_hal.h>
+#include <stm32f4xx_hal_dma.h>
+
+void HAL_SAI_RxCpltCallback(SAI_HandleTypeDef *hsai)
+{
+ /* Prevent unused argument(s) compilation warning */
+ UNUSED(hsai);
+
+ /* NOTE : This function should not be modified, when the callback is needed,
+           the HAL_SAI_RxCpltCallback could be implemented in the user file
+  */
+}
+
+/**
+ * @brief Rx Transfer half completed callback.
+ * @param  hsai: pointer to a SAI_HandleTypeDef structure that contains
+ *                the configuration information for SAI module.
+ * @retval None
+ */
+void HAL_SAI_RxHalfCpltCallback(SAI_HandleTypeDef *hsai)
+{
+ /* Prevent unused argument(s) compilation warning */
+ UNUSED(hsai);
+
+ /* NOTE : This function should not be modified, when the callback is needed,
+           the HAL_SAI_RxHalfCpltCallback could be implemented in the user file
+  */
+}
+
+/**
+ * @brief SAI error callback.
+ * @param  hsai: pointer to a SAI_HandleTypeDef structure that contains
+ *                the configuration information for SAI module.
+ * @retval None
+ */
+void HAL_SAI_ErrorCallback(SAI_HandleTypeDef *hsai)
+{
+/* Prevent unused argument(s) compilation warning */
+//UNUSED(hsai);
+	log_error("dfffffff");
+
+/* NOTE : This function should not be modified, when the callback is needed,
+          the HAL_SAI_ErrorCallback could be implemented in the user file
+ */
+}
--- platform/adar7251/drivers/audio/nucleo_f429zi_audio.c
+++ platform/adar7251/drivers/audio/nucleo_f429zi_audio.c
@@ -9,21 +9,7 @@
 #include <kernel/irq.h>
 
 #include <stm32f4xx_hal.h>
-
-#if 0
-#define AUDIO_SAIx_FS_GPIO_PORT              GPIOG
-#define AUDIO_SAIx_FS_AF                     GPIO_AF10_SAI2
-#define AUDIO_SAIx_FS_PIN                    GPIO_PIN_9
-#define AUDIO_SAIx_SCK_GPIO_PORT             GPIOA
-#define AUDIO_SAIx_SCK_AF                    GPIO_AF8_SAI2
-#define AUDIO_SAIx_SCK_PIN                   GPIO_PIN_2
-#define AUDIO_SAIx_SD_GPIO_PORT              GPIOG
-#define AUDIO_SAIx_SD_AF                     GPIO_AF10_SAI2
-#define AUDIO_SAIx_SD_PIN                    GPIO_PIN_10
-#define AUDIO_SAIx_MCLK_GPIO_PORT            GPIOA
-#define AUDIO_SAIx_MCLK_AF                   GPIO_AF10_SAI2
-#define AUDIO_SAIx_MCLK_PIN                  GPIO_PIN_1
-#endif
+#include <stm32f4xx_hal_dma.h>
 
 struct sai_device {
 	uint32_t buf[4096];
@@ -32,10 +18,62 @@ struct sai_device {
 static struct sai_device sai_device;
 
 static SAI_HandleTypeDef hsai_BlockA1;
-DMA_HandleTypeDef hdma_sai1_a;
+DMA_HandleTypeDef hdma_sai_rx;
+
+/**
+  * @brief  Initializes SAI Audio IN MSP.
+  * @param  hsai: SAI handle
+  * @retval None
+  */
+static void SAI_AUDIO_IN_MspInit(SAI_HandleTypeDef *hsai, void *Params) {
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+	/* Enable SAI clock */
+	__HAL_RCC_SAI1_CLK_ENABLE();
+
+	/* Configure PE-4 (SAI1_FS),
+	 * PE-5 (SAI_SCK),
+	 * PE-6 (SAI_SD) */
+	GPIO_InitStructure.Pin = GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6;
+	GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
+	GPIO_InitStructure.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStructure.Pull = GPIO_NOPULL;
+	GPIO_InitStructure.Alternate = GPIO_AF6_SAI1;
+	HAL_GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+#if 0
+  /* Configure the hdma_sai_rx handle parameters */
+    hdma_sai_rx.Instance = DMA2_Stream1;
+    hdma_sai_rx.Init.Channel             = DMA_CHANNEL_0; /*stream 1 channel 0 */
+    hdma_sai_rx.Init.Direction           = DMA_PERIPH_TO_MEMORY;
+    hdma_sai_rx.Init.PeriphInc           = DMA_PINC_DISABLE;
+    hdma_sai_rx.Init.MemInc              = DMA_MINC_ENABLE;
+    hdma_sai_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD; /* 32 bit */
+    hdma_sai_rx.Init.MemDataAlignment    = DMA_PDATAALIGN_WORD; /* 32 bit */
+    hdma_sai_rx.Init.Mode                = DMA_CIRCULAR;
+    hdma_sai_rx.Init.Priority            = DMA_PRIORITY_HIGH;
+    hdma_sai_rx.Init.FIFOMode            = DMA_FIFOMODE_DISABLE;
+    hdma_sai_rx.Init.FIFOThreshold       = DMA_FIFO_THRESHOLD_FULL;
+    hdma_sai_rx.Init.MemBurst            = DMA_MBURST_SINGLE;
+    hdma_sai_rx.Init.PeriphBurst         = DMA_MBURST_SINGLE;
+
+
+
+    /* Associate the DMA handle */
+    __HAL_LINKDMA(hsai, hdmarx, hdma_sai_rx);
+
+    /* Deinitialize the Stream for new transfer */
+    HAL_DMA_DeInit(&hdma_sai_rx);
+
+    /* Configure the DMA Stream */
+    HAL_DMA_Init(&hdma_sai_rx);
+#endif
+
+}
+
 
 static irq_return_t sai_interrupt(unsigned int irq_num, void *dev_id) {
-
+	HAL_DMA_IRQHandler(hsai_BlockA1.hdmarx);
 	return IRQ_HANDLED;
 }
 
@@ -55,6 +93,11 @@ static void MX_SAI1_Init(void)
 
   /* USER CODE END SAI1_Init 1 */
   hsai_BlockA1.Instance = SAI1_Block_A;
+  /* Disable SAI peripheral */
+  __HAL_SAI_DISABLE(&hsai_BlockA1);
+  HAL_SAI_DeInit(&hsai_BlockA1);
+
+  hsai_BlockA1.Instance = SAI1_Block_A;
   hsai_BlockA1.Init.Protocol = SAI_FREE_PROTOCOL;
   hsai_BlockA1.Init.AudioMode = SAI_MODESLAVE_RX;
   hsai_BlockA1.Init.DataSize = SAI_DATASIZE_32;
@@ -72,10 +115,14 @@ static void MX_SAI1_Init(void)
   hsai_BlockA1.SlotInit.SlotSize = SAI_SLOTSIZE_32B;
   hsai_BlockA1.SlotInit.SlotNumber = 1;
   hsai_BlockA1.SlotInit.SlotActive = 0x00000000;
+
   if (HAL_SAI_Init(&hsai_BlockA1) != HAL_OK)
   {
    // Error_Handler();
   }
+  /* Enable SAI peripheral */
+  __HAL_SAI_ENABLE(&hsai_BlockA1);
+
   /* USER CODE BEGIN SAI1_Init 2 */
 
   /* USER CODE END SAI1_Init 2 */
@@ -115,22 +162,27 @@ static void MX_GPIO_Init(void)
 
 }
 
-int sai_init(void) {
+struct sai_device *sai_init(void) {
 	int res;
 
 	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
 	MX_DMA_Init();
+	SAI_AUDIO_IN_MspInit(&hsai_BlockA1, NULL);
 	MX_SAI1_Init();
+
+
 
 	res = irq_attach(DMA2_Stream1_IRQn + 16, sai_interrupt, 0, &sai_device, "");
 	if (res < 0) {
-		return res;
+		return NULL;
 	}
-	return 0;
+	return &sai_device;
 }
 
-int sai_recieve(struct sai_device *sai_dev, uint8_t buf, int len) {
+int sai_recieve(struct sai_device *sai_dev, uint8_t *buf, int len) {
+//	HAL_SAI_Receive_DMA(&hsai_BlockA1, (uint8_t*)buf, len);
+	HAL_SAI_Receive(&hsai_BlockA1, (uint8_t*)buf, len, 0xFFFFFFFF);
 	return 0;
 }
 
