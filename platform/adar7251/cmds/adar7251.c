6082bf2a846ced40ffc6df1cd4c9a374c749ae55
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-06-10 15:21:26 +0300

    adar7251/cmds/adar7251.c

--- platform/adar7251/cmds/adar7251.c
+++ platform/adar7251/cmds/adar7251.c
@@ -3,10 +3,11 @@
 #include <drivers/adar7251_driver.h>
 #include <drivers/nucleo_f429zi_audio.h>
 
-static uint8_t rx_buf[0x400];
+static uint8_t rx_buf[SAI_SAMPLES_BUFFER * 2]; /* half of sai buffer */
 static struct adar7251_dev adar7251_dev;
 
 int main(int argc, char **argv) {
+	int data_len;
 
 	printf("starting adar7251\n");
 	adar7251_hw_init(&adar7251_dev);
@@ -14,7 +15,8 @@ int main(int argc, char **argv) {
 	printf("conversation loop\n");
 
 	while(1) {
-		sai_recieve(adar7251_dev.sai_dev, rx_buf, sizeof(rx_buf));
+		data_len = sai_receive(adar7251_dev.sai_dev, rx_buf, sizeof(rx_buf));
+		printf("%d\n", data_len);
 	}
 
 	return 0;
