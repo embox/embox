041e56dbafc617efee8f254498002e85fc3a1fe0
Alex Kalmuk <alexkalmuk@gmail.com>
2020-06-26 18:50:23 +0300

    usb: Move host and device common defines

--- src/include/drivers/usb/usb.h
+++ src/include/drivers/usb/usb.h
@@ -16,6 +16,7 @@
 #include <util/indexator.h>
 #include <util/dlist.h>
 
+#include <drivers/usb/usb_defines.h>
 #include <drivers/usb/usb_queue.h>
 #include <drivers/usb/usb_desc.h>
 #include <drivers/usb/usb_token.h>
@@ -55,61 +56,6 @@ struct usb_request;
 struct usb_interface;
 
 typedef void (*usb_request_notify_hnd_t)(struct usb_request *req, void *arg);
-
-#define USB_DIR_OUT                 0x00
-#define USB_DIR_IN                  0x80
-
-#define USB_REQ_TYPE_MASK           (0x03 << 5)
-#define USB_REQ_TYPE_STANDARD       (0x00 << 5)
-#define USB_REQ_TYPE_CLASS          (0x01 << 5)
-#define USB_REQ_TYPE_VENDOR         (0x02 << 5)
-
-#define USB_REQ_RECIP_DEVICE        0x00
-#define USB_REQ_RECIP_IFACE         0x01
-#define USB_REQ_RECIP_ENDP          0x02
-#define USB_REQ_RECIP_OTHER         0x03
-
-#define USB_REQ_GET_STATUS          0x00
-#define USB_REQ_CLEAR_FEATURE       0x01
-#define USB_REQ_SET_FEATURE         0x03
-#define USB_REQ_SET_ADDRESS         0x05
-#define USB_REQ_GET_DESCRIPTOR      0x06
-#define USB_REQ_SET_CONFIG          0x09
-#define USB_REQ_SET_INTERFACE       0x0B
-
-#define USB_PORT_FEATURE_CONNECTION      0
-#define USB_PORT_FEATURE_ENABLE          1
-#define USB_PORT_FEATURE_RESET           4
-#define USB_PORT_FEATURE_POWER           8
-#define USB_PORT_FEATURE_C_CONNECTION    16
-#define USB_PORT_FEATURE_C_ENABLE        17
-#define USB_PORT_FEATURE_C_RESET         20
-
-#define USB_PORT_STAT_CONNECTION    0x0001
-#define USB_PORT_STAT_ENABLE        0x0002
-#define USB_PORT_STAT_SUSPEND       0x0004
-#define USB_PORT_STAT_OVERCURRENT   0x0008
-#define USB_PORT_STAT_RESET         0x0010
-
-#define USB_CLASS_HUB  0x9
-#define USB_DT_HUB     (USB_REQ_TYPE_CLASS | USB_CLASS_HUB)
-
-/* Hub request types */
-#define USB_RT_HUB  (USB_REQ_TYPE_CLASS | USB_REQ_RECIP_DEVICE)
-#define USB_RT_PORT (USB_REQ_TYPE_CLASS | USB_REQ_RECIP_OTHER)
-
-/* Class requests from the USB 2.0 hub spec, table 11-15 */
-#define HUB_CLASS_REQ(dir, type, request) ((((dir) | (type)) << 8) | (request))
-
-#define USB_CLEAR_HUB_FEATURE     HUB_CLASS_REQ(USB_DIR_OUT, USB_RT_HUB, USB_REQ_CLEAR_FEATURE)
-#define USB_CLEAR_PORT_FEATURE    HUB_CLASS_REQ(USB_DIR_OUT, USB_RT_PORT, USB_REQ_CLEAR_FEATURE)
-#define USB_GET_HUB_DESCRIPTOR    HUB_CLASS_REQ(USB_DIR_IN, USB_RT_HUB, USB_REQ_GET_DESCRIPTOR)
-#define USB_GET_HUB_STATUS        HUB_CLASS_REQ(USB_DIR_IN, USB_RT_HUB, USB_REQ_GET_STATUS)
-#define USB_GET_PORT_STATUS       HUB_CLASS_REQ(USB_DIR_IN, USB_RT_PORT, USB_REQ_GET_STATUS)
-#define USB_SET_HUB_FEATURE       HUB_CLASS_REQ(USB_DIR_OUT, USB_RT_HUB, USB_REQ_SET_FEATURE)
-#define USB_SET_PORT_FEATURE      HUB_CLASS_REQ(USB_DIR_OUT, USB_RT_PORT, USB_REQ_SET_FEATURE)
-
-#define HUB_PORT_STATUS     0
 
 enum usb_request_status {
 	USB_REQ_NOERR,
