b48a92918646245c43280dc0d25bc8665c30da3e
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-08-09 16:28:11 +0300

    stm32_usart: Clean stm32_usart_conf_f4.h

--- src/drivers/serial/stm32_usart/stm32_usart_conf_f4.h
+++ src/drivers/serial/stm32_usart/stm32_usart_conf_f4.h
@@ -61,26 +61,6 @@ static_assert(USART2_IRQ == USART2_IRQn);
 #error Unsupported USARTx
 #endif
 
-#define STM32_USART2_ENABLED             1
-#define USART2_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
-#define USART2_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
-#define USART2_TX_PIN                    GPIO_PIN_2
-#define USART2_TX_GPIO_PORT              GPIOA
-#define USART2_TX_AF                     GPIO_AF7_USART2
-#define USART2_RX_PIN                    GPIO_PIN_3
-#define USART2_RX_GPIO_PORT              GPIOA
-#define USART2_RX_AF                     GPIO_AF7_USART2
-
-#define STM32_USART3_ENABLED             1
-#define USART3_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
-#define USART3_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
-#define USART3_TX_PIN                    GPIO_PIN_8
-#define USART3_TX_GPIO_PORT              GPIOD
-#define USART3_TX_AF                     GPIO_AF7_USART3
-#define USART3_RX_PIN                    GPIO_PIN_9
-#define USART3_RX_GPIO_PORT              GPIOD
-#define USART3_RX_AF                     GPIO_AF7_USART3
-
 
 #define STM32_USART_FLAGS(uart)   uart->SR
 #define STM32_USART_RXDATA(uart)  uart->DR
