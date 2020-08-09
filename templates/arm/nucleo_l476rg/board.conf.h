155bbeed8d1111222dfc6ffbec7ab3371e05f09b
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-08-09 16:25:27 +0300

    templates: stm32: Add board.conf.h

---
+++ templates/arm/nucleo_l476rg/board.conf.h
@@ -0,0 +1,6 @@
+#include <nucleo_l476rg.conf.h>
+
+CONFIG {
+	uarts[1].status = DISABLED;
+	uarts[2].status = ENABLED;
+}
---
+++ templates/arm/stm32l4-discovery/board.conf.h
@@ -0,0 +1,6 @@
+#include <stm32l4discovery.conf.h>
+
+CONFIG {
+	uarts[1].status = ENABLED;
+	uarts[2].status = DISABLED;
+}
