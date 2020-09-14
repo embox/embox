2fe5228e060c171115ddf364a39a68bca220b977
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-09-14 17:05:39 +0300

    third-party: Clean lua port

--- third-party/lua/lib/luasocket/include/fcntl.h
+++
@@ -1,8 +0,0 @@
-#ifndef LUASOCKET_FCNTL_H_
-#define LUASOCKET_FCNTL_H_
-
-#include_next <fcntl.h>
-
-#define O_NOCTTY -1
-
-#endif /* LUASOCKET_FCNTL_H_ */
--- third-party/lua/lib/luasocket/include/netinet/in.h
+++
@@ -1,6 +0,0 @@
-#ifndef LUASOCKET_NETINET_IN_H_
-#define LUASOCKET_NETINET_IN_H_
-
-#include_next <netinet/in.h>
-
-#endif /* LUASOCKET_NETINET_IN_H_ */
