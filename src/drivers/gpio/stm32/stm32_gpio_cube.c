42ece7ebfb52b976bfb78d95616ba806c90c8d79
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-09-25 16:07:18 +0300

    drivers: Add stm32f1 support to gpio/stm32

--- src/drivers/gpio/stm32/stm32_gpio_cube.c
+++ src/drivers/gpio/stm32/stm32_gpio_cube.c
@@ -74,8 +74,9 @@ static int stm32_gpio_setup_mode(unsigned char port, gpio_mask_t pins,
 			GPIO_InitStruct.Pull = GPIO_PULLDOWN;
 		}
 	} else if (mode & GPIO_MODE_OUT_ALTERNATE) {
+#ifndef STM32F1_CUBE_GPIO /* There is no Alternate field in GPIO_InitTypeDef */
 		GPIO_InitStruct.Alternate = GPIO_GET_ALTERNATE(mode);
-
+#endif
 		if (mode & GPIO_MODE_OUT_OPEN_DRAIN) {
 			GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
 		}
