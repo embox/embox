2c25978e5dddd5c3ef4e76d2e56d34e3008fa709
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-09-27 17:34:03 +0300

    cmds: Enable set up stack size in service command

--- src/cmds/service.c
+++ src/cmds/service.c
@@ -5,16 +5,18 @@
  * @date 16.04.16
  * @author Alexander Kalmuk
  */
-#include <util/log.h>
+
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <mem/misc/pool.h>
-#include <util/dlist.h>
 #include <sys/types.h>
+#include <sys/resource.h>
 #include <signal.h>
+
+#include <util/dlist.h>
+#include <mem/misc/pool.h>
 #include <framework/mod/options.h>
 
 #define SERVICE_NAME_LEN OPTION_GET(NUMBER,service_name_len)
@@ -30,6 +32,12 @@ POOL_DEF(pool, struct service_list, SERVICES_COUNT);
 
 static DLIST_DEFINE(head);
 
+static void print_usage(void) {
+	printf("USAGE: service - print list of available services\n"
+			"service [-s stack_size] service_name - start a new service\n"
+			"service service_name stop - deactivate the service\n");
+}
+
 static void service_add(const char *name, pid_t service_pid) {
   	struct service_list *new_element;
 	struct service_list *tmp;
@@ -44,7 +52,7 @@ static void service_add(const char *name, pid_t service_pid) {
 	new_element = pool_alloc(&pool);
 
 	if (new_element == NULL) {
-		log_error("Pool is full.");
+		printf("could not add service (the pool is full)\n");
 		return;
 	}
 
@@ -109,7 +117,9 @@ static int service_run(const char *path, char *const argv[]) {
 }
 
 int main(int argc, char **argv) {
+	int opt;
 	const char *command;
+	int arg_offset = 1;
 
 	if (argc == 0) {
 		return -EINVAL;
@@ -120,9 +130,33 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
-	command = argv[1];
+	opt = getopt(argc, argv, "s:h");
+	switch (opt) {
+		case 'h':
+			print_usage();
+			return 0;
+		case 's': {
+			int stack_size;
+			struct rlimit rlp;
 
-	if (argc > 2 && strcmp(argv[2], "stop") == 0) {
+			if (!optarg) {
+				print_usage();
+				return -1;
+			}
+			stack_size = (int) strtoul(optarg, NULL, 0);
+			rlp.rlim_max = stack_size;
+			rlp.rlim_cur = stack_size;
+			setrlimit(RLIMIT_STACK, &rlp);
+			printf("service will be launched with %d stack size\n", stack_size);
+			arg_offset = 3;
+			break;
+
+		}
+	}
+
+	command = argv[arg_offset];
+
+	if ((argc > (arg_offset + 1)) && strcmp(argv[arg_offset + 1], "stop") == 0) {
 		service_delete(command);
 		return 0;
 	}
@@ -130,5 +164,5 @@ int main(int argc, char **argv) {
 	argv[argc] = NULL;
 	printf("Starting service: %s\n", command);
 
-	return service_run(command, &argv[1]);
+	return service_run(command, &argv[arg_offset]);
 }
