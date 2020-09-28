be321d4bc4a53c7411ba135c90eaa7a644ebdbc1
Alex Kalmuk <alexkalmuk@gmail.com>
2020-09-28 12:12:59 +0300

    thread_allocator_heap: Allocate pure stack for threads
    Move struct thread and other information out from stack

--- src/kernel/thread/thread_allocator/thread_allocator_heap.c
+++ src/kernel/thread/thread_allocator/thread_allocator_heap.c
@@ -10,6 +10,7 @@
 #include <kernel/thread/thread_alloc.h>
 #include <kernel/thread/stack_protect.h>
 #include <mem/page.h>
+#include <mem/misc/pool.h>
 #include <hal/mmu.h>
 #include <assert.h>
 #include <embox/unit.h>
@@ -18,39 +19,60 @@ EMBOX_UNIT_INIT(thread_allocator_heap_init);
 
 #define THREAD_HEAP_SIZE     OPTION_GET(NUMBER, thread_heap_size)
 
+#define POOL_SZ \
+	OPTION_MODULE_GET(embox__kernel__thread__core, NUMBER, thread_pool_size)
+
+struct thread_info {
+	struct thread thread;
+	size_t pages;
+	void *stack;
+};
+
+POOL_DEF(thread_info_pool, struct thread_info, POOL_SZ);
+
 static struct page_allocator *thread_heap_allocator;
 
 struct thread *thread_alloc(size_t stack_sz) {
-	struct thread *t;
+	struct thread_info *ti;
+	uint8_t *stack;
 	size_t pages = (stack_sz + PAGE_SIZE() - 1) / PAGE_SIZE();
 
-	t = (struct thread *) page_alloc(thread_heap_allocator, pages);
-	if (!t) {
+	stack_sz = pages * PAGE_SIZE();
+
+	ti = (struct thread_info *) pool_alloc(&thread_info_pool);
+	if (!ti) {
 		return NULL;
 	}
-	memset(t, 0x53, stack_sz);
-	*(uint32_t *) (t + 1) = pages;
 
-	thread_stack_set(t, (uint8_t *)(t + 1) + 4);
-	thread_stack_set_size(t, stack_sz - sizeof *t - 4);
+	stack = (uint8_t *) page_alloc(thread_heap_allocator, pages);
+	if (!stack) {
+		pool_free(&thread_info_pool, ti);
+		return NULL;
+	}
+	memset(stack, 0x53, stack_sz);
 
-	stack_protect(t, stack_sz);
+	ti->stack = stack;
+	ti->pages = pages;
 
-	return t;
+	thread_stack_set(&ti->thread, stack);
+	thread_stack_set_size(&ti->thread, stack_sz);
+
+	stack_protect(&ti->thread, stack_sz);
+
+	return &ti->thread;
 }
 
 void thread_free(struct thread *t) {
-	size_t pages;
+	struct thread_info *ti = (struct thread_info *) t;
 
 	assert(t != NULL);
 
 	stack_protect_release(t);
 
-	pages = *(uint32_t *) (t + 1);
+	page_free(thread_heap_allocator, ti->stack, ti->pages);
+	memset(ti->stack, 0xa5, ti->pages * PAGE_SIZE());
 
-	page_free(thread_heap_allocator, t, pages);
-
-	memset(t, 0xa5, pages * PAGE_SIZE());
+	pool_free(&thread_info_pool, ti);
 }
 
 static int thread_allocator_heap_init(void) {
