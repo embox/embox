fefa1897a97663367c280ef269b223d3b9524b4f
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-09-29 19:13:54 +0300

    kernel: Make thread user stack available

--- src/kernel/thread/thread_allocator/thread_allocator_pool.c
+++ src/kernel/thread/thread_allocator/thread_allocator_pool.c
@@ -19,16 +19,31 @@
 
 #include <framework/mod/options.h>
 
+#define USE_USER_STACK    OPTION_GET(NUMBER, thread_user_stack)
 
-#define STACK_SZ     THREAD_DEFAULT_STACK_SIZE
+#define STACK_SZ          THREAD_DEFAULT_STACK_SIZE
 
 #define POOL_SZ \
 	OPTION_MODULE_GET(embox__kernel__thread__core, NUMBER, thread_pool_size)
+
+#if (USE_USER_STACK == 0)
 
 typedef union thread_pool_entry {
 	struct thread thread;
 	char stack[STACK_SZ];
 } thread_pool_entry_t;
+
+#else
+
+extern void *thread_user_stack_alloc(struct thread *t, size_t stack_sz);
+extern void thread_user_stack_free(void *t);
+
+typedef struct thread_pool_entry {
+	struct thread thread;
+	void *user_stack;
+} thread_pool_entry_t;
+
+#endif
 
 #ifdef STACK_PROTECT_MMU
 #include <mem/vmem.h>
@@ -49,11 +64,14 @@ struct thread *thread_alloc(size_t stack_sz) {
 	memset(block, 0x53, sizeof(*block));
 
 	t = &block->thread;
-
+#if (USE_USER_STACK == 0)
 	stack_sz = STACK_SZ;
 	thread_stack_init(t, stack_sz);
+#else
+	block->user_stack = thread_user_stack_alloc(t, stack_sz);
+#endif
 
-    stack_protect(t, stack_sz);
+	stack_protect(t, stack_sz);
 
 	return t;
 }
@@ -66,6 +84,12 @@ void thread_free(struct thread *t) {
 	block = (thread_pool_entry_t *)t;
 
 	stack_protect_release(t);
+
+#if (USE_USER_STACK == 0)
+
+#else
+	thread_user_stack_free(block->user_stack);
+#endif
 
 	memset(block, 0xa5, sizeof(*block));
 
