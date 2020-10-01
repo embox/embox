5e36aaa49ca031e5aaedeff69e27e38f5bb4a8b0
Alex Kalmuk <alexkalmuk@gmail.com>
2020-10-01 16:06:19 +0300

    dns_file: Implement dns_get_nameserver

--- src/net/lib/dns/dns_file.c
+++ src/net/lib/dns/dns_file.c
@@ -10,24 +10,79 @@
 #include <errno.h>
 
 #include <util/err.h>
+#include <util/log.h>
 
 #include <net/lib/dns.h>
 
 #include <framework/mod/options.h>
 
-/**
- * DNS nameservers
- */
-#define NAMESERVER_DEFAULT OPTION_STRING_GET(nameserver)
 #define RESOLV_FILE        OPTION_STRING_GET(resolv_file)
 
+static int nameserver_inited = 0;
 static char nameserver_ip[16]; /* only for IPv4 */
 
 const char *dns_get_nameserver(void) {
-	return nameserver_ip;
+	char buf[128];
+	FILE *input;
+	int ret;
+	char *res;
+	unsigned char ip4_addr[sizeof(struct in_addr)];
+
+	if (nameserver_inited) {
+		return nameserver_ip;
+	}
+
+	/* Read resolv.conf only once and store obtained IP address
+	 * to nameserver_ip */
+	nameserver_inited = 1;
+
+	input = fopen(RESOLV_FILE, "r");
+	if (!input) {
+		log_error("No file %s", RESOLV_FILE);
+		return NULL;
+	}
+
+	/* Skip comments */
+	while ((ret = fscanf(input, "%s", buf)) != EOF
+		    && !strncmp(buf, "#", 1)) {
+		char c;
+
+		do {
+			c = fgetc(input);
+		} while (c != '\n' && c != EOF);
+	}
+
+	if (ret == EOF) {
+		goto out_close;
+	}
+
+	if (strcmp(buf, "nameserver")) {
+		log_error("Unknown keyword: %s", buf);
+		goto out_close;
+	}
+
+	fscanf(input, "%s", buf);
+
+	if (1 != inet_pton(AF_INET, buf, &ip4_addr)) {
+		log_error("wrong nameserver %s", buf);
+		goto out_close;
+	}
+
+	memcpy(nameserver_ip, buf, sizeof nameserver_ip);
+
+	res = nameserver_ip;
+
+out_close:
+	fclose(input);
+
+	log_debug("nameserver = %s", res ? res : "unresolved");
+
+	return (const char *) res;
 }
 
 char *dns_set_nameserver(char *nameserver) {
+/* FIXME */
+#if 0
 	FILE *file;
 	char buf[0x40];
 	char *res;
@@ -66,6 +121,8 @@ char *dns_set_nameserver(char *nameserver) {
 	fclose(file);
 
 	return nameserver_ip;
+#endif
+	return NULL;
 }
 
 char *dns_init_nameserver(char *nameserver) {
