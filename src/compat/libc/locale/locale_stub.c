e69f8f7c5a0053c5d1c944796fabe96312a927c3
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-10-28 12:09:59 +0300

    compat: Make setlocale() as symbol (not static inline function)

---
+++ src/compat/libc/locale/locale_stub.c
@@ -0,0 +1,14 @@
+/**
+ * @file
+ *
+ * @date Oct 28, 2020
+ * @author Anton Bondarev
+ */
+
+#include <stddef.h>
+#include <locale.h>
+
+char *setlocale(int category, const char *locale) {
+	(void)category; (void)locale;
+	return NULL;
+}
