a2866ec413a3ef0e2e823911f1a0e2345d70ddf4
Alex Kalmuk <alexkalmuk@gmail.com>
2020-10-28 12:21:02 +0300

    libc: Add strtof

---
+++ src/compat/libc/stdlib/strtof.c
@@ -0,0 +1,12 @@
+/**
+ * @file
+ *
+ * @date   28.10.2020
+ * @author Alexander Kalmuk
+ */
+
+#include <stdlib.h>
+
+float strtof(const char *nptr, char **endptr) {
+	return strtod(nptr, endptr);
+}
