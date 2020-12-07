817e6fc598289de2a927934fdce280aa674ea2b3
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-12-07 17:01:20 +0300

    drivers: stm32: Enable ethernet for STM32H7 series

---
+++ src/drivers/net/stm32cube/stm32h7cube_eth.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ *
+ * @date 17.03.2017
+ * @author Anton Bondarev
+ */
+
+#ifndef STM32H7CUBE_ETH_H_
+#define STM32H7CUBE_ETH_H_
+
+#include <stm32h7xx_hal.h>
+
+#define PHY_ADDRESS       0x00
+
+#endif /* STM32H7CUBE_ETH_H_ */
