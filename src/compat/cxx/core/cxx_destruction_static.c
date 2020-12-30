fb9a7aa9d1f93ac00f346fe2fa96c709ebf733f5
Alex Kalmuk <alexkalmuk@gmail.com>
2020-12-30 23:26:52 +0300

    cxx: (minor) Refactoring code style cxx_destruction_static.c

--- src/compat/cxx/core/cxx_destruction_static.c
+++ src/compat/cxx/core/cxx_destruction_static.c
@@ -18,50 +18,45 @@ struct atexit_func_entry {
 	void *dso_handle;
 };
 
-static struct atexit_func_entry __atexit_funcs[TABLE_SIZE];
-static int __atexit_func_count = 0;
+static struct atexit_func_entry atexit_funcs[TABLE_SIZE];
+static int atexit_func_count = 0;
 
 /* must be available for C++ ABI */
 void *__dso_handle = 0;
 
-int __cxa_atexit(void (*f)(void *), void *objptr, void *dso)
-{
-	if (__atexit_func_count >= TABLE_SIZE) {
+int __cxa_atexit(void (*f)(void *), void *objptr, void *dso) {
+	if (atexit_func_count >= TABLE_SIZE) {
 		printf("__cxa_atexit: static destruction table overflow.\n");
-		// FIXME: assert(false);
 		return -1;
-	};
-	__atexit_funcs[__atexit_func_count].destructor_func = f;
-	__atexit_funcs[__atexit_func_count].obj_ptr = objptr;
-	__atexit_funcs[__atexit_func_count].dso_handle = dso;
-	__atexit_func_count++;
+	}
+
+	atexit_funcs[atexit_func_count].destructor_func = f;
+	atexit_funcs[atexit_func_count].obj_ptr = objptr;
+	atexit_funcs[atexit_func_count].dso_handle = dso;
+	atexit_func_count++;
+
 	return 0;
 };
 
 void __cxa_finalize(void *f) {
-	int i = __atexit_func_count;
-	if (!f)
-	{
-		while (i--)
-		{
-			if (__atexit_funcs[i].destructor_func)
-			{
-				(*__atexit_funcs[i].destructor_func)(__atexit_funcs[i].obj_ptr);
-				__atexit_funcs[i].destructor_func = 0;
-			};
-		};
-		__atexit_func_count = 0;
-		return;
-	};
+	int i = atexit_func_count;
 
-	for ( ; i >= 0; --i)
-	{
-		if (__atexit_funcs[i].destructor_func == f)
-		{
-			(*__atexit_funcs[i].destructor_func)(__atexit_funcs[i].obj_ptr);
-			__atexit_funcs[i].destructor_func = 0;
-		};
-	};
+	if (!f) {
+		while (i--) {
+			if (atexit_funcs[i].destructor_func) {
+				(*atexit_funcs[i].destructor_func)(atexit_funcs[i].obj_ptr);
+				atexit_funcs[i].destructor_func = 0;
+			}
+		}
+		atexit_func_count = 0;
+	} else {
+		for ( ; i >= 0; --i) {
+			if (atexit_funcs[i].destructor_func == f) {
+				(*atexit_funcs[i].destructor_func)(atexit_funcs[i].obj_ptr);
+				atexit_funcs[i].destructor_func = 0;
+			}
+		}
+	}
 }
 
 void cxx_invoke_destructors(void) {
