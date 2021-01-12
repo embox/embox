c0ff914c24ab161b6df176a8b5d5997f7b7e2014
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-01-12 22:29:39 +0300

    efm32: Update drivers/video/efm32_lcd

--- src/drivers/video/efm32_lcd/efm32_lcd.c
+++ src/drivers/video/efm32_lcd/efm32_lcd.c
@@ -5,17 +5,54 @@
  * @author  Roman Kurbatov
  * @date    05.04.2014
  */
-
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <embox/unit.h>
 
-#include <displayls013b7dh03.h>
+#include <display.h>
 
-EMBOX_UNIT_INIT(init);
 
-static int init(void) {
-	DISPLAY_Ls013b7dh03Init();
+EMBOX_UNIT_INIT(efm_lcd_init);
 
+#define START_WITH_LOGO OPTION_GET(NUMBER, start_with_logo)
+
+#define DISPLAY_DEVICE_NO (0)
+
+extern const uint8_t demo_image_mono_128x128[128][16];
+
+static int efm_lcd_init(void) {
+	DISPLAY_Device_t      displayDevice;
+	EMSTATUS status;
+	DISPLAY_PixelMatrix_t pixelMatrixBuffer;
+
+	/* Initialize the DISPLAY module. */
+	status = DISPLAY_Init();
+	if (DISPLAY_EMSTATUS_OK != status) {
+		return status;
+	}
+
+	/* Retrieve the properties of the DISPLAY. */
+	status = DISPLAY_DeviceGet(DISPLAY_DEVICE_NO, &displayDevice);
+	if (DISPLAY_EMSTATUS_OK != status) {
+		return status;
+	}
+	/* Allocate a framebuffer from the DISPLAY device driver. */
+	displayDevice.pPixelMatrixAllocate(&displayDevice,
+			displayDevice.geometry.width,
+			displayDevice.geometry.height,
+			&pixelMatrixBuffer);
+#if START_WITH_LOGO
+	memcpy(pixelMatrixBuffer, demo_image_mono_128x128,
+			displayDevice.geometry.width * displayDevice.geometry.height / 8 );
+
+	status = displayDevice.pPixelMatrixDraw(&displayDevice,
+			pixelMatrixBuffer,
+			0,
+			displayDevice.geometry.width,
+			0,
+			displayDevice.geometry.height);
+#endif
 	return 0;
 }
