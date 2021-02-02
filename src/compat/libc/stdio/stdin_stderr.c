0f690b9c9b89632ece81014045788bd45e974bb4
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-02-02 22:41:40 +0300

    stdio: Extract stdout for a global version

--- src/compat/libc/stdio/stdin_stderr.c
+++ src/compat/libc/stdio/stdin_stderr.c
@@ -18,21 +18,9 @@ static FILE stdin_struct = {
 };
 FILE *stdin = &stdin_struct;
 
-/* stdout */
-static char stdout_obuf[16];
-static FILE stdout_struct = {
-	.fd = STDOUT_FILENO,
-	.flags = O_WRONLY,
-	.buftype = _IOLBF,
-	.obuf = stdout_obuf,
-	.obuf_sz = sizeof(stdout_obuf),
-};
-FILE *stdout = &stdout_struct;
-
 /* stderr */
 static FILE stderr_struct = {
 	.fd = STDERR_FILENO,
 	.flags = O_WRONLY,
 };
 FILE *stderr = &stderr_struct;
-
