22d4b035e86a62d0e5dbbe8a9c1feeb2147af01d
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-02-08 15:23:00 +0300

    minor: website: Rm init action treating from led_driver cmd

--- project/website/cmds/http_admin/embox-cgi-bin/led_driver/led_driver.c
+++ project/website/cmds/http_admin/embox-cgi-bin/led_driver/led_driver.c
@@ -18,9 +18,7 @@ int main(int argc, char *argv[]) {
 	const char *action = argv[1];
 
 	if (action) {
-		if (0 == strcmp(action, "init")) {
-			return led_driver_init();
-		} else if (0 == strcmp(action, "set")) {
+		if (0 == strcmp(action, "set")) {
 			return led_driver_set(atoi(argv[2]));
 		} else if (0 == strcmp(action, "clr")) {
 			return led_driver_clear(atoi(argv[2]));
