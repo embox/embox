6e4eb53ccd2f268e3043d40078de35839f866168
Alex Kalmuk <alexkalmuk@gmail.com>
2021-02-10 17:32:25 +0300

    templates: Add spi to board.conf.h for stm32

--- platform/stm32/templates/l4/nucleo_l476rg/board.conf.h
+++ platform/stm32/templates/l4/nucleo_l476rg/board.conf.h
@@ -1,6 +1,11 @@
 #include <nucleo_l476rg.conf.h>
 
 CONFIG {
+	/* uarts */
 	uarts[1].status = DISABLED;
 	uarts[2].status = ENABLED;
+
+	/* spis */
+	spis[1].status = ENABLED;
+	spis[2].status = DISABLED;
 }
