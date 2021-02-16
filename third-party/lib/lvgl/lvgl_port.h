89096841e76601681ddb943b6d6e9de5a23798c2
Alex Kalmuk <alexkalmuk@gmail.com>
2021-02-16 10:17:17 +0300

    lvgl: Implement input device porting layer

--- third-party/lib/lvgl/lvgl_port.h
+++ third-party/lib/lvgl/lvgl_port.h
@@ -15,8 +15,14 @@
 
 #include "lvgl.h"
 
+/* Display */
 extern int lvgl_port_fbdev_init(const char *fb_path);
 extern void lvgl_port_fbdev_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area,
 	    lv_color_t *color_p);
+
+/* Input device */
+extern int lvgl_port_input_dev_init(const char *path);
+extern void lvgl_port_mouse_handle(void);
+extern bool lvgl_port_mouse_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data);
 
 #endif /* THIRD_PARTY_LIB_LVGL_LVGL_PORT_H_ */
