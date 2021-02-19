4674f110e3578436b8623a04b8ee0cedf90a2ad9
Alex Kalmuk <alexkalmuk@gmail.com>
2021-02-19 12:08:54 +0300

    lvgl: Split display flush in memcpy and stm32 implementations

--- third-party/lib/lvgl/display_port.c
+++ third-party/lib/lvgl/display_port.c
@@ -17,57 +17,9 @@
 
 #include "lvgl.h"
 
-static uint8_t *fbp = NULL;
-static struct fb_var_screeninfo vinfo;
-static struct fb_fix_screeninfo finfo;
-
-void lvgl_port_fbdev_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area,
-	    lv_color_t *color_p) {
-	int32_t act_x1, act_y1, act_x2, act_y2;
-	long int location = 0;
-	lv_coord_t w;
-
-	if (fbp == NULL ||
-		    area->x2 < 0 ||
-		    area->y2 < 0 ||
-		    area->x1 > (int32_t)vinfo.xres - 1 ||
-		    area->y1 > (int32_t)vinfo.yres - 1) {
-		lv_disp_flush_ready(disp_drv);
-		return;
-	}
-
-	/* Truncate the area to the screen */
-	act_x1 = area->x1 < 0 ? 0 : area->x1;
-	act_y1 = area->y1 < 0 ? 0 : area->y1;
-	act_x2 = area->x2 > (int32_t)vinfo.xres - 1 ? (int32_t)vinfo.xres - 1 : area->x2;
-	act_y2 = area->y2 > (int32_t)vinfo.yres - 1 ? (int32_t)vinfo.yres - 1 : area->y2;
-
-	w = (act_x2 - act_x1 + 1);
-
-	if (vinfo.bits_per_pixel == 32 || vinfo.bits_per_pixel == 24) {
-		uint32_t * fbp32 = (uint32_t *)fbp;
-		int32_t y;
-
-		for(y = act_y1; y <= act_y2; y++) {
-			location = (act_x1 + vinfo.xoffset) + (y + vinfo.yoffset) * finfo.line_length / 4;
-			memcpy(&fbp32[location], (uint32_t *)color_p, (act_x2 - act_x1 + 1) * 4);
-			color_p += w;
-		}
-	} else if (vinfo.bits_per_pixel == 16) {
-		uint16_t * fbp16 = (uint16_t *)fbp;
-		int32_t y;
-
-		for(y = act_y1; y <= act_y2; y++) {
-			location = (act_x1 + vinfo.xoffset) + (y + vinfo.yoffset) * finfo.line_length / 2;
-			memcpy(&fbp16[location], (uint32_t *)color_p, (act_x2 - act_x1 + 1) * 2);
-			color_p += w;
-		}
-	} else {
-		log_error("Unsupported bits_per_pixel=%d", vinfo.bits_per_pixel);
-	}
-
-	lv_disp_flush_ready(disp_drv);
-}
+uint8_t *lv_fbp = NULL;
+struct fb_var_screeninfo lv_vinfo;
+struct fb_fix_screeninfo lv_finfo;
 
 int lvgl_port_fbdev_init(const char *fb_path) {
 	int fbfd;
@@ -78,20 +30,20 @@ int lvgl_port_fbdev_init(const char *fb_path) {
 		log_error("Error open framebuffer %s", fb_path);
 		return -1;
 	}
-	if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+	if (ioctl(fbfd, FBIOGET_FSCREENINFO, &lv_finfo) == -1) {
 		log_error("Error reading finfo for framebuffer %s", fb_path);
 		return -1;
 	}
-	if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+	if (ioctl(fbfd, FBIOGET_VSCREENINFO, &lv_vinfo) == -1) {
 		log_error("Error reading vinfo for framebuffer %s", fb_path);
 		return -1;
 	}
 
-	fb_size = finfo.line_length * vinfo.yres;
+	fb_size = lv_finfo.line_length * lv_vinfo.yres;
 
-	fbp = (uint8_t *) mmap(0, fb_size, PROT_READ | PROT_WRITE,
+	lv_fbp = (uint8_t *) mmap(0, fb_size, PROT_READ | PROT_WRITE,
 				              MAP_SHARED, fbfd, 0);
-	if (fbp == MAP_FAILED) {
+	if (lv_fbp == MAP_FAILED) {
 		log_error("Error mmap framebuffer %s", fb_path);
 	}
 
