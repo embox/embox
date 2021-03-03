0624afc572b33512096652721806ecd5382b8dbb
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-03-03 11:11:40 +0300

    net: Revert namespaces in src/net/l3/ipv4/ip_input.c

--- src/net/l3/ipv4/ip_input.c
+++ src/net/l3/ipv4/ip_input.c
@@ -114,8 +114,12 @@ static int ip_rcv(struct sk_buff *skb, struct net_device *dev) {
 		 * Check the destination address, and if it doesn't match
 		 * any of own addresses, retransmit packet according to the routing table.
 		 */
+#if defined(NET_NAMESPACE_ENABLED) && (NET_NAMESPACE_ENABLED == 1)
 		if (!ip_is_local_net_ns(iph->daddr, IP_LOCAL_BROADCAST,
 					skb->dev->net_ns)) {
+#else
+		if (!ip_is_local(iph->daddr, IP_LOCAL_BROADCAST)) {
+#endif
 			if (0 != nf_test_skb(NF_CHAIN_FORWARD, NF_TARGET_ACCEPT, skb)) {
 				log_debug("ip_rcv: dropped by forward netfilter");
 				stats->rx_dropped++;
