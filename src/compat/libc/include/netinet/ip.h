981e375f128d58a540f3dff60d171690bfd434b3
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-04-25 18:25:26 +0300

    compat: Improve IP_TOS & IP_HDRINCL definitions

--- src/compat/libc/include/netinet/ip.h
+++ src/compat/libc/include/netinet/ip.h
@@ -9,6 +9,11 @@
 #define _NETINET_IP_H_
 
 #define IPTOS_LOWDELAY  0x10
-#define IP_TOS          0x1
+#define IP_TOS          0x01
+/* Socket option.
+ * The obvious way to tell the IP layer not to prepend its own header
+ * is by calling the setsockopt() syscall and setting the IP_HDRINCL.
+ * See http://sock-raw.org/papers/sock_raw */
+#define IP_HDRINCL      0x03
 
 #endif
