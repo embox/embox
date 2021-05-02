0f39cd949115b6073fb63cb28971b103fcb89b38
Alex Kalmuk <alexkalmuk@gmail.com>
2021-05-02 14:24:10 +0300

    opencv: Add grayscale images support to cv_embox_imshowfb

--- project/opencv/lib/cv_embox_imshowfb.cpp
+++ project/opencv/lib/cv_embox_imshowfb.cpp
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <drivers/video/fb.h>
+#include <util/log.h>
 #include <algorithm>
 #include <cv_embox_imshowfb.hpp>
 
@@ -21,19 +22,51 @@ void imshowfb(cv::Mat& img, int fbx) {
         return;
     }
 
+	log_debug("\nimage width:    %d\n"
+	          "image height:   %d\n"
+	          "image size:     %dx%d\n"
+	          "image depth:    %d\n"
+	          "image channels: %d\n"
+	          "image type:     %d",
+	          img.cols, img.rows, img.size().width, img.size().height,
+	          img.depth(), img.channels(), img.type());
+
+	if (img.channels() != 1 && img.channels() != 3) {
+		fprintf(stderr, "Unsupported channels count: %d\n", img.channels());
+
+		return;
+	}
+
     h = std::min((int) fbi->var.yres, img.rows);
-    w = std::min((int) (fbi->var.bits_per_pixel * fbi->var.xres) / 8, 3 * img.cols);
+    w = std::min((int) (fbi->var.bits_per_pixel * fbi->var.xres) / 8, img.channels() * img.cols);
 
     for (int y = 0; y < h; y++) {
         const uchar *row = &img.at<uchar>(y, 0);
-        for (int x = 0; x < w; x += 3) {
-            unsigned rgb888    =
-                0xFF000000 |
-                unsigned(row[x + 2]) |
-                (unsigned(row[x + 1]) << 8) |
-                (unsigned(row[x]) << 16);
+        for (int x = 0; x < w; x += img.channels()) {
+            unsigned rgb888;
 
-            ((uint32_t *) fbi->screen_base)[fbi->var.xres * y + x / 3] = rgb888;
+			switch (img.channels()) {
+			case 1:
+			{
+				unsigned val = unsigned(row[x]);
+
+            	rgb888 = 0xFF000000 | val | (val << 8) | (val << 16);
+
+				break;
+			}
+			case 3:
+            	rgb888 =
+            	    0xFF000000 |
+            	    unsigned(row[x + 2]) |
+            	    (unsigned(row[x + 1]) << 8) |
+            	    (unsigned(row[x]) << 16);
+
+				break;
+			default:
+				break;
+			}
+
+            ((uint32_t *) fbi->screen_base)[fbi->var.xres * y + x / img.channels()] = rgb888;
         }
     }
 }
