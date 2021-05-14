e6c7e4c277b5a8afbb5cbffe7de4c44538746745
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-05-14 18:02:16 +0300

    drivers: Add stm32_audio_h7 driver

---
+++ src/drivers/audio/stm32/stm32h745i_discovery_audio.h
@@ -0,0 +1,18 @@
+/**
+ * @file
+ *
+ * @date 04.08.2019
+ * @author Alexander Kalmuk
+ */
+
+#ifndef STM32H745I_DISCOVERY_AUDIO_H_
+#define STM32H745I_DISCOVERY_AUDIO_H_
+
+#include "stm32h745i_discovery.h"
+#include "stm32h745i_discovery_audio.h"
+
+#define STM32H745I_DISCOVERY
+
+extern int stm32h7_audio_init(void);
+
+#endif /* STM32H745I_DISCOVERY_AUDIO_H_ */
