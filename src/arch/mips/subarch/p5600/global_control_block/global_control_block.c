2dd0a9c4fe30163a540b1bd45d7c9f4e3ad27e6f
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-05-30 20:54:45 +0300

    mips: Add l2 cache routines to p5600/global_control_block

--- src/arch/mips/subarch/p5600/global_control_block/global_control_block.c
+++ src/arch/mips/subarch/p5600/global_control_block/global_control_block.c
@@ -22,3 +22,13 @@ uint32_t mips32_gcb_set_register(uint32_t offset, uint32_t val) {
 uint32_t mips32_gcb_get_register(uint32_t offset) {
 	return REG32_LOAD(GCB_BASE + offset);
 }
+
+uint32_t mips_cm_l2_line_size(void) {
+	unsigned long l2conf, line_sz;
+
+	l2conf = REG32_LOAD(GCB_BASE + GCR_L2_CONFIG);
+
+	line_sz = l2conf >> GCR_L2_CONFIG_LINESZ_SHIFT;
+	line_sz &= ((1 << GCR_L2_CONFIG_LINESZ_BITS) - 1);
+	return line_sz ? (2 << line_sz) : 0;
+}
