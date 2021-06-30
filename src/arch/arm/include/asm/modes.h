479b5d08bd507b2a92a771b3923e028c9eb6f310
Kevin Peck <kevindpeck@gmail.com>
2021-06-29 21:42:06 -0400

    Add macro definition and comments for clarity, consistancy

--- src/arch/arm/include/asm/modes.h
+++ src/arch/arm/include/asm/modes.h
@@ -14,6 +14,7 @@
 #define ARM_MODE_FIQ 0x11
 #define ARM_MODE_IRQ 0x12
 #define ARM_MODE_SVC 0x13
+#define ARM_MODE_MON 0x16
 #define ARM_MODE_ABT 0x17
 #define ARM_MODE_UND 0x1B
 #define ARM_MODE_USR 0x10
