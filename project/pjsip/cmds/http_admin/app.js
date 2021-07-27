9a7579fb360d936a4cd210a29b6182b1316ceda4
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-07-27 17:17:03 +0300

    project: Update sip-nuklear web site

--- project/pjsip/cmds/http_admin/app.js
+++ project/pjsip/cmds/http_admin/app.js
@@ -9,7 +9,7 @@ angular.module("HttpAdmin", ['ngRoute', 'ui.bootstrap'])
 .controller("InterfacesAdminCtrl", ['$scope', '$http', function($scope, $http) {
     $scope.interfaces = [];
 
-    $http.get('cgi-bin/http_admin_ifaces').success(function (data) {
+    $http.get('cgi-bin/http_admin_iface_list').success(function (data) {
         $scope.interfaces = data;
     });
 
@@ -19,7 +19,7 @@ angular.module("HttpAdmin", ['ngRoute', 'ui.bootstrap'])
             'data' : iface
         };
 
-        $http.post('cgi-bin/http_admin_ifaces', post_data);
+        $http.post('cgi-bin/http_admin_iface_list', post_data);
     };
 
     $scope.flash = function() {
@@ -27,7 +27,7 @@ angular.module("HttpAdmin", ['ngRoute', 'ui.bootstrap'])
             'action' : 'flash_settings',
         };
 
-        $http.post('cgi-bin/http_admin_ifaces', post_data);
+        $http.post('cgi-bin/http_admin_iface_list', post_data);
     };
 
 }])
