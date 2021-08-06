4cf8d559dc483961d65ed7920fec8b51c0b87f17
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-08-06 12:17:22 +0300

    cmds: Improve 'time' command

--- src/cmds/sys/time.c
+++ src/cmds/sys/time.c
@@ -13,21 +13,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
 	struct timespec ts_before, ts_after, ts_res;
+	char concat[128] = "";
 
 	if (argc <= 1) {
 		printf("Please enter command name\n");
 		return -EINVAL;
 	}
 
+	for (int i = 1; i < argc; i++) {
+		strncat(concat, argv[i], sizeof(concat) - 1);
+		strncat(concat, " ", sizeof(concat) - 1);
+		if (strlen(concat) >= (sizeof(concat) - 1)) {
+			printf("Please use shorter command line\n");
+			return -EINVAL;
+		}
+
+	}
+
 	clock_gettime(CLOCK_MONOTONIC, &ts_before);
-	system(argv[1]);
+	system(concat);
 	clock_gettime(CLOCK_MONOTONIC, &ts_after);
 	timespecsub(&ts_after, &ts_before, &ts_res);
 
-	printf("\ntime: %d.%03dS\n", (int)ts_res.tv_sec, (int)(ts_res.tv_nsec/1000000L));
+	printf("\ntime: %d.%03dS\n", (int) ts_res.tv_sec,
+			(int) (ts_res.tv_nsec / 1000000L));
 
 	return 0;
 }
