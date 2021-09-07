1911bc5691550ca409f5755d3b10b9aac60e9c3f
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-09-07 15:58:06 +0300

    third-party: Porting bpfjit library

---
+++ third-party/lib/bpfjit/embox_compat.h
@@ -0,0 +1,17 @@
+/**
+ * @file
+ *
+ * @date Sep 7, 2021
+ * @author Anton Bondarev
+ */
+
+#ifndef THIRD_PARTY_LIB_BPFJIT_EMBOX_COMPAT_H_
+#define THIRD_PARTY_LIB_BPFJIT_EMBOX_COMPAT_H_
+
+
+#define __linux     1
+
+#include <linux/types.h>
+#include <string.h>
+
+#endif /* THIRD_PARTY_LIB_BPFJIT_EMBOX_COMPAT_H_ */
