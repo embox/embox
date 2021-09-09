5c9061db23a32b1cd6d0cde3e8333831b467619f
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-09-09 12:27:35 +0300

    compat: Extract IN_CLASSx_NET mocros to netinet/in.h

--- src/compat/posix/net/inet_addr.c
+++ src/compat/posix/net/inet_addr.c
@@ -276,21 +276,6 @@ int inet_pton(int af, const char *buff, void *addr) {
 	return ret == 0 ? 1 : 0;
 }
 
-#define	IN_CLASSA(a)		((((in_addr_t)(a)) & 0x80000000) == 0)
-#define	IN_CLASSA_NET		0xff000000
-#define	IN_CLASSA_NSHIFT	24
-#define	IN_CLASSA_HOST		(0xffffffff & ~IN_CLASSA_NET)
-#define	IN_CLASSA_MAX		128
-#define	IN_CLASSB(a)		((((in_addr_t)(a)) & 0xc0000000) == 0x80000000)
-#define	IN_CLASSB_NET		0xffff0000
-#define	IN_CLASSB_NSHIFT	16
-#define	IN_CLASSB_HOST		(0xffffffff & ~IN_CLASSB_NET)
-#define	IN_CLASSB_MAX		65536
-#define	IN_CLASSC(a)		((((in_addr_t)(a)) & 0xe0000000) == 0xc0000000)
-#define	IN_CLASSC_NET		0xffffff00
-#define	IN_CLASSC_NSHIFT	8
-#define	IN_CLASSC_HOST		(0xffffffff & ~IN_CLASSC_NET)
-
 struct in_addr inet_makeaddr(in_addr_t net, in_addr_t host) {
 	struct in_addr in;
 
