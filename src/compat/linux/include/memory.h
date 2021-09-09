8d0543aa109ced1c3ed75f5946372359afcb9957
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-09-09 16:05:52 +0300

    compat: Add memory.h

---
+++ src/compat/linux/include/memory.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ *
+ * @date Sep 9, 2021
+ * @author Anton Bondarev
+ */
+
+#ifndef SRC_COMPAT_LINUX_INCLUDE_MEMORY_H_
+#define SRC_COMPAT_LINUX_INCLUDE_MEMORY_H_
+
+#include <string.h>
+
+#endif /* SRC_COMPAT_LINUX_INCLUDE_MEMORY_H_ */
