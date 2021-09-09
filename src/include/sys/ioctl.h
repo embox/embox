0d1de03f43254bdee07b2814185c75b5349bd83a
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-09-09 13:29:20 +0300

    compat: Add some macros definitions to sys/ioctl.h

--- src/include/sys/ioctl.h
+++ src/include/sys/ioctl.h
@@ -59,6 +59,45 @@
 #define FIONBIO   _IOW('a', 0, const int)
 #define FIONREAD  _IO ('a', 1)
 
+/* socket i/o controls */
+#define SIOCSHIWAT _IOW('s',  0, int)       /* set high watermark */
+#define SIOCGHIWAT _IOR('s',  1, int)       /* get high watermark */
+#define SIOCSLOWAT _IOW('s',  2, int)       /* set low watermark */
+#define SIOCGLOWAT _IOR('s',  3, int)       /* get low watermark */
+#define SIOCATMARK _IOR('s',  7, int)       /* at oob mark? */
+#define SIOCSPGRP  _IOW('s',  8, int)       /* set process group */
+#define SIOCGPGRP  _IOR('s',  9, int)       /* get process group */
+
+#define SIOCADDRT  _IOW('r', 10, struct ortentry) /* add route */
+#define SIOCDELRT  _IOW('r', 11, struct ortentry) /* delete route */
+
+#define SIOCSIFADDR     _IOW('i', 12, struct ifreq) /* set ifnet address */
+#define OSIOCGIFADDR    _IOWR('i',13, struct ifreq) /* get ifnet address */
+#define SIOCGIFADDR     _IOWR('i',33, struct ifreq) /* get ifnet address */
+#define SIOCSIFDSTADDR  _IOW('i', 14, struct ifreq) /* set p-p address */
+#define OSIOCGIFDSTADDR _IOWR('i',15, struct ifreq) /* get p-p address */
+#define SIOCGIFDSTADDR  _IOWR('i',34, struct ifreq) /* get p-p address */
+#define SIOCSIFFLAGS    _IOW('i', 16, struct ifreq) /* set ifnet flags */
+#define SIOCGIFFLAGS    _IOWR('i',17, struct ifreq) /* get ifnet flags */
+#define OSIOCGIFBRDADDR _IOWR('i',18, struct ifreq) /* get broadcast addr */
+#define SIOCGIFBRDADDR  _IOWR('i',35, struct ifreq) /* get broadcast addr */
+#define SIOCSIFBRDADDR  _IOW('i',19, struct ifreq)  /* set broadcast addr */
+#define OSIOCGIFCONF    _IOWR('i',20, struct ifconf) /* get ifnet list */
+#define SIOCGIFCONF     _IOWR('i',36, struct ifconf) /* get ifnet list */
+#define OSIOCGIFNETMASK _IOWR('i',21, struct ifreq)  /* get net addr mask */
+#define SIOCGIFNETMASK  _IOWR('i',37, struct ifreq)  /* get net addr mask */
+#define SIOCSIFNETMASK  _IOW('i',22, struct ifreq)   /* set net addr mask */
+#define SIOCGIFMETRIC   _IOWR('i',23, struct ifreq)  /* get IF metric */
+#define SIOCSIFMETRIC   _IOW('i',24, struct ifreq)   /* set IF metric */
+#define SIOCDIFADDR     _IOW('i',25, struct ifreq)   /* delete IF addr */
+#define SIOCAIFADDR     _IOW('i',26, struct ifaliasreq) /* add/chg IF alias */
+
+#define SIOCSARP        _IOW('i', 30, struct arpreq)  /* set arp entry */
+#define OSIOCGARP       _IOWR('i',31, struct arpreq)  /* get arp entry */
+#define	SIOCGARP        _IOWR('i',38, struct arpreq)  /* get arp entry */
+#define	SIOCDARP        _IOW('i', 32, struct arpreq)  /* delete arp entry */
+
+
 __BEGIN_DECLS
 
 extern int ioctl(int d, int request, ...);
