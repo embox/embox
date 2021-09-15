3cff3b77404580c2ad7a5a1144471c3c82133419
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-09-15 19:20:53 +0300

    third-party: Clean quake3 port

---
+++ third-party/games/quake3/embox_compat.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ *
+ * @date Sep 15, 2021
+ * @author Anton Bondarev
+ */
+
+#ifndef THIRD_PARTY_GAMES_QUAKE3_EMBOX_COMPAT_H_
+#define THIRD_PARTY_GAMES_QUAKE3_EMBOX_COMPAT_H_
+
+
+#include <alloca.h>
+
+
+#endif /* THIRD_PARTY_GAMES_QUAKE3_EMBOX_COMPAT_H_ */
