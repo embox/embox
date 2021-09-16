b96df4d4a4152de0b3582cf48e254affb7c923a0
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-09-16 12:07:26 +0300

    compat: Improve qsort() implementation

--- src/compat/libc/stdlib/qsort.c
+++ src/compat/libc/stdlib/qsort.c
@@ -8,68 +8,140 @@
  * @author Avdyukhin Dmitry
  */
 
+#include <sys/types.h>
 #include <stdlib.h>
-#include <string.h>
-#include <alloca.h>
 
-/**
- * Swap two elements of specified size.
+static inline char	*med3 (char *, char *, char *, int (*)());
+static inline void	 swapfunc (char *, char *, int, int);
+
+#define min(a, b)	(a) < (b) ? a : b
+
+/*
+ * Qsort routine from Bentley & McIlroy's "Engineering a Sort Function".
  */
-static inline void swap(void *fst, void *snd, size_t size) {
-	void *temp = alloca(size);
-
-	memcpy(temp, snd, size);
-	memcpy(snd, fst, size);
-	memcpy(fst, temp, size);
+#define swapcode(TYPE, parmi, parmj, n) { 		\
+	long i = (n) / sizeof (TYPE); 			\
+	register TYPE *pi = (TYPE *) (parmi); 		\
+	register TYPE *pj = (TYPE *) (parmj); 		\
+	do { 						\
+		register TYPE	t = *pi;		\
+		*pi++ = *pj;				\
+		*pj++ = t;				\
+        } while (--i > 0);				\
 }
 
-void qsort(void *vbase, size_t nmemb, size_t size,
-		int(*compar)(const void *, const void *)) {
-	char *base = (char *) vbase;
+#define SWAPINIT(a, es) swaptype = ((char *)a - (char *)0) % sizeof(long) || \
+	es % sizeof(long) ? 2 : es == sizeof(long)? 0 : 1;
 
-	if (nmemb < 4) {
-		if (nmemb == 2) {
-			if (compar(base + size, base) < 0) {
-				swap(base, base + size, size);
-			}
-		} else if (nmemb == 3) {
-			if (compar(base + size, base) < 0) {
-				swap(base, base + size, size);
-			}
-			if (compar(base + (size << 1), base + size) < 0) {
-				swap(base + size, base + (size << 1), size);
-				if (compar(base + size, base) < 0) {
-					swap(base, base + size, size);
-				}
-			}
-		}
+static inline void
+swapfunc(a, b, n, swaptype)
+	char *a, *b;
+	int n, swaptype;
+{
+	if(swaptype <= 1)
+		swapcode(long, a, b, n)
+	else
+		swapcode(char, a, b, n)
+}
+
+#define swap(a, b)					\
+	if (swaptype == 0) {				\
+		long t = *(long *)(a);			\
+		*(long *)(a) = *(long *)(b);		\
+		*(long *)(b) = t;			\
+	} else						\
+		swapfunc(a, b, es, swaptype)
+
+#define vecswap(a, b, n) 	if ((n) > 0) swapfunc(a, b, n, swaptype)
+
+static inline char *
+med3(a, b, c, cmp)
+	char *a, *b, *c;
+	int (*cmp)();
+{
+	return cmp(a, b) < 0 ?
+	       (cmp(b, c) < 0 ? b : (cmp(a, c) < 0 ? c : a ))
+              :(cmp(b, c) > 0 ? b : (cmp(a, c) < 0 ? a : c ));
+}
+
+void
+qsort(a, n, es, cmp)
+	void *a;
+	size_t n, es;
+	int (*cmp)();
+{
+	char *pa, *pb, *pc, *pd, *pl, *pm, *pn;
+	int d, r, swaptype, swap_cnt;
+
+loop:	SWAPINIT(a, es);
+	swap_cnt = 0;
+	if (n < 7) {
+		for (pm = a + es; pm < (char *) a + n * es; pm += es)
+			for (pl = pm; pl > (char *) a && cmp(pl - es, pl) > 0;
+			     pl -= es)
+				swap(pl, pl - es);
 		return;
-	} else {
-		char *pos = (rand() % nmemb) * size + base;
-		char *key = alloca(size);
-		char *i = base, *j = base + (size * (nmemb - 1));
-
-		memcpy(key, pos, size);
-
-		while (i <= j) {
-			while (compar(i, key) < 0) {
-				i += size;
-			}
-			while (compar(key, j) < 0) {
-				j -= size;
-			}
-			if (i <= j) {
-				swap(i, j, size);
-				i += size;
-				j -= size;
-			}
-		}
-		if (j > base) {
-			qsort(base, (j - base) / size + 1, size, compar);
-		}
-		if (i < base + (nmemb - 1) * size) {
-			qsort(i, nmemb - (i - base) / size, size, compar);
-		}
-
 	}
+	pm = a + (n / 2) * es;
+	if (n > 7) {
+		pl = a;
+		pn = a + (n - 1) * es;
+		if (n > 40) {
+			d = (n / 8) * es;
+			pl = med3(pl, pl + d, pl + 2 * d, cmp);
+			pm = med3(pm - d, pm, pm + d, cmp);
+			pn = med3(pn - 2 * d, pn - d, pn, cmp);
+		}
+		pm = med3(pl, pm, pn, cmp);
+	}
+	swap(a, pm);
+	pa = pb = a + es;
+
+	pc = pd = a + (n - 1) * es;
+	for (;;) {
+		while (pb <= pc && (r = cmp(pb, a)) <= 0) {
+			if (r == 0) {
+				swap_cnt = 1;
+				swap(pa, pb);
+				pa += es;
+			}
+			pb += es;
+		}
+		while (pb <= pc && (r = cmp(pc, a)) >= 0) {
+			if (r == 0) {
+				swap_cnt = 1;
+				swap(pc, pd);
+				pd -= es;
+			}
+			pc -= es;
+		}
+		if (pb > pc)
+			break;
+		swap(pb, pc);
+		swap_cnt = 1;
+		pb += es;
+		pc -= es;
+	}
+	if (swap_cnt == 0) {  /* Switch to insertion sort */
+		for (pm = a + es; pm < (char *) a + n * es; pm += es)
+			for (pl = pm; pl > (char *) a && cmp(pl - es, pl) > 0;
+			     pl -= es)
+				swap(pl, pl - es);
+		return;
+	}
+
+	pn = a + n * es;
+	r = min(pa - (char *)a, pb - pa);
+	vecswap(a, pb - r, r);
+	r = min(pd - pc, pn - pd - es);
+	vecswap(pb, pn - r, r);
+	if ((r = pb - pa) > es)
+		qsort(a, r / es, es, cmp);
+	if ((r = pd - pc) > es) {
+		/* Iterate rather than recurse to save stack space */
+		a = pn - r;
+		n = r / es;
+		goto loop;
+	}
+/*		qsort(pn - r, r / es, es, cmp);*/
 }
