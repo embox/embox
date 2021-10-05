9e6568cf54cba103bd83a66d20cad323e88e9ece
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-10-05 18:31:21 +0300

    dvfs: Clean dirent_dvfs

--- src/compat/posix/fs/dirent/dirent_dvfs.c
+++ src/compat/posix/fs/dirent/dirent_dvfs.c
@@ -72,10 +72,6 @@ struct dirent *readdir(DIR *dir) {
 		return NULL;
 	}
 
-	if (NULL != dir->prv_dentry) {
-		dentry_ref_dec(dir->prv_dentry);
-	}
-
 	l = (struct lookup) {
 		.parent = dir->dir_dentry,
 	};
@@ -90,8 +86,6 @@ struct dirent *readdir(DIR *dir) {
 	}
 
 	fill_dirent(&dir->dirent, l.item);
-
-	dir->prv_dentry = l.item;
 
 	return &dir->dirent;
 }
