1cb6a35c12a8e9d4da188582de5b997a098d5a8f
Anton Bondarev <anton.bondarev2310@gmail.com>
2022-02-10 19:31:15 +0300

    kernel: Start working on irq/msi

---
+++ src/kernel/irq/msi/irq_msi.c
@@ -0,0 +1,29 @@
+/**
+ * @file
+ *
+ * @date Feb 9, 2022
+ * @author Anton Bondarev
+ */
+
+#include <mem/sysmalloc.h>
+
+#include <kernel/irq/irq_msi.h>
+
+struct msi_desc *msi_entry_alloc(void *dev, int nvec) {
+	struct msi_desc *desc;
+
+	desc = sysmalloc(sizeof(*desc));
+	if (!desc) {
+		return NULL;
+	}
+
+	desc->dev = dev;
+	desc->nvec_used = nvec;
+
+	return desc;
+}
+
+void msi_entry_free(struct msi_desc *entry) {
+
+	sysfree(entry);
+}
