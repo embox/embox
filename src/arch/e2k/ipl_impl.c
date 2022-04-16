aeb7c9d4f2a6fff62406d7c5a2bc05021fc9d496
Anton Bondarev <anton.bondarev2310@gmail.com>
2022-04-16 13:22:46 +0300

    e2k: Improve CPU mode for float point work

--- src/arch/e2k/ipl_impl.c
+++ src/arch/e2k/ipl_impl.c
@@ -11,11 +11,32 @@
 
 void ipl_init(void) {
 	e2k_pfpfr_t pfpfr;
+	e2k_fpcr_t fpcr;
+
 	/* XXX E2k
 	 * we have comment in e2k_kernel_start()
 	 * XXX Disable FPU. Should be enabled later. */
 	e2k_upsr_write(e2k_upsr_read() | UPSR_FE);
 	e2k_upsr_write(e2k_upsr_read() | (UPSR_IE | UPSR_NMIE));
+
+	pfpfr.word = e2k_pfpfr_read();
+	pfpfr.fields.im = 1;
+	pfpfr.fields.dm = 1;
+	pfpfr.fields.zm = 1;
+	pfpfr.fields.om = 1;
+	pfpfr.fields.um = 1;
+	pfpfr.fields.pm = 1;
+	e2k_pfpfr_write(pfpfr.word);
+
+	fpcr.word = e2k_fpcr_read();
+	fpcr.fields.im = 1;
+	fpcr.fields.dm = 1;
+	fpcr.fields.zm = 1;
+	fpcr.fields.om = 1;
+	fpcr.fields.um = 1;
+	fpcr.fields.pm = 1;
+	fpcr.fields.pc = 3;
+	e2k_fpcr_write(fpcr.word);
 }
 
 unsigned int ipl_save(void) {
