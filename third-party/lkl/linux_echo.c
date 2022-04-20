358325edcd50509ba0571ca044e23e1350b05d24
Anton Ostrouhhov <aostrouhhov@azul.com>
2022-04-21 02:04:54 +0300

    Update comments in 'linux_echo.c'

--- third-party/lkl/linux_echo.c
+++ third-party/lkl/linux_echo.c
@@ -1,7 +1,10 @@
-/**
- *  Simple Hello World which uses i386 Linux syscall convention
+/*
+ *  Simple `echo` which uses i386 Linux syscall convention
  */
 
+/*  Can't use `read`, `write` etc. because they are just wrappers from GLIBC which is not ported yet.
+ *  `write` -> GLIBC implementation -> actual `sys_write` (INT 0x80)
+ */
 static inline long os_syscall(int syscall,
 							  unsigned long arg1, unsigned long arg2,
 							  unsigned long arg3, unsigned long arg4) {
@@ -24,21 +27,22 @@ int main(char* param_1, char* param_2) {
 
 	char* string = param_2;
 
-	/* We use special file '/vda' (it points to virtual block device) for STDOUT in LKL.
-	 * '/vda' handler redirects everything to Embox terminal.
+	/*  We use special file '/vda' (it points to virtual block device) for STDOUT in LKL.
+	 *  '/vda' handler redirects everything to Embox terminal.
 	 */
 
-	// 'lseek(1, 0, 0)' is required for '/vda'
-	//   because it is shared between LKL's kthreads (LKL has one process).
-	// This doesn't matter for Linux 'tty' device.
+	/*  'lseek(1, 0, 0)' is required for '/vda' because it is shared between LKL's kthreads.
+	 *  This doesn't matter for Linux 'tty' device.
+	 */
 	os_syscall(19, 1, 0, 0, 0);
 
 	// Write to STDOUT.
 	os_syscall(4, 1, (unsigned long int)string, len, 0);
 	os_syscall(4, 1, (unsigned long int)"\n", 1, 0);
 
-	// 'fsync(1)' is required to trigger '/vda' handler in LKL.
-	// This doesn't matter for Linux 'tty' device.
+	/*  'fsync(1)' is required to trigger '/vda' handler in LKL.
+	 *  This doesn't matter for Linux 'tty' device.
+	 */
 	os_syscall(118, 1, 0, 0, 0);
 
 	// Exit.
