fce063d5370e192c4b366f98080826b1962cdc75
Anton Ostrouhhov <aostrouhhov@azul.com>
2022-05-03 19:28:51 +0300

    exception_handler.c: make the IF logic for Linux syscall more transparent

--- src/arch/x86/kernel/exception_handler.c
+++ src/arch/x86/kernel/exception_handler.c
@@ -21,8 +21,8 @@ __trap_handler __exception_table[0x20];
 
 fastcall void exception_handler(pt_regs_t *st) {
 	if(NULL != __exception_table[st->trapno]) {
-		// Don't handle this if it is a Linux syscall, but the task isn't marked as an LKL task
-		if(!(st->trapno == 0xd && task_lkl_resources(task_self())->lkl_allowed == 0)) {
+		// Handle Linux syscall (0xd) only if the task is allowed to use LKL
+		if(st->trapno != 0xd || task_lkl_resources(task_self())->lkl_allowed == 1) {
 			__exception_table[st->trapno](st->trapno, st);
 			return;
 		}
