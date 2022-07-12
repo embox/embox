c6dcbbac6672f052e67a06d3d06bf57156630a1a
dlopatinsky <dmitrij.lopatinskij@gmail.com>
2022-07-12 19:48:37 +0300

    add '\0' at the end of dest string so that strcmp works

--- src/tests/posix/memccpy_test.c
+++ src/tests/posix/memccpy_test.c
@@ -64,10 +64,14 @@ TEST_CASE("empty static allocated string with character on the string") {
 
     char original[] = "To be copied";
     char tested[128];
+    char c = 'c';
 
     void* test;
+    test = memccpy(tested, original, c, strlen(original)+1);
 
-    test = memccpy(tested, original, 'c', strlen(original)+1);
+    char* cPos = strchr(original, c);
+    tested[cPos - original] = '\0';
+
     test_assert_zero(strcmp(tested, "To be "));
     test_assert_not_null(test);
 }
