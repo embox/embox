42d807d7377008f851f8515206a4a3c5a9eb1367
Anton Bondarev <anton.bondarev2310@gmail.com>
2022-08-20 16:56:27 +0300

    fault_detection_vehicle: Clean motor's driver

--- project/fault_detection_vehicle/libactuators/motor.c
+++ project/fault_detection_vehicle/libactuators/motor.c
@@ -5,48 +5,31 @@
  * @author Anton Bondarev
  */
 #include <libactuators/motor.h>
-#include "stm32f3xx_hal.h"
-#include <stm32f3xx_hal_gpio.h>
-#include <stm32f3_discovery.h>
 
+#include <drivers/gpio/gpio.h>
 
 static void stm32f3_delay(uint32_t delay) {
 	while(delay--)
 		;
 }
 
-
-static void init_pins(GPIO_TypeDef  *GPIOx, uint16_t pins) {
-	GPIO_InitTypeDef  GPIO_InitStruct;
-
-	//TODO Is this required?
-	__HAL_RCC_GPIOD_CLK_ENABLE();
-
-	GPIO_InitStruct.Pin = pins;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-
-	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
-}
-
 void motor_init(struct motor *m) {
 
-	m->port = GPIOD;
+	m->port = GPIO_PORT_D;
 	m->enable = MOTOR_ENABLE1;
 	m->input[0] = MOTOR_INPUT1;
 	m->input[1] = MOTOR_INPUT2;
 
-	init_pins(m->port, m->enable | m->input[0] | m->input[1]);
+	gpio_setup_mode(m->port, m->enable | m->input[0] | m->input[1], GPIO_MODE_OUTPUT);
 }
 
 void motor_enable(struct motor *m) {
-	HAL_GPIO_WritePin(m->port, m->enable, GPIO_PIN_SET);
+	gpio_set(m->port, m->enable, GPIO_PIN_HIGH);
 }
 
 
 void motor_disable(struct motor *m) {
-	HAL_GPIO_WritePin(m->port, m->enable, GPIO_PIN_RESET);
+	gpio_set(m->port, m->enable, GPIO_PIN_LOW);
 }
 
 
@@ -55,17 +38,18 @@ void motor_run(struct motor *m, enum motor_run_direction dir) {
 
 	input_pin = (dir == MOTOR_RUN_FORWARD) ? 1 : 0;
 
-	HAL_GPIO_WritePin(m->port, m->input[!input_pin], GPIO_PIN_RESET);
+	gpio_set(m->port, m->input[!input_pin], GPIO_PIN_LOW);
 	stm32f3_delay(0xFF); /* FIXME may be it is not needed */
-	HAL_GPIO_WritePin(m->port, m->input[input_pin], GPIO_PIN_SET);
+	gpio_set(m->port, m->input[input_pin], GPIO_PIN_HIGH);
 }
 
 void motor_stop(struct motor *m) {
 	stm32f3_delay(1000);
-	HAL_GPIO_WritePin(m->port, m->enable, GPIO_PIN_RESET);
+
+	motor_disable(m);
 	stm32f3_delay(1000);
-	HAL_GPIO_WritePin(m->port, m->input[0], GPIO_PIN_RESET);
+	gpio_set(m->port, m->input[0], GPIO_PIN_LOW);
 	stm32f3_delay(1000);
-	HAL_GPIO_WritePin(m->port, m->input[1], GPIO_PIN_RESET);
+	gpio_set(m->port, m->input[1], GPIO_PIN_LOW);
 	stm32f3_delay(1000);
 }
--- project/fault_detection_vehicle/libactuators/motor.h
+++ project/fault_detection_vehicle/libactuators/motor.h
@@ -11,16 +11,24 @@
 #include <stdint.h>
 
 /* motor 1*/
-#define MOTOR_ENABLE1  GPIO_PIN_3
-#define MOTOR_INPUT1   GPIO_PIN_5
-#define MOTOR_INPUT2   GPIO_PIN_7
+//#define MOTOR_ENABLE1  GPIO_PIN_3
+//#define MOTOR_INPUT1   GPIO_PIN_5
+//#define MOTOR_INPUT2   GPIO_PIN_7
+#define MOTOR_ENABLE1_PIN  3
+#define MOTOR_INPUT1_PIN   5
+#define MOTOR_INPUT2_PIN   7
+
+#define MOTOR_ENABLE1  (1 << MOTOR_ENABLE1_PIN)
+#define MOTOR_INPUT1   (1 << MOTOR_INPUT1_PIN)
+#define MOTOR_INPUT2   (1 << MOTOR_INPUT2_PIN)
+
 /* motor 2 */
 #define MOTOR_ENABLE2  GPIO_PIN_2
 #define MOTOR_INPUT3   GPIO_PIN_4
 #define MOTOR_INPUT4   GPIO_PIN_6
 
 struct motor {
-	void *port;
+	uint16_t port;
 	uint16_t enable;
 	uint16_t input[2];
 };
