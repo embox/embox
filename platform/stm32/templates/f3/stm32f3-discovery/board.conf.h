af0b5cf70b1f96c329ef66c654d69847d8029c02
Anton Bondarev <anton.bondarev2310@gmail.com>
2022-09-13 17:21:48 +0300

    templates: Add LEDs enable to platform/stm32/f3/stm32f3-discovery/

--- platform/stm32/templates/f3/stm32f3-discovery/board.conf.h
+++ platform/stm32/templates/f3/stm32f3-discovery/board.conf.h
@@ -2,4 +2,13 @@
 
 CONFIG {
 	uarts[1].status = ENABLED;
+
+	leds[0].status = ENABLED;
+	leds[1].status = ENABLED;
+	leds[2].status = ENABLED;
+	leds[3].status = ENABLED;
+	leds[4].status = ENABLED;
+	leds[5].status = ENABLED;
+	leds[6].status = ENABLED;
+	leds[7].status = ENABLED;
 }
