9673258d5f0b9ecd98734e53eedfb495bb339826
Anton Bondarev <anton.bondarev2310@gmail.com>
2022-09-15 16:00:55 +0300

    fault_detection_vehicle: Update led.h

--- project/fault_detection_vehicle/libmisc/led.h
+++ project/fault_detection_vehicle/libmisc/led.h
@@ -9,6 +9,9 @@
 
 #include <stdint.h>
 
+#include <drivers/gpio/gpio.h>
+#include <config/board_config.h>
+
 struct led {
 	uint16_t port;
 	uint16_t pin;
@@ -18,31 +21,4 @@ extern void led_init(struct led *led, int port, int pin);
 extern void led_on(struct led *led);
 extern void led_off(struct led *led);
 
-#include <drivers/gpio/gpio.h>
-
-#define STM_LED6_PIN                         15
-#define STM_LED6_GPIO_PORT                   GPIO_PORT_E
-
-#define STM_LED8_PIN                         14
-#define STM_LED8_GPIO_PORT                   GPIO_PORT_E
-
-#define STM_LED10_PIN                        13
-#define STM_LED10_GPIO_PORT                  GPIO_PORT_E
-
-#define STM_LED9_PIN                         12
-#define STM_LED9_GPIO_PORT                   GPIO_PORT_E
-
-#define STM_LED7_PIN                         11
-#define STM_LED7_GPIO_PORT                   GPIO_PORT_E
-
-#define STM_LED5_PIN                         10
-#define STM_LED5_GPIO_PORT                   GPIO_PORT_E
-
-#define STM_LED3_PIN                         9
-#define STM_LED3_GPIO_PORT                   GPIO_PORT_E
-
-#define STM_LED4_PIN                         8
-#define STM_LED4_GPIO_PORT                   GPIO_PORT_E
-
 #endif /* PLATFORM_STM32F3_SENSORS_LIBMISC_LED_H_ */
-
