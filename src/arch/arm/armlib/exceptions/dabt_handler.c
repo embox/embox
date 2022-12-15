dec6903bb599892bc535b0bb443c2804837e7c88
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2022-12-15 21:42:12 +0300

    arm: Fix testtrap framework

--- src/arch/arm/armlib/exceptions/dabt_handler.c
+++ src/arch/arm/armlib/exceptions/dabt_handler.c
@@ -9,18 +9,26 @@
 #include <inttypes.h>
 
 #include <kernel/printk.h>
+#include <hal/test/traps_core.h>
 
 #include "exceptions.h"
 
 extern uint32_t _get_mmu_data_fault_status(void);
 
-void _NORETURN arm_dabt_handler(excpt_context_t *ctx) {
+fault_handler_t __fault_table[0x10];
+
+void arm_dabt_handler(excpt_context_t *ctx) {
 	uint32_t fault_status;
 
-	fault_status = _get_mmu_data_fault_status();
-	printk("\nUnresolvable data abort exception!\n");
-	printk("Fault status = %" PRIu32 "\n", fault_status);
-	PRINT_PTREGS(&ctx->ptregs);
-	while (1)
-		;
+	fault_status = _get_mmu_data_fault_status() & 0xf;
+	if (__fault_table[fault_status]) {
+		__fault_table[fault_status](fault_status, &ctx->ptregs);
+	}
+	else {
+		printk("\nUnresolvable data abort exception!\n");
+		printk("Fault status = %" PRIu32 "\n", fault_status);
+		PRINT_PTREGS(&ctx->ptregs);
+		while (1)
+			;
+	}
 }
