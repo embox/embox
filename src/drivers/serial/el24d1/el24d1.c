af30c391cba1e81147598359f2e48fb28c8fc829
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-02-02 14:38:02 +0300

    drivers: Update serial/el24d1 driver

--- src/drivers/serial/el24d1/el24d1.c
+++ src/drivers/serial/el24d1/el24d1.c
@@ -13,11 +13,15 @@
 #include <embox/unit.h>
 #include <framework/mod/options.h>
 
+
 EMBOX_UNIT_INIT(el24d1_uart_init);
 
-#define UART_BASE OPTION_GET(NUMBER, base_addr)
-#define UART(x)   (*(volatile uint32_t *)(UART_BASE + (x)))
-#define IRQ_NUM OPTION_GET(NUMBER, irq_num)
+#define UART_BASE      OPTION_GET(NUMBER, base_addr)
+#define IRQ_NUM        OPTION_GET(NUMBER, irq_num)
+
+#define PINS_INIT      OPTION_GET(NUMBER, pins_init)
+
+#define UART(x)        (*(volatile uint32_t *)(UART_BASE + (x)))
 
 #define RBR  0x00
 #define THR  0x00
@@ -44,14 +48,57 @@ EMBOX_UNIT_INIT(el24d1_uart_init);
 #define STET 0xA0
 #define HTX  0XA4
 
-#define USR_BUSY (1 << 0)
-#define USR_TFNF (1 << 1)
-#define LSR_RDR  (1 << 0)
-#define LSR_THRE (1 << 5)
+#define USR_BUSY  (1 << 0)
+#define USR_TFNF  (1 << 1)
+#define LSR_RDR   (1 << 0)
+#define LSR_THRE  (1 << 5)
+#define LCR_DLAB  (1 << 7)
+
+static inline void  el24d1_uart_setup_baud_rate(struct uart *dev, int baudrate_bps) {
+	uint32_t src_clock_hz = 48000000;
+    uint32_t divisor = (uint32_t) (src_clock_hz / (baudrate_bps * 16));
+    uint32_t tmp_lcr;
+
+
+    tmp_lcr = UART(LCR);
+    UART(LCR) = tmp_lcr | LCR_DLAB;
+
+    UART(DLH) =  (divisor >> 8) & 0xFF;
+
+    UART(DLL)= divisor & 0xFF;
+
+    UART(LCR) = tmp_lcr;
+}
+
+#if PINS_INIT == 0
 
 static int el24d1_uart_setup(struct uart *dev, const struct uart_params *params) {
 	return 0;
 }
+
+#else
+
+#include <drivers/gpio/gpio_driver.h>
+static int el24d1_uart_setup(struct uart *dev, const struct uart_params *params) {
+#define GPIO_ALT_FUNC_UART   (4)
+
+	gpio_setup_mode(3, 1 << 0, GPIO_MODE_OUT_ALTERNATE | GPIO_ALTERNATE(GPIO_ALT_FUNC_UART));
+	gpio_setup_mode(3, 1 << 1, GPIO_MODE_OUT_ALTERNATE | GPIO_ALTERNATE(GPIO_ALT_FUNC_UART));
+
+	UART(SRR) = 1; /* soft reset */
+	UART(FCR) = 0; /* without fifo */
+	UART(LCR) = 3; /* 8 bit */
+
+	el24d1_uart_setup_baud_rate(dev, 115200);
+
+	/*enable rx interrupt*/
+	if (params->uart_param_flags & UART_PARAM_FLAGS_USE_IRQ) {
+		/*enable rx interrupt*/
+	}
+
+	return 0;
+}
+#endif
 
 static int el24d1_uart_has_symbol(struct uart *dev) {
 	return UART(LSR) & LSR_RDR;
