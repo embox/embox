16d25a9249da2bb9f991376c44b0c6834b190592
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-02-21 11:41:10 +0300

    arm: Update arm-m exceptions

--- src/arch/arm/armmlib/exception.h
+++ src/arch/arm/armmlib/exception.h
@@ -14,13 +14,16 @@
 #include <module/embox/kernel/irq_static_impl.h>
 #endif
 
-#define PENDSV_IRQ       14
-#define SYSTICK_IRQ      15
+#define DEBUG_MONITOR 12
+#define PENDSV_IRQ    14
+#define SYSTICK_IRQ   15
+
+#define SAVED_CORE_REGS_SIZE (8 * 4)
+#define SAVED_FPU_REGS_SIZE  (16 * 4 + 4 + 4)
 
 #define STATIC_EXC_ATTACH(_exc_nr, _hnd, _data) \
 	STATIC_IRQ_HANDLER_DEF(_exc_nr, EXC_HANDLER_NAME(_exc_nr), _hnd, _data)
 
-#define EXC_HANDLER_NAME(_exc_nr) \
-	MACRO_CONCAT(exc_handler_, _exc_nr)
+#define EXC_HANDLER_NAME(_exc_nr) MACRO_CONCAT(exc_handler_, _exc_nr)
 
 #endif /* ARCH_ARM_ARMMLIB_EXCEPTION_H_ */
