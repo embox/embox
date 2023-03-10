25dc8a376cb34990dfff9dc9c01476d6aa1b6606
Andrew Bursian <bursian@mail.ru>
2023-03-10 19:01:43 +0300

    enable ethernet

--- src/drivers/net/stm32cube/stm32f7cube_eth_msp.c
+++ src/drivers/net/stm32cube/stm32f7cube_eth_msp.c
@@ -18,47 +18,54 @@
  * @param  heth: ETH handle
  * @retval None
  */
-void HAL_ETH_MspInit(ETH_HandleTypeDef *heth) {
-	GPIO_InitTypeDef GPIO_InitStructure;
+void HAL_ETH_MspInit(ETH_HandleTypeDef *heth)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+  
+  /* Enable GPIOs clocks */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
 
-	/* Enable GPIOs clocks */
-	__HAL_RCC_GPIOA_CLK_ENABLE()
-	;
-	__HAL_RCC_GPIOC_CLK_ENABLE()
-	;
-	__HAL_RCC_GPIOG_CLK_ENABLE();
+/* Ethernet pins configuration ************************************************/
+  /*
+        RMII_REF_CLK ----------------------> PA1
+        RMII_MDIO -------------------------> PA2
+        RMII_MDC --------------------------> PC1
+        RMII_MII_CRS_DV -------------------> PA7
+        RMII_MII_RXD0 ---------------------> PC4
+        RMII_MII_RXD1 ---------------------> PC5
+        RMII_MII_RXER ---------------------> PG2
+        RMII_MII_TX_EN --------------------> PG11
+        RMII_MII_TXD0 ---------------------> PG13
+        RMII_MII_TXD1 ---------------------> PB13
+  */
 
-	/* Ethernet pins configuration *****************************************/
-	/*
-	 RMII_REF_CLK ----------------------> PA1
-	 RMII_MDIO -------------------------> PA2
-	 RMII_MDC --------------------------> PC1
-	 RMII_MII_CRS_DV -------------------> PA7
-	 RMII_MII_RXD0 ---------------------> PC4
-	 RMII_MII_RXD1 ---------------------> PC5
-	 RMII_MII_RXER ---------------------> PG2
-	 RMII_MII_TX_EN --------------------> PG11
-	 RMII_MII_TXD0 ---------------------> PG13
-	 RMII_MII_TXD1 ---------------------> PG14
-	 */
+  /* Configure PA1, PA2 and PA7 */
+  GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
+  GPIO_InitStructure.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStructure.Pull = GPIO_NOPULL; 
+  GPIO_InitStructure.Alternate = GPIO_AF11_ETH;
+  GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_7;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  /* Configure PB13 */
+  GPIO_InitStructure.Pin = GPIO_PIN_13;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
+  
+  /* Configure PC1, PC4 and PC5 */
+  GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-	/* Configure PA1, PA2 and PA7 */
-	GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
-	GPIO_InitStructure.Mode = GPIO_MODE_AF_PP;
-	GPIO_InitStructure.Pull = GPIO_NOPULL;
-	GPIO_InitStructure.Alternate = GPIO_AF11_ETH;
-	GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_7;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	/* Configure PC1, PC4 and PC5 */
-	GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5;
-	HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-	/* Configure PG2, PG11, PG13 and PG14 */
-	GPIO_InitStructure.Pin =
-			GPIO_PIN_2 | GPIO_PIN_11 | GPIO_PIN_13 | GPIO_PIN_14;
-	HAL_GPIO_Init(GPIOG, &GPIO_InitStructure);
-
-	/* Enable ETHERNET clock  */
-	__HAL_RCC_ETH_CLK_ENABLE();
+  /* Configure PG2, PG11, PG13 and PG14 */
+  GPIO_InitStructure.Pin =  GPIO_PIN_2 | GPIO_PIN_11 | GPIO_PIN_13;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStructure);
+  
+ 
+  /* Enable ETHERNET clock  */
+  __HAL_RCC_ETH_CLK_ENABLE();
+  
+    /* Output HSE clock (25MHz) on MCO pin (PA8) to clock the PHY */
+    HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_PLLCLK, RCC_MCODIV_4);
 }
