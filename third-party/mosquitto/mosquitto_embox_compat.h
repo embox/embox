9588edac29ab7956321ac01bd853fe473578dcc0
Yuriy Paramonov <yp@softregen.com>
2023-03-12 17:11:53 -0400

    add-third-party-mosquitto-folder
    1. Added subfolder "include" with
    1.1. ifaddrs.h - contains:
    1.1.1. Declarations for function: getifaddrs and freeifaddrs
    1.1.2. Declaration for struct ifaddrs
    1.2. syslog.h - contains: LOG_* #defines and inline functions: closelog, openlog, syslog.
    2. Added patch file "pkg_patch.patch" which contains patches for:
    2.1. mosquitto-2.0.15/plugins/auth-by-ip/Makefile:
    2.1.1. Updated the .PHONY statement: from .PHONY : all binary check clean reallyclean test install uninstall to .PHONY : all binary.
    2.1.2. binary target was updated: from binary : ${PLUGIN_NAME}.so to binary : ${PLUGIN_NAME}.o.
    2.1.3. Removed all statements related to dynamic linkage.
    2.2. mosquitto-2.0.15/plugins/deny-protocol-version/Makefile:
    2.2.1. binary target was updated: from binary : ${PLUGIN_NAME}.so to binary : ${PLUGIN_NAME}.o.
    2.2.2. All statements related to dynamic linkage were removed.
    2.3. mosquitto-2.0.15/plugins/message-timestamp/Makefile:
    2.3.1. Updated the .PHONY statement: from .PHONY : all binary check clean reallyclean test install uninstall to .PHONY : all binary.
    2.3.2. Updated binary target: from binary : ${PLUGIN_NAME}.so to binary : ${PLUGIN_NAME}.o.
    2.3.3. Removed all statements related to dynamic linkage.
    2.4. mosquitto-2.0.15/plugins/payload-modification/Makefile:
    2.4.1. Updated the .PHONY statement: from .PHONY : all binary check clean reallyclean test install uninstall to .PHONY : all binary.
    2.4.2. Updated binary target: from binary : ${PLUGIN_NAME}.so to binary : ${PLUGIN_NAME}.o.
    2.4.3. Removed all statements related to dynamic linkage.
    2.5. mosquitto-2.0.15/src/conf.c: Removed #includes of: syslog.h, mosquitto_broker_internal.h, memory_mosq.h, misc_mosq.h.
    2.6. mosquitto-2.0.15/src/config.mk - selected correct for Embox settings of WITH_*:
    2.6.1. Commented out WITH_TLS:=yes statement
    2.6.2. Set WITH_TLS_PSK:=no
    2.6.3. Set WITH_THREADING:=no
    2.6.4. Do not build man page documentation with WITH_DOCS:=no.
    2.6.5. Build static library with WITH_STATIC_LIBRARIES:=yes.
    2.6.6. Do not build shared libraries with WITH_SHARED_LIBRARIES:=no.
    2.6.7. No epoll support with WITH_EPOLL:=no.
    2.6.8. No cJson support with WITH_CJSON:=no.
    2.6.9. Removed dynamic linkage - related flags (-lm, -ldl and -lrt) from BROKER_LDADD:=$(BROKER_LDADD) statements.
    2.7. mosquitto-2.0.15/src/conf_includedir.c - substituted #  include <sys/syslog.h> with #  include <syslog.h>.
    2.8. mosquitto-2.0.15/mosquitto.conf - set socket_domain to ipv4.
    3. Added MyBuild with @Cmd mosquitto and module mosquitto.
    4. Added mosquitto_embox_compat.h file with static inline functions: initgroups, setsid, getppid, fork.
    5. Added Makefile.

---
+++ third-party/mosquitto/mosquitto_embox_compat.h
@@ -0,0 +1,65 @@
+/*
+ * mosquitto_embox_compat.h
+ *
+ *  Created on: 2023-03-01
+ *      Author: Yuriy Paramonov
+ */
+
+#ifndef MOSQUITTO_EMBOX_COMPAT_H_
+#define MOSQUITTO_EMBOX_COMPAT_H_
+
+
+#ifdef linux
+#undef linux
+#endif
+
+#ifdef __linux__
+#undef __linux__
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#if 1
+#define DPRINT() printf(">>> mosq CALL %s\n", __FUNCTION__)
+#else
+#define DPRINT()
+#endif
+
+#include <ctype.h>
+
+
+#include <errno.h>
+
+__BEGIN_DECLS
+
+#include <pthread.h>
+
+static inline int initgroups(const char *user, gid_t group) {
+	(void) user;
+	(void) group;
+	return 0;
+}
+
+static inline pid_t setsid(void) {
+	DPRINT();
+	return -1;
+}
+
+static inline
+pid_t getppid(void) {
+	DPRINT();
+	return 0;
+}
+
+
+static inline pid_t fork() {
+	printf(">>> fork()\n");
+	errno = ENOSYS;
+	return -1;
+}
+
+__END_DECLS
+
+#endif /* MOSQUITTO_EMBOX_COMPAT_H_ */
