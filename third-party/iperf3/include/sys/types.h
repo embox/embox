979525ccee63cb8a11273ccae3edc113854914d9
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-05-11 11:40:26 +0300

    third-party: Add iperf3

---
+++ third-party/iperf3/include/sys/types.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 7.05.23
+ * @author Aleksey Zhmulin
+ */
+#ifndef IPERF3_SYS_TYPES_H_
+#define IPERF3_SYS_TYPES_H_
+
+#include <linux/types.h>
+#include_next <sys/types.h>
+
+#endif /* IPERF3_SYS_TYPES_H_ */
