11f391f6679f635198c72f065607448d6793971b
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-05-12 15:46:51 +0300

    third-party: Update iperf

---
+++ third-party/iperf/include/netinet/tcp.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 11.05.23
+ * @author Aleksey Zhmulin
+ */
+#ifndef IPERF_NETINET_TCP_H_
+#define IPERF_NETINET_TCP_H_
+
+#include_next <netinet/tcp.h>
+#undef TCP_NODELAY
+
+#endif /* IPERF_NETINET_TCP_H_ */
