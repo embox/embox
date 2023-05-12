249952578fa35a96ecb2f107dc5d855617af66a8
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-05-12 14:50:49 +0300

    drivers: Fix pci/msi/pci_msi E2K version

--- src/drivers/pci/msi/pci_msi.h
+++ src/drivers/pci/msi/pci_msi.h
@@ -14,6 +14,8 @@
 #include <drivers/pci/pci_regs.h>
 
 static inline void pci_msi_set_enable(struct pci_slot_dev *dev, int enable) {
+#if 0
+	/* x86 version */
 	uint16_t control;
 
 	pci_read_config_word(dev, dev->msi_cap + PCI_MSI_FLAGS, &control);
@@ -22,17 +24,38 @@ static inline void pci_msi_set_enable(struct pci_slot_dev *dev, int enable) {
 		control |= PCI_MSI_FLAGS_ENABLE;
 	}
 	pci_write_config_word(dev, dev->msi_cap + PCI_MSI_FLAGS, control);
+#else
+	/* e2k version */
+	uint32_t control;
+
+	pci_read_config_dword(dev, dev->msi_cap, &control);
+	control &= ~(PCI_MSI_FLAGS_ENABLE << 16);
+	if (enable) {
+		control |= (PCI_MSI_FLAGS_ENABLE << 16);
+	}
+	pci_write_config_dword(dev, dev->msi_cap, control);
+#endif
+
 }
 
 static inline void pci_msix_clear_and_set_ctrl(struct pci_slot_dev *dev,
 		uint16_t clear, uint16_t set) {
+#if 0 /* x86 version */
 	uint16_t ctrl;
 
 	pci_read_config_word(dev, dev->msix_cap + PCI_MSIX_FLAGS, &ctrl);
 	ctrl &= ~clear;
 	ctrl |= set;
 	pci_write_config_word(dev, dev->msix_cap + PCI_MSIX_FLAGS, ctrl);
+#else
+	/* e2k version */
+	uint32_t ctrl;
 
+	pci_read_config_dword(dev, dev->msix_cap, &ctrl);
+	ctrl &= ~clear;
+	ctrl |= ((uint32_t)set << 16);
+	pci_write_config_dword(dev, dev->msix_cap, ctrl);
 }
+#endif
 
 #endif /* SRC_DRIVERS_PCI_MSI_PCI_MSI_H_ */
