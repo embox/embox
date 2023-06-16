ae7c9594f3e73fa3f654087bbe5c27f8ca60c9ab
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-06-16 19:15:06 +0300

    drivers: Enable bits_per_word & clk_div in stm32cube

--- src/drivers/spi/stm32cube/stm32_spi.h
+++ src/drivers/spi/stm32cube/stm32_spi.h
@@ -28,6 +28,8 @@ struct stm32_spi {
 	SPI_HandleTypeDef handle;
 	GPIO_TypeDef *nss_port;
 	uint32_t nss_pin;
+	uint16_t clk_div;
+	uint16_t bits_per_word;
 };
 
 extern int stm32_spi_init(struct stm32_spi *dev, void *instance);
