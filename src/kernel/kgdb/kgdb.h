c8859b05d3f8b71df22eb9859635c9dbff3b12ed
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-06-28 17:56:09 +0300

    kernel: Add kgdb

---
+++ src/kernel/kgdb/kgdb.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author Aleksey Zhmulin
+ * @date 13.06.23
+ */
+#ifndef KERNEL_KGDB_H_
+#define KERNEL_KGDB_H_
+
+extern void kgdb_init(void *entry);
+
+#endif /* KERNEL_KGDB_H_ */
