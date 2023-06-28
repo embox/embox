3684214e986a60caca5f83a0dd04d27d4e2078f8
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-06-28 18:41:32 +0300

    drivers: Add uart_dev into __uart_device_registry in TTYS_DEF()

--- src/drivers/tty/serial/ttys.h
+++ src/drivers/tty/serial/ttys.h
@@ -8,6 +8,8 @@
 #ifndef TTYS_H_
 #define TTYS_H_
 
+#include <util/array.h>
+
 #include <kernel/task/resource/idesc.h>
 #include <drivers/tty.h>
 #include <drivers/char_dev.h>
@@ -21,7 +23,9 @@ struct tty_uart {
 
 extern struct idesc *uart_cdev_open(struct dev_module *cdev, void *priv);
 
-#define TTYS_DEF(name, uart) \
-		CHAR_DEV_DEF(name, uart_cdev_open, NULL, NULL, uart)
+#define TTYS_DEF(name, uart_dev) \
+		ARRAY_SPREAD_DECLARE(struct uart *const, __uart_device_registry); \
+		ARRAY_SPREAD_ADD(__uart_device_registry, uart_dev); \
+		CHAR_DEV_DEF(name, uart_cdev_open, NULL, NULL, uart_dev)
 
 #endif /* TTYS_H_ */
