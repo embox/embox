81b2231b8b599ddb71e0d03202be188327e6f635
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-06-28 17:55:18 +0300

    arm: Improve gdbstub api

---
+++ src/arch/arm/armmlib/gdbstub/gdbstub.h
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author Aleksey Zhmulin
+ * @date 13.05.23
+ */
+#ifndef ARMMLIB_GDBSTUB_H_
+#define ARMMLIB_GDBSTUB_H_
+
+#include <stdint.h>
+
+#define PACKET_SIZE 0x80
+
+#define FEATURE_STR "l<target><architecture>arm</architecture></target>"
+
+#endif /* ARMMLIB_GDBSTUB_H_ */
