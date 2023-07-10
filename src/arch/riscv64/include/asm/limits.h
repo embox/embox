becbc39bdd556a4e24b1fe24b98a655df6ac0075
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-07-10 16:48:57 +0300

    riscv64: Make riscv64 a symbolic link to riscv

--- src/arch/riscv64/include/asm/limits.h
+++
@@ -1,6 +0,0 @@
-#ifndef RISCV64_LIMITS_H_
-#define RISCV64_LIMITS_H_
-
-#include <asm-generic/limits32.h>
-
-#endif /* RISCV64_LIMITS_H_ */
--- src/arch/riscv64/include/asm/types.h
+++
@@ -1,6 +0,0 @@
-#ifndef RISCV64_TYPES_H_
-#define RISCV64_TYPES_H_
-
-#include <asm-generic/types64.h>
-
-#endif /* RISCV64_TYPES_H_ */
