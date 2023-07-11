5cad8a0a64508bfa76cf985bc62b6e10de753bdd
Andrey Kalmykov <wawuwo@ya.ru>
2023-07-11 22:17:19 +0300

    scripts: Fix gdb_run_stm32h7_qspi.sh according to ShellCheck guidelines
     - Prevents path variables from globbing and word splitting
     - Groups multiple individual 'echo and redirect' lines
       into a group command with single redirect

--- scripts/gdb_run_stm32h7_qspi.sh
+++ scripts/gdb_run_stm32h7_qspi.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 GDB=arm-none-eabi-gdb
-GDB_CMD_FILE=$(mktemp --tmpdir=$PWD embox_gdb_stm32h7_qspi.XXXX)
+GDB_CMD_FILE=$(mktemp --tmpdir="$PWD" embox_gdb_stm32h7_qspi.XXXX)
 EMBOX_ELF=build/base/bin/embox
-EMBOX_BIN=$(mktemp --tmpdir=$PWD embox_XXXX.bin)
-EMBOX_BIN0=$(mktemp --tmpdir=$PWD embox_bank0_XXXX.bin)
-EMBOX_BIN1=$(mktemp --tmpdir=$PWD embox_bank1_XXXX.bin)
-QSPI_BIN=$(mktemp --tmpdir=$PWD qspi_XXXX.bin)
+EMBOX_BIN=$(mktemp --tmpdir="$PWD" embox_XXXX.bin)
+EMBOX_BIN0=$(mktemp --tmpdir="$PWD" embox_bank0_XXXX.bin)
+EMBOX_BIN1=$(mktemp --tmpdir="$PWD" embox_bank1_XXXX.bin)
+QSPI_BIN=$(mktemp --tmpdir="$PWD" qspi_XXXX.bin)
 
 prepare_bin() {
 	EMBOX_SECTIONS="--only-section=.text \
@@ -24,45 +24,52 @@ prepare_bin() {
 			       --remove-section=.data \
 			       --remove-section=.bss"
 
-	arm-none-eabi-objcopy -O binary build/base/bin/embox $QSPI_BIN $QSPI_SECTIONS
-	arm-none-eabi-objcopy -O binary build/base/bin/embox $EMBOX_BIN $EMBOX_SECTIONS
-	dd if=$EMBOX_BIN of=$EMBOX_BIN0 bs=1 count=$((1024 * 1024))
-	dd if=$EMBOX_BIN of=$EMBOX_BIN1 bs=1 skip=$((1024 * 1024))	
+	arm-none-eabi-objcopy -O binary build/base/bin/embox "$QSPI_BIN" $QSPI_SECTIONS
+	arm-none-eabi-objcopy -O binary build/base/bin/embox "$EMBOX_BIN" $EMBOX_SECTIONS
+	dd if="$EMBOX_BIN" of="$EMBOX_BIN0" bs=1 count=$((1024 * 1024))
+	dd if="$EMBOX_BIN" of="$EMBOX_BIN1" bs=1 skip=$((1024 * 1024))
 }
 
 create_gdb_script() {
-	rm -f $GDB_CMD_FILE
+	rm -f "$GDB_CMD_FILE"
 
-	echo "target ext :3333" >> $GDB_CMD_FILE
-	echo "monitor reset init" >> $GDB_CMD_FILE
-	echo "monitor flash banks" >> $GDB_CMD_FILE
+	{
+	    echo "target ext :3333"
+	    echo "monitor reset init"
+	    echo "monitor flash banks"
 
-	# Write embox.bin
-	echo "monitor stm32h7x mass_erase 0" >> $GDB_CMD_FILE
-	echo "monitor flash write_bank 0 $EMBOX_BIN0" >> $GDB_CMD_FILE
+	    # Write embox.bin
+	    echo "monitor stm32h7x mass_erase 0"
+	    echo "monitor flash write_bank 0 $EMBOX_BIN0"
 
-	echo "monitor stm32h7x mass_erase 1" >> $GDB_CMD_FILE
-	echo "monitor flash write_bank 1 $EMBOX_BIN1" >> $GDB_CMD_FILE
+	    echo "monitor stm32h7x mass_erase 1"
+	    echo "monitor flash write_bank 1 $EMBOX_BIN1"
+
+	} >> "$GDB_CMD_FILE"
 
 	# Write qspi.bin
-	sect_size=$(stat -c%s $QSPI_BIN)
+	sect_size=$(stat -c%s "$QSPI_BIN")
 	sectors=$((($sect_size + 65535) / 65536)) # Sector size is 64K for stm32h7 QSPI
-	echo "monitor flash erase_sector 2 0 $sectors" >> $GDB_CMD_FILE
-	echo "monitor flash write_bank 2 $QSPI_BIN" >> $GDB_CMD_FILE
-	## Check qspi.bin is written correctly
-	echo "monitor flash verify_bank 2 $QSPI_BIN" >> $GDB_CMD_FILE
 
-	echo "monitor reset halt" >> $GDB_CMD_FILE
-	echo "continue" >> $GDB_CMD_FILE
+	{
+	    echo "monitor flash erase_sector 2 0 $sectors"
+	    echo "monitor flash write_bank 2 $QSPI_BIN"
+	    ## Check qspi.bin is written correctly
+	    echo "monitor flash verify_bank 2 $QSPI_BIN"
+
+	    echo "monitor reset halt"
+	    echo "continue"
+
+	} >> "$GDB_CMD_FILE"
 
 	# Show generated GDB sctipt
-	echo "GDB script:" && cat $GDB_CMD_FILE && echo ""
+	echo "GDB script:" && cat "$GDB_CMD_FILE" && echo ""
 }
 
 prepare_bin
 create_gdb_script
 
-$GDB -x $GDB_CMD_FILE $EMBOX_ELF
+$GDB -x "$GDB_CMD_FILE" "$EMBOX_ELF"
 
-rm $GDB_CMD_FILE
-rm $EMBOX_BIN $QSPI_BIN $EMBOX_BIN0 $EMBOX_BIN1
+rm "$GDB_CMD_FILE"
+rm "$EMBOX_BIN" "$QSPI_BIN" "$EMBOX_BIN0" "$EMBOX_BIN1"
