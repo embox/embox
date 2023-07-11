3ca5c783f598fb78f762d3fcd9bf9826e1220ae7
Andrey Kalmykov <wawuwo@ya.ru>
2023-07-11 22:40:55 +0300

    scripts: Fix mandatory_img.sh according to ShellCheck guidelines
     - Quotes variables with values coming from the cmdline args
       and variables with path-values to avoid globbing and word splitting

--- scripts/mandatory_img.sh
+++ scripts/mandatory_img.sh
@@ -7,19 +7,19 @@ IMG=${2:-mandatory.img}
 
 LABEL=SMAC32LABEL
 
-dd if=/dev/zero bs=1 count=$SZ of=$IMG
-MKE2FS_DEVICE_SECTSIZE=512 mke2fs -q -b 1024 $IMG
+dd if=/dev/zero bs=1 count="$SZ" of="$IMG"
+MKE2FS_DEVICE_SECTSIZE=512 mke2fs -q -b 1024 "$IMG"
 
 tmp_mount=$(mktemp -d)
 
-mount -o loop $IMG $tmp_mount
+mount -o loop "$IMG" "$tmp_mount"
 
 for i in confidentially secret service unclassified; do
-	echo hello > $tmp_mount/$i
-	chmod 777 $tmp_mount/$i
-	attr -s $LABEL -V $i $tmp_mount/$i
+	echo hello > "$tmp_mount"/$i
+	chmod 777 "$tmp_mount"/$i
+	attr -s $LABEL -V $i "$tmp_mount"/$i
 
 done
 
-umount $tmp_mount
-rmdir $tmp_mount
+umount "$tmp_mount"
+rmdir "$tmp_mount"
