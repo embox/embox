81121f5c09f3fc21733fc6ba0a186789c44315df
Andrey Kalmykov <wawuwo@ya.ru>
2023-07-12 20:26:17 +0300

    scripts: Fix autotest/run_scripts/* according to ShellCheck guidelines
    Rewrites action sequences like 'cd to dir; action; cd back' with using
    subshells. Changing dir in a subshell don't change working dir of outer
    shell making script caller's environment protected.

--- scripts/autotest/run_scripts/run_qemu_usb.sh
+++ scripts/autotest/run_scripts/run_qemu_usb.sh
@@ -7,6 +7,8 @@ qemu-img create -f qcow2 $image_file_name 512M
 
 /sbin/mkfs.vfat $image_file_name
 
-cd $TEST_EMBOX_ROOT
-$QEMU -drive id=my_usb_disk,file=$image_file_name,if=none,format=raw -device usb-storage,drive=my_usb_disk $@
-cd -
+# Using subshell to avoid having to cd back
+(
+    cd "$TEST_EMBOX_ROOT" || { echo "Failed to cd to $TEST_EMBOX_ROOT"; exit 1; }
+    $QEMU -drive id=my_usb_disk,file=$image_file_name,if=none,format=raw -device usb-storage,drive=my_usb_disk "$@"
+) || exit 1
