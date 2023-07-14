ab8203b0f80d937afd25b5b6886310d58d239f07
Andrey Kalmykov <wawuwo@ya.ru>
2023-07-14 15:41:38 +0300

    scripts: Fix usermode_start.sh according to ShellCheck guidelines

--- scripts/usermode_start.sh
+++ scripts/usermode_start.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
 SUDO=sudo
-ROOT=$(dirname $0)/../.
+ROOT=$(dirname "$0")/../.
 
 EMBOX=${ROOT}/build/base/bin/embox
 
 [ -z "$TAP" ] && TAP=tap77
 
 rm_tap() {
-	$SUDO ip tuntap del dev $TAP mode tap
+	$SUDO ip tuntap del dev "$TAP" mode tap
 }
 
 trap rm_tap INT TERM
 
-if [ ! -z "$(ip addr | grep $TAP)" ]; then
+if ip addr | grep -q "$TAP"; then
 	rm_tap
 fi
 
-$SUDO ip tuntap add dev $TAP mode tap
-$SUDO $ROOT/scripts/qemu/start_script $TAP
-export EMBOX_USERMODE_TAP_NAME=$TAP
+$SUDO ip tuntap add dev "$TAP" mode tap
+$SUDO "$ROOT"/scripts/qemu/start_script "$TAP"
+export EMBOX_USERMODE_TAP_NAME="$TAP"
 
-if [ $USERMODE_START_OUTPUT ]; then
-	$EMBOX $TAP >$USERMODE_START_OUTPUT 2>$USERMODE_START_OUTPUT
+if [ "$USERMODE_START_OUTPUT" ]; then
+	$EMBOX "$TAP" >"$USERMODE_START_OUTPUT" 2>"$USERMODE_START_OUTPUT"
 else
-	$EMBOX $TAP
+	$EMBOX "$TAP"
 fi
 
 rm_tap
