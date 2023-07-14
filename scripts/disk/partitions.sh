9bf98e384493535d239cffd41982fd1ceb3d4699
Andrey Kalmykov <wawuwo@ya.ru>
2023-07-14 18:50:19 +0300

    scripts: Fix disk/partitions.sh according to ShellCheck guidelines

--- scripts/disk/partitions.sh
+++ scripts/disk/partitions.sh
@@ -26,27 +26,27 @@ if [[ ! $PARTS = 1 && ! $PARTS = 2 && ! $PARTS = 3 && ! $PARTS = 4 ]]; then
 	exit 1
 fi
 
-MKFS_PATH=`sudo which mkfs.$FS 2> /dev/null`
+MKFS_PATH=$(sudo which mkfs."$FS" 2> /dev/null)
 [ "$MKFS_PATH" == "" ] && echo "Command mkfs.$FS not found" && exit
 
-dd if=/dev/zero of=$IMG count=$SIZE bs=1M
-sudo parted $IMG -s mktable msdos || exit
+dd if=/dev/zero "of=$IMG" "count=$SIZE" bs=1M
+sudo parted "$IMG" -s mktable msdos || exit
 
-for i in $(seq 1 $PARTS)
+for i in $(seq 1 "$PARTS")
 do
-	sudo parted $IMG -s mkpart primary fat32 \
+	sudo parted "$IMG" -s mkpart primary fat32 \
 		$(( $i * $SIZE / ($PARTS + 1) )) $(( (1 + $i) * $SIZE / ($PARTS + 1) ))
 done
 
-sudo kpartx -d $IMG || exit
+sudo kpartx -d "$IMG" || exit
 
-LOOPDEV=$(sudo kpartx -av $IMG | head -n 1 | awk "{ print \$3"})
+LOOPDEV=$(sudo kpartx -av "$IMG" | head -n 1 | awk "{ print \$3 }")
 LOOPDEV=${LOOPDEV::-1}
 
-for i in $(seq 1 $PARTS)
+for i in $(seq 1 "$PARTS")
 do
-	sudo mkfs.$FS -n "PART$i" -I /dev/mapper/$LOOPDEV$i
-	sudo mount /dev/mapper/$LOOPDEV$i /mnt
+	sudo "mkfs.$FS" -n "PART$i" -I "/dev/mapper/$LOOPDEV$i"
+	sudo mount "/dev/mapper/$LOOPDEV$i" /mnt
 	sudo bash -c "echo Partition $i file >> /mnt/part$i\_file"
 	sudo umount /mnt
 	sync
