76110005709caadb65578f31905b064af25583f9
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-07-14 16:04:16 +0300

    drivers: clean: Add idesc_serial_ops in TTYS_DEF

--- src/drivers/tty/serial/ttys.h
+++ src/drivers/tty/serial/ttys.h
@@ -22,10 +22,11 @@ struct tty_uart {
 };
 
 extern struct idesc *uart_cdev_open(struct dev_module *cdev, void *priv);
+extern const struct idesc_ops idesc_serial_ops;
 
 #define TTYS_DEF(name, uart_dev) \
 		ARRAY_SPREAD_DECLARE(struct uart *const, __uart_device_registry); \
 		ARRAY_SPREAD_ADD(__uart_device_registry, uart_dev); \
-		CHAR_DEV_DEF(name, uart_cdev_open, NULL, uart_dev)
+		CHAR_DEV_DEF(name, uart_cdev_open, &idesc_serial_ops, uart_dev)
 
 #endif /* TTYS_H_ */
