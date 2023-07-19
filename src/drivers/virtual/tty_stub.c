49e53405cbed26cda1617e10dcfe955659698f9d
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-07-19 16:06:24 +0300

    minor: Modify drivers/virtual/tty_stub.c

--- src/drivers/virtual/tty_stub.c
+++ src/drivers/virtual/tty_stub.c
@@ -15,7 +15,7 @@
 #include <drivers/char_dev.h>
 #include <framework/mod/options.h>
 
-#define TTY_NAME OPTION_STRING_GET(tty_name)
+#define TTY_NAME     OPTION_MODULE_GET(__EMBUILD_MOD__,STRING,tty_name)
 
 static void tty_stub_close(struct idesc *desc) {
 }
@@ -55,4 +55,4 @@ static struct idesc *tty_stub_open(struct dev_module *cdev, void *priv) {
 	return char_dev_idesc_create(cdev);
 }
 
-CHAR_DEV_DEF(TTY_NAME, tty_stub_open, &tty_stub_ops, NULL);
+CHAR_DEV_DEF(MACRO_STRING(TTY_NAME), tty_stub_open, &tty_stub_ops, NULL);
