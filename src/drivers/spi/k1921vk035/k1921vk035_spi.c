7359f025aea604546c57094ae30d26ac97123bdc
Mikhail Liamaev <32442117+kenny5660@users.noreply.github.com>
2023-07-21 18:14:36 +1000

    fix spi cs

--- src/drivers/spi/k1921vk035/k1921vk035_spi.c
+++ src/drivers/spi/k1921vk035/k1921vk035_spi.c
@@ -66,16 +66,17 @@ static int k1921vk035_spi_init(struct spi_device *dev) {
 	int pin = 0;
 	int port = 0;
 
-	pin = k1921vk035_spi_dev->cs_map[k1921vk035_spi_dev->cs][K1921VK035_SPI_CS_MAP_PIN];
-	port = k1921vk035_spi_dev->cs_map[k1921vk035_spi_dev->cs][K1921VK035_SPI_CS_MAP_PORT];
+
 
 	//Init SPI_TX - PB7, SPI_RX - PB6 , SPI_SCK - PB5
 	gpio_setup_mode(GPIO_PORT_B, ( 1 << 5 ) | ( 1 << 6 ) | ( 1 << 7 ), GPIO_MODE_OUT_ALTERNATE | GPIO_ALTERNATE(0));
 
 	for (int i = 0; i < K1921VK035_SPI_MAX_CS; i++) {
+		pin = k1921vk035_spi_dev->cs_map[i][K1921VK035_SPI_CS_MAP_PIN];
+		port = k1921vk035_spi_dev->cs_map[i][K1921VK035_SPI_CS_MAP_PORT];
 		if (port != K1921VK035_SPI_CS_UNDEFINED && pin != K1921VK035_SPI_CS_UNDEFINED ) {
-		gpio_setup_mode(port, 1 << pin, GPIO_MODE_OUT_PUSH_PULL);
-		gpio_set(port, 1 << pin, GPIO_PIN_HIGH);
+			gpio_setup_mode(port, 1 << pin, GPIO_MODE_OUT_PUSH_PULL);
+			gpio_set(port, 1 << pin, GPIO_PIN_HIGH);
 		}
 	}
 
@@ -109,20 +110,22 @@ static int k1921vk035_spi_select(struct spi_device *dev, int cs) {
 	int pin = 0;
 	int port = 0;
 
-	pin = k1921vk035_spi_dev->cs_map[k1921vk035_spi_dev->cs][K1921VK035_SPI_CS_MAP_PIN];
-	port = k1921vk035_spi_dev->cs_map[k1921vk035_spi_dev->cs][K1921VK035_SPI_CS_MAP_PORT];
-
 	if (cs < 0 || cs > 4) {
 		log_error("Only cs=0..4 are available!");
 		return -EINVAL;
 	}
+
+	pin = k1921vk035_spi_dev->cs_map[cs][K1921VK035_SPI_CS_MAP_PIN];
+	port = k1921vk035_spi_dev->cs_map[cs][K1921VK035_SPI_CS_MAP_PORT];
+
 	if(pin == K1921VK035_SPI_CS_UNDEFINED && port == K1921VK035_SPI_CS_UNDEFINED){
 		log_error("cs=%d, not configured. port_cs=%d, pin_cs=%d", 
 				  cs, port, pin);
 		return -EINVAL;
 	}
+
 	k1921vk035_spi_dev->cs = cs;
-	k1921vk035_spi_config(dev);
+	//k1921vk035_spi_config(dev);
 	return res;
 }
 
