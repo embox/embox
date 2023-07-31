1252e9af56bbec7bdb179c654cf5eb46a78ef342
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-07-31 13:11:02 +0300

    drivers: Change CHAR_DEV_DEF() (devfs version)

--- src/drivers/char/char_dev.h
+++ src/drivers/char/char_dev.h
@@ -18,7 +18,7 @@
 
 #define MAX_CDEV_QUANTITY OPTION_MODULE_GET(embox__driver__char_dev, NUMBER, dev_quantity)
 
-#if 1
+#if 0
 #define CHAR_DEV_DEF(chname, open_fn, idesc_op, priv) \
 	ARRAY_SPREAD_DECLARE(const struct dev_module, __char_device_registry); \
 	ARRAY_SPREAD_ADD(__char_device_registry, { \
@@ -32,7 +32,18 @@
 
 #define CHAR_DEV_DEF(chname, open_fn, idesc_op, priv) \
 	ARRAY_SPREAD_DECLARE(const struct dev_module, __char_device_registry); \
-	const struct dev_module const __char_device_registry_##chname = { \
+	ARRAY_SPREAD_ADD_NAMED(__char_device_registry, \
+			__char_device_registry_##chname_##ptr, \
+			{ \
+			.dev_id = DEVID_CDEV,\
+			.name = MACRO_STRING(chname), \
+			.dev_priv = priv, \
+			.dev_iops = idesc_op, \
+			.dev_open = open_fn, \
+			 } )
+
+
+	/* const struct dev_module const __char_device_registry_##chname = { \
 			.dev_id = DEVID_CDEV,\
 			.name = MACRO_STRING(chname), \
 			.dev_priv = priv, \
@@ -40,6 +51,7 @@
 			.dev_open = open_fn, \
 			 }; \
 	ARRAY_SPREAD_ADD(__char_device_registry, __char_device_registry_##chname)
+	*/
 #endif
 
 struct dev_module;
