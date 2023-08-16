1bd7536cfed01301aafff795b7acaa069d6d8567
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-08-16 18:30:12 +0300

    drivers: Clean i2c/adapters/stm32cube

--- src/drivers/i2c/adapters/stm32cube/i2c_conf_f4.h
+++ src/drivers/i2c/adapters/stm32cube/i2c_conf_f4.h
@@ -10,10 +10,13 @@
 
 #include "stm32f4xx_hal.h"
 
+#include <config/board_config.h>
+
 #include <assert.h>
 #include <framework/mod/options.h>
 #include <module/embox/driver/i2c/stm32_i2c_f4.h>
 
+#if 0
 /* I2C1 */
 #define I2C1_SCL_PIN  GPIO_PIN_6
 #define I2C1_SDA_PIN  GPIO_PIN_9
@@ -21,6 +24,7 @@
 #define I2C1_SDA_AF   GPIO_AF4_I2C1
 #define I2C1_SCL_PORT GPIO_PORT_B
 #define I2C1_SDA_PORT GPIO_PORT_B
+#endif
 
 #define I2C1_EVENT_IRQ  \
 	OPTION_MODULE_GET(embox__driver__i2c__stm32_i2c_f4, NUMBER, i2c1_ev_irq)
@@ -30,13 +34,7 @@ static_assert(I2C1_EVENT_IRQ == I2C1_EV_IRQn, "");
 	OPTION_MODULE_GET(embox__driver__i2c__stm32_i2c_f4, NUMBER, i2c1_er_irq)
 static_assert(I2C1_ERROR_IRQ == I2C1_ER_IRQn, "");
 
-static inline void i2c1_enable_gpio_clocks(void) {
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-}
-static inline void i2c1_enable_i2c_clocks(void) {
-	__HAL_RCC_I2C1_CLK_ENABLE();
-}
-
+#if 0
 /* I2C2 */
 #define I2C2_SCL_PIN  GPIO_PIN_10
 #define I2C2_SDA_PIN  GPIO_PIN_11
@@ -44,6 +42,7 @@ static inline void i2c1_enable_i2c_clocks(void) {
 #define I2C2_SDA_AF   GPIO_AF4_I2C2
 #define I2C2_SCL_PORT GPIO_PORT_B
 #define I2C2_SDA_PORT GPIO_PORT_B
+#endif
 
 #define I2C2_EVENT_IRQ  \
 	OPTION_MODULE_GET(embox__driver__i2c__stm32_i2c_f4, NUMBER, i2c2_ev_irq)
@@ -53,11 +52,5 @@ static_assert(I2C2_EVENT_IRQ == I2C2_EV_IRQn, "");
 	OPTION_MODULE_GET(embox__driver__i2c__stm32_i2c_f4, NUMBER, i2c2_er_irq)
 static_assert(I2C2_ERROR_IRQ == I2C2_ER_IRQn, "");
 
-static inline void i2c2_enable_gpio_clocks(void) {
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-}
-static inline void i2c2_enable_i2c_clocks(void) {
-	__HAL_RCC_I2C2_CLK_ENABLE();
-}
 
 #endif /* SRC_DRIVERS_ADAPTERS_STM32_I2C_CONF_F4_H_ */
--- src/drivers/i2c/adapters/stm32cube/i2c_conf_l4.h
+++ src/drivers/i2c/adapters/stm32cube/i2c_conf_l4.h
@@ -10,6 +10,8 @@
 
 #include "stm32l4xx_hal.h"
 
+#include <config/board_config.h>
+
 /* FIXME from cube B-L475E-IOT01/stm32l475e_iot01.h */
 #ifndef DISCOVERY_I2Cx_TIMING
 #define DISCOVERY_I2Cx_TIMING                     ((uint32_t)0x00702681)
@@ -19,6 +21,7 @@
 #include <framework/mod/options.h>
 #include <module/embox/driver/i2c/stm32_i2c_l4.h>
 
+#if 0
 /* I2C1 */
 #define I2C1_SCL_PIN  GPIO_PIN_8
 #define I2C1_SDA_PIN  GPIO_PIN_9
@@ -26,6 +29,7 @@
 #define I2C1_SDA_AF   GPIO_AF4_I2C1
 #define I2C1_SCL_PORT GPIO_PORT_B
 #define I2C1_SDA_PORT GPIO_PORT_B
+#endif
 
 #define I2C1_EVENT_IRQ  \
 	OPTION_MODULE_GET(embox__driver__i2c__stm32_i2c_l4, NUMBER, i2c1_ev_irq)
@@ -35,13 +39,7 @@ static_assert(I2C1_EVENT_IRQ == I2C1_EV_IRQn, "");
 	OPTION_MODULE_GET(embox__driver__i2c__stm32_i2c_l4, NUMBER, i2c1_er_irq)
 static_assert(I2C1_ERROR_IRQ == I2C1_ER_IRQn, "");
 
-static inline void i2c1_enable_gpio_clocks(void) {
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-}
-static inline void i2c1_enable_i2c_clocks(void) {
-	__HAL_RCC_I2C1_CLK_ENABLE();
-}
-
+#if 0
 /* I2C2 */
 #define I2C2_SCL_PIN  GPIO_PIN_10
 #define I2C2_SDA_PIN  GPIO_PIN_11
@@ -49,6 +47,7 @@ static inline void i2c1_enable_i2c_clocks(void) {
 #define I2C2_SDA_AF   GPIO_AF4_I2C2
 #define I2C2_SCL_PORT GPIO_PORT_B
 #define I2C2_SDA_PORT GPIO_PORT_B
+#endif
 
 #define I2C2_EVENT_IRQ  \
 	OPTION_MODULE_GET(embox__driver__i2c__stm32_i2c_l4, NUMBER, i2c2_ev_irq)
@@ -57,12 +56,5 @@ static_assert(I2C2_EVENT_IRQ == I2C2_EV_IRQn, "");
 #define I2C2_ERROR_IRQ  \
 	OPTION_MODULE_GET(embox__driver__i2c__stm32_i2c_l4, NUMBER, i2c2_er_irq)
 static_assert(I2C2_ERROR_IRQ == I2C2_ER_IRQn, "");
-
-static inline void i2c2_enable_gpio_clocks(void) {
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-}
-static inline void i2c2_enable_i2c_clocks(void) {
-	__HAL_RCC_I2C2_CLK_ENABLE();
-}
 
 #endif /* SRC_DRIVERS_ADAPTERS_STM32_I2C_CONF_L4_H_ */
