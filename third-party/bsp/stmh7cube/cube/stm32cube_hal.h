879099ebf683d178bdb5ebb2c8c763cb25404b04
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-08-17 16:23:48 +0300

    third-party: Add stm32cube_hal.h for stmh7cube

---
+++ third-party/bsp/stmh7cube/cube/stm32cube_hal.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ *
+ * @date Aug 17, 2023
+ * @author Anton Bondarev
+ */
+
+#ifndef THIRD_PARTY_BSP_STMH7CUBE_CUBE_STM32CUBE_HAL_H_
+#define THIRD_PARTY_BSP_STMH7CUBE_CUBE_STM32CUBE_HAL_H_
+
+
+#include "stm32h7xx_hal.h"
+
+
+#endif /* THIRD_PARTY_BSP_STMH7CUBE_CUBE_STM32CUBE_HAL_H_ */
