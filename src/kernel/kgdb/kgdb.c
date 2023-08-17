38d45976e2a05a486ab848d7593a406b14205e16
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-08-17 15:54:24 +0300

    kernel: Improve kgdb

--- src/kernel/kgdb/kgdb.c
+++ src/kernel/kgdb/kgdb.c
@@ -6,6 +6,7 @@
  * @date 13.06.23
  */
 #include <stddef.h>
+#include <assert.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -13,9 +14,9 @@
 #include <kernel/printk.h>
 #include <debug/gdbstub.h>
 
-static struct gdb_arch gdb_arch;
+static int default_bpt_type;
 
-static void read_packet(char *dst, size_t nbyte) {
+static void kgdb_read(char *dst, size_t nbyte) {
 	int i;
 
 	do {
@@ -30,53 +31,83 @@ static void read_packet(char *dst, size_t nbyte) {
 	}
 }
 
-static void write_packet(const char *src, size_t nbyte) {
+static void kgdb_write(const char *src, size_t nbyte) {
 	while (nbyte--) {
 		diag_putc(*src++);
 	}
 }
 
-static void handle_breakpoint(struct gdb_regs *regs) {
+static void kgdb_bpt_handler(struct gdb_regs *regs) {
 	static bool connected = false;
 
 	struct gdbstub_env env;
+	struct gdb_packet pkt;
+	int cmd;
 
-	env.arch = &gdb_arch;
+	gdbstub_env_save(&env);
 	env.regs = regs;
+	gdbstub_env_restore(&env);
+
+	gdb_deactivate_all_bpts();
 
 	if (connected) {
-		env.packet.buf[1] = 'c';
-		gdb_process_packet(&env);
-		write_packet(env.packet.buf, env.packet.size);
+		gdb_process_cmd(GDB_CMD_CONT, &pkt);
+		kgdb_write(pkt.buf, pkt.size);
 	}
 	else {
-		gdb_arch.remove_all_bpts();
 		connected = true;
 	}
 
 	for (;;) {
-		read_packet(env.packet.buf, sizeof(env.packet.buf));
-		write_packet("+", 1);
+		kgdb_read(pkt.buf, sizeof(pkt.buf));
+		kgdb_write("+", 1);
 
-		gdb_process_packet(&env);
+		cmd = gdb_process_packet(&pkt);
 
-		if (env.cmd == GDBSTUB_DETACH) {
-			env.arch->remove_all_bpts();
+		if (cmd == GDB_CMD_CONT) {
 			break;
 		}
 
-		if (env.cmd == GDBSTUB_CONT) {
+		if (cmd == GDB_CMD_DETACH) {
+			connected = false;
+			gdb_remove_all_bpts();
 			break;
 		}
 
-		write_packet(env.packet.buf, env.packet.size);
+		kgdb_write(pkt.buf, pkt.size);
+	}
+
+	gdb_remove_bpt(default_bpt_type, GDB_REGS_PC(regs), 0);
+	gdb_activate_all_bpts();
+}
+
+static bool kgdb_break_required(void) {
+	while (diag_kbhit()) {
+		if (diag_getc() == GDB_INTERRUPT_CHAR) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void kgdb_break_pending(void *irq_ctx) {
+	if (kgdb_break_required()) {
+		assert(gdb_set_bpt(default_bpt_type, IRQ_CONTEXT_PC(irq_ctx), 0));
 	}
 }
 
-void kgdb_init(void *entry) {
+void kgdb_start(void *entry) {
 	printk("Remote debugging using diag\n");
 
-	gdb_arch_init(&gdb_arch);
-	gdb_arch_prepare(handle_breakpoint);
-	gdb_arch.insert_bpt(entry, 0);
+	if (gdb_enable_bpts(GDB_BPT_TYPE_HARD)) {
+		default_bpt_type = GDB_BPT_TYPE_HARD;
+	}
+	else {
+		assert(gdb_enable_bpts(GDB_BPT_TYPE_SOFT));
+		default_bpt_type = GDB_BPT_TYPE_SOFT;
+	}
+
+	gdb_set_handler(kgdb_bpt_handler);
+	assert(gdb_set_bpt(default_bpt_type, entry, 0));
 }
