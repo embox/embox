52af51d8beeb22ddc6be1d8349f804b0113a5ee7
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-09-09 12:11:14 +0300

    drivers: vic: Add const specifier

--- src/drivers/interrupt/vic/vic_util.c
+++ src/drivers/interrupt/vic/vic_util.c
@@ -21,11 +21,11 @@ static_assert(VIC_DEVICE_COUNT > 0, "VIC_DEVICE_COUNT must be "
 #define VIC1 OPTION_GET(NUMBER, vic1)
 
 #if VIC_DEVICE_COUNT == 1
-uintptr_t vic_devices[] = {VIC0};
+const uintptr_t vic_devices[] = {VIC0};
 
 PERIPH_MEMORY_DEFINE(vic0, VIC0, 0x1000);
 #elif VIC_DEVICE_COUNT == 2
-uintptr_t vic_devices[] = {VIC0, VIC1};
+const uintptr_t vic_devices[] = {VIC0, VIC1};
 
 PERIPH_MEMORY_DEFINE(vic0, VIC0, 0x1000);
 PERIPH_MEMORY_DEFINE(vic1, VIC1, 0x1000);
