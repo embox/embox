e310a1dd483a25c4d99efbcddda277d5db099490
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-09-16 17:55:11 +0300

    drivers: Clean stm32cube_usart/stm32_usart_conf_f4.h

--- src/drivers/serial/stm32cube_usart/stm32_usart_conf_f4.h
+++ src/drivers/serial/stm32cube_usart/stm32_usart_conf_f4.h
@@ -12,51 +12,7 @@
 /* TODO RM these. I hope it's enough to update CUbe version */
 #include <stm32f4xx_hal_usart.h>
 #include <stm32f4xx_hal_uart.h>
-#include <config/board_config.h>
 
-#include <framework/mod/options.h>
-
-#define MODOPS_USARTX OPTION_GET(NUMBER, usartx)
-
-
-#if MODOPS_USARTX == 6
-
-#define USARTx                           USART6
-
-/* Definition for USARTx's NVIC */
-#define USARTx_IRQn                      CONF_USART6_IRQ
-
-#elif MODOPS_USARTX == 1
-#define USARTx                           USART1
-
-/* Definition for USARTx's NVIC */
-#define USARTx_IRQn                      CONF_USART1_IRQ
-
-#elif MODOPS_USARTX == 2
-#define USARTx                           USART2
-
-/* Definition for USARTx's NVIC */
-#define USARTx_IRQn                      CONF_USART2_IRQ
-
-#elif MODOPS_USARTX == 3
-#define USARTx                           USART3
-
-/* Definition for USARTx's NVIC */
-#define USARTx_IRQn                      CONF_USART3_IRQ
-
-#elif MODOPS_USARTX == 8
-#if !defined(UART8)
-#error Unsupported UART8 for this target
-#endif
-
-#define USARTx                           UART8
-
-/* Definition for USARTx's NVIC */
-#define USARTx_IRQn                      CONF_UART8_IRQ
-
-#else
-#error Unsupported USARTx
-#endif
 
 #define STM32_USART_FLAGS(uart)   uart->SR
 #define STM32_USART_RXDATA(uart)  uart->DR
