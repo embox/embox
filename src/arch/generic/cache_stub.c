84126ba8cea466a0bd25ecffba44946562b01975
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-09-19 12:32:14 +0300

    arch: Add cache stub

---
+++ src/arch/generic/cache_stub.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author Aleksey Zhmulin
+ * @date 19.09.23
+ */
+#include <stddef.h>
+
+#include <hal/cache.h>
+
+void dcache_inval(const void *data, size_t size) {
+}
+
+void dcache_flush(const void *data, size_t size) {
+}
