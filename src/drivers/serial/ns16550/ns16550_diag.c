6a2afd48d684673a57439a52f5335de339548c14
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-10-10 16:49:21 +0300

    drivers: serial: Separate ns16550_ttyS0 and ns16550_diag

--- src/drivers/serial/ns16550/ns16550_diag.c
+++ src/drivers/serial/ns16550/ns16550_diag.c
@@ -4,33 +4,27 @@
  * @date May 19, 2023
  * @author Anton Bondarev
  */
-
-#include <drivers/serial/uart_dev.h>
-#include <drivers/serial/diag_serial.h>
-
-#include <drivers/diag.h>
-
 #include <drivers/common/memory.h>
-
+#include <drivers/diag.h>
+#include <drivers/serial/diag_serial.h>
+#include <drivers/serial/uart_dev.h>
 #include <framework/mod/options.h>
 
-#define UART_BASE      OPTION_GET(NUMBER, base_addr)
-#define IRQ_NUM        OPTION_GET(NUMBER, irq_num)
-#define BAUD_RATE      OPTION_GET(NUMBER, baud_rate)
+#define BASE_ADDR OPTION_GET(NUMBER, base_addr)
+#define BAUD_RATE OPTION_GET(NUMBER, baud_rate)
 
 extern const struct uart_ops ns16550_uart_ops;
 
 static struct uart ns16550_diag = {
-		.uart_ops = &ns16550_uart_ops,
-		.irq_num = IRQ_NUM,
-		.base_addr = UART_BASE,
+    .uart_ops = &ns16550_uart_ops,
+    .base_addr = BASE_ADDR,
 };
 
 static const struct uart_params ns16550_diag_params = {
-		.baud_rate = BAUD_RATE,
-		.uart_param_flags = UART_PARAM_FLAGS_8BIT_WORD,
+    .baud_rate = BAUD_RATE,
+    .uart_param_flags = UART_PARAM_FLAGS_8BIT_WORD,
 };
 
 DIAG_SERIAL_DEF(&ns16550_diag, &ns16550_diag_params);
 
-PERIPH_MEMORY_DEFINE(ns16550_diag, UART_BASE, 0x1000);
+PERIPH_MEMORY_DEFINE(ns16550_diag, BASE_ADDR, 0x1000);
--- src/drivers/serial/ns16550/ns16550_ttys1.c
+++ src/drivers/serial/ns16550/ns16550_ttys1.c
@@ -4,39 +4,35 @@
  * @date May 19, 2023
  * @author Anton Bondarev
  */
-
-#include <kernel/irq.h>
-
-#include <util/macro.h>
-
+#include <drivers/common/memory.h>
 #include <drivers/serial/uart_dev.h>
 #include <drivers/ttys.h>
-
-#include <drivers/common/memory.h>
-
 #include <framework/mod/options.h>
+#include <kernel/irq.h>
 
-#define UART_BASE      OPTION_GET(NUMBER, base_addr)
-#define IRQ_NUM        OPTION_GET(NUMBER, irq_num)
-#define BAUD_RATE      OPTION_GET(NUMBER,baud_rate)
+#define BASE_ADDR OPTION_GET(NUMBER, base_addr)
+#define IRQ_NUM   OPTION_GET(NUMBER, irq_num)
+#define BAUD_RATE OPTION_GET(NUMBER, baud_rate)
 
-#define TTY_NAME    ttyS1
+#define TTY_NAME ttyS1
 
 extern irq_return_t uart_irq_handler(unsigned int irq_nr, void *data);
 
 extern const struct uart_ops ns16550_uart_ops;
 
 static struct uart ns16550_ttyS1 = {
-		.uart_ops = &ns16550_uart_ops,
-		.irq_num = IRQ_NUM,
-		.base_addr = UART_BASE,
-		.params =  {
-				.baud_rate = BAUD_RATE,
-				.uart_param_flags = UART_PARAM_FLAGS_8BIT_WORD | UART_PARAM_FLAGS_USE_IRQ,
-		},
+    .uart_ops = &ns16550_uart_ops,
+    .irq_num = IRQ_NUM,
+    .base_addr = BASE_ADDR,
+    .params =
+        {
+            .baud_rate = BAUD_RATE,
+            .uart_param_flags = UART_PARAM_FLAGS_8BIT_WORD
+                                | UART_PARAM_FLAGS_USE_IRQ,
+        },
 };
 
-PERIPH_MEMORY_DEFINE(ns16550, UART_BASE, 0x1000);
+PERIPH_MEMORY_DEFINE(ns16550, BASE_ADDR, 0x1000);
 
 STATIC_IRQ_ATTACH(IRQ_NUM, uart_irq_handler, &ns16550_ttyS1);
 
