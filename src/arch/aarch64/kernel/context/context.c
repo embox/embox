1e60df63c20947eda364dd0a3b8a34b4bf06def6
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-10-18 17:16:30 +0300

    aarch64: Clean context

--- src/arch/aarch64/kernel/context/context.c
+++ src/arch/aarch64/kernel/context/context.c
@@ -5,7 +5,6 @@
  * @version
  * @date 19.07.2019
  */
-
 #include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -17,10 +16,11 @@
 #include <hal/context.h>
 
 void context_init(struct context *ctx, unsigned int flags,
-		void (*routine_fn)(void), void *sp) {
+    void (*routine_fn)(void), void *sp) {
 	memset(ctx, 0, sizeof(*ctx));
-	ctx->x[30] = (uint64_t) routine_fn;
-	ctx->sp = (uint64_t) sp;
+
+	ctx->lr = (uint64_t)routine_fn;
+	ctx->sp = (uint64_t)sp;
 
 	if (!(flags & CONTEXT_IRQDISABLE)) {
 		ctx->daif |= DAIF_I_BIT | DAIF_F_BIT;
