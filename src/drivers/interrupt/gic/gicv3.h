9935de643ba7a5ade864ecc79d919c0f644779da
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-10-19 14:18:12 +0300

    drivers: Improve GIC

---
+++ src/drivers/interrupt/gic/gicv3.h
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author Aleksey Zhmulin
+ * @date 20.10.23
+ */
+#ifndef DRIVERS_INTERRUPT_GIC_GICV3_H_
+#define DRIVERS_INTERRUPT_GIC_GICV3_H_
+
+#endif /* DRIVERS_INTERRUPT_GIC_GICV3_H_ */
