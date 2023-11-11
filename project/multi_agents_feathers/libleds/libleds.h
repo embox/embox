ed336f2581e8b400068ef45bf2c4f0c3193e15ac
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-11-11 18:15:25 +0300

    project/multi_agents_feathers: Add libleds

---
+++ project/multi_agents_feathers/libleds/libleds.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version 0.1
+ * @date 2016-04-04
+ */
+#ifndef _FEATHER_LIBLEDS_H_
+#define _FEATHER_LIBLEDS_H_
+
+extern void libleds_init(void);
+extern void libleds_on(int led_num);
+extern void libleds_off(int led_num);
+
+#endif /* _FEATHER_LIBLEDS_H_ */
