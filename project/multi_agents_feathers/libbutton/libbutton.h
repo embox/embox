c93847180cf5a9784172598dc7e9e3bc3cb6c1d3
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-11-13 15:37:22 +0300

    project/multi_agents_feathers: Add libbutton

---
+++ project/multi_agents_feathers/libbutton/libbutton.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version 0.1
+ * @date 2016-04-04
+ */
+#ifndef _FEATHER_LIBBUTTON_H_
+#define _FEATHER_LIBBUTTON_H_
+
+extern void libbutton_init(void);
+extern int libbutton_get_state(void);
+
+#endif /* _FEATHER_LIBBUTTON_H_ */
