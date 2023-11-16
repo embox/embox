df3d053bedc4f3b5201e8ca0b3a1579f81abad50
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-11-16 23:34:44 +0300

    framework: printk -> logs in unit.c

--- src/framework/unit.c
+++ src/framework/unit.c
@@ -11,7 +11,7 @@
 #include <embox/unit.h>
 #include <framework/mod/api.h>
 #include <framework/mod/ops.h>
-#include <kernel/printk.h>
+#include <util/log.h>
 
 static int unit_mod_enable(const struct mod *mod);
 static int unit_mod_disable(const struct mod *mod);
@@ -25,16 +25,13 @@ static int unit_mod_enable(const struct mod *mod) {
 	int ret = 0;
 	struct unit *unit = (struct unit *)mod;
 
-	if (NULL == unit->init) {
+	if (unit->init == NULL) {
 		return 0;
 	}
 
-	printk("\tunit: initializing %s.%s: ", mod_pkg_name(mod), mod_name(mod));
-	if (0 == (ret = unit->init())) {
-		printk("done\n");
-	}
-	else {
-		printk("error: %s\n", strerror(-ret));
+	log_info("unit: initializing %s.%s", mod_pkg_name(mod), mod_name(mod));
+	if ((ret = unit->init())) {
+		log_error("%s", strerror(-ret));
 	}
 
 	return ret;
@@ -44,16 +41,13 @@ static int unit_mod_disable(const struct mod *mod) {
 	int ret = 0;
 	struct unit *unit = (struct unit *)mod;
 
-	if (NULL == unit->fini) {
+	if (unit->fini == NULL) {
 		return 0;
 	}
 
-	printk("unit: finalizing %s.%s: ", mod_pkg_name(mod), mod_name(mod));
-	if (0 == (ret = unit->fini())) {
-		printk("done\n");
-	}
-	else {
-		printk("error: %s\n", strerror(-ret));
+	log_info("unit: finalizing %s.%s", mod_pkg_name(mod), mod_name(mod));
+	if ((ret = unit->fini())) {
+		log_error("%s", strerror(-ret));
 	}
 
 	return ret;
