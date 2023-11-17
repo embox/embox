4822e126f8582080c5bf751dad7d413bb2a93a85
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-11-17 20:03:06 +0300

    drivers: Clean pl011

--- src/drivers/serial/pl011/pl011.c
+++ src/drivers/serial/pl011/pl011.c
@@ -1,52 +1,44 @@
 /**
  * @file
- * @brief
- *    PrimeCell UART (PL011)
- *    http://infocenter.arm.com/help/topic/com.arm.doc.ddi0183g/DDI0183G_uart_pl011_r1p5_trm.pdf
+ * @brief PrimeCell UART (PL011)
+ * http://infocenter.arm.com/help/topic/com.arm.doc.ddi0183g/DDI0183G_uart_pl011_r1p5_trm.pdf
  *
- * @data 04 aug 2015
+ * @date 04 aug 2015
  * @author: Anton Bondarev
  */
 #include <stdint.h>
 
-#include <hal/reg.h>
-#include <drivers/common/memory.h>
-#include <drivers/diag.h>
 #include <drivers/serial/uart_dev.h>
-#include <drivers/serial/diag_serial.h>
-
 #include <framework/mod/options.h>
+#include <hal/reg.h>
 
-
-#define UART_BASE OPTION_GET(NUMBER,base_addr)
-#define IRQ_NUM   OPTION_GET(NUMBER,irq_num)
-#define UARTCLK   OPTION_GET(NUMBER,uartclk)
-#define BAUD_RATE OPTION_GET(NUMBER,baud_rate)
+#define UARTCLK         OPTION_GET(NUMBER, uartclk)
 
 /* UART Registers */
-#define UART_DR		(UART_BASE + 0x00)
-#define UART_RSR	(UART_BASE + 0x04)
-#define UART_ECR	(UART_BASE + 0x04)
-#define UART_FR		(UART_BASE + 0x18)
-#define UART_IBRD	(UART_BASE + 0x24)
-#define UART_FBRD	(UART_BASE + 0x28)
-#define UART_LCRH	(UART_BASE + 0x2c)
-# define UART_FEN         (1 << 4)
-# define UART_WLEN_8BIT   0x3
-# define UART_WLEN_SHIFT  5
-#define UART_CR		(UART_BASE + 0x30)
-# define UART_UARTEN      (1 << 0)
-# define UART_TXE         (1 << 8)
-# define UART_RXE         (1 << 9)
-#define UART_IMSC	(UART_BASE + 0x38)
-#define UART_MIS	(UART_BASE + 0x40)
-#define UART_ICR	(UART_BASE + 0x44)
+#define UART_DR(base)   (base + 0x00)
+#define UART_RSR(base)  (base + 0x04)
+#define UART_ECR(base)  (base + 0x04)
+#define UART_FR(base)   (base + 0x18)
+#define UART_IBRD(base) (base + 0x24)
+#define UART_FBRD(base) (base + 0x28)
+#define UART_LCRH(base) (base + 0x2c)
+#define UART_CR(base)   (base + 0x30)
+#define UART_IMSC(base) (base + 0x38)
+#define UART_MIS(base)  (base + 0x40)
+#define UART_ICR(base)  (base + 0x44)
+
+#define UART_FEN        (1 << 4)
+#define UART_UARTEN     (1 << 0)
+#define UART_TXE        (1 << 8)
+#define UART_RXE        (1 << 9)
+#define UART_WLEN_8BIT  0x3
+#define UART_WLEN_SHIFT 5
 
 /* Flag register */
-#define FR_RXFE		0x10	/* Receive FIFO empty */
-#define FR_TXFF		0x20	/* Transmit FIFO full */
+#define FR_RXFE         0x10 /* Receive FIFO empty */
+#define FR_TXFF         0x20 /* Transmit FIFO full */
 
-#define IMSC_RXIM   (0x1 << 4)
+#define IMSC_RXIM       (0x1 << 4)
 
 static void pl011_set_baudrate(struct uart *dev) {
 	/* FIXME Init baud rate only if UARTCLK is really used.
@@ -63,60 +55,62 @@ static void pl011_set_baudrate(struct uart *dev) {
 #endif
 }
 
-static int pl011_irq_enable(struct uart *dev, const struct uart_params *params) {
+static int pl011_irq_enable(struct uart *dev,
+    const struct uart_params *params) {
 	if (params->uart_param_flags & UART_PARAM_FLAGS_USE_IRQ) {
-		REG32_STORE(UART_IMSC, IMSC_RXIM);
+		REG32_STORE(UART_IMSC(dev->base_addr), IMSC_RXIM);
 	}
 	return 0;
 }
 
-static int pl011_irq_disable(struct uart *dev, const struct uart_params *params) {
-	REG32_STORE(UART_IMSC, 0);
+static int pl011_irq_disable(struct uart *dev,
+    const struct uart_params *params) {
+	REG32_STORE(UART_IMSC(dev->base_addr), 0);
 
 	return 0;
 }
 
 static int pl011_setup(struct uart *dev, const struct uart_params *params) {
 	/* Disable uart. */
-	REG32_STORE(UART_CR, 0);
+	REG32_STORE(UART_CR(dev->base_addr), 0);
 
 	if (params->uart_param_flags & UART_PARAM_FLAGS_USE_IRQ) {
-		REG32_STORE(UART_IMSC, IMSC_RXIM);
+		REG32_STORE(UART_IMSC(dev->base_addr), IMSC_RXIM);
 	}
 
 	pl011_set_baudrate(dev);
 
 	/* Word len 8 bit. */
-	REG32_STORE(UART_LCRH, UART_WLEN_8BIT << UART_WLEN_SHIFT);
+	REG32_STORE(UART_LCRH(dev->base_addr), UART_WLEN_8BIT << UART_WLEN_SHIFT);
 
 	/* Enable uart. */
-	REG32_STORE(UART_CR, UART_UARTEN | UART_TXE | UART_RXE);
+	REG32_STORE(UART_CR(dev->base_addr), UART_UARTEN | UART_TXE | UART_RXE);
 
 	return 0;
 }
 
 static int pl011_putc(struct uart *dev, int ch) {
-	while (REG32_LOAD(UART_FR) & FR_TXFF) ;
+	while (REG32_LOAD(UART_FR(dev->base_addr)) & FR_TXFF)
+		;
 
-	REG32_STORE(UART_DR, (uint32_t)ch);
+	REG32_STORE(UART_DR(dev->base_addr), (uint32_t)ch);
 
 	return 0;
 }
 
 static int pl011_getc(struct uart *dev) {
-	return REG32_LOAD(UART_DR);
+	return REG32_LOAD(UART_DR(dev->base_addr));
 }
 
 static int pl011_has_symbol(struct uart *dev) {
-	return !(REG32_LOAD(UART_FR) & FR_RXFE);
+	return !(REG32_LOAD(UART_FR(dev->base_addr)) & FR_RXFE);
 }
 
 const struct uart_ops pl011_uart_ops = {
-		.uart_getc = pl011_getc,
-		.uart_putc = pl011_putc,
-		.uart_hasrx = pl011_has_symbol,
-		.uart_setup = pl011_setup,
-		.uart_irq_en = pl011_irq_enable,
-		.uart_irq_dis = pl011_irq_disable,
+    .uart_getc = pl011_getc,
+    .uart_putc = pl011_putc,
+    .uart_hasrx = pl011_has_symbol,
+    .uart_setup = pl011_setup,
+    .uart_irq_en = pl011_irq_enable,
+    .uart_irq_dis = pl011_irq_disable,
 };
-
--- src/drivers/serial/pl011/pl011_diag.c
+++ src/drivers/serial/pl011/pl011_diag.c
@@ -1,33 +1,28 @@
 /**
  * @file
- * @data 04 aug 2015
+ * @date 04 aug 2015
  * @author: Anton Bondarev
  */
 
 #include <drivers/common/memory.h>
 #include <drivers/diag.h>
-#include <drivers/serial/uart_dev.h>
 #include <drivers/serial/diag_serial.h>
-
+#include <drivers/serial/uart_dev.h>
 #include <framework/mod/options.h>
 
-#define UART_BASE OPTION_GET(NUMBER,base_addr)
-#define IRQ_NUM   OPTION_GET(NUMBER,irq_num)
-#define UARTCLK   OPTION_GET(NUMBER,uartclk)
-#define BAUD_RATE OPTION_GET(NUMBER,baud_rate)
+#define UART_BASE OPTION_GET(NUMBER, base_addr)
+#define BAUD_RATE OPTION_GET(NUMBER, baud_rate)
 
 extern const struct uart_ops pl011_uart_ops;
 
 static struct uart uart0 = {
-		.uart_ops = &pl011_uart_ops,
-		.irq_num = IRQ_NUM,
-		.base_addr = UART_BASE,
+    .uart_ops = &pl011_uart_ops,
+    .base_addr = UART_BASE,
 };
 
-
 static const struct uart_params uart_diag_params = {
-		.baud_rate = BAUD_RATE,
-		.uart_param_flags = UART_PARAM_FLAGS_8BIT_WORD,
+    .baud_rate = BAUD_RATE,
+    .uart_param_flags = UART_PARAM_FLAGS_8BIT_WORD,
 };
 
 DIAG_SERIAL_DEF(&uart0, &uart_diag_params);
