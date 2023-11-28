91ace1e671b72366c3853d07e732ee905bc5d2e4
InfernalKn1ght <arzhaev.dmitry@yandex.ru>
2023-11-28 22:26:06 +0300

    more precise time measurement added in ktime_gettv_test.c

--- src/cmds/testing/ktime_get_timeval_test/ktime_gettv_test.c
+++ src/cmds/testing/ktime_get_timeval_test/ktime_gettv_test.c
@@ -9,22 +9,23 @@
 #define DEFAULT_N 5000
 
 int main(int argc, char** argv) {
-    struct timeval now;
-    int N_times = 0;
-    if (argc<2) {
-        printf("Usage: ktime_gettv_test <N_times>\n N_times positive integer\n");
-        return 0;
-    } else {
-        N_times = strtoul(argv[1], NULL, 10);
-        if(!N_times){
-            printf("Invalid argument, defaulting to N=%d\n", DEFAULT_N);
-            N_times = DEFAULT_N;
-        }
-    }
+    	int N_times = 0;
+	struct timespec now;
+	
+	if (argc<2) {
+		printf("Usage: ktime_gettv_test <N_times>\n N_times positive integer\n");
+		return 0;
+	} else {
+		N_times = strtoul(argv[1], NULL, 10);
+		if(!N_times){
+			printf("Invalid argument, defaulting to N=%d\n", DEFAULT_N);
+			N_times = DEFAULT_N;
+		}
+	}
 
-    for (int i=0; i<N_times; i++) {
-        ktime_get_timeval(&now);
-        printf("now.tv_sec=%ld\tnow.tv_usec=%ld\n", now.tv_sec, now.tv_usec);
-    }
-    return 0;
+	for (int i=0; i<N_times; i++) {
+		ktime_get_timespec(&now);
+		printf("now.tv_sec=%ld\tnow.tv_usec=%ld\tnow.tv_nsec=%ld\n", now.tv_sec, now.tv_nsec / 1000, now.tv_nsec);
+	}
+	return 0;
 }
