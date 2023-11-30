de4a148b060d33b0ce6533f7f0c7d20409194aa8
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-11-30 16:04:50 +0300

    drivers: Clean at91_aic

--- src/drivers/interrupt/at91_aic/at91_aic.c
+++ src/drivers/interrupt/at91_aic/at91_aic.c
@@ -9,33 +9,30 @@
 #include <assert.h>
 #include <stdint.h>
 
-#include <asm/psr.h>
 #include <asm/modes.h>
 #include <drivers/at91sam7s256.h>
-#include <kernel/irq.h>
-#include <hal/arch.h>
 #include <drivers/irqctrl.h>
-#include <hal/reg.h>
-#include <kernel/panic.h>
-
 #include <embox/unit.h>
+#include <hal/arch.h>
+#include <hal/ipl.h>
+#include <hal/reg.h>
+#include <kernel/irq.h>
+#include <kernel/panic.h>
 
 static void irq_def_handler(void) {
 	panic(__func__);
 }
 
 static int at91_intc_init(void) {
-
-	REG_STORE(AT91C_PMC_PCER, 1 << AT91C_ID_IRQ0 |
-			1 << AT91C_ID_IRQ1 | 1 << AT91C_ID_FIQ);
-	REG_STORE(AT91C_PIOA_PDR, AT91C_PA20_IRQ0 |
-			AT91C_PA30_IRQ1 | AT91C_PA19_FIQ);
-	REG_STORE(AT91C_PIOA_BSR, AT91C_PA20_IRQ0 |
-			AT91C_PA30_IRQ1 | AT91C_PA19_FIQ);
+	REG_STORE(AT91C_PMC_PCER,
+	    1 << AT91C_ID_IRQ0 | 1 << AT91C_ID_IRQ1 | 1 << AT91C_ID_FIQ);
+	REG_STORE(AT91C_PIOA_PDR,
+	    AT91C_PA20_IRQ0 | AT91C_PA30_IRQ1 | AT91C_PA19_FIQ);
+	REG_STORE(AT91C_PIOA_BSR,
+	    AT91C_PA20_IRQ0 | AT91C_PA30_IRQ1 | AT91C_PA19_FIQ);
 	for (int i = 0; i < 32; i++) {
-		REG_STORE(AT91C_AIC_SVR[i],(uint32_t) &irq_def_handler);
-		REG_STORE(AT91C_AIC_SMR + i,
-				AT91C_AIC_SRCTYPE_INT_EDGE_TRIGGERED);
+		REG_STORE(AT91C_AIC_SVR[i], (uint32_t)&irq_def_handler);
+		REG_STORE(AT91C_AIC_SMR + i, AT91C_AIC_SRCTYPE_INT_EDGE_TRIGGERED);
 	}
 
 	REG_STORE(AT91C_AIC_IDCR, ~0); /* disabling all interrupts */
@@ -46,7 +43,7 @@ static int at91_intc_init(void) {
 
 void irqctrl_enable(unsigned int interrupt_nr) {
 	REG_STORE(AT91C_AIC_SMR + interrupt_nr,
-			AT91C_AIC_SRCTYPE_INT_EDGE_TRIGGERED);
+	    AT91C_AIC_SRCTYPE_INT_EDGE_TRIGGERED);
 	REG_STORE(AT91C_AIC_SVR + interrupt_nr, 0);
 	REG_STORE(AT91C_AIC_IECR, 1 << interrupt_nr);
 }
@@ -64,11 +61,11 @@ void irqctrl_force(unsigned int interrupt_nr) {
 }
 
 static inline void disable_interrupts(void) {
-	__set_cpsr(__get_cpsr() | I_BIT | F_BIT);
+	ipl_disable();
 }
 
 static inline void enable_interrupts(void) {
-	__set_cpsr(__get_cpsr() & ~(I_BIT | F_BIT));
+	ipl_enable();
 }
 
 void interrupt_handle(void) {
