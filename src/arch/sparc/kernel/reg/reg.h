39780ad80688e29a366cc65b320d0861bbfd8d25
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-11-30 16:04:20 +0300

    sparc: Move registers access macros from include/asm/ to embox.arch.reg module

--- src/arch/sparc/kernel/reg/reg.h
+++ src/arch/sparc/kernel/reg/reg.h
@@ -1,6 +1,5 @@
-#ifndef HAL_REG_H_
-# error "Do not include this file directly!"
-#endif /* HAL_REG_H_ */
+#ifndef SPARC_KERNEL_REG_REG_H_
+#define SPARC_KERNEL_REG_REG_H_
 
 #ifdef CONFIG_SPARC_LEON
 /*
@@ -13,32 +12,29 @@
 
 /* do a physical address bypass write, i.e. for 0x80000000 */
 static __inline__ void __leon_store_reg(unsigned long paddr,
-		unsigned long value) {
-	__asm__ __volatile__(
-		"sta %0, [%1] %2\n\t"
-		:
-		: "r"(value), "r"(paddr), "i"(ASI_LEON_BYPASS)
-		: "memory"
-	);
+    unsigned long value) {
+	__asm__ __volatile__("sta %0, [%1] %2\n\t"
+	                     :
+	                     : "r"(value), "r"(paddr), "i"(ASI_LEON_BYPASS)
+	                     : "memory");
 }
 
 /* do a physical address bypass load, i.e. for 0x80000000 */
 static __inline__ unsigned long __leon_load_reg(unsigned long paddr) {
 	unsigned long retval;
-	__asm__ __volatile__(
-		"lda [%1] %2, %0\n\t"
-		: "=r"(retval)
-		: "r"(paddr), "i"(ASI_LEON_BYPASS)
-	);
+	__asm__ __volatile__("lda [%1] %2, %0\n\t"
+	                     : "=r"(retval)
+	                     : "r"(paddr), "i"(ASI_LEON_BYPASS));
 	return retval;
 }
 
 #define __REG_STORE(addr, value) \
-		__leon_store_reg((unsigned long) (addr), (unsigned long) (value))
+	__leon_store_reg((unsigned long)(addr), (unsigned long)(value))
 
-#define __REG_LOAD(addr) \
-		__leon_load_reg((unsigned long) (addr))
+#define __REG_LOAD(addr) __leon_load_reg((unsigned long)(addr))
 
 #else
 /* TODO not yet implemented. -- Eldar */
 #endif /* CONFIG_SPARC_LEON */
+
+#endif /* SPARC_KERNEL_REG_REG_H_ */
