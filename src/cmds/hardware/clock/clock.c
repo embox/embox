e9fba230ba8071faa1c6b48cde7dde2201393ce7
InfernalKn1ght <arzhaev.dmitry@yandex.ru>
2023-12-04 11:43:33 +0300

    clock.c fix

--- src/cmds/hardware/clock/clock.c
+++ src/cmds/hardware/clock/clock.c
@@ -7,7 +7,7 @@
  */
 
 #include <errno.h>
-#include <util/array.h>
+#include <util/dlist.h>
 
 #include <unistd.h>
 #include <stdio.h>
@@ -18,24 +18,25 @@ static void print_usage(void) {
 	printf("Usage: clock [-i] [-h]\n");
 }
 
-static int print_info(const struct clock_source *cs, int num) {
-	printf("%02d:\n", num);
+static int print_info(struct clock_source *cs, int num) {
+	printf("%02d: name: ", num);
 	cs->name ? printf("%s\n", cs->name) : printf("%s", "no name");
-	printf("flags: %04d\n", cs->flags);
-	if (cs->counter_device)
-		printf("counter_device frequency: %d\n", cs->counter_device->cycle_hz);
-	if (cs->event_device)
-		printf("event_device frequency: %d\n\n", cs->event_device->event_hz);
+	if (cs->counter_device) {
+		printf("counter_device frequency: %u\n", cs->counter_device->cycle_hz);
+	}
+	if (cs->event_device) {
+		printf("event_device frequency: %u\n", cs->event_device->event_hz);
+	}
+	printf("\n");
 	return 0;
 }
 
-extern const struct clock_source * __clock_sources[];
-
 static int clock_source_info(void) {
-	const struct clock_source *cs;
+	extern struct clock_source *kernel_clock_source;
+	struct clock_source *cs;
 	int k = 0;
 
-	array_spread_foreach(cs, __clock_sources) {
+	dlist_foreach_entry(cs, &kernel_clock_source->lnk, lnk) {
 		print_info(cs, ++k);
 	}
 
