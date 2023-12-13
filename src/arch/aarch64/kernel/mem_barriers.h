4afeafd43459c7ac3ee7e083cfdd1c05262254e8
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-12-13 13:45:36 +0300

    aarch64: Add mem barriers

--- src/arch/aarch64/kernel/mem_barriers.h
+++ src/arch/aarch64/kernel/mem_barriers.h
@@ -8,6 +8,29 @@
 
 #ifndef __ASSEMBLER__
 
-/* Stub */
+/** 
+ * DMB/DSB options:
+ * 
+ ** oshld  | Load-Load, Load-Store | Outer shareable
+ ** oshst  |     Store-Store       | Outer shareable
+ ** osh    |       Any-Any         | Outer shareable
+ ** nshld  | Load-Load, Load-Store | Non-shareable
+ ** nshst  |     Store-Store       | Non-shareable
+ ** nsh    |       Any-Any         | Non-shareable
+ ** ishld  | Load-Load, Load-Store | Inner shareable
+ ** ishst  |     Store-Store       | Inner shareable
+ ** ish    |       Any-Any         | Inner shareable
+ ** ld     | Load-Load, Load-Store | Full system
+ ** st     |     Store-Store       | Full system
+ ** sy     |       Any-Any         | Full system
+ */
+
+#define isb()    __asm__ __volatile__("isb" : : : "memory")
+#define dsb(opt) __asm__ __volatile__("dsb " #opt : : : "memory")
+#define dmb(opt) __asm__ __volatile__("dmb " #opt : : : "memory")
+
+static inline void data_mem_barrier(void) {
+	dmb(sy);
+}
 
 #endif /* __ASSEMBLER__ */
