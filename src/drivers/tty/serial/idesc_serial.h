bf43e732451ef962fa20ac2272c2aeda03d9e3a7
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-12-15 16:32:24 +0300

    drivers: Clean tty/serial

--- src/drivers/tty/serial/idesc_serial.h
+++ src/drivers/tty/serial/idesc_serial.h
@@ -8,12 +8,10 @@
 #ifndef IDESC_SERIAL_H_
 #define IDESC_SERIAL_H_
 
-#include <sys/types.h>
 struct idesc;
 struct uart;
 
 extern struct idesc *idesc_serial_open(struct uart *uart, int __oflags);
-
 extern const struct idesc_ops *idesc_serial_get_ops(void);
 
 #endif /* IDESC_SERIAL_H_ */
