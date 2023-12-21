b3e1880787cbd97efbf8dfdc46c11ac0c99c0ccb
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-12-21 15:00:14 +0300

    drivers: riscv_plic: Clean riscv_plic.c

--- src/drivers/interrupt/riscv_plic/riscv_plic.c
+++ src/drivers/interrupt/riscv_plic/riscv_plic.c
@@ -7,43 +7,32 @@
  * @author Anastasia Nizharadze
  */
 
-#include <stdint.h>
 #include <assert.h>
+#include <stdint.h>
 
-#include <hal/reg.h>
-
-#include <asm/regs.h>
 #include <asm/interrupts.h>
-
+#include <asm/regs.h>
 #include <drivers/irqctrl.h>
-
 #include <embox/unit.h>
+#include <hal/reg.h>
 
 #define PLIC_ADDR                 OPTION_GET(NUMBER, base_addr)
 #define PRIORITY_THRESHOLD_OFFSET OPTION_GET(NUMBER, threshold_offset)
 #define INTERRUPT_ENABLE_OFFSRT   OPTION_GET(NUMBER, interrupt_enable_offset)
 
-#define IPL_ADDR(num)            (PLIC_ADDR + (num * 4))
+#define IPL_ADDR(num)             (PLIC_ADDR + (num * 4))
 
-#define PRIORITY_THRESHOLD_ADDR  (PLIC_ADDR + PRIORITY_THRESHOLD_OFFSET)
-#define CLAIM_COMPLETE_ADDR      (PLIC_ADDR + PRIORITY_THRESHOLD_OFFSET + 0x4U) /* offset for hart 0 claim/complere reg */
-#define INTERRUPT_ENABLE_REG_1   (PLIC_ADDR + INTERRUPT_ENABLE_OFFSRT)
-#define INTERRUPT_ENABLE_REG_2   (PLIC_ADDR + INTERRUPT_ENABLE_OFFSRT + 0x4U)
-
+#define PRIORITY_THRESHOLD_ADDR   (PLIC_ADDR + PRIORITY_THRESHOLD_OFFSET)
+/* offset for hart 0 claim/complere reg */
+#define CLAIM_COMPLETE_ADDR       (PLIC_ADDR + PRIORITY_THRESHOLD_OFFSET + 0x4U)
+#define INTERRUPT_ENABLE_REG_1    (PLIC_ADDR + INTERRUPT_ENABLE_OFFSRT)
+#define INTERRUPT_ENABLE_REG_2    (PLIC_ADDR + INTERRUPT_ENABLE_OFFSRT + 0x4U)
 
 static int plic_init(void) {
 	REG32_STORE(PRIORITY_THRESHOLD_ADDR, 0);
-
 	enable_external_interrupts();
+
 	return 0;
-}
-
-int irqctrl_get_num(void) {
-	uint32_t claim;
-
-	claim = REG32_LOAD(CLAIM_COMPLETE_ADDR);
-
-	return claim;
 }
 
 void irqctrl_enable(unsigned int interrupt_nr) {
@@ -55,7 +44,8 @@ void irqctrl_enable(unsigned int interrupt_nr) {
 		reg = REG32_LOAD(INTERRUPT_ENABLE_REG_1);
 		reg |= (1U << interrupt_nr);
 		REG32_STORE(INTERRUPT_ENABLE_REG_1, reg);
-	} else {
+	}
+	else {
 		reg = REG32_LOAD(INTERRUPT_ENABLE_REG_2);
 		reg |= (1U << (interrupt_nr - 32));
 		REG32_STORE(INTERRUPT_ENABLE_REG_2, reg);
@@ -69,7 +59,8 @@ void irqctrl_disable(unsigned int interrupt_nr) {
 		reg = REG32_LOAD(INTERRUPT_ENABLE_REG_1);
 		reg &= ~(1U << interrupt_nr);
 		REG32_STORE(INTERRUPT_ENABLE_REG_1, reg);
-	} else {
+	}
+	else {
 		reg = REG32_LOAD(INTERRUPT_ENABLE_REG_2);
 		reg &= ~(1U << (interrupt_nr - 32));
 		REG32_STORE(INTERRUPT_ENABLE_REG_2, reg);
@@ -78,6 +69,10 @@ void irqctrl_disable(unsigned int interrupt_nr) {
 
 void irqctrl_eoi(unsigned int irq) {
 	REG32_STORE(CLAIM_COMPLETE_ADDR, irq);
+}
+
+unsigned int irqctrl_get_intid(void) {
+	return REG32_LOAD(CLAIM_COMPLETE_ADDR);
 }
 
 IRQCTRL_DEF(riscv_plic, plic_init);
