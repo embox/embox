74c8cf2e005b324b4cd30e924d4a1cce0fb10105
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-12-22 20:03:54 +0300

    drivers: Clean stm32 flash driver

--- src/drivers/flash/stm32cube_flash/stm32_in_chip_flash.h
+++ src/drivers/flash/stm32cube_flash/stm32_in_chip_flash.h
@@ -8,9 +8,8 @@
 #ifndef SRC_DRIVERS_FLASH_STM32_IN_CHIP_FLASH_H_
 #define SRC_DRIVERS_FLASH_STM32_IN_CHIP_FLASH_H_
 
-
 #include <framework/mod/options.h>
 
-#define STM32_FLASH_FLASH_SIZE         OPTION_GET(NUMBER,flash_size)
+#define STM32_FLASH_FLASH_SIZE OPTION_GET(NUMBER, flash_size)
 
 #endif /* SRC_DRIVERS_FLASH_STM32_IN_CHIP_FLASH_H_ */
