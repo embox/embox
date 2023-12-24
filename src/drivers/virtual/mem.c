1b78b0b840e8fc257a48f2e77dcf3cda49477956
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2023-12-24 18:07:35 +0300

    drivers: virtual: Clean mem.c

--- src/drivers/virtual/mem.c
+++ src/drivers/virtual/mem.c
@@ -5,16 +5,17 @@
  * @date 29.04.20
  * @author Puranjay Mohan <puranjay12@gmail.com>
  */
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <sys/uio.h>
-#include <sys/stat.h>
 #include <errno.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+
 #include <drivers/char_dev.h>
-#include <kernel/task/resource/index_descriptor.h>
 #include <kernel/task/resource/idesc.h>
+#include <kernel/task/resource/index_descriptor.h>
 #include <util/err.h>
 #include <util/macro.h>
 
@@ -51,21 +52,22 @@ static ssize_t mem_write(struct idesc *desc, const struct iovec *iov, int cnt) {
 	return ret_size;
 }
 
-static void *mem_mmap(struct idesc *idesc, void *addr, size_t len, int prot, int flags, int fd, off_t off){
-	void *vmem =  mmap_device_memory(addr, len, prot, flags, off);
+static void *mem_mmap(struct idesc *idesc, void *addr, size_t len, int prot,
+    int flags, int fd, off_t off) {
+	void *vmem = mmap_device_memory(addr, len, prot, flags, off);
 	return vmem;
 }
 
-static const struct idesc_ops mem_ops = {
-	.id_readv  = mem_read,
-	.id_writev = mem_write,
-	.close     = mem_close,
-	.fstat     = char_dev_idesc_fstat,
-	.idesc_mmap = mem_mmap,
-};
-
-static struct idesc *mem_open(struct dev_module *cdev, void *priv) {
+static struct idesc *mem_cdev_open(struct dev_module *cdev, void *priv) {
 	return char_dev_idesc_create(cdev);
 }
 
-CHAR_DEV_DEF(MEM_DEV_NAME, mem_open, &mem_ops, NULL);
+static const struct idesc_ops mem_idesc_ops = {
+    .id_readv = mem_read,
+    .id_writev = mem_write,
+    .close = mem_close,
+    .fstat = char_dev_idesc_fstat,
+    .idesc_mmap = mem_mmap,
+};
+
+CHAR_DEV_DEF(MEM_DEV_NAME, mem_cdev_open, &mem_idesc_ops, NULL);
