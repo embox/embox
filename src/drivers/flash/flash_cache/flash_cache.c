028cbc098348f1cb3728b9d3a0c3c2f6b1d4c369
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-12-29 16:23:02 +0300

    flash: Rework flash_write_buffered()

--- src/drivers/flash/flash_cache/flash_cache.c
+++ src/drivers/flash/flash_cache/flash_cache.c
@@ -21,39 +21,41 @@
  */
 int flash_write_buffered(struct flash_dev *flashdev,
 					int pos, void *buff, size_t size) {
-	int start_bk;
+	int blk;
 	int err;
 	int block_size;
 
 	assert(buff);
 
-	block_size = flashdev->block_info[0].block_size;
-
-	start_bk = pos / block_size;
+	blk = flash_get_block_by_offset(flashdev, pos);
 
 	err = 0;
 	
-	while(size) {
-		block_size = flashdev->block_info[0].block_size;
+	while(0 < size) {
+		int off_in_blk;
+
+		block_size = flash_get_block_size(flashdev, blk);
 
 		flash_cache_clean(flashdev);
 
-		if ( 0 != (pos % block_size)) {
+		off_in_blk = (pos % block_size);
+
+		if ( 0 != off_in_blk) {
 			/* load into the cache unchanged bytes from the block */
 			err = flash_cache_load(flashdev,
-						CACHE_OFFSET(flashdev),
-						start_bk * block_size,
-						(pos % block_size));
+						CACHE_OFFSET(flashdev) + 0,
+						flash_get_offset_by_block(flashdev, blk),
+						off_in_blk);
 			if (err != 0) {
 				break;
 			}
 		}
 
 		/* store data in cache */
-		if (size <= (block_size - (pos % block_size))) {
+		if (size <= (block_size - off_in_blk)) {
 			/*  if data fits in the block */
 			err = flash_cache_write(flashdev,
-							CACHE_OFFSET(flashdev) + pos, buff,
+							CACHE_OFFSET(flashdev) + off_in_blk, buff,
 							size);
 			if (err != 0) {
 				break;
@@ -64,33 +66,35 @@ int flash_write_buffered(struct flash_dev *flashdev,
 		} else {
 			/*  if data doesn't fit in the block */
 			err = flash_cache_write(flashdev,
-							CACHE_OFFSET(flashdev) + pos, buff, 
-							block_size - (pos % block_size) );
+							CACHE_OFFSET(flashdev) + off_in_blk, buff,
+							block_size - off_in_blk);
 			if (err != 0) {
 				break;
 			}
 
-			pos += block_size - (pos % block_size);
-			size -= block_size - (pos % block_size);
+			pos += block_size - off_in_blk;
+			size -= block_size - off_in_blk;
 		}
 
-		if (0 != (pos % block_size)) {
+		off_in_blk = (pos % block_size);
+
+		if (0 != off_in_blk) {
 			/* load into the cache unchanged bytes from a tail of the block */
 			flash_cache_load(flashdev,
-					CACHE_OFFSET(flashdev) + pos,
-					start_bk * block_size + pos,
-					block_size - (pos % block_size));
+					CACHE_OFFSET(flashdev) + off_in_blk,
+					pos,
+					block_size - off_in_blk);
 			if (err != 0) {
 				break;
 			}
 		}
 
 		/* store whole block from cache into the flash */
-		flash_cache_restore(flashdev, start_bk, flashdev->fld_cache);		
+		flash_cache_restore(flashdev, blk, flashdev->fld_cache);
 		if (err != 0) {
 			break;
 		}
-		start_bk ++;
+		blk ++;
 	}
 
 	return err;
