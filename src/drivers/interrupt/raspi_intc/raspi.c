013e84b52b540e921fdca3de6a833458aa1a39fc
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2024-01-14 17:05:17 +0300

    drivers: raspi_intc: Improve raspi interrupt controller

--- src/drivers/interrupt/raspi_intc/raspi.c
+++ src/drivers/interrupt/raspi_intc/raspi.c
@@ -10,27 +10,22 @@
 
 #include <assert.h>
 #include <stdint.h>
-
 #include <sys/mman.h>
 
+#include <drivers/common/memory.h>
+#include <drivers/irqctrl.h>
+#include <embox/unit.h>
+#include <framework/mod/options.h>
+#include <hal/mmu.h>
+#include <kernel/irq.h>
+#include <kernel/panic.h>
+#include <mem/vmem.h>
 #include <util/binalign.h>
 #include <util/bit.h>
 
-#include <hal/mmu.h>
-#include <mem/vmem.h>
+#define BCM2835_INTERRUPT_BASE OPTION_GET(NUMBER, base_addr)
 
-#include <drivers/common/memory.h>
-
-#include <kernel/irq.h>
-#include <drivers/irqctrl.h>
-#include <kernel/panic.h>
-#include <framework/mod/options.h>
-
-#include <embox/unit.h>
-
-#define BCM2835_INTERRUPT_BASE OPTION_GET(NUMBER,base_addr)
-
-#define BANK_CAPACITY 32
+#define BANK_CAPACITY          32
 
 /**
  * Layout of the Interrupt registers.
@@ -39,38 +34,37 @@ struct raspi_interrupt_regs {
 	/**
 	 * Shows which interrupt are pending.
 	 */
-    uint32_t irq_basic_pending;
-    uint32_t irq_pending_1;
-    uint32_t irq_pending_2;
+	uint32_t irq_basic_pending;
+	uint32_t irq_pending_1;
+	uint32_t irq_pending_2;
 
-    /**
+	/**
      * TODO: not used yet.
      * There is no priority for any interrupt. If one interrupt is much more
      * important then all others it can be routed to the FIQ.
      */
-    uint32_t fiq_control;
+	uint32_t fiq_control;
 
-    /**
+	/**
      * Writing a 1 to a bit will set the corresponding IRQ enable bit. All
      * other IRQ enable bits are unaffected. Only bits which are enabled
      * can be seen in the basic pending register.
      */
-    uint32_t enable_irqs_1;
-    uint32_t enable_irqs_2;
-    uint32_t enable_basic_irqs;
+	uint32_t enable_irqs_1;
+	uint32_t enable_irqs_2;
+	uint32_t enable_basic_irqs;
 
-    /**
+	/**
      * Writing a 1 to a bit will clear the corresponding IRQ enable bit.
      * All other IRQ enable bits are unaffected.
      */
-    uint32_t disable_irqs_1;
-    uint32_t disable_irqs_2;
-    uint32_t disable_basic_irqs;
+	uint32_t disable_irqs_1;
+	uint32_t disable_irqs_2;
+	uint32_t disable_basic_irqs;
 };
 
-static volatile struct raspi_interrupt_regs * const regs =
-        (volatile struct raspi_interrupt_regs*)((int)BCM2835_INTERRUPT_BASE);
-
+static volatile struct raspi_interrupt_regs *const regs =
+    (volatile struct raspi_interrupt_regs *)((int)BCM2835_INTERRUPT_BASE);
 
 static int raspi_intc_init(void) {
 #if 0
@@ -87,18 +81,30 @@ static int raspi_intc_init(void) {
 }
 
 void irqctrl_enable(unsigned int interrupt_nr) {
-	switch( interrupt_nr / BANK_CAPACITY ) {
-		case 0:	regs->enable_irqs_1 = 1 << interrupt_nr % BANK_CAPACITY;		break;
-		case 1:	regs->enable_irqs_2 = 1 << interrupt_nr % BANK_CAPACITY;		break;
-		case 2:	regs->enable_basic_irqs = 1 << interrupt_nr % BANK_CAPACITY;	break;
+	switch (interrupt_nr / BANK_CAPACITY) {
+	case 0:
+		regs->enable_irqs_1 = 1 << interrupt_nr % BANK_CAPACITY;
+		break;
+	case 1:
+		regs->enable_irqs_2 = 1 << interrupt_nr % BANK_CAPACITY;
+		break;
+	case 2:
+		regs->enable_basic_irqs = 1 << interrupt_nr % BANK_CAPACITY;
+		break;
 	}
 }
 
 void irqctrl_disable(unsigned int interrupt_nr) {
-	switch( interrupt_nr / BANK_CAPACITY ) {
-		case 0:	regs->disable_irqs_1 = 1 << interrupt_nr % BANK_CAPACITY;		break;
-		case 1:	regs->disable_irqs_2 = 1 << interrupt_nr % BANK_CAPACITY;		break;
-		case 2:	regs->disable_basic_irqs = 1 << interrupt_nr % BANK_CAPACITY;	break;
+	switch (interrupt_nr / BANK_CAPACITY) {
+	case 0:
+		regs->disable_irqs_1 = 1 << interrupt_nr % BANK_CAPACITY;
+		break;
+	case 1:
+		regs->disable_irqs_2 = 1 << interrupt_nr % BANK_CAPACITY;
+		break;
+	case 2:
+		regs->disable_basic_irqs = 1 << interrupt_nr % BANK_CAPACITY;
+		break;
 	}
 }
 
@@ -110,40 +116,44 @@ void irqctrl_force(unsigned int interrupt_nr) {
 	panic(__func__);
 }
 
-static void __raspi__dispatch_bank(uint32_t pend_bank, int32_t add_to) {
+static int __irqctrl_get_intid(uint32_t pend_bank, int32_t add_to) {
 	uint32_t bit;
 
-	while(pend_bank) {
+	while (pend_bank) {
 		bit = bit_ffs(pend_bank) - 1;
 		pend_bank ^= (1 << bit);
-		irq_dispatch(bit + add_to);
+		return bit + add_to;
 	}
+
+	return -1;
 }
 
-void interrupt_handle(void) {
-	assert(!critical_inside(CRITICAL_IRQ_LOCK));
+unsigned int irqctrl_get_intid(void) {
+	int intid;
 
-	critical_enter(CRITICAL_IRQ_HANDLER);
+	intid = __irqctrl_get_intid(regs->irq_pending_1, 0);
+	if (intid >= 0) {
+		return intid;
+	}
 
-	__raspi__dispatch_bank(regs->irq_pending_1, 0);
-	__raspi__dispatch_bank(regs->irq_pending_2, (1 << 5));
+	intid = __irqctrl_get_intid(regs->irq_pending_2, (1 << 5));
+	if (intid >= 0) {
+		return intid;
+	}
+
 	/*
 	 * 31:21 bits are unused, 20:8 are used for speeding up interrupts
 	 * processing by adding a number of 'normal' interrupt status bits there.
 	 * It might be used in order to improve this driver later, but for now
 	 * we apply a 0xFF mask to distinguish unique interrupt requests.
 	 */
-	__raspi__dispatch_bank(regs->irq_basic_pending & 0xFF, (2 << 5));
-
-	critical_leave(CRITICAL_IRQ_HANDLER);
-
-	critical_dispatch_pending();
+	return __irqctrl_get_intid(regs->irq_basic_pending & 0xFF, (2 << 5));
 }
 
-void swi_handle(void) {
-	panic(__func__);
+void irqctrl_eoi(unsigned int irq) {
 }
 
-PERIPH_MEMORY_DEFINE(raspi_intc, BCM2835_INTERRUPT_BASE, sizeof(struct raspi_interrupt_regs));
+PERIPH_MEMORY_DEFINE(raspi_intc, BCM2835_INTERRUPT_BASE,
+    sizeof(struct raspi_interrupt_regs));
 
 IRQCTRL_DEF(raspi_intc, raspi_intc_init);
