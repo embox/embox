48a2c09dbbdfba97e91e42587103b38ff154abe6
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-01-25 15:11:05 +0300

    compat: Clean posix/fs/lseek/lseek_oldfs

--- src/compat/posix/fs/lseek/lseek_oldfs.c
+++ src/compat/posix/fs/lseek/lseek_oldfs.c
@@ -7,33 +7,49 @@
 
 #include <errno.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
-#include <fs/kfile.h>
 #include <fs/file_desc.h>
 #include <kernel/task/resource/index_descriptor.h>
-#include <kernel/task/resource/idesc.h>
-
 #include <kernel/task/resource/idesc_table.h>
 
 off_t lseek(int fd, off_t offset, int origin) {
-	struct file_desc *desc;
-	off_t ret;
+	struct stat sb;
+	struct file_desc *file;
+	off_t pos;
 
-	if (!idesc_index_valid(fd)
-			|| (NULL == index_descriptor_get(fd))) {
+	if (!idesc_index_valid(fd)) {
+		return SET_ERRNO(EBADF);
+	}
+	if (NULL == (file = (struct file_desc *)index_descriptor_get(fd))) {
 		return SET_ERRNO(EBADF);
 	}
 
-	desc = file_desc_get(fd);
-	if (desc == NULL) {
-		return SET_ERRNO(ESPIPE);
+	fstat(fd, &sb);
+	switch (sb.st_mode & S_IFMT) {
+		case S_IFIFO:
+		case S_IFSOCK:
+		return SET_ERRNO(EPIPE);
 	}
 
-	ret = kseek(desc, offset, origin);
-	if (ret < 0) {
-		return SET_ERRNO(-ret);
-	}
+	pos = file_get_pos(file);
+	switch (origin) {
+		case SEEK_SET:
+			pos = offset;
+			break;
 
-	return ret;
+		case SEEK_CUR:
+			pos += offset;
+			break;
+
+		case SEEK_END:
+			pos = sb.st_size + offset;
+			break;
+
+		default:
+			return SET_ERRNO(EINVAL);
+	}
+	file_set_pos(file, pos);
+	return pos;
 }
