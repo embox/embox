427a1336e8b1fcbcc4994285f98ff124da96a83c
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-02-09 14:45:17 +0300

    minor: oldfs: Clean binfs_oldfs

--- src/fs/driver/binfs/binfs_oldfs.c
+++ src/fs/driver/binfs/binfs_oldfs.c
@@ -9,9 +9,6 @@
 #include <string.h>
 #include <limits.h>
 
-#include <embox/cmd.h>
-
-//#include <fs/vfs.h>
 #include <fs/inode.h>
 #include <fs/inode_operation.h>
 #include <fs/fs_driver.h>
@@ -20,11 +17,6 @@
 #include <fs/file_desc.h>
 
 #define BINFS_NAME "binfs"
-#if 0
-static int binfs_mount(struct super_block *sb, struct inode *dest) {
-	return 0;
-}
-#endif
 
 extern int binfs_destroy_inode(struct inode *inode) ;
 
@@ -39,16 +31,9 @@ struct super_block_operations binfs_sbops = {
 	.destroy_inode = binfs_destroy_inode,
 };
 
-#if 0
-static struct fsop_desc binfs_fsop = {
-	//.mount = binfs_mount,
-};
-#endif
 static struct fs_driver binfs_driver = {
 	.name = BINFS_NAME,
 	.fill_sb   = binfs_fill_sb,
-	//.fsop = &binfs_fsop,
-
 };
 
 DECLARE_FILE_SYSTEM_DRIVER(binfs_driver);
