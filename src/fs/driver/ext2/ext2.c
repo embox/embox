388855c284b9acc7a97510a90617e712172fb6a6
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-02-19 15:23:27 +0300

    oldfs: Extract file_ops in 'ext2' fs driver

--- src/fs/driver/ext2/ext2.c
+++ src/fs/driver/ext2/ext2.c
@@ -34,7 +34,7 @@ extern int ext2_buf_read_file(struct inode *inode, char **, size_t *);
 extern int ext2_read_inode(struct inode *node, uint32_t);
 
 static int ext2_block_map(struct inode *node, int32_t, uint32_t *);
-static size_t ext2_write_file(struct inode *inode, char *buf_p, size_t size);
+
 static int ext2_new_block(struct inode *node, long position);
 static int ext2_search_directory(struct inode *node, const char *, int, uint32_t *);
 static int ext2_read_sblock(struct super_block *sb);
@@ -45,17 +45,7 @@ extern int ext2_mount_entry(struct inode *node);
 
 /* TODO link counter */
 
-static struct idesc *ext2fs_open(struct inode *node, struct idesc *idesc, int __oflag);
-static int ext2fs_close(struct file_desc *desc);
-static size_t ext2fs_read(struct file_desc *desc, void *buf, size_t size);
-static size_t ext2fs_write(struct file_desc *desc, void *buf, size_t size);
-
-struct file_operations ext2_fop = {
-	.open = ext2fs_open,
-	.close = ext2fs_close,
-	.read = ext2fs_read,
-	.write = ext2fs_write,
-};
+extern struct file_operations ext2_fop;
 
 static int e2fs_destroy_inode(struct inode *inode) {
 	return 0;
@@ -345,96 +335,6 @@ int ext2_open(struct inode *node) {
 
 	out: ext2_close(node);
 	return rc;
-}
-
-/*
- * file_operation
- */
-static struct idesc *ext2fs_open(struct inode *node, struct idesc *idesc, int __oflag) {
-	int rc;
-	struct ext2_file_info *fi;
-	struct ext2_fs_info *fsi;
-
-	fi = inode_priv(node);
-	fsi = node->i_sb->sb_data;
-	fi->f_pointer = file_get_pos(file_desc_from_idesc(idesc));
-
-	if (NULL == (fi->f_buf = ext2_buff_alloc(fsi, fsi->s_block_size))) {
-		return err2ptr(ENOMEM);
-	}
-
-	if (0 != (rc = ext2_read_inode(node, fi->f_num))) {
-		ext2_close(node);
-		return err2ptr(rc);
-	}
-	else {
-		file_set_size(file_desc_from_idesc(idesc), fi->f_di.i_size);
-	}
-
-	return idesc;
-}
-
-static int ext2fs_close(struct file_desc *desc) {
-	if (NULL == desc) {
-		return 0;
-	}
-
-	return ext2_close(desc->f_inode);
-}
-
-static size_t ext2fs_read(struct file_desc *desc, void *buff, size_t size) {
-	int rc;
-	size_t csize;
-	char *buf;
-	size_t buf_size;
-	char *addr = buff;
-	struct inode *node;
-	struct ext2_file_info *fi;
-
-	node = desc->f_inode;
-	fi = inode_priv(node);
-	fi->f_pointer = file_get_pos(desc);
-
-	while (size != 0) {
-		/* XXX should handle LARGEFILE */
-		if (fi->f_pointer >= (long) fi->f_di.i_size) {
-			break;
-		}
-
-		if (0 != (rc = ext2_buf_read_file(node, &buf, &buf_size))) {
-			SET_ERRNO(rc);
-			return 0;
-		}
-
-		csize = size;
-		if (csize > buf_size) {
-			csize = buf_size;
-		}
-
-		memcpy(addr, buf, csize);
-
-		fi->f_pointer += csize;
-		addr += csize;
-		size -= csize;
-	}
-
-	return (addr - (char *) buff);
-}
-
-static size_t ext2fs_write(struct file_desc *desc, void *buff, size_t size) {
-	uint32_t bytecount;
-	struct inode *node;
-	struct ext2_file_info *fi;
-
-	node = desc->f_inode;
-	fi = inode_priv(node);
-	fi->f_pointer = file_get_pos(desc);
-
-	bytecount = ext2_write_file(node, buff, size);
-
-	file_set_size(desc, fi->f_di.i_size);
-
-	return bytecount;
 }
 
 extern int ext2_create(struct inode *i_new, struct inode *i_dir);
@@ -960,7 +860,7 @@ int ext2_buf_read_file(struct inode *node, char **buf_p, size_t *size_p) {
 /*
  * Write a portion to a file from an internal buffer.
  */
-static size_t ext2_write_file(struct inode *node, char *buf, size_t size) {
+size_t ext2_write_file(struct inode *node, char *buf, size_t size) {
 	long inblock_off;
 	int32_t file_block;
 	uint32_t disk_block;
---
+++ src/fs/driver/ext2/ext2fs_file_ops.c
@@ -0,0 +1,120 @@
+/**
+ * @file
+ * @brief ext file system
+ *
+ * @date 04.12.2012
+ * @author Andrey Gazukin
+ */
+
+#include <stddef.h>
+
+#include <fs/inode.h>
+
+#include <fs/ext2.h>
+
+#include <fs/file_desc.h>
+
+#include <util/err.h>
+
+extern int ext2_buf_read_file(struct inode *inode, char **, size_t *);
+extern int ext2_read_inode(struct inode *node, uint32_t);
+extern size_t ext2_write_file(struct inode *inode, char *buf_p, size_t size);
+extern int ext2_close(struct inode *node);
+extern int ext2_open(struct inode *node);
+
+/*
+ * file_operation
+ */
+static struct idesc *ext2fs_open(struct inode *node, struct idesc *idesc, int __oflag) {
+	int rc;
+	struct ext2_file_info *fi;
+	struct ext2_fs_info *fsi;
+
+	fi = inode_priv(node);
+	fsi = node->i_sb->sb_data;
+	fi->f_pointer = file_get_pos(file_desc_from_idesc(idesc));
+
+	if (NULL == (fi->f_buf = ext2_buff_alloc(fsi, fsi->s_block_size))) {
+		return err2ptr(ENOMEM);
+	}
+
+	if (0 != (rc = ext2_read_inode(node, fi->f_num))) {
+		ext2_close(node);
+		return err2ptr(rc);
+	}
+	else {
+		file_set_size(file_desc_from_idesc(idesc), fi->f_di.i_size);
+	}
+
+	return idesc;
+}
+
+static int ext2fs_close(struct file_desc *desc) {
+	if (NULL == desc) {
+		return 0;
+	}
+
+	return ext2_close(desc->f_inode);
+}
+
+static size_t ext2fs_read(struct file_desc *desc, void *buff, size_t size) {
+	int rc;
+	size_t csize;
+	char *buf;
+	size_t buf_size;
+	char *addr = buff;
+	struct inode *node;
+	struct ext2_file_info *fi;
+
+	node = desc->f_inode;
+	fi = inode_priv(node);
+	fi->f_pointer = file_get_pos(desc);
+
+	while (size != 0) {
+		/* XXX should handle LARGEFILE */
+		if (fi->f_pointer >= (long) fi->f_di.i_size) {
+			break;
+		}
+
+		if (0 != (rc = ext2_buf_read_file(node, &buf, &buf_size))) {
+			SET_ERRNO(rc);
+			return 0;
+		}
+
+		csize = size;
+		if (csize > buf_size) {
+			csize = buf_size;
+		}
+
+		memcpy(addr, buf, csize);
+
+		fi->f_pointer += csize;
+		addr += csize;
+		size -= csize;
+	}
+
+	return (addr - (char *) buff);
+}
+
+static size_t ext2fs_write(struct file_desc *desc, void *buff, size_t size) {
+	uint32_t bytecount;
+	struct inode *node;
+	struct ext2_file_info *fi;
+
+	node = desc->f_inode;
+	fi = inode_priv(node);
+	fi->f_pointer = file_get_pos(desc);
+
+	bytecount = ext2_write_file(node, buff, size);
+
+	file_set_size(desc, fi->f_di.i_size);
+
+	return bytecount;
+}
+
+struct file_operations ext2_fop = {
+	.open = ext2fs_open,
+	.close = ext2fs_close,
+	.read = ext2fs_read,
+	.write = ext2fs_write,
+};
