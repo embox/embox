76a2ab811e2add5dd8825d78dc0aa54c128e22f3
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2024-02-20 14:25:00 +0300

    util: Clean err.h

--- src/include/util/err.h
+++ src/include/util/err.h
@@ -5,16 +5,15 @@
  * @author: Anton Bondarev
  */
 
-#ifndef ERR_H_
-#define ERR_H_
-
+#ifndef UTIL_ERR_H_
+#define UTIL_ERR_H_
 
 static inline void *err_ptr(long err_code) {
-	return (void *) -err_code;
+	return (void *)-err_code;
 }
 
 static inline int err(const void *res) {
 	return (((long)res) < 0) && (((long)res) > -4096) ? ((long)res) : 0;
 }
 
-#endif /* ERR_H_ */
+#endif /* UTIL_ERR_H_ */
