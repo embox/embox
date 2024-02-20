a99014460e9a8982c2364349234fa7610b3aae75
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2024-02-20 14:19:48 +0300

    device: char_dev: Improve char_dev api

--- src/device/char_dev/char_dev.c
+++ src/device/char_dev/char_dev.c
@@ -1,144 +1,143 @@
 /**
- * @file
- * @brief Handle char devices
- * @author Denis Deryugin <deryugin.denis@gmail.com>
- * @version 0.1
- * @date 2015-10-05
+ * @brief
+ *
+ * @author Aleksey Zhmulin
+ * @date 19.02.24
  */
+
 #include <assert.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <stddef.h>
 #include <string.h>
-#include <sys/stat.h>
 
 #include <drivers/char_dev.h>
-#include <drivers/device.h>
+#include <embox/unit.h>
 #include <lib/libds/array.h>
-#include <lib/libds/indexator.h>
-#include <util/log.h>
+#include <lib/libds/dlist.h>
 
-static struct dev_module *devtab[MAX_CDEV_QUANTITY];
-INDEX_DEF(char_dev_idx, 0, MAX_CDEV_QUANTITY);
+EMBOX_UNIT_INIT(char_dev_registry_init);
 
-struct dev_module **get_cdev_tab(void) {
-	return &devtab[0];
+static DLIST_DEFINE(char_dev_list);
+
+struct dlist_head *const __char_dev_registry = &char_dev_list;
+
+static int char_dev_check_name(const char *name) {
+	struct char_dev *tmp;
+	size_t len;
+
+	len = strlen(name);
+
+	if (len == 0) {
+		return -EINVAL;
+	}
+
+	if (len >= sizeof(((struct char_dev *)0)->name)) {
+		return -ENAMETOOLONG;
+	}
+
+	char_dev_foreach(tmp) {
+		if (!strcmp(tmp->name, name)) {
+			return -EEXIST;
+		}
+	}
+
+	return 0;
 }
-extern void devfs_notify_new_module(struct dev_module *devmod);
-extern void devfs_notify_del_module(struct dev_module *devmod);
 
-struct dev_module *idesc_to_dev_module(struct idesc *idesc) {
-	return idesc->idesc_priv;
+struct char_dev *char_dev_iterate(struct char_dev *cdev) {
+	if (!cdev) {
+		return dlist_first_entry_or_null(&char_dev_list, struct char_dev,
+		    list_item);
+	}
+
+	return dlist_next_entry_if_not_last(cdev, &char_dev_list, struct char_dev,
+	    list_item);
 }
 
-static int char_dev_register_static(struct dev_module *cdev) {
-	int cdev_id;
+struct char_dev *char_dev_find(const char *name) {
+	struct char_dev *cdev;
+
+	assert(name);
+
+	char_dev_foreach(cdev) {
+		if (!strcmp(name, cdev->name)) {
+			return cdev;
+		}
+	}
+
+	return NULL;
+}
+
+int char_dev_register(struct char_dev *cdev) {
+	int err;
+
+	assert(cdev);
+	assert(cdev->ops);
+
+	if ((err = char_dev_check_name(cdev->name))) {
+		return err;
+	}
+
+	dlist_add_next(&cdev->list_item, __char_dev_registry);
+
+	return 0;
+}
+
+int char_dev_unregister(struct char_dev *cdev) {
+	struct char_dev *tmp;
 
 	assert(cdev);
 
-	/* This index is not supposed to be freed */
-	cdev_id = index_alloc(&char_dev_idx, INDEX_MIN);
-	if (cdev_id == INDEX_NONE) {
-		log_error("Failed to register char dev %s", cdev->name);
-		return -ENOMEM;
+	char_dev_foreach(tmp) {
+		if (tmp == cdev) {
+			dlist_del_init(&cdev->list_item);
+			return 0;
+		}
 	}
 
-	devtab[cdev_id] = cdev;
-
-	/* XXX Since cdevs were probably allocated as read-only in array spread
-	 * we cannot modify its dev_id. Since dev_id is unused currently
-	 * it's safely do not assign it at all. */
-	/* cdev->dev_id = cdev_id; */
-
-	devfs_notify_new_module(cdev);
-
-	return 0;
+	return -EINVAL;
 }
 
-int char_dev_register(struct dev_module *cdev) {
-	int cdev_id;
+int char_dev_rename(struct char_dev *cdev, const char *new_name) {
+	int err;
 
 	assert(cdev);
+	assert(new_name);
 
-	/* This index is not supposed to be freed */
-	cdev_id = index_alloc(&char_dev_idx, INDEX_MIN);
-	if (cdev_id == INDEX_NONE) {
-		log_error("Failed to register char dev %s", cdev->name);
-		return -ENOMEM;
+	if ((err = char_dev_check_name(cdev->name))) {
+		return err;
 	}
-	cdev->dev_id = DEVID_CDEV | cdev_id;
 
-	devtab[cdev_id] = cdev;
-
-	/* XXX Since cdevs were probably allocated as read-only in array spread
-	 * we cannot modify its dev_id. Since dev_id is unused currently
-	 * it's safely do not assign it at all. */
-	/* cdev->dev_id = cdev_id; */
-
-	devfs_notify_new_module(cdev);
+	strcpy(cdev->name, new_name);
 
 	return 0;
 }
 
-int char_dev_unregister(struct dev_module *cdev) {
+void char_dev_init(struct char_dev *cdev, const char *name,
+    const struct char_dev_ops *ops) {
 	assert(cdev);
+	assert(name);
+	assert(ops);
 
-	if (devtab[cdev->dev_id & DEVID_ID_MASK] == cdev) {
-		return -ENODEV;
+	dlist_init(&cdev->list_item);
+	cdev->usage_count = 0;
+	cdev->ops = ops;
+
+	strncpy(cdev->name, name, sizeof(cdev->name) - 1);
+	cdev->name[sizeof(cdev->name) - 1] = '\0';
+}
+
+ARRAY_SPREAD_DEF(struct char_dev *const, __char_dev_static_registry);
+
+static int char_dev_registry_init(void) {
+	struct char_dev *cdev;
+	int err;
+
+	array_spread_foreach(cdev, __char_dev_static_registry) {
+		if ((err = char_dev_register(cdev))) {
+			return err;
+		}
 	}
 
-	devfs_notify_del_module(cdev);
-
-	index_free(&char_dev_idx, cdev->dev_id & DEVID_ID_MASK);
-
-	devtab[cdev->dev_id & DEVID_ID_MASK] = NULL;
-
 	return 0;
-}
-
-ARRAY_SPREAD_DEF(const struct dev_module, __char_device_registry);
-
-/* NOTE: this function doesn't actually init anything, it just stores
- * statically allocated char devices to devtab[] */
-int char_dev_init_all(void) {
-	struct dev_module *cdev;
-
-	array_spread_foreach_ptr(cdev, __char_device_registry) {
-		char_dev_register_static(cdev);
-	}
-
-	return 0;
-}
-
-/* This stub is supposed to be used when there's no need
- * for device-specific idesc_ops.fstat() */
-int char_dev_default_fstat(struct idesc *idesc, void *buff) {
-	struct stat *sb;
-
-	assert(buff);
-
-	sb = buff;
-	memset(sb, 0, sizeof(struct stat));
-	sb->st_mode = S_IFCHR;
-
-	return 0;
-}
-
-int char_dev_default_open(struct idesc *idesc, void *source) {
-	idesc->idesc_priv = source;
-
-	return 0;
-}
-
-void char_dev_default_close(struct idesc *idesc) {
-}
-
-static const struct idesc_ops char_dev_default_ops = {
-    .open = char_dev_default_open,
-    .close = char_dev_default_close,
-    .fstat = char_dev_default_fstat,
-};
-
-const struct idesc_ops *char_dev_get_default_ops(void) {
-	return &char_dev_default_ops;
 }
--- src/device/char_dev/char_dev.h
+++ src/device/char_dev/char_dev.h
@@ -1,43 +1,74 @@
 /**
- * @file
- * @brief Declarations for char devices
- * @author Denis Deryugin <deryugin.denis@gmail.com>
- * @version 0.1
- * @date 2015-10-05
+ * @brief
+ *
+ * @author Aleksey Zhmulin
+ * @date 19.02.24
  */
 
-#ifndef CHAR_DEV_DVFS_H_
-#define CHAR_DEV_DVFS_H_
+#ifndef DEVICE_CHAR_DEV_H_
+#define DEVICE_CHAR_DEV_H_
+
+#include <limits.h>
 
 #include <drivers/device.h>
-#include <framework/mod/options.h>
 #include <kernel/task/resource/idesc.h>
 #include <lib/libds/array.h>
+#include <lib/libds/dlist.h>
 
-#include <config/embox/device/char_dev_api.h>
+#include <config/embox/device/char_dev.h>
 
-#define MAX_CDEV_QUANTITY \
-	OPTION_MODULE_GET(embox__device__char_dev_api, NUMBER, dev_quantity)
+#define CHAR_DEV_INIT(_self, _name, _ops)         \
+	((struct char_dev){                           \
+	    .list_item = DLIST_INIT(_self.list_item), \
+	    .usage_count = 0,                         \
+	    .ops = _ops,                              \
+	    .name = _name,                            \
+	})
 
-#define CHAR_DEV_DEF(cdev_name, idesc_ops, priv)                           \
-	ARRAY_SPREAD_DECLARE(const struct dev_module, __char_device_registry); \
-	ARRAY_SPREAD_ADD_NAMED(__char_device_registry,                         \
-	    MACRO_CONCAT(__char_device_registry_ptr_, cdev_name),              \
-	    {                                                                  \
-	        .dev_id = DEVID_CDEV,                                          \
-	        .name = MACRO_STRING(cdev_name),                               \
-	        .dev_iops = idesc_ops,                                         \
-	        .dev_priv = priv,                                              \
-	    })
+#define CHAR_DEV_REGISTER(_cdev_ptr)                                          \
+	ARRAY_SPREAD_DECLARE(struct char_dev *const, __char_dev_static_registry); \
+	ARRAY_SPREAD_ADD(__char_dev_static_registry, _cdev_ptr)
 
-extern int char_dev_init_all(void);
-extern int char_dev_register(struct dev_module *cdev);
-extern int char_dev_unregister(struct dev_module *cdev);
-extern int char_dev_default_fstat(struct idesc *idesc, void *buff);
-extern int char_dev_default_open(struct idesc *idesc, void *source);
-extern void char_dev_default_close(struct idesc *idesc);
+struct char_dev;
 
-extern const struct idesc_ops *char_dev_get_default_ops(void);
-extern struct dev_module *idesc_to_dev_module(struct idesc *desc);
+struct char_dev_ops {
+	void (*close)(struct char_dev *cdev);
+	int (*open)(struct char_dev *cdev, struct idesc *idesc);
+	int (*status)(struct char_dev *cdev, int mask);
+	int (*ioctl)(struct char_dev *cdev, int request, void *data);
+	ssize_t (*read)(struct char_dev *cdev, void *buf, size_t nbyte);
+	ssize_t (*write)(struct char_dev *cdev, const void *buf, size_t nbyte);
+	void *(*direct_access)(struct char_dev *cdev, off_t off, size_t len);
+};
 
-#endif /* CHAR_DEV_DVFS_H_ */
+struct char_dev {
+	const struct char_dev_ops *ops;
+	struct dlist_head list_item;
+	int usage_count;
+	char name[NAME_MAX];
+};
+
+struct char_dev_idesc {
+	struct idesc idesc;
+	struct char_dev *cdev;
+};
+
+extern struct dlist_head *const __char_dev_registry;
+extern const struct idesc_ops __char_dev_idesc_ops;
+
+extern struct char_dev *char_dev_iterate(struct char_dev *cdev);
+extern struct char_dev *char_dev_find(const char *name);
+extern int char_dev_rename(struct char_dev *cdev, const char *new_name);
+extern int char_dev_register(struct char_dev *cdev);
+extern int char_dev_unregister(struct char_dev *cdev);
+extern void char_dev_init(struct char_dev *cdev, const char *name,
+    const struct char_dev_ops *ops);
+
+extern struct idesc *char_dev_open(struct char_dev *cdev, int oflag);
+
+#define char_dev_foreach(cdev_ptr) \
+	dlist_foreach_entry(cdev_ptr, __char_dev_registry, list_item)
+
+#define char_dev_idesc_ops() (&__char_dev_idesc_ops)
+
+#endif /* DEVICE_CHAR_DEV_H_ */
