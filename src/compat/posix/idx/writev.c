4e51263e5e246da6bfbf49d8030688e357204d0f
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2024-02-20 17:08:27 +0300

    posix: idx: Improve readv() writev() and write()

--- src/compat/posix/idx/writev.c
+++ src/compat/posix/idx/writev.c
@@ -20,16 +20,19 @@ ssize_t writev(int fd, const struct iovec *iov, int iovcnt) {
 	int ret;
 	struct idesc *idesc;
 
-	if (iovcnt < 0) {
+	if (iovcnt <= 0) {
 		return SET_ERRNO(EINVAL);
 	}
+
 	if (!idesc_index_valid(fd)
 			|| (NULL == (idesc = index_descriptor_get(fd)))
 			|| ((idesc->idesc_flags & O_ACCESS_MASK) == O_RDONLY)) {
 		return SET_ERRNO(EBADF);
 	}
 
-	assert(idesc->idesc_ops != NULL);
+	assert(idesc->idesc_ops);
+	assert(idesc->idesc_ops->id_writev);
+
 	ret = idesc->idesc_ops->id_writev(idesc, iov, iovcnt);
 	if (ret < 0) {
 		return SET_ERRNO(-ret);
