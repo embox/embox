2376f7766c1b041d1628eecb6f45d22ea77f6e3e
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2024-02-21 16:49:28 +0300

    drivers: uart_dev: Clean uart_dev_dynamic_repo.c

--- src/drivers/serial/uart_dev/uart_dev_dynamic_repo.c
+++ src/drivers/serial/uart_dev/uart_dev_dynamic_repo.c
@@ -6,43 +6,34 @@
  * @date    09.08.2013
  */
 
-#include <string.h>
-#include <stdio.h>
 #include <errno.h>
-
-#include <util/err.h>
-#include <util/log.h>
-#include <lib/libds/indexator.h>
-#include <lib/libds/dlist.h>
-#include <lib/libds/ring_buff.h>
-#include <lib/libds/array.h>
-
-#include <kernel/irq.h>
-#include <mem/misc/pool.h>
+#include <stdio.h>
+#include <string.h>
 
 #include <drivers/device.h>
 #include <drivers/serial/uart_dev.h>
+#include <kernel/irq.h>
+#include <lib/libds/array.h>
+#include <lib/libds/dlist.h>
+#include <lib/libds/indexator.h>
+#include <lib/libds/ring_buff.h>
+#include <mem/misc/pool.h>
+#include <util/err.h>
+#include <util/log.h>
 
 ARRAY_SPREAD_DECLARE(struct uart *const, __uart_device_registry);
 
-extern int ttys_register(const char *name, void *dev_info);
-
-
-#define UART_MAX_N OPTION_GET(NUMBER,uart_max_n)
-
+#define UART_MAX_N OPTION_GET(NUMBER, uart_max_n)
 
 INDEX_DEF(serial_indexator, 0, UART_MAX_N);
 static struct uart *__uart_device_dynamic_registry[UART_MAX_N];
 
-
-
-int uart_register(struct uart *uart,
-		const struct uart_params *uart_defparams) {
+int uart_register(struct uart *uart, const struct uart_params *uart_defparams) {
 	int res;
 	size_t allocated_idx;
-	struct uart * existed_uart_dev;
+	struct uart *existed_uart_dev;
 
-	array_spread_foreach(existed_uart_dev, __uart_device_registry)  {
+	array_spread_foreach(existed_uart_dev, __uart_device_registry) {
 		if (uart == existed_uart_dev) {
 			/* uart has been inited statically */
 			return 0;
@@ -59,7 +50,8 @@ int uart_register(struct uart *uart,
 
 	if (uart_defparams) {
 		memcpy(&uart->params, uart_defparams, sizeof(struct uart_params));
-	} else {
+	}
+	else {
 		memset(&uart->params, 0, sizeof(struct uart_params));
 	}
 
@@ -70,13 +62,12 @@ int uart_register(struct uart *uart,
 		return res;
 	}
 
-	__uart_device_dynamic_registry[uart->idx ] = uart;
+	__uart_device_dynamic_registry[uart->idx] = uart;
 
 	return 0;
 }
 
 void uart_deregister(struct uart *uart) {
-
 	dlist_del(&uart->uart_lnk);
 
 	index_free(&serial_indexator, uart->idx);
@@ -87,20 +78,19 @@ struct uart *uart_dev_lookup(const char *name) {
 	struct uart *uart = NULL;
 
 	array_spread_foreach(uart, __uart_device_registry) {
-		if (0 == strcmp(uart->dev_name, name) ) {
+		if (0 == strcmp(uart->dev_name, name)) {
 			return uart;
 		}
 	}
-	for(i = 0 ; i < ARRAY_SIZE(__uart_device_dynamic_registry); i++) {
+	for (i = 0; i < ARRAY_SIZE(__uart_device_dynamic_registry); i++) {
 		if (NULL == __uart_device_dynamic_registry[i]) {
 			continue;
 		}
 		uart = __uart_device_dynamic_registry[i];
-		if (0 == strcmp(uart->dev_name, name) ) {
+		if (0 == strcmp(uart->dev_name, name)) {
 			return uart;
 		}
 	}
 
 	return NULL;
- }
-
+}
