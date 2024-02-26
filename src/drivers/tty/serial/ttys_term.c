50c42a2e1c3fdcbf78ae46b593c158335426e4d6
AlekseyZhmulin <ZhmulinAleksej@gmail.com>
2024-02-26 17:01:26 +0300

    drivers: tty: serial: Improve tty operations

--- src/drivers/tty/serial/ttys_term.c
+++ src/drivers/tty/serial/ttys_term.c
@@ -9,9 +9,11 @@
 #include <drivers/tty.h>
 #include <drivers/ttys.h>
 
-static inline struct uart *tty2uart(struct tty *tty) {
+static struct uart *tty2uart(struct tty *tty) {
 	struct serial_dev *tu;
+
 	tu = member_cast_out(tty, struct serial_dev, tty);
+
 	return tu->uart;
 }
 
@@ -27,20 +29,31 @@ static void uart_out_wake(struct tty *t) {
 	irq_unlock();
 }
 
-static void uart_term_setup(struct tty *tty, struct termios *termios) {
+static void uart_setup_term(struct tty *tty) {
 	struct uart *uart_dev = tty2uart(tty);
 	struct uart_params params;
 
 	uart_get_params(uart_dev, &params);
 
 	/* TODO baud rate is ospeed. What's with ispeed ? */
-	if (params.baud_rate != termios->c_ospeed) {
-		params.baud_rate = termios->c_ospeed;
+	if (params.baud_rate != tty->termios.c_ospeed) {
+		params.baud_rate = tty->termios.c_ospeed;
 		uart_set_params(uart_dev, &params);
 	}
 }
 
+static void uart_fill_term(struct tty *tty) {
+	struct uart *uart_dev = tty2uart(tty);
+	struct uart_params params;
+
+	uart_get_params(uart_dev, &params);
+
+	tty->termios.c_ospeed = params.baud_rate;
+	tty->termios.c_ispeed = params.baud_rate;
+}
+
 const struct tty_ops __uart_tty_ops = {
-    .setup = uart_term_setup,
+    .setup_term = uart_setup_term,
+    .fill_term = uart_fill_term,
     .out_wake = uart_out_wake,
 };
