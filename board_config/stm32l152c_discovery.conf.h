4964de057bf5b8276d295623b7618727864bc55d
Andrew Bursian <bursian@mail.ru>
2024-03-25 12:30:28 +0300

    Board config: Add USART2,3 & LEDs for STM32L152C-Discovery

--- board_config/stm32l152c_discovery.conf.h
+++ board_config/stm32l152c_discovery.conf.h
@@ -21,6 +21,57 @@ struct uart_conf uarts[] = {
 		},
 		.baudrate = 115200,
 	},
+	[2] = {
+		.status = DISABLED,
+		.name = "USART2",
+		.dev = {
+			.irqs = {
+				VAL("", 38),
+			},
+			.pins = {
+				PIN("TX", PA, PIN_2, AF7),
+				PIN("RX", PA, PIN_3, AF7),
+			},
+			.clocks = {
+				VAL("TX",   CLK_GPIOA),
+				VAL("RX",   CLK_GPIOA),
+				VAL("UART", CLK_USART2),
+			}
+		},
+		.baudrate = 115200,
+	},
+	[3] = {
+		.status = DISABLED,
+		.name = "USART3",
+		.dev = {
+			.irqs = {
+				VAL("", 39),
+			},
+			.pins = {
+				PIN("TX", PB, PIN_10, AF7),
+				PIN("RX", PB, PIN_11, AF7),
+			},
+			.clocks = {
+				VAL("TX",   CLK_GPIOB),
+				VAL("RX",   CLK_GPIOB),
+				VAL("UART", CLK_USART3),
+			}
+		},
+		.baudrate = 115200,
+	},
+};
+
+struct led_conf leds[] = {
+	[0] = {
+		.name = "LED1",
+		.port = VAL("", GPIO_PORT_B),
+		.pin = VAL("", 7),
+	},
+	[1] = {
+		.name = "LED2",
+		.port = VAL("", GPIO_PORT_B),
+		.pin = VAL("", 6),
+	},
 };
 
 EXPORT_CONFIG(UART(uarts), LED(leds))
