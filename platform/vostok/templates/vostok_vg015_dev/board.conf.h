904083e8ca9e3085205d697f5641236927d38598
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-04-08 17:44:58 +0300

    templates: Add board.conf.h to platform/vostok/vostok_vg015_dev

---
+++ platform/vostok/templates/vostok_vg015_dev/board.conf.h
@@ -0,0 +1,10 @@
+#include <vostok_vg015_dev.conf.h>
+
+CONFIG {
+	uarts[1].status = ENABLED;
+	uarts[2].status = DISABLED;
+	uarts[3].status = DISABLED;
+	uarts[4].status = DISABLED;
+
+//	spis[1].status = ENABLED;
+}
