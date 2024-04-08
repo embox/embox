fddd6e70958a58c51df65afd1547de1cfd0ade97
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-04-08 17:44:11 +0300

    board_config: Add board_config/vostok_vg015_dev.conf.h

---
+++ board_config/vostok_vg015_dev.conf.h
@@ -0,0 +1,132 @@
+#include <gen_board_conf.h>
+
+
+struct uart_conf uarts[] = {
+	[1] = {
+		.status = DISABLED,
+		.name = "USART1",
+		.dev = {
+			.regs = {
+				REGMAP("BASE_ADDR", UART0_BASE, 0x100),
+			},
+			.irqs = {
+				VAL("", 37),
+			},
+			.pins = {
+				PIN("TX", PA, PIN_9, AF7),
+				PIN("RX", PA, PIN_10, AF7),
+			},
+			.clocks = {
+				VAL("TX",   CLK_GPIOA),
+				VAL("RX",   CLK_GPIOA),
+				VAL("UART", CLK_USART1),
+			}
+		},
+		.baudrate = 115200,
+	},
+	[2] = {
+		.status = DISABLED,
+		.name = "USART2",
+		.dev = {
+			.irqs = {
+				VAL("", 38),
+			},
+			.pins = {
+				PIN("TX", PA, PIN_2, AF7),
+				PIN("RX", PA, PIN_3, AF7),
+			},
+			.clocks = {
+				VAL("TX",   CLK_GPIOA),
+				VAL("RX",   CLK_GPIOA),
+				VAL("UART", CLK_USART2),
+			}
+		},
+		.baudrate = 115200,
+	},
+	[3] = {
+		.status = ENABLED,
+		.name = "USART3",
+		.dev = {
+			.irqs = {
+				VAL("", 39),
+			},
+			.pins = {
+				PIN("TX", PD, PIN_8, AF7),
+				PIN("RX", PD, PIN_9, AF7),
+			},
+			.clocks = {
+				VAL("TX",   CLK_GPIOD),
+				VAL("RX",   CLK_GPIOD),
+				VAL("UART", CLK_USART3),
+			}
+		},
+		.baudrate = 115200,
+	},
+};
+
+struct spi_conf spis[] = {
+	[1] = {
+		.status = DISABLED,
+		.name = "SPI1",
+		.dev = {
+			.pins = {
+				PIN("SCK",  PA, PIN_5, AF5),
+				PIN("MISO", PA, PIN_6, AF5),
+				PIN("MOSI", PA, PIN_7, AF5),
+				PIN("CS",   PD, PIN_14, NOAF),
+			},
+			.clocks = {
+				VAL("SCK",  CLK_GPIOA),
+				VAL("MISO", CLK_GPIOA),
+				VAL("MOSI", CLK_GPIOA),
+				VAL("CS",   CLK_GPIOD),
+				VAL("SPI",  CLK_SPI1),
+			}
+		},
+	},
+
+};
+
+struct i2c_conf i2cs[] = {
+	[1] = {
+		.status = ENABLED,
+		.name = "I2C1",
+		.dev = {
+			.irqs = {
+				VAL("EVENT_IRQ", 31),
+				VAL("ERROR_IRQ", 32),
+			},
+			.pins = {
+				PIN("SCL", GPIO_PORT_B, PIN_6, AF4),
+				PIN("SDA", GPIO_PORT_B, PIN_9, AF4),
+			},
+			.clocks = {
+				VAL("SCL", CLK_GPIOB),
+				VAL("SDA", CLK_GPIOB),
+				VAL("I2C", CLK_I2C1),
+			}
+		},
+	},
+	[2] = {
+		.status = DISABLED,
+		.name = "I2C2",
+		.dev = {
+			.irqs = {
+				VAL("EVENT_IRQ", 33),
+				VAL("ERROR_IRQ", 34),
+			},
+			.pins = {
+				PIN("SCL", GPIO_PORT_B, PIN_10, AF4),
+				PIN("SDA", GPIO_PORT_B, PIN_11, AF4),
+			},
+			.clocks = {
+				VAL("SCL", CLK_GPIOB),
+				VAL("SDA", CLK_GPIOB),
+				VAL("I2C", CLK_I2C1),
+			}
+		},
+	},
+
+};
+
+EXPORT_CONFIG(UART(uarts), SPI(spis), I2C(i2cs))
