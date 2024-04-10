cfebf7f6b02d577293ea6aa0e7b10eb0ff3cba25
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-04-10 12:50:27 +0300

    board_config: Add genaration of 'struct field_reg_map'

--- mk/board_conf/gen_board_conf.c
+++ mk/board_conf/gen_board_conf.c
@@ -55,6 +55,41 @@ static int gen_field_int(const char *dev_name,
 	return 0;
 }
 
+static int gen_field_reg_map(const char *dev_name,
+		const char *prop_name, const struct field_reg_map *f) {
+	char buf[128];
+	char def[64];
+	char def_len[64];
+
+	if (!f->name) {
+		return -1;
+	}
+
+	if (!strcmp(f->name, "")) {
+		sprintf(def, "#define CONF_%s_%s",
+					dev_name, prop_name);
+		sprintf(def_len, "#define CONF_%s_%s_LEN",
+					dev_name, prop_name);
+	} else {
+		if (prop_name != NULL && strlen(prop_name) != 0) {
+			sprintf(def, "#define CONF_%s_%s_%s",
+					dev_name, prop_name, f->name);
+			sprintf(def_len, "#define CONF_%s_%s_%s_LEN",
+					dev_name, prop_name, f->name);
+		} else {
+			sprintf(def, "#define CONF_%s_%s",
+					dev_name, f->name);
+			sprintf(def_len, "#define CONF_%s_%s_LEN",
+					dev_name, f->name);
+		}
+	}
+	sprintf(buf, "%-50s %s", def, f->baseaddr);
+	printf("%s\n", buf);
+	sprintf(buf, "%-50s %s", def_len, f->len);
+	printf("%s\n", buf);		
+
+	return 0;
+}
 static int gen_field_func(const char *dev_name,
 		const char *prop_name, const struct field_int *f) {
 	char buf[128];
@@ -124,23 +159,26 @@ static int gen_device_conf(const struct device_conf *dev) {
 
 	gen_dev_enabled(dev->name);
 
+	for (j = 0; j < ARRAY_SIZE(dev->regs); j++) {
+		if (gen_field_reg_map(dev->name, "REGION", &dev->regs[j]) != 0) {
+			break;
+		}
+	}
+
 	for (j = 0; j < ARRAY_SIZE(dev->irqs); j++) {
-		if (gen_field_int(dev->name,
-				"IRQ", &dev->irqs[j]) != 0) {
+		if (gen_field_int(dev->name, "IRQ", &dev->irqs[j]) != 0) {
 			break;
 		}
 	}
 
 	for (j = 0; j < ARRAY_SIZE(dev->pins); j++) {
-		if (gen_field_pin(dev->name,
-				"PIN", &dev->pins[j]) != 0) {
+		if (gen_field_pin(dev->name, "PIN", &dev->pins[j]) != 0) {
 			break;
 		}
 	}
 
 	for (j = 0; j < ARRAY_SIZE(dev->clocks); j++) {
-		if (gen_field_func(dev->name,
-				"CLK_ENABLE", &dev->clocks[j]) != 0) {
+		if (gen_field_func(dev->name, "CLK_ENABLE", &dev->clocks[j]) != 0) {
 			break;
 		}
 	}
