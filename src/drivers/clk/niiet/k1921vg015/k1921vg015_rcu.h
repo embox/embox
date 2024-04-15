f3159b3ddb4bef936b113e2a46f49a0efe14e0ad
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-04-15 19:35:21 +0300

    minor: k1921vg015: Clean k1921vg015_rcu.h

--- src/drivers/clk/niiet/k1921vg015/k1921vg015_rcu.h
+++ src/drivers/clk/niiet/k1921vg015/k1921vg015_rcu.h
@@ -9,8 +9,8 @@
 #ifndef DRIVERS_CLK_NIIET_K1921VG015_H
 #define DRIVERS_CLK_NIIET_K1921VG015_H
 
-extern int niiet_gpio_clock_setup(unsigned char port);
-extern void niiet_uart_set_rcu(int uart_num);
 extern void niiet_sysclk_init(void);
+
+extern int clk_enable(char *clk_name);
 
 #endif
