8d47cfe0e187091fcd5fe9779ee095a2c7acebc5
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-04-16 20:12:44 +0300

    mips: Fix variable name in mm/cache.c

--- src/arch/mips/mm/cache.c
+++ src/arch/mips/mm/cache.c
@@ -2,7 +2,7 @@
  * @file
  *
  * @date Mar 11, 2020
- * @Author Anton Bondarev
+ * @author Anton Bondarev
  */
 
 #include <stddef.h>
@@ -11,12 +11,13 @@
 #include <hal/cache.h>
 
 extern void flush_dcache_range(unsigned long start_addr, unsigned long stop);
-extern void invalidate_dcache_range(unsigned long start_addr, unsigned long stop);
+extern void invalidate_dcache_range(unsigned long start_addr,
+    unsigned long stop);
 
 void dcache_inval(const void *data, size_t size) {
-	invalidate_dcache_range((uintptr_t)(data), (uintptr_t)(p + size));
+	invalidate_dcache_range((uintptr_t)(data), (uintptr_t)(data + size));
 }
 
 void dcache_flush(const void *data, size_t size) {
-	flush_dcache_range((uintptr_t)(data), (uintptr_t)(p + size));
+	flush_dcache_range((uintptr_t)(data), (uintptr_t)(data + size));
 }
