aae0053c4bcba97d9f1c1ef5e368753928693f32
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-04-16 12:53:55 +0300

    board_config: Update board_config/vostok_vg015_dev.conf.h

--- board_config/vostok_vg015_dev.conf.h
+++ board_config/vostok_vg015_dev.conf.h
@@ -1,7 +1,52 @@
 #include <gen_board_conf.h>
 
-#define RCU_BASE       0x3000E000UL
+#define RCU_BASE      (0x3000E000UL)
+#define FLASH_BASE    (0x3000D000UL)
+#define PMUSYS_BASE   (0x3000F000UL)
 
+#define GPIOA_BASE              0x28000000UL
+#define GPIOB_BASE              0x28001000UL
+#define GPIOC_BASE              0x28002000UL
+
+#define UART0_BASE              0x30006000UL
+#define UART1_BASE              0x30007000UL
+#define UART2_BASE              0x30008000UL
+#define UART3_BASE              0x30009000UL
+#define UART4_BASE              0x3000A000UL
+
+
+#define PLIC_NUM_VECTORS 32
+#define PLIC_WDT_VECTNUM        1
+#define PLIC_CAN0_VECTNUM       2
+#define PLIC_CAN1_VECTNUM       3
+#define PLIC_USB_VECTNUM        4
+#define PLIC_GPIO_VECTNUM       5
+#define PLIC_TMR32_VECTNUM      6
+#define PLIC_TMR0_VECTNUM       7
+#define PLIC_TMR1_VECTNUM       8
+#define PLIC_TMR2_VECTNUM       9
+#define PLIC_QSPI0_VECTNUM      10
+#define PLIC_QSPI1_VECTNUM      11
+#define PLIC_QSPI2_VECTNUM      12
+#define PLIC_DMA0_VECTNUM       13
+#define PLIC_DMA1_VECTNUM       14
+#define PLIC_DMA2_VECTNUM       15
+#define PLIC_DMA3_VECTNUM       16
+#define PLIC_DMA4_VECTNUM       17
+#define PLIC_DMA5_VECTNUM       18
+#define PLIC_DMA6_VECTNUM       19
+#define PLIC_DMA7_VECTNUM       20
+#define PLIC_I2C_VECTNUM        21
+#define PLIC_UART0_VECTNUM      22
+#define PLIC_UART1_VECTNUM      23
+#define PLIC_UART2_VECTNUM      24
+#define PLIC_UART3_VECTNUM      25
+#define PLIC_UART4_VECTNUM      26
+#define PLIC_CRYPTOHASH_VECTNUM 27
+#define PLIC_TRNG_VECTNUM       28
+#define PLIC_ADC_SAR_VECTNUM    29
+#define PLIC_CMP_VECTNUM        30
+#define PLIC_PMURTC_VECTNUM     31
 
 struct clk_conf clks[] = {
 	[0] = {
@@ -28,7 +73,7 @@ struct gpio_conf gpios[] = {
 				REGMAP("BASE", (GPIOA_BASE), 0x100),
 			},
 			.irqs = {
-				VAL("", 0),
+				VAL("", PLIC_GPIO_VECTNUM),
 			},
 			.clocks = {
 				VAL("", "CLK_GPIOA"),
@@ -45,7 +90,7 @@ struct gpio_conf gpios[] = {
 				REGMAP("BASE", (GPIOB_BASE), 0x100),
 			},
 			.irqs = {
-				VAL("", 0),
+				VAL("", PLIC_GPIO_VECTNUM),
 			},
 			.clocks = {
 				VAL("", "CLK_GPIOB"),
@@ -62,7 +107,7 @@ struct gpio_conf gpios[] = {
 				REGMAP("BASE", (GPIOC_BASE), 0x100),
 			},
 			.irqs = {
-				VAL("", 0),
+				VAL("", PLIC_GPIO_VECTNUM),
 			},
 			.clocks = {
 				VAL("", "CLK_GPIOC"),
@@ -80,13 +125,13 @@ struct uart_conf uarts[] = {
 		.dev = {
 			.name = "USART0",
 			.regs = {
-				REGMAP("BASE_ADDR", UART0_BASE, 0x100),
+				REGMAP("BASE_ADDR", (UART0_BASE), 0x100),
 			},
 			.irqs = {
-				//VAL("", 37),
+				VAL("", PLIC_UART0_VECTNUM),
 			},
 			.pins = {
-				PIN("TX", PGPIO_PORT_A, 1, 1),
+				PIN("TX", GPIO_PORT_A, 1, 1),
 				PIN("RX", GPIO_PORT_A, 0, 1),
 			},
 			.clocks = {
@@ -101,13 +146,13 @@ struct uart_conf uarts[] = {
 		.dev = {
 			.name = "UART1",
 			.regs = {
-				REGMAP("BASE_ADDR", UART1_BASE, 0x100),
+				REGMAP("BASE_ADDR", (UART1_BASE), 0x100),
 			},
 			.irqs = {
-				//VAL("", 37),
+				VAL("", PLIC_UART1_VECTNUM),
 			},
 			.pins = {
-				//PIN("TX", PGPIO_PORT_A, 1, 1),
+				//PIN("TX", GPIO_PORT_A, 1, 1),
 				//PIN("RX", GPIO_PORT_A, 0, 1),
 			},
 			.clocks = {
@@ -122,13 +167,13 @@ struct uart_conf uarts[] = {
 		.dev = {
 			.name = "UART2",
 			.regs = {
-				REGMAP("BASE_ADDR", UART2_BASE, 0x100),
+				REGMAP("BASE_ADDR", (UART2_BASE), 0x100),
 			},
 			.irqs = {
-				//VAL("", 38),
+				VAL("", PLIC_UART2_VECTNUM),
 			},
 			.pins = {
-				//PIN("TX", PGPIO_PORT_A, 1, 1),
+				//PIN("TX", GPIO_PORT_A, 1, 1),
 				//PIN("RX", GPIO_PORT_A, 0, 1),
 			},
 			.clocks = {
@@ -143,13 +188,13 @@ struct uart_conf uarts[] = {
 		.dev = {
 			.name = "UART3",
 			.regs = {
-				REGMAP("BASE_ADDR", UART3_BASE, 0x100),
+				REGMAP("BASE_ADDR", (UART3_BASE), 0x100),
 			},
 			.irqs = {
-				//VAL("", 39),
+				VAL("", PLIC_UART3_VECTNUM),
 			},
 			.pins = {
-				//PIN("TX", PGPIO_PORT_A, 1, 1),
+				//PIN("TX", GPIO_PORT_A, 1, 1),
 				//PIN("RX", GPIO_PORT_A, 0, 1),
 			},
 			.clocks = {
@@ -164,13 +209,13 @@ struct uart_conf uarts[] = {
 		.dev = {
 			.name = "UART4",
 			.regs = {
-				REGMAP("BASE_ADDR", UART4_BASE, 0x100),
+				REGMAP("BASE_ADDR", (UART4_BASE), 0x100),
 			},
 			.irqs = {
-				//VAL("", 39),
+				VAL("", PLIC_UART4_VECTNUM),
 			},
 			.pins = {
-				//PIN("TX", PGPIO_PORT_A, 1, 1),
+				//PIN("TX", GPIO_PORT_A, 1, 1),
 				//PIN("RX", GPIO_PORT_A, 0, 1),
 			},
 			.clocks = {
@@ -188,13 +233,13 @@ struct spi_conf spis[] = {
 		.dev = {
 			.name = "SPI1",
 			.pins = {
-				PIN("SCK",  PA, PIN_5, AF5),
-				PIN("MISO", PA, PIN_6, AF5),
-				PIN("MOSI", PA, PIN_7, AF5),
-				PIN("CS",   PD, PIN_14, NOAF),
+				// PIN("SCK",  PA, PIN_5, AF5),
+				// PIN("MISO", PA, PIN_6, AF5),
+				// PIN("MOSI", PA, PIN_7, AF5),
+				// PIN("CS",   PD, PIN_14, NOAF),
 			},
 			.clocks = {
-				VAL("SPI",  "CLK_SPI1"),
+				// VAL("SPI",  "CLK_SPI1"),
 			}
 		},
 	},
@@ -208,36 +253,18 @@ struct i2c_conf i2cs[] = {
 		.dev = {
 			.name = "I2C1",
 			.irqs = {
-				VAL("EVENT_IRQ", 31),
-				VAL("ERROR_IRQ", 32),
+				VAL("", PLIC_I2C_VECTNUM),
 			},
 			.pins = {
-				PIN("SCL", GPIO_PORT_B, PIN_6, AF4),
-				PIN("SDA", GPIO_PORT_B, PIN_9, AF4),
+				// PIN("SCL", GPIO_PORT_B, PIN_6, AF4),
+				// PIN("SDA", GPIO_PORT_B, PIN_9, AF4),
 			},
 			.clocks = {
-				VAL("", "CLK_I2C1"),
+				//VAL("", "CLK_I2C1"),
 			}
 		},
 	},
-	[2] = {
-		.status = DISABLED,
-		.name = "I2C2",
-		.dev = {
-			.name = "I2C2",
-			.irqs = {
-				VAL("EVENT_IRQ", 33),
-				VAL("ERROR_IRQ", 34),
-			},
-			.pins = {
-				PIN("SCL", GPIO_PORT_B, PIN_10, AF4),
-				PIN("SDA", GPIO_PORT_B, PIN_11, AF4),
-			},
-			.clocks = {
-				VAL("", "CLK_I2C2"),
-			}
-		},
-	},
+
 };
 
 
