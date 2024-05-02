d466bff44d81ad85ed28057fd8dc2288f8f3261d
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-05-03 01:01:26 +0300

    hal: Clean system.h

--- src/include/hal/system.h
+++ src/include/hal/system.h
@@ -8,15 +8,14 @@
 #ifndef HAL_SYSTEM_H_
 #define HAL_SYSTEM_H_
 
-#include <framework/mod/options.h>
-#include <module/embox/arch/system.h>
-
 #include <stdint.h>
+
+#include <framework/mod/options.h>
+
+#include <module/embox/arch/system.h>
 
 typedef uint64_t freq64_t;
 
-
-#define SYS_CLOCK \
-	OPTION_MODULE_GET(embox__arch__system,NUMBER,core_freq)
+#define SYS_CLOCK OPTION_MODULE_GET(embox__arch__system, NUMBER, core_freq)
 
 #endif /* HAL_SYSTEM_H_ */
