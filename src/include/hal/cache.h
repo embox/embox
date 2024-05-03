f079af2d81844ad4eadd44760997952bbe1bbb91
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-05-03 17:21:58 +0300

    hal: Clean cache.h

--- src/include/hal/cache.h
+++ src/include/hal/cache.h
@@ -1,19 +1,16 @@
-/*
+/**
  * @file
  * @brief
  *
  * @date 12.08.2023
  * @author Denis Deryugin <deryugin.denis@gmail.com>
  */
+#ifndef HAL_CACHE_H_
+#define HAL_CACHE_H_
 
-#ifndef ARCH_DCACHE_H_
-#define ARCH_DCACHE_H_
-
-#ifndef __ASSEMBLER__
 #include <stddef.h>
 
 extern void dcache_inval(const void *data, size_t size);
 extern void dcache_flush(const void *data, size_t size);
-#endif /* __ASSEMBLER__ */
 
-#endif /* ARCH_DCACHE_H_ */
+#endif /* HAL_CACHE_H_ */
