d5f66ef5000527429754ffdfef3be4726d0896a2
Zeng Zixian <sycamoremoon376@outlook.com>
2024-05-17 22:43:37 +0800

    templates: Add a platform stm32/f1/stm32f103rct6

---
+++ board_config/stm32f103rct6.conf.h
@@ -0,0 +1,70 @@
+#include <gen_board_conf.h>
+#include <stm32.h>
+
+struct uart_conf uarts[] = {
+	[1] = {
+		.status = DISABLED,
+		.name = "USART1",
+		.dev = {
+			.name = "USART1",
+			.irqs = {
+				VAL("", 37),
+			},
+			.pins = {
+				PIN("TX", PA, PIN_9, AF7),
+				PIN("RX", PA, PIN_10, AF7),
+			},
+			.clocks = {
+				VAL("TX",   CLK_GPIOA),
+				VAL("RX",   CLK_GPIOA),
+				VAL("UART", CLK_USART1),
+			}
+		},
+		.baudrate = 115200,
+	},
+	[2] = {
+		.status = DISABLED,
+		.name = "USART2",
+		.dev = {
+			.name = "USART2",
+			.irqs = {
+				VAL("", 38),
+			},
+			.pins = {
+				PIN("TX", PA, PIN_2, AF7),
+				PIN("RX", PA, PIN_3, AF7),
+			},
+			.clocks = {
+				VAL("TX",   CLK_GPIOA),
+				VAL("RX",   CLK_GPIOA),
+				VAL("UART", CLK_USART2),
+			}
+		},
+		.baudrate = 115200,
+	},
+};
+
+struct spi_conf spis[] = {
+	[1] = {
+		.status = DISABLED,
+		.name = "SPI1",
+		.dev = {
+			.name = "SPI1",
+			.pins = {
+				PIN("SCK",  GPIO_PORT_A, PIN_5, AF5),
+				PIN("MISO", GPIO_PORT_A, PIN_6, AF5),
+				PIN("MOSI", GPIO_PORT_A, PIN_7, AF5),
+				PIN("CS",   GPIO_PORT_A, PIN_4, NOAF),
+			},
+			.clocks = {
+				VAL("SCK",  CLK_GPIOA),
+				VAL("MISO", CLK_GPIOA),
+				VAL("MOSI", CLK_GPIOA),
+				VAL("CS",   CLK_GPIOA),
+				VAL("SPI",  CLK_SPI1),
+			}
+		},
+	},
+};
+
+EXPORT_CONFIG(UART(uarts), SPI(spis))
---
+++ platform/stm32/templates/f1/stm32f103rct6/board.conf.h
@@ -0,0 +1,6 @@
+#include <stm32f103_bluepill.conf.h>
+
+CONFIG {
+	uarts[1].status = ENABLED;
+	uarts[2].status = DISABLED;
+}
