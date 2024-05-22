16e345af6db4373fd57c23e6c6130f57307edd70
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-05-22 10:06:32 +0300

    mips: Fix setjmp

--- src/arch/mips/include/asm/setjmp.h
+++ src/arch/mips/include/asm/setjmp.h
@@ -1,40 +1,39 @@
 /**
  * @file
- *
  * @brief
  *
  * @date 22.06.2012
  * @author Anton Bondarev
  */
 
-#ifndef MIPS_SETJMP_H_
-#define MIPS_SETJMP_H_
+#ifndef MIPS_ASM_SETJMP_H_
+#define MIPS_ASM_SETJMP_H_
+
+#include <asm/asm.h>
 
 #ifndef __ASSEMBLER__
+
 typedef struct {
-	unsigned int s[8];     /* static registers */
-	unsigned int fp;       /* frame pointer */
-	unsigned int ra;       /* return address */
-	unsigned int gp;       /* global pointer */
-	unsigned int sp;       /* stack pointer */
+	unsigned long s[8]; /* static registers */
+	unsigned long fp;   /* frame pointer */
+	unsigned long ra;   /* return address */
+	unsigned long gp;   /* global pointer */
+	unsigned long sp;   /* stack pointer */
 } __jmp_buf[1];
 
-#else
+#endif /* !__ASSEMBLER__ */
 
-#define SETJMP_S0 0x00
-#define SETJMP_S1 0x04
-#define SETJMP_S2 0x08
-#define SETJMP_S3 0x0C
-#define SETJMP_S4 0x10
-#define SETJMP_S5 0x14
-#define SETJMP_S6 0x18
-#define SETJMP_S7 0x1C
-#define SETJMP_FP 0x20
-#define SETJMP_RA 0x24
-#define SETJMP_GP 0x28
-#define SETJMP_SP 0x2C
+#define SETJMP_S0 (LONGSIZE * 0)
+#define SETJMP_S1 (LONGSIZE * 1)
+#define SETJMP_S2 (LONGSIZE * 2)
+#define SETJMP_S3 (LONGSIZE * 3)
+#define SETJMP_S4 (LONGSIZE * 4)
+#define SETJMP_S5 (LONGSIZE * 5)
+#define SETJMP_S6 (LONGSIZE * 6)
+#define SETJMP_S7 (LONGSIZE * 7)
+#define SETJMP_FP (LONGSIZE * 8)
+#define SETJMP_RA (LONGSIZE * 9)
+#define SETJMP_GP (LONGSIZE * 10)
+#define SETJMP_SP (LONGSIZE * 11)
 
-
-#endif
-
-#endif /* MIPS_SETJMP_H_ */
+#endif /* MIPS_ASM_SETJMP_H_ */
