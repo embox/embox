9ae782ab0a2b92b2a0ce976e6843b107c0d36ba1
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-05-23 13:07:31 +0300

    drivers: interrupt: Improve mips_intc

--- src/drivers/interrupt/mips_intc/mips_intc.c
+++ src/drivers/interrupt/mips_intc/mips_intc.c
@@ -1,34 +1,62 @@
 /**
  * @file
- *
  * @brief MIPS build-in interrupt controller
  *
  * @date 04.07.2012
  * @author Anton Bondarev
  */
+
 #include <assert.h>
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #include <asm/mipsregs.h>
-
 #include <drivers/irqctrl.h>
+#include <kernel/irq.h>
 
-void irqctrl_enable(unsigned int interrupt_nr) {
+void irqctrl_enable(unsigned int irq) {
 	uint32_t c0;
 
+	assert(irq_nr_valid(irq));
+
 	c0 = mips_read_c0_status();
-	c0 |= 1 << (interrupt_nr + ST0_IRQ_MASK_OFFSET);
+	c0 |= 1U << (irq + ST0_IRQ_MASK_OFFSET);
 	mips_write_c0_status(c0);
 }
 
-void irqctrl_disable(unsigned int interrupt_nr) {
+void irqctrl_disable(unsigned int irq) {
 	uint32_t c0;
 
+	assert(irq_nr_valid(irq));
+
 	c0 = mips_read_c0_status();
-	c0 &= ~(1 << (interrupt_nr + ST0_IRQ_MASK_OFFSET));
+	c0 &= ~(1U << (irq + ST0_IRQ_MASK_OFFSET));
 	mips_write_c0_status(c0);
+}
+
+void irqctrl_force(unsigned int irq) {
+}
+
+int irqctrl_pending(unsigned int irq) {
+	return 0;
+}
+
+void irqctrl_eoi(unsigned int irq) {
+}
+
+unsigned int irqctrl_get_intid(void) {
+	unsigned int irq;
+	uint32_t pending;
+
+	pending = (mips_read_c0_cause() & CAUSE_IM) >> ST0_IRQ_MASK_OFFSET;
+
+	for (irq = 0; irq < IRQCTRL_IRQS_TOTAL; irq++) {
+		if (pending & (1U << irq)) {
+			return irq;
+		}
+	}
+
+	return -1;
 }
 
 IRQCTRL_DEF(mips_intc, NULL);
-
