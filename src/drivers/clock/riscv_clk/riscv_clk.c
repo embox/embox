eb8cb8422331a8351d807d5574c91ff0bcec4ebe
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-05-24 11:47:41 +0300

    drivers: clock: Improve riscv_clk

--- src/drivers/clock/riscv_clk/riscv_clk.c
+++ src/drivers/clock/riscv_clk/riscv_clk.c
@@ -8,36 +8,36 @@
  */
 
 #include <errno.h>
+#include <stdint.h>
 
-#include <asm/regs.h>
 #include <asm/interrupts.h>
-
+#include <asm/regs.h>
+#include <framework/mod/options.h>
 #include <hal/clock.h>
-#include <hal/system.h>
 #include <hal/reg.h>
+#include <hal/system.h>
 #include <kernel/irq.h>
 #include <kernel/time/clock_source.h>
+#include <kernel/time/time.h>
 
-#include <framework/mod/options.h>
+#define COUNT_OFFSET  (RTC_CLOCK / JIFFIES_PERIOD)
+#define RTC_CLOCK     OPTION_GET(NUMBER, rtc_freq)
 
-#define HZ 1000
-#define COUNT_OFFSET (RTC_CLOCK / HZ)
-#define RTC_CLOCK    OPTION_GET(NUMBER, rtc_freq)
-
-#define MTIME        OPTION_GET(NUMBER, base_mtime)
-#define MTIMECMP     OPTION_GET(NUMBER, base_mtimecmp)
+#define MTIME         OPTION_GET(NUMBER, base_mtime)
+#define MTIMECMP      OPTION_GET(NUMBER, base_mtimecmp)
 
 #define OPENSBI_TIMER 0x54494D45
 
 static int clock_handler(unsigned int irq_nr, void *dev_id) {
 #if SMODE
-	register uintptr_t a7 asm ("a7") = (uintptr_t)(OPENSBI_TIMER);
-	register uintptr_t a6 asm ("a6") = (uintptr_t)(0);
-	register uintptr_t a0 asm ("a0") = 0;
-	asm volatile ("rdtime a0");
+	register uintptr_t a7 asm("a7") = (uintptr_t)(OPENSBI_TIMER);
+	register uintptr_t a6 asm("a6") = (uintptr_t)(0);
+	register uintptr_t a0 asm("a0") = 0;
+	asm volatile("rdtime a0");
 	a0 = a0 + COUNT_OFFSET;
-	(void)a7; (void)a6;
-	asm volatile ("ecall");
+	(void)a7;
+	(void)a6;
+	asm volatile("ecall");
 #else
 	REG64_STORE(MTIMECMP, REG64_LOAD(MTIME) + COUNT_OFFSET);
 #endif
@@ -49,13 +49,14 @@ static int clock_handler(unsigned int irq_nr, void *dev_id) {
 
 static int riscv_clock_setup(struct clock_source *cs) {
 #if SMODE
-	register uintptr_t a7 asm ("a7") = (uintptr_t)(OPENSBI_TIMER);
-	register uintptr_t a6 asm ("a6") = (uintptr_t)(0);
-	register uintptr_t a0 asm ("a0") = 0;
-	asm volatile ("rdtime a0");
+	register uintptr_t a7 asm("a7") = (uintptr_t)(OPENSBI_TIMER);
+	register uintptr_t a6 asm("a6") = (uintptr_t)(0);
+	register uintptr_t a0 asm("a0") = 0;
+	asm volatile("rdtime a0");
 	a0 = a0 + COUNT_OFFSET;
-	(void)a7; (void)a6;
-	asm volatile ("ecall");
+	(void)a7;
+	(void)a6;
+	asm volatile("ecall");
 #else
 	REG64_STORE(MTIMECMP, REG64_LOAD(MTIME) + COUNT_OFFSET);
 #endif
@@ -65,13 +66,12 @@ static int riscv_clock_setup(struct clock_source *cs) {
 	return ENOERR;
 }
 
-static struct time_event_device riscv_event_device  = {
-	.set_periodic = riscv_clock_setup,
-	.name = "riscv_clk",
-	.irq_nr = IRQ_TIMER
+static struct time_event_device riscv_event_device = {
+    .set_periodic = riscv_clock_setup,
+    .name = "riscv_clk",
+    .irq_nr = IRQ_TIMER,
 };
 
-CLOCK_SOURCE_DEF(riscv_clk, NULL, NULL,
-	&riscv_event_device, NULL);
+CLOCK_SOURCE_DEF(riscv_clk, NULL, NULL, &riscv_event_device, NULL);
 
 RISCV_TIMER_IRQ_DEF(clock_handler, &CLOCK_SOURCE_NAME(riscv_clk));
