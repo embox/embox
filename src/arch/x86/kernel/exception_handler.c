b3bc1a3eb173d9d8c29b3af46b52463aa1b9a457
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-06-06 11:01:41 +0300

    x86: Fix print specifiers

--- src/arch/x86/kernel/exception_handler.c
+++ src/arch/x86/kernel/exception_handler.c
@@ -7,32 +7,42 @@
  * @author Anton Bondarev
  */
 
-#include <asm/traps.h>
-#include <asm/ptrace.h>
-#include <asm/linkage.h>
-#include <kernel/panic.h>
-#include <kernel/irq.h>
+#include <inttypes.h>
 #include <stddef.h>
 
 #include <asm/hal/env/traps_core.h>
+#include <asm/linkage.h>
+#include <asm/ptrace.h>
+#include <asm/traps.h>
+#include <kernel/irq.h>
+#include <kernel/panic.h>
 
 __trap_handler __exception_table[0x20];
 
 fastcall void exception_handler(pt_regs_t *st) {
-	if(NULL != __exception_table[st->trapno]) {
+	if (NULL != __exception_table[st->trapno]) {
 		__exception_table[st->trapno](st->trapno, st);
 		return;
 	}
 
-	panic("EXCEPTION [0x%x]: error = %08x\n"
-		"EAX=%08x    EBX=%08x ECX=%08x EDX=%08x\n"
-		" GS=%08x     FS=%08x  ES=%08x  DS=%08x\n"
-		"EDI=%08x    ESI=%08x EBP=%08x EIP=%08x\n"
-		" CS=%08x EFLAGS=%08x ESP=%08x  SS=%08x\n",
-		st->trapno, st->err,
-		st->eax, st->ebx, st->ecx, st->edx,
-		st->gs, st->fs, st->es, st->ds,
-		st->edi, st->esi, st->ebp, st->eip,
-		st->cs, st->eflags, st->esp, st->ss
-	);
+	panic("EXCEPTION [0x%" PRIx32 "]: error = %08" PRIx32 "\n"
+	      "EAX    =%" PRIx32 "\n"
+	      "EBX    =%" PRIx32 "\n"
+	      "ECX    =%" PRIx32 "\n"
+	      "EDX    =%" PRIx32 "\n"
+	      "GS     =%" PRIx32 "\n"
+	      "FS     =%" PRIx32 "\n"
+	      "ES     =%" PRIx32 "\n"
+	      "DS     =%" PRIx32 "\n"
+	      "EDI    =%" PRIx32 "\n"
+	      "ESI    =%" PRIx32 "\n"
+	      "EBP    =%" PRIx32 "\n"
+	      "EIP    =%" PRIx32 "\n"
+	      "CS     =%" PRIx32 "\n"
+	      "EFLAGS =%" PRIx32 "\n"
+	      "ESP    =%" PRIx32 "\n"
+	      "SS     =%" PRIx32 "\n",
+	    st->trapno, st->err, st->eax, st->ebx, st->ecx, st->edx, st->gs, st->fs,
+	    st->es, st->ds, st->edi, st->esi, st->ebp, st->eip, st->cs, st->eflags,
+	    st->esp, st->ss);
 }
