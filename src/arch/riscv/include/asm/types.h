567820e262c4f19676124f2340a88b2833fab223
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-06-23 15:15:02 +0300

    compat: Improve linux headers

--- src/arch/riscv/include/asm/types.h
+++
@@ -1,6 +0,0 @@
-#ifndef RISCV_TYPES_H_
-#define RISCV_TYPES_H_
-
-#include <asm-generic/types32.h>
-
-#endif /* RISCV_TYPES_H_ */
---
+++ src/compat/linux/include/asm/bitsperlong.h
@@ -0,0 +1,15 @@
+/**
+ * @brief
+ *
+ * @author Aleksey Zhmulin
+ * @date 22.06.24
+ */
+
+#ifndef COMPAT_LINUX_ASM_BITSPERLONG_H_
+#define COMPAT_LINUX_ASM_BITSPERLONG_H_
+
+#include <limits.h>
+
+#define __BITS_PER_LONG LONG_BIT
+
+#endif /* COMPAT_LINUX_ASM_BITSPERLONG_H_ */
