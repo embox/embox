e21efdb1f5f4a7aeccb83c52492822988632bc8c
Ankith Veldandi <143205588+aveldan@users.noreply.github.com>
2024-07-07 12:51:28 -0400

    Added tests for math.h fabs, floor, round and tanh

---
+++ src/compat/libc/math/tests/round_tests.c
@@ -0,0 +1,42 @@
+/**
+ * @file
+ *
+ * @date July 07, 2024
+ * @author Ankith Veldandi
+ */
+
+
+
+#include <embox/test.h>
+#include <math.h>
+
+EMBOX_TEST_SUITE("round() tests");
+
+
+TEST_CASE("Test for round(0.23)") {
+	test_assert(round(0.23) == 0.0);
+}
+
+TEST_CASE("Test for round(0.73)") {
+	test_assert(round(0.73) == 1.0);
+}
+
+TEST_CASE("Test for round(0.0)") {
+	test_assert(round(0.0) == 0.0);
+}
+
+TEST_CASE("Test for round(3.5)") {
+	test_assert(round(3.5) == 4.0);
+}
+
+TEST_CASE("Test for round(-3.5)") {
+	test_assert(round(-3.5) == -4.0);
+}
+
+TEST_CASE("Test for round(+INFINITY)") {
+	test_assert(isinf(round(INFINITY)));
+}
+
+TEST_CASE("Test for round(NAN)") {
+	test_assert(isnan(round(NAN)));
+}
