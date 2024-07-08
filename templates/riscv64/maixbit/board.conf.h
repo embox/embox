6c1a8ccf361ecd683d31faab9e7b1dfb8b20ea5c
loharmurtaza <loharmurtaza788@gmail.com>
2024-07-08 17:04:03 +0300

    updated maix_bit gpio drivers

---
+++ templates/riscv64/maixbit/board.conf.h
@@ -0,0 +1,11 @@
+#include <maix_bit_dev.conf.h>
+
+CONFIG {
+	// uarts[0].status = ENABLED;
+	// uarts[1].status = ENABLED;
+	// uarts[2].status = DISABLED;
+	// uarts[3].status = DISABLED;
+	// uarts[4].status = DISABLED;
+
+//	spis[1].status = ENABLED;
+}
