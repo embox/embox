4b6ba703e4408a750608dad541482565d89a8224
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-07-09 17:06:20 +0300

    drivers: Add clk/mikron_pm clk driver

---
+++ src/drivers/clk/mikron/mikron_pm.h
@@ -0,0 +1,16 @@
+/**
+ * @file
+ *
+ * @author Anton Bondarev
+ *
+ * @date 08.07.2024
+ */
+
+#ifndef DRIVERS_CLK_MICRON_PM_H
+#define DRIVERS_CLK_MICRON_PM_H
+
+extern void mikron_pm_init(void);
+
+extern int clk_enable(char *clk_name);
+
+#endif /* DRIVERS_CLK_MICRON_PM_H */
