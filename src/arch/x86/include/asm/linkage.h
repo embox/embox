0d485ec2efd4b931994e940d5ff921bdabdd2e6b
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-07-11 16:23:27 +0300

    x86: Fix after multiboot SMP modification

--- src/arch/x86/include/asm/linkage.h
+++ src/arch/x86/include/asm/linkage.h
@@ -5,20 +5,29 @@
  * @author: Anton Bondarev
  */
 
-#include <asm/gdt.h>
-#include <framework/mod/options.h>
-#include <module/embox/arch/x86/kernel/smp.h>
+//#include <asm/gdt.h>
+
 
 #ifndef LINKAGE_H_
 #define LINKAGE_H_
+
+#include <module/embox/arch/cpu.h>
+#include <module/embox/arch/smp.h>
+
+#if !defined(NOSMP)
+
+#include <framework/mod/options.h>
+#include <module/embox/arch/x86/kernel/smp.h>
+
+#define MAX_SUPPORTED_CORES OPTION_MODULE_GET(embox__arch__x86__kernel__smp, NUMBER, max_supported_cores)
+
+#endif /* !defined(NOSMP) */
 
 #ifdef __ASSEMBLER__
 
 #define C_ENTRY(name) .globl name; .align 2; name
 
 #define C_LABEL(name) .globl name; name
-
-#define MAX_SUPPORTED_CORES OPTION_MODULE_GET(embox__arch__x86__kernel__smp, NUMBER, max_supported_cores)
 
 #else
 
