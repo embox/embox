ebbe4653bed720860c8ce95ff46847e09f9b6a02
loharmurtaza <loharmurtaza788@gmail.com>
2024-07-15 17:29:18 +0300

    Separate clk drivers for MAiX BiT board.

---
+++ src/drivers/clk/maix/maix_bit/maix_bit_sysctl.h
@@ -0,0 +1,12 @@
+/**
+ * @file Clock
+ * @author Ghulam Murtaza
+ * @date 10.07.2024
+ */
+
+#ifndef DRIVERS_CLK_MAIX_BIT_H
+#define DRIIVERS_CLK_MAIX_BIT_H
+
+extern int clk_enable(char *clk_name);
+
+#endif
