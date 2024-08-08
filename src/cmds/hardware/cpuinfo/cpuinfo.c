5dbf0e24c063d9478e6721b32e6816f7bd447832
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-08-08 14:30:54 +0300

    minor: cmds: Clean cmds/hardware/cpuinfo/cpuinfo

--- src/cmds/hardware/cpuinfo/cpuinfo.c
+++ src/cmds/hardware/cpuinfo/cpuinfo.c
@@ -13,13 +13,21 @@
 #include <lib/libcpu_info.h>
 
 int main(int argc, char **argv) {
-	struct cpu_info *info = get_cpu_info();
+	uint64_t cpu_cnt;
+	struct cpu_info *info;
+
+	info = get_cpu_info();
+
 	printf("%-20s %s\n", "CPU Vendor ID ", info->vendor_id);
 	
 	for(int i = 0; i < info->feature_count; i++) {
 		printf("CPU %-16s %u\n", info->feature[i].name, info->feature[i].val);
 	}
-	
-	printf("Current time stamp counter: %" PRId64 "\n", get_cpu_counter());
+
+	cpu_cnt = get_cpu_counter();
+	if (cpu_cnt) {
+		printf("Current time stamp counter: %" PRId64 "\n", cpu_cnt);
+	}
+
 	return 0;
 }
