24bf1458eb3458f19d32d79b27da709d6474c5c6
Zeng Zixian <sycamoremoon376@outlook.com>
2024-08-11 11:47:56 +0800

    tests:smp: Add smp test cases
    test_smp module is currently a testing command for printing characters,
    which will be improve in funture.

---
+++ src/tests/smp/simult_thread_test.c
@@ -0,0 +1,82 @@
+/**
+ * @file
+ *
+ * @brief
+ *
+ * @date 2024.8.7
+ * @author Zeng Zixian
+ */
+
+#include <embox/test.h>
+
+#include <util/log.h>
+
+#include <asm/ap.h>
+
+#include <kernel/thread.h>
+#include <kernel/task.h>
+#include <kernel/thread/thread_stack.h>
+
+#include <module/embox/kernel/stack.h>
+
+#define MAX_TRYTIMES    ((unsigned int)(-1) - 1)
+
+EMBOX_TEST_SUITE("test simultaneous thread on CPUs");
+
+static unsigned int try_time = 0;
+static unsigned int thread_status[4] = {0};
+
+static int search_threads(void) {
+	struct thread * th;
+	struct task *task;
+	int ret = 0;
+
+	task_foreach(task) {
+		task_foreach_thread(th, task) {
+			if(th->schedee.active == 1 && th != thread_self()){
+				ret = 1;
+				thread_status[0] = th->schedee.active;
+				thread_status[1] = th->schedee.ready;
+				thread_status[2] = th->schedee.waiting;
+				thread_status[3] = (unsigned int)th;
+				return ret;
+			}
+		}
+	}
+
+	return ret;
+}
+
+
+void * test_thread_1(void *arg){
+	struct thread * self = thread_self();
+	int founded = 0;
+	test_assert(self->schedee.active);
+
+	while(try_time++ < MAX_TRYTIMES) {
+		founded = search_threads();
+		if(founded) break;
+	}
+
+	log_debug("found %d try_time %u",founded,try_time);
+	if(founded){
+		log_debug("Self thread %#x A %u R %u W %u", (unsigned int)self, self->schedee.active, self->schedee.ready, self->schedee.waiting);
+		log_debug("Found thread %#x A %u R %u W %u", thread_status[3], thread_status[0], thread_status[1], thread_status[2]);
+	}
+
+	test_assert_not_zero(founded);
+	return 0;
+}
+
+void * test_thread_2(void * arg){
+	test_assert_zero(thread_join((struct thread *)arg,NULL));
+	return 0;
+}
+
+
+TEST_CASE("two threads exsit simultaneously") {
+	struct thread * thread1, * thread2;
+	thread1 = thread_create(THREAD_FLAG_PRIORITY_HIGHER,test_thread_1,NULL);
+	thread2 = thread_create(0,test_thread_2,thread1);
+	thread_join(thread2,NULL);
+}
--- src/tests/smp/test_smp.c
+++ src/tests/smp/test_smp.c
@@ -1,7 +1,10 @@
 #include <kernel/thread.h>
 #include <kernel/cpu/cpu.h>
 #include <kernel/spinlock.h>
+#include <kernel/thread/thread_alloc.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 static volatile int shared_counter = 0;
 static spinlock_t lock = SPIN_STATIC_UNLOCKED;
@@ -27,4 +30,52 @@ void start_thread_on_cpu(int cpu_id) {
 
     cpu_bind(cpu_id, t);
     thread_launch(t);
+}
+
+int count, all_chars;
+char b = '\n';
+void * thd_1(void *arg){
+	for(int i = 0;count < all_chars; i++){
+		if((i % 10000000) == 0){
+			count++;
+			if(count % 70 == 0) write(2,&b,1);
+
+			write(2,(char *)arg,1);
+		}
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	struct thread * tid1, *tid2, *tid3, *tid4, *tid5;
+	char string[]="0123456789";
+
+	if(argc > 1){
+		all_chars = atoi(argv[1]);
+		count = 0;
+	} else{
+		all_chars = 200;
+		count = 0;
+	}
+
+	sched_lock();
+	tid1 = thread_create(0,thd_1,string+1);
+	tid2 = thread_create(0,thd_1,string+2);
+	tid3 = thread_create(0,thd_1,string+3);
+	tid4 = thread_create(0,thd_1,string+4);
+	tid5 = thread_create(0,thd_1,string+5);
+
+	sched_affinity_set(&tid1->schedee.affinity,1);
+	sched_affinity_set(&tid2->schedee.affinity,1);
+	sched_affinity_set(&tid3->schedee.affinity,1);
+	sched_affinity_set(&tid4->schedee.affinity,2);
+	sched_affinity_set(&tid5->schedee.affinity,2);
+
+	sched_unlock();
+
+	thread_join(tid1,NULL);
+	thread_join(tid2,NULL);
+	thread_join(tid3,NULL);
+	thread_join(tid4,NULL);
+	thread_join(tid5,NULL);
 }
