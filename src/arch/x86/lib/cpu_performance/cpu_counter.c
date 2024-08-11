d72de202b5a7f73bb4fd191ac8364556db819f22
Zeng Zixian <sycamoremoon376@outlook.com>
2024-08-11 21:14:56 +0800

    arch:x86: Update the info printed by cpu_info cmd
    Add information of CPU numbers
    Fix the time stamp of tsc clock
    Need to check max_cpuid_leaf before printing information

--- src/arch/x86/lib/cpu_performance/cpu_counter.c
+++ src/arch/x86/lib/cpu_performance/cpu_counter.c
@@ -14,7 +14,8 @@ uint64_t get_cpu_counter(void) {
 	uint32_t hi = 0, lo = 0;
 	asm volatile (	"rdtsc\n\t"
 					"movl %%eax, %0\n\t"
-					"movl %%edx, %1\n\t" :
-					"=r"(lo), "=r"(hi) :);
-	return (((uint64_t) hi) << 32) + lo;
+					"movl %%edx, %1\n\t"
+					: "=a"(lo), "=d"(hi)
+					: );
+	return (((uint64_t) hi) << 32) | (uint64_t)lo;
 }
--- src/arch/x86/lib/cpu_performance/cpu_info.c
+++ src/arch/x86/lib/cpu_performance/cpu_info.c
@@ -10,39 +10,65 @@
 #include <string.h>
 #include <assert.h>
 #include <lib/libcpu_info.h>
+#include <hal/cpu.h>
 
 struct cpu_info current_cpu;
+static uint32_t max_cpuid_leaf;
+
+static uint32_t check_max_cpuid_leaf(void) {
+	uint32_t max_leaf;
+
+	asm volatile (	"xorl %%eax, %%eax	\n\t"
+					"cpuid				\n\t"
+					"movl %%eax, %0		\n\t"
+					: "=a" (max_leaf)
+					: );
+	return max_leaf;
+}
 
 static void set_cpu_features(struct cpu_info *info) {
 	uint32_t reg; /* register: edx */
 
-	asm volatile (	"movl $0x1, %%eax   \n\t"
-					"cpuid				\n\t"
-					"movl %%edx, %0		\n\t"
-					: "=g"(reg)
-					:
-					: "%eax", "%ebx", "%ecx", "%edx");
-	
-	set_feature_val(info, "FPU", reg & 0x1);
-	set_feature_val(info, "MMU", (reg >> 23) & 0x1);
-	set_feature_val(info, "SSE", (reg >> 25) & 0x1);
-	return;
+	/* 0x1 is initial EAX number */
+	if(max_cpuid_leaf < 0x1){
+		set_feature_strval(info, "FPU", "NaN");
+		set_feature_strval(info, "MMU", "NaN");
+		set_feature_strval(info, "SSE", "NaN");
+		return;
+	} else {
+		asm volatile (	"movl $0x1, %%eax   \n\t"
+						"cpuid				\n\t"
+						"movl %%edx, %0		\n\t"
+						: "=d"(reg)
+						: );
+
+		set_feature_val(info, "FPU", reg & 0x1);
+		set_feature_val(info, "MMU", (reg >> 23) & 0x1);
+		set_feature_val(info, "SSE", (reg >> 25) & 0x1);
+		return;
+	}
 }
 
 static void set_cpu_freq(struct cpu_info *info) {
 	uint32_t reg[2]; /* registers: eax, ebx */
 
-	asm volatile (	"movl $0x16, %%eax   \n\t"
-					"cpuid				\n\t"
-					"movl %%eax, %0		\n\t"
-					"movl %%ebx, %1		\n\t"
-					: "=g"(reg[0]), "=g"(reg[1])
-					:
-					: "%eax", "%ebx", "%ecx", "%edx");
-	
-	set_feature_val(info, "Base Freq", reg[0]);
-	set_feature_val(info, "Max Freq", reg[1]);
-	return;
+	/* 0x16 is initial EAX number */
+	if(max_cpuid_leaf < 0x16){
+		set_feature_strval(info, "Base Freq", "NaN");
+		set_feature_strval(info, "Max Freq", "NaN");
+		return;
+	} else {
+		asm volatile (	"movl $0x16, %%eax   \n\t"
+						"cpuid				\n\t"
+						"movl %%eax, %0		\n\t"
+						"movl %%ebx, %1		\n\t"
+						: "=a"(reg[0]), "=b"(reg[1])
+						: );
+
+		set_feature_val(info, "Base Freq", reg[0]);
+		set_feature_val(info, "Max Freq", reg[1]);
+		return;
+	}
 }
 
 static void set_vendor_id(struct cpu_info *info) {
@@ -61,26 +87,18 @@ static void set_vendor_id(struct cpu_info *info) {
 					"xorl %%ebx, %%eax\n\t"
 					"movl %%eax, %0"
 					: "=r"(a)
-					: );*/
+					: );
+	*/
 
 	/* Getting vendor id */
-	/* For some mysterious reason I can't include all three registers
-	 * EBX, ECX, EDX to clobber list, so I have to call CPUID twice.
-	 */
+	/* Leaf 0x0 is supported in all CPUs, No need to test max_cpuid_leaf */
+
 	asm volatile (	"xorl %%eax, %%eax	\n\t"
 					"cpuid				\n\t"
 					"movl %%ebx, %0		\n\t"
 					"movl %%ecx, %1		\n\t"
-					: "=g"(r[0]), "=g"(r[2])
-					:
-					: "%ebx", "%ecx");
-
-	asm volatile (	"xorl %%eax, %%eax	\n\t"
-					"cpuid				\n\t"
-					"movl %%edx, %0		\n\t"
-					: "=g"(r[1])
-					:
-					: "%edx");
+					: "=b"(r[0]), "=d"(r[1]), "=c"(r[2])
+					: );
 
 	/* Parsing registers */
 	for (i = 0; i < 3; i++) {
@@ -92,10 +110,17 @@ static void set_vendor_id(struct cpu_info *info) {
 	return;
 }
 
+static void set_cpu_number(struct cpu_info *info) {
+	set_feature_val(info, "CPU NUM", NCPU);
+	return;
+}
+
 struct cpu_info* get_cpu_info(void) {
 	current_cpu.feature_count = 0;
+	max_cpuid_leaf = check_max_cpuid_leaf();
 	set_vendor_id(&current_cpu);
 	set_cpu_freq(&current_cpu);
 	set_cpu_features(&current_cpu);
+	set_cpu_number(&current_cpu);
 	return &current_cpu;
 }
