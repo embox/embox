1ab77138eb06fd2e2eb7ba48d75a6640f303dce2
Zeng Zixian <sycamoremoon376@outlook.com>
2024-08-24 21:10:42 +0800

    arch:riscv: Implement cpu_get_id in riscv
    This version of cpu_get_id should be called in M-mode

---
+++ src/arch/riscv/kernel/smp/cpu_get_id.c
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 21.08.2024
+ * @author Zeng Zixian
+ */
+
+#include <hal/cpu.h>
+#include <asm/entry.h>
+#include <asm/interrupts.h>
+#include <asm/ptrace.h>
+#include <asm/regs.h>
+
+#define INVALIDHARTID (unsigned int)(-1)
+static unsigned int __cpuid_to_hartid_map[NCPU] = {
+	[0] = 0, /* BSP always has mhartid and cpuid 0 */
+	[1 ... NCPU-1] = INVALIDHARTID
+};
+
+/**
+ * Since mhartids are not guaranteed to be consecutive
+ * so each AP needs to register mhartid to cpuid at boot time
+ *
+ * return cpuid
+ */
+unsigned int cpuid_register(void) {
+	int cpuid = 1; /* skip BSP */
+	unsigned int mhartid = read_mhartid();
+	for(;cpuid < NCPU; cpuid++) {
+		if(__cpuid_to_hartid_map[cpuid] == INVALIDHARTID) {
+			__cpuid_to_hartid_map[cpuid] = mhartid;
+			break;
+		}
+	}
+	return cpuid;
+}
+
+unsigned int cpu_get_id(void) {
+	int cpuid = 0;
+	unsigned int mhartid = read_mhartid();
+	for(cpuid = 0; cpuid < NCPU; cpuid++) {
+		if(__cpuid_to_hartid_map[cpuid] == mhartid)
+			break;
+	}
+	return cpuid;
+}
