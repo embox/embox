51c8cd64c79faf9a8fd43af0fc6199a52359b1b7
Diamond Rivero <diamant3@proton.me>
2024-10-16 20:42:55 +0800

    add math testsuite: copysign

---
+++ src/compat/libc/math/tests/copysign_test.c
@@ -0,0 +1,44 @@
+/**
+ * @file
+ *
+ * @date October 16, 2024
+ * @author Diamond Rivero
+ */
+
+#include <math.h>
+
+#include <embox/test.h>
+
+EMBOX_TEST_SUITE("copysign() tests");
+
+TEST_CASE("Test for copysign(+1.0, +2.0)") {
+	test_assert(copysign(1.0, 2.0) == 1.0);
+}
+
+TEST_CASE("Test for copysign(+1.0, -2.0)") {
+	test_assert(copysign(1.0, -2.0) == -1.0);
+}
+
+TEST_CASE("Test for copysign(-10.0, -1.0)") {
+	test_assert(copysign(-10.0, -1.0) == -10.0);
+}
+
+TEST_CASE("Test for copysign(-10.0, +1.0)") {
+	test_assert(copysign(-10.0, 1.0) == 10.0);
+}
+
+TEST_CASE("Test for copysign(+8.0, -1.0)") {
+	test_assert(copysign(8.0, -1.0) == -8.0);
+}
+
+TEST_CASE("Test for copysign(+INFINITY, -2.0)") {
+	test_assert(copysign(INFINITY, -2.0) == -INFINITY);
+}
+
+TEST_CASE("Test for copysign(NAN, -2.0)") {
+	test_assert(copysign(NAN, -2.0) == -NAN);
+}
+
+TEST_CASE("Test for copysign(2.0, -0.0)") {
+	test_assert(copysign(2.0, -0.0) == -2.0);
+}
