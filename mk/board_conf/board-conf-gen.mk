9cf0b70dca8ac65bbd6b7532a82466db3420277b
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-11-07 00:34:32 +0300

    mk: Add board config for external projects

--- mk/board_conf/board-conf-gen.mk
+++ mk/board_conf/board-conf-gen.mk
@@ -1,15 +1,22 @@
 
-HOSTCC  = cc
+HOSTCC := cc
 
-BOARD_CONF_DIR := board_config/
-BOARD_CONF := $(CONF_DIR)/board.conf.h
+BOARD_CONF         := $(CONF_DIR)/board.conf.h
+BOARD_CONF_DIR     := $(ROOT_DIR)/board_config
 BOARD_CONF_GEN_DIR := $(ROOT_DIR)/mk/board_conf
+BOARD_CONF_EXT_DIR := $(EXT_PROJECT_DIR)/board_config
+
 board_config_h := $(SRCGEN_DIR)/include/config/board_config.h
-GEN_EXE := $(BUILD_DIR)/gen_board_conf
+GEN_EXE        := $(BUILD_DIR)/gen_board_conf
+
+BOARD_CONF_INCLUDE := -I $(CONF_DIR) -I $(BOARD_CONF_DIR) -I $(BOARD_CONF_GEN_DIR)
+
+ifneq ($(wildcard $(BOARD_CONF_EXT_DIR)),)
+BOARD_CONF_INCLUDE += -I $(BOARD_CONF_EXT_DIR)
+endif
 
 all:
 ifneq ($(wildcard $(BOARD_CONF)),)
-	$(HOSTCC) -I $(CONF_DIR) -I $(BOARD_CONF_DIR) -I $(BOARD_CONF_GEN_DIR) \
-		-o $(GEN_EXE) $(BOARD_CONF_GEN_DIR)/gen_board_conf.c
+	$(HOSTCC) $(BOARD_CONF_INCLUDE) -o $(GEN_EXE) $(BOARD_CONF_GEN_DIR)/gen_board_conf.c
 	$(GEN_EXE) > $(board_config_h)
 endif
