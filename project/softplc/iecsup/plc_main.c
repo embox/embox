c186001e18e4d2885ce3ec98c0b1ea3da896543d
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-11-19 12:28:11 +0300

    project: softplc: Clean plc_main.c

--- project/softplc/iecsup/plc_main.c
+++ project/softplc/iecsup/plc_main.c
@@ -18,11 +18,11 @@
 /**
  * Functions and variables provied by generated C softPLC
  */
-extern unsigned long long common_ticktime__;
-extern unsigned long greatest_tick_count__;
+extern unsigned long long common_ticktime__; // ns
+extern unsigned long greatest_tick_count__;  // tick
 
-extern void config_run__(int tick);
 extern void config_init__(void);
+extern void config_run__(unsigned long tick);
 
 /**
  * Functions and variables to export to generated C softPLC
@@ -30,15 +30,14 @@ extern void config_init__(void);
 TIME __CURRENT_TIME;
 BOOL __DEBUG;
 
+static unsigned long tick_counter;
+
 static void plc_handler(sys_timer_t *timer, void *param) {
-	unsigned int *tick;
 	struct timespec ts;
 
-	tick = ((unsigned int *)param);
-
-	*tick += 1;
+	tick_counter += 1;
 	if (greatest_tick_count__) {
-		*tick %= greatest_tick_count__;
+		tick_counter %= greatest_tick_count__;
 	}
 
 	clock_gettime(CLOCK_REALTIME, &ts);
@@ -46,21 +45,20 @@ static void plc_handler(sys_timer_t *timer, void *param) {
 	__CURRENT_TIME.tv_sec = ts.tv_sec;
 	__CURRENT_TIME.tv_nsec = ts.tv_nsec;
 
-	config_run__(*tick);
+	config_run__(tick_counter);
 }
 
 int main(int argc, char const *argv[]) {
-	volatile unsigned int tick;
 	sys_timer_t *timer;
-	uint32_t period;
+	uint32_t ticktime_ms;
 	int err;
 
 	config_init__();
 
-	tick = 0;
-	period = common_ticktime__ / NSEC_PER_MSEC;
+	tick_counter = 0;
+	ticktime_ms = common_ticktime__ / NSEC_PER_MSEC;
 
-	err = timer_set(&timer, TIMER_PERIODIC, period, plc_handler, (void *)&tick);
+	err = timer_set(&timer, TIMER_PERIODIC, ticktime_ms, plc_handler, NULL);
 
 	if (!err) {
 		err = SCHED_WAIT(0);
