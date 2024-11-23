bfbf6ab33fe07da25f515fe08359adf9f4e28f4f
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2024-11-23 13:28:36 +0300

    compat: posix: Format mprotect.c

--- src/compat/posix/sys/mman/mprotect.c
+++ src/compat/posix/sys/mman/mprotect.c
@@ -9,31 +9,27 @@
 #include <errno.h>
 #include <stddef.h>
 #include <stdint.h>
-
 #include <sys/mman.h>
-#include <hal/mmu.h>
-#include <mem/vmem.h>
-#include <mem/mmap.h>
-#include <kernel/task/resource/mmap.h>
 
-int mprotect(void* addr, size_t len, int prot) {
+#include <hal/mmu.h>
+#include <kernel/task/resource/mmap.h>
+#include <mem/mmap.h>
+#include <mem/vmem.h>
+
+int mprotect(void *addr, size_t len, int prot) {
 	int err;
 	struct emmap *emmap = task_self_resource_mmap();
 
-	if (((uintptr_t) addr % VMEM_PAGE_SIZE) != 0) {
+	if (((uintptr_t)addr % VMEM_PAGE_SIZE) != 0) {
 		err = EINVAL;
 		return SET_ERRNO(err);
 	}
 
 	prot &= PROT_READ | PROT_WRITE | PROT_EXEC;
 
-	if ((err = vmem_set_flags(emmap->ctx,
-				(mmu_vaddr_t) addr,
-				len,
-				prot))) {
+	if ((err = vmem_set_flags(emmap->ctx, (mmu_vaddr_t)addr, len, prot))) {
 		return err;
 	}
 
 	return 0;
-
 }
