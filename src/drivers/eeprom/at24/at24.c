4123c7d1fb323b54c57ea78af4873166122e592d
Anton Bondarev <anton.bondarev2310@gmail.com>
2024-12-13 14:24:12 +0300

    drivers: Work on drivers/eeprom/at24

--- src/drivers/eeprom/at24/at24.c
+++ src/drivers/eeprom/at24/at24.c
@@ -13,7 +13,7 @@
 EMBOX_UNIT_INIT(at24_init);
 
 #define I2C_BUS_NR         OPTION_GET(NUMBER,i2c_bus_nr)
-#define I2C_BUS_ADDR       OPTION_GET(NUMBER,i2c_bus_nr)
+#define I2C_BUS_ADDR       OPTION_GET(NUMBER,i2c_bus_addr)
 
 struct at24_dev {
     struct eeprom_dev eeprom_dev;
@@ -26,6 +26,7 @@ static int at24_init(void) {
     int res;
 
     at24_dev.eeprom_dev.eed_name = "at24_eeprom";
+    at24_dev.eeprom_dev.eed_io_limit = 1024;
 
     res = eeprom_dev_register(&at24_dev.eeprom_dev, I2C_BUS_NR, I2C_BUS_ADDR);
 
