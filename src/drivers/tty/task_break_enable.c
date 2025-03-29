1013e57005202ee3362976bf53a158e0a4c79a63
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-03-29 13:00:29 +0300

    minor: Clean task_break_enable.c

--- src/drivers/tty/task_break_enable.c
+++ src/drivers/tty/task_break_enable.c
@@ -8,16 +8,15 @@
 #include <termios.h>
 #include <signal.h>
 
-
-#define TC_I(t, flag) ((t)->termios.c_iflag & (flag))
-#define TC_O(t, flag) ((t)->termios.c_oflag & (flag))
-#define TC_C(t, flag) ((t)->termios.c_cflag & (flag))
-#define TC_L(t, flag) ((t)->termios.c_lflag & (flag))
+//#define TC_I(t, flag) ((t)->termios.c_iflag & (flag))
+//#define TC_O(t, flag) ((t)->termios.c_oflag & (flag))
+//#define TC_C(t, flag) ((t)->termios.c_cflag & (flag))
+#define TTY_L(t, flag) ((t)->termios.c_lflag & (flag))
 
 void tty_task_break_check(struct tty *t, char ch) {
 	cc_t *cc = t->termios.c_cc;
 
-	if (TC_L(t, ISIG) && (ch == cc[VINTR]) && t->pgrp) {
+	if (TTY_L(t, ISIG) && (ch == cc[VINTR]) && t->pgrp) {
 		kill(t->pgrp, SIGINT);
 		/*TODO normal handling when process groups will be realized*/
 		t->pgrp = 0;
