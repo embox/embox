037421bf10f0d86886a29f7f8fd2ecdd58513f2c
Atharv Dubey <atharvd440@gmail.com>
2025-04-05 23:53:00 +0530

    math/tests: Adding llrint & lrint test

---
+++ src/compat/libc/math/tests/lrint_test.c
@@ -0,0 +1,32 @@
+/**
+ * @file
+ *
+ * @date April 05, 2025
+ * @author Atharv Dubey
+ */
+
+ #include <math.h>
+ #include <embox/test.h>
+ 
+ EMBOX_TEST_SUITE("lrint() tests");
+ 
+ TEST_CASE("Test for lrint(0.23)") {
+     test_assert(lrint(0.23) == 0L);
+ }
+ 
+ TEST_CASE("Test for lrint(0.73)") {
+     test_assert(lrint(0.73) == 1L);
+ }
+ 
+ TEST_CASE("Test for lrint(0.0)") {
+     test_assert(lrint(0.0) == 0L);
+ }
+ 
+ TEST_CASE("Test for lrint(3.5)") {
+     test_assert(lrint(3.5) == 4L);
+ }
+ 
+ TEST_CASE("Test for lrint(-3.5)") {
+     test_assert(lrint(-3.5) == -4L);
+ }
+ 
