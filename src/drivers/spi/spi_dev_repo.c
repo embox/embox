1e9ca1de4761449b0515301f7029f1484c76187f
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-04-09 10:41:47 +0300

    minor: Clean drivers/spi/spi_dev_repo.c

--- src/drivers/spi/spi_dev_repo.c
+++ src/drivers/spi/spi_dev_repo.c
@@ -8,58 +8,15 @@
  * @version
  * @date 03.12.2018
  */
+#include <util/log.h>
 
 #include <assert.h>
 #include <errno.h>
 
-#include <drivers/char_dev.h>
 #include <drivers/spi.h>
-#include <embox/unit.h>
-#include <framework/mod/options.h>
-#include <util/log.h>
-#if 0
-/* Assume we have 4 SPI devices at most */
-#if SPI_REGISTRY_SZ > 0
-struct spi_device spi_device0 __attribute__((weak));
-#endif
-#if SPI_REGISTRY_SZ > 1
-struct spi_device spi_device1 __attribute__((weak));
-#endif
-#if SPI_REGISTRY_SZ > 2
-struct spi_device spi_device2 __attribute__((weak));
-#endif
-#if SPI_REGISTRY_SZ > 3
-struct spi_device spi_device3 __attribute__((weak));
-#endif
-#if SPI_REGISTRY_SZ > 4
-struct spi_device spi_device4 __attribute__((weak));
-#endif
-#if SPI_REGISTRY_SZ > 5
-struct spi_device spi_device5 __attribute__((weak));
-#endif
 
-/* Note: it's array of pointers, not structures as usual */
-struct spi_device *spi_device_registry[SPI_REGISTRY_SZ] = {
-#if SPI_REGISTRY_SZ > 0
-    &spi_device0,
-#endif
-#if SPI_REGISTRY_SZ > 1
-    &spi_device1,
-#endif
-#if SPI_REGISTRY_SZ > 1
-    &spi_device2,
-#endif
-#if SPI_REGISTRY_SZ > 1
-    &spi_device3,
-#endif
-#if SPI_REGISTRY_SZ > 1
-    &spi_device4,
-#endif
-#if SPI_REGISTRY_SZ > 1
-    &spi_device5
-#endif
-};
-#endif
+#include <framework/mod/options.h>
+
 ARRAY_SPREAD_DEF(struct spi_device *, __spi_device_registry);
 
 /**
