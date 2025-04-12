ee146f00ec5191a73a0da4d22b9b7ca4acc89c75
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2025-04-12 08:10:24 +0300

    third-party: compiler_headers: Fix narrowing conversion error in arm_neon.h

---
+++ third-party/compiler_headers/include/arm_neon.h
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author Aleksey Zhmulin
+ * @date 11.04.25
+ */
+
+#ifndef COMPILER_HEADERS_ARM_NEON_H_
+#define COMPILER_HEADERS_ARM_NEON_H_
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnarrowing"
+
+#include_next <arm_neon.h>
+
+#pragma GCC diagnostic pop
+
+#endif /* COMPILER_HEADERS_ARM_NEON_H_ */
