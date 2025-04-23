627b15b9e4d31f128ecf9881cd3304bd5a1849b1
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-04-23 11:53:44 +0300

    fs: Improve fs/driver/ext3

---
+++ src/fs/driver/ext3/ext3fs_dvfs.c
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @brief ext file system
+ *
+ * @date 04.12.2012
+ * @author Andrey Gazukin
+ */
+
+#include <fs/inode.h>
+#include <fs/super_block.h>
+
+extern struct idesc *dvfs_file_open_idesc(struct lookup *lookup, int __oflag);
+
+extern int ext3fs_destroy_inode(struct inode *inode);
+
+struct super_block_operations ext3fs_sbops = {
+	.open_idesc    = dvfs_file_open_idesc,
+	.destroy_inode = &ext3fs_destroy_inode,
+};
+
