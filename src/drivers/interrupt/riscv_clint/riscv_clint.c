1b8cae094d97272b066f60ec014725c96630a0e2
Zixian Zeng <sycamoremoon376@gmail.com>
2025-04-29 21:40:32 +0800

    riscv: fix some clints don't support 64 bits memory IO
    Some platform such as T-HEAD C910 core based TH1520 CLINT
    has quirk which cannot be loaded 64 width at a time.
    
    Otherwise the top 32 bits of the 64 bits value will be overwritten
    by the bottom 32 bits, for example:
    	0xAA00BB00_FFDDCCDD	->	0xFFDDCCDD_FFDDCCDD
    
    So introduce a boolean `has64bitmmio` to fix the quirk.

--- src/drivers/interrupt/riscv_clint/riscv_clint.c
+++ src/drivers/interrupt/riscv_clint/riscv_clint.c
@@ -7,21 +7,43 @@
  * @authored by Suraj Ravindra Sonawane
  */
 
-#include <drivers/irqctrl.h>
-#include <hal/reg.h>
-#include <hal/cpu.h>
 #include <asm/interrupts.h>
+#include <drivers/irqctrl.h>
+#include <hal/cpu.h>
+#include <hal/reg.h>
 
+#define HAS64BITMMIO	OPTION_GET(BOOLEAN, has64bitmmio)
 #define CLINT_ADDR      OPTION_GET(NUMBER, base_addr)
 #define MSIP_OFFSET     OPTION_GET(NUMBER, msip_offset)
 #define MTIMECMP_OFFSET OPTION_GET(NUMBER, mtimecmp_offset)
 #define MTIME_OFFSET    OPTION_GET(NUMBER, mtime_offset)
 
-#define MSIP_ADDR(hart)       (CLINT_ADDR + MSIP_OFFSET + ((hart) * 4))
-#define MTIMECMP_ADDR(hart)   (CLINT_ADDR + MTIMECMP_OFFSET + ((hart) * 8))
+#define MSIP_ADDR(hart)     (CLINT_ADDR + MSIP_OFFSET + ((hart) * 4))
+#define MTIMECMP_ADDR(hart) (CLINT_ADDR + MTIMECMP_OFFSET + ((hart) * 8))
 
 /* time counter register is read only*/
-#define MTIME_ADDR            (CLINT_ADDR + MTIME_OFFSET)
+#define MTIME_ADDR (CLINT_ADDR + MTIME_OFFSET)
+
+#if __riscv_xlen != 32 && !HAS64BITMMIO
+
+#define READ_64BIT(addr) ({		\
+	uint32_t lo, hi;		\
+	lo = REG32_LOAD(addr);		\
+	hi = REG32_LOAD(addr + 4);	\
+	(((uint64_t)hi << 32) | lo);	\
+})
+
+#define WRITE_64BIT(addr, value) do {			\
+	REG32_STORE(addr, value & 0xFFFFFFFF); \
+	REG32_STORE(addr + 4, (value >> 32) & 0xFFFFFFFF); \
+} while(0)
+
+#else /* __riscv_xlen == 32 || !HAS64BITMMIO */
+
+#define WRITE_64BIT(addr, value)	REG64_STORE(addr, value)
+#define READ_64BIT(addr)		REG64_LOAD(addr)
+
+#endif /* __riscv_xlen == 32 || !HAS64BITMMIO */
 
 /**
  * Initializes the CLINT.
@@ -31,16 +53,15 @@
  *
  * @return 0 on success.
  */
-int clint_init(void) __attribute__((unused));
 
 /* In SMP case, each CPU will call this function independently */
 int clint_init(void) {
 	int hartid = cpu_get_id();
-    REG32_STORE(MSIP_ADDR(hartid), 0); /* Clear Pending bit */
-    REG64_STORE(MTIMECMP_ADDR(hartid), 0xFFFFFFFFFFFFFFFF); 
+	REG32_STORE(MSIP_ADDR(hartid), 0); /* Clear Pending bit */
+	WRITE_64BIT(MTIMECMP_ADDR(hartid), 0xFFFFFFFFFFFFFFFFU);
 
-    enable_software_interrupts();
-    return 0;
+	enable_software_interrupts();
+	return 0;
 }
 
 /**
@@ -52,7 +73,7 @@ int clint_init(void) {
  * @param hart_id The hart id (only for SiFive CLINT).
  */
 void clint_configure_msip(uint8_t value, int hart_id) {
-    REG32_STORE(MSIP_ADDR(hart_id), value & 1);
+	REG32_STORE(MSIP_ADDR(hart_id), value & 1);
 }
 
 /**
@@ -64,7 +85,18 @@ void clint_configure_msip(uint8_t value, int hart_id) {
  * @param hart_id The hart id (only for SiFive CLINT).
  */
 void clint_set_mtimecmp(uint64_t value, int hart_id) {
-    REG64_STORE(MTIMECMP_ADDR(hart_id), value);
+	WRITE_64BIT(MTIMECMP_ADDR(hart_id), value);
+}
+
+/**
+ * Sets the MTIMECMP register value.
+ *
+ * This function gets the MTIMECMP register to the provided 64-bit value.
+ *
+ * @param hart_id The hart id (only for SiFive CLINT).
+ */
+uint64_t clint_get_mtimecmp(int hart_id) {
+	return READ_64BIT(MTIMECMP_ADDR(hart_id));
 }
 
 /**
@@ -75,5 +107,5 @@ void clint_set_mtimecmp(uint64_t value, int hart_id) {
  * @return The current value of MTIME.
  */
 uint64_t clint_get_mtime(void) {
-    return REG64_LOAD(MTIME_ADDR);
+	return READ_64BIT(MTIME_ADDR);
 }
