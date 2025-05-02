7b882a5c70bbb1c9a0dc79282057793b2cbaef6b
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-05-02 16:12:01 +0300

    drivers: Work on drivers/spi/spi_controller

--- src/drivers/spi/spi_controller.h
+++ src/drivers/spi/spi_controller.h
@@ -53,15 +53,15 @@ extern int spi_controller_id(struct spi_controller *dev);
 extern const struct char_dev_ops __spi_cdev_ops;
 
  #define SPI_CONTROLLER_DEF(dev_name, spi_dev_ops, dev_priv, idx)         \
- struct spi_controller MACRO_CONCAT(spi_controller, idx) = {           \
-	 .cdev = CHAR_DEV_INIT(MACRO_CONCAT(spi_controller, idx).cdev, \
-		 MACRO_STRING(dev_name), &__spi_cdev_ops),             \
-	 .spi_ops = spi_dev_ops,                                   \
-	 .priv = dev_priv,                                         \
-	 .spic_bus_num = idx,                                      \
- };                                                            \
- CHAR_DEV_REGISTER((struct char_dev *)&MACRO_CONCAT(spi_controller, idx)) \
- ARRAY_SPREAD_DECLARE(struct spi_controller *, __spi_controller_registry);       \
- ARRAY_SPREAD_ADD(__spi_controller_registry, &MACRO_CONCAT(spi_controller, idx))
+ 	struct spi_controller MACRO_CONCAT(spi_controller, idx) = {           \
+		.cdev = CHAR_DEV_INIT(MACRO_CONCAT(spi_controller, idx).cdev, \
+			MACRO_STRING(dev_name), &__spi_cdev_ops),             \
+		.spi_ops = spi_dev_ops,                                   \
+		.priv = dev_priv,                                         \
+		.spic_bus_num = idx,                                      \
+ 	};                                                            \
+	CHAR_DEV_REGISTER((struct char_dev *)&MACRO_CONCAT(spi_controller, idx)); \
+	ARRAY_SPREAD_DECLARE(struct spi_controller *, __spi_controller_registry);       \
+	ARRAY_SPREAD_ADD(__spi_controller_registry, &MACRO_CONCAT(spi_controller, idx))
 
 #endif /* DRIVERS_SPI_CONTROLLER_H_ */
