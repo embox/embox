06b760bafcd93d12072f28087d0dbab5c77567e3
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-05-02 16:19:31 +0300

    drivers: Switch spi/imx6_ecspi/ to 'spi_controller'

--- src/drivers/spi/imx6_ecspi/imx6_ecspi1.c
+++ src/drivers/spi/imx6_ecspi/imx6_ecspi1.c
@@ -38,7 +38,11 @@ static int imx6_ecspi1_init(void) {
 	return imx6_ecspi_init(&imx6_ecspi1);
 }
 
-#define SPI_DEV_NAME      ixm6_ecspi1
+#define SPI_BUS_NAME      ixm6_ecspi1
 
 PERIPH_MEMORY_DEFINE(imx6_ecspi1, BASE_ADDR, 0x44);
-SPI_DEV_DEF(SPI_DEV_NAME, &imx6_ecspi_ops, &imx6_ecspi1, 0);
+SPI_CONTROLLER_DEF(SPI_BUS_NAME, &imx6_ecspi_ops, &imx6_ecspi1, 0);
+
+
+#define SPI_DEV_NAME      spi_bus_0_
+SPI_DEV_DEF(MACRO_CONCAT(SPI_DEV_NAME,0), NULL, &imx6_ecspi1, 0);
