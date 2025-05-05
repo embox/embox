372bf49e070156d2c6cec032d48656d91cc9ecac
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2025-05-05 13:46:08 +0300

    compat: math: tests: Format some tests

--- src/compat/libc/math/tests/llrint_test.c
+++ src/compat/libc/math/tests/llrint_test.c
@@ -5,28 +5,28 @@
  * @author Atharv Dubey
  */
 
- #include <math.h>
- #include <embox/test.h>
- 
- EMBOX_TEST_SUITE("llrint() tests");
- 
- TEST_CASE("Test for llrint(0.23)") {
-     test_assert(llrint(0.23) == 0LL);
- }
- 
- TEST_CASE("Test for llrint(0.73)") {
-     test_assert(llrint(0.73) == 1LL);
- }
- 
- TEST_CASE("Test for llrint(0.0)") {
-     test_assert(llrint(0.0) == 0LL);
- }
- 
- TEST_CASE("Test for llrint(3.5)") {
-     test_assert(llrint(3.5) == 4LL);
- }
- 
- TEST_CASE("Test for llrint(-3.5)") {
-     test_assert(llrint(-3.5) == -4LL);
- }
- 
+#include <math.h>
+
+#include <embox/test.h>
+
+EMBOX_TEST_SUITE("llrint() tests");
+
+TEST_CASE("Test for llrint(0.23)") {
+	test_assert(llrint(0.23) == 0LL);
+}
+
+TEST_CASE("Test for llrint(0.73)") {
+	test_assert(llrint(0.73) == 1LL);
+}
+
+TEST_CASE("Test for llrint(0.0)") {
+	test_assert(llrint(0.0) == 0LL);
+}
+
+TEST_CASE("Test for llrint(3.5)") {
+	test_assert(llrint(3.5) == 4LL);
+}
+
+TEST_CASE("Test for llrint(-3.5)") {
+	test_assert(llrint(-3.5) == -4LL);
+}
