469dc5effa3bb1cda0909cdace375ab16aa851bc
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2025-05-23 21:45:21 +0300

    fs: Clean ext3

--- src/fs/driver/ext3/ext3fs_drv.c
+++ src/fs/driver/ext3/ext3fs_drv.c
@@ -6,35 +6,29 @@
  * @author Andrey Gazukin
  */
 
+#include <arpa/inet.h>
 #include <errno.h>
 #include <stdint.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
-//#include <util/getopt.h>
-
-#include <mem/sysmalloc.h>
-
+#include <fs/ext2.h>
 #include <fs/fs_driver.h>
 #include <fs/inode.h>
 #include <fs/mount.h>
 #include <fs/super_block.h>
- 
-#include <fs/ext2.h>
-
+#include <mem/sysmalloc.h>
 
 #include "ext3_journal.h"
 
 #define EXT2_NAME "ext2"
 #define EXT3_NAME "ext3"
-#define EXT3_JOURNAL_SUPERBLOCK_INODE 8
 
+#define EXT3_JOURNAL_SUPERBLOCK_INODE 8
 
 /* fs operations */
 static int ext3fs_format(struct block_dev *bdev, void *priv);
 static int ext3fs_fill_sb(struct super_block *sb, const char *source);
-
 
 static const struct fs_driver *ext2fs_driver;
 static struct fs_driver ext3fs_driver;
@@ -43,28 +37,7 @@ int ext3fs_destroy_inode(struct inode *inode) {
 	return 0;
 }
 
-
-//extern int main_mke2fs(int argc, char **argv);
-
 static int ext3fs_format(struct block_dev *bdev, void *priv) {
-#if 0
-	int argc = 6;
-	char *argv[6];
-	char dev_path[64];
-
-	strcpy(dev_path, "/dev/");
-	strcat(dev_path, block_dev_name(bdev));
-
-	argv[0] = "mke2fs";
-	argv[1] = "-b";
-	argv[2] = "1024";
-	argv[3] = "-t";
-	argv[4] = "ext3";
-	argv[5] = dev_path;
-
-	getopt_init();
-	return main_mke2fs(argc, argv);
-#endif
 	char cmd_line[128] = "mke2fs -b 1024 -t ext3 /dev/";
 
 	strcat(cmd_line, block_dev_name(bdev));
@@ -72,51 +45,50 @@ static int ext3fs_format(struct block_dev *bdev, void *priv) {
 }
 
 static int ext3_journal_load(journal_t *jp, struct block_dev *jdev, block_t start) {
-    ext3_journal_superblock_t *sb;
-    ext3_journal_specific_t *spec = (ext3_journal_specific_t *)jp->j_fs_specific.data;
-    char buf[4096];
+	ext3_journal_superblock_t *sb;
+	ext3_journal_specific_t *spec = (ext3_journal_specific_t *)jp->j_fs_specific.data;
+	char buf[4096];
 
-    assert(jp);
-    assert(jdev);
+	assert(jp);
+	assert(jdev);
 
-    jp->j_dev = jdev;
-    jp->j_disk_sectorsize = block_dev_block_size(jdev);
+	jp->j_dev = jdev;
+	jp->j_disk_sectorsize = block_dev_block_size(jdev);
 
-    assert(jp->j_disk_sectorsize >= 512);
+	assert(jp->j_disk_sectorsize >= 512);
 
-    /* Load superblock from the log. */
-    if (!jp->j_dev->driver->bdo_read(jp->j_dev, buf,
-    		4096, start)) {
-    	return -1;
-    }
+	/* Load superblock from the log. */
+	if (!jp->j_dev->driver->bdo_read(jp->j_dev, buf, 4096, start)) {
+		return -1;
+	}
 
-    sb = (ext3_journal_superblock_t *)buf;
+	sb = (ext3_journal_superblock_t *)buf;
 
-    assert(sb->s_blocksize);
+	assert(sb->s_blocksize);
 
-    jp->j_maxlen         = ntohl(sb->s_maxlen);
-    jp->j_blocksize      = ntohl(sb->s_blocksize);
-    jp->j_blk_offset     = journal_db2jb(jp, start);
-    jp->j_first          = 1;
-    jp->j_last           = jp->j_maxlen;
-    spec->j_format_version = ntohl(sb->s_header.h_blocktype);
+	jp->j_maxlen = ntohl(sb->s_maxlen);
+	jp->j_blocksize = ntohl(sb->s_blocksize);
+	jp->j_blk_offset = journal_db2jb(jp, start);
+	jp->j_first = 1;
+	jp->j_last = jp->j_maxlen;
+	spec->j_format_version = ntohl(sb->s_header.h_blocktype);
 
-    /* Initialize transaction specific data */
-    jp->j_head                 = jp->j_first;
-    jp->j_tail                 = jp->j_head;
-    jp->j_free                 = jp->j_last - jp->j_first;
-    jp->j_tail_sequence        = 1;
-    jp->j_transaction_sequence = 1;
-    jp->j_running_transaction  = journal_new_trans(jp);
-    dlist_init(&jp->j_checkpoint_transactions);
+	/* Initialize transaction specific data */
+	jp->j_head = jp->j_first;
+	jp->j_tail = jp->j_head;
+	jp->j_free = jp->j_last - jp->j_first;
+	jp->j_tail_sequence = 1;
+	jp->j_transaction_sequence = 1;
+	jp->j_running_transaction = journal_new_trans(jp);
+	dlist_init(&jp->j_checkpoint_transactions);
 
-    /* Update journal superblock */
-    spec->j_sb_buffer  = journal_new_block(jp, jp->j_blk_offset);
-    spec->j_superblock = (ext3_journal_superblock_t *)spec->j_sb_buffer->data;
-    memcpy(spec->j_sb_buffer->data, buf, jp->j_blocksize);
-    jp->j_fs_specific.update(jp);
+	/* Update journal superblock */
+	spec->j_sb_buffer = journal_new_block(jp, jp->j_blk_offset);
+	spec->j_superblock = (ext3_journal_superblock_t *)spec->j_sb_buffer->data;
+	memcpy(spec->j_sb_buffer->data, buf, jp->j_blocksize);
+	jp->j_fs_specific.update(jp);
 
-    return 0;
+	return 0;
 }
 
 extern struct file_operations ext3_fop;
@@ -129,13 +101,10 @@ static int ext3fs_create_jornal(struct super_block *sb, struct inode *dest) {
 	int inode_sector, rsize;
 	journal_t *jp = NULL;
 	ext3_journal_specific_t *ext3_spec;
-	journal_fs_specific_t spec = {
-			.bmap = ext3_journal_bmap,
-			.commit = ext3_journal_commit,
-			.update = ext3_journal_update,
-			.trans_freespace = ext3_journal_trans_freespace
-	};
-
+	journal_fs_specific_t spec = {.bmap = ext3_journal_bmap,
+	    .commit = ext3_journal_commit,
+	    .update = ext3_journal_update,
+	    .trans_freespace = ext3_journal_trans_freespace};
 
 	if (NULL == (ext3_spec = ext3_journal_cache_alloc())) {
 		return -1;
@@ -161,8 +130,8 @@ static int ext3fs_create_jornal(struct super_block *sb, struct inode *dest) {
 	/* set pointer to inode struct in read buffer */
 	dip = sysmalloc(sizeof(struct ext2fs_dinode));
 	memcpy(dip,
-		(buf + EXT2_DINODE_SIZE(fsi) * ino_to_fsbo(fsi, EXT3_JOURNAL_SUPERBLOCK_INODE)),
-		sizeof(struct ext2fs_dinode));
+	    (buf + EXT2_DINODE_SIZE(fsi) * ino_to_fsbo(fsi, EXT3_JOURNAL_SUPERBLOCK_INODE)),
+	    sizeof(struct ext2fs_dinode));
 
 	/* XXX Hack to use ext2 functions */
 	ext3_spec->ext3_journal_inode = dip;
@@ -197,7 +166,6 @@ static int ext3fs_fill_sb(struct super_block *sb, const char *source) {
 	sb->sb_fops = &ext3_fop;
 	sb->sb_iops = &ext3_iops;
 
-
 	ret = ext3fs_create_jornal(sb, sb->sb_root);
 
 	return ret;
@@ -222,10 +190,10 @@ static int ext3fs_clean_sb(struct super_block *sb) {
 }
 
 static struct fs_driver ext3fs_driver = {
-	.name = EXT3_NAME,
-	.format   = ext3fs_format,
-	.fill_sb = ext3fs_fill_sb,
-	.clean_sb = ext3fs_clean_sb,
+    .name = EXT3_NAME,
+    .format = ext3fs_format,
+    .fill_sb = ext3fs_fill_sb,
+    .clean_sb = ext3fs_clean_sb,
 };
 
 DECLARE_FILE_SYSTEM_DRIVER(ext3fs_driver);
