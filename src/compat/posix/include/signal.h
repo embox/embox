60850049d9c762c493304d0e1cff355349c74b21
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2025-05-24 18:05:35 +0300

    compat: posix: Add SI_USER macro and Format signal.h

--- src/compat/posix/include/signal.h
+++ src/compat/posix/include/signal.h
@@ -10,62 +10,62 @@
 #ifndef COMPAT_POSIX_SIGNAL_H_
 #define COMPAT_POSIX_SIGNAL_H_
 
+#include <defines/_sig_total_define.h>
+#include <defines/pthread_attr_t_define.h>
+#include <defines/sigset_t_define.h>
+#include <defines/stack_t_define.h>
+#include <defines/ucontext_t_define.h>
 #include <errno.h>
 #include <sys/cdefs.h>
 #include <sys/types.h>
 
-#include <defines/_sig_total_define.h>
-#include <defines/sigset_t_define.h>
-#include <defines/stack_t_define.h>
-#include <defines/ucontext_t_define.h>
-
-#include <defines/pthread_attr_t_define.h>
-
 #include <lib/libds/bitmap.h>
 
-#define SIG_DFL      ((sighandler_t)0x1)
-#define SIG_IGN      ((sighandler_t)0x3)
-#define SIG_ERR      ((sighandler_t)0x5)
+#define SIG_DFL ((sighandler_t)0x1)
+#define SIG_IGN ((sighandler_t)0x3)
+#define SIG_ERR ((sighandler_t)0x5)
+
+#define SI_USER 0
 
 /* Signals. */
-#define SIGHUP       1       /* (POSIX)    Hangup. */
-#define SIGINT       2       /* (ANSI)     Interrupt. */
-#define SIGQUIT      3       /* (POSIX)    Quit. */
-#define SIGILL       4       /* (ANSI)     Illegal instruction. */
-#define SIGTRAP      5       /* (POSIX)    Trace trap. */
-#define SIGABRT      6       /* (ANSI)     Abort. */
-#define SIGIOT       6       /* (4.2 BSD)  IOT trap. */
-#define SIGBUS       7       /* (4.2 BSD)  BUS error. */
-#define SIGFPE       8       /* (ANSI)     Floating-point exception. */
-#define SIGKILL      9       /* (POSIX)    Kill, unblockable. */
-#define SIGUSR1      10      /* (POSIX)    User-defined signal 1. */
-#define SIGSEGV      11      /* (ANSI)     Segmentation violation. */
-#define SIGUSR2      12      /* (POSIX)    User-defined signal 2. */
-#define SIGPIPE      13      /* (POSIX)    Broken pipe. */
-#define SIGALRM      14      /* (POSIX)    Alarm clock. */
-#define SIGTERM      15      /* (ANSI)     Termination. */
-#define SIGSTKFLT    16      /*            Stack fault. */
-#define SIGCLD       SIGCHLD /* (System V) Same as SIGCHLD. */
-#define SIGCHLD      17      /* (POSIX)    Child status has changed. */
-#define SIGCONT      18      /* (POSIX)    Continue. */
-#define SIGSTOP      19      /* (POSIX)    Stop, unblockable. */
-#define SIGTSTP      20      /* (POSIX)    Keyboard stop. */
-#define SIGTTIN      21      /* (POSIX)    Background read from tty. */
-#define SIGTTOU      22      /* (POSIX)    Background write to tty. */
-#define SIGURG       23      /* (4.2 BSD)  Urgent condition on socket. */
-#define SIGXCPU      24      /* (4.2 BSD)  CPU limit exceeded. */
-#define SIGXFSZ      25      /* (4.2 BSD)  File size limit exceeded. */
-#define SIGVTALRM    26      /* (4.2 BSD)  Virtual alarm clock. */
-#define SIGPROF      27      /* (4.2 BSD)  Profiling alarm clock. */
-#define SIGWINCH     28      /* (4.3 BSD, Sun) Window size change. */
-#define SIGPOLL      SIGIO   /* (System V) Pollable event occurred. */
-#define SIGIO        29      /* (4.2 BSD)  I/O now possible. */
-#define SIGPWR       30      /* (System V) Power failure restart. */
-#define SIGSYS       31      /*            Bad system call. */
-#define SIGUNUSED    31
+#define SIGHUP    1       /* (POSIX)    Hangup. */
+#define SIGINT    2       /* (ANSI)     Interrupt. */
+#define SIGQUIT   3       /* (POSIX)    Quit. */
+#define SIGILL    4       /* (ANSI)     Illegal instruction. */
+#define SIGTRAP   5       /* (POSIX)    Trace trap. */
+#define SIGABRT   6       /* (ANSI)     Abort. */
+#define SIGIOT    6       /* (4.2 BSD)  IOT trap. */
+#define SIGBUS    7       /* (4.2 BSD)  BUS error. */
+#define SIGFPE    8       /* (ANSI)     Floating-point exception. */
+#define SIGKILL   9       /* (POSIX)    Kill, unblockable. */
+#define SIGUSR1   10      /* (POSIX)    User-defined signal 1. */
+#define SIGSEGV   11      /* (ANSI)     Segmentation violation. */
+#define SIGUSR2   12      /* (POSIX)    User-defined signal 2. */
+#define SIGPIPE   13      /* (POSIX)    Broken pipe. */
+#define SIGALRM   14      /* (POSIX)    Alarm clock. */
+#define SIGTERM   15      /* (ANSI)     Termination. */
+#define SIGSTKFLT 16      /*            Stack fault. */
+#define SIGCLD    SIGCHLD /* (System V) Same as SIGCHLD. */
+#define SIGCHLD   17      /* (POSIX)    Child status has changed. */
+#define SIGCONT   18      /* (POSIX)    Continue. */
+#define SIGSTOP   19      /* (POSIX)    Stop, unblockable. */
+#define SIGTSTP   20      /* (POSIX)    Keyboard stop. */
+#define SIGTTIN   21      /* (POSIX)    Background read from tty. */
+#define SIGTTOU   22      /* (POSIX)    Background write to tty. */
+#define SIGURG    23      /* (4.2 BSD)  Urgent condition on socket. */
+#define SIGXCPU   24      /* (4.2 BSD)  CPU limit exceeded. */
+#define SIGXFSZ   25      /* (4.2 BSD)  File size limit exceeded. */
+#define SIGVTALRM 26      /* (4.2 BSD)  Virtual alarm clock. */
+#define SIGPROF   27      /* (4.2 BSD)  Profiling alarm clock. */
+#define SIGWINCH  28      /* (4.3 BSD, Sun) Window size change. */
+#define SIGPOLL   SIGIO   /* (System V) Pollable event occurred. */
+#define SIGIO     29      /* (4.2 BSD)  I/O now possible. */
+#define SIGPWR    30      /* (System V) Power failure restart. */
+#define SIGSYS    31      /*            Bad system call. */
+#define SIGUNUSED 31
 
-#define SIGRTMIN     32
-#define SIGRTMAX     63
+#define SIGRTMIN 32
+#define SIGRTMAX 63
 
 #define SA_NOCLDSTOP (0x1ul << 0)
 #define SA_NOCLDWAIT (0x1ul << 1)
@@ -86,8 +86,8 @@ typedef __SIG_ATOMIC_TYPE__ sig_atomic_t;
 #define NSIG _SIG_TOTAL
 
 union sigval {
-	int    sival_int;      /* Integer signal value. */
-	void  *sival_ptr;      /* Pointer signal value. */
+	int sival_int;   /* Integer signal value. */
+	void *sival_ptr; /* Pointer signal value. */
 };
 
 typedef struct {
@@ -132,8 +132,7 @@ static inline int sigprocmask(int how, const sigset_t *set, sigset_t *oldset) {
 	return -ENOSYS;
 }
 
-static inline int pthread_sigmask(int how, const sigset_t *set,
-    sigset_t *oldset) {
+static inline int pthread_sigmask(int how, const sigset_t *set, sigset_t *oldset) {
 	(void)how;
 	(void)set;
 	(void)oldset;
@@ -155,23 +154,22 @@ extern const char *const sys_siglist[];
 #define MINSIGSTKSZ 2048
 extern int sigaltstack(const stack_t *ss, stack_t *oss);
 
-#define SIGEV_NONE        0
-#define SIGEV_SIGNAL      1
-#define SIGEV_THREAD      2
+#define SIGEV_NONE   0
+#define SIGEV_SIGNAL 1
+#define SIGEV_THREAD 2
 
-#define SIGEV_THREAD_ID   SIGEV_THREAD
+#define SIGEV_THREAD_ID SIGEV_THREAD
 
 struct sigevent {
-	int                sigev_notify; /* Notification type. */
-	int                sigev_signo;  /* Signal number. */
-	union sigval       sigev_value;  /* Signal value. */
-	void(*sigev_notify_function)(union sigval) ; /* Notification function. */
-	pthread_attr_t     *sigev_notify_attributes; /* Notification attributes. */
+	int sigev_notify;                            /* Notification type. */
+	int sigev_signo;                             /* Signal number. */
+	union sigval sigev_value;                    /* Signal value. */
+	void (*sigev_notify_function)(union sigval); /* Notification function. */
+	pthread_attr_t *sigev_notify_attributes;     /* Notification attributes. */
 	/* Linux only: */
 	/* ID of thread to signal (SIGEV_THREAD_ID) */
-	pid_t           sigev_notify_thread_id;
+	pid_t sigev_notify_thread_id;
 };
-
 
 __END_DECLS
 
