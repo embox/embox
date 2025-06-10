6b28a33b0d3bb0020cdbadd3a33163ad9e625496
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-06-10 16:32:48 +0300

    drivers: Work on pwm/niiet

--- src/drivers/pwm/niiet/niiet_pwm0.c
+++ src/drivers/pwm/niiet/niiet_pwm0.c
@@ -10,6 +10,9 @@
 
 #include <util/macro.h>
 
+#include <config/board_config.h>
+#include <drivers/gpio.h>
+
 #include <drivers/pwm.h>
 
 #include "niiet_pwm.h"
@@ -17,15 +20,24 @@
 #define PWM_DEV_ID                0
 #define PWM_DEV_PRIV_STRUCT_NAME  MACRO_CONCAT(pwm_dev_priv, PWM_DEV_ID)
 
+#define CONF_PWM       MACRO_CONCAT(CONF_PWM,PWM_DEV_ID)
+
+#if defined CONF_PWM0_ENABLED
+#define PWM_BASE_ADDR  MACRO_CONCAT(CONF_PWM,_REGION_BASE_ADDR)
+#define PWM_PORT       MACRO_CONCAT(CONF_PWM,_PIN_OUT_PORT)
+#define PWM_PIN        MACRO_CONCAT(CONF_PWM,_PIN_OUT_NR)
+#define PWM_FUNC       MACRO_CONCAT(CONF_PWM,_PIN_OUT_AF)
+#endif
+
 extern struct pwm_ops niiet_pwm_ops;
 
-struct niiet_pwm_priv    PWM_DEV_PRIV_STRUCT_NAME = {
+struct niiet_pwm_priv PWM_DEV_PRIV_STRUCT_NAME = {
     .pin_desc = {
-        .pd_port = 0,
-        .pd_pin = 0,
-        .pd_func = 0
+        .pd_port = PWM_PORT,
+        .pd_pin = PWM_PIN,
+        .pd_func = PWM_FUNC
     },
-    .base_addr = 0,
+    .base_addr = PWM_BASE_ADDR,
 };
 
 PWM_DEV_DEF(PWM_DEV_ID, &niiet_pwm_ops, &PWM_DEV_PRIV_STRUCT_NAME);
--- src/drivers/pwm/niiet/niiet_pwm_drv.c
+++ src/drivers/pwm/niiet/niiet_pwm_drv.c
@@ -10,11 +10,28 @@
 
 #include <drivers/pwm.h>
 
+#include <drivers/gpio.h>
+
+#include "niiet_pwm.h"
+
+extern int clk_enable(char *clk_name);
+
 int niiet_pwm_init(struct pwm_device *dev) {
+    struct niiet_pwm_priv *priv;
+    struct pin_description *pin;
+
+    priv = dev->pwmd_priv;
+    dev->pwmd_base_addr = priv->base_addr;
+    dev->pwmd_pin = priv->pin_desc; /* copy */
+    pin = &priv->pin_desc;
+
+    gpio_setup_mode(pin->pd_port, (1 << pin->pd_pin), pin->pd_func);
+
     return 0;
 }
 
 int niiet_pwm_config(struct pwm_device *dev,  int duty_ns, int period_ns) {
+
     return 0;
 }
 
@@ -23,7 +40,6 @@ int niiet_pwm_enable(struct pwm_device *dev) {
 }
 
 void niiet_pwm__disable(struct pwm_device *dev) {
-
 }
 
 struct pwm_ops niiet_pwm_ops = {
