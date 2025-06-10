a11f22f69c828356619bc3c81b129785ee065e99
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2025-06-10 03:30:11 +0300

    third-party: bsp: Improve niiet sdk
    - Add abstract module third_party.bsp.niiet.plib
    - Fix k1921vg015-sdk

---
+++ third-party/bsp/niiet/k1921vg015-sdk/plib015.h
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author Aleksey Zhmulin
+ * @date 08.06.25
+ */
+
+#ifndef THIRD_PARTY_BSP_NIIET_PLIB015_H_
+#define THIRD_PARTY_BSP_NIIET_PLIB015_H_
+
+#include <assert.h>
+#include <plib015.h>
+
+#undef assert_param
+#define assert_param(expr) assert(expr)
+
+#endif /* THIRD_PARTY_BSP_NIIET_PLIB015_H_ */
