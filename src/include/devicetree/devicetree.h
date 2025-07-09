e31e1222de10a7fa63e4bb4093f6a41385284faa
Shankari Anand <shankari.ak0208@gmail.com>
2025-07-09 11:28:45 +0530

    Header inclusion for source devicetree.c

---
+++ src/include/devicetree/devicetree.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Shankari Anand
+ * @date    5 Jul 2025
+ */
+
+#ifndef ARM_M_DEVICETREE_H_
+#define ARM_M_DEVICETREE_H_
+
+void probe_uart_from_dtb_file(void);
+
+#endif /* ARM_M_DEVICETREE_H_ */
