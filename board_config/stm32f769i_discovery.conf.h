8fe73deceabcb0d8b4bafb3a9f3dce0c43c99809
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-07-14 19:07:50 +0300

    board_config: Update board_config/stm32f769i_discovery.conf.h

--- board_config/stm32f769i_discovery.conf.h
+++ board_config/stm32f769i_discovery.conf.h
@@ -81,4 +81,49 @@ struct led_conf leds[] = {
 	},
 };
 
-EXPORT_CONFIG(UART(uarts), LED(leds))
+struct mmc_conf mmcs[] = {
+	[0] = {
+		.status = ENABLED,
+		.dev = {
+			.name = "SDIO1",
+			.irqs = {
+				VAL("", 49),
+			},
+			.pins = {
+				PIN("TX", PC, PIN_6, AF8),
+				PIN("RX", PC, PIN_7, AF8),
+			},
+			.clocks = {
+				VAL("", CLK_SDMMC1),
+			}
+		},
+	},
+	[1] = {
+		.status = ENABLED,
+		.dev = {
+			.name = "SDIO2",
+			.irqs = {
+				VAL("", 103),
+			},
+			.pins = {
+				PIN("D0", GPIO_PORT_B, 3, AF10),
+				PIN("D1", GPIO_PORT_B, 4, AF10),
+				PIN("D2", GPIO_PORT_D, 6, AF11),
+				PIN("D3", GPIO_PORT_D, 7, AF11),
+				PIN("CK", GPIO_PORT_G, 9, AF11),
+				PIN("CMD", GPIO_PORT_G, 10, AF11),
+
+				PIN("DETECT", GPIO_PORT_I, 15, 0),
+
+			},
+			.clocks = {
+				VAL("", STM32_CLK_ENABLE(SDMMC2)),
+				VAL("TRANSFER_DIV", 0),
+			}
+		},
+	}
+		
+};
+
+
+EXPORT_CONFIG(UART(uarts), LED(leds), MMC(mmcs))
