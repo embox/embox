878267a52d6e83fe949b819f75a38242ad118935
Pechenovadasha <pechenkadasha250500@gmail.com>
2025-07-16 17:00:42 +0300

    change HAL functions to gpio_setup_mode()
    change numbers to Macros

--- project/ism43362_eS_wifi/cmds/ism43362.c
+++ project/ism43362_eS_wifi/cmds/ism43362.c
@@ -11,6 +11,9 @@
 
 #include <drivers/gpio.h>
 #include <drivers/spi.h>
+#include <config/board_config.h>
+#include "stm32l475e_iot01.h"
+
 
 #include "ism43362.h"
 
@@ -18,6 +21,8 @@
 #define WIFI_CHIP_SELECT()	do {gpio_set(GPIO_PORT_E, 1<<0, GPIO_PIN_LOW);} while(0)
 #define WIFI_CHIP_DESELECT()	do {gpio_set(GPIO_PORT_E, 1<<0, GPIO_PIN_HIGH);} while(0)
 #define WIFI_IS_CMDDATA_READY()	(gpio_get(GPIO_PORT_E, 1<<1) != 0)
+#define GPIO_MODE_AF   0x08
+
 
 static struct spi_device *spi_dev;
 
@@ -27,69 +32,20 @@ static inline int gpio_setup_out_mode(unsigned short port, gpio_mask_t pins, int
 	return ret;
 }
 
-#include "stm32l475e_iot01.h"
-static int SPI3_HAL_setup(void) {
-	// HAL based spi configuration
+static int SPI3_setup(void){
 
-/*	__HAL_RCC_SPI3_CLK_ENABLE();
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-	__HAL_RCC_GPIOC_CLK_ENABLE();
-	__HAL_RCC_GPIOE_CLK_ENABLE();
-*/
-	/* configure SPI pins */
-	GPIO_InitTypeDef GPIO_Init;
-	memset(&GPIO_Init, 0, sizeof(GPIO_Init));
-
-	/* configure SPI soft NSS pin for WiFi module */
-	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_0, GPIO_PIN_SET);
-	GPIO_Init.Pin       =  GPIO_PIN_0;
-	GPIO_Init.Mode      = GPIO_MODE_OUTPUT_PP;
-	GPIO_Init.Pull      = GPIO_NOPULL;
-	GPIO_Init.Speed     = GPIO_SPEED_FREQ_MEDIUM;
-	HAL_GPIO_Init(GPIOE, &GPIO_Init);
-
+ 	/* configure SPI soft NSS pin for WiFi module */
+	gpio_setup_out_mode(CONF_SPI3_PIN_CS_PORT, CONF_SPI3_PIN_CS_NR, GPIO_MODE_OUT_PUSH_PULL, GPIO_PIN_HIGH);
+	
 	/* configure SPI CLK pin */
-	GPIO_Init.Pin       =  GPIO_PIN_10;
-	GPIO_Init.Mode      = GPIO_MODE_AF_PP;
-	GPIO_Init.Pull      = GPIO_NOPULL;
-	GPIO_Init.Speed     = GPIO_SPEED_FREQ_MEDIUM;
-	GPIO_Init.Alternate = GPIO_AF6_SPI3;
-	HAL_GPIO_Init(GPIOC, &GPIO_Init);
-
+	gpio_setup_mode(CONF_SPI3_PIN_SCK_PORT, CONF_SPI3_PIN_SCK_NR, GPIO_MODE_ALT_SET(CONF_SPI3_PIN_SCK_AF) | GPIO_MODE_OUT_PUSH_PULL);
+	
 	/* configure SPI MISO pin */
-	GPIO_Init.Pin       = GPIO_PIN_11;
-	GPIO_Init.Mode      = GPIO_MODE_AF_PP;
-	GPIO_Init.Pull      = GPIO_PULLUP;
-	GPIO_Init.Speed     = GPIO_SPEED_FREQ_MEDIUM;
-	GPIO_Init.Alternate = GPIO_AF6_SPI3;
-	HAL_GPIO_Init(GPIOC,&GPIO_Init);
+	gpio_setup_mode(CONF_SPI3_PIN_MISO_PORT, CONF_SPI3_PIN_MISO_NR, GPIO_MODE_ALT_SET(CONF_SPI3_PIN_MISO_AF) | GPIO_MODE_OUT_PUSH_PULL);
 
 	/* configure SPI MOSI pin */
-	GPIO_Init.Pin       = GPIO_PIN_12;
-	GPIO_Init.Mode      = GPIO_MODE_AF_PP;
-	GPIO_Init.Pull      = GPIO_NOPULL;
-	GPIO_Init.Speed     = GPIO_SPEED_FREQ_MEDIUM;
-	GPIO_Init.Alternate = GPIO_AF6_SPI3;
-	HAL_GPIO_Init(GPIOC, &GPIO_Init);
+	gpio_setup_mode(CONF_SPI3_PIN_MOSI_PORT, CONF_SPI3_PIN_MOSI_NR, GPIO_MODE_ALT_SET(CONF_SPI3_PIN_MOSI_AF) | GPIO_MODE_OUT_PUSH_PULL);
 
-	/* SPI hardware init */
-/*	SPI_HandleTypeDef hspi;
-	memset(&hspi, 0, sizeof(hspi));
-	hspi.Instance = SPI3;
-	hspi.Init.Mode              = SPI_MODE_MASTER;
-	hspi.Init.Direction         = SPI_DIRECTION_2LINES;
-	hspi.Init.DataSize          = SPI_DATASIZE_16BIT;
-	hspi.Init.CLKPolarity       = SPI_POLARITY_LOW;
-	hspi.Init.CLKPhase          = SPI_PHASE_1EDGE;
-	hspi.Init.NSS               = SPI_NSS_SOFT;
-	hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8; // 80/8= 10MHz (Inventek WIFI module supports up to 20MHz)
-	hspi.Init.FirstBit          = SPI_FIRSTBIT_MSB;
-	hspi.Init.TIMode            = SPI_TIMODE_DISABLE;
-	hspi.Init.CRCCalculation    = SPI_CRCCALCULATION_DISABLE;
-	hspi.Init.CRCPolynomial     = 0;
-
-	for (int res = HAL_SPI_Init(&hspi); res != HAL_OK;) return res;
-*/
 	return 0;
 }
 
@@ -101,7 +57,7 @@ int ism43362_init() {
 	gpio_setup_out_mode(GPIO_PORT_C, 1<<9, GPIO_MODE_OUT_PUSH_PULL, GPIO_PIN_HIGH); // WiFi LED pin
 
 	// SPI3 HAL setup
-	for (int res = SPI3_HAL_setup(); res;) {return -10500;}
+	for (int res = SPI3_setup(); res;) {return -10500;}
 
 	// Get SPI device pointer
 	if (!(spi_dev = spi_dev_by_id(WIFI_SPI_BUS))){
@@ -131,12 +87,12 @@ int ism43362_init() {
 		if (spi_transfer(spi_dev, (uint8_t*)pad, (uint8_t*)in, 1) < 0) {
 			WIFI_CHIP_DESELECT(); 
 			return -10002;
-		} // SPI transfer error while reading initial prompt
-		
+		}
+
 		if (i > 5) {
 			WIFI_CHIP_DESELECT(); 
 			return -10003;
-		} // Too many characters while reading initial prompt
+		}
 	}
 	WIFI_CHIP_DESELECT();
 
@@ -212,7 +168,11 @@ int ism43362_exchange(char *txb, int txl, char *rxb, int rxl) {
 	}
 
 	// Ensure module readiness
-	for (clock_t t0 = clock(); !WIFI_IS_CMDDATA_READY(); usleep(10000)) if ((clock() - t0) > 10000) return -10009;			// No CMD/DATA_READY sign (no answer) for more than 10 s
+	for (clock_t t0 = clock(); !WIFI_IS_CMDDATA_READY(); usleep(40000)) {
+		if ((clock() - t0) > 10000){
+			return -10009;			// No CMD/DATA_READY sign (no answer) for more than 10 s
+		}
+	}
 
 	// Get an answer
 	WIFI_CHIP_SELECT();
