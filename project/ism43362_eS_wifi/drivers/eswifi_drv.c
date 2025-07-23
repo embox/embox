0acbf50af1307d19482694cf2403bd5220a24d58
Pechenovadasha <pechenkadasha250500@gmail.com>
2025-07-23 17:11:51 +0300

    just a hint

--- project/ism43362_eS_wifi/drivers/eswifi_drv.c
+++ project/ism43362_eS_wifi/drivers/eswifi_drv.c
@@ -10,6 +10,8 @@
 
 #include <errno.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
 #include <net/l2/ethernet.h>
 #include <net/l0/net_entry.h>
@@ -22,6 +24,25 @@
 #include <embox/unit.h>
 #include <framework/mod/options.h>
 
+#define WIFI_NAME "Embox2.4"
+#define WIFI_PASSWORD "pX6tsPeEN6"
+#define BUFFER_SIZE 2048
+
+typedef struct {
+    const char *command;
+	const char *command_value;
+	// const int command_len;
+} AT_Command;
+
+static const AT_Command wifi_setup_commands[] = {
+	{"C1=Embox2.4", WIFI_NAME},
+	{"C2=pX6tsPeEN6", WIFI_PASSWORD},
+	{"C3=4", "4"},
+	{"C4=1", "1"},
+	{"C0", NULL},
+	{NULL, NULL}
+};
+
 EMBOX_UNIT_INIT(eswifi_init);
 
 static int eswifi_xmit(struct net_device *dev, struct sk_buff *skb) {
@@ -29,6 +50,28 @@ static int eswifi_xmit(struct net_device *dev, struct sk_buff *skb) {
 }
 
 static int eswifi_open(struct net_device *dev) {
+	int wifi_request;
+	char rx_buffer[BUFFER_SIZE];
+	// char tx_buffer[256];
+	// buffer[BUFFER_SIZE - 1]=0;
+	log_info("Starting Wi-Fi initialization");
+	int result = 0;
+    if (result != 0) {
+        // printf("ISM43362 initialization error %d\n", result);
+        return -1;
+    } 	
+	if (result > 0){
+	for (int i = 0; wifi_setup_commands[i].command != NULL; i++){
+		wifi_request = ism43362_exchange((char *)wifi_setup_commands[i].command, strlen(wifi_setup_commands[i].command), rx_buffer, sizeof(rx_buffer));
+		// printf("Response (%d bytes): [", wifi_request);
+		if (wifi_request < 0) {
+			printf("ism43362_exchange error #%d\n", wifi_request);
+		}
+		usleep(100000);
+	}}
+	// gpio_set(2, 1 << 9, 1);
+
+	log_info("Wi-Fi initialization finished");
 	return 0;
 }
 
@@ -46,12 +89,11 @@ static const struct net_driver eswifi_drv_ops = {
 static int eswifi_init(void) {
 	struct net_device *nic;
 
-	nic = etherdev_alloc(0);
+	nic = etherdev_alloc(1);
 	if (NULL == nic) {
 		return -ENOMEM;
 	}
 
 	nic->drv_ops = &eswifi_drv_ops;
-
 	return inetdev_register_dev(nic);
 }
