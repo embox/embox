089def83aed60a6d1cd257e445342a88fc41e0ff
Pechenovadasha <pechenkadasha250500@gmail.com>
2025-07-24 17:44:24 +0300

    add setup ip and mac addr

--- project/ism43362_eS_wifi/drivers/eswifi_drv.c
+++ project/ism43362_eS_wifi/drivers/eswifi_drv.c
@@ -43,6 +43,7 @@ static const AT_Command wifi_setup_commands[] = {
 	{"C3=4\r", "4"},
 	{"C4=1\r", "1"},
 	{"C0\r", NULL},
+	{"Z5\r", NULL},
 	{NULL, NULL}
 };
 
@@ -55,6 +56,10 @@ static int eswifi_xmit(struct net_device *dev, struct sk_buff *skb) {
 static int eswifi_open(struct net_device *dev) {
 	int wifi_request;
 	char rx_buffer[BUFFER_SIZE];
+	char *ip;
+	uint32_t ip_num;
+	struct in_device *iface;
+	unsigned char mac_bytes[6];
 
 	log_info("Starting Wi-Fi initialization");
 	int result = ism43362_init();
@@ -68,12 +73,38 @@ static int eswifi_open(struct net_device *dev) {
 		if (wifi_request < 0) {
 			log_info("ism43362_exchange error");
 		}
+
 		log_info(wifi_setup_commands[i].command);
 		log_info(rx_buffer);
+		
+		if (wifi_setup_commands[i].command[0] == 'C' && wifi_setup_commands[i].command[1]== '0')
+		{
+			ip = strtok(rx_buffer, ",");
+			ip = strtok(NULL, ",");
+			if (ip){
+				ip_num = inet_addr(ip); 
+				iface = inetdev_get_by_dev(dev);
+				inetdev_set_addr(iface, ip_num);
+			}
+		} else if (wifi_setup_commands[i].command[0] == 'Z' && wifi_setup_commands[i].command[1] == '5') {
+			
+			if (sscanf(strchr(rx_buffer, '\n') + 1, 
+					"%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
+					&mac_bytes[0], &mac_bytes[1], &mac_bytes[2],
+					&mac_bytes[3], &mac_bytes[4], &mac_bytes[5]) == 6) {
+				
+				memcpy(dev->dev_addr, mac_bytes, 6);
+				log_info("MAC set");  
+			} else {
+				log_error("Invalid MAC format in response: %s", rx_buffer);
+				return -1;
+			}
+		}
 		memset(rx_buffer, '\0', BUFFER_SIZE);
 	}
 	
 	log_info("Wi-Fi initialization finished");
+	
 	return 0;
 }
 
