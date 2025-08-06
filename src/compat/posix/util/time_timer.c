020032b8b918b6fee50d98bd03608c6914e03f13
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2025-08-06 14:40:52 +0300

    compat: posix: Remove unused headers in time_timer.c

--- src/compat/posix/util/time_timer.c
+++ src/compat/posix/util/time_timer.c
@@ -7,16 +7,14 @@
  * @author Anton Bondarev
  */
 
-#include <errno.h>
 #include <time.h>
-#include <signal.h>
 
 int timer_create(clockid_t clockid, struct sigevent *evp, timer_t *timerid) {
 	return 0;
 }
 
-int timer_settime(timer_t timerid, int flags,
-				const struct itimerspec *value, struct itimerspec *ovalue) {
+int timer_settime(timer_t timerid, int flags, const struct itimerspec *value,
+    struct itimerspec *ovalue) {
 	return 0;
 }
 
