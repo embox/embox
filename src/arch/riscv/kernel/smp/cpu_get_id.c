af724e2e4db014c58acc1104e26354ef9463b66d
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2025-08-25 23:25:57 +0300

    riscv: Add missing riscv/smode.h inclusion

--- src/arch/riscv/kernel/smp/cpu_get_id.c
+++ src/arch/riscv/kernel/smp/cpu_get_id.c
@@ -11,6 +11,7 @@
 #include <asm/interrupts.h>
 #include <asm/ptrace.h>
 #include <asm/csr.h>
+#include <riscv/smode.h>
 
 #define INVALIDHARTID (unsigned int)(-1)
 static unsigned int __cpuid_to_hartid_map[NCPU] = {
