2077afe1a0d7076a91c8756cdc2e9d530c2c06d7
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-08-25 19:00:26 +0300

    fixup!: mdr1206_bsp_gpio.c

--- platform/milandr/mdr1206fi/mdr1206fi_platform_uart.c
+++ platform/milandr/mdr1206fi/mdr1206fi_platform_uart.c
@@ -1,28 +1,42 @@
 #include "rst_clk.h"
 #include "mdr_uart.h"
-#include "mdr_gpio.h"
+
 #include "mdr1206fi.h"
 
+#include <drivers/gpio.h>
+extern int clk_enable(char *clk_name);
 
 void UART_Config()
 {
+        UART_InitTypeDef UART_Print;
+ #if 0
     PORT_InitTypeDef UART_Pins = {
         .PORT_Pin       = PORT_PIN_0,
         .PORT_Mode      = PORT_MODE_DIGITAL,
-        .PORT_Direction = PORT_DIRECTION_OUTPUT,
+        .PORT_Direction = PORT_DIRECTION_INPUT,
         .PORT_Function  = PORT_FUNCTION_MAIN,
         .PORT_Power     = PORT_POWER_NOMINAL_UPTO_2mA,
         .PORT_PullUp = PORT_PULL_UP_OFF,
         .PORT_PullDown = PORT_PULL_DOWN_OFF,
     };
-    UART_InitTypeDef UART_Print;
 
-    RST_CLK_PCLKCmd(RST_CLK_PCLK_PORTB, ENABLE);
+
+    //RST_CLK_PCLKCmd(RST_CLK_PCLK_PORTB, ENABLE);
+    clk_enable("CLK_GPIOB");
 
     PORT_Init(MDR_PORTB, &UART_Pins);
     UART_Pins.PORT_Pin = PORT_PIN_1;
-    UART_Pins.PORT_Direction = PORT_DIRECTION_INPUT;
+    //UART_Pins.PORT_Direction = PORT_DIRECTION_INPUT;
     PORT_Init(MDR_PORTB, &UART_Pins);
+#endif
+
+	gpio_setup_mode(1,
+			(1 << 0),
+			GPIO_MODE_ALT_SET(1));
+
+	gpio_setup_mode(1,
+			(1 << 1),
+			GPIO_MODE_ALT_SET(1));
 
     RST_CLK_PER1_C2_ClkSelection(RST_CLK_PER1_C2_CLK_SRC_CPU_C1);
     RST_CLK_PER1_C2_SetPrescaler(RST_CLK_PER1_C2_UART1, RST_CLK_PER1_PRESCALER_DIV_1);
