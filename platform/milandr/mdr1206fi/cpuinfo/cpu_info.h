a7366fe8bc9f6b3f528bb3a53e631420efd56f04
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-08-27 14:59:39 +0300

    platform: Add platform/milandr/mdr1206fi/cpuinfo

---
+++ platform/milandr/mdr1206fi/cpuinfo/cpu_info.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Bondarev
+ * @date    05.08.2024
+ */
+
+#ifndef PLATFORM_MIK32_CPUINFO_H_
+#define PLATFORM_MIK32_CPUINFO_H_
+
+
+#endif /* PLATFORM_MIK32_CPUINFO_H_ */
