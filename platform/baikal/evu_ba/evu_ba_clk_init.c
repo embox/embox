18e5ae176cd1ef4cda8ab612c122467c25c6cf29
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-09-26 17:30:15 +0300

    platform: Start adding baikal/evu_ba

---
+++ platform/baikal/evu_ba/evu_ba_clk_init.c
@@ -0,0 +1,11 @@
+/**
+ * @file
+ *
+ * @date Sep 26, 2025
+ * @author Anton Bondarev
+ */
+
+
+
+void board_clocks_init(void) {
+}
---
+++ platform/baikal/evu_ba/platform_init.c
@@ -0,0 +1,17 @@
+/**
+ * @file
+ *
+ * @date Sep 26, 2025
+ * @author Anton Bondarev
+ */
+
+#include <hal/platform.h>
+#include <hal/reg.h>
+
+
+extern void board_clocks_init(void);
+
+void platform_init(void) {
+	board_clocks_init();
+
+}
