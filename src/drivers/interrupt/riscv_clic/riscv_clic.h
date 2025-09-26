a02d26ec4f2ea444dc1dc2fefbfc3f6f220fda50
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-09-26 17:36:25 +0300

    drivers: riscv_clic: Add vectored mode

--- src/drivers/interrupt/riscv_clic/riscv_clic.h
+++ src/drivers/interrupt/riscv_clic/riscv_clic.h
@@ -9,8 +9,10 @@
 #ifndef DRIVERS_INTERRUPT_RISCV_CLIC_H_
 #define DRIVERS_INTERRUPT_RISCV_CLIC_H_
 
-#ifdef __EMBOX_PLATFORM__mdr1206fi__
+#if defined(__EMBOX_PLATFORM__mdr1206fi__)
 #define __IRQCTRL_IRQS_TOTAL 47
+#elif defined(__EMBOX_PLATFORM__baikal_evu_ba__)
+#define __IRQCTRL_IRQS_TOTAL 75
 #endif
 
 #endif /* DRIVERS_INTERRUPT_RISCV_CLIC_H_ */
