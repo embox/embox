d091341c582473cd89c19c21281190aba7b3c008
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-10-07 13:05:28 +0300

    drivers: Improve drivers/spi/pl022

--- src/drivers/spi/pl022/pl022_spi0.c
+++ src/drivers/spi/pl022/pl022_spi0.c
@@ -15,22 +15,22 @@
 
 #include "pl022_spi.h"
 
-#define SPI_BUS_NUM        1
+#define SPI_BUS_NUM        0
 
 #define SPI_BASE_ADDR	  (uintptr_t)OPTION_GET(NUMBER,base_addr)
 
 #define SPI_BUS_NAME      MACRO_CONCAT(spibus,SPI_BUS_NUM)
 
-EMBOX_UNIT_INIT(pl022_spi1_init);
+EMBOX_UNIT_INIT(pl022_spi0_init);
 
-static struct pl022_spi pl022_spi1 = {
+static struct pl022_spi pl022_spi0 = {
 	.base_addr = SPI_BASE_ADDR,
 };
 
-static int pl022_spi1_init(void) {
-	pl022_spi_init(&pl022_spi1);
+static int pl022_spi0_init(void) {
+	pl022_spi_init(&pl022_spi0);
 
 	return 0;
 }
 
-SPI_CONTROLLER_DEF(SPI_BUS_NAME, &pl022_spi_ops, &pl022_spi1, SPI_BUS_NUM);
+SPI_CONTROLLER_DEF(SPI_BUS_NAME, &pl022_spi_ops, &pl022_spi0, SPI_BUS_NUM);
