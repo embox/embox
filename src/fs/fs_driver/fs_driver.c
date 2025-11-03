3f9360b73334f462f264c4c7c22df632df3972e4
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-11-03 18:07:42 +0300

    minor: fs: Add log_debug into fs/fs_driver

--- src/fs/fs_driver/fs_driver.c
+++ src/fs/fs_driver/fs_driver.c
@@ -6,6 +6,8 @@
  * @author Nikolay Korotky
  */
 
+#include <util/log.h>
+
 #include <string.h>
 
 #include <fs/fs_driver.h>
@@ -16,11 +18,14 @@ ARRAY_SPREAD_DEF(const struct fs_driver *const, fs_drivers_registry);
 const struct fs_driver *fs_driver_find(const char *name) {
 	const struct fs_driver *fs_drv;
 
+	log_debug("looking for %s there are %d drvs", name, ARRAY_SPREAD_SIZE(fs_drivers_registry));
 	array_spread_foreach(fs_drv, fs_drivers_registry) {
+		log_debug("checking %s", fs_drv->name);
 		if (!strcmp(name, fs_drv->name)) {
+			log_debug("found %s", fs_drv->name);
 			return fs_drv;
 		}
 	}
-
+	log_debug("did not find %s", name);
 	return NULL;
 }
