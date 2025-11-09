0d109e98cde7efd8574809aac373386b50ab4220
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2025-11-10 02:17:46 +0300

    third-party: Remove LIBGCC_FINDER

--- third-party/lib/toolchain/libstdcxx_toolchain/c++_headers_finder.sh
+++ third-party/lib/toolchain/libstdcxx_toolchain/c++_headers_finder.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-LIBGCC_FINDER=$1
-AWK=$2
+AWK=$1
 
-$LIBGCC_FINDER -Wp,-v -x c++ /dev/null -fsyntax-only 2>&1 >/dev/null | \
+${EMBOX_CROSS_COMPILE}gcc -Wp,-v -x c++ /dev/null -fsyntax-only 2>&1 >/dev/null | \
 	$AWK 'BEGIN { ORS = " " }; \
 	      /End of search list/ { found = 0 }; \
 	      found && /c\+\+/ {print "-I" $1}; \
