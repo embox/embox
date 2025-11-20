eb94154115c031cda46fc4b1a67b7873503f7bfa
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-11-20 15:16:09 +0300

    platform: Add platform/csky/sky_avega_lite

---
+++ platform/csky/templates/csky_avega_lite/board.conf.h
@@ -0,0 +1,13 @@
+#include <csky_avega_lite.conf.h>
+
+CONFIG {
+	uarts[1].status = ENABLED;
+	uarts[2].status = DISABLED;
+	uarts[3].status = DISABLED;
+	uarts[4].status = DISABLED;
+	uarts[5].status = DISABLED;
+	uarts[6].status = DISABLED;
+	
+	spis[1].status = ENABLED;
+	spis[2].status = DISABLED;
+}
