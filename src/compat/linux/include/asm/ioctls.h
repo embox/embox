775d07e336fe5a7066516ce25b098aba3847ca77
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2025-11-24 19:50:28 +0300

    compat: linux: Add asm/ioctls.h

---
+++ src/compat/linux/include/asm/ioctls.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 24.11.2026
+ * @author Aleksey Zhmulin
+ */
+
+#ifndef COMPAT_LINUX_ASM_IOCTLS_H_
+#define COMPAT_LINUX_ASM_IOCTLS_H_
+
+/* For compatibility with Linux */
+#include <sys/ioctl.h>
+
+#endif /* COMPAT_LINUX_ASM_IOCTLS_H_ */
