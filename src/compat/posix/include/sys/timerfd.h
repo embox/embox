bed0690a83b64742361400524aae2b4625b49498
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-12-02 12:34:45 +0300

    compat: Improve sys/timerfd.h

--- src/compat/posix/include/sys/timerfd.h
+++ src/compat/posix/include/sys/timerfd.h
@@ -11,9 +11,23 @@
 
 #include <time.h>
 
+#include <sys/cdefs.h>
+
+/* Bits to be set in the FLAGS parameter of `timerfd_create'.  */
+enum {
+	TFD_CLOEXEC = 02000000,
+#define TFD_CLOEXEC TFD_CLOEXEC
+	TFD_NONBLOCK = 04000
+#define TFD_NONBLOCK TFD_NONBLOCK
+};
+
+
+/* Bits to be set in the FLAGS parameter of `timerfd_settime'.  */
 #define TFD_TIMER_ABSTIME (1 << 0)
 #define TFD_TIMER_CANCEL_ON_SET (1 << 1)
 #define TFD_SETTIME_FLAGS (TFD_TIMER_ABSTIME | TFD_TIMER_CANCEL_ON_SET)
+
+__BEGIN_DECLS
 
 extern int timerfd_create(int clockid, int flags);
 
@@ -22,5 +36,7 @@ extern int timerfd_settime(int fd, int flags,
 	struct itimerspec *old_value);
 
 extern int timerfd_gettime(int fd, struct itimerspec *curr_value);
+
+__END_DECLS
 
 #endif /* SYS_TIMERFD_H  */
