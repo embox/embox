b56abb648b6e1fccf6beca49fe45197167a3c373
Ruslan Nafikov <ruslan.nafikov.05@bk.ru>
2025-12-02 12:37:02 +0300

    project/usb_dev: Add templates for nucleo_f429zi
    Signed-off-by: Ruslan Nafikov <ruslan.nafikov.05@bk.ru>

---
+++ project/usb_dev/f4/nucleo_f429zi/clock_config.c
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Kozlov
+ * @date    30.10.2014
+ */
+
+#include <bsp/stm32cube_hal.h>
+
+extern void stm32_sysclock_init(void);
+
+void SystemClock_Config(void) {
+	/* Enable Power Control clock */
+	__HAL_RCC_PWR_CLK_ENABLE();
+
+	/* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	
+	stm32_sysclock_init();
+
+	__HAL_FLASH_PREFETCH_BUFFER_ENABLE();
+}
