f49dfb3f2164a863b4684ba9a5c1c005f56b24a4
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-01-12 16:49:28 +0300

    tests: Concatenate 2 version of wcslen_test

--- src/tests/wchar/wcslen_test.c
+++ src/tests/wchar/wcslen_test.c
@@ -1,7 +1,8 @@
 /**
- * @file wcslen_test.c
+ * @file
  *
- * @date July 1, 2022
+ * @date Feb 20, 2021
+ * @author Vedant Paranjape
  * @author Dmitry Lopatinsky
  */
 
@@ -9,7 +10,25 @@
 #include <wchar.h>
 #include <wctype.h>
 
-EMBOX_TEST_SUITE("wcslen test suite");
+EMBOX_TEST_SUITE("test suit for wcslen()");
+
+TEST_CASE("get length with wcslen()") {
+	wchar_t str[] = L"covid19sucks";
+	size_t len;
+
+	len = sizeof(str) / 4 - 1;
+
+	test_assert_equal(len, wcslen(str));
+}
+
+TEST_CASE("use wcsnlen() with maxlen less than string length") {
+	wchar_t str[] = L"covid19sucks";
+	size_t len;
+
+	len = sizeof(str) / 4 - 1;
+
+	test_assert_equal(len, wcsnlen(str, len));
+}
 
 TEST_CASE("normal string") {
 	wchar_t str[] = L"abcde";
