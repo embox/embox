d799458106f7a38e1773799efe99ff877301533c
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-01-14 17:58:31 +0300

    complat: Add stub for compat/posix/signal/tests/sigwait_test

---
+++ src/compat/posix/signal/tests/sigwait_test.c
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Bondarev
+ * @date    14.01.2026
+ */
+
+#include <embox/test.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+
+EMBOX_TEST_SUITE("signal/sigwait");
+
+TEST_CASE("signal/sigwait ") {
+
+}
