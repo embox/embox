14fe81e6919ff5a8aadb117d99e060d66b0c25fe
Efim <es.perevalov@mail.ru>
2026-01-19 13:50:23 +0300

    drivers: Use direct addresses instead of structure for clock/esp32c3_systimer
    Signed-off-by: Efim <es.perevalov@mail.ru>

--- src/drivers/clock/esp32c3_systimer/esp32c3_systimer.c
+++ src/drivers/clock/esp32c3_systimer/esp32c3_systimer.c
@@ -12,8 +12,16 @@
 
 #include <framework/mod/options.h>
 
-#define BASE_ADDR       OPTION_GET(NUMBER,base_addr)
-#define IRQ_NUM         OPTION_GET(NUMBER,irq_num)
+#define RTC_FREQ		OPTION_GET(NUMBER, rtc_freq)
+#define BASE_ADDR       OPTION_GET(NUMBER, base_addr)
+#define IRQ_NUM         OPTION_GET(NUMBER, irq_num)
+
+#define SYSTIMER_CONF			(BASE_ADDR + 0x000)
+#define SYSTIMER_TARGET0_CONF	(BASE_ADDR + 0x034)
+#define SYSTIMER_TARGET0_HI		(BASE_ADDR + 0x01c)
+#define SYSTIMER_TARGET0_LO		(BASE_ADDR + 0x020)
+#define SYSTIMER_COMP0_LOAD		(BASE_ADDR + 0x050)
+#define SYSTIMER_INT_ENA		(BASE_ADDR + 0x064)
 
 struct esp32_systimer_val64 {
 	volatile uint32_t hi;
@@ -68,15 +76,22 @@ struct esp32_systimer_regs {
 	uint32_t reserved_0f4;
 	uint32_t reserved_0f8;
 	volatile uint32_t date;                                  /* 0x070 */
-} __attribute__((packed));
+} /*__attribute__((packed))*/;
+
+// static struct esp32_systimer_regs *ESP32_SYSTIMER = (void *)(uintptr_t)BASE_ADDR;
 
 static int esp32c3_systimer_set_periodic(struct clock_source *cs) {
-	volatile struct esp32_systimer_regs *esp32_systimer = (void *)BASE_ADDR;
+	// REG32_STORE(&ESP32_SYSTIMER->target_conf[0], 0);
+	REG32_STORE(SYSTIMER_TARGET0_CONF, 0);
 
-	REG32_STORE(&esp32_systimer->target_conf[0], 0);
-	REG32_ORIN(&esp32_systimer->target_conf[0], 16000000);
-	REG32_ORIN(&esp32_systimer->comp_load[0], 1);
-	REG32_ORIN(&esp32_systimer->target_conf[0], 1 << 30);
+	// REG32_ORIN(&ESP32_SYSTIMER->target_conf[0], RTC_FREQ);
+	REG32_ORIN(SYSTIMER_TARGET0_CONF, RTC_FREQ/10000);
+
+	// REG32_ORIN(&ESP32_SYSTIMER->comp_load[0], 1);
+	REG32_ORIN(SYSTIMER_COMP0_LOAD, 1);
+
+	// REG32_ORIN(&ESP32_SYSTIMER->target_conf[0], 1 << 30);
+	REG32_ORIN(SYSTIMER_TARGET0_CONF, 1 << 30);
 
 	return 0;
 }
@@ -100,11 +115,11 @@ static struct time_counter_device esp32c3_systimer_cd = {
 */
 
 static irq_return_t esp32c3_systimer_irq_handler(unsigned int irq_nr,
-														void *data) {													
-	volatile struct esp32_systimer_regs *esp32_systimer = (void *)BASE_ADDR;
+														void *data) {
 
 	//systimer_ll_clear_alarm_int(systimer_hal.dev, SYSTIMER_ALARM_OS_TICK_CORE0);
-	REG32_ORIN(&esp32_systimer->int_ena, 1);
+	// REG32_ORIN(&ESP32_SYSTIMER->int_ena, 1);
+	REG32_ORIN(SYSTIMER_INT_ENA, 1);
 	
 	clock_tick_handler(data);
 
@@ -119,37 +134,45 @@ static int esp32c3_systimer_init(struct clock_source *cs) {
 		return 0;
 	}
 
-	volatile struct esp32_systimer_regs *esp32_systimer = (void *)BASE_ADDR;
-
 	// systimer_ll_enable_clock(hal->dev, true);
-	REG32_ORIN(&esp32_systimer->conf,  1 << 31);
+	// REG32_ORIN(&ESP32_SYSTIMER->conf,  (uint32_t)(1 << 31));
+	REG32_ORIN(SYSTIMER_CONF,  (uint32_t)(1 << 31));
 
 	// systimer_ll_connect_alarm_counter(hal->dev, alarm_id, counter_id);
-	REG32_STORE(&esp32_systimer->target_conf[0], 1 << 31);
+	// REG32_STORE(&ESP32_SYSTIMER->target_conf[0], 1 << 31);
+	REG32_STORE(SYSTIMER_TARGET0_CONF, 1 << 31);
 
 	// systimer_ll_enable_counter(hal->dev, counter_id, true);
-	REG32_ORIN(&esp32_systimer->conf,  1 << 30);
+	// REG32_ORIN(&ESP32_SYSTIMER->conf,  1 << 30);
+	REG32_ORIN(SYSTIMER_CONF,  1 << 30);
 
 	// systimer_ll_counter_can_stall_by_cpu(hal->dev, counter_id, cpu_id, can);
-	REG32_ORIN(&esp32_systimer->conf,  1 << 26);
+	//REG32_ORIN(&ESP32_SYSTIMER->conf,  1 << 26);
+	REG32_ORIN(SYSTIMER_CONF,  1 << 26);
 
 	// systimer_ll_enable_alarm(systimer_hal.dev, SYSTIMER_ALARM_OS_TICK_CORE0, false);
-	REG32_ANDIN(&esp32_systimer->conf, ~(1 << 24));
+	// REG32_ANDIN(&ESP32_SYSTIMER->conf, ~(1 << 24));
+	REG32_ANDIN(SYSTIMER_CONF, ~(1 << 24));
 
 	// systimer_ll_set_alarm_target(systimer_hal.dev, SYSTIMER_ALARM_OS_TICK_CORE0, alarm.val);
 	// esp32_systimer->target_val[0].hi = alarm.val >> 32;
-	REG32_STORE(&esp32_systimer->target_val[0].hi, 0);
+	// REG32_STORE(&ESP32_SYSTIMER->target_val[0].hi, 0);
+	REG32_STORE(SYSTIMER_TARGET0_HI, 0);
     // esp32_systimer->target_val[0].low = alarm.val & 0xFFFFFFFF;
-	REG32_STORE(&esp32_systimer->target_val[0].low, 0);
+	// REG32_STORE(&ESP32_SYSTIMER->target_val[0].low, 0);
+	REG32_STORE(SYSTIMER_TARGET0_LO, 0);
 
 	// systimer_ll_apply_alarm_value(systimer_hal.dev, SYSTIMER_ALARM_OS_TICK_CORE0);
-	REG32_STORE(&esp32_systimer->comp_load[0], 1);
+	// REG32_STORE(&ESP32_SYSTIMER->comp_load[0], 1);
+	REG32_STORE(SYSTIMER_COMP0_LOAD, 1);
 
 	// systimer_ll_enable_alarm(systimer_hal.dev, SYSTIMER_ALARM_OS_TICK_CORE0, true);
-	REG32_ORIN(&esp32_systimer->conf,  1 << 24);
+	// REG32_ORIN(&ESP32_SYSTIMER->conf,  1 << 24);
+	REG32_ORIN(SYSTIMER_CONF,  1 << 24);
 
 	// systimer_ll_enable_alarm_int(systimer_hal.dev, SYSTIMER_ALARM_OS_TICK_CORE0, true);
-	REG32_ORIN(&esp32_systimer->int_ena, 1);
+	// REG32_ORIN(&ESP32_SYSTIMER->int_ena, 1);
+	REG32_ORIN(SYSTIMER_INT_ENA, 1);
 
 	return 0;
 }
