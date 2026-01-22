9e3935a845d31ce8c3938574423c074acbea7350
Dmitry Pilyuk <dmitriipiliuk@gmail.com>
2026-01-22 12:05:45 +0300

    third_party/lib/openssl: Improve OpenSSL to 3.6.0

--- third-party/lib/openssl3/in_ext.h
+++
@@ -1,8 +0,0 @@
-#include <netinet/in.h>
-
-struct in_pktinfo
-  {
-    int ipi_ifindex;			/* Interface index  */
-    struct in_addr ipi_spec_dst;	/* Routing destination address  */
-    struct in_addr ipi_addr;		/* Header destination address  */
-  };
