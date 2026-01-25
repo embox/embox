b264a3c2f548f8f43d4469ab9447ebfab052f091
Dmitry Pilyuk <dmitriipiliuk@gmail.com>
2026-01-25 17:08:36 +0300

    third_party/rt-tests: Add dependencies

--- third-party/rt-tests/numa.h
+++ third-party/rt-tests/numa.h
@@ -1,29 +1,53 @@
 #ifndef _NUMA_H
-#define _NUMA_H 
+#define _NUMA_H
+
+#include <stddef.h>
+#include <sys/types.h>
 
 struct bitmask {
 	unsigned long size; /* number of bits in the map */
 	unsigned long *maskp;
 };
 
-int numa_available(void);
-unsigned int numa_bitmask_weight(const struct bitmask *);
-int numa_bitmask_isbitset(const struct bitmask *, unsigned int);
-struct bitmask *numa_allocate_cpumask(void);
-struct bitmask *numa_bitmask_clearbit(struct bitmask *, unsigned int);
-void numa_bitmask_free(struct bitmask *);
-struct bitmask *numa_parse_cpustring_all(const char *);
+static inline int numa_available(void) {
+	return -1;
+}
+static inline unsigned int numa_bitmask_weight(const struct bitmask *bm) {
+	return 0;
+}
+static inline int numa_bitmask_isbitset(const struct bitmask *bm, unsigned int n) {
+	return 0;
+}
+static inline struct bitmask *numa_allocate_cpumask(void) {
+	return NULL;
+}
+static inline struct bitmask *numa_bitmask_clearbit(struct bitmask *bm,
+    unsigned int n) {
+	return NULL;
+}
+static inline void numa_bitmask_free(struct bitmask *bm) {
+}
+static inline struct bitmask *numa_parse_cpustring_all(const char *s) {
+	return NULL;
+}
+static inline void *numa_alloc_onnode(size_t size, int node) {
+	return NULL;
+}
+static inline void numa_free(void *mem, size_t size) {
+}
+static inline int numa_run_on_node(int node) {
+	return 0;
+}
 
-void *numa_alloc_onnode(size_t size, int node);
+static inline int numa_node_of_cpu(int cpu) {
+	return 0;
+}
 
-void numa_free(void *mem, size_t size);
-
-int numa_run_on_node(int node);
-
-int numa_node_of_cpu(int cpu);
-
-int numa_sched_getaffinity(pid_t, struct bitmask *);
-int numa_sched_setaffinity(pid_t, struct bitmask *);
-
+static inline int numa_sched_getaffinity(pid_t pid, struct bitmask *bm) {
+	return 0;
+}
+static inline int numa_sched_setaffinity(pid_t pid, struct bitmask *bm) {
+	return 0;
+}
 
 #endif
