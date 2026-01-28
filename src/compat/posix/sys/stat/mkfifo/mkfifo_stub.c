49ebd2803d83d0f0884d832b4e082772dbb87df2
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-01-28 16:04:50 +0300

    compat: Add mkfifo() stub

---
+++ src/compat/posix/sys/stat/mkfifo/mkfifo_stub.c
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 28.01.2026
+ * @author Anton Bondarev
+ */
+
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int mkfifo(const char *path, mode_t mode) {
+	return 0;
+}
