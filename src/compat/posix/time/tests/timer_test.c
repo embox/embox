602bcc6f1512b4af148b76ae1b9de4711ad00e56
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-02-02 16:05:56 +0300

    compat: Work on posix/time/tests/timer_test

--- src/compat/posix/time/tests/timer_test.c
+++ src/compat/posix/time/tests/timer_test.c
@@ -15,10 +15,18 @@
 EMBOX_TEST_SUITE("time/timer_*");
 
 static void timer_handler(int sig, siginfo_t *si, void *uc) {
+    int overrun_count;
+    timer_t  *tidp;
+
+    tidp = si->si_value.sival_ptr;
     printf("Timer expired\n");
+
+    overrun_count = timer_getoverrun(*tidp);
+    printf("Timer expired overrun (%d)\n", overrun_count);
+    //signal(sig, SIG_IGN);
 }
 
-TEST_CASE("time/timer_") {
+TEST_CASE("time/call timer_handler") {
     timer_t timerid;
     struct sigevent sev;
     struct itimerspec its;
@@ -27,22 +35,59 @@ TEST_CASE("time/timer_") {
     sa.sa_flags = SA_SIGINFO;
     sa.sa_sigaction = timer_handler;
     sigemptyset(&sa.sa_mask);
-    sigaction(SIGRTMIN, &sa, NULL);
+    test_assert(-1 != sigaction(SIGRTMIN, &sa, NULL));
 
     sev.sigev_notify = SIGEV_SIGNAL;
     sev.sigev_signo = SIGRTMIN;
     sev.sigev_value.sival_ptr = &timerid;
-    timer_create(CLOCK_REALTIME, &sev, &timerid);
+    test_assert(-1 != timer_create(CLOCK_REALTIME, &sev, &timerid));
 
     /* Arm the timer (start 1s, repeat every 1s) */
     its.it_value.tv_sec = 1;
     its.it_value.tv_nsec = 0;
     its.it_interval.tv_sec = 1;
     its.it_interval.tv_nsec = 0;
-    timer_settime(timerid, 0, &its, NULL);
+    test_assert(-1 !=timer_settime(timerid, 0, &its, NULL));
 
     sleep(5);
 
     timer_delete(timerid);
+
+}
+
+
+TEST_CASE("time/sigprocmask") {
+    timer_t timerid;
+    sigset_t mask;
+    struct sigevent sev;
+    struct itimerspec its;
+    struct sigaction sa;
+
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = timer_handler;
+    sigemptyset(&sa.sa_mask);
+    test_assert(-1 != sigaction(SIGRTMIN, &sa, NULL));
+
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGRTMIN);
+    test_assert(-1 != sigprocmask(SIG_SETMASK, &mask, NULL));
+
+    sev.sigev_notify = SIGEV_SIGNAL;
+    sev.sigev_signo = SIGRTMIN;
+    sev.sigev_value.sival_ptr = &timerid;
+    test_assert(-1 != timer_create(CLOCK_REALTIME, &sev, &timerid));
+
+    /* Arm the timer (start 1s, repeat every 1s) */
+    its.it_value.tv_sec = 1;
+    its.it_value.tv_nsec = 0;
+    its.it_interval.tv_sec = 1;
+    its.it_interval.tv_nsec = 0;
+    test_assert(-1 != timer_settime(timerid, 0, &its, NULL));
+
+    sleep(5);
+
+    test_assert(-1 != sigprocmask(SIG_UNBLOCK, &mask, NULL));
+
+    test_assert(-1 != timer_delete(timerid));
 
 }
