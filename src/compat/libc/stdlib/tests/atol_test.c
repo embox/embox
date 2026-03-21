e2002d75c6febcc29d29ccdfc2eb010a9192698c
preetham v <preetham.vish@proton.me>
2026-03-21 14:59:43 +0530

    fix: correct atol_test macro syntax

--- src/compat/libc/stdlib/tests/atol_test.c
+++ src/compat/libc/stdlib/tests/atol_test.c
@@ -14,7 +14,7 @@ EMBOX_TEST_SUITE("stdlib/atol");
 TEST_CASE("Check atol() function") { 
     test_assert_equal(0, atol("0")); 
     test_assert_equal(123, atol("+123")); 
-    test_assert_equal(-123, atol("-123"); 
+    test_assert_equal(-123, atol("-123")); 
     test_assert_equal(123, atol("00123")); 
     test_assert_equal(123, atol("    123")); 
     test_assert_equal(123, atol("123junk")); 
