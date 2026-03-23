dd7ef01cee37ca6cf678f758949079699ae830b5
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-03-23 14:53:03 +0300

    drivers: Add drivers/i2c/i2c_dev.h

--- src/drivers/i2c/i2c.h
+++ src/drivers/i2c/i2c.h
@@ -9,5 +9,6 @@
 #define DRIVERS_I2C_H_
 
 #include <drivers/i2c/i2c_bus.h>
+#include <drivers/i2c/i2c_dev.h>
 
 #endif /* DRIVERS_I2C_H_ */
---
+++ src/drivers/i2c/i2c_dev.h
@@ -0,0 +1,24 @@
+/**
+ * @file
+ *
+ * @date Nov 16, 2018
+ * @author Anton Bondarev
+ */
+
+#ifndef DRIVERS_I2C_DEV_H_
+#define DRIVERS_I2C_DEV_H_
+
+#include <sys/cdefs.h>
+
+struct i2c_dev {
+	int i2cd_bus;
+	int i2cd_addr;
+};
+
+__BEGIN_DECLS
+
+extern int i2c_dev_probe(const struct i2c_dev *dev);
+
+__END_DECLS
+
+#endif /* DRIVERS_I2C_DEV_H_ */
