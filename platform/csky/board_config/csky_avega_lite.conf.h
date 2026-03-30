9e877e4af05408b5c19792f92d119116ce8267a1
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-03-30 19:40:16 +0300

    board_config: Update platform/csky/board_config/csky_avega_lite.conf.h

--- platform/csky/board_config/csky_avega_lite.conf.h
+++ platform/csky/board_config/csky_avega_lite.conf.h
@@ -80,12 +80,10 @@ struct uart_conf uarts[] = {
 				VAL("", 37),
 			},
 			.pins = {
-				PIN("TX", PB, PIN_6, AF7),
-				PIN("RX", PB, PIN_7, AF7),
+				PIN("TX", GPIO_PORT_B, 6, AF7),
+				PIN("RX", GPIO_PORT_B, 7, AF7),
 			},
 			.clocks = {
-				VAL("TX",   CLK_GPIOB),
-				VAL("RX",   CLK_GPIOB),
 				VAL("UART", CLK_USART1),
 			}
 		},
@@ -103,8 +101,8 @@ struct uart_conf uarts[] = {
 				VAL("", 38),
 			},
 			.pins = {
-				PIN("TX", PA, PIN_2, AF7),
-				PIN("RX", PA, PIN_3, AF7),
+				PIN("TX", GPIO_PORT_A, 2, AF7),
+				PIN("RX", GPIO_PORT_A, 3, AF7),
 			},
 			.clocks = {
 				VAL("UART", CLK_USART2),
@@ -124,8 +122,8 @@ struct uart_conf uarts[] = {
 				VAL("", 39),
 			},
 			.pins = {
-				PIN("TX", PD, PIN_8, AF7),
-				PIN("RX", PD, PIN_9, AF7),
+				PIN("TX", GPIO_PORT_D, 8, AF7),
+				PIN("RX", GPIO_PORT_D, 9, AF7),
 			},
 			.clocks = {
 				VAL("UART", CLK_USART3),
@@ -145,8 +143,8 @@ struct uart_conf uarts[] = {
 				VAL("", 52),
 			},
 			.pins = {
-				PIN("TX", PA, PIN_0, AF8),
-				PIN("RX", PA, PIN_1, AF8),
+				PIN("TX", GPIO_PORT_A, 0, AF8),
+				PIN("RX", GPIO_PORT_A, 1, AF8),
 			},
 			.clocks = {
 				VAL("UART", CLK_UART4),
@@ -166,8 +164,8 @@ struct uart_conf uarts[] = {
 				VAL("", 53),
 			},
 			.pins = {
-				PIN("TX", PC, PIN_12, AF8),
-				PIN("RX", PD, PIN_2, AF8),
+				PIN("TX", GPIO_PORT_C, 12, AF8),
+				PIN("RX", GPIO_PORT_D, 2, AF8),
 			},
 			.clocks = {
 				VAL("UART", CLK_UART5),
@@ -187,8 +185,8 @@ struct uart_conf uarts[] = {
 				VAL("", 71),
 			},
 			.pins = {
-				PIN("TX", PG, PIN_14, AF8),
-				PIN("RX", PG, PIN_9, AF8),
+				PIN("TX", GPIO_PORT_G, 14, AF8),
+				PIN("RX", GPIO_PORT_G, 9, AF8),
 			},
 			.clocks = {
 				VAL("UART", CLK_USART6),
@@ -208,8 +206,8 @@ struct uart_conf uarts[] = {
 				VAL("", 82),
 			},
 			.pins = {
-				PIN("TX", PE, PIN_8, AF8),
-				PIN("RX", PE, PIN_7, AF8),
+				PIN("TX", GPIO_PORT_E, 8, AF8),
+				PIN("RX", GPIO_PORT_E, 7, AF8),
 			},
 			.clocks = {
 				VAL("UART", CLK_UART7),
@@ -229,8 +227,8 @@ struct uart_conf uarts[] = {
 				VAL("", 83),
 			},
 			.pins = {
-				PIN("TX", PE, PIN_1, AF8),
-				PIN("RX", PE, PIN_0, AF8),
+				PIN("TX", GPIO_PORT_E, 1, AF8),
+				PIN("RX", GPIO_PORT_E, 0, AF8),
 			},
 			.clocks = {
 				VAL("UART", CLK_UART8),
@@ -309,7 +307,7 @@ struct spi_conf spis[] = {
 
 struct i2c_conf i2cs[] = {
 	[1] = {
-		.status = DISABLED,
+		.status = ENABLED,
 		.name = "I2C1",
 		.dev = {
 			.name = "I2C1",
@@ -321,8 +319,8 @@ struct i2c_conf i2cs[] = {
 				VAL("ERROR", 32),
 			},
 			.pins = {
-				PIN("SCL", GPIO_PORT_B, PIN_8, AF4),
-				PIN("SDA", GPIO_PORT_B, PIN_9, AF4),
+				PIN("SCL", GPIO_PORT_B, 8, AF4),
+				PIN("SDA", GPIO_PORT_B, 9, AF4),
 			},
 			.clocks = {
 				VAL("I2C", CLK_I2C1),
@@ -330,7 +328,7 @@ struct i2c_conf i2cs[] = {
 		},
 	},
 	[2] = {
-		.status = DISABLED,
+		.status = ENABLED,
 		.name = "I2C2",
 		.dev = {
 			.name = "I2C2",
@@ -342,8 +340,8 @@ struct i2c_conf i2cs[] = {
 				VAL("ERROR", 34),
 			},
 			.pins = {
-				PIN("SCL", GPIO_PORT_F, PIN_0, AF4),
-				PIN("SDA", GPIO_PORT_F, PIN_1, AF4),
+				PIN("SCL", GPIO_PORT_F, 0, AF4),
+				PIN("SDA", GPIO_PORT_F, 1, AF4),
 			},
 			.clocks = {
 				VAL("I2C", CLK_I2C2),
@@ -363,8 +361,8 @@ struct i2c_conf i2cs[] = {
 				VAL("ERROR", 73),
 			},
 			.pins = {
-				PIN("SCL", GPIO_PORT_H, PIN_4, AF4),
-				PIN("SDA", GPIO_PORT_H, PIN_5, AF4),
+				PIN("SCL", GPIO_PORT_H, 4, AF4),
+				PIN("SDA", GPIO_PORT_H, 5, AF4),
 			},
 			.clocks = {
 				VAL("I2C", CLK_I2C3),
