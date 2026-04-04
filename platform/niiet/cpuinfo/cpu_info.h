1bcc74d192e1c6de22ee7f47e8a839250822ae0d
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-04-04 18:46:16 +0300

    platfrom: Add niiet/cpuinfo

---
+++ platform/niiet/cpuinfo/cpu_info.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Bondarev
+ * @date    05.08.2024
+ */
+
+#ifndef PLATFORM_NIIET_SOC_CPUINFO_H_
+#define PLATFORM_NIIET_SOC_CPUINFO_H_
+
+
+#endif /* PLATFORM_NIIET_SOC_CPUINFO_H_ */
