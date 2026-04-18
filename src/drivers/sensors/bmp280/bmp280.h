26e7775d3dedccecb03f7505cd04cda44d194fd7
Efim <es.perevalov@mail.ru>
2026-04-18 14:43:32 +0300

    drivers: Add pressure measurement for sensors/bmp280
    Signed-off-by: Efim <es.perevalov@mail.ru>

--- src/drivers/sensors/bmp280/bmp280.h
+++ src/drivers/sensors/bmp280/bmp280.h
@@ -12,8 +12,8 @@
 #include <stdint.h>
 
 extern int bmp280_init(void);
-extern int16_t bmp280_get_temperature(void);
-extern int16_t bmp280_get_humidity(void);
-extern int16_t bmp280_get_pressure(void);
+extern int32_t bmp280_get_temperature(void);
+extern int32_t bmp280_get_humidity(void);
+extern int32_t bmp280_get_pressure(void);
 
 #endif /* BMP280_H_ */
