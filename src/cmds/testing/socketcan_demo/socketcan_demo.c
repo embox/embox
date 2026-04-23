561b9d48f6b6f0dae2e7744710145173261e8cba
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-04-23 20:36:30 +0300

    cmds: Work on testing/socketcan_demo

--- src/cmds/testing/socketcan_demo/socketcan_demo.c
+++ src/cmds/testing/socketcan_demo/socketcan_demo.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <linux/can.h>
 //#include <linux/can/raw.h>
+#include <util/math.h>
 
 int main(void) {
     int s;
