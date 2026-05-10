3b5f333715fbc7673a3a8fba87d639b1e074fa07
Ruslan Nafikov <ruslan.nafikov.05@bk.ru>
2026-05-10 17:17:56 +0300

    lvgl: add missing v9 port files and patch-9.2.2.txt
    Signed-off-by: Ruslan Nafikov <ruslan.nafikov.05@bk.ru>

---
+++ third-party/lib/lvgl/lvgl_port_v9.h
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * @brief LVGL porting layer (LVGL v9)
+ *
+ * @date 27.04.2026
+ * @author Ruslan Nafikov
+ */
+#ifndef THIRD_PARTY_LIB_LVGL_LVGL_PORT_V9_H_
+#define THIRD_PARTY_LIB_LVGL_LVGL_PORT_V9_H_
+
+#include "lvgl.h"
+
+/* Display */
+extern int lvgl_port_fbdev_init(const char *fb_path);
+extern void lvgl_port_fbdev_flush(lv_display_t *disp, const lv_area_t *area,
+        uint8_t *px_map);
+
+/* Input device */
+extern int lvgl_port_input_dev_init(const char *path);
+extern void lvgl_port_input_dev_read(lv_indev_t *indev, lv_indev_data_t *data);
+extern void lvgl_port_mouse_handle(void);
+extern void lvgl_port_touchscreen_handle(void);
+
+#endif /* THIRD_PARTY_LIB_LVGL_LVGL_PORT_V9_H_ */
