9056759e9e6056de6bebfd0d723fa35eca4cb395
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2026-06-08 16:49:57 +0300

    cmds: Improve candump

--- src/cmds/hardware/can_utils/candump/candump.c
+++ src/cmds/hardware/can_utils/candump/candump.c
@@ -10,21 +10,30 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(void) {
-	struct can_frame frame;
+int main(int argc, char **argv) {
+	struct canfd_frame frame;
 	int fd;
 
-	fd = open("/dev/can0", O_RDWR);
-
-	if (read(fd, &frame, sizeof(struct can_frame)) != sizeof(struct can_frame)) {
-		printf("read failed\n");
-		return -1;
+	if (argc == 2) {
+		fd = open(argv[1], O_RDWR);
+	}
+	else {
+		fd = open("/dev/can0", O_RDWR);
 	}
 
-	printf("Recv: can_id = 0x%08x, len=%d\n"
-	       "data(%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x)\n",
-	    frame.can_id, frame.len, frame.data[0], frame.data[1], frame.data[2],
-	    frame.data[3], frame.data[4], frame.data[5], frame.data[6], frame.data[7]);
+	while (1) {
+		if (read(fd, &frame, sizeof(struct canfd_frame))
+		    != sizeof(struct canfd_frame)) {
+			printf("read failed\n");
+			return -1;
+		}
+
+		printf("Recv: can_id = 0x%08x, len=%d\n"
+		       "data(%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x)\n",
+		    frame.can_id, frame.len, frame.data[0], frame.data[1],
+		    frame.data[2], frame.data[3], frame.data[4], frame.data[5],
+		    frame.data[6], frame.data[7]);
+	}
 
 	close(fd);
 
