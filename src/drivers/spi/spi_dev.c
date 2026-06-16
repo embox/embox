e5d50bba58bde0fa4f17be61a4c65ab0a6845299
AlekseyZhmulin <ZhmulinAleksey@yandex.ru>
2026-06-16 19:48:58 +0300

    spi_dev: Add 'flags' param

--- src/drivers/spi/spi_dev.c
+++ src/drivers/spi/spi_dev.c
@@ -14,7 +14,7 @@
 #include <drivers/spi.h>
 #include <util/log.h>
 
-static ssize_t spi_read(struct char_dev *cdev, void *buf, size_t nbyte) {
+static ssize_t spi_read(struct char_dev *cdev, void *buf, size_t nbyte, int flags) {
 	struct spi_device *dev;
 
 	assert(cdev);
@@ -27,7 +27,8 @@ static ssize_t spi_read(struct char_dev *cdev, void *buf, size_t nbyte) {
 	return nbyte;
 }
 
-static ssize_t spi_write(struct char_dev *cdev, const void *buf, size_t nbyte) {
+static ssize_t spi_write(struct char_dev *cdev, const void *buf, size_t nbyte,
+    int flags) {
 	struct spi_device *dev;
 
 	assert(cdev);
@@ -56,8 +57,7 @@ static int spi_ioctl(struct char_dev *cdev, int request, void *data) {
 		break;
 	case SPI_IOCTL_TRANSFER:
 		transfer_arg = data;
-		spi_transfer(dev, transfer_arg->in, transfer_arg->out,
-		    transfer_arg->count);
+		spi_transfer(dev, transfer_arg->in, transfer_arg->out, transfer_arg->count);
 		break;
 	case SPI_IOCTL_CS_MODE:
 		dev->spid_flags = (int)(intptr_t)data;
