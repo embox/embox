689535299a724ce90ae537f1fb2a2580a370bed4
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-07-17 20:04:28 +0300

    drivers: Work on can/niiet_can

--- src/drivers/can/niiet_can/niiet_can.c
+++ src/drivers/can/niiet_can/niiet_can.c
@@ -38,12 +38,105 @@ EMBOX_UNIT_INIT(niiet_can_init);
 #define CAN_REG_ANDIN(reg, mask) REG32_ANDIN(CAN_BASE_ADDR + reg, mask)
 #define CAN_REG_CLEAR(reg, mask) REG32_CLEAR(CAN_BASE_ADDR + reg, mask)
 
+static inline void niiet_can_mo_move_to_list(struct niiet_can_regs *regs,
+    							int idx, int list) {
+	regs->PANCTR = (CAN_PANCTR_PANCMD_STAT_INC << CAN_PANCTR_PANCMD_Pos)
+	               | (idx << CAN_PANCTR_PANAR1_Pos)
+	               | (list << CAN_PANCTR_PANAR2_Pos);
+
+	while (regs->PANCTR & (CAN_PANCTR_BUSY | CAN_PANCTR_RBUSY)) {};
+}
+
+static inline int niiet_can_get_free_mo(struct niiet_can_dev_priv *priv, int node) {
+	struct niiet_node_desc *node_desc;
+	int i;
+
+	node_desc = &priv->node_desc[node];
+	for (i = 0; i < NIIET_MO_TX_MAX; i++) {
+		if (0 != (node_desc->tx_mo_array[i].flags & NIIET_MO_DESC_USED)) {
+			return node_desc->tx_mo_array[i].num;
+		}
+	}
+
+	return -1;
+}
+
+static inline void niiet_can_mo_conf_tx(struct niiet_canmsg_regs *canmsg_regs,
+    			int obj_num, struct can_frame *frame) {
+	uint32_t flags = 0;
+
+	if (frame->can_id & CAN_RTR_FLAG) {
+		flags = CANMSG_MOCTR_RESDIR;
+	}
+	else {
+		flags = CANMSG_MOCTR_SETDIR;
+	}
+	flags |= CANMSG_MOCTR_SETTXEN0 | CANMSG_MOCTR_SETTXEN1;
+
+	canmsg_regs->Msg[obj_num].MOCTR = flags;
+	canmsg_regs->Msg[obj_num].MOFCR = CANMSG_MOFCR_DLC(frame->len); /* CANMSG_MOFCR_TXIE */
+
+	if (frame->can_id & CAN_EFF_FLAG) {
+		flags = CANMSG_MOAR_ID_EFF(frame->can_id & CAN_EFF_MASK);
+		flags |= CANMSG_MOAR_IDE;
+	} else {
+		flags = CANMSG_MOAR_ID_SFF(frame->can_id & CAN_SFF_MASK);
+
+	}
+	flags |= CANMSG_MOAR_PRI(0x2); /* by identifier + IDE + DIR */
+	canmsg_regs->Msg[obj_num].MOAR = flags;
+
+	/* for FIFO canmsg_regs->Msg[obj_num].MOFGPR */
+	/* with IRQ canmsg_regs->Msg[obj_num].MOIPR */
+
+	flags  = frame->data[0];
+	flags |= frame->data[1] << 8;
+	flags |= frame->data[2] << 16;
+	flags |= frame->data[3] << 24;
+	canmsg_regs->Msg[obj_num].MODATAL = flags;
+
+	flags  = frame->data[4];
+	flags |= frame->data[5] << 8;
+	flags |= frame->data[6] << 16;
+	flags |= frame->data[7] << 24;
+	canmsg_regs->Msg[obj_num].MODATAH = flags;
+
+}
+
+static inline void niiet_can_mo_conf_rx(struct niiet_canmsg_regs *canmsg_regs,
+					int obj_num, struct can_frame *frame) {
+	uint32_t flags = 0;
+
+	if (frame->can_id & CAN_RTR_FLAG) {
+		flags = CANMSG_MOCTR_SETDIR;
+	}
+	else {
+		flags = CANMSG_MOCTR_RESDIR;
+	}
+	flags |= CANMSG_MOCTR_SETRXEN;
+
+	canmsg_regs->Msg[obj_num].MOCTR = flags;
+	/* canmsg_regs->Msg[obj_num].MOAMR */
+}
+
+
+static void niiet_can_mo_send(struct niiet_canmsg_regs *canmsg_regs, int obj_num) {
+    canmsg_regs->Msg[obj_num].MOCTR = CANMSG_MOCTR_SETTXRQ | CANMSG_MOCTR_SETMSGVAL;
+}
+
+#if 0
+void CAN_Object_Receive(uint32_t obj_num)
+{
+    CANMSG->Msg[obj_num].MOCTR = CANMSG_Msg_MOCTR_SETMSGVAL_Msk;
+}
+#endif
+
 static void niiet_can_config(struct can_dev *can) {
 	uint32_t reg;
 
 	CAN_REG_STORE(CAN_CLC, 0);
 
-	while (CAN_REG_LOAD(CAN_CLC) & CAN_CLC_DISS) {}
+	while (CAN_REG_LOAD(CAN_CLC) & CAN_CLC_DISS) {}	
 
 	reg = CAN_REG_LOAD(CAN_FDR);
 	reg = FIELD_SET(reg, CAN_FDR_STEP, 0x3ff);
@@ -55,6 +148,9 @@ static inline int niiet_can_node_init(struct can_dev *can, int node_num) {
 	struct niiet_can_dev_priv *priv;
 	struct niiet_can_regs *regs;
 	struct niiet_can_node_regs *node;
+	struct niiet_node_desc *node_desc;
+	struct niiet_mo_desc *mo_desc;
+	int i;
 
 	priv = can->priv;
 	regs = priv->regs;
@@ -64,8 +160,6 @@ static inline int niiet_can_node_init(struct can_dev *can, int node_num) {
 	/* write regs NBTRx & NPCRx */
 	node->NBTR = 2305;/* Tbit = 1 / (1 Мбит/с) */
 
-
-
 	node->NIPR = (1 << CAN_NODE_NIPR_TRINP_Pos);
 
 	//for loopback mode
@@ -73,6 +167,23 @@ static inline int niiet_can_node_init(struct can_dev *can, int node_num) {
 
 	/* lock writing & IRQ EN*/
 	node->NCR = CAN_NODE_NCR_TRIE;
+
+	/* first MO set as RX (0) than as TX (0) than  set node 1 */
+	node_desc = &priv->node_desc[0];
+
+	for (i = 0; i < (NIIET_MO_RX_MAX); i++) {
+		mo_desc = &node_desc->rx_mo_array[i];
+		mo_desc->flags = NIIET_MO_DESC_INITED;
+		mo_desc->num   = i; 
+		niiet_can_mo_move_to_list(regs, i, CAN_NODE_LIST_ID(0));
+	}
+
+	for (i = 0; i < (NIIET_MO_RX_MAX + NIIET_MO_TX_MAX) ; i++) {
+		mo_desc = &node_desc->tx_mo_array[i];
+		mo_desc->flags = NIIET_MO_DESC_INITED;
+		mo_desc->num   = i; 
+		niiet_can_mo_move_to_list(regs, i, CAN_NODE_LIST_ID(0));
+	}
 
 	return 0;
 }
@@ -90,8 +201,31 @@ static void niiet_can_close(struct can_dev *can) {
 }
 
 static int niiet_can_send(struct can_dev *can, const void *data) {
+	struct can_frame *frame;
+	struct niiet_can_dev_priv *priv;
+	int mo_idx;
+	uint8_t frame_len;
+
+	priv = can->priv;
+	frame = (struct can_frame *)data;
+
+	frame_len = frame->len;
+	if (frame_len > 8) {
+		frame_len = 8;
+	}
+
+	mo_idx = niiet_can_get_free_mo(can->priv, 0);
+	if (mo_idx == -1) {
+		log_error("couldn't get free MO");
+		return -1;
+	}
+
+	niiet_can_mo_conf_tx(priv->canmsg_regs, mo_idx, frame);
+
+	niiet_can_mo_send(priv->canmsg_regs, mo_idx);
+
 	return 0;
-}
+}	
 
 static const struct can_dev_ops niiet_can_ops = {
     .cdo_config = niiet_can_config,
--- src/drivers/can/niiet_can/niiet_can.h
+++ src/drivers/can/niiet_can/niiet_can.h
@@ -12,6 +12,17 @@
 
 #include <config/board_config.h>
 
+
+/* hardware */
+#define NIIET_CAN_NODE_MAX        2
+#define NIIET_CAN_MSG_MAX       128
+
+/* software */
+#define NIIET_MO_RX_MAX          16
+#define NIIET_MO_TX_MAX          16
+
+#define CAN_NODE_LIST_ID(node)   (node + 1)
+
 /* Global Module Registers */
 #define CAN_CLC     0x000 /* Clock Control Register */
 #define CAN_ID      0x008 /* Module Identification Register */
@@ -87,13 +98,13 @@ struct niiet_can_node_regs {
 #define CAN_NODE_NCR_CCE_Pos    6
 #define CAN_NODE_NCR_CALM_Pos   7
 
-#define CAN_NODE_NCR_INIT       (1 << CAN_NODE_NCR_INIT_Pos)
-#define CAN_NODE_NCR_TRIE       (1 << CAN_NODE_NCR_TRIE_Pos)
-#define CAN_NODE_NCR_LECIE      (1 << CAN_NODE_NCR_LECIE_Pos)
-#define CAN_NODE_NCR_ALIE       (1 << CAN_NODE_NCR_ALIE_Pos)
-#define CAN_NODE_NCR_CANDIS     (1 << CAN_NODE_NCR_CANDIS_Pos)
-#define CAN_NODE_NCR_CCE        (1 << CAN_NODE_NCR_CCE_Pos)
-#define CAN_NODE_NCR_CALM       (1 << CAN_NODE_NCR_CALM_Pos)
+#define CAN_NODE_NCR_INIT   (1 << CAN_NODE_NCR_INIT_Pos)
+#define CAN_NODE_NCR_TRIE   (1 << CAN_NODE_NCR_TRIE_Pos)
+#define CAN_NODE_NCR_LECIE  (1 << CAN_NODE_NCR_LECIE_Pos)
+#define CAN_NODE_NCR_ALIE   (1 << CAN_NODE_NCR_ALIE_Pos)
+#define CAN_NODE_NCR_CANDIS (1 << CAN_NODE_NCR_CANDIS_Pos)
+#define CAN_NODE_NCR_CCE    (1 << CAN_NODE_NCR_CCE_Pos)
+#define CAN_NODE_NCR_CALM   (1 << CAN_NODE_NCR_CALM_Pos)
 
 /* NBTR  */
 #define CAN_NODE_NBTR_BRP_Pos   0  /*!< Prescaler rate */
@@ -109,7 +120,7 @@ struct niiet_can_node_regs {
 #define CAN_NODE_NIPR_CFCINP_Pos 12
 
 /* Bit field positions: */
-#define CAN_NODE_NPCR_LBM_Pos    8
+#define CAN_NODE_NPCR_LBM_Pos 8
 
 struct niiet_can_regs {
 	volatile uint32_t CLC;
@@ -128,10 +139,28 @@ struct niiet_can_regs {
 	volatile uint32_t MCR;
 	volatile uint32_t MITR;
 	volatile uint32_t Reserved5[12];
-	struct niiet_can_node_regs Node[2];
+	struct niiet_can_node_regs Node[NIIET_CAN_NODE_MAX];
 };
 
-struct niiet_canmsg_regs {
+/* PANCTR */
+#define CAN_PANCTR_PANCMD_Pos 0
+#define CAN_PANCTR_BUSY_Pos   8
+#define CAN_PANCTR_RBUSY_Pos  9
+#define CAN_PANCTR_PANAR1_Pos 16
+#define CAN_PANCTR_PANAR2_Pos 24
+
+#define CAN_PANCTR_BUSY       (1 << CAN_PANCTR_BUSY_Pos)
+#define CAN_PANCTR_RBUSY      (1 << CAN_PANCTR_RBUSY_Pos)
+
+#define CAN_PANCTR_PANCMD_INIT       0x01
+#define CAN_PANCTR_PANCMD_STAT_INC   0x02
+#define CAN_PANCTR_PANCMD_DYN_INC    0x03
+#define CAN_PANCTR_PANCMD_MOV_UP     0x04
+#define CAN_PANCTR_PANCMD_INS_UP     0x05
+#define CAN_PANCTR_PANCMD_MOV_DOWN   0x06
+#define CAN_PANCTR_PANCMD_INS_DOWN   0x07
+
+struct niiet_canmsg_msg_regs {
 	volatile uint32_t MOFCR;
 	volatile uint32_t MOFGPR;
 	volatile uint32_t MOIPR;
@@ -142,11 +171,118 @@ struct niiet_canmsg_regs {
 	volatile uint32_t MOCTR; /* MOSTAT*/
 };
 
+struct niiet_canmsg_regs {
+	struct niiet_canmsg_msg_regs Msg[NIIET_CAN_MSG_MAX];
+};
+
+#define CANMSG_MOCTR_RESRXPND_Pos  0
+#define CANMSG_MOCTR_RESTXPND_Pos  1
+#define CANMSG_MOCTR_RESRXUPD_Pos  2
+#define CANMSG_MOCTR_RESNEWDAT_Pos 3
+#define CANMSG_MOCTR_RESMSGLST_Pos 4
+#define CANMSG_MOCTR_RESMSGVAL_Pos 5
+#define CANMSG_MOCTR_RESRTSEL_Pos  6
+#define CANMSG_MOCTR_RESRXEN_Pos   7
+#define CANMSG_MOCTR_RESTXRQ_Pos   8
+#define CANMSG_MOCTR_RESTXEN0_Pos  9
+#define CANMSG_MOCTR_RESTXEN1_Pos  10
+#define CANMSG_MOCTR_RESDIR_Pos    11
+#define CANMSG_MOCTR_SETRXPND_Pos  16
+#define CANMSG_MOCTR_SETTXPND_Pos  17
+#define CANMSG_MOCTR_SETRXUPD_Pos  18
+#define CANMSG_MOCTR_SETNEWDAT_Pos 19
+#define CANMSG_MOCTR_SETMSGLST_Pos 20
+#define CANMSG_MOCTR_SETMSGVAL_Pos 21
+#define CANMSG_MOCTR_SETRTSEL_Pos  22
+#define CANMSG_MOCTR_SETRXEN_Pos   23
+#define CANMSG_MOCTR_SETTXRQ_Pos   24
+#define CANMSG_MOCTR_SETTXEN0_Pos  25
+#define CANMSG_MOCTR_SETTXEN1_Pos  26
+#define CANMSG_MOCTR_SETDIR_Pos    27
+
+#define CANMSG_MOCTR_RESRXPND    (1 << CANMSG_MOCTR_RESRXPND_Pos )
+#define CANMSG_MOCTR_RESTXPND    (1 << CANMSG_MOCTR_RESTXPND_Pos )
+#define CANMSG_MOCTR_RESRXUPD    (1 << CANMSG_MOCTR_RESRXUPD_Pos )
+#define CANMSG_MOCTR_RESNEWDAT   (1 << CANMSG_MOCTR_RESNEWDAT_Pos)
+#define CANMSG_MOCTR_RESMSGLST   (1 << CANMSG_MOCTR_RESMSGLST_Pos)
+#define CANMSG_MOCTR_RESMSGVAL   (1 << CANMSG_MOCTR_RESMSGVAL_Pos)
+#define CANMSG_MOCTR_RESRTSEL    (1 << CANMSG_MOCTR_RESRTSEL_Pos )
+#define CANMSG_MOCTR_RESRXEN     (1 << CANMSG_MOCTR_RESRXEN_Pos  )
+#define CANMSG_MOCTR_RESTXRQ     (1 << CANMSG_MOCTR_RESTXRQ_Pos  )
+#define CANMSG_MOCTR_RESTXEN0    (1 << CANMSG_MOCTR_RESTXEN0_Pos )
+#define CANMSG_MOCTR_RESTXEN1    (1 << CANMSG_MOCTR_RESTXEN1_Pos )
+#define CANMSG_MOCTR_RESDIR      (1 << CANMSG_MOCTR_RESDIR_Pos   )
+#define CANMSG_MOCTR_SETRXPND    (1 << CANMSG_MOCTR_SETRXPND_Pos )
+#define CANMSG_MOCTR_SETTXPND    (1 << CANMSG_MOCTR_SETTXPND_Pos )
+#define CANMSG_MOCTR_SETRXUPD    (1 << CANMSG_MOCTR_SETRXUPD_Pos )
+#define CANMSG_MOCTR_SETNEWDAT   (1 << CANMSG_MOCTR_SETNEWDAT_Pos)
+#define CANMSG_MOCTR_SETMSGLST   (1 << CANMSG_MOCTR_SETMSGLST_Pos)
+#define CANMSG_MOCTR_SETMSGVAL   (1 << CANMSG_MOCTR_SETMSGVAL_Pos)
+#define CANMSG_MOCTR_SETRTSEL    (1 << CANMSG_MOCTR_SETRTSEL_Pos )
+#define CANMSG_MOCTR_SETRXEN     (1 << CANMSG_MOCTR_SETRXEN_Pos  )
+#define CANMSG_MOCTR_SETTXRQ     (1 << CANMSG_MOCTR_SETTXRQ_Pos  )
+#define CANMSG_MOCTR_SETTXEN0    (1 << CANMSG_MOCTR_SETTXEN0_Pos )
+#define CANMSG_MOCTR_SETTXEN1    (1 << CANMSG_MOCTR_SETTXEN1_Pos )
+#define CANMSG_MOCTR_SETDIR      (1 << CANMSG_MOCTR_SETDIR_Pos   )
+
+#define CANMSG_MOFCR_MMC_Pos   0
+#define CANMSG_MOFCR_GDFS_Pos  8
+#define CANMSG_MOFCR_IDC_Pos   9
+#define CANMSG_MOFCR_DLCC_Pos  10
+#define CANMSG_MOFCR_DATC_Pos  11
+#define CANMSG_MOFCR_RXIE_Pos  16
+#define CANMSG_MOFCR_TXIE_Pos  17
+#define CANMSG_MOFCR_OVIE_Pos  18
+#define CANMSG_MOFCR_FRREN_Pos 20
+#define CANMSG_MOFCR_RMM_Pos   21
+#define CANMSG_MOFCR_SDT_Pos   22
+#define CANMSG_MOFCR_STT_Pos   23
+#define CANMSG_MOFCR_DLC_Pos   24
+
+#define CANMSG_MOFCR_MMC(mmc) (mmc << CANMSG_MOFCR_MMC_Pos)
+#define CANMSG_MOFCR_GDFS     (1 << CANMSG_MOFCR_GDFS_Pos)
+#define CANMSG_MOFCR_IDC      (1 << CANMSG_MOFCR_IDC_Pos)
+#define CANMSG_MOFCR_DLCC     (1 << CANMSG_MOFCR_DLCC_Pos)
+#define CANMSG_MOFCR_DATC     (1 << CANMSG_MOFCR_DATC_Pos)
+#define CANMSG_MOFCR_RXIE     (1 << CANMSG_MOFCR_RXIE_Pos)
+#define CANMSG_MOFCR_TXIE     (1 << CANMSG_MOFCR_TXIE_Pos)
+#define CANMSG_MOFCR_OVIE     (1 << CANMSG_MOFCR_OVIE_Pos)
+#define CANMSG_MOFCR_FRREN    (1 << CANMSG_MOFCR_FRREN_Pos)
+#define CANMSG_MOFCR_RMM      (1 << CANMSG_MOFCR_RMM_Pos)
+#define CANMSG_MOFCR_SDT      (1 << CANMSG_MOFCR_SDT_Pos)
+#define CANMSG_MOFCR_STT      (1 << CANMSG_MOFCR_STT_Pos)
+#define CANMSG_MOFCR_DLC(dlc) (dlc << CANMSG_MOFCR_DLC_Pos)
+
+#define CANMSG_MOAR_ID_EFF_Pos  0
+#define CANMSG_MOAR_ID_SFF_Pos  18
+#define CANMSG_MOAR_IDE_Pos     29
+#define CANMSG_MOAR_PRI_Pos     30
+
+#define CANMSG_MOAR_ID_EFF(id) (id << CANMSG_MOAR_ID_EFF_Pos)
+#define CANMSG_MOAR_ID_SFF(id) (id << CANMSG_MOAR_ID_SFF_Pos)
+#define CANMSG_MOAR_IDE        (1 << CANMSG_MOAR_IDE_Pos)
+#define CANMSG_MOAR_PRI(pri)   (pri << CANMSG_MOAR_PRI_Pos)
+
+#define NIIET_MO_DESC_INITED   (1 << 0)
+#define NIIET_MO_DESC_USED     (1 << 1)
+
+struct niiet_mo_desc {
+	int num;
+	uint32_t flags;
+};
+
+struct niiet_node_desc {
+	struct niiet_mo_desc rx_mo_array[NIIET_MO_RX_MAX];
+	struct niiet_mo_desc tx_mo_array[NIIET_MO_TX_MAX];
+};
+
 struct niiet_can_dev_priv {
-    struct can_dev *can_dev;
-    struct niiet_can_regs *regs;
-    struct niiet_canmsg_regs *canmsg_regs;
-    int can_node_max;
+	struct can_dev *can_dev;
+	struct niiet_can_regs *regs;
+	struct niiet_canmsg_regs *canmsg_regs;
+	int can_node_max;
+
+	struct niiet_node_desc node_desc[NIIET_CAN_NODE_MAX];
 };
 
 extern void niiet_can_bconf_init(struct niiet_can_dev_priv *niiet_can);
