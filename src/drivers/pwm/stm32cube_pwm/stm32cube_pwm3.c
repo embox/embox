5ea761bbc0726f7851bd997974364f56f2b875b0
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-08-04 17:21:12 +0300

    drivers: Work on PWM with DMA

--- src/drivers/pwm/stm32cube_pwm/stm32cube_pwm3.c
+++ src/drivers/pwm/stm32cube_pwm/stm32cube_pwm3.c
@@ -90,6 +90,8 @@
                 PWM_CHAN3_MASK_AVAIL \
             )
 
+#define PWM_DMAS    (NULL)
+
 static const struct pin_description pwm_pin_desc[STM_PWM_CHAN_MAX] = {
     {
         .pd_port = PWM_CHAN0_PORT,
@@ -126,4 +128,5 @@ static struct stm32cube_pwm_priv PWM_DEV_PRIV_STRUCT_NAME = {
 
 PWM_DEV_DEF(PWM_DEV_ID, &stm32cube_pwm_ops, &PWM_DEV_PRIV_STRUCT_NAME,
                         &pwm_pin_desc[0], PWM_BASE_ADDR,
-                        PWM_AVAIL_MASK, STM_PWM_CHAN_MAX);
+                        PWM_AVAIL_MASK, STM_PWM_CHAN_MAX,
+                    PWM_DMAS);
