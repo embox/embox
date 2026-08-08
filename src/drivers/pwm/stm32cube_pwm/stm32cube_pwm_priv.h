670892adcdb1fae7c4d58b86ae0a4abbb13b41d3
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-08-08 18:27:03 +0300

    drivers: Work on PWM's dma

--- src/drivers/pwm/stm32cube_pwm/stm32cube_pwm_priv.h
+++ src/drivers/pwm/stm32cube_pwm/stm32cube_pwm_priv.h
@@ -11,6 +11,8 @@
 
 #include <stdint.h>
 
+#include <kernel/irq.h>
+
 #include <drivers/pwm.h> /* only for struct pin_description  */
 
 #include <bsp/stm32cube_hal.h>
@@ -33,5 +35,7 @@ struct stm32cube_pwm_priv {
 	TIM_HandleTypeDef             TimHandle;
 	TIM_OC_InitTypeDef            sConfig;
 };
+
+extern irq_return_t stm32cube_pwm_dma_irq_handler(unsigned int irq_num, void *priv) ;
 
 #endif /* DRIVERS_PWM_STM32CUBE_PWM_PRIV_H_ */
