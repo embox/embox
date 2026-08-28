814f1c027dd9078fa5da90c5cf49811fd969f045
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-08-28 16:36:39 +0300

    drivers: Work on pwm/stm32cube_pwm

--- src/drivers/pwm/stm32cube_pwm/stm32cube_pwm_priv.h
+++ src/drivers/pwm/stm32cube_pwm/stm32cube_pwm_priv.h
@@ -19,10 +19,26 @@
 
 #include <config/board_config.h>
 
+#define STM32CUBE_PWM_DMA_BUF_SIZE    (32)
 
-#define STM_PWM_CHAN_MAX    (4)
+#define STM_PWM_CHAN_MAX              (4)
 
 #define STM32CUBE_PWM_WRONG_CHAN  (0xFFFFFFF)
+
+#define STM32CUBE_PWM_DMA_NUM_OFF     (0)
+#define STM32CUBE_PWM_DMA_STREAM_OFF  (8)
+#define STM32CUBE_PWM_DMA_CHAN_OFF    (16)
+#define STM32CUBE_PWM_DMA_EN_OFF      (31)
+
+#define STM32CUBE_PWM_DMA_EN(desc)    \
+				(desc & (1 << STM32CUBE_PWM_DMA_EN_OFF))
+
+#define STM32CUBE_PWM_DMA_NUM(desc)    \
+				((desc >> STM32CUBE_PWM_DMA_NUM_OFF) & 0xFF)
+#define STM32CUBE_PWM_DMA_CHAN(desc)   \
+				((desc >> STM32CUBE_PWM_DMA_CHAN_OFF) & 0xFF)
+#define STM32CUBE_PWM_DMA_STREAM(desc) \
+				((desc >> STM32CUBE_PWM_DMA_STREAM_OFF) & 0xFF)
 
 struct stm32cube_pwm_priv {
 	const struct pin_description *pin_desc;
@@ -34,6 +50,8 @@ struct stm32cube_pwm_priv {
 
 	TIM_HandleTypeDef             TimHandle;
 	TIM_OC_InitTypeDef            sConfig;
+	DMA_HandleTypeDef             dma_handle[STM_PWM_CHAN_MAX];
+	uint32_t                      dma_buffer[STM_PWM_CHAN_MAX][STM32CUBE_PWM_DMA_BUF_SIZE];
 };
 
 extern irq_return_t stm32cube_pwm_dma_irq_handler(unsigned int irq_num, void *priv) ;
