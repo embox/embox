dffd264e8ef53b06ca8fc3b70e9d9316116491ee
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-09-26 17:41:15 +0300

    drivers: Improve pwm/skeleton_pwm

--- src/drivers/pwm/skeleton_pwm/skeleton_pwm_drv.c
+++ src/drivers/pwm/skeleton_pwm/skeleton_pwm_drv.c
@@ -61,9 +61,11 @@ static int skeleton_pwm_set_duty_array(struct pwm_device *dev, int chan_num,
 	return 0;
 }
 
-struct pwm_ops skeleton_pwm_ops = {.pwmo_init = skeleton_pwm_init,
+struct pwm_ops skeleton_pwm_ops = {
+	.pwmo_init = skeleton_pwm_init,
     .pwmo_set_period = skeleton_pwm_set_period,
     .pwmo_set_duty = skeleton_pwm_set_duty,
     .pwmo_set_duty_array = skeleton_pwm_set_duty_array,
     .pwmo_enable = skeleton_pwm_enable,
-    .pwmo_disable = skeleton_pwm_disable};
+    .pwmo_disable = skeleton_pwm_disable
+};
